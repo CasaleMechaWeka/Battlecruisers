f1e54fd761148138d562cdb7479dd9a8cd52a3c7
Felix Fei Mann <the_man@Felixs-Mac-mini.local>
2017-02-16 21:39:51 -0800

    Camera moves smoothly between cruisers :)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,42 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CameraController : MonoBehaviour, ICameraController 
 {
+	private Vector3 _cameraVelocity = Vector3.zero;
+	private CameraPosition _cameraPosition;
+	private Vector3 _cameraTarget;
+
 	public GameObject friendlyCruiser;
 	public GameObject enemyCruiser;
-
-	public CameraPosition CameraPosition { get; private set; }
+	
+	public Transform target;
+	public float smoothTime = 0.3F;
 
 	void Start() 
 	{
-		FocusOnFriendlyCruiser();	
+		_cameraPosition = CameraPosition.Unitialized;
+		MoveCameraGradually(CameraPosition.FriendlyCruiser);
 	}
 
 	public void FocusOnFriendlyCruiser()
 	{
-		if (CameraPosition != CameraPosition.FriendlyCruiser)
-		{
-			Vector3 cameraPosition = transform.position;
-			cameraPosition.x = friendlyCruiser.transform.position.x;
-			transform.position = cameraPosition;
-			CameraPosition = CameraPosition.FriendlyCruiser;
-		}
+		MoveCameraGradually(CameraPosition.FriendlyCruiser);
 	}
 
 	public void FocusOnEnemyCruiser()
 	{
-		if (CameraPosition != CameraPosition.EnemyCruiser)
-		{
-			Vector3 cameraPosition = transform.position;
-			cameraPosition.x = enemyCruiser.transform.position.x;
-			transform.position = cameraPosition;
-			CameraPosition = CameraPosition.EnemyCruiser;
-		}
+		MoveCameraGradually(CameraPosition.EnemyCruiser);
 	}
 
 	public void ShowFullMapView()
 	{
+		throw new NotImplementedException();
+	}
+
+	void Update()
+	{
+		if (transform.position != _cameraTarget)
+		{
+			transform.position = Vector3.SmoothDamp(transform.position, _cameraTarget, ref _cameraVelocity, smoothTime);
+		}
+	}
+
+	private void MoveCameraGradually(CameraPosition cameraDestination)
+	{
+		if (cameraDestination != _cameraPosition)
+		{
+			_cameraTarget = transform.position;
+
+			switch (cameraDestination)
+			{
+				case CameraPosition.FriendlyCruiser:
+					_cameraTarget.x = friendlyCruiser.transform.position.x;
+					break;
+				case CameraPosition.EnemyCruiser:
+					_cameraTarget.x = enemyCruiser.transform.position.x;
+					break;
+				default:
+					throw new ArgumentException();
+			}
+
+			_cameraPosition = cameraDestination;
+		}
 	}
 }
