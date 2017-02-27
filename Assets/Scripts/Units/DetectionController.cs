1f76866b47fcb045c1e3520f627311c01471c074
the_mann <felix.fei.mann@gmail.com>
2017-02-26 17:25:11 -0800

    Simplified DetectionController again :P

--- Assets/Scripts/Units/DetectionController.cs
+++ Assets/Scripts/Units/DetectionController.cs
@@ -9,27 +9,16 @@ public interface IDetectionController
 	Action<Collider2D> OnExited { set; }
 }
 
-// FELIX  Assume we can only encouter one collider at a time for now :P
 public class DetectionController : MonoBehaviour, IDetectionController
 {
-	private Collider2D _collider;
-
 	public Action<Collider2D> OnEntered { set; private get; }
 	public Action<Collider2D> OnExited { set; private get; }
-	public Action<Collider2D> OnColliderDestroyed { set; private get; }
 
 	public int Radius { private get; set; }
 
 	void OnTriggerEnter2D(Collider2D collider)
 	{
 		Debug.Log("DetectionController.OnTriggerEnter2D()");
-
-		if (_collider != null)
-		{
-			throw new InvalidOperationException();
-		}
-
-		_collider = collider;
 
 		if (OnEntered != null)
 		{
@@ -41,21 +30,9 @@ public class DetectionController : MonoBehaviour, IDetectionController
 	{
 		Debug.Log("DetectionController.OnTriggerExit2D()");
 
-		if (_collider != collider)
-		{
-			throw new InvalidOperationException();
-		}
-
-		_collider = null;
-
 		if (OnExited != null)
 		{
 			OnExited(collider);
 		}
-	}
-
-	void Update () 
-	{
-//		if (_collider 		
 	}
 }
