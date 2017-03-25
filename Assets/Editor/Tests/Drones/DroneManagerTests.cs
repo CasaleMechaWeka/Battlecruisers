488eeca96ddf8fbd488f739c01d44f2e9a9a63d6
the_mann <felix.fei.mann@gmail.com>
2017-03-24 21:43:40 -0700

    Added some tests for the DroneManager :)

--- Assets/Editor/Tests/Drones/DroneManagerTests.cs
+++ Assets/Editor/Tests/Drones/DroneManagerTests.cs
@@ -1,24 +1,66 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using NUnit.Framework;
+using BattleCruisers.Drones;
+using UnityAsserts = UnityEngine.Assertions;
 
 namespace BattleCruisers.Tests.Drones
 {
 	[TestFixture]
 	public class DroneManagerTests 
 	{
-		// FELIX NEXT
+		private IDroneManager _droneManager;
+
+		private IDroneConsumer _droneConsumer1, _droneConsumer2, _droneConsumer3;
 
 		[SetUp]
 		public void TestSetup()
 		{
+			_droneManager = new DroneManager();
+
+			_droneConsumer1 = new DroneConsumer(1);
+			_droneConsumer2 = new DroneConsumer(2);
+			_droneConsumer3 = new DroneConsumer(4);
+
+			UnityAsserts.Assert.raiseExceptions = true;
 		}
 
 		[Test]
-		public void EditorTest() 
+		public void Initialisation() 
 		{
-//			Assert.IsTrue(false);
-			Assert.IsTrue(true);
+			Assert.AreEqual(0, _droneManager.NumOfDrones);
 		}
+
+		#region NumOfDrones
+		[Test]
+		public void NumOfDrones_Set()
+		{
+			_droneManager.NumOfDrones = 12;
+			Assert.AreEqual(12, _droneManager.NumOfDrones);
+		}
+
+		[ExpectedException(typeof(UnityAsserts.AssertionException))]
+		[Test]
+		public void NumOfDrones_Set_Invalid()
+		{
+			_droneManager.NumOfDrones = -1;
+		}
+		#endregion NumOfDrones
+
+		#region CanSupportDroneConsumer()
+		[Test]
+		public void CanSupportDroneConsumer_True()
+		{
+			_droneManager.NumOfDrones = 1;
+			Assert.IsTrue(_droneManager.CanSupportDroneConsumer(_droneConsumer1));
+		}
+
+		[Test]
+		public void CanSupportDroneConsumer_False()
+		{
+			_droneManager.NumOfDrones = 1;
+			Assert.IsFalse(_droneManager.CanSupportDroneConsumer(_droneConsumer2));
+		}
+		#endregion CanSupportDroneConsumer()
 	}
 }
