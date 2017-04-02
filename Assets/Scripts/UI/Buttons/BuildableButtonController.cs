d15f0b0d44cb064c1649522c017c59ba20799fa5
the_mann <felix.fei.mann@gmail.com>
2017-04-02 16:06:50 -0700

    Create BuildableButtonController class :)

---
+++ Assets/Scripts/UI/Buttons/BuildableButtonController.cs
@@ -0,0 +1,54 @@
+﻿using BattleCruisers.Drones;
+using BattleCruisers.UI;
+using BattleCruisers.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BattleCruisers.Buildings.Buttons
+{
+	public abstract class BuildableButtonController : Presentable
+	{
+		private IDroneConsumer _droneConsumer;
+		private IDroneManager _droneManager;
+		private Button _button;
+
+		public CanvasGroup canvasGroup;
+
+		public void Initialize(Buildable buildable, IDroneManager droneManager)
+		{
+			base.Initialize();
+
+			_droneConsumer = buildable.DroneConsumer;
+			_droneManager = droneManager;
+			_button = GetComponent<Button>();
+		}
+
+		public override void OnPresenting(object activationParameter)
+		{
+			base.OnPresenting(activationParameter);
+
+			if (_droneManager.CanSupportDroneConsumer(_droneConsumer))
+			{
+				_button.enabled = true;
+				_button.onClick.AddListener(OnClick);
+			}
+			else
+			{
+				_button.enabled = false;
+				canvasGroup.alpha = Constants.DISABLED_UI_ALPHA;
+			}
+		}
+
+		public override void OnDismissing()
+		{
+			base.OnDismissing();
+
+			_button.onClick.RemoveListener(OnClick);
+			canvasGroup.alpha = Constants.ENABLED_UI_ALPHA;
+		}
+
+		protected abstract void OnClick();
+	}
+}
--- Assets/Scripts/UI/Buttons/BuildingButtonController.cs
+++ Assets/Scripts/UI/Buttons/BuildingButtonController.cs
@@ -8,27 +8,22 @@ using UnityEngine.UI;
 
 namespace BattleCruisers.Buildings.Buttons
 {
-	public class BuildingButtonController : Presentable
+	public class BuildingButtonController : BuildableButtonController
 	{
 		private Building _building;
 		private UIManager _uiManager;
-		private IDroneManager _droneManager;
-		private Button _button;
 
 		public Image buildingImage;
 		public Image slotImage;
 		public Text buildingName;
 		public Text droneLevel;
-		public CanvasGroup canvasGroup;
 
 		public void Initialize(Building building, UIManager uiManager, IDroneManager droneManager, Sprite slotSprite)
 		{
-			base.Initialize();
+			base.Initialize(building, droneManager);
 			
 			_building = building;
 			_uiManager = uiManager;
-			_droneManager = droneManager;
-			_button = GetComponent<Button>();
 
 			buildingName.text = _building.buildableName;
 			droneLevel.text = _building.numOfDronesRequired.ToString();
@@ -36,31 +31,7 @@ namespace BattleCruisers.Buildings.Buttons
 			slotImage.sprite = slotSprite;
 		}
 
-		public override void OnPresenting(object activationParameter)
-		{
-			base.OnPresenting(activationParameter);
-
-			if (_droneManager.CanSupportDroneConsumer(_building.DroneConsumer))
-			{
-				_button.enabled = true;
-				_button.onClick.AddListener(OnClick);
-			}
-			else
-			{
-				_button.enabled = false;
-				canvasGroup.alpha = Constants.DISABLED_UI_ALPHA;
-			}
-		}
-
-		public override void OnDismissing()
-		{
-			base.OnDismissing();
-
-			_button.onClick.RemoveListener(OnClick);
-			canvasGroup.alpha = Constants.ENABLED_UI_ALPHA;
-		}
-
-		private void OnClick()
+		protected override void OnClick()
 		{
 			_uiManager.SelectBuildingFromMenu(_building);
 		}
