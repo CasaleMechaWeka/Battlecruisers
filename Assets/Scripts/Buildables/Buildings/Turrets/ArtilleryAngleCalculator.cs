d1ec4ec6a8e30e2b729c849897240d576c47f658
the_mann <felix.fei.mann@gmail.com>
2017-04-14 18:07:35 -0700

    Enforce that the source must be facing the target :)

--- Assets/Scripts/Buildables/Buildings/Turrets/ArtilleryAngleCalculator.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/ArtilleryAngleCalculator.cs
@@ -14,16 +14,27 @@ namespace BattleCruisers.Buildables.Buildings.Turrets
 	public class ArtilleryAngleCalculator : AngleCalculator
 	{
 		/// <summary>
-		/// Assumes no y axis difference in source and target
+		/// Assumes:
+		/// 1. No y axis difference in source and target
+		/// 2. Target is in facing direction of source
 		/// </summary>
 		public override float FindDesiredAngle(Vector2 source, Vector2 target, bool isSourceMirrored, float projectileVelocityInMPerS)
 		{
-			// FELIX  Limit target to valid quarter/half/direction
-
 			float distanceInM = Math.Abs(source.x - target.x);
 			if (distanceInM > FindMaxRange(projectileVelocityInMPerS))
 			{
-				throw new InvalidProgramException();
+				throw new ArgumentException("Out of range");
+			}
+
+			if (isSourceMirrored && target.x >= source.x)
+			{
+				throw new ArgumentException("Source faces left, but target is to the right");
+			}
+
+
+			if (!isSourceMirrored && target.x <= source.x)
+			{
+				throw new ArgumentException("Source faces right, but target is to the left");
 			}
 
 			float angleInRadians = (float) (0.5 * Math.Asin(Constants.GRAVITY * distanceInM / (projectileVelocityInMPerS * projectileVelocityInMPerS)));
