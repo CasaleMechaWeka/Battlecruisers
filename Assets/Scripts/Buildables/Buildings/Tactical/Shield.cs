80fd30112405c62ec72e061d65aab255ba28bcc9
the_mann <felix.fei.mann@gmail.com>
2017-04-17 18:32:29 -0700

    We have a shield :D  (It doesn't work yet)

---
+++ Assets/Scripts/Buildables/Buildings/Tactical/Shield.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Buildables.Buildings.Tactical
+{
+	public class Shield : Building
+	{
+		public Ring ring;
+		public float shieldRadiusInM;
+		public float shieldHealth;
+		public float shieldRechargeDelayInS;
+		public float shieldRechargeRatePerS;
+
+
+	}
+}
---
+++ Assets/Scripts/Ring.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BattleCruisers
+{
+	public class Ring : MonoBehaviour
+	{
+		public int numOfPoints;
+		public float radiusInM;
+		public LineRenderer lineRenderer;
+
+		public bool Enabled
+		{
+			set { lineRenderer.enabled = value; }
+		}
+
+		void Start()
+		{
+			lineRenderer.SetVertexCount(numOfPoints);
+			CreatePoints();
+		}
+
+		private void CreatePoints()
+		{
+			float angle = 0;
+
+			for (int i = 0; i < (numOfPoints); ++i)
+			{
+				float x = Mathf.Sin(Mathf.Deg2Rad * angle);
+				float y = Mathf.Cos(Mathf.Deg2Rad * angle);
+
+				lineRenderer.SetPosition(i, new Vector3(x, y, 0) * radiusInM);
+
+				angle += (360f / numOfPoints);
+			}
+		}
+	}
+}
