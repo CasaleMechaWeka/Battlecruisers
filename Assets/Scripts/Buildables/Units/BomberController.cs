5998c0c3f619081d8529c362f797aec5eec6a460
the_mann <felix.fei.mann@gmail.com>
2017-04-21 22:47:26 -0700

    Created aircraft test scene.  I have a plane :D

---
+++ Assets/Scripts/Buildables/Units/BomberController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Units
+{
+	public class BomberController : Unit
+	{
+		private IList<Vector2> _patrolPoints;
+		private Vector2 _targetPatrolPoint;
+
+		public void Initialise(IList<Vector2> patrolPoints)
+		{
+			Assert.IsTrue(patrolPoints.Count >= 2);
+
+			_patrolPoints = patrolPoints;
+		}
+
+		protected override void OnUpdate()
+		{
+			base.OnUpdate();
+
+
+		}
+
+		// FELIX TEMP
+		public void TempStartPatrolling()
+		{
+			StartPatrolling();
+		}
+
+		private void StartPatrolling()
+		{
+
+		}
+
+		private Vector2 FindNearestPatrolPoint()
+		{
+			NEXT
+
+			return new Vector2();
+		}
+
+		private Vector2 FindNextPatrolPoint()
+		{
+			return new Vector2();
+		}
+	}
+}
---
+++ Assets/Scripts/TestScenes/AircraftTestsGod.cs
@@ -0,0 +1,18 @@
+﻿using BattleCruisers.Buildables.Units;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.TestScenes
+{
+	public class AircraftTestsGod : MonoBehaviour 
+	{
+		public BomberController bomber;
+
+		void Start() 
+		{
+			IList<Vector2> patrolPoints = new List<Vector2> { new Vector2(-5, 3), new Vector2(5, 3) };
+			bomber.Initialise(patrolPoints);
+		}
+	}
+}
