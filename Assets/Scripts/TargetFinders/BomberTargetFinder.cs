ae675caa6398fb7531e7f71f86ce738ffa7393e3
the_mann <felix.fei.mann@gmail.com>
2017-04-26 21:16:34 -0700

    Moved bomber target finder to own script :)

--- Assets/Scripts/TargetFinders/BomberTargetFinder.cs
+++ Assets/Scripts/TargetFinders/BomberTargetFinder.cs
@@ -7,28 +7,6 @@ using UnityEngine;
 
 namespace BattleCruisers.TargetFinders
 {
-	public class TargetEventArgs : EventArgs
-	{
-		public TargetEventArgs(IFactionable target)
-		{
-			Target = target;
-		}
-
-		public IFactionable Target { get; private set; }
-	}
-
-	public interface ITargetFinder
-	{
-		// When a new target is found, usually when a target comes within range.
-		event EventHandler<TargetEventArgs> TargetFound;
-		// When an existing target is lost, either because it moves out of
-		// range or is destroyed.
-		event EventHandler<TargetEventArgs> TargetLost;
-
-		/// <returns>A target faction object, or null if no valid targets can be found.</returns>
-		IFactionable FindTarget();
-	}
-
 	// FELIX  Implement properly :P
 	public class BomberTargetFinder : MonoBehaviour, ITargetFinder
 	{
---
+++ Assets/Scripts/TargetFinders/ITargetFinder.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Cruisers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.TargetFinders
+{
+	public class TargetEventArgs : EventArgs
+	{
+		public TargetEventArgs(IFactionable target)
+		{
+			Target = target;
+		}
+
+		public IFactionable Target { get; private set; }
+	}
+
+	public interface ITargetFinder
+	{
+		// When a new target is found, usually when a target comes within range.
+		event EventHandler<TargetEventArgs> TargetFound;
+		// When an existing target is lost, either because it moves out of
+		// range or is destroyed.
+		event EventHandler<TargetEventArgs> TargetLost;
+
+		/// <returns>A target faction object, or null if no valid targets can be found.</returns>
+		IFactionable FindTarget();
+	}
+}
