9a15b3c18f17ffd16feef901180bbbd12d47c32d
the_mann <felix.fei.mann@gmail.com>
2017-04-26 17:49:48 -0700

    Created test scene with patrolling aircraft, in preparation for air turret :)

---
+++ Assets/Scripts/TestScenes/Turrets/AntiAirTurretTestsGod.cs
@@ -0,0 +1,19 @@
+﻿using BattleCruisers.Units.Aircraft;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.TestScenes.Turrets
+{
+	public class AntiAirTurretTestsGod : MonoBehaviour 
+	{
+		public Vector2[] aircraftPatrolPoints;
+		public AircraftController aircraft;
+
+		void Start () 
+		{
+			aircraft.PatrolPoints = aircraftPatrolPoints;
+			aircraft.StartPatrolling();
+		}
+	}
+}
