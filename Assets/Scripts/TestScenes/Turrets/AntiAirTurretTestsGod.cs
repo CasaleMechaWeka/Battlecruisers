60e37e13df3268a4520ac9f5ae7e4d7692985824
the_mann <felix.fei.mann@gmail.com>
2017-04-26 17:54:39 -0700

    Turret tries to shoot aircraft with no logic change, just need to set target to aircraft :)  Misses by a mile :D

--- Assets/Scripts/TestScenes/Turrets/AntiAirTurretTestsGod.cs
+++ Assets/Scripts/TestScenes/Turrets/AntiAirTurretTestsGod.cs
@@ -1,4 +1,6 @@
-﻿using BattleCruisers.Units.Aircraft;
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Units.Aircraft;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,10 +12,15 @@ namespace BattleCruisers.TestScenes.Turrets
 		public Vector2[] aircraftPatrolPoints;
 		public AircraftController aircraft;
 
+		public TurretBarrelController turretBarrel;
+
 		void Start () 
 		{
 			aircraft.PatrolPoints = aircraftPatrolPoints;
 			aircraft.StartPatrolling();
+
+			turretBarrel.Initialise(Faction.Blues);
+			turretBarrel.Target = aircraft.gameObject;
 		}
 	}
 }
