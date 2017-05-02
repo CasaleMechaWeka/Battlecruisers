adf64e67a8007faa2ceec0199c27b79765cb0142
the_mann <felix.fei.mann@gmail.com>
2017-05-01 17:39:17 -0700

    Created IFactionable file, to make Factionable file smaller :)

--- Assets/Scripts/Buildables/FactionObject.cs
+++ Assets/Scripts/Buildables/FactionObject.cs
@@ -10,64 +10,6 @@ using UnityEngine.Assertions;
 
 namespace BattleCruisers.Buildables
 {
-	public enum Faction
-	{
-		Blues, Reds
-	}
-
-	public enum TargetType
-	{
-		Aircraft, Ships, Cruiser, Buildings
-	}
-
-	public class DestroyedEventArgs : EventArgs
-	{
-		public IFactionable DestroyedFactionable { get; private set; }
-
-		public DestroyedEventArgs(IFactionable destroyedFactionable)
-		{
-			DestroyedFactionable = destroyedFactionable;
-		}
-	}
-
-	public class HealthChangedEventArgs : EventArgs
-	{
-		public float NewHealth { get; private set; }
-
-		public HealthChangedEventArgs(float newHealth)
-		{
-			NewHealth = newHealth;
-		}
-	}
-
-	public interface IDamagable
-	{
-		/// <value><c>true</c> if healht is 0; otherwise, <c>false</c>.</value>
-		bool IsDestroyed { get; }
-		float Health { get; }
-
-		// When health reaches 0
-		event EventHandler<DestroyedEventArgs> Destroyed;
-
-		// When health changes
-		event EventHandler<HealthChangedEventArgs> HealthChanged;
-
-		// FELIX  Unused?  Remove?
-		// When health reaches its maximum value
-		event EventHandler FullyRepaired;
-
-		void TakeDamage(float damageAmount);
-		void Repair(float repairAmount);
-	}
-
-	public interface IFactionable : IDamagable
-	{
-		Faction Faction { get; }
-		TargetType TargetType { get; }
-		GameObject GameObject { get; }
-		Vector2 Velocity { get; }
-	}
-
 	public abstract class FactionObject : MonoBehaviour, IFactionable
 	{
 		private bool _wasDestroyTriggeredInternally;
