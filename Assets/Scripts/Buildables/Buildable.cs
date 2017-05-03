4f38102fc8246746d8a240d66ecfdaedd749d095
the_mann <felix.fei.mann@gmail.com>
2017-05-02 21:34:36 -0700

    Moved IBuildable to own file.

--- Assets/Scripts/Buildables/Buildable.cs
+++ Assets/Scripts/Buildables/Buildable.cs
@@ -12,41 +12,6 @@ using BattleCruisers.Targets;
 
 namespace BattleCruisers.Buildables
 {
-	public enum BuildableState
-	{
-		NotStarted, InProgress, Paused, Completed
-	}
-
-	public class BuildProgressEventArgs : EventArgs
-	{
-		/// <summary>
-		/// 0-1
-		/// </summary>
-		public float BuildProgress { get; private set; }
-
-		public BuildProgressEventArgs(float buildProgress)
-		{
-			BuildProgress = buildProgress;
-		}
-	}
-
-	public interface IBuildable
-	{
-		BuildableState BuildableState { get; }
-		float Damage { get; }
-		Vector3 Size { get; }
-//		Sprite Sprite { get; }
-		IDroneConsumer DroneConsumer { get; }
-
-		event EventHandler StartedConstruction;
-		event EventHandler CompletedBuildable;
-		event EventHandler<BuildProgressEventArgs> BuildableProgress;
-
-		void Initialise(Faction faction, UIManager uiManager, ICruiser parentCruiser, ICruiser enemyCruiser, BuildableFactory buildableFactory, ITargetsFactory targetsFactory);
-		void StartConstruction();
-		void InitiateDelete();
-	}
-
 	public abstract class Buildable : FactionObject, IBuildable
 	{
 		private float _buildTimeInDroneSeconds;
---
+++ Assets/Scripts/Buildables/IBuildable.cs
@@ -0,0 +1,49 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Drones;
+using BattleCruisers.Targets.TargetFinders;
+using BattleCruisers.UI;
+using BattleCruisers.UI.ProgressBars;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using BattleCruisers.Targets;
+
+namespace BattleCruisers.Buildables
+{
+	public enum BuildableState
+	{
+		NotStarted, InProgress, Paused, Completed
+	}
+
+	public class BuildProgressEventArgs : EventArgs
+	{
+		/// <summary>
+		/// 0-1
+		/// </summary>
+		public float BuildProgress { get; private set; }
+
+		public BuildProgressEventArgs(float buildProgress)
+		{
+			BuildProgress = buildProgress;
+		}
+	}
+
+	public interface IBuildable
+	{
+		BuildableState BuildableState { get; }
+		float Damage { get; }
+		Vector3 Size { get; }
+		Sprite Sprite { get; }
+		IDroneConsumer DroneConsumer { get; }
+
+		event EventHandler StartedConstruction;
+		event EventHandler CompletedBuildable;
+		event EventHandler<BuildProgressEventArgs> BuildableProgress;
+
+		void Initialise(Faction faction, UIManager uiManager, ICruiser parentCruiser, ICruiser enemyCruiser, BuildableFactory buildableFactory, ITargetsFactory targetsFactory);
+		void StartConstruction();
+		void InitiateDelete();
+	}
+}
