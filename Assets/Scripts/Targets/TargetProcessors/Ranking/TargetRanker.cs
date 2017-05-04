51f0f24d3c467a46ffc431558a4a06446d3a281e
the_mann <felix.fei.mann@gmail.com>
2017-05-03 20:55:20 -0700

    Added ITargetRanker inerface, in preparation for ranking targets :D

---
+++ Assets/Scripts/Targets/TargetProcessors/Ranking/TargetRanker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BattleCruisers.Buildables;
+
+namespace BattleCruisers.Targets.TargetProcessors.Ranking
+{
+	public interface ITargetRanker
+	{
+		/// <returns>
+		/// Ranks the target according to priority.
+		/// 
+		/// The highest priority is 100, the lowest is 0.
+		/// </returns>
+		int RankTarget(IFactionable target);
+	}
+}
