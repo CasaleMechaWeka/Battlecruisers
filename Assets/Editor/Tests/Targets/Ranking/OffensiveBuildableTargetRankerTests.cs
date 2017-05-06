a196664cd291fcfb6dc35fc2b1cfe3b558178c9a
the_mann <felix.fei.mann@gmail.com>
2017-05-05 17:21:51 -0700

    Created OffensiveBuildableTargetRankerTests

---
+++ Assets/Editor/Tests/Targets/Ranking/OffensiveBuildableTargetRankerTests.cs
@@ -0,0 +1,38 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Targets.TargetProcessors.Ranking;
+using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEditor;
+using UnityAsserts = UnityEngine.Assertions;
+
+namespace BattleCruisers.Tests.Targets.Ranking
+{
+	/// <summary>
+	/// Note:  Targets are ranked in ascending priority.
+	/// </summary>
+	public class OffensiveBuildableTargetRankerTests : BaseTargetRankerTests
+	{
+		[SetUp]
+		public override void SetuUp()
+		{
+			_targetRanker = new OffensiveBuildableTargetRanker();
+		}
+
+		[Test]
+		public void LowValueAntiCruiser_TrumpsHighValue()
+		{
+			ITarget lowValueAnitCruiser = CreateMockTarget(TargetValue.Low, TargetType.Cruiser);
+			ITarget highValue = CreateMockTarget(TargetValue.High);
+
+			_rankedTargets = new List<ITarget>(new ITarget[] { lowValueAnitCruiser, highValue });
+			RankTargets();
+
+			_expectedOrder = new List<ITarget>(new ITarget[] { highValue, lowValueAnitCruiser });
+
+			Assert.AreEqual(_expectedOrder, _rankedTargets);
+		}
+	}
+}
