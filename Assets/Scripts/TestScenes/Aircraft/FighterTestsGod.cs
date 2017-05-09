5f40d8fc5ca4338c45d4b60b6765cb3ec7d5447c
the_mann <felix.fei.mann@gmail.com>
2017-05-08 18:11:08 -0700

    Figher patrols in test scene :D

--- Assets/Scripts/TestScenes/Aircraft/FighterTestsGod.cs
+++ Assets/Scripts/TestScenes/Aircraft/FighterTestsGod.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿using BattleCruisers.Buildables.Units;
+using BattleCruisers.Targets;
+using BattleCruisers.TestScenes.Utilities;
+using BattleCruisers.Units.Aircraft;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +11,26 @@ namespace BattleCruisers.TestScenes.Aircraft
 {
 	public class FighterTestsGod : MonoBehaviour 
 	{
-		void Start () 
+		public FighterController fighter;
+		public GameObject target;
+		public List<Vector2> patrolPoints;
+
+		void Start() 
 		{
-			
+			Helper helper = new Helper();
+//			ITargetsFactory targetsFactory = helper.CreateTargetsFactory(target);
+
+			helper.InitialiseBuildable(fighter, parentCruiserDirection: Direction.Right);
+//			helper.InitialiseBuildable(fighter, targetsFactory: targetsFactory, parentCruiserDirection: Direction.Right);
+			fighter.CompletedBuildable += Fighter_CompletedBuildable;
+			fighter.StartConstruction();
 		}
+
+		private void Fighter_CompletedBuildable(object sender, EventArgs e)
+		{
+			FighterController fighter = sender as FighterController;
+			fighter.PatrolPoints = patrolPoints;
+			fighter.StartPatrolling();
+		}			
 	}
 }
