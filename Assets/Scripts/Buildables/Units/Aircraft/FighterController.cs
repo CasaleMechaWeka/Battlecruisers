ae374dd9769fa4854f889592ec84698b3bc0dc42
the_mann <felix.fei.mann@gmail.com>
2017-05-08 17:58:56 -0700

    Created (as yet not functional) fighter script, prefab and test scene :)

---
+++ Assets/Scripts/Buildables/Units/Aircraft/FighterController.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Targets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Units.Aircraft
+{
+	public class FighterController : AircraftController, ITargetConsumer
+	{
+		public ITarget Target { private get; set; }
+	}
+}
---
+++ Assets/Scripts/TestScenes/Aircraft/FighterTestsGod.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.TestScenes.Aircraft
+{
+	public class FighterTestsGod : MonoBehaviour 
+	{
+		void Start () 
+		{
+			
+		}
+	}
+}
