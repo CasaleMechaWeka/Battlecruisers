1e85a33099b37b26a6b63a11e244c6c60e00bb16
the_mann <felix.fei.mann@gmail.com>
2017-05-09 21:42:46 -0700

    Created BerrelController base class, to hopefully avoid duplicate functionality between the existing TurretBarrelController and the new Fighter's InvisibleTurretController :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelController.cs
@@ -0,0 +1,76 @@
+﻿using BattleCruisers.Buildables.Units;
+using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+using BattleCruisers.Projectiles;
+using BattleCruisers.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets
+{
+	/// <summary>
+	/// FELIX  Take accuracy into consideration.  Perhaps in AngleCalculator?
+	/// </summary>
+	public abstract class BarrelController : MonoBehaviour 
+	{
+		private Faction _faction;
+		private float _timeSinceLastFireInS;
+		private ShellStats _shellStats;
+
+		public ShellSpawnerController shellSpawner;
+		public TurretStats turretStats;
+		public AngleCalculator angleCalculator;
+
+		public ITarget Target { get; set; }
+		private bool IsSourceMirrored { get { return transform.rotation.eulerAngles.y == 180; } }
+
+		public void Initialise(Faction faction)
+		{
+			_faction = faction;
+			_timeSinceLastFireInS = float.MaxValue;
+			_shellStats = new ShellStats(turretStats.shellPrefab, turretStats.damage, turretStats.ignoreGravity, turretStats.bulletVelocityInMPerS);
+			shellSpawner.Initialise(_faction, _shellStats);
+		}
+
+		void Update()
+		{
+			if (Target != null)
+			{
+				_timeSinceLastFireInS += Time.deltaTime;
+
+				Vector2 sourcePosition = new Vector2(transform.position.x, transform.position.y);
+				Vector3 targetPositionV3 = Target.GameObject.transform.position;
+				Vector2 targetPosition = new Vector2(targetPositionV3.x, targetPositionV3.y);
+
+				float desiredAngleInDegrees = angleCalculator.FindDesiredAngle(sourcePosition, targetPosition, IsSourceMirrored, turretStats.bulletVelocityInMPerS, Target.Velocity);
+
+				bool isOnTarget = IsOnTarget(desiredAngleInDegrees);
+
+				if (isOnTarget)
+				{
+					if (_timeSinceLastFireInS >= turretStats.FireIntervalInS)
+					{
+						Fire(desiredAngleInDegrees);
+						_timeSinceLastFireInS = 0;
+					}
+				}
+				else
+				{
+					AdjustBarrel(desiredAngleInDegrees);
+				}
+			}
+		}
+
+		protected abstract bool IsOnTarget(float desiredAngleInDegrees);
+
+		protected abstract void AdjustBarrel(float desiredAngleInDegrees);
+
+		private void Fire(float angleInDegrees)
+		{
+			Logging.Log(Tags.TURRET_BARREL_CONTROLLER, "Fire()");
+			shellSpawner.SpawnShell(angleInDegrees, IsSourceMirrored);
+		}
+	}
+}
--- Assets/Scripts/Buildables/Units/Aircraft/FighterController.cs
+++ Assets/Scripts/Buildables/Units/Aircraft/FighterController.cs
@@ -1,5 +1,9 @@
 ﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
 using BattleCruisers.Targets;
+using BattleCruisers.Targets.TargetFinders;
+using BattleCruisers.Targets.TargetProcessors;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +12,15 @@ namespace BattleCruisers.Units.Aircraft
 {
 	public class FighterController : AircraftController, ITargetConsumer
 	{
+		private ITargetFinder _targetFinder;
+		private ITargetProcessor _targetProcessor;
+		
+		public TargetDetector enemyDetector;
+		public TurretStats turretStats;
+		public AngleCalculator angleCalculator;
+
 		public ITarget Target { private get; set; }
+
+
 	}
 }
