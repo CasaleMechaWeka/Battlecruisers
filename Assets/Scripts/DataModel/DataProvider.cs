553dd1639a5cd4b9519cc6c895a92c082d778dc7
the_mann <felix.fei.mann@gmail.com>
2017-05-19 19:08:23 -0700

    Created DataProvider stub, I don't think I'll be able to get away with not using a db :P

---
+++ Assets/Scripts/DataModel/DataProvider.cs
@@ -0,0 +1,53 @@
+﻿using BattleCruisers.Fetchers.PrefabKeys;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.DataModel
+{
+	public interface IDataProvider
+	{
+		IList<UnitKey> GetAllUnits();
+		IList<UnitKey> GetAllUnlockedUnits();
+
+		IList<BuildingKey> GetAllBuildings();
+		IList<BuildingKey> GetAllUnlockedBuildings();
+
+		IList<HullKey> GetAllHulls();
+		IList<HullKey> GetAllUnlockedHulls();
+	}
+
+	public class DataProvider : IDataProvider
+	{
+		public IList<UnitKey> GetAllUnits()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IList<UnitKey> GetAllUnlockedUnits()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IList<BuildingKey> GetAllBuildings()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IList<BuildingKey> GetAllUnlockedBuildings()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IList<HullKey> GetAllHulls()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IList<HullKey> GetAllUnlockedHulls()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
