abdc6c48e98ff8b77f8b7c0327555e4b226bf27f
the_mann <felix.fei.mann@gmail.com>
2017-05-27 15:46:54 -0700

    Created MovementControllerFactory

---
+++ Assets/Scripts/Movement/MovementControllerFactory.cs
@@ -0,0 +1,20 @@
+﻿using BattleCruisers.Movement;
+using System;
+using UnityEngine;
+
+namespace BattleCruisers.Movement
+{
+	public interface IMovementControllerFactory
+	{
+		IHomingMovementController CreateMissileMovementController(Rigidbody2D rigidBody, float maxVelocityInMPerS);
+	}
+
+	public class MovementControllerFactory
+	{
+		public IHomingMovementController CreateMissileMovementController(Rigidbody2D rigidBody, float maxVelocityInMPerS)
+		{
+			return new MissileMovementController(rigidBody, maxVelocityInMPerS);
+		}
+	}
+}
+
