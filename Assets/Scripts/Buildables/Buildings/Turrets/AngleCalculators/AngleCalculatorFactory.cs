2b9793692ab4c340786638d63a13a33dd9e59c0d
the_mann <felix.fei.mann@gmail.com>
2017-05-28 12:12:56 -0700

    Created AngleCalculatorFactory :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AngleCalculators/AngleCalculatorFactory.cs
@@ -0,0 +1,37 @@
+﻿using BattleCruisers.Movement.Predictors;
+using System;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators
+{
+	public interface IAngleCalculatorFactory
+	{
+		IAngleCalculator CreateAngleCalcultor(float projectileVelocityInMPerS, bool isSourceMirrored, ITargetPositionPredictorFactory targetPositionPredictorFactory);
+		IAngleCalculator CreateArtilleryAngleCalcultor(float projectileVelocityInMPerS, bool isSourceMirrored, ITargetPositionPredictorFactory targetPositionPredictorFactory);
+		IAngleCalculator CreateMortarAngleCalcultor(float projectileVelocityInMPerS, bool isSourceMirrored, ITargetPositionPredictorFactory targetPositionPredictorFactory);
+		IAngleCalculator CreateLeadingAngleCalcultor(float projectileVelocityInMPerS, bool isSourceMirrored, ITargetPositionPredictorFactory targetPositionPredictorFactory);
+	}
+
+	public class AngleCalculatorFactory
+	{
+		public IAngleCalculator CreateAngleCalcultor(float projectileVelocityInMPerS, bool isSourceMirrored, ITargetPositionPredictorFactory targetPositionPredictorFactory)
+		{
+			return new AngleCalculator(projectileVelocityInMPerS, isSourceMirrored, targetPositionPredictorFactory);
+		}
+
+		public IAngleCalculator CreateArtilleryAngleCalcultor(float projectileVelocityInMPerS, bool isSourceMirrored, ITargetPositionPredictorFactory targetPositionPredictorFactory)
+		{
+			return new ArtilleryAngleCalculator(projectileVelocityInMPerS, isSourceMirrored, targetPositionPredictorFactory);
+		}
+
+		public IAngleCalculator CreateMortarAngleCalcultor(float projectileVelocityInMPerS, bool isSourceMirrored, ITargetPositionPredictorFactory targetPositionPredictorFactory)
+		{
+			return new MortarAngleCalculator(projectileVelocityInMPerS, isSourceMirrored, targetPositionPredictorFactory);
+		}
+
+		public IAngleCalculator CreateLeadingAngleCalcultor(float projectileVelocityInMPerS, bool isSourceMirrored, ITargetPositionPredictorFactory targetPositionPredictorFactory)
+		{
+			return new LeadingAngleCalculator(projectileVelocityInMPerS, isSourceMirrored, targetPositionPredictorFactory);
+		}
+	}
+}
+
