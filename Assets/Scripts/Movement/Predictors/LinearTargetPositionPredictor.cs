97e4ebe4b7df3a54b8456e4bf6501a466df26007
the_mann <felix.fei.mann@gmail.com>
2017-05-27 23:22:24 -0700

    Created Linear and Mortar target predictors :)

---
+++ Assets/Scripts/Movement/Predictors/LinearTargetPositionPredictor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace BattleCruisers.Movement.Predictors
+{
+	public class LinearTargetPositionPredictor : TargetPositionPredictor
+	{
+		protected override float EstimateTimeToTarget(Vector2 sourcePosition, Vector2 targetPosition, float projectileVelocityInMPerS, float currentAngleInRadians)
+		{
+			return Vector2.Distance(sourcePosition, targetPosition) / projectileVelocityInMPerS;
+		}
+	}
+}
+
---
+++ Assets/Scripts/Movement/Predictors/MortarTargetPositionPredictor.cs
@@ -0,0 +1,18 @@
+﻿using BattleCruisers.Utils;
+using System;
+using UnityEngine;
+
+namespace BattleCruisers.Movement.Predictors
+{
+	public class MortarTargetPositionPredictor : TargetPositionPredictor
+	{
+		protected override float EstimateTimeToTarget(Vector2 sourcePosition, Vector2 targetPosition, float projectileVelocityInMPerS, float currentAngleInRadians)
+		{
+			float sourceElevationInM = sourcePosition.y - targetPosition.y;
+			float vSin = projectileVelocityInMPerS * Mathf.Sin(currentAngleInRadians);
+			float squareRootArg = (vSin * vSin) + 2 * Constants.GRAVITY * sourceElevationInM;
+			return (vSin + Mathf.Sqrt(squareRootArg)) / Constants.GRAVITY;
+		}
+	}
+}
+
