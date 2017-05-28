c3190eef0003c03a81a52d9352d0bc22fd4e1c87
the_mann <felix.fei.mann@gmail.com>
2017-05-27 23:29:33 -0700

    Created target position predictor factory :D

---
+++ Assets/Scripts/Movement/Predictors/TargetPositionPredictorFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BattleCruisers.Movement.Predictors
+{
+	public interface ITargetPositionPredictorFactory
+	{
+		ITargetPositionPredictor CreateLinearPredictor();
+		ITargetPositionPredictor CreateMortarPredictor();
+	}
+
+	public class TargetPositionPredictorFactory : ITargetPositionPredictorFactory
+	{
+		public ITargetPositionPredictor CreateLinearPredictor()
+		{
+			return new LinearTargetPositionPredictor();
+		}
+
+		public ITargetPositionPredictor CreateMortarPredictor()
+		{
+			return new MortarTargetPositionPredictor();
+		}
+	}
+}
