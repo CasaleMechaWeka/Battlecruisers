9ef1aa80fe0fdc68b66350687d7b793c14151ac7
the_mann <felix.fei.mann@gmail.com>
2017-05-28 22:13:10 -0700

    Created AntiAirTurretController :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AntiAirTurretController.cs
@@ -0,0 +1,17 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+using System;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets
+{
+	public class AntiAirTurretController : DefensiveTurret
+	{
+		protected override void OnInitialised()
+		{
+			base.OnInitialised();
+
+			IAngleCalculator angleCalculator = _angleCalculatorFactory.CreateLeadingAngleCalcultor(_targetPositionPredictorFactory);
+			turretBarrelController.Initialise(Faction, angleCalculator);
+		}
+	}
+}
+
