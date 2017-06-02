d8c595efd34cdfbc497290ef56afce0c135a1a66
the_mann <felix.fei.mann@gmail.com>
2017-06-01 22:53:14 -0700

    Created shield detection test scene, where attack boat should initially be out of range :)

---
+++ Assets/Scripts/Scenes/Test/Shields/ShieldDetectionTestGod.cs
@@ -0,0 +1,34 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Tactical;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.Shields
+{
+	public class ShieldDetectionTestGod : MonoBehaviour 
+	{
+		private ShieldGenerator _shield;
+		private AttackBoatController _attackBoat;
+
+		void Start () 
+		{
+			Helper helper = new Helper();
+
+			// Setup shield
+			_shield = GameObject.FindObjectOfType<ShieldGenerator>();
+			Assert.IsNotNull(_shield);
+			helper.InitialiseBuildable(_shield, Faction.Blues);
+			_shield.StartConstruction();
+
+			// Setup turret
+			_attackBoat = GameObject.FindObjectOfType<AttackBoatController>();
+			Assert.IsNotNull(_attackBoat);
+			helper.InitialiseBuildable(_attackBoat, Faction.Reds, parentCruiserDirection: Direction.Left);
+			_attackBoat.StartConstruction();
+		}
+	}
+}
--- Assets/Scripts/Scenes/Test/Shields/ShieldTestsGod.cs
+++ Assets/Scripts/Scenes/Test/Shields/ShieldTestsGod.cs
@@ -8,7 +8,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace BattleCruisers.Scenes.Test
+namespace BattleCruisers.Scenes.Test.Shields
 {
 	public class ShieldTestsGod : MonoBehaviour 
 	{
