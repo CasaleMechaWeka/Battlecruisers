4407c52bc0e8e3a7f65143ea493a760aa6244326
the_mann <felix.fei.mann@gmail.com>
2017-06-02 17:04:48 -0700

    Created DetectableFilter

---
+++ Assets/Scripts/Targets/TargetFinders/Filters/DetectableFilter.cs
@@ -0,0 +1,26 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BattleCruisers.Targets.TargetFinders.Filters
+{
+	public class DetectableFilter : FactionAndTargetTypeFilter
+	{
+		private readonly bool _isDetectable;
+
+		public DetectableFilter(Faction faction, bool isDetectable, params TargetType[] targetTypes)
+			: base(faction, targetTypes)
+		{
+			_isDetectable = isDetectable;
+		}
+
+		public override bool IsMatch(ITarget target)
+		{
+			return base.IsMatch(target) && target.IsDetectable == _isDetectable;
+		}
+	}
+}
