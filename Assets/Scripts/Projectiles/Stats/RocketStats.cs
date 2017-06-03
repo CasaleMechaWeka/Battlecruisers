3bf7637875b9ffceb5415ce927dc0ce233a18e95
the_mann <felix.fei.mann@gmail.com>
2017-06-03 15:16:09 -0700

    Created rocket stats class :)

---
+++ Assets/Scripts/Projectiles/Stats/RocketStats.cs
@@ -0,0 +1,27 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Movement;
+using BattleCruisers.Movement.Predictors;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Projectiles.Stats
+{
+	// FELIX  Extract common property with MissileController?
+	public class RocketStats : ProjectileStats
+	{
+		private const float INITIAL_VELOCITY_MULTIPLIER = 0.25f;
+
+		public RocketController RocketPrefab { get; private set; }
+		public float InitialVelocityInMPerS { get { return MaxVelocityInMPerS * INITIAL_VELOCITY_MULTIPLIER; } }
+
+		public RocketStats(RocketController rocketPrefab, float damage, float maxVelocityInMPerS)
+			: base(damage, maxVelocityInMPerS, ignoreGravity: true)
+		{
+			RocketPrefab = rocketPrefab;
+		}
+	}
+}
