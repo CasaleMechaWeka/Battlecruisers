2cb4598a89343b0ddf531617988262f70137ca09
the_mann <felix.fei.mann@gmail.com>
2017-06-03 21:18:18 -0700

    Created TeslaCoil stub :)

---
+++ Assets/Scripts/Buildables/Buildings/Tactical/TeslaCoil.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Buildables.Buildings.Tactical
+{
+	public class TeslaCoil : Building
+	{
+		public override TargetValue TargetValue { get { return TargetValue.Medium; } }
+
+		protected override void OnInitialised()
+		{
+			base.OnInitialised();
+			// FELIX
+		}
+
+		protected override void OnBuildableCompleted()
+		{
+			base.OnBuildableCompleted();
+			// FELIX
+		}
+	}
+}
