9946e46d8ad69017eac15ba819e69bf462c12c7c
the_mann <felix.fei.mann@gmail.com>
2017-06-05 19:05:39 -0700

    Created rocket launcher test scene :)

---
+++ Assets/Scripts/Scenes/Test/RocketLauncherTestGod.cs
@@ -0,0 +1,44 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Buildings.Offensive;
+using BattleCruisers.Movement;
+using BattleCruisers.Movement.Predictors;
+using BattleCruisers.Projectiles;
+using BattleCruisers.Projectiles.Spawners;
+using BattleCruisers.Projectiles.Stats;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Targets;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Buildables.Units.Aircraft;
+using NSubstitute;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test
+{
+	public class RocketLauncherTestGod : MonoBehaviour 
+	{
+		void Start()
+		{
+			Helper helper = new Helper();
+
+
+			// Setup target
+			AirFactory target = GameObject.FindObjectOfType<AirFactory>();
+			helper.InitialiseBuildable(target, Faction.Blues);
+			target.StartConstruction();
+
+
+			// Setup rocket launcher
+			RocketLauncherController rocketLauncher = GameObject.FindObjectOfType<RocketLauncherController>();
+			ITargetFilter targetFilter = new ExactMatchTargetFilter() 
+			{
+				Target = target
+			};
+			ITargetsFactory targetsFactory = helper.CreateTargetsFactory(target.GameObject, targetFilter);
+			helper.InitialiseBuildable(rocketLauncher, Faction.Reds, targetsFactory: targetsFactory);
+			rocketLauncher.StartConstruction();
+		}
+	}
+}
