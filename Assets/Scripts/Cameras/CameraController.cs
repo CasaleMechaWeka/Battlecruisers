31f4254ed3fa070521f18245ca77576eeb1440d1
the_mann <felix.fei.mann@gmail.com>
2017-06-09 20:14:33 -0700

    Replace switch with CameraTarget objects :D  Zoran would be so proud, sniff sniff

--- Assets/Scripts/Cameras/CameraController.cs
+++ Assets/Scripts/Cameras/CameraController.cs
@@ -10,7 +10,7 @@ namespace BattleCruisers.Cameras
 {
 	public enum CameraState
 	{
-		PlayerCruiser, AiCruiser, Center, InTransition
+		PlayerCruiser, AiCruiser, Overview, InTransition
 	}
 
 	public class CameraTransitionArgs : EventArgs
@@ -36,22 +36,10 @@ namespace BattleCruisers.Cameras
 
 	public class CameraController : MonoBehaviour, ICameraController 
 	{
-		private ICruiser _playerCruiser;
-		private ICruiser _aiCruiser;
 		private Camera _camera;
-		private ICameraCalculator _cameraCalculator;
-		private float _playerCruiserOrthographicSize;
-		private float _aiCruiserOrthographicSize;
-		private Vector3 _playerCruiserTargetPosition;
-		private Vector3 _aiCruiserTargetPosition;
-		private Vector3 _centerTargetPosition;
-
-		private CameraState _cameraStateTarget;
-		private Vector3 _cameraVelocity = Vector3.zero;
-		private Vector3 _cameraPositionTarget;
-
+		private ICameraTarget _currentTarget, _playerCruiserTarget, _aiCruiserTarget, _overviewTarget;
+		private Vector3 _cameraPositionChangeVelocity = Vector3.zero;
 		private float _cameraOrthographicSizeChangeVelocity = 0;
-		private float _cameraOrthographicSizeTarget;
 
 		public float smoothTime;
 		public float overviewOrthographicSize;
@@ -68,52 +56,52 @@ namespace BattleCruisers.Cameras
 
 		public void Initialise(ICruiser playerCruiser, ICruiser aiCruiser)
 		{
-			_playerCruiser = playerCruiser;
-			_aiCruiser = aiCruiser;
-			_cameraState = CameraState.Center;
-			_cameraStateTarget = CameraState.PlayerCruiser;
+			_cameraState = CameraState.Overview;
 
 			_camera = GetComponent<Camera>();
 			Assert.IsNotNull(_camera);
 
-			_cameraCalculator = new CameraCalculator(_camera);
+			ICameraCalculator cameraCalculator = new CameraCalculator(_camera);
 
-			_playerCruiserOrthographicSize = _cameraCalculator.FindCameraOrthographicSize(_playerCruiser);
-			_playerCruiserTargetPosition = new Vector3(_playerCruiser.Position.x, _cameraCalculator.FindCameraYPosition(_playerCruiserOrthographicSize), transform.position.z);
+			float playerCruiserOrthographicSize = cameraCalculator.FindCameraOrthographicSize(playerCruiser);
+			Vector3 playerCruiserTargetPosition = new Vector3(playerCruiser.Position.x, cameraCalculator.FindCameraYPosition(playerCruiserOrthographicSize), transform.position.z);
+			_playerCruiserTarget = new CameraTarget(playerCruiserTargetPosition, playerCruiserOrthographicSize, CameraState.PlayerCruiser);
 
-			_aiCruiserOrthographicSize = _cameraCalculator.FindCameraOrthographicSize(_aiCruiser);
-			_aiCruiserTargetPosition = new Vector3(_aiCruiser.Position.x, _cameraCalculator.FindCameraYPosition(_aiCruiserOrthographicSize), transform.position.z);
+			float aiCruiserOrthographicSize = cameraCalculator.FindCameraOrthographicSize(aiCruiser);
+			Vector3 aiCruiserTargetPosition = new Vector3(aiCruiser.Position.x, cameraCalculator.FindCameraYPosition(aiCruiserOrthographicSize), transform.position.z);
+			_aiCruiserTarget = new CameraTarget(aiCruiserTargetPosition, aiCruiserOrthographicSize, CameraState.AiCruiser);
 
-			_centerTargetPosition = new Vector3(0, centerPositionY, transform.position.z);
+			Vector3 overviewTargetPosition = new Vector3(0, centerPositionY, transform.position.z);
+			_overviewTarget = new CameraTarget(overviewTargetPosition, overviewOrthographicSize, CameraState.Overview);
 
 			FocusOnPlayerCruiser();
 		}
 
 		void Update()
 		{
-			if (_cameraState != _cameraStateTarget)
+			if (_cameraState != _currentTarget.State)
 			{
 				// Camera position
-				bool isInPosition = (transform.position - _cameraPositionTarget).magnitude < POSITION_EQUALITY_MARGIN;
+				bool isInPosition = (transform.position - _currentTarget.Position).magnitude < POSITION_EQUALITY_MARGIN;
 				if (!isInPosition)
 				{
-					transform.position = Vector3.SmoothDamp(transform.position, _cameraPositionTarget, ref _cameraVelocity, smoothTime);
+					transform.position = Vector3.SmoothDamp(transform.position, _currentTarget.Position, ref _cameraPositionChangeVelocity, smoothTime);
 				}
-				else if (transform.position != _cameraPositionTarget)
+				else if (transform.position != _currentTarget.Position)
 				{
-					transform.position = _cameraPositionTarget;
+					transform.position = _currentTarget.Position;
 					Logging.Log(Tags.CAMERA_CONTROLLER, "CameraController position done");
 				}
 
 				// Camera zoom
-				bool isRightOrthographicSize = Math.Abs(Camera.main.orthographicSize - _cameraOrthographicSizeTarget) < ORTHOGRAPHIC_SIZE_EQUALITY_MARGIN;
+				bool isRightOrthographicSize = Math.Abs(_camera.orthographicSize - _currentTarget.OrthographicSize) < ORTHOGRAPHIC_SIZE_EQUALITY_MARGIN;
 				if (!isRightOrthographicSize)
 				{
-					Camera.main.orthographicSize = Mathf.SmoothDamp(Camera.main.orthographicSize, _cameraOrthographicSizeTarget, ref _cameraOrthographicSizeChangeVelocity, smoothTime);
+					_camera.orthographicSize = Mathf.SmoothDamp(_camera.orthographicSize, _currentTarget.OrthographicSize, ref _cameraOrthographicSizeChangeVelocity, smoothTime);
 				}
-				else if (Camera.main.orthographicSize != _cameraOrthographicSizeTarget)
+				else if (_camera.orthographicSize != _currentTarget.OrthographicSize)
 				{
-					Camera.main.orthographicSize = _cameraOrthographicSizeTarget;
+					_camera.orthographicSize = _currentTarget.OrthographicSize;
 					Logging.Log(Tags.CAMERA_CONTROLLER, "CameraController zoom done");
 				}
 
@@ -122,75 +110,54 @@ namespace BattleCruisers.Cameras
 				{
 					if (CameraTransitionCompleted != null)
 					{
-						CameraTransitionCompleted.Invoke(this, new CameraTransitionArgs(_cameraState, _cameraStateTarget));
+						CameraTransitionCompleted.Invoke(this, new CameraTransitionArgs(_cameraState, _currentTarget.State));
 					}
 
-					_cameraState = _cameraStateTarget;
+					_cameraState = _currentTarget.State;
 				}
 			}
 		}
 
 		public void FocusOnPlayerCruiser()
 		{
-			bool isInstant = _cameraState != CameraState.Center;
-			MoveCamera(CameraState.PlayerCruiser, isInstant);
+			bool isInstant = _cameraState != CameraState.Overview;
+			_currentTarget = _playerCruiserTarget;
+			MoveCamera(isInstant);
 		}
 
 		public void FocusOnAiCruiser()
 		{
-			bool isInstant = _cameraState != CameraState.Center;
-			MoveCamera(CameraState.AiCruiser, isInstant);
+			bool isInstant = _cameraState != CameraState.Overview;
+			_currentTarget = _aiCruiserTarget;
+			MoveCamera(isInstant);
 		}
 
 		public void ShowFullMapView()
 		{
-			MoveCamera(CameraState.Center, isInstant: false);
+			_currentTarget = _overviewTarget;
+			MoveCamera(isInstant: false);
 		}
 
 		/// <returns><c>true</c>, if camera was or will be moved, <c>false</c> otherwise.</returns>
-		private bool MoveCamera(CameraState targetState, bool isInstant)
+		private bool MoveCamera(bool isInstant)
 		{
 			bool willMoveCamera = 
 				_cameraState != CameraState.InTransition
-			    && _cameraState != targetState;
+			    && _cameraState != _currentTarget.State;
 
-			Logging.Log(Tags.CAMERA_CONTROLLER, "MoveCamera targetState: " + targetState + "  willMoveCamera: " + willMoveCamera + "  _cameraState: " + _cameraState);
+			Logging.Log(Tags.CAMERA_CONTROLLER, "MoveCamera _currentTarget.State: " + _currentTarget.State + "  willMoveCamera: " + willMoveCamera + "  _cameraState: " + _cameraState);
 
 			if (willMoveCamera)
 			{
-				_cameraPositionTarget = transform.position;
-				_cameraStateTarget = targetState;
-
 				if (CameraTransitionStarted != null)
 				{
-					CameraTransitionStarted.Invoke(this, new CameraTransitionArgs(_cameraState, _cameraStateTarget));
-				}
-
-				switch (targetState)
-				{
-					case CameraState.AiCruiser:
-						_cameraPositionTarget = _aiCruiserTargetPosition;
-						_cameraOrthographicSizeTarget = _aiCruiserOrthographicSize;
-						break;
-
-					case CameraState.PlayerCruiser:
-						_cameraPositionTarget = _playerCruiserTargetPosition;
-						_cameraOrthographicSizeTarget = _playerCruiserOrthographicSize;
-						break;
-
-					case CameraState.Center:
-						_cameraPositionTarget = _centerTargetPosition;
-						_cameraOrthographicSizeTarget = overviewOrthographicSize;
-						break;
-
-					default:
-						throw new ArgumentException();
+					CameraTransitionStarted.Invoke(this, new CameraTransitionArgs(_cameraState, _currentTarget.State));
 				}
 
 				if (isInstant)
 				{
-					transform.position = _cameraPositionTarget;
-					Camera.main.orthographicSize = _cameraOrthographicSizeTarget;
+					transform.position = _currentTarget.Position;
+					_camera.orthographicSize = _currentTarget.OrthographicSize;
 				}
 				else
 				{
---
+++ Assets/Scripts/Cameras/CameraTarget.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace BattleCruisers.Cameras
+{
+	public interface ICameraTarget
+	{
+		Vector3 Position { get; } 
+		float OrthographicSize { get; }
+		CameraState State { get; }
+	}
+
+	public class CameraTarget : ICameraTarget
+	{
+		public Vector3 Position { get; private set; } 
+		public float OrthographicSize { get; private set; }
+		public CameraState State { get; private set; }
+
+		public CameraTarget(Vector3 position, float orthographicSize, CameraState state)
+		{
+			Position = position;
+			OrthographicSize = orthographicSize;
+			State = state;
+		}
+	}
+}
+
