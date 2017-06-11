bcd2089030c497b96624c9e8d486ec2978812957
the_mann <felix.fei.mann@gmail.com>
2017-06-10 17:39:14 -0700

    Moved LoadoutScreenController ot own folder :)

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/LoadoutScreenController.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/LoadoutScreenController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-namespace BattleCruisers.UI.ScreensScene
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen
 {
 	public class LoadoutScreenController : ScreenController
 	{
