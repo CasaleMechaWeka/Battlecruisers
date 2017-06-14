49c226055b0a8ef136791ac3c01e959af4f1485f
the_mann <felix.fei.mann@gmail.com>
2017-06-13 21:38:23 -0700

    Show feedback when unlocked item is in loadout :)

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/UnlockedItem.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/UnlockedItem.cs
@@ -13,16 +13,22 @@ namespace BattleCruisers.UI.ScreensScene.LoadoutScreen
 		private RectTransform _rectTransform;
 
 		public Image itemImage;
+		public Image isInLoadoutFeedback;
 
 		public Vector2 Size { get { return _rectTransform.sizeDelta; } }
 		public Building Building { get; private set; }
 
 		public void Initialise(Building building, bool isBuildingInLoadout)
 		{
+			Assert.IsNotNull(itemImage);
+			Assert.IsNotNull(isInLoadoutFeedback);
+
 			itemImage.sprite = building.Sprite;
 
 			_rectTransform = transform as RectTransform;
 			Assert.IsNotNull(_rectTransform);
+
+			isInLoadoutFeedback.gameObject.SetActive(isBuildingInLoadout);
 		}
 	}
 }
