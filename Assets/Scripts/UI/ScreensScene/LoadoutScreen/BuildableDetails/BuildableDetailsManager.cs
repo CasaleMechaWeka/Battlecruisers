cb10ce2a2ed3514d884aa2db7d1153a8ac65cf74
the_mann <felix.fei.mann@gmail.com>
2017-06-18 21:00:48 -0700

    Have modal overlay when details are showing, so nothing is clickable

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/BuildableDetails/BuildableDetailsManager.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/BuildableDetails/BuildableDetailsManager.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.BuildableDetails
 {
@@ -19,6 +20,7 @@ namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.BuildableDetails
 	{
 		private IBuildableDetailsState _state;
 
+		public Image modelBackground;
 		public ComparableBuildableDetailsController singleBuildableDetails;
 		public ComparableBuildableDetailsController leftComparableBuildableDetails, rightComparableBuildableDetails;
 
@@ -31,6 +33,8 @@ namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.BuildableDetails
 			rightComparableBuildableDetails.Initialise(spriteFetcher);
 
 			_state = new DismissedState(this);
+
+			HideBuildableDetails();
 		}
 
 		public void SelectBuildable(Buildable buildable)
@@ -50,17 +54,20 @@ namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.BuildableDetails
 
 		public void ShowBuildableDetails(Buildable buildable)
 		{
+			modelBackground.gameObject.SetActive(true);
 			singleBuildableDetails.ShowBuildableDetails(buildable);
 		}
 
 		public void CompareBuildableDetails(Buildable buildable1, Buildable buildable2)
 		{
+			modelBackground.gameObject.SetActive(true);
 			leftComparableBuildableDetails.ShowBuildableDetails(buildable1);
 			rightComparableBuildableDetails.ShowBuildableDetails(buildable2);
 		}
 
 		public void HideBuildableDetails()
 		{
+			modelBackground.gameObject.SetActive(false);
 			singleBuildableDetails.Hide();
 			leftComparableBuildableDetails.Hide();
 			rightComparableBuildableDetails.Hide();
