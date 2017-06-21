51832bd90bc5db0e1953ce3d321b55b00496e7d2
the_mann <felix.fei.mann@gmail.com>
2017-06-20 22:40:17 -0700

    Created IComparableItemDetails interface :)

---
+++ Assets/Scripts/UI/Common/BuildableDetails/IComparableItemDetails.cs
@@ -0,0 +1,11 @@
+﻿using BattleCruisers.Buildables;
+using System;
+
+namespace BattleCruisers.UI.Common.BuildingDetails
+{
+	public interface IComparableItemDetails<T> where T : Target
+	{
+		void ShowItemDetails(T item, T itemToCompareTo = null);
+	}
+}
+
