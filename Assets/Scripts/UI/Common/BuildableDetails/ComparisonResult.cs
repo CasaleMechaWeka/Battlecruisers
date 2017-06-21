882f833061aa6d4ff56e171a98b82ae799d18bae
the_mann <felix.fei.mann@gmail.com>
2017-06-20 21:18:26 -0700

    Moved ComparisonResult classes to own file :)

---
+++ Assets/Scripts/UI/Common/BuildableDetails/ComparisonResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.Common.BuildingDetails
+{
+	public interface IComparisonResult
+	{
+		Color Color { get; }
+	}
+
+	public abstract class ComparisonResult : IComparisonResult
+	{
+		protected static Color NEUTRAL_COLOR = Color.clear;
+		protected static Color BETTER_COLOR = Color.green;
+		protected static Color WORSE_COLOR = Color.red;
+
+		public Color Color { get; private set; }
+
+		protected ComparisonResult(Color color)
+		{
+			Color = color;
+		}
+	}
+
+	public class NeutralResult : ComparisonResult
+	{
+		public NeutralResult() : base(NEUTRAL_COLOR) { }
+	}
+
+	public class BetterResult : ComparisonResult
+	{
+		public BetterResult() : base(BETTER_COLOR) { }
+	}
+
+	public class WorseResult : ComparisonResult
+	{
+		public WorseResult() : base(WORSE_COLOR) { }
+	}
+}
+
--- Assets/Scripts/UI/Common/BuildableDetails/StatsRow.cs
+++ Assets/Scripts/UI/Common/BuildableDetails/StatsRow.cs
@@ -4,41 +4,6 @@ using UnityEngine.UI;
 
 namespace BattleCruisers.UI.Common.BuildingDetails
 {
-	// FELIX  Move to own class
-	public interface IComparisonResult
-	{
-		Color Color { get; }
-	}
-
-	public abstract class ComparisonResult : IComparisonResult
-	{
-		protected static Color NEUTRAL_COLOR = Color.clear;
-		protected static Color BETTER_COLOR = Color.green;
-		protected static Color WORSE_COLOR = Color.red;
-
-		public Color Color { get; private set; }
-
-		protected ComparisonResult(Color color)
-		{
-			Color = color;
-		}
-	}
-
-	public class NeutralResult : ComparisonResult
-	{
-		public NeutralResult() : base(NEUTRAL_COLOR) { }
-	}
-
-	public class BetterResult : ComparisonResult
-	{
-		public BetterResult() : base(BETTER_COLOR) { }
-	}
-
-	public class WorseResult : ComparisonResult
-	{
-		public WorseResult() : base(WORSE_COLOR) { }
-	}
-
 	public abstract class StatsRow : MonoBehaviour
 	{
 		public Text rowLabel;
