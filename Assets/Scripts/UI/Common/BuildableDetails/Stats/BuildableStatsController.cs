d0225bd58de3085609c97f444c24a8f4d7ed9996
the_mann <felix.fei.mann@gmail.com>
2017-06-21 18:43:22 -0700

    Made BuildableStatsController extend base class :)

--- Assets/Scripts/UI/Common/BuildableDetails/Stats/BuildableStatsController.cs
+++ Assets/Scripts/UI/Common/BuildableDetails/Stats/BuildableStatsController.cs
@@ -8,37 +8,19 @@ using UnityEngine;
 
 namespace BattleCruisers.UI.Common.BuildingDetails.Stats
 {
-	public class BuildableStatsController : MonoBehaviour 
+	public class BuildableStatsController : StatsController<Buildable>
 	{
-		private ValueToStarsConverter _valueToStarsConverter;
-		private IStatsComparer _higherIsBetterComparer, _lowerIsBetterComparer;
-
 		public StatsRowNumberController droneRow;
 		public StatsRowNumberController buildTimeRow;
 		public StatsRowStarsController healthRow;
 		public StatsRowStarsController damageRow;
 
-		private const string HEALTH_LABEL = "Health";
 		private const string DAMAGE_LABEL = "Damage";
-		private const string DRONES_LABEL = "Drones";
 		private const string BUILD_TIME_LABEL = "BuildTime";
-
 		private const string BUILD_TIME_SUFFIX = "s";
 
-		void Awake()
+		protected override void InternalShowStats(Buildable buildable, Buildable buildableToCompareTo)
 		{
-			_valueToStarsConverter = new ValueToStarsConverter();
-			_higherIsBetterComparer = new HigherIsBetterComparer();
-			_lowerIsBetterComparer = new LowerIsBetterComparer();
-		}
-
-		public void ShowBuildableStats(Buildable buildable, Buildable buildableToCompareTo = null)
-		{
-			if (buildableToCompareTo == null)
-			{
-				buildableToCompareTo = buildable;
-			}
-
 			droneRow.Initialise(DRONES_LABEL, buildable.numOfDronesRequired.ToString(), _lowerIsBetterComparer.CompareStats(buildable.numOfDronesRequired, buildableToCompareTo.numOfDronesRequired));
 			buildTimeRow.Initialise(BUILD_TIME_LABEL, buildable.buildTimeInS.ToString() + BUILD_TIME_SUFFIX, _lowerIsBetterComparer.CompareStats(buildable.buildTimeInS, buildableToCompareTo.buildTimeInS));
 			healthRow.Initialise(HEALTH_LABEL, _valueToStarsConverter.HealthValueToStars(buildable.maxHealth), _higherIsBetterComparer.CompareStats(buildable.Health, buildableToCompareTo.Health));
