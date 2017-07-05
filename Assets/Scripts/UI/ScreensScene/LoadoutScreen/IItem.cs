7bbd38ab5199b41e1d4604c1b5acd0c60c9dc6c6
the_mann <felix.fei.mann@gmail.com>
2017-07-04 21:59:05 -0700

    Removed old TODO comment

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/IItem.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/IItem.cs
@@ -3,7 +3,6 @@ using System;
 
 namespace BattleCruisers.UI.ScreensScene.LoadoutScreen
 {
-	// FELIX  Maybe create Item base class shared by Loadout- and Unlocked- Items?
 	public interface IItem<TItem> where TItem : IComparableItem
 	{
 		TItem Item { get; }
