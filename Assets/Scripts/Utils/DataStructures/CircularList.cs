d795bc9d9392e43305eacc647574397e3511d813
the_mann <felix.fei.mann@gmail.com>
2017-07-06 22:09:33 -0700

    Created CircularList data structure :)

---
+++ Assets/Scripts/Utils/DataStructures/CircularList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.DataStrctures
+{
+	public class CircularList<T> : ICircularList<T>
+	{
+		private T[] _items;
+		private int _index;
+
+		public CircularList(T[] items)
+		{
+			Assert.IsTrue(items.Length != 0);
+
+			_items = items;
+			_index = _items.Length -1;
+		}
+
+		public T Next()
+		{
+			_index = (_index + 1) % _items.Length;
+			return _items[_index];
+		}
+	}
+}
---
+++ Assets/Scripts/Utils/DataStructures/ICircularList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.DataStrctures
+{
+	public interface ICircularList<T>
+	{
+		T Next();
+	}
+}
