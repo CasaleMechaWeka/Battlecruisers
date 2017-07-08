e3d51da2fe06e15fb9f7fda0156a7b5b26b98673
the_mann <felix.fei.mann@gmail.com>
2017-07-07 21:30:20 -0700

    Created LaserTest scene that currently just has a target building :)

---
+++ Assets/Scripts/Scenes/Test/Projectiles/LaserTestGod.cs
@@ -0,0 +1,22 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test
+{
+	public class LaserTestGod : MonoBehaviour 
+	{
+		private Buildable _target;
+
+		void Start () 
+		{
+			// Setup target
+			Helper helper = new Helper();
+			_target = GameObject.FindObjectOfType<Buildable>();
+			helper.InitialiseBuildable(_target, Faction.Blues);
+			_target.StartConstruction();			
+		}
+	}
+}
