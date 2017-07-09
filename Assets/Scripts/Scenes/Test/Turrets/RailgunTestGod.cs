23dec8d9f62a834af139659b7732166225fbe28e
the_mann <felix.fei.mann@gmail.com>
2017-07-09 13:38:09 -0700

    Created Railgun Test.  Seems to work :D

---
+++ Assets/Scripts/Scenes/Test/Turrets/RailgunTestGod.cs
@@ -0,0 +1,37 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Buildings.Turrets.Offensive;
+using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Targets;
+
+namespace BattleCruisers.Scenes.Test
+{
+	public class RailgunTestGod : MonoBehaviour 
+	{
+		void Start()
+		{
+			Helper helper = new Helper();
+
+
+			// Setup target
+			Buildable target = GameObject.FindObjectOfType<AirFactory>();
+			helper.InitialiseBuildable(target, Faction.Reds);
+			target.StartConstruction();
+
+
+			// Setup railgun
+			Buildable railgun = GameObject.FindObjectOfType<RailgunController>();
+			ITargetFilter targetFilter = new ExactMatchTargetFilter() 
+			{
+				Target = target
+			};
+			ITargetsFactory targetsFactory = helper.CreateTargetsFactory(target.GameObject, targetFilter);
+			helper.InitialiseBuildable(railgun, Faction.Blues, targetsFactory: targetsFactory);
+			railgun.StartConstruction();
+		}
+	}
+}
