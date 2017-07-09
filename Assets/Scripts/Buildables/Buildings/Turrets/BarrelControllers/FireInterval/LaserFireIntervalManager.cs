29b42532288169a3c30091cf344f59a0ecbcbb07
the_mann <felix.fei.mann@gmail.com>
2017-07-08 20:11:02 -0700

    Created LaserTurretStats and LaserFireIntervalManager :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/FireInterval/LaserFireIntervalManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers.FireInterval
+{
+	// FELIX  Avoid duplicate code with FireIntervalmManager
+	public class LaserFireIntervalManager : MonoBehaviour, IFireIntervalManager
+	{
+		private LaserTurretStats _laserTurretStats;
+		private float _currentFireIntervalInS;
+		private float _timeSinceLastFireInS;
+		private bool _inLaserBurst;
+		private float _timeSinceLaserBurstStartInS;
+
+		public void Initialise(LaserTurretStats laserTurretStats)
+		{
+			_laserTurretStats = laserTurretStats;
+			_currentFireIntervalInS = _laserTurretStats.NextFireIntervalInS;
+			_timeSinceLastFireInS = float.MaxValue;
+			_inLaserBurst = false;
+			_timeSinceLaserBurstStartInS = 0;
+		}
+
+		/// <returns>
+		/// Always returns true while in the laser burst length.  Ie, if the laser burst length
+		/// is 2 seconds, this method will return true during those two seconds.
+		/// </returns>
+		public bool IsIntervalUp()
+		{
+			bool isIntervalUp = false;
+
+			if (_inLaserBurst)
+			{
+				if (_timeSinceLaserBurstStartInS <= _laserTurretStats.laserDurationInS)
+				{
+					isIntervalUp = true;
+				}
+				else
+				{
+					_timeSinceLaserBurstStartInS = 0;
+
+					// Stop laser burst
+					_inLaserBurst = false;
+				}
+			}
+			else
+			{
+				if (_timeSinceLastFireInS >= _currentFireIntervalInS)
+				{
+					_timeSinceLastFireInS = 0;
+					_currentFireIntervalInS = _laserTurretStats.NextFireIntervalInS;
+
+					// Start laser burst
+					_inLaserBurst = true;
+
+					isIntervalUp = true;
+				}
+			}
+			return isIntervalUp;
+		}
+
+		// FELIX  Create state classes to avoid if/else?
+		void Update()
+		{
+			if (_inLaserBurst)
+			{
+				_timeSinceLaserBurstStartInS += Time.deltaTime;
+			}
+			else
+			{
+				_timeSinceLastFireInS += Time.deltaTime;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Buildables/Buildings/Turrets/Stats/LaserTurretStats.cs
@@ -0,0 +1,17 @@
+﻿using BattleCruisers.Projectiles;
+using BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers;
+using BattleCruisers.Utils;
+using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets
+{
+	public class LaserTurretStats : TurretStats
+	{
+		public float damagePerS;
+		public float laserDurationInS;
+
+		public override float DamagePerS { get { return damagePerS; } }
+	}
+}
