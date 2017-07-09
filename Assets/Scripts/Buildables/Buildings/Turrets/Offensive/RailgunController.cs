eb41d26f8aff8b604da016e5460f1b5bd2777b71
the_mann <felix.fei.mann@gmail.com>
2017-07-09 12:30:48 -0700

    Finished RailgunController implementation I think :/

--- Assets/Scripts/Buildables/Buildings/Turrets/Offensive/RailgunController.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/Offensive/RailgunController.cs
@@ -8,14 +8,6 @@ namespace BattleCruisers.Buildables.Buildings.Turrets.Offensive
 {
 	public class RailgunController : OffensiveTurret
 	{
-		// FELIX  Fix
-		protected override void InitialiseTurretBarrel()
-		{
-//			RocketBarrelController barrelController = _barrelController as RocketBarrelController;
-//			Assert.IsNotNull(barrelController);
-//			barrelController.Initialise(CreateTargetFilter(), CreateAngleCalculator(), _movementControllerFactory, Faction);
-		}
-
 		protected override IAngleCalculator CreateAngleCalculator()
 		{
 			return _angleCalculatorFactory.CreateAngleCalcultor(_targetPositionPredictorFactory);
