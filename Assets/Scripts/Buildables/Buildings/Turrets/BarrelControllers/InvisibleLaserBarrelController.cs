2d7dada82293da75256d0065aa95f5c50da16bd3
the_mann <felix.fei.mann@gmail.com>
2017-07-09 19:59:55 -0700

    Created InvisibleLaserBarrelController

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/InvisibleLaserBarrelController.cs
@@ -0,0 +1,17 @@
+﻿namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers
+{
+	/// <summary>
+	/// Do not have a barrel to move.
+	/// 
+	/// Decide if we are on target by checking if the target is within the firing zone.
+	/// </summary>
+	public class InvisibleLaserBarrelController : ShellTurretBarrelController 
+	{
+		protected override bool IsOnTarget(float desiredAngleInDegrees)
+		{
+			return true;
+		}
+
+		protected override void AdjustBarrel(float desiredAngleInDegrees) { }
+	}
+}
