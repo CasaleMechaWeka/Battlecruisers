63037626655a9c9721ed0ef12f6b15111f1a9d60
the_mann <felix.fei.mann@gmail.com>
2017-07-12 16:57:27 -0700

    Fixed artilery barrel controller tests

--- Assets/Scripts/Scenes/Test/Turrets/ArtilleryBarrelControllerTests.cs
+++ Assets/Scripts/Scenes/Test/Turrets/ArtilleryBarrelControllerTests.cs
@@ -8,6 +8,7 @@ using NSubstitute;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BattleCruisers.Targets.TargetFinders.Filters;
 
 namespace BattleCruisers.Scenes.Test
 {
@@ -21,13 +22,15 @@ namespace BattleCruisers.Scenes.Test
 			target.GameObject.Returns(targetGameObject);
 
 			IAngleCalculator angleCalculator = new ArtilleryAngleCalculator(new TargetPositionPredictorFactory());
+			ITargetFilter targetFilter = Substitute.For<ITargetFilter>();
 
 			BarrelController[] artilleryBarrels = GameObject.FindObjectsOfType<BarrelController>() as BarrelController[];
 			foreach (BarrelController barrel in artilleryBarrels)
 			{
+				barrel.StaticInitialise();
 				IRotationMovementController rotationMovementController = new RotationMovementController(angleCalculator, barrel.TurretStats.turretRotateSpeedInDegrees, barrel.transform);
 				barrel.Target = target;
-				barrel.Initialise(null, angleCalculator, rotationMovementController);
+				barrel.Initialise(targetFilter, angleCalculator, rotationMovementController);
 			}
 		}
 	}
