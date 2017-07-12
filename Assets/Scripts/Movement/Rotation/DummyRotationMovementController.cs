c527b42baeb8c4d71a6da80a14fe0551ba6a0d75
the_mann <felix.fei.mann@gmail.com>
2017-07-11 22:01:51 -0700

    Created DummyRotationMovementController :)

---
+++ Assets/Scripts/Movement/Rotation/DummyRotationMovementController.cs
@@ -0,0 +1,12 @@
+﻿namespace BattleCruisers.Movement.Rotation
+{
+	public class DummyRotationMovementController : IRotationMovementController
+	{
+		public bool IsOnTarget(float desiredAngleInDegrees)
+		{
+			return true;
+		}
+
+		public void AdjustRotation(float desiredAngleInDegrees) { }
+	}
+}
