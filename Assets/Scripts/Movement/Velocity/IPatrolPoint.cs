ac24d0e712104139bc91cb705cdeddf1dd2c51b6
the_mann <felix.fei.mann@gmail.com>
2017-07-13 22:00:10 -0700

    Created PatrolPoint class and interface.  These ripple effects are going to be massive :D

---
+++ Assets/Scripts/Movement/Velocity/IPatrolPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace BattleCruisers.Movement.Velocity
+{	
+	public interface IPatrolPoint
+	{
+		Vector2 Position { get; }
+		bool RemoveOnceReached { get; }
+		Action ActionOnReached { get; }
+	}
+}
---
+++ Assets/Scripts/Movement/Velocity/PatrolPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace BattleCruisers.Movement.Velocity
+{	
+	public class PatrolPoint
+	{
+		public Vector2 Position { get; private set; }
+		public bool RemoveOnceReached { get; private set; }
+		public Action ActionOnReached { get; private set; }
+
+		public PatrolPoint(Vector2 position, bool removeOnceReached = false, Action actionOnReached = null)
+		{
+			Position = position;
+			RemoveOnceReached = removeOnceReached;
+			ActionOnReached = actionOnReached;
+
+			if (ActionOnReached == null)
+			{
+				ActionOnReached = () => { };
+			}
+		}
+	}
+}
