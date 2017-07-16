e6062c41b08a8e7f06129a979354f1c6a185195b
the_mann <felix.fei.mann@gmail.com>
2017-07-15 20:02:15 -0700

    Created nuke prefab that just renders build progress and nothing else :D

---
+++ Assets/Scripts/Buildables/Buildings/Offensive/NukeLauncherController.cs
@@ -0,0 +1,20 @@
+﻿using BattleCruisers.Buildables.Units;
+using BattleCruisers.Buildables.Units.Aircraft;
+using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Offensive
+{
+	public class NukeLauncherController : Building
+	{
+		public override TargetValue TargetValue { get { return TargetValue.High; } }
+
+		protected override void OnBuildableCompleted()
+		{
+			base.OnBuildableCompleted();
+
+			// FELIX  Open & launch nuke :D
+		}
+	}
+}
