c7925871686f674bc4aca0b39b54b89f6f5aa2c6
the_mann <felix.fei.mann@gmail.com>
2017-07-18 21:48:48 -0700

    Implemented DamageApplierTestGod

--- Assets/Scripts/Scenes/Test/Projectiles/DamageApplierTestGod.cs
+++ Assets/Scripts/Scenes/Test/Projectiles/DamageApplierTestGod.cs
@@ -1,14 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Projectiles.DamageAppliers;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Targets.TargetFinders.Filters;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Projectiles
 {
-	public class DamageApplierTestGod : MonoBehaviour 
+    public class DamageApplierTestGod : MonoBehaviour 
     {
+        public LayerMask targetLayerMask;
+
 	    void Start() 
         {
-	        
+            Helper helper = new Helper();
+            Faction factionToTarget = Faction.Blues;
+
+
+            // Setup targets
+            Buildable baseTarget = FindObjectOfType<AirFactory>();
+            helper.InitialiseBuildable(baseTarget, factionToTarget);
+
+            DroneStation[] targets = FindObjectsOfType<DroneStation>();
+            foreach (DroneStation target in targets)
+            {
+                helper.InitialiseBuildable(target, factionToTarget);
+            }
+
+
+            // Setup damage applier
+            float damage = 50;
+            float radiusInM = 5;
+            ITargetFilter targetFilter = new FactionAndTargetTypeFilter(factionToTarget, TargetType.Buildings);
+
+            IDamageApplier damageApplier = new AreaOfEffectDamageApplier(damage, radiusInM, targetFilter, targetLayerMask);
+            damageApplier.ApplyDamage(baseTarget);
 	    }
     }
 }
