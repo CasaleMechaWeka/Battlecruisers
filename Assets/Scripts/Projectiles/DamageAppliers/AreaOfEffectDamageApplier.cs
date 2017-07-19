7de408dc11f6480ca9a73d6d0eef794cf2e0b9e2
the_mann <felix.fei.mann@gmail.com>
2017-07-18 21:37:11 -0700

    Created AreaOfEffectDamageApplier :D

---
+++ Assets/Scripts/Projectiles/DamageAppliers/AreaOfEffectDamageApplier.cs
@@ -0,0 +1,39 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using UnityEngine;
+
+namespace BattleCruisers.Projectiles.DamageAppliers
+{
+    public class AreaOfEffectDamageApplier : IDamageApplier
+    {
+        private readonly float _damage;
+        private readonly float _radiusInM;
+        private readonly ITargetFilter _targetFilter;
+        private readonly LayerMask _targetLayerMask;
+
+
+		public AreaOfEffectDamageApplier(float damage, float radiusInM, ITargetFilter targetFilter, LayerMask targetLayerMask)
+        {
+            _damage = damage;
+            _radiusInM = radiusInM;
+            _targetFilter = targetFilter;
+            _targetLayerMask = targetLayerMask;
+        }
+
+        public void ApplyDamage(ITarget baseTarget)
+        {
+            baseTarget.TakeDamage(_damage);
+
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(baseTarget.Position, _radiusInM, _targetLayerMask);
+            foreach (Collider2D collider in colliders)
+            {
+				ITarget target = collider.gameObject.GetComponent<ITarget>();
+
+                if (target != null && _targetFilter.IsMatch(target))
+                {
+                    target.TakeDamage(_damage);
+                }
+            }
+		}
+    }
+}
