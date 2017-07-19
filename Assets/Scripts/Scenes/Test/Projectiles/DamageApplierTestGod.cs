a75f8517656c0782657072207783bef72fbc8a47
the_mann <felix.fei.mann@gmail.com>
2017-07-18 21:22:19 -0700

    Created empty DamageAppliter test scene

---
+++ Assets/Scripts/Scenes/Test/Projectiles/DamageApplierTestGod.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Projectiles
+{
+	public class DamageApplierTestGod : MonoBehaviour 
+    {
+	    void Start() 
+        {
+	        
+	    }
+    }
+}
