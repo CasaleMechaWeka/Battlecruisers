1c6b5bdb47505a3073be0b2a81871ca44b7875bc
the_mann <felix.fei.mann@gmail.com>
2017-07-23 15:00:15 -0700

    Completed TaskConsumerTests :)

--- Assets/Editor/Tests/AI/TaskConsumerTests.cs
+++ Assets/Editor/Tests/AI/TaskConsumerTests.cs
@@ -23,13 +23,55 @@ namespace BattleCruisers.Tests.AI
             _taskConsumer = new TaskConsumer(_tasks);
         }
 
+        #region ITaskList.HighestPriorityTaskChanged
         [Test]
-        public void NewHighestPriorityEvent_StartsNewEvent()
+        public void NewHighestPriorityTask_StartsNewTask()
         {
             _tasks.HighestPriorityTask.Returns(_task1);
             _tasks.HighestPriorityTaskChanged += Raise.EventWith(_tasks, EventArgs.Empty);
 
-			_task1.Received().Start();
+            _task1.Received().Start();
         }
+
+        [Test]
+        public void NewHighestPriorityTask_StopsCurrentTask_StartsNewTask()
+        {
+            NewHighestPriorityTask_StartsNewTask();
+
+            _tasks.HighestPriorityTask.Returns(_task2);
+            _tasks.HighestPriorityTaskChanged += Raise.Event();
+
+            _task1.Received().Stop();
+            _task2.Received().Start();
+        }
+		#endregion ITaskList.HighestPriorityTaskChanged
+
+		#region ITask.Completed
+        [Test]
+        public void TaskCompleted_StartsNextTask()
+        {
+            NewHighestPriorityTask_StartsNewTask();
+
+			_tasks.HighestPriorityTask.Returns(_task2);
+            _task1.Completed += Raise.Event();
+
+            // Receives stop even though it has completed
+			_task1.Received().Stop();
+			_task2.Received().Start();
+        }
+
+        [Test]
+		public void TaskCompleted_HandlesNoMoreTasks()
+		{
+			NewHighestPriorityTask_StartsNewTask();
+
+			_tasks.HighestPriorityTask.Returns(default(ITask));
+			_task1.Completed += Raise.Event();
+
+			// Receives stop even though it has completed
+			_task1.Received().Stop();
+            _task2.DidNotReceive().Start();
+		}
+		#endregion ITask.Completed
 	}
 }
--- Assets/Scripts/AI/ITask.cs
+++ Assets/Scripts/AI/ITask.cs
@@ -13,8 +13,10 @@ namespace BattleCruisers.AI
 
         event EventHandler<EventArgs> Completed;
 
-        // FELIX If started when already completed, should emit Completed event.
+        // FELIX  If started when already completed, should emit Completed event.
         void Start();
+
+        // FELIX  Should have no effect if already completed (ie, no crash :P )
         void Stop();
     }
 }
