be70191e39ee6025b474ad5e2f019b62fff898cd
the_mann <felix.fei.mann@gmail.com>
2017-07-23 19:22:12 -0700

    Tried to expand test, but crashes because BuildableWrapper.Buildable is a concrete type, not an interface :(

--- Assets/Editor/Tests/AI/Tasks/ConstructBuildingTaskTests.cs
+++ Assets/Editor/Tests/AI/Tasks/ConstructBuildingTaskTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using BattleCruisers.AI;
 using BattleCruisers.AI.Tasks;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
 using BattleCruisers.Cruisers;
 using BattleCruisers.Data.PrefabKeys;
 using BattleCruisers.Fetchers;
@@ -16,6 +18,9 @@ namespace BattleCruisers.Tests.AI
 		private IPrefabKey _key;
 		private IPrefabFactory _prefabFactory;
 		private ICruiserController _cruiser;
+        private IBuildableWrapper<Building> _prefab;
+        private IBuildable _building;
+        private ISlot _slot;
 
         [SetUp]
         public void SetuUp()
@@ -25,12 +30,23 @@ namespace BattleCruisers.Tests.AI
             _cruiser = Substitute.For<ICruiserController>();
 
             _task = new ConstructBuildingTask(TaskPriority.High, _key, _prefabFactory, _cruiser);
+
+            _prefab = Substitute.For<IBuildableWrapper<Building>>();
+            _building = Substitute.For<IBuildable>();
+            _slot = Substitute.For<ISlot>();
         }
 
         [Test]
         public void Start_StartsConstructingBuilding()
         {
-            
+            _prefab.Buildable.Returns(_building);
+
+            _prefabFactory.GetBuildingWrapperPrefab(_key).Returns(_prefab);
+            _cruiser.IsSlotAvailable(_building.SlotType).Returns(true);
+            _cruiser.GetFreeSlot(_building.SlotType).Returns(_slot);
+            _cruiser.ConstructBuilding(_prefab.UnityObject, _slot).Returns(_building);
+
+            _task.Start();
         }
     }
 }
