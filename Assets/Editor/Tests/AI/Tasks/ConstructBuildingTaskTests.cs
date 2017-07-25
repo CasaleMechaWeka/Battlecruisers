f0425e6e6090dd522f93c0ce80695f3d2edfbfd5
the_mann <felix.fei.mann@gmail.com>
2017-07-24 20:05:34 -0700

    Made ConstructBuildingTask implement IInternalTask instead of ITask, and simplified its tests :)

--- Assets/Editor/Tests/AI/Tasks/ConstructBuildingTaskTests.cs
+++ Assets/Editor/Tests/AI/Tasks/ConstructBuildingTaskTests.cs
@@ -1,114 +1,89 @@
-﻿//using System;
-//using BattleCruisers.AI.Tasks;
-//using BattleCruisers.Buildables;
-//using BattleCruisers.Buildables.Buildings;
-//using BattleCruisers.Cruisers;
-//using BattleCruisers.Data.PrefabKeys;
-//using BattleCruisers.Fetchers;
-//using NSubstitute;
-//using NUnit.Framework;
+﻿using System;
+using BattleCruisers.AI.Tasks;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Data.PrefabKeys;
+using BattleCruisers.Fetchers;
+using NSubstitute;
+using NUnit.Framework;
 
-//namespace BattleCruisers.Tests.AI.Tasks
-//{
-//    public class ConstructBuildingTaskTests
-//    {
-//        private ITask _task;
+namespace BattleCruisers.Tests.AI.Tasks
+{
+    public class ConstructBuildingTaskTests
+    {
+        private IInternalTask _task;
 
-//		private IPrefabKey _key;
-//		private IPrefabFactory _prefabFactory;
-//		private ICruiserController _cruiser;
-//        private IBuildableWrapper<IBuilding> _prefab;
-//        private IBuilding _building;
-//        private ISlot _slot;
+		private IPrefabKey _key;
+		private IPrefabFactory _prefabFactory;
+		private ICruiserController _cruiser;
+        private IBuildableWrapper<IBuilding> _prefab;
+        private IBuilding _building;
+        private ISlot _slot;
 
-//        private int _numOfCompletedEvents;
+        private int _numOfCompletedEvents;
 
-//        [SetUp]
-//        public void SetuUp()
-//        {
-//            _key = Substitute.For<IPrefabKey>();
-//            _prefabFactory = Substitute.For<IPrefabFactory>();
-//            _cruiser = Substitute.For<ICruiserController>();
+        [SetUp]
+        public void SetuUp()
+        {
+            _key = Substitute.For<IPrefabKey>();
+            _prefabFactory = Substitute.For<IPrefabFactory>();
+            _cruiser = Substitute.For<ICruiserController>();
 
-//            _task = new ConstructBuildingTask(TaskPriority.High, _key, _prefabFactory, _cruiser);
+            _task = new ConstructBuildingTask(_key, _prefabFactory, _cruiser);
 
-//            _task.Completed += _task_Completed;
+            _task.Completed += _task_Completed;
 
-//			_building = Substitute.For<IBuilding>();
-//            _prefab = Substitute.For<IBuildableWrapper<IBuilding>>();
-//			_prefab.Buildable.Returns(_building);
-//            _slot = Substitute.For<ISlot>();
+			_building = Substitute.For<IBuilding>();
+            _prefab = Substitute.For<IBuildableWrapper<IBuilding>>();
+			_prefab.Buildable.Returns(_building);
+            _slot = Substitute.For<ISlot>();
 
-//			_numOfCompletedEvents = 0;
-//        }
+			_numOfCompletedEvents = 0;
+        }
 
-//        [Test]
-//        public void Start_StartsConstructingBuilding()
-//        {
-//            _prefabFactory.GetBuildingWrapperPrefab(_key).Returns(_prefab);
-//            _cruiser.IsSlotAvailable(_building.SlotType).Returns(true);
-//            _cruiser.GetFreeSlot(_building.SlotType).Returns(_slot);
-//            _cruiser.ConstructBuilding(_prefab.UnityObject, _slot).Returns(_building);
+        [Test]
+        public void Start_StartsConstructingBuilding()
+        {
+            _prefabFactory.GetBuildingWrapperPrefab(_key).Returns(_prefab);
+            _cruiser.IsSlotAvailable(_building.SlotType).Returns(true);
+            _cruiser.GetFreeSlot(_building.SlotType).Returns(_slot);
+            _cruiser.ConstructBuilding(_prefab.UnityObject, _slot).Returns(_building);
 
-//            _task.Start();
+            _task.Start();
 
-//            _cruiser.Received().ConstructBuilding(_prefab.UnityObject, _slot);
-//        }
+            _cruiser.Received().ConstructBuilding(_prefab.UnityObject, _slot);
+        }
 
-//		[Test]
-//		public void Start_NoAvailabeSlots_EmitsCompletedEvent()
-//		{
-//			_prefabFactory.GetBuildingWrapperPrefab(_key).Returns(_prefab);
-//			_cruiser.IsSlotAvailable(_building.SlotType).Returns(false);
+		[Test]
+		public void Start_NoAvailabeSlots_EmitsCompletedEvent()
+		{
+			_prefabFactory.GetBuildingWrapperPrefab(_key).Returns(_prefab);
+			_cruiser.IsSlotAvailable(_building.SlotType).Returns(false);
 
-//			_task.Start();
+			_task.Start();
 
-//            _cruiser.DidNotReceiveWithAnyArgs().ConstructBuilding(null, null);
-//			Assert.AreEqual(1, _numOfCompletedEvents);
-//		}
+            _cruiser.DidNotReceiveWithAnyArgs().ConstructBuilding(null, null);
+			Assert.AreEqual(1, _numOfCompletedEvents);
+		}
 
-//		[Test]
-//		public void Start_AlreadyCompleted_EmitsCompletedEvent()
-//		{
-//            BuildableCompleted_EmitsCompletedEvent();
+        [Test]
+        public void BuildableCompletedEvent_CausesTaskCompletedEvent()
+        {
+            Start_StartsConstructingBuilding();
 
-//            ResetEventCounter();
-//            _cruiser.ClearReceivedCalls();
+            _building.CompletedBuildable += Raise.Event();
+			Assert.AreEqual(1, _numOfCompletedEvents);
+		}
 
-//            _task.Start();
+        private void _task_Completed(object sender, EventArgs e)
+        {
+            _numOfCompletedEvents++;
+        }
 
-//            _cruiser.DidNotReceiveWithAnyArgs().ConstructBuilding(null, null);
-//			Assert.AreEqual(1, _numOfCompletedEvents);
-//		}
-
-
-//		[Test]
-//		public void Start_InProgressTask_DoesNothing()
-//        {
-//            Start_StartsConstructingBuilding();
-
-//            _cruiser.ClearReceivedCalls();
-//            _task.Start();
-//            _cruiser.DidNotReceiveWithAnyArgs().ConstructBuilding(null, null);
-//        }
-
-//		[Test]
-//        public void BuildableCompleted_EmitsCompletedEvent()
-//        {
-//            Start_StartsConstructingBuilding();
-
-//            _building.CompletedBuildable += Raise.Event();
-//            Assert.AreEqual(1, _numOfCompletedEvents);
-//        }
-
-//        private void _task_Completed(object sender, EventArgs e)
-//        {
-//            _numOfCompletedEvents++;
-//        }
-
-//        private void ResetEventCounter()
-//        {
-//            _numOfCompletedEvents = 0;
-//        }
-//    }
-//}
+        private void ResetEventCounter()
+        {
+            _numOfCompletedEvents = 0;
+        }
+    }
+}
--- Assets/Scripts/AI/Tasks/ConstructBuildingTask.cs
+++ Assets/Scripts/AI/Tasks/ConstructBuildingTask.cs
@@ -1,62 +1,71 @@
-﻿//using System;
-//using BattleCruisers.Buildables;
-//using BattleCruisers.Buildables.Buildings;
-//using BattleCruisers.Cruisers;
-//using BattleCruisers.Data.PrefabKeys;
-//using BattleCruisers.Fetchers;
-//using UnityEngine.Assertions;
+﻿using System;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Data.PrefabKeys;
+using BattleCruisers.Fetchers;
+using UnityEngine.Assertions;
 
-//namespace BattleCruisers.AI.Tasks
-//{
-//    public class ConstructBuildingTask : TaskController
-//    {
-//        private readonly IPrefabKey _key;
-//        private readonly IPrefabFactory _prefabFactory;
-//        private readonly ICruiserController _cruiser;
+namespace BattleCruisers.AI.Tasks
+{
+    public class ConstructBuildingTask : IInternalTask
+    {
+        private readonly IPrefabKey _key;
+        private readonly IPrefabFactory _prefabFactory;
+        private readonly ICruiserController _cruiser;
 
-//        private IBuildable _building;
+        private IBuildable _building;
+		
+		public event EventHandler Completed;
 
-//        public ConstructBuildingTask(TaskPriority priority, IPrefabKey key, IPrefabFactory prefabFactory, ICruiserController cruiser) 
-//            : base(priority)
-//        {
-//            _key = key;
-//            _prefabFactory = prefabFactory;
-//            _cruiser = cruiser;
-//        }
+        public ConstructBuildingTask(IPrefabKey key, IPrefabFactory prefabFactory, ICruiserController cruiser) 
+        {
+            _key = key;
+            _prefabFactory = prefabFactory;
+            _cruiser = cruiser;
+        }
 
-//        // FELIX  States to avoid branching?
-//        public override void Start()
-//        {
-//            base.Start();
+        public void Start()
+        {
+            IBuildableWrapper<IBuilding> buildingWrapperPrefab = _prefabFactory.GetBuildingWrapperPrefab(_key);
 
-//            if (_isCompleted)
-//            {
-//                return;
-//            }
-
-//            IBuildableWrapper<IBuilding> buildingWrapperPrefab = _prefabFactory.GetBuildingWrapperPrefab(_key);
-
-//            if (_cruiser.IsSlotAvailable(buildingWrapperPrefab.Buildable.SlotType))
-//            {
-//				ISlot slot = _cruiser.GetFreeSlot(buildingWrapperPrefab.Buildable.SlotType);
-//				Assert.IsNotNull(slot);
+            if (_cruiser.IsSlotAvailable(buildingWrapperPrefab.Buildable.SlotType))
+            {
+				ISlot slot = _cruiser.GetFreeSlot(buildingWrapperPrefab.Buildable.SlotType);
+				Assert.IsNotNull(slot);
 				
-//				_building = _cruiser.ConstructBuilding(buildingWrapperPrefab.UnityObject, slot);
-//				_building.CompletedBuildable += Building_CompletedBuildable;
-//            }
-//            else
-//            {
-//				// Cruiser has no available slot for this building.  Task is completed (perhaps with a failure result?).
-//				_isCompleted = true;
-//				EmitCompletedEvent();
-//            }
-//        }
+                _building = _cruiser.ConstructBuilding(buildingWrapperPrefab.UnityObject, slot);
+				_building.CompletedBuildable += Building_CompletedBuildable;
+            }
+            else
+            {
+				// Cruiser has no available slot for this building.  Task is completed (perhaps with a failure result?).
+				EmitCompletedEvent();
+            }
+        }
 
-//        private void Building_CompletedBuildable(object sender, EventArgs e)
-//        {
-//            _building.CompletedBuildable -= Building_CompletedBuildable;
-//            _isCompleted = true;
-//            EmitCompletedEvent();
-//        }
-//    }
-//}
+        public void Stop()
+        {
+            // Empty
+        }
+
+		public void Resume()
+		{
+			// Emtpy
+		}
+
+		private void Building_CompletedBuildable(object sender, EventArgs e)
+        {
+            _building.CompletedBuildable -= Building_CompletedBuildable;
+            EmitCompletedEvent();
+        }
+
+        private void EmitCompletedEvent()
+        {
+            if (Completed != null)
+            {
+                Completed.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
