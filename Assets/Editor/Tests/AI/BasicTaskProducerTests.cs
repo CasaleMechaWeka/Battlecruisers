f226ebd77fe67e2eb90dfb777efdbd1c6b005b06
the_mann <felix.fei.mann@gmail.com>
2017-07-26 22:02:24 -0700

    Created BasicTaskProducerTests stub :)

---
+++ Assets/Editor/Tests/AI/BasicTaskProducerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.AI;
+using BattleCruisers.AI.Tasks;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Data.PrefabKeys;
+using BattleCruisers.Fetchers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.AI
+{
+    public class BasicTaskProducerTests
+	{
+        private ITaskList _tasks;
+        private ICruiserController _cruiser;
+        private IPrefabFactory _prefabFactory;
+        private ITaskFactory _taskFactory;
+
+		[SetUp]
+		public void SetuUp()
+		{
+            IList<IPrefabKey> buildOrder;
+
+			_tasks = Substitute.For<ITaskList>();
+            _cruiser = Substitute.For<ICruiserController>();
+
+            // FELIX  NEXT
+		}
+
+		[Test]
+		public void SweetTest()
+		{
+		}
+	}
+}
