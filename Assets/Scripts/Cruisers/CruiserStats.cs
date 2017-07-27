e34220bdcd7957b6dd695a9e7a4abb1f8f376ca8
the_mann <felix.fei.mann@gmail.com>
2017-07-26 19:58:21 -0700

    Created CruiserStats class :)

---
+++ Assets/Scripts/Cruisers/CruiserStats.cs
@@ -0,0 +1,28 @@
+﻿namespace BattleCruisers.Cruisers
+{
+	public class CruiserStats : ICruiserStats
+	{
+		public int Health { get; private set; }
+		public int NumOfDrones { get; private set; }
+		public int NumOfPlatformSlots { get; private set; }
+		public int NumOfDeckSlots { get; private set; }
+		public int NumOfUtilitySlots { get; private set; }
+		public int NumOfMastSlots { get; private set; }
+
+		public CruiserStats(
+			int health,
+			int numOfDrones,
+			int numOfPlatformSlots,
+			int numOfDeckSlots,
+			int numOfUtilitySlots,
+			int numOfMastSlots)
+		{
+            Health = health;
+            NumOfDrones = numOfDrones;
+            NumOfPlatformSlots = numOfPlatformSlots;
+            NumOfDeckSlots = numOfDeckSlots;
+            NumOfUtilitySlots = numOfUtilitySlots;
+            NumOfMastSlots = numOfMastSlots;
+		}
+	}
+}
---
+++ Assets/Scripts/Cruisers/ICruiserStats.cs
@@ -0,0 +1,12 @@
+﻿namespace BattleCruisers.Cruisers
+{
+    public interface ICruiserStats
+    {
+        int Health { get; }
+        int NumOfDrones { get; }
+        int NumOfPlatformSlots { get; }
+        int NumOfDeckSlots { get; }
+        int NumOfUtilitySlots { get; }
+        int NumOfMastSlots { get; }
+    }
+}
