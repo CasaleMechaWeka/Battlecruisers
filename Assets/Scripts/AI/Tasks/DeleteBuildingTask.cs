02d8906d22c1fa10d0c1b6644047b10f2c7fe9d3
the_mann <felix.fei.mann@gmail.com>
2017-07-26 18:45:09 -0700

    Created DeleteBuildingTask :)

---
+++ Assets/Scripts/AI/Tasks/DeleteBuildingTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+
+namespace BattleCruisers.AI.Tasks
+{
+    public class DeleteBuildingTask : IInternalTask
+	{
+		private readonly IBuilding _building;
+
+		public event EventHandler Completed;
+
+        public DeleteBuildingTask(IBuilding building)
+		{
+            _building = building;
+		}
+
+		public void Start()
+		{
+            _building.Destroyed += _building_Destroyed;
+
+            _building.InitiateDelete();
+		}
+
+        private void _building_Destroyed(object sender, DestroyedEventArgs e)
+        {
+            _building.Destroyed -= _building_Destroyed;
+
+			if (Completed != null)
+			{
+				Completed.Invoke(this, EventArgs.Empty);
+			}
+        }
+
+        public void Stop()
+		{
+			// Empty
+		}
+
+		public void Resume()
+		{
+			// Emtpy
+		}
+	}
+}
