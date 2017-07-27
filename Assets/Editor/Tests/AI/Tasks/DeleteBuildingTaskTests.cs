099e25b50ec99b8aa13b003b9fa7e61f9ef05f35
the_mann <felix.fei.mann@gmail.com>
2017-07-26 18:55:12 -0700

    Created DeleteBuildingTaskTests :)

---
+++ Assets/Editor/Tests/AI/Tasks/DeleteBuildingTaskTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using BattleCruisers.AI.Tasks;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.AI.Tasks
+{
+    public class DeleteBuildingTaskTests
+    {
+        private IInternalTask _task;
+        private IBuilding _building;
+        private int _numOfCompletedEvents;
+
+        [SetUp]
+        public void SetuUp()
+        {
+			_building = Substitute.For<IBuilding>();
+
+            _task = new DeleteBuildingTask(_building);
+
+            _task.Completed += _task_Completed;
+
+            _numOfCompletedEvents = 0;
+        }
+
+        [Test]
+        public void Start_InitiatesBuildingDelete()
+        {
+            _task.Start();
+            _building.Received().InitiateDelete();
+        }
+
+        [Test]
+        public void BuildingDestroyedEvent_CausesTaskCompletedEvent()
+        {
+            Start_InitiatesBuildingDelete();
+
+            _building.Destroyed += Raise.EventWith(_building, new DestroyedEventArgs(_building));
+            Assert.AreEqual(1, _numOfCompletedEvents);
+        }
+
+		private void _task_Completed(object sender, EventArgs e)
+		{
+			_numOfCompletedEvents++;
+		}
+	}
+}
