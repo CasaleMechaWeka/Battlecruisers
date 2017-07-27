67fab3730af51b8bde0aad5ab695ee07f425def8
the_mann <felix.fei.mann@gmail.com>
2017-07-26 21:51:10 -0700

    Created BasicTaskProducer:)

---
+++ Assets/Scripts/AI/BasicTaskProducer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.AI.Tasks;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Data.PrefabKeys;
+using BattleCruisers.Fetchers;
+
+namespace BattleCruisers.AI
+{
+    /// <summary>
+    /// 1. Uses preset build order
+    /// 2. Replaces destroyed buildings
+    /// </summary>
+    public class BasicTaskProducer : ITaskProducer
+    {
+        private readonly ITaskList _tasks;
+        private readonly ICruiserController _cruiser;
+        private readonly IPrefabFactory _prefabFactory;
+
+        public BasicTaskProducer(ITaskList tasks, ICruiserController cruiser, IPrefabFactory prefabFactory, IList<IPrefabKey> buildOrder)
+        {
+            _tasks = tasks;
+            _cruiser = cruiser;
+            _prefabFactory = prefabFactory;
+
+            CreateTasks(buildOrder);
+        }
+
+        private void CreateTasks(IList<IPrefabKey> buildOrder)
+        {
+            IDictionary<SlotType, int> slotTypeToBuildingCount = new Dictionary<SlotType, int>();
+
+            foreach (IPrefabKey key in buildOrder)
+            {
+                IBuildableWrapper<IBuilding> buildingWrapper = _prefabFactory.GetBuildingWrapperPrefab(key);
+
+                if (ShouldCreateTask(slotTypeToBuildingCount, buildingWrapper.Buildable.SlotType))
+                {
+                    IInternalTask constructBuildingTask = new ConstructBuildingTask(key, _prefabFactory, _cruiser);
+                    _tasks.Add(new TaskController(TaskPriority.Normal, constructBuildingTask));
+                }
+            }
+        }
+
+        private bool ShouldCreateTask(IDictionary<SlotType, int> slotTypeToBuildingCount, SlotType slotType)
+        {
+            bool shouldCreateTask = false;
+
+			if (!slotTypeToBuildingCount.ContainsKey(slotType))
+			{
+                slotTypeToBuildingCount.Add(slotType, 0);
+			}
+
+            if (slotTypeToBuildingCount[slotType] < _cruiser.GetSlotCount(slotType))
+            {
+                slotTypeToBuildingCount[slotType]++;
+                shouldCreateTask = true;
+            }
+
+            return shouldCreateTask;
+        }
+    }
+}
