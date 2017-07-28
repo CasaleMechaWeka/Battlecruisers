ef86ffb9926d39a969e65fb70259caf0613040d6
the_mann <felix.fei.mann@gmail.com>
2017-07-27 20:45:47 -0700

    Created first test for BasicTaskProducer :D

--- Assets/Editor/Tests/AI/BasicTaskProducerTests.cs
+++ Assets/Editor/Tests/AI/BasicTaskProducerTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using BattleCruisers.AI;
 using BattleCruisers.AI.Tasks;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
 using BattleCruisers.Cruisers;
 using BattleCruisers.Data.PrefabKeys;
 using BattleCruisers.Fetchers;
@@ -15,21 +17,55 @@ namespace BattleCruisers.Tests.AI
         private ICruiserController _cruiser;
         private IPrefabFactory _prefabFactory;
         private ITaskFactory _taskFactory;
+        private IList<IPrefabKey> _buildOrder;
+        private IBuildableWrapper<IBuilding> _platformSlotBuildingWrapper, _deckSlotBuildingWrapper;
+        private IBuilding _platformSlotBuilding, _deckSlotBuilding;
+		private IPrefabKey _platformBuildingKey, _deckBuildingKey;
+        private ITask _platformBuildingTask, _deckBuildingTask;
 
 		[SetUp]
 		public void SetuUp()
 		{
-            IList<IPrefabKey> buildOrder;
-
 			_tasks = Substitute.For<ITaskList>();
             _cruiser = Substitute.For<ICruiserController>();
+            _prefabFactory = Substitute.For<IPrefabFactory>();
+            _taskFactory = Substitute.For<ITaskFactory>();
+            _buildOrder = new List<IPrefabKey>();
 
-            // FELIX  NEXT
+            _platformSlotBuilding = Substitute.For<IBuilding>();
+            _platformSlotBuilding.SlotType.Returns(SlotType.Platform);
+            _platformSlotBuildingWrapper = Substitute.For<IBuildableWrapper<IBuilding>>();
+            _platformSlotBuildingWrapper.Buildable.Returns(_platformSlotBuilding);
+            _platformBuildingKey = Substitute.For<IPrefabKey>();
+            _prefabFactory.GetBuildingWrapperPrefab(_platformBuildingKey).Returns(_platformSlotBuildingWrapper);
+            _platformBuildingTask = Substitute.For<ITask>();
+            _taskFactory.CreateConstructBuildingTask(TaskPriority.Normal, _platformBuildingKey).Returns(_platformBuildingTask);
+
+            _deckSlotBuilding = Substitute.For<IBuilding>();
+            _deckSlotBuilding.SlotType.Returns(SlotType.Deck);
+            _deckSlotBuildingWrapper = Substitute.For<IBuildableWrapper<IBuilding>>();
+            _deckSlotBuildingWrapper.Buildable.Returns(_deckSlotBuilding);
+            _deckBuildingKey = Substitute.For<IPrefabKey>();
+            _prefabFactory.GetBuildingWrapperPrefab(_deckBuildingKey).Returns(_deckSlotBuildingWrapper);
+            _deckBuildingTask = Substitute.For<ITask>();
+            _taskFactory.CreateConstructBuildingTask(TaskPriority.Normal, _deckBuildingKey).Returns(_deckBuildingTask);
 		}
 
 		[Test]
-		public void SweetTest()
-		{
-		}
-	}
+		public void SingleKey_CreatesSingleTask()
+        {
+            _buildOrder.Add(_platformBuildingKey);
+
+            _cruiser.GetSlotCount(SlotType.Platform).Returns(1);
+
+            CreateTaskProducer();
+
+            _tasks.Received().Add(_platformBuildingTask);
+        }
+
+        private void CreateTaskProducer()
+        {
+            new BasicTaskProducer(_tasks, _cruiser, _prefabFactory, _taskFactory, _buildOrder);
+        }
+    }
 }
