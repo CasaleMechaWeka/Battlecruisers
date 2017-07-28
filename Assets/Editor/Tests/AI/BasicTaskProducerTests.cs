c0cbb2bd29b816e7186b5971ccafde2e9257c699
the_mann <felix.fei.mann@gmail.com>
2017-07-27 20:53:08 -0700

    Completed BasicTaskProducerTests :)

--- Assets/Editor/Tests/AI/BasicTaskProducerTests.cs
+++ Assets/Editor/Tests/AI/BasicTaskProducerTests.cs
@@ -63,6 +63,48 @@ namespace BattleCruisers.Tests.AI
             _tasks.Received().Add(_platformBuildingTask);
         }
 
+		[Test]
+		public void MultipleKeys_CreatesMultipleTask()
+		{
+			_buildOrder.Add(_platformBuildingKey);
+            _buildOrder.Add(_deckBuildingKey);
+
+			_cruiser.GetSlotCount(SlotType.Platform).Returns(1);
+            _cruiser.GetSlotCount(SlotType.Deck).Returns(1);
+
+			CreateTaskProducer();
+
+			_tasks.Received().Add(_platformBuildingTask);
+            _tasks.Received().Add(_deckBuildingTask);
+		}
+
+		[Test]
+        public void SingleKey_CruiserDoesNotHaveCapacity_DoesNotCreateTask()
+        {
+            _buildOrder.Add(_platformBuildingKey);
+
+			_cruiser.GetSlotCount(SlotType.Platform).Returns(0);
+
+			CreateTaskProducer();
+
+            _tasks.DidNotReceive().Add(_platformBuildingTask);
+        }
+
+		[Test]
+		public void MultipleKeys_CruiserDoesNotHaveCapacity_OnlyCreatesTaskForKeysWithCapacity()
+		{
+			_buildOrder.Add(_platformBuildingKey);
+			_buildOrder.Add(_deckBuildingKey);
+
+			_cruiser.GetSlotCount(SlotType.Platform).Returns(1);
+			_cruiser.GetSlotCount(SlotType.Deck).Returns(0);
+
+			CreateTaskProducer();
+
+			_tasks.Received().Add(_platformBuildingTask);
+            _tasks.DidNotReceive().Add(_deckBuildingTask);
+		}
+
         private void CreateTaskProducer()
         {
             new BasicTaskProducer(_tasks, _cruiser, _prefabFactory, _taskFactory, _buildOrder);
