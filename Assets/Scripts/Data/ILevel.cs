b65dd55083f407882dae6758b682732497c920f6
the_mann <felix.fei.mann@gmail.com>
2017-07-29 16:47:00 -0700

    Moved ILevel to own file :)

---
+++ Assets/Scripts/Data/ILevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Data.PrefabKeys;
+
+namespace BattleCruisers.Data
+{
+    public interface ILevel
+    {
+        Loadout AiLoadout { get; }
+        string Name { get; }
+        IList<IPrefabKey> BuildOrder { get; }
+    }
+}
--- Assets/Scripts/Data/Level.cs
+++ Assets/Scripts/Data/Level.cs
@@ -3,13 +3,6 @@ using BattleCruisers.Data.PrefabKeys;
 
 namespace BattleCruisers.Data
 {
-    public interface ILevel
-	{
-		Loadout AiLoadout { get; }
-		string Name { get; }
-        IList<IPrefabKey> BuildOrder { get; }
-	}
-
     // FELIX  Eventually add:
     // + Loot given on accomplishment
     // + Level specific graphics (ie, background image, lighting perhaps?)
