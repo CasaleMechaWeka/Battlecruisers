c739e0546856731fd237f8fa34a93a94f31c1f1c
the_mann <felix.fei.mann@gmail.com>
2017-07-28 22:15:46 -0700

    Created StaticPrefabKeys :)

---
+++ Assets/Scripts/Data/StaticPrefabKeys.cs
@@ -0,0 +1,34 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Data.PrefabKeys;
+
+namespace BattleCruisers.Data
+{
+	public static class StaticPrefabKeys
+	{
+		public static class Buildings
+		{
+			// Factories
+			public static BuildingKey AirFactory { get { return new BuildingKey(BuildingCategory.Factory, "AirFactory"); } }
+			public static BuildingKey NavalFactory { get { return new BuildingKey(BuildingCategory.Factory, "NavalFactory"); } }
+			public static BuildingKey DroneStation { get { return new BuildingKey(BuildingCategory.Factory, "EngineeringBay"); } }
+
+			// Tactical
+			public static BuildingKey ShieldGenerator { get { return new BuildingKey(BuildingCategory.Tactical, "ShieldGenerator"); } }
+
+			// Defence
+			public static BuildingKey AntiShipTurret { get { return new BuildingKey(BuildingCategory.Defence, "AntiShipTurret"); } }
+			public static BuildingKey AntiAirTurret { get { return new BuildingKey(BuildingCategory.Defence, "AntiAirTurret"); } }
+			public static BuildingKey Mortar { get { return new BuildingKey(BuildingCategory.Defence, "Mortar"); } }
+			public static BuildingKey TeslaCoil { get { return new BuildingKey(BuildingCategory.Defence, "TeslaCoil"); } }
+
+			// Offence
+			public static BuildingKey Artillery { get { return new BuildingKey(BuildingCategory.Offence, "Artillery"); } }
+			public static BuildingKey RocketLauncher { get { return new BuildingKey(BuildingCategory.Offence, "RocketLauncher"); } }
+			public static BuildingKey Railgun { get { return new BuildingKey(BuildingCategory.Offence, "Railgun"); } }
+
+			// Ultras
+			public static BuildingKey DeathstarLauncher { get { return new BuildingKey(BuildingCategory.Ultra, "DeathstarLauncher"); } }
+            // FELIX  Nuke
+		}
+	}
+}
