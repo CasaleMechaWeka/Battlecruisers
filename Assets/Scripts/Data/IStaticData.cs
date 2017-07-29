34750d10c42b978014251d25e3f36d7584942740
the_mann <felix.fei.mann@gmail.com>
2017-07-29 16:46:06 -0700

    Moved IStaticData to own file :)

---
+++ Assets/Scripts/Data/IStaticData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using BattleCruisers.Data.PrefabKeys;
+
+namespace BattleCruisers.Data
+{
+    /// <summary>
+    /// Provides data that does not change throughout the game.
+    /// 
+    /// This is in contrast to the GameModel, which changes as the player
+    /// progresses and unlocks new prefabs.
+    /// </summary>
+    public interface IStaticData
+    {
+        GameModel InitialGameModel { get; }
+        IList<ILevel> Levels { get; }
+        ReadOnlyCollection<IPrefabKey> BuildingKeys { get; }
+    }
+}
+	
--- Assets/Scripts/Data/StaticData.cs
+++ Assets/Scripts/Data/StaticData.cs
@@ -7,20 +7,6 @@ using BattleCruisers.Data.PrefabKeys;
 
 namespace BattleCruisers.Data
 {
-    /// <summary>
-    /// Provides data that does not change throughout the game.
-    /// 
-    /// This is in contrast to the GameModel, which changes as the player
-    /// progresses and unlocks new prefabs.
-    /// </summary>
-    /// FELIX  Own file
-    public interface IStaticData
-	{
-		GameModel InitialGameModel { get; }
-		IList<ILevel> Levels { get; }
-        ReadOnlyCollection<IPrefabKey> BuildingKeys { get; }
-	}
-
 	public class StaticData : IStaticData
 	{
 		public GameModel InitialGameModel { get; private set; }
