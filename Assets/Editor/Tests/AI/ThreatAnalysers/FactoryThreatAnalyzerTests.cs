2e58e812ca8dbb53ccc44de2b5e217ef25d06bcf
the_mann <felix.fei.mann@gmail.com>
2017-07-30 15:25:38 -0700

    Created first test case for FactoryThreatAnalyser :)

---
+++ Assets/Editor/Tests/AI/ThreatAnalysers/FactoryThreatAnalyzerTests.cs
@@ -0,0 +1,39 @@
+﻿using BattleCruisers.AI.ThreatAnalysers;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Cruisers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.AI
+{
+    public class FactoryThreatAnalyzerTests
+    {
+        private IThreatAnalyser _threatAnalyzer;
+        private ICruiserController _cruiser;
+        private IFactory _threateningFactory, _nonThreateningFactory;
+        private IBuilding _nonFactoryBuilding;
+        private UnitCategory _threatCategory, _nonThreatCategory;
+
+        [SetUp]
+        public void SetuUp()
+        {
+            _cruiser = Substitute.For<ICruiserController>();
+            _threatAnalyzer = new FactoryThreatAnalyzer(_cruiser, _threatCategory);
+
+            _threateningFactory = Substitute.For<IFactory>();
+            _threateningFactory.NumOfDrones.Returns(7);
+            _threateningFactory.UnitCategory.Returns(_threatCategory);
+
+            _nonThreateningFactory = Substitute.For<IFactory>();
+            _nonThreateningFactory.UnitCategory.Returns(_nonThreatCategory);
+        }
+
+        [Test]
+        public void InitialThreatLevel()
+        {
+            Assert.AreEqual(ThreatLevel.None, _threatAnalyzer.CurrentThreatLevel);
+        }
+    }
+}
