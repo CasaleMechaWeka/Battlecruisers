a2d6b292170f183ca080c76c6c34e2c4491ac624
the_mann <felix.fei.mann@gmail.com>
2017-07-31 19:22:16 -0700

    Created SlotNumCalculattorFactory :)

---
+++ Assets/Scripts/AI/TaskProducers/SlotNumber/ISlotNumCalculatorFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace BattleCruisers.AI.TaskProducers.SlotNumber
+{
+    public interface ISlotNumCalculatorFactory
+    {
+        ISlotNumCalculator CreateAntiAirSlotNumCalculator(int maxNumOfSlots);
+    }
+}
---
+++ Assets/Scripts/AI/TaskProducers/SlotNumber/SlotNumCalculatorFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleCruisers.AI.TaskProducers.SlotNumber
+{
+	public class SlotNumCalculatorFactory : ISlotNumCalculatorFactory
+	{
+		public ISlotNumCalculator CreateAntiAirSlotNumCalculator(int maxNumOfSlots)
+        {
+            return new AntiAirSlotNumCalculator(maxNumOfSlots);
+        }
+	}
+}
