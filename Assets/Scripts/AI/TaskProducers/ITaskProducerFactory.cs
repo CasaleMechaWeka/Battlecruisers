417525ad4aa4578de3f835efb1ad75c431b9d3c2
the_mann <felix.fei.mann@gmail.com>
2017-07-31 21:15:01 -0700

    Created TaskProducerFactory.CreateAntiAirTaskProducer() :)

--- Assets/Scripts/AI/TaskProducers/ITaskProducerFactory.cs
+++ Assets/Scripts/AI/TaskProducers/ITaskProducerFactory.cs
@@ -1,14 +1,11 @@
 ﻿using System.Collections.Generic;
-using BattleCruisers.AI.Tasks;
-using BattleCruisers.Cruisers;
 using BattleCruisers.Data.Models.PrefabKeys;
-using BattleCruisers.Fetchers;
 
 namespace BattleCruisers.AI.TaskProducers
 {
     public interface ITaskProducerFactory
     {
-		void CreateReplaceDestroyedBuildingsTaskProducer(ITaskList tasks, ICruiserController cruiser,
-			IPrefabFactory prefabFactory, ITaskFactory taskFactory, IList<IPrefabKey> unlockedBuildingKeys);
-    }
+        void CreateReplaceDestroyedBuildingsTaskProducer(ITaskList tasks, IList<IPrefabKey> unlockedBuildingKeys);
+        void CreateAntiAirTaskProducer(ITaskList tasks);
+	}
 }
--- Assets/Scripts/AI/TaskProducers/TaskProducerFactory.cs
+++ Assets/Scripts/AI/TaskProducers/TaskProducerFactory.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
+using BattleCruisers.AI.TaskProducers.SlotNumber;
 using BattleCruisers.AI.Tasks;
+using BattleCruisers.AI.ThreatMonitors;
+using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Cruisers;
+using BattleCruisers.Data.BuildOrders;
 using BattleCruisers.Data.Models.PrefabKeys;
 using BattleCruisers.Fetchers;
 
@@ -8,10 +12,34 @@ namespace BattleCruisers.AI.TaskProducers
 {
     public class TaskProducerFactory : ITaskProducerFactory
 	{
-		public void CreateReplaceDestroyedBuildingsTaskProducer(ITaskList tasks, ICruiserController cruiser,
-            IPrefabFactory prefabFactory, ITaskFactory taskFactory, IList<IPrefabKey> unlockedBuildingKeys)
+        private readonly ICruiserController _aiCruiser, _playerCruiser;
+        private readonly IPrefabFactory _prefabFactory;
+        private readonly ITaskFactory _taskFactory;
+        private ISlotNumCalculatorFactory _slotNumCalculatorFactory;
+
+        private const int AIR_HIGH_THREAT_DRONE_NUM = 6;
+
+        public TaskProducerFactory(ICruiserController aiCruiser, ICruiserController playerCruiser, IPrefabFactory prefabFactory, 
+            ITaskFactory taskFactory, ISlotNumCalculatorFactory slotNumCalculatorFactory)
         {
-            new ReplaceDestroyedBuildingsTaskProducer(tasks, cruiser, prefabFactory, taskFactory, unlockedBuildingKeys);
+            _aiCruiser = aiCruiser;
+            _playerCruiser = playerCruiser;
+            _prefabFactory = prefabFactory;
+            _taskFactory = taskFactory;
+            _slotNumCalculatorFactory = slotNumCalculatorFactory;
+        }
+
+		public void CreateReplaceDestroyedBuildingsTaskProducer(ITaskList tasks, IList<IPrefabKey> unlockedBuildingKeys)
+        {
+            new ReplaceDestroyedBuildingsTaskProducer(tasks, _aiCruiser, _prefabFactory, _taskFactory, unlockedBuildingKeys);
+        }
+
+		public void CreateAntiAirTaskProducer(ITaskList tasks)
+        {
+            IThreatEvaluator threatEvaluator = new ThreatEvaluator(AIR_HIGH_THREAT_DRONE_NUM);
+            IThreatMonitor airThreatMonitor = new BuildingThreatMonitor<AirFactory>(_playerCruiser, threatEvaluator);
+
+            new AntiAirTaskProducer(tasks, _aiCruiser, _prefabFactory, _taskFactory, AntiAir.BuildOrder, airThreatMonitor, _slotNumCalculatorFactory);
         }
 	}
 }
