9ad180e23814e89db54b7a63c6b350920f558775
the_mann <felix.fei.mann@gmail.com>
2017-07-31 17:43:44 -0700

    Created AntiAir build order

---
+++ Assets/Scripts/Data/BuildOrders/AntiAir.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.Data.BuildOrders
+{
+	public static class AntiAir
+	{
+		public static IList<IPrefabKey> BuildOrder
+		{
+			get
+			{
+				return new List<IPrefabKey>()
+				{
+					StaticPrefabKeys.Buildings.AntiAirTurret,
+					StaticPrefabKeys.Buildings.SamSite,
+					StaticPrefabKeys.Buildings.SamSite,
+					StaticPrefabKeys.Buildings.SamSite
+				};
+			}
+		}
+	}
+}
