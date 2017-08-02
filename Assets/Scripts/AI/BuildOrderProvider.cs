82157f95f16f7d85bdff6f8be7fc8482519b5672
the_mann <felix.fei.mann@gmail.com>
2017-08-01 22:05:47 -0700

    Extended BuildOrderProvider to handle the anti rocket launcher build order :)

--- Assets/Scripts/AI/BuildOrderProvider.cs
+++ Assets/Scripts/AI/BuildOrderProvider.cs
@@ -8,6 +8,10 @@ namespace BattleCruisers.AI
     {
 		private const int LEVEL_SAM_SITE_IS_UNLOCKED = 2;
 		private const int LEVEL_MORTAR_IS_UNLOCKED = 1;
+		private const int LEVEL_TESLA_COIL_IS_UNLOCKED = 12;
+
+
+        public IList<IPrefabKey> AntiRocketBuildOrder { get { return BuildOrders.AntiRocketLauncher; } }
 
         public IList<IPrefabKey> GetBasicBuildOrder(int levelNum)
         {
@@ -29,6 +33,11 @@ namespace BattleCruisers.AI
         public IList<IPrefabKey> GetAntiNavalBuildOrder(int levelNum)
         {
             return levelNum > LEVEL_MORTAR_IS_UNLOCKED ? BuildOrders.AntiNaval : BuildOrders.BasicAntiNaval;
+        }
+
+        public bool IsAntiRocketBuildOrderAvailable(int levelNum)
+        {
+            return levelNum > LEVEL_TESLA_COIL_IS_UNLOCKED;
         }
     }
 }
--- Assets/Scripts/AI/IBuildOrderProvider.cs
+++ Assets/Scripts/AI/IBuildOrderProvider.cs
@@ -5,9 +5,12 @@ namespace BattleCruisers.AI
 {
     public interface IBuildOrderProvider
     {
-		IList<IPrefabKey> GetBasicBuildOrder(int levelNum);
-		IList<IPrefabKey> GetAdvancedBuildOrder(int levelNum);
-		IList<IPrefabKey> GetAntiAirBuildOrder(int levelNum);
+        IList<IPrefabKey> AntiRocketBuildOrder { get; }
+
+        IList<IPrefabKey> GetBasicBuildOrder(int levelNum);
+        IList<IPrefabKey> GetAdvancedBuildOrder(int levelNum);
+        IList<IPrefabKey> GetAntiAirBuildOrder(int levelNum);
         IList<IPrefabKey> GetAntiNavalBuildOrder(int levelNum);
+		bool IsAntiRocketBuildOrderAvailable(int levelNum);
 	}
 }
