397b9bbf62bea76d58135b5cdc15431a0275fa7a
the_mann <felix.fei.mann@gmail.com>
2017-08-03 21:24:42 -0700

    Created ICruiserFactory :)

---
+++ Assets/Scripts/Cruisers/CruiserFactory.cs
@@ -0,0 +1,37 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Drones;
+using BattleCruisers.Fetchers;
+using BattleCruisers.UI.BattleScene;
+using BattleCruisers.UI.BattleScene.ProgressBars;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Cruisers
+{
+	public class CruiserFactory : ICruiserFactory
+	{
+        private readonly UIManager _uiManager;
+        private readonly IPrefabFactory _prefabFactory;
+
+        public CruiserFactory(UIManager uiManager, IPrefabFactory prefabFactory)
+        {
+            Helper.AssertIsNotNull(uiManager, prefabFactory);
+            
+            _uiManager = uiManager;
+            _prefabFactory = prefabFactory;
+        }
+
+        // FELIX  Use interface for:
+        // + enemyCruiser
+        // + uiManager
+        public void InitialiseCruiser(Cruiser cruiser, Cruiser enemyCruiser, 
+             HealthBarController healthBar, Faction faction, Direction facingDirection)
+        {
+            IFactoryProvider factoryProvider = new FactoryProvider(_prefabFactory, cruiser, enemyCruiser);
+            IDroneManager droneManager = new DroneManager();
+            IDroneConsumerProvider droneConsumerProvider = new DroneConsumerProvider(droneManager);
+            cruiser.Initialise(faction, enemyCruiser, healthBar, _uiManager, droneManager,
+                droneConsumerProvider, factoryProvider, facingDirection);
+        }
+	}
+}
---
+++ Assets/Scripts/Cruisers/ICruiserFactory.cs
@@ -0,0 +1,11 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.UI.BattleScene.ProgressBars;
+
+namespace BattleCruisers.Cruisers
+{
+    public interface ICruiserFactory
+    {
+        void InitialiseCruiser(Cruiser cruiser, Cruiser enemyCruiser, HealthBarController healthBar, Faction faction, Direction facingDirection);       
+    }
+}
