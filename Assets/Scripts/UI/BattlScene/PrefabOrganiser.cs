fc2f4d046893a0e61fef8dbed00f7fbcfbce16b2
the_mann <felix.fei.mann@gmail.com>
2017-08-03 21:01:44 -0700

    Creatd PrefabOrganiser :)

---
+++ Assets/Scripts/UI/BattlScene/PrefabOrganiser.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Data.Models;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.BattleScene
+{
+    // FELIX  Use IPrefabKey intead of concrete Unit/Building Key classes?
+    public class PrefabOrganiser : IPrefabOrganiser
+    {
+        private readonly ILoadout _playerLoadout;
+        private readonly IFactoryProvider _playerFactoryProvider;
+		private readonly IBuildingGroupFactory _buildingGroupFactory;
+
+		// User needs to be able to build at least one building
+		private const int MIN_NUM_OF_BUILDING_GROUPS = 1;
+		// Currently only support 6 types of buildings, so the UI is optimsed for this.  Ie, there is no space for more!
+		private const int MAX_NUM_OF_BUILDING_GROUPS = 6;
+
+        public PrefabOrganiser(ILoadout playerLoadout, IFactoryProvider playerFactoryProvider, IBuildingGroupFactory buildingGroupFactory)
+        {
+            Helper.AssertIsNotNull(playerLoadout, playerFactoryProvider, buildingGroupFactory);
+
+            _playerLoadout = playerLoadout;
+            _playerFactoryProvider = playerFactoryProvider;
+            _buildingGroupFactory = buildingGroupFactory;
+        }
+
+        public IList<IBuildingGroup> GetBuildingGroups()
+        {
+			IDictionary<BuildingCategory, IList<IBuildableWrapper<IBuilding>>> buildings = GetBuildingsFromKeys(_playerLoadout, _playerFactoryProvider);
+            return CreateBuildingGroups(buildings, _buildingGroupFactory);
+
+        }
+
+		private IDictionary<BuildingCategory, IList<IBuildableWrapper<IBuilding>>> GetBuildingsFromKeys(ILoadout loadout, IFactoryProvider factoryProvider)
+		{
+			IDictionary<BuildingCategory, IList<IBuildableWrapper<IBuilding>>> categoryToBuildings = new Dictionary<BuildingCategory, IList<IBuildableWrapper<IBuilding>>>();
+
+			foreach (BuildingCategory category in Enum.GetValues(typeof(BuildingCategory)))
+			{
+				IList<BuildingKey> buildingKeys = loadout.GetBuildings(category);
+
+				if (buildingKeys.Count != 0)
+				{
+					IList<IBuildableWrapper<IBuilding>> buildings = new List<IBuildableWrapper<IBuilding>>();
+					categoryToBuildings[category] = buildings;
+
+					foreach (BuildingKey buildingKey in buildingKeys)
+					{
+						IBuildableWrapper<IBuilding> buildingWrapper = factoryProvider.PrefabFactory.GetBuildingWrapperPrefab(buildingKey).UnityObject;
+						categoryToBuildings[buildingWrapper.Buildable.Category].Add(buildingWrapper);
+					}
+				}
+			}
+
+			return categoryToBuildings;
+		}
+
+		private IList<IBuildingGroup> CreateBuildingGroups(
+            IDictionary<BuildingCategory, IList<IBuildableWrapper<IBuilding>>> buildingCategoryToGroups,
+            IBuildingGroupFactory buildingGroupFactory)
+		{
+			IList<IBuildingGroup> buildingGroups = new List<IBuildingGroup>();
+
+			foreach (KeyValuePair<BuildingCategory, IList<IBuildableWrapper<IBuilding>>> categoryToBuildings in buildingCategoryToGroups)
+			{
+				IBuildingGroup group = buildingGroupFactory.CreateBuildingGroup(categoryToBuildings.Key, categoryToBuildings.Value);
+				buildingGroups.Add(group);
+			}
+
+			if (buildingGroups.Count < MIN_NUM_OF_BUILDING_GROUPS
+				|| buildingGroups.Count > MAX_NUM_OF_BUILDING_GROUPS)
+			{
+				throw new InvalidProgramException();
+			}
+
+			return buildingGroups;
+		}
+
+		public IDictionary<UnitCategory, IList<IBuildableWrapper<IUnit>>> GetUnits()
+		{
+			IDictionary<UnitCategory, IList<IBuildableWrapper<IUnit>>> categoryToUnits = new Dictionary<UnitCategory, IList<IBuildableWrapper<IUnit>>>();
+
+			foreach (UnitCategory unitCategory in Enum.GetValues(typeof(UnitCategory)))
+			{
+				IList<UnitKey> unitKeys = _playerLoadout.GetUnits(unitCategory);
+
+				if (unitKeys.Count != 0)
+				{
+					categoryToUnits[unitCategory] = GetUnits(unitKeys, _playerFactoryProvider);
+				}
+			}
+
+			return categoryToUnits;
+		}
+
+		private IList<IBuildableWrapper<IUnit>> GetUnits(IList<UnitKey> unitKeys, IFactoryProvider factoryProvider)
+		{
+			IList<IBuildableWrapper<IUnit>> unitWrappers = new List<IBuildableWrapper<IUnit>>(unitKeys.Count);
+
+			foreach (UnitKey unitKey in unitKeys)
+			{
+				IBuildableWrapper<IUnit> unitWrapper = factoryProvider.PrefabFactory.GetUnitWrapperPrefab(unitKey);
+				unitWrappers.Add(unitWrapper);
+			}
+
+			return unitWrappers;
+		}
+    }
+}
