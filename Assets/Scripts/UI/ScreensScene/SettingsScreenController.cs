b83c3901e67cd9c2cef59dc01ef0141234daf263
the_mann <felix.fei.mann@gmail.com>
2017-08-04 18:51:07 -0700

    Update settings when user changes AI difficulty :)

--- Assets/Scripts/UI/ScreensScene/SettingsScreenController.cs
+++ Assets/Scripts/UI/ScreensScene/SettingsScreenController.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using BattleCruisers.Data.Models;
 using BattleCruisers.Data.Settings;
 using BattleCruisers.Scenes;
-using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 namespace BattleCruisers.UI.ScreensScene
 {
+    // FELIX  Select right difficulty for dropdown :P
     public class SettingsScreenController : ScreenController
     {
         private ISettingsManager _settingsManager;
+        private IList<Difficulty> _difficulties;
 
 		public Dropdown difficultyDropdown;
 		
@@ -22,17 +22,17 @@ namespace BattleCruisers.UI.ScreensScene
             difficultyDropdown.AddOptions(CreateDropdownOptions());
 
 			difficultyDropdown.onValueChanged.AddListener(OnDropdownChange);
-
-            //settingsManager.
 		}
 
         private List<Dropdown.OptionData> CreateDropdownOptions()
         {
             List<Dropdown.OptionData> options = new List<Dropdown.OptionData>();
+            _difficulties = new List<Difficulty>();
 
             foreach (Difficulty difficulty in Enum.GetValues(typeof(Difficulty)))
 			{
                 options.Add(new Dropdown.OptionData(difficulty.ToString()));
+                _difficulties.Add(difficulty);
 			}
 
             return options;
@@ -40,9 +40,8 @@ namespace BattleCruisers.UI.ScreensScene
 
         private void OnDropdownChange(int newIndex)
         {
-            Debug.Log("newIndex: " + newIndex);
-
-
+            Assert.IsTrue(newIndex < _difficulties.Count);
+            _settingsManager.AIDifficulty = _difficulties[newIndex];
         }
 	}
 }
