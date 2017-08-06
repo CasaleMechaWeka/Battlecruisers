0fd000f3ba91608301392bfacd8d2d82bb0af76e
the_mann <felix.fei.mann@gmail.com>
2017-08-05 18:59:31 -0700

    Created BuildingKeyProvider :)

---
+++ Assets/Scripts/AI/BuildOrderProvider/BuildingKeyProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Data;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.DataStrctures;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.AI.Providers
+{
+    public class BuildingKeyProvider : IBuildingKeyProvider
+    {
+        private readonly ICircularList<IPrefabKey> _buildingKeys;
+
+        public BuildingKeyProvider(IStaticData staticData, BuildingCategory buildingCategory, int levelNum)
+        {
+            IList<IPrefabKey> buildingKeys = staticData.GetAvailableBuildings(buildingCategory, levelNum);
+
+            Assert.IsTrue(buildingKeys.Count != 0);
+
+            buildingKeys.Shuffle();
+
+            _buildingKeys = new CircularList<IPrefabKey>(buildingKeys);
+        }
+
+        public IPrefabKey Next()
+        {
+            return _buildingKeys.Next();
+        }
+    }
+}
---
+++ Assets/Scripts/AI/BuildOrderProvider/IBuildingKeyProvider.cs
@@ -0,0 +1,9 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.AI.Providers
+{
+    public interface IBuildingKeyProvider
+    {
+        IPrefabKey Next();
+    }
+}
