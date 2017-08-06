a8267d9c8ed7f7395dbbab168c39b301ab442351
the_mann <felix.fei.mann@gmail.com>
2017-08-06 14:57:18 -0700

    Craeted base strategies :()

---
+++ Assets/Scripts/AI/Providers/Strategies/BoomStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Data.Static;
+
+namespace BattleCruisers.AI.Providers.Strategies
+{
+    public class BoomStrategy : IBaseStrategy
+    {
+        public IList<IPrefabKeyWrapper> BuildOrder { get { return StaticBuildOrders.Adaptive.Boom; } }
+    }
+}
---
+++ Assets/Scripts/AI/Providers/Strategies/IBaseStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.AI.Providers.Strategies
+{
+    public interface IBaseStrategy
+    {
+        IList<IPrefabKeyWrapper> BuildOrder { get; }
+    }
+}
