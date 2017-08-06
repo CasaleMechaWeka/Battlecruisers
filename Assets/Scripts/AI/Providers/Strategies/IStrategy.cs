a672825238b6f93e62c484c19c32c754e87fa965
the_mann <felix.fei.mann@gmail.com>
2017-08-06 15:10:12 -0700

    Created Strategy class :)

---
+++ Assets/Scripts/AI/Providers/Strategies/IStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BattleCruisers.AI.Providers.Strategies
+{
+    public interface IStrategy
+    {
+        IBaseStrategy BaseStrategy { get; }
+        IEnumerable<IBasicOffensiveRequest> Offensives { get; }
+    }
+}
---
+++ Assets/Scripts/AI/Providers/Strategies/Strategy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.AI.Providers.Strategies
+{
+    public class Strategy : IStrategy
+    {
+        public IBaseStrategy BaseStrategy { get; private set; }
+        public IEnumerable<IBasicOffensiveRequest> Offensives { get; private set; }
+
+        public Strategy(IBaseStrategy baseStrategy, params IBasicOffensiveRequest[] offensives)
+        {
+            Assert.IsNotNull(baseStrategy);
+            Assert.IsTrue(offensives.Length != 0);
+
+            BaseStrategy = baseStrategy;
+            Offensives = offensives;
+        }
+    }
+}
