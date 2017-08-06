c67e4ba9729948e6ccffc2abb31bd5d678bcaa5a
the_mann <felix.fei.mann@gmail.com>
2017-08-06 15:28:00 -0700

    Created static LevelStrategies for levels 1-14 :)

---
+++ Assets/Scripts/Data/Static/LevelStrategies.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.AI.Providers.Strategies;
+
+namespace BattleCruisers.Data.Static
+{
+    public static class LevelStrategies
+    {
+        private static IList<IStrategy> _strategies;
+        public static IList<IStrategy> Strategies
+        {
+            get
+            {
+                if (_strategies == null)
+                {
+                    _strategies = CreateStrategies();
+                }
+                return _strategies;
+            }
+        }
+
+        private static IList<IStrategy> CreateStrategies()
+        {
+            return new List<IStrategy>()
+            {
+                // Set 1:  Levels 1 - 7
+                new Strategy(
+                    new BalancedStrategy(), 
+                    new BasicOffensiveRequest(OffensiveType.Naval, OffensiveFocus.Low)),
+				new Strategy(
+                    new BalancedStrategy(), 
+                    new BasicOffensiveRequest(OffensiveType.Air, OffensiveFocus.Low)),
+				new Strategy(
+                    new RushStrategy(),
+					new BasicOffensiveRequest(OffensiveType.Naval, OffensiveFocus.Low),
+                    new BasicOffensiveRequest(OffensiveType.Buildings, OffensiveFocus.Low)),
+				new Strategy(
+                    new BoomStrategy(),
+                    new BasicOffensiveRequest(OffensiveType.Air, OffensiveFocus.Low),
+                    new BasicOffensiveRequest(OffensiveType.Buildings, OffensiveFocus.High)),
+				new Strategy(
+                    new BoomStrategy(),
+					new BasicOffensiveRequest(OffensiveType.Naval, OffensiveFocus.Low),
+                    new BasicOffensiveRequest(OffensiveType.Air, OffensiveFocus.High)),
+				new Strategy(
+                    new RushStrategy(),
+					new BasicOffensiveRequest(OffensiveType.Naval, OffensiveFocus.Low),
+					new BasicOffensiveRequest(OffensiveType.Air, OffensiveFocus.Low),
+                    new BasicOffensiveRequest(OffensiveType.Buildings, OffensiveFocus.High)),
+				new Strategy(
+					new BalancedStrategy(),
+                    new BasicOffensiveRequest(OffensiveType.Air, OffensiveFocus.High),
+					new BasicOffensiveRequest(OffensiveType.Buildings, OffensiveFocus.High)),
+                
+                // Set 2:  Levels 8 - 14
+                new Strategy(
+                    new BoomStrategy(),
+                    new BasicOffensiveRequest(OffensiveType.Ultras, OffensiveFocus.Low)),
+				new Strategy(
+                    new RushStrategy(),
+                    new BasicOffensiveRequest(OffensiveType.Naval, OffensiveFocus.Low),
+                    new BasicOffensiveRequest(OffensiveType.Air, OffensiveFocus.Low),
+                    new BasicOffensiveRequest(OffensiveType.Buildings, OffensiveFocus.Low)),
+				new Strategy(
+                    new BalancedStrategy(),
+					new BasicOffensiveRequest(OffensiveType.Air, OffensiveFocus.Low),
+                    new BasicOffensiveRequest(OffensiveType.Buildings, OffensiveFocus.High)),
+				new Strategy(
+                    new BoomStrategy(),
+					new BasicOffensiveRequest(OffensiveType.Naval, OffensiveFocus.Low),
+					new BasicOffensiveRequest(OffensiveType.Air, OffensiveFocus.Low),
+					new BasicOffensiveRequest(OffensiveType.Buildings, OffensiveFocus.Low),
+                    new BasicOffensiveRequest(OffensiveType.Ultras, OffensiveFocus.Low)),
+				new Strategy(
+					new RushStrategy(),
+                    new BasicOffensiveRequest(OffensiveType.Air, OffensiveFocus.High)),
+				new Strategy(
+                    new BoomStrategy(),
+                    new BasicOffensiveRequest(OffensiveType.Ultras, OffensiveFocus.Low),
+                    new BasicOffensiveRequest(OffensiveType.Buildings, OffensiveFocus.High)),
+				new Strategy(
+                    new BalancedStrategy(),
+					new BasicOffensiveRequest(OffensiveType.Naval, OffensiveFocus.Low),
+					new BasicOffensiveRequest(OffensiveType.Air, OffensiveFocus.Low),
+                    new BasicOffensiveRequest(OffensiveType.Ultras, OffensiveFocus.Low),
+                    new BasicOffensiveRequest(OffensiveType.Buildings, OffensiveFocus.High)),
+
+                // Set 3:  Levels 15 - 21
+                // FELIX
+            };
+        }
+    }
+}
