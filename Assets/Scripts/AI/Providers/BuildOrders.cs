e7795eb8e41bf85c30ad0b45213a2f876ede1552
the_mann <felix.fei.mann@gmail.com>
2017-08-06 13:35:51 -0700

    Created BuildOrders files :)

---
+++ Assets/Scripts/AI/Providers/BuildOrders.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.AI.Providers
+{
+	public class BuildOrders : IBuildOrders
+	{
+        public IEnumerator<IPrefabKey> OffensiveBuildOrder { get; private set; }
+
+        public BuildOrders(IList<IPrefabKey> offensiveBuildOrder)
+        {
+            OffensiveBuildOrder = offensiveBuildOrder.GetEnumerator();
+        }
+	}
+}
---
+++ Assets/Scripts/AI/Providers/IBuildOrders.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.AI.Providers
+{
+    public interface IBuildOrders
+    {
+		IEnumerator<IPrefabKey> OffensiveBuildOrder { get; }
+
+        // FELIX  Add:  Anti-Air & Anti-Naval
+	}
+}
