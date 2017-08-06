f871ee5e8b6486a0130c5712ef2fea7f1f840249
the_mann <felix.fei.mann@gmail.com>
2017-08-05 19:07:36 -0700

    Created BuildingKeyProviderFactory :)

---
+++ Assets/Scripts/AI/Providers/BuildingKeyProviderFactory.cs
@@ -0,0 +1,22 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Data;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.AI.Providers
+{
+    public class BuildingKeyProviderFactory
+    {
+        private readonly IStaticData _staticData;
+
+        public BuildingKeyProviderFactory(IStaticData staticData)
+        {
+            Assert.IsNotNull(staticData);
+            _staticData = staticData;
+        }
+
+        public IBuildingKeyProvider CreateBuildingKeyProvider(BuildingCategory buildingCategory, int levelNum)
+        {
+            return new BuildingKeyProvider(_staticData, buildingCategory, levelNum);
+        }
+	}
+}
---
+++ Assets/Scripts/AI/Providers/IBuildingKeyProviderFactory.cs
@@ -0,0 +1,9 @@
+﻿using BattleCruisers.Buildables.Buildings;
+
+namespace BattleCruisers.AI.Providers
+{
+    public interface IBuildingKeyProviderFactory
+	{
+        IBuildingKeyProvider CreateBuildingKeyProvider(BuildingCategory buildingCategory, int levelNum);
+	}
+}
