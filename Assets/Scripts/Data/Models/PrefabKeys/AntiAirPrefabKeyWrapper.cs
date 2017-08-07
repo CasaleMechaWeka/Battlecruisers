e33bc74a58a48e4b23f093b2edd0e23efd41d68e
the_mann <felix.fei.mann@gmail.com>
2017-08-07 12:19:34 -0700

    Created anti -air/-naval prefab key wrappers :)

---
+++ Assets/Scripts/Data/Models/PrefabKeys/AntiAirPrefabKeyWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.AI.Providers;
+
+namespace BattleCruisers.Data.Models.PrefabKeys
+{
+	public class AntiAirPrefabKeyWrapper : BasePrefabKeyWrapper
+	{
+		protected override IEnumerator<IPrefabKey> GetBuildOrder(IBuildOrders buildOrders)
+		{
+            return buildOrders.AntiAirBuildOrder;
+		}
+	}
+}
