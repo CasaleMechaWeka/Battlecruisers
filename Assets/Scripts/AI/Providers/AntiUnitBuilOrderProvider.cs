34c95fae492626a1f12309d4fedbccd59a5c15ad
the_mann <felix.fei.mann@gmail.com>
2017-08-07 16:21:03 -0700

    Created AntiUnitBuilOrderProvider :)

---
+++ Assets/Scripts/AI/Providers/AntiUnitBuilOrderProvider.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Data.Static;
+using BattleCruisers.Utils;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.AI.Providers
+{
+    public abstract class AntiUnitBuilOrderProvider
+    {
+        private readonly IStaticData _staticData;
+        private readonly IPrefabKey _basicDefenceKey, _advancedDefenceKey;
+
+        private const int MIN_NUM_OF_SLOTS = 1;
+
+        public AntiUnitBuilOrderProvider(IStaticData staticData, IPrefabKey basicDefenceKey, IPrefabKey advancedDefenceKey)
+        {
+            Helper.AssertIsNotNull(staticData, basicDefenceKey, advancedDefenceKey);
+
+            _staticData = staticData;
+            _basicDefenceKey = basicDefenceKey;
+            _advancedDefenceKey = advancedDefenceKey;
+        }
+
+        public IList<IPrefabKey> CreateBuildOrder(int numOfDeckSlots, int levelNum)
+        {
+			IList<IPrefabKey> buildOrder = new List<IPrefabKey>();
+
+            int numOfSlotsToUse = FindNumOfSlotsToUse(numOfDeckSlots);
+            Assert.IsTrue(numOfDeckSlots > MIN_NUM_OF_SLOTS);
+
+            buildOrder.Add(_basicDefenceKey);
+
+            IPrefabKey defenceKey = _staticData.IsBuildableAvailable(_advancedDefenceKey, levelNum) ? _advancedDefenceKey : _basicDefenceKey;               
+
+            while (buildOrder.Count < numOfSlotsToUse)
+            {
+                buildOrder.Add(defenceKey);
+            }
+
+            return buildOrder;
+        }
+
+        protected abstract int FindNumOfSlotsToUse(int numOfDeckSlots);
+    }
+}
