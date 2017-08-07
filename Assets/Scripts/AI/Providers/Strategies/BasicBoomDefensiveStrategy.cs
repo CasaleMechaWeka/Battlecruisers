7327ee09392198c04ff5d11844f279400d011937
the_mann <felix.fei.mann@gmail.com>
2017-08-07 13:41:24 -0700

    Created basic BaseStrategies :)

---
+++ Assets/Scripts/AI/Providers/Strategies/BasicBoomDefensiveStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Data.Static;
+
+namespace BattleCruisers.AI.Providers.Strategies
+{
+    public class BasicBoomDefensiveStrategy : IBaseStrategy
+	{
+        public IList<IPrefabKeyWrapper> BuildOrder { get { return StaticBuildOrders.Basic.BoomDefensive; } }
+	}
+}
