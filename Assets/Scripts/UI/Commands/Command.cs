110290c7258cf4a61d57cc52460c50546ff2db4b
the_mann <felix.fei.mann@gmail.com>
2017-08-08 19:50:30 -0700

    Created own implementation of ICommand :P

---
+++ Assets/Scripts/UI/Commands/Command.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.Commands
+{
+    public class Command : ICommand
+	{
+        private readonly Action _action;
+        private readonly Func<bool> _canExecute;
+
+		public bool CanExecute 
+        { 
+            get { return _canExecute.Invoke(); }
+        }
+
+		public event EventHandler CanExecuteChanged;
+
+        public Command(Action action, Func<bool> canExecute)
+        {
+            Helper.AssertIsNotNull(action, canExecute);
+
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+		public void Execute()
+        {
+            _action.Invoke();
+        }
+
+		public void EmitCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged.Invoke(this, EventArgs.Empty);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/UI/Commands/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BattleCruisers.UI.Commands
+{
+    public interface ICommand
+    {
+        bool CanExecute { get; }
+
+        event EventHandler CanExecuteChanged;
+
+        void Execute();
+        void EmitCanExecuteChanged();
+    }
+}
