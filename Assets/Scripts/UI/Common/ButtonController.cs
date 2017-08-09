8193f0f843b48dc92831d9bc03dd71bb0c4686ce
the_mann <felix.fei.mann@gmail.com>
2017-08-08 20:08:46 -0700

    Created ButtonController that updates its visibility as its Command.CanExecute udpates :)

---
+++ Assets/Scripts/UI/Common/ButtonController.cs
@@ -0,0 +1,29 @@
+﻿using BattleCruisers.UI.Commands;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Common
+{
+    public class ButtonController : MonoBehaviour
+    {
+        private ICommand _command;
+
+        public void Initialise(ICommand command)
+        {
+            Assert.IsNotNull(command);
+			
+            _command = command;
+            _command.CanExecuteChanged += (sender, e) => UpdateVisibility();
+        }
+
+        public void Execute()
+        {
+            _command.Execute();
+        }
+
+        private void UpdateVisibility()
+        {
+            gameObject.SetActive(_command.CanExecute);
+        }
+    }
+}
