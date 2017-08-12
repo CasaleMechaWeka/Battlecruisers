4cdab17e0aa82e39960b41901c66bb53245910a5
the_mann <felix.fei.mann@gmail.com>
2017-08-11 20:27:34 -0700

    Tided up imports

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -1,13 +1,10 @@
-﻿using BattleCruisers.UI.BattleScene;
-using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace BattleCruisers
 {
-	public class BackgroundController : MonoBehaviour, IPointerClickHandler
+    public class BackgroundController : MonoBehaviour, IPointerClickHandler
 	{
 		public event EventHandler BackgroundClicked;
 
