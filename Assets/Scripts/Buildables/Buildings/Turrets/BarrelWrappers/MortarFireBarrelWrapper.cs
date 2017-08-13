c4e27f67ba4d4f12f95b1fb847d02ac99ae9960a
the_mann <felix.fei.mann@gmail.com>
2017-08-13 10:07:07 -0700

    Created MortarFireBarrelWrapper :D

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/MortarFireBarrelWrapper.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
+{
+	public class MortarFireBarrelWrapper : BarrelWrapper
+	{
+		protected override IAngleCalculator CreateAngleCalculator()
+		{
+            return _factoryProvider.AngleCalculatorFactory.CreateMortarAngleCalcultor(_factoryProvider.TargetPositionPredictorFactory);
+		}
+	}
+}
