ac24f5808a001d08dea88b19811cdcfca9e742fa
the_mann <felix.fei.mann@gmail.com>
2017-08-13 11:00:23 -0700

    Created GravityAffectedBarrelWrapper :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/GravityAffectedBarrelWrapper.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
+{
+    public class GravityAffectedBarrelWrapper : ProximityTargetBarrelWrapper
+	{
+		protected override IAngleCalculator CreateAngleCalculator()
+		{
+            return _factoryProvider.AngleCalculatorFactory.CreateArtilleryAngleCalcultor(_factoryProvider.TargetPositionPredictorFactory);
+		}
+	}
+}
