2c8e4ccbd740f4edbd77e11e3e2ff51545d712da
the_mann <felix.fei.mann@gmail.com>
2017-08-13 14:29:07 -0700

    Fixed mortar moving targets test :)

--- Assets/Scripts/Scenes/Test/Turrets/AntiShip/MortarMovingTargetTestGod.cs
+++ Assets/Scripts/Scenes/Test/Turrets/AntiShip/MortarMovingTargetTestGod.cs
@@ -1,16 +1,12 @@
-﻿using BattleCruisers.Buildables;
+﻿using System.Collections.Generic;
+using BattleCruisers.Buildables;
 using BattleCruisers.Buildables.Buildings.Turrets;
-using BattleCruisers.Buildables.Units;
-using BattleCruisers.Targets;
 using BattleCruisers.Scenes.Test.Utilities;
-using BattleCruisers.Buildables.Units.Aircraft;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Turrets.AntiShip
 {
-	public class MortarMovingTargetTestGod : MonoBehaviour 
+    public class MortarMovingTargetTestGod : MonoBehaviour 
 	{
 		public List<Vector2> targetPatrolPoints;
 
@@ -19,14 +15,14 @@ namespace BattleCruisers.Scenes.Test.Turrets.AntiShip
 			Helper helper = new Helper();
 
 			// Setup target
-			TestAircraftController target = GameObject.FindObjectOfType<TestAircraftController>();
+			TestAircraftController target = FindObjectOfType<TestAircraftController>();
 			target.PatrolPoints = targetPatrolPoints;
 			target.SetTargetType(TargetType.Ships);  // So mortars will attack this
 			helper.InitialiseBuildable(target, Faction.Blues);
 			target.StartConstruction();
 
-			// Setup mortars
-			MortarController[] mortars = GameObject.FindObjectsOfType(typeof(MortarController)) as MortarController[];
+            // Setup mortars
+            MortarController[] mortars = FindObjectsOfType<MortarController>();
 			foreach (MortarController mortar in mortars)
 			{
 				helper.InitialiseBuildable(mortar, Faction.Reds);
