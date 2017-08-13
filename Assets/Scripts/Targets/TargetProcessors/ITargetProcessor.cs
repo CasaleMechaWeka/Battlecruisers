dee9ae9f1432d34b538096a6ae15c693ff4e781f
the_mann <felix.fei.mann@gmail.com>
2017-08-13 11:10:37 -0700

    Tidied up imports

--- Assets/Scripts/Targets/TargetProcessors/ITargetProcessor.cs
+++ Assets/Scripts/Targets/TargetProcessors/ITargetProcessor.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 namespace BattleCruisers.Targets.TargetProcessors
 {
-	/// <summary>
-	/// Ranks all targets, and assigns the highest ranked target to ITargetConsumers.
-	/// </summary>
-	public interface ITargetProcessor : IDisposable
+    /// <summary>
+    /// Ranks all targets, and assigns the highest ranked target to ITargetConsumers.
+    /// </summary>
+    public interface ITargetProcessor : IDisposable
 	{
 		/// <exception cref="ArgumentException">If the target consumer is already added.</exception>
 		void AddTargetConsumer(ITargetConsumer targetConsumer);
