37180568eb5d16884b234d50c40b2dbbaebf013d
the_mann <felix.fei.mann@gmail.com>
2017-08-13 16:18:26 -0700

    Updted frigate test scene in preparation for adding aircraft :)

---
+++ Assets/Scripts/Scenes/Test/Naval/ShipWithAircraftTestGod.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Buildables;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Naval
+{
+    public class ShipWithAircraftTestGod : ShipTestsGod
+	{
+		public List<Vector2> leftSidePatrolPoints, rightSidePatrolPoints;
+
+		protected override void OnStart()
+        {
+            Helper helper = new Helper();
+            TestAircraftController[] planes = FindObjectsOfType<TestAircraftController>();
+
+            foreach (TestAircraftController plane in planes)
+            {
+                Faction faction;
+
+                // Give plane opposite faction to factories, so that ships can attack them.
+                if (plane.Position.x < 0)
+                {
+                    plane.PatrolPoints = leftSidePatrolPoints;
+                    faction = FactoryFacingLeftFaction;
+                }
+                else
+                {
+                    plane.PatrolPoints = rightSidePatrolPoints;
+                    faction = FactoryFacingRightFaction;
+                }
+
+                helper.InitialiseBuildable(plane);
+                plane.StartConstruction();
+            }
+        }
+	}
+}
