453eb3db5c0489a5969231ffb2f0b273477f7997
the_mann <felix.fei.mann@gmail.com>
2017-08-12 22:32:07 -0700

    Extended BarrelWrapper to initialise target finder and processor :)

--- Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/BarrelWrapper.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/BarrelWrapper.cs
@@ -1,21 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
 using BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers;
 using BattleCruisers.Buildables.Buildings.Turrets.Stats;
 using BattleCruisers.Movement;
 using BattleCruisers.Movement.Rotation;
 using BattleCruisers.Targets;
+using BattleCruisers.Targets.TargetFinders;
 using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Targets.TargetProcessors;
+using BattleCruisers.Targets.TargetProcessors.Ranking;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
 {
-    public abstract class BarrelWrapper : MonoBehaviour, ITargetConsumer
+    // FELIX  Create interface
+    public abstract class BarrelWrapper : MonoBehaviour, ITargetConsumer, IDisposable
     {
+        private ITargetsFactory _targetsFactory;
+        private Faction _enemyFaction;
+        private ITargetFinder _targetFinder;
+        private ITargetProcessor _targetProcessor;
+
         protected BarrelController _barrelController;
 
-        protected abstract IList<TargetType> AttackCapabilities { get; }
+		public CircleTargetDetector enemyDetector;
+
+		protected abstract IList<TargetType> AttackCapabilities { get; }
 		public TurretStats TurretStats { get { return _barrelController.TurretStats; } }
 
         public ITarget Target 
@@ -33,11 +45,30 @@ namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
 
         public void Initialise(ITargetsFactory targetsFactory, IMovementControllerFactory movementControllerFactory, Faction enemyFaction)
         {
+            _targetsFactory = targetsFactory;
+            _enemyFaction = enemyFaction;
+
             _barrelController
                 .Initialise(
                     CreateTargetFilter(targetsFactory, enemyFaction), 
                     CreateAngleCalculator(), 
                     CreateRotationMovementController(movementControllerFactory));
+        }
+
+        public void StartAttackingTargets()
+        {
+            // FELIX  Specific to defensive turrets.  Perhaps DefensiveBarrelWrapper and OffensiveBarrelWrapper classes?
+
+            // Create target finder
+            enemyDetector.Initialise(_barrelController.TurretStats.rangeInM);
+			bool isDetectable = true;
+			ITargetFilter enemyDetectionFilter = _targetsFactory.CreateDetectableTargetFilter(_enemyFaction, isDetectable, AttackCapabilities);
+            _targetFinder = _targetsFactory.CreateRangedTargetFinder(enemyDetector, enemyDetectionFilter);
+
+            // Start processing targets
+			ITargetRanker targetRanker = _targetsFactory.CreateEqualTargetRanker();
+            _targetProcessor = _targetsFactory.CreateTargetProcessor(_targetFinder, targetRanker);
+			_targetProcessor.AddTargetConsumer(this);            
         }
 
         protected virtual ITargetFilter CreateTargetFilter(ITargetsFactory targetsFactory, Faction enemyFaction)
@@ -50,6 +81,16 @@ namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
         protected virtual IRotationMovementController CreateRotationMovementController(IMovementControllerFactory movementControllerFactory)
         {
             return movementControllerFactory.CreateRotationMovementController(_barrelController.TurretStats.turretRotateSpeedInDegrees, _barrelController.transform);
+        }
+
+        public void Dispose()
+        {
+			_targetProcessor.RemoveTargetConsumer(this);
+			_targetProcessor.Dispose();
+			_targetProcessor = null;
+
+			_targetFinder.Dispose();
+			_targetFinder = null;
         }
     }
 }
