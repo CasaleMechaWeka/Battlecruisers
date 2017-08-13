571bc5dd7a1c22549527b8a14a417b321e422236
the_mann <felix.fei.mann@gmail.com>
2017-08-13 14:34:32 -0700

    Fixed mortar stationary target test scene.  That was the last test scene, phew :)

--- Assets/Scripts/Scenes/Test/Turrets/AntiShip/MortarStationaryTargetTestsGod.cs
+++ Assets/Scripts/Scenes/Test/Turrets/AntiShip/MortarStationaryTargetTestsGod.cs
@@ -15,14 +15,14 @@ namespace BattleCruisers.Scenes.Test.Turrets.AntiShip
 
 			// Setup target
 			TestAircraftController target = FindObjectOfType<TestAircraftController>();
-            target.PatrolPoints = new List<Vector2>() { new Vector2(0, -5), new Vector2(0, -5.01f) };
+            target.PatrolPoints = new List<Vector2>() { new Vector2(0, 0), new Vector2(0, -0.01f) };
 			target.SetTargetType(TargetType.Ships);  // So mortars will attack this
 			helper.InitialiseBuildable(target, Faction.Blues);
 			target.StartConstruction();
 
 
 			// Setup mortars
-			MortarController[] mortars = FindObjectsOfType(typeof(MortarController)) as MortarController[];
+            MortarController[] mortars = FindObjectsOfType<MortarController>();
 			foreach (MortarController mortar in mortars)
 			{
 				helper.InitialiseBuildable(mortar, Faction.Reds);
