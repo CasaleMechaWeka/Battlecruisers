086ab68cea28c6df89aac2f967612178be28ff59
the_mann <felix.fei.mann@gmail.com>
2017-08-13 10:57:37 -0700

    Created LeadingDirectFireBarrelWrapper :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/LeadingDirectFireBarrelWrapper.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
+{
+	public class LeadingDirectFireBarrelWrapper : ProximityTargetBarrelWrapper
+	{
+		protected override IAngleCalculator CreateAngleCalculator()
+		{
+			return _factoryProvider.AngleCalculatorFactory.CreateLeadingAngleCalcultor(_factoryProvider.TargetPositionPredictorFactory);
+		}
+	}
+}
