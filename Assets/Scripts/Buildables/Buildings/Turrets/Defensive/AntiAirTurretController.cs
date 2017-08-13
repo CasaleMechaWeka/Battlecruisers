74a8d84201bf9f44ab48cb446e5eb0c2cb01f02f
the_mann <felix.fei.mann@gmail.com>
2017-08-13 12:16:29 -0700

    Created new AntiAirTurret prefab, that uses a LeadingDirectFireBarrelWrapper :)

--- Assets/Scripts/Buildables/Buildings/Turrets/Defensive/AntiAirTurretController.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/Defensive/AntiAirTurretController.cs
@@ -1,18 +1,11 @@
-﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
-
-namespace BattleCruisers.Buildables.Buildings.Turrets.Defensive
+﻿namespace BattleCruisers.Buildables.Buildings.Turrets.Defensive
 {
-    public class AntiAirTurretController : DefensiveTurret
+    public class AntiAirTurretController : Turret
 	{
 		public override void StaticInitialise()
 		{
 			base.StaticInitialise();
 			_attackCapabilities.Add(TargetType.Aircraft);
-		}
-
-		protected override IAngleCalculator CreateAngleCalculator()
-		{
-			return _angleCalculatorFactory.CreateLeadingAngleCalcultor(_targetPositionPredictorFactory);
 		}
 	}
 }
