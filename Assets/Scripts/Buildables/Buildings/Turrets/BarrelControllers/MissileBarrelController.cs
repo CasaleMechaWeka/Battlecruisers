d84cec5547fadc15af1624da67fb3554204bde28
the_mann <felix.fei.mann@gmail.com>
2017-08-13 20:11:54 -0700

    Created MissileBarrelController (that may work :P )

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/MissileBarrelController.cs
@@ -0,0 +1,47 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+using BattleCruisers.Movement;
+using BattleCruisers.Movement.Predictors;
+using BattleCruisers.Movement.Rotation;
+using BattleCruisers.Projectiles;
+using BattleCruisers.Projectiles.Spawners;
+using BattleCruisers.Projectiles.Stats;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Utils.DataStrctures;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers
+{
+    public class MissileBarrelController : BarrelController
+	{
+        private ICircularList<MissileSpawner> _missileSpawners;
+
+		public MissileController missilePrefab;
+
+		public void Initialise(ITargetFilter targetFilter, IAngleCalculator angleCalculator, IRotationMovementController rotationMovementController,
+			IMovementControllerFactory movementControllerFactory, ITargetPositionPredictorFactory targetPositionPredictorFactory)
+		{
+			base.Initialise(targetFilter, angleCalculator, rotationMovementController);
+
+            Assert.IsNotNull(missilePrefab);
+
+            MissileSpawner[] missileSpawners = gameObject.GetComponentsInChildren<MissileSpawner>();
+			Assert.IsTrue(missileSpawners.Length != 0);
+            _missileSpawners = new CircularList<MissileSpawner>(missileSpawners);
+
+			MissileStats missileStats = new MissileStats(missilePrefab, TurretStats.damage, TurretStats.bulletVelocityInMPerS);
+            foreach (MissileSpawner missileSpawner in _missileSpawners.Items)
+            {
+                missileSpawner.Initialise(missileStats, movementControllerFactory, targetPositionPredictorFactory);
+			}
+		}
+
+		protected override void Fire(float angleInDegrees)
+		{
+            _missileSpawners.Next().SpawnMissile(
+                angleInDegrees,
+                IsSourceMirrored,
+                Target,
+                _targetFilter);
+		}
+	}
+}
