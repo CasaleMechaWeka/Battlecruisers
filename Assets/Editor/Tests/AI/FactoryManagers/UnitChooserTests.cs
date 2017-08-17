023e1d95ca4900d1785de29b97eb59ebb2c194ea
the_mann <felix.fei.mann@gmail.com>
2017-08-16 20:52:23 -0700

    Finished UnitChooserTests :)

--- Assets/Editor/Tests/AI/FactoryManagers/UnitChooserTests.cs
+++ Assets/Editor/Tests/AI/FactoryManagers/UnitChooserTests.cs
@@ -10,11 +10,36 @@ namespace BattleCruisers.Tests.AI.FactoryManagers
 {
 	public class UnitChooserTests
 	{
+        private IUnitChooser _unitChooser;
+        private IList<IBuildableWrapper<IUnit>> _units;
+        private IBuildableWrapper<IUnit> _unit2Drones, _unit4Drones, _unit6Drones;
+
 		[SetUp]
 		public void SetuUp()
 		{
 			UnityAsserts.Assert.raiseExceptions = true;
+
+            _unit2Drones = CreateUnit(numOfDrones: 2);
+            _unit4Drones = CreateUnit(numOfDrones: 4);
+            _unit6Drones = CreateUnit(numOfDrones: 6);
+
+            _units = new List<IBuildableWrapper<IUnit>>()
+            {
+                _unit2Drones,
+                _unit4Drones,
+                _unit6Drones
+            };
+
+            _unitChooser = new UnitChooser(_units);
 		}
+
+        private IBuildableWrapper<IUnit> CreateUnit(int numOfDrones)
+        {
+            IBuildableWrapper<IUnit> unit = Substitute.For<IBuildableWrapper<IUnit>>();
+            unit.Buildable.Returns(Substitute.For<IUnit>());
+            unit.Buildable.NumOfDronesRequired.Returns(numOfDrones);
+            return unit;
+        }
 
 		[Test]
 		public void Constructor_NullUnitsThrows()
@@ -28,5 +53,35 @@ namespace BattleCruisers.Tests.AI.FactoryManagers
             IList<IBuildableWrapper<IUnit>> units = new List<IBuildableWrapper<IUnit>>();
 			Assert.Throws<UnityAsserts.AssertionException>(() => new UnitChooser(units));
 		}
+
+        [Test]
+        public void ChooseUnit_ReturnsNullIfCannotAffordAnyUnits()
+        {
+            Assert.IsNull(_unitChooser.ChooseUnit(numOfDrones: 0));
+        }
+
+        [Test]
+        public void ChooseUnit_ReturnsMostExpensiveUnit_2Drones()
+        {
+            Assert.AreSame(_unit2Drones, _unitChooser.ChooseUnit(numOfDrones: 2));
+        }
+
+		[Test]
+		public void ChooseUnit_ReturnsMostExpensiveUnit_4Drones()
+		{
+			Assert.AreSame(_unit4Drones, _unitChooser.ChooseUnit(numOfDrones: 4));
+		}
+
+        [Test]
+        public void ChooseUnit_ReturnsMostExpensiveUnit_6Drones()
+        {
+            Assert.AreSame(_unit6Drones, _unitChooser.ChooseUnit(numOfDrones: 6));
+        }
+
+        [Test]
+        public void ChooseUnit_ReturnsMostExpensiveUnit_8Drones()
+        {
+            Assert.AreSame(_unit6Drones, _unitChooser.ChooseUnit(numOfDrones: 8));
+        }
 	}
 }
