3d79431f7f1ecee741f128ff8497770025bbc423
the_mann <felix.fei.mann@gmail.com>
2017-08-16 20:39:39 -0700

    Wrote tests for UnitChooser constructor :Pg

---
+++ Assets/Editor/Tests/AI/FactoryManagers/UnitChooserTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.AI.FactoryManagers;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
+using NSubstitute;
+using NUnit.Framework;
+using UnityAsserts = UnityEngine.Assertions;
+
+namespace BattleCruisers.Tests.AI.FactoryManagers
+{
+	public class UnitChooserTests
+	{
+		[SetUp]
+		public void SetuUp()
+		{
+			UnityAsserts.Assert.raiseExceptions = true;
+		}
+
+		[Test]
+		public void Constructor_NullUnitsThrows()
+		{
+            Assert.Throws<UnityAsserts.AssertionException>(() => new UnitChooser(units: null));
+		}
+
+		[Test]
+		public void Constructor_EmptyUnitsThrows()
+		{
+            IList<IBuildableWrapper<IUnit>> units = new List<IBuildableWrapper<IUnit>>();
+			Assert.Throws<UnityAsserts.AssertionException>(() => new UnitChooser(units));
+		}
+	}
+}
