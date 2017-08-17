9c187c3cd3d14f4c610261689202cbd187a0f4f2
the_mann <felix.fei.mann@gmail.com>
2017-08-16 21:54:05 -0700

    Created FactoryManagerFactory :D

---
+++ Assets/Scripts/AI/FactoryManagers/FactoryManagerFactory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Data.Static;
+using BattleCruisers.Drones;
+using BattleCruisers.Fetchers;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.AI.FactoryManagers
+{
+    public class FactoryManagerFactory
+    {
+        private readonly IStaticData _staticData;
+        private readonly IPrefabFactory _prefabFactory;
+
+        public FactoryManagerFactory(IStaticData staticData, IPrefabFactory prefabFactory)
+        {
+            Helper.AssertIsNotNull(staticData, prefabFactory);
+
+            _staticData = staticData;
+            _prefabFactory = prefabFactory;
+        }
+
+        public IFactoryManager CreateNavalFactoryManager(int levelNum, ICruiserController friendlyCruiser, IDroneManager droneManager)
+        {
+            IList<IPrefabKey> availableShipKeys = _staticData.GetAvailableUnits(UnitCategory.Naval, levelNum);
+            IList<IBuildableWrapper<IUnit>> availableShips =
+                availableShipKeys
+                    .Select(key => _prefabFactory.GetUnitWrapperPrefab(key))
+                    .ToList();
+            IUnitChooser unitChooser = new UnitChooser(availableShips);
+
+            return new NavalFactoryManager(friendlyCruiser, droneManager, unitChooser);
+        }
+    }
+}
---
+++ Assets/Scripts/AI/FactoryManagers/IFactoryManagerFactory.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Drones;
+
+namespace BattleCruisers.AI.FactoryManagers
+{
+    public interface IFactoryManagerFactory
+    {
+        IFactoryManager CreateNavalFactoryManager(int levelNum, ICruiserController friendlyCruiser, IDroneManager droneManager);
+    }
+}
