090ea78aa7d03d36533e8bfa1efb8a895b7416e6
Felix Fei Mann <the_man@Felixs-Mac-mini.local>
2017-08-17 21:25:28 -0700

    Added comment

--- Assets/Scripts/AI/ThreatMonitor/IThreatMonitor.cs
+++ Assets/Scripts/AI/ThreatMonitor/IThreatMonitor.cs
@@ -2,6 +2,8 @@
 
 namespace BattleCruisers.AI.ThreatMonitors
 {
+    // Do not change enum order, as the values are used for comparisons.
+    // Ie:  myThreatLevel > ThreatLevel.Low
     public enum ThreatLevel
     {
         None, Low, High
