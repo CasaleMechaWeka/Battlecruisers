6a6c7eacd2eeafc70ea91b36dafd54a4c28a128f
Felix Fei Mann <the_man@Felixs-Mac-mini.local>
2017-08-19 16:44:15 -0700

    Created first test for new BasicTaskProducer :)

--- Assets/Editor/Tests/AI/TaskProducers/BasicTaskProducerTests.cs
+++ Assets/Editor/Tests/AI/TaskProducers/BasicTaskProducerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using BattleCruisers.AI;
 using BattleCruisers.AI.TaskProducers;
 using BattleCruisers.AI.Tasks;
@@ -14,12 +15,13 @@ namespace BattleCruisers.Tests.AI.TaskProducers
 {
     public class BasicTaskProducerTests
 	{
+        private BasicTaskProducer _taskProducer;
         private ITaskList _tasks;
         private ICruiserController _cruiser;
 		private ISlotWrapper _slotWrapper;
 		private IPrefabFactory _prefabFactory;
         private ITaskFactory _taskFactory;
-        private IList<IPrefabKey> _buildOrder;
+        private IEnumerator<IPrefabKey> _buildOrder;
         private IBuildableWrapper<IBuilding> _platformSlotBuildingWrapper, _deckSlotBuildingWrapper;
         private IBuilding _platformSlotBuilding, _deckSlotBuilding;
 		private IPrefabKey _platformBuildingKey, _deckBuildingKey;
@@ -29,12 +31,15 @@ namespace BattleCruisers.Tests.AI.TaskProducers
 		public void SetuUp()
 		{
 			_tasks = Substitute.For<ITaskList>();
+            _tasks.IsEmpty.Returns(true);
+
             _slotWrapper = Substitute.For<ISlotWrapper>();
+            _slotWrapper.IsSlotAvailable(SlotType.Deck).ReturnsForAnyArgs(true);
+
             _cruiser = Substitute.For<ICruiserController>();
             _cruiser.SlotWrapper.Returns(_slotWrapper);
             _prefabFactory = Substitute.For<IPrefabFactory>();
             _taskFactory = Substitute.For<ITaskFactory>();
-            _buildOrder = new List<IPrefabKey>();
 
             _platformSlotBuilding = Substitute.For<IBuilding>();
             _platformSlotBuilding.SlotType.Returns(SlotType.Platform);
@@ -53,61 +58,73 @@ namespace BattleCruisers.Tests.AI.TaskProducers
             _prefabFactory.GetBuildingWrapperPrefab(_deckBuildingKey).Returns(_deckSlotBuildingWrapper);
             _deckBuildingTask = Substitute.For<ITask>();
             _taskFactory.CreateConstructBuildingTask(TaskPriority.Normal, _deckBuildingKey).Returns(_deckBuildingTask);
+
+			_buildOrder = Substitute.For<IEnumerator<IPrefabKey>>();
+			_buildOrder.MoveNext().Returns(true);
+            _buildOrder.Current.Returns(_platformBuildingKey);
+
+            _taskProducer = new BasicTaskProducer(_tasks, _cruiser, _prefabFactory, _taskFactory, _buildOrder);
 		}
 
-		[Test]
-		public void SingleKey_CreatesSingleTask()
+        [Test]
+        public void Constructor_CreatesFirstTask()
         {
-            _buildOrder.Add(_platformBuildingKey);
-
-            _cruiser.SlotWrapper.GetSlotCount(SlotType.Platform).Returns(1);
-
-            CreateTaskProducer();
-
             _tasks.Received().Add(_platformBuildingTask);
         }
 
-		[Test]
-		public void MultipleKeys_CreatesMultipleTask()
-		{
-			_buildOrder.Add(_platformBuildingKey);
-            _buildOrder.Add(_deckBuildingKey);
+		//[Test]
+		//public void SingleKey_CreatesSingleTask()
+  //      {
+  //          _buildOrder.Add(_platformBuildingKey);
 
-			_cruiser.SlotWrapper.GetSlotCount(SlotType.Platform).Returns(1);
-            _cruiser.SlotWrapper.GetSlotCount(SlotType.Deck).Returns(1);
+  //          _cruiser.SlotWrapper.GetSlotCount(SlotType.Platform).Returns(1);
 
-			CreateTaskProducer();
+  //          CreateTaskProducer();
 
-			_tasks.Received().Add(_platformBuildingTask);
-            _tasks.Received().Add(_deckBuildingTask);
-		}
+  //          _tasks.Received().Add(_platformBuildingTask);
+  //      }
 
-		[Test]
-        public void SingleKey_CruiserDoesNotHaveCapacity_DoesNotCreateTask()
-        {
-            _buildOrder.Add(_platformBuildingKey);
+		//[Test]
+		//public void MultipleKeys_CreatesMultipleTask()
+		//{
+		//	_buildOrder.Add(_platformBuildingKey);
+  //          _buildOrder.Add(_deckBuildingKey);
 
-			_cruiser.SlotWrapper.GetSlotCount(SlotType.Platform).Returns(0);
+		//	_cruiser.SlotWrapper.GetSlotCount(SlotType.Platform).Returns(1);
+  //          _cruiser.SlotWrapper.GetSlotCount(SlotType.Deck).Returns(1);
 
-			CreateTaskProducer();
+		//	CreateTaskProducer();
 
-            _tasks.DidNotReceive().Add(_platformBuildingTask);
-        }
+		//	_tasks.Received().Add(_platformBuildingTask);
+  //          _tasks.Received().Add(_deckBuildingTask);
+		//}
 
-		[Test]
-		public void MultipleKeys_CruiserDoesNotHaveCapacity_OnlyCreatesTaskForKeysWithCapacity()
-		{
-			_buildOrder.Add(_platformBuildingKey);
-			_buildOrder.Add(_deckBuildingKey);
+		//[Test]
+  //      public void SingleKey_CruiserDoesNotHaveCapacity_DoesNotCreateTask()
+  //      {
+  //          _buildOrder.Add(_platformBuildingKey);
 
-			_cruiser.SlotWrapper.GetSlotCount(SlotType.Platform).Returns(1);
-			_cruiser.SlotWrapper.GetSlotCount(SlotType.Deck).Returns(0);
+		//	_cruiser.SlotWrapper.GetSlotCount(SlotType.Platform).Returns(0);
 
-			CreateTaskProducer();
+		//	CreateTaskProducer();
 
-			_tasks.Received().Add(_platformBuildingTask);
-            _tasks.DidNotReceive().Add(_deckBuildingTask);
-		}
+  //          _tasks.DidNotReceive().Add(_platformBuildingTask);
+  //      }
+
+		//[Test]
+		//public void MultipleKeys_CruiserDoesNotHaveCapacity_OnlyCreatesTaskForKeysWithCapacity()
+		//{
+		//	_buildOrder.Add(_platformBuildingKey);
+		//	_buildOrder.Add(_deckBuildingKey);
+
+		//	_cruiser.SlotWrapper.GetSlotCount(SlotType.Platform).Returns(1);
+		//	_cruiser.SlotWrapper.GetSlotCount(SlotType.Deck).Returns(0);
+
+		//	CreateTaskProducer();
+
+		//	_tasks.Received().Add(_platformBuildingTask);
+  //          _tasks.DidNotReceive().Add(_deckBuildingTask);
+		//}
 
         private void CreateTaskProducer()
         {
