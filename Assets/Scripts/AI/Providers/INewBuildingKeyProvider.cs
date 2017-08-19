9e45e157e96f7e0bc8d3aa8eed6659198f877861
Felix Fei Mann <the_man@Felixs-Mac-mini.local>
2017-08-19 15:41:25 -0700

    Created INewBuildingKeyProvider :)

---
+++ Assets/Scripts/AI/Providers/INewBuildingKeyProvider.cs
@@ -0,0 +1,11 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.AI.Providers
+{
+    // FELIX  Rename
+    public interface INewBuildingKeyProvider
+    {
+        bool HasNext { get; }
+        IPrefabKey Next { get; }
+    }
+}
