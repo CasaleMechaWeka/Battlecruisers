f0115db4e77f6d1bdfebf0abdaae97c51524482d
Felix Fei Mann <the_man@Felixs-Mac-mini.local>
2017-08-20 12:23:35 -0700

    Created ISlotAssigner specifically for the AssignSlots() method :)

---
+++ Assets/Scripts/AI/Providers/BuildingKey/ISlotAssigner.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.AI.Providers.Strategies.Requests;
+
+namespace BattleCruisers.AI.Providers.BuildingKey
+{
+    public interface ISlotAssigner
+	{
+		void AssignSlots(IEnumerable<IOffensiveRequest> slotRequests, int numOfSlotsAvailable);
+	}
+}
---
+++ Assets/Scripts/AI/Providers/BuildingKey/SlotAssigner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BattleCruisers.AI.Providers.Strategies.Requests;
+
+namespace BattleCruisers.AI.Providers.BuildingKey
+{
+    public class SlotAssigner : ISlotAssigner
+	{
+		/// <summary>
+		/// 1. High focus requests are served once before low focus requests
+		/// 2. Low focus request get one slot at most
+		/// 3. Remaining slots are split evenly between high focus requests
+		/// </summary>
+		public void AssignSlots(IEnumerable<IOffensiveRequest> slotRequests, int numOfSlotsAvailable)
+		{
+			IEnumerable<IOffensiveRequest> highFocusRequests = slotRequests.Where(request => request.Focus == OffensiveFocus.High);
+			IEnumerable<IOffensiveRequest> lowFocusRequest = slotRequests.Where(request => request.Focus == OffensiveFocus.Low);
+
+			int numOfSlotsUsed = 0;
+
+			// Assign a round of high focus requests
+			numOfSlotsUsed = AssignSlotsToRequests(highFocusRequests, numOfSlotsAvailable, numOfSlotsUsed);
+
+			// Assign a round of low focus request
+			numOfSlotsUsed = AssignSlotsToRequests(lowFocusRequest, numOfSlotsAvailable, numOfSlotsUsed);
+
+			// Assign remaining slots to high focus requests
+			if (highFocusRequests.Any())
+			{
+				while (numOfSlotsUsed < numOfSlotsAvailable)
+				{
+					numOfSlotsUsed = AssignSlotsToRequests(highFocusRequests, numOfSlotsAvailable, numOfSlotsUsed);
+				}
+			}
+		}
+
+		private int AssignSlotsToRequests(IEnumerable<IOffensiveRequest> requests, int numOfPlatformSlots, int numOfSlotsUsed)
+		{
+			foreach (IOffensiveRequest request in requests)
+			{
+				if (numOfSlotsUsed < numOfPlatformSlots)
+				{
+					request.NumOfSlotsToUse++;
+					numOfSlotsUsed++;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			return numOfSlotsUsed;
+		}
+	}
+}
