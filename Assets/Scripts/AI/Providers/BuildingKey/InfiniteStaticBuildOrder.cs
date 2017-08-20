3ac65160fe20fff71d1934e8dd1acc05e26a308f
Felix Fei Mann <the_man@Felixs-Mac-mini.local>
2017-08-20 12:12:52 -0700

    Created InfiniteStaticBuildOrder :)

---
+++ Assets/Scripts/AI/Providers/BuildingKey/InfiniteStaticBuildOrder.cs
@@ -0,0 +1,19 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.AI.Providers.BuildingKey
+{
+    public class InfiniteStaticBuildOrder : IDynamicBuildOrder
+	{
+		public IPrefabKey Current { get; private set; }
+
+        public InfiniteStaticBuildOrder(IPrefabKey buildingKey)
+		{
+			Current = buildingKey;
+		}
+
+        public bool MoveNext()
+        {
+            return true;
+        }
+    }
+}
