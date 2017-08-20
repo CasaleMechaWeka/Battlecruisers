1144c1918ac2daa04c99cc103c84155a1c74c0eb
Felix Fei Mann <the_man@Felixs-Mac-mini.local>
2017-08-20 12:58:50 -0700

    Created FiniteBuildOrder :D

---
+++ Assets/Scripts/AI/Providers/BuildingKey/FiniteBuildOrder.cs
@@ -0,0 +1,39 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.AI.Providers.BuildingKey
+{
+    public class FiniteBuildOrder : IDynamicBuildOrder
+	{
+        private readonly IDynamicBuildOrder _infiniteBuildOrder;
+        private readonly int _size;
+        private int _index;
+
+        public IPrefabKey Current { get; private set; }
+
+		public FiniteBuildOrder(IDynamicBuildOrder infiniteBuildOrder, int size)
+		{
+            Assert.IsNotNull(infiniteBuildOrder);
+
+            _infiniteBuildOrder = infiniteBuildOrder;
+            _size = size;
+            _index = 0;
+		}
+
+		public bool MoveNext()
+		{
+            bool hasKey = false;
+
+            if (_index < _size)
+            {
+                hasKey = _infiniteBuildOrder.MoveNext();
+                Assert.IsTrue(hasKey, "Infinite build order should never run out of keys :/");
+
+                Current = _infiniteBuildOrder.Current;
+                _index++;
+            }
+
+            return hasKey;
+		}
+	}
+}
