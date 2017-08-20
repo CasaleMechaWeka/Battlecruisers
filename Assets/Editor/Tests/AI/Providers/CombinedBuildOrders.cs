c58d794cd9761efb34fac8be03e59f9e1b9190f8
Felix Fei Mann <the_man@Felixs-Mac-mini.local>
2017-08-20 13:50:03 -0700

    Created CombinedBuildOrders :)

---
+++ Assets/Editor/Tests/AI/Providers/CombinedBuildOrders.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using BattleCruisers.Data.Models.PrefabKeys;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.AI.Providers.BuildingKey
+{
+    public class CombinedBuildOrders : IDynamicBuildOrder
+	{
+        private readonly IDynamicBuildOrder[] _buildOrders;
+
+		public IPrefabKey Current { get; private set; }
+
+        public CombinedBuildOrders(params IDynamicBuildOrder[] buildOrders)
+		{
+            Assert.IsNotNull(buildOrders);
+            Assert.IsTrue(buildOrders.Length > 0);
+
+            _buildOrders = buildOrders;
+		}
+
+		public bool MoveNext()
+		{
+            IDynamicBuildOrder validBuildOrder = _buildOrders.FirstOrDefault(buildOrder => buildOrder.MoveNext());
+            Current = validBuildOrder != null ? validBuildOrder.Current : null;
+            return Current != null;
+		}
+	}
+}
