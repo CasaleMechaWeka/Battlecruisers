8cc6b7fa745f30141491a2db5d88311034e02747
Felix Fei Mann <the_man@Felixs-Mac-mini.local>
2017-08-20 11:58:55 -0700

    Created InfiniteBuildOrder :D

---
+++ Assets/Scripts/AI/Providers/BuildingKey/InfiniteBuildOrder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Utils;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.AI.Providers.BuildingKey
+{
+    public class InfiniteBuildOrder : IDynamicBuildOrder
+	{
+        private readonly IBuildingKeyHelper _buildingKeyHelper;
+        private readonly IList<IPrefabKey> _availableBuildings;
+
+        public IPrefabKey Current { get; private set; }
+
+        public InfiniteBuildOrder(
+            BuildingCategory buildingCategory, 
+            IBuildingKeyHelper buildingKeyHelper)
+		{
+            Assert.IsNotNull(buildingKeyHelper);
+
+            _buildingKeyHelper = buildingKeyHelper;
+
+            _availableBuildings = _buildingKeyHelper.GetAvailableBuildings(buildingCategory);
+            Assert.IsTrue(_availableBuildings.Count != 0);
+		}
+
+        public bool MoveNext()
+        {
+            Current
+                = _availableBuildings
+                    .Where(buildingKey => _buildingKeyHelper.CanConstructBuilding(buildingKey))
+                    .Shuffle()
+                    .FirstOrDefault();
+            
+            return Current != null;
+        }
+    }
+}
