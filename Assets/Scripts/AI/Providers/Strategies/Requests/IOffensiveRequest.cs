a08b6fc093144c95291857935aa4e53d1649fbce
Felix Fei Mann <the_man@Felixs-Mac-mini.local>
2017-08-20 17:41:53 -0700

    Moved CombinedBuildOrders class out of test cases folder :P

--- Assets/Scripts/AI/Providers/Strategies/Requests/IOffensiveRequest.cs
+++ Assets/Scripts/AI/Providers/Strategies/Requests/IOffensiveRequest.cs
@@ -4,7 +4,9 @@ namespace BattleCruisers.AI.Providers.Strategies.Requests
 {
     public interface IOffensiveRequest : IBasicOffensiveRequest
     {
+        // FELIX  Remove
         IBuildingKeyProvider BuildingKeyProvider { get; }
+
         int NumOfSlotsToUse { get; set; }
     }
 }
