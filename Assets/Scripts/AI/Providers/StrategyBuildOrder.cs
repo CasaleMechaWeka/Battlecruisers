c11fd39251c116e697fbf12f4ca556ad52c0e3af
Felix Fei Mann <the_man@Felixs-Mac-mini.local>
2017-08-21 17:11:44 -0700

    Created StrategyBuildOrder :)

---
+++ Assets/Scripts/AI/Providers/StrategyBuildOrder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Data.Models.PrefabKeys.Wrappers;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.AI.Providers
+{
+    public class StrategyBuildOrder : IDynamicBuildOrder
+    {
+        private readonly IEnumerator<IPrefabKeyWrapper> _baseBuildOrder;
+
+        public IPrefabKey Current { get; private set; }
+
+        public StrategyBuildOrder(
+            IList<IPrefabKeyWrapper> baseBuildOrder)
+        {
+            Assert.IsNotNull(baseBuildOrder);
+            _baseBuildOrder = baseBuildOrder.GetEnumerator();
+        }
+
+        public bool MoveNext()
+        {
+            while (_baseBuildOrder.MoveNext())
+            {
+                if (_baseBuildOrder.Current.HasKey)
+                {
+                    Current = _baseBuildOrder.Current.Key;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
