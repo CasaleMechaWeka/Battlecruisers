c8b62136fcd49f3b906ff978380441aa24f15061
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-08-27 13:59:00 -0700

    Created RepairCommand class :)

---
+++ Assets/Scripts/Buildables/IRepairCommand.cs
@@ -0,0 +1,9 @@
+﻿using BattleCruisers.UI.Commands;
+
+namespace BattleCruisers.Buildables
+{
+    public interface IRepairCommand : IParameterisedCommand<float>
+    {
+        IRepairable Repairable { get; }
+    }
+}
---
+++ Assets/Scripts/Buildables/RepairCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BattleCruisers.UI.Commands;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables
+{
+    public class RepairCommand : ParameterisedCommand<float>, IRepairCommand
+	{
+		public IRepairable Repairable { get; private set; }
+
+        public RepairCommand(Action<float> action, Func<bool> canExecute, IRepairable repairable) 
+            : base(action, canExecute)
+        {
+            Assert.IsNotNull(repairable);
+            Repairable = repairable;
+        }
+	}
+}
