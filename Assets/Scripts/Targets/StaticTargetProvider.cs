0eb360bfb8d29990d73c9ef3580493b5c7128b0d
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-08-27 16:14:26 -0700

    Created StaticTargetProvider :)

---
+++ Assets/Scripts/Targets/StaticTargetProvider.cs
@@ -0,0 +1,14 @@
+﻿using BattleCruisers.Buildables;
+
+namespace BattleCruisers.Targets
+{
+    public class StaticTargetProvider : ITargetProvider
+    {
+        public ITarget Target { get; private set; }
+
+        public StaticTargetProvider(ITarget target)
+        {
+            Target = target;
+        }
+    }
+}
