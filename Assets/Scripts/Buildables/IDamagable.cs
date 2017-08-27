a2884331c26e73aeb24c54ebdfb83086c82e62f9
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-08-26 19:40:12 -0700

    Moved IDamagable to own file

---
+++ Assets/Scripts/Buildables/IDamagable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace BattleCruisers.Buildables
+{
+    public class DestroyedEventArgs : EventArgs
+	{
+		public ITarget DestroyedTarget { get; private set; }
+
+		public DestroyedEventArgs(ITarget destroyedTarget)
+		{
+			DestroyedTarget = destroyedTarget;
+		}
+	}
+
+	public class HealthChangedEventArgs : EventArgs
+	{
+		public float NewHealth { get; private set; }
+
+		public HealthChangedEventArgs(float newHealth)
+		{
+			NewHealth = newHealth;
+		}
+	}
+
+	public interface IDamagable
+	{
+		/// <value><c>true</c> if health is 0; otherwise, <c>false</c>.</value>
+		bool IsDestroyed { get; }
+		float Health { get; }
+		GameObject GameObject { get; }
+
+		// When health reaches 0
+		event EventHandler<DestroyedEventArgs> Destroyed;
+
+		// When health changes
+		event EventHandler<HealthChangedEventArgs> HealthChanged;
+
+		void TakeDamage(float damageAmount);
+		void Destroy();
+	}
+}
--- Assets/Scripts/Buildables/ITarget.cs
+++ Assets/Scripts/Buildables/ITarget.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace BattleCruisers.Buildables
@@ -20,43 +19,6 @@ namespace BattleCruisers.Buildables
 	public enum TargetValue
 	{
 		Low, Medium, High
-	}
-
-	public class DestroyedEventArgs : EventArgs
-	{
-		public ITarget DestroyedTarget { get; private set; }
-
-		public DestroyedEventArgs(ITarget destroyedTarget)
-		{
-			DestroyedTarget = destroyedTarget;
-		}
-	}
-
-	public class HealthChangedEventArgs : EventArgs
-	{
-		public float NewHealth { get; private set; }
-
-		public HealthChangedEventArgs(float newHealth)
-		{
-			NewHealth = newHealth;
-		}
-	}
-
-	public interface IDamagable
-	{
-		/// <value><c>true</c> if health is 0; otherwise, <c>false</c>.</value>
-		bool IsDestroyed { get; }
-		float Health { get; }
-		GameObject GameObject { get; }
-
-		// When health reaches 0
-		event EventHandler<DestroyedEventArgs> Destroyed;
-
-		// When health changes
-		event EventHandler<HealthChangedEventArgs> HealthChanged;
-
-		void TakeDamage(float damageAmount);
-        void Destroy();
 	}
 
 	public interface ITarget : IDamagable
