26d6fc79761a0566bbabe0076f048e6a12e9907a
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-08-30 21:42:56 -0700

    Made BomberMovementController extend TargetVelocityMovementController :)

--- Assets/Scripts/Movement/Velocity/BomberMovementController.cs
+++ Assets/Scripts/Movement/Velocity/BomberMovementController.cs
@@ -1,23 +1,10 @@
 ﻿using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace BattleCruisers.Movement.Velocity
 {
-    public class BomberMovementController : MovementController, IBomberMovementController
+    public class BomberMovementController : TargetVelocityMovementController, IBomberMovementController
 	{
-		private readonly Rigidbody2D _rigidBody;
-		private readonly float _maxVelocityInMPerS;
-
 		private float _velocitySmoothTime;
-		private Vector2 _velocity;
-
-		private const float VELOCITY_EQUALITY_MARGIN = 0.1f;
-
-		public override Vector2 Velocity
-		{
-			get { return _rigidBody.velocity; }
-			set { _rigidBody.velocity = value; }
-		}
 
 		private Vector2 _targetVelocity;
 		public Vector2 TargetVelocity
@@ -32,31 +19,16 @@ namespace BattleCruisers.Movement.Velocity
 		}
 
 		public BomberMovementController(Rigidbody2D rigidBody, float maxVelocityInMPerS)
-		{
-			Assert.IsNotNull(rigidBody);
-			Assert.IsTrue(maxVelocityInMPerS > 0);
+            : base(rigidBody, maxVelocityInMPerS) { }
 
-			_rigidBody = rigidBody;
-			_maxVelocityInMPerS = maxVelocityInMPerS;
-		}
+        protected override Vector2 FindDesiredVelocity()
+        {
+            return TargetVelocity;
+        }
 
-		public override void AdjustVelocity()
-		{ 
-			if (_rigidBody.velocity != TargetVelocity)
-			{
-				Vector2 oldVelocity = _rigidBody.velocity;
-
-				if ((_rigidBody.velocity - TargetVelocity).magnitude <= VELOCITY_EQUALITY_MARGIN)
-				{
-					_rigidBody.velocity = TargetVelocity;
-				}
-				else
-				{
-					_rigidBody.velocity = Vector2.SmoothDamp(_rigidBody.velocity, TargetVelocity, ref _velocity, _velocitySmoothTime, _maxVelocityInMPerS, Time.deltaTime);
-				}
-
-				HandleDirectionChange(oldVelocity, _rigidBody.velocity);
-			}
-		}
-	}
+        protected override float FindVelocitySmoothTime()
+        {
+            return _velocitySmoothTime;
+        }
+    }
 }
