ad9d782f2f2eca1890a90852269f58c6bb535e56
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-08-30 21:59:18 -0700

    Created FollowingXAxisMovementController :)

---
+++ Assets/Scripts/Movement/Velocity/FollowingXAxisMovementController.cs
@@ -0,0 +1,33 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Targets;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Movement.Velocity
+{
+    /// <summary>
+    /// Moves along the x-axis until we are on the same y-axis as the target.
+    /// </summary>
+    public class FollowingXAxisMovementController : TargetVelocityMovementController, ITargetConsumer
+    {
+        public ITarget Target { private get; set; }
+		
+        public FollowingXAxisMovementController(Rigidbody2D rigidBody, float maxVelocityInMPerS) 
+            : base(rigidBody, maxVelocityInMPerS) { }
+
+        protected override Vector2 FindDesiredVelocity()
+        {
+            Assert.IsNotNull(Target);
+
+            if (Target.Position.x < _rigidBody.position.x)
+            {
+                return new Vector2(-_maxVelocityInMPerS, 0);
+            }
+            else if (Target.Position.x > _rigidBody.position.x)
+            {
+                return new Vector2(_maxVelocityInMPerS, 0);
+            }
+            return new Vector2(0, 0);
+        }
+    }
+}
