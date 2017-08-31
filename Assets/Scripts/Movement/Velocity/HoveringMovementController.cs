a85dd3f17865c5de98cf879fe0607c790fac4883
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-08-30 21:50:21 -0700

    Created HoveringMovementController :D

---
+++ Assets/Scripts/Movement/Velocity/HoveringMovementController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Movement.Velocity
+{
+    /// <summary>
+    /// Reduces velocity to 0, and hovers (does not move).
+    /// 
+    /// FELIX  Face target
+    /// </summary>
+    public class HoveringMovementController : TargetVelocityMovementController
+	{
+        private readonly Vector2 _targetVelocity;
+
+        public HoveringMovementController(Rigidbody2D rigidBody, float maxVelocityInMPerS)
+            : base(rigidBody, maxVelocityInMPerS)
+        {
+            _targetVelocity = new Vector2(0, 0);
+        }
+
+        protected override Vector2 FindDesiredVelocity()
+        {
+            return _targetVelocity;
+        }
+    }
+}
