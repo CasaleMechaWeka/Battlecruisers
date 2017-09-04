eb720af831f2b41cd6f821ed76964e4f8dc3b839
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-04 13:08:58 -0700

    Updated FogOfWar to actually turn on and off a fog game object :)

--- Assets/Scripts/Cruisers/Fog/FogOfWar.cs
+++ Assets/Scripts/Cruisers/Fog/FogOfWar.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace BattleCruisers.Cruisers.Fog
 {
     public class FogOfWar : IFogOfWar
     {
+        private readonly GameObject _fogGameObject;
+
 		private bool _isFogEnabled;
 		public bool IsFogEnabled
 		{
@@ -13,6 +17,7 @@ namespace BattleCruisers.Cruisers.Fog
 				if (_isFogEnabled != value)
 				{
 					_isFogEnabled = value;
+                    _fogGameObject.SetActive(_isFogEnabled);
 
 					if (IsFogEnabledChanged != null)
 					{
@@ -23,6 +28,14 @@ namespace BattleCruisers.Cruisers.Fog
 		}
 
         public event EventHandler IsFogEnabledChanged;
+
+        public FogOfWar(GameObject fogGameObject)
+        {
+            Assert.IsNotNull(fogGameObject);
+
+            _fogGameObject = fogGameObject;
+            IsFogEnabled = false;
+        }
 
         public void UpdateIsEnabled(int numOfFriendlyStealthGenerators, int numOfEnemySpySatellites)
         {
