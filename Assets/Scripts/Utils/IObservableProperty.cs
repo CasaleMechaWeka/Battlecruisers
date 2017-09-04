d850071957a46e6ccd4e20c72f2dac7ae3deb5f3
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-04 10:36:08 -0700

    Created ObservableProperty :)

---
+++ Assets/Scripts/Utils/IObservableProperty.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BattleCruisers.Utils
+{
+    public interface IObservableProperty<T>
+    {
+        T Value { get; }
+
+        event EventHandler PropertyChanged;
+    }
+}
---
+++ Assets/Scripts/Utils/ObservableProperty.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BattleCruisers.Utils
+{
+    public class ObservableProperty<T> : IObservableProperty<T>
+	{
+        private T _value;
+		public T Value 
+        { 
+            get { return _value; }
+            set
+            {
+                if (!value.Equals(_value))
+                {
+                    _value = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged.Invoke(this, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+		public event EventHandler PropertyChanged;
+	}
+}
