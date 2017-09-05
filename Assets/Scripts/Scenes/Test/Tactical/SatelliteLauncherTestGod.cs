3a77bb567508a1313f096c705594f1d6344f3e35
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-04 19:00:16 -0700

    Use same test god for both deathstar and spy satellite test scenes :)

---
+++ Assets/Scripts/Scenes/Test/Tactical/SatelliteLauncherTestGod.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Buildables.Buildings.Offensive;
+using BattleCruisers.Buildables.Units.Aircraft.Providers;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Offensive
+{
+    public class SatelliteLauncherTestGod : CameraToggleTestGod
+    {
+        public int numOfDrones;
+
+        protected override void OnStart()
+        {
+            SatelliteLauncherController launcher = FindObjectOfType<SatelliteLauncherController>();
+
+            Vector2 parentCruiserPosition = launcher.transform.position;
+            Vector2 enemyCruiserPosition = new Vector2(launcher.transform.position.x + 30, launcher.transform.position.y);
+            IAircraftProvider aircraftProvider = new AircraftProvider(parentCruiserPosition, enemyCruiserPosition);
+
+            Helper helper = new Helper(numOfDrones);
+            helper.InitialiseBuildable(launcher, aircraftProvider: aircraftProvider);
+            launcher.StartConstruction();
+        }
+    }
+}
