6c56ecdf500cafed53bcf4314fdc9adad40a06d9
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-04 18:16:42 -0700

    Created spy satellite test scene :)

---
+++ Assets/Scripts/Scenes/Test/Aircraft/SpySatelliteTestGod.cs
@@ -0,0 +1,23 @@
+﻿using BattleCruisers.Buildables.Units.Aircraft;
+using BattleCruisers.Buildables.Units.Aircraft.Providers;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Aircraft
+{
+    public class SpySatelliteTestGod : MonoBehaviour
+    {
+        void Start()
+        {
+            Helper helper = new Helper();
+
+            Vector2 parentCruiserPosition = new Vector2(-35, 0);
+            Vector2 enemyCruiserPosition = new Vector2(35, 0);
+            IAircraftProvider aircraftProvider = new AircraftProvider(parentCruiserPosition, enemyCruiserPosition);
+
+            SpySatelliteController satellite = FindObjectOfType<SpySatelliteController>();
+            helper.InitialiseBuildable(satellite, aircraftProvider: aircraftProvider);
+            satellite.StartConstruction();
+        }
+    }
+}
