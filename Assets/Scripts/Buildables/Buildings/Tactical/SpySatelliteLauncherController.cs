e2b06c936353d4e8f48ae1a846dbb6cb28f1c736
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-04 18:40:52 -0700

    Created SpySatelliteLauncher prefab :)

---
+++ Assets/Scripts/Buildables/Buildings/Tactical/SpySatelliteLauncherController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Buildables.Buildings.Offensive
+{
+	public class SpySatelliteLauncherController : SatelliteLauncherController
+	{
+		protected override Vector3 SpawnPositionAdjustment { get { return new Vector3(0, 0.063f, 0); } }
+        public override TargetValue TargetValue { get { return TargetValue.Medium; } }
+	}
+}
