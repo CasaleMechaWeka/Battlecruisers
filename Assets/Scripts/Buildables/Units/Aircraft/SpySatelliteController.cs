39de8c47f2acf59e64373a62c69fb091f2dd8391
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-04 17:59:51 -0700

    Created SpySatelliteController, that just finds its patrol points :)

---
+++ Assets/Scripts/Buildables/Units/Aircraft/SpySatelliteController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Movement.Velocity;
+using UnityEngine;
+
+namespace BattleCruisers.Buildables.Units.Aircraft
+{
+    public class SpySatelliteController : AircraftController
+    {
+        public float cruisingAltitudeInM;
+
+        protected override IList<IPatrolPoint> GetPatrolPoints()
+        {
+			IList<Vector2> patrolPositions = _aircraftProvider.FindSpySatellitePatrolPoints(transform.position, cruisingAltitudeInM);
+
+            IList<IPatrolPoint> patrolPoints = new List<IPatrolPoint>(patrolPositions.Count)
+            {
+                new PatrolPoint(patrolPositions[0], removeOnceReached: true)
+            };
+
+            for (int i = 1; i < patrolPositions.Count; ++i)
+			{
+				patrolPoints.Add(new PatrolPoint(patrolPositions[i]));
+			}
+
+			return patrolPoints;
+        }
+    }
+}
