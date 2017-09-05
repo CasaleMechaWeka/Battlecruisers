3432446b5bbd1a50c86c8c3450ec77583b31a3ea
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-04 18:51:38 -0700

    Created SpySatelliteLauncherTest scene :)

---
+++ Assets/Scripts/Scenes/Test/Tactical/SpySatelliteLauncherTestGod.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.Buildables.Buildings.Offensive;
+using BattleCruisers.Buildables.Units.Aircraft.Providers;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Offensive
+{
+    // FELIX  Avoid duplicate code with deathstar launcher test
+	public class SpySatelliteLauncherTestGod : CameraToggleTestGod
+	{
+		protected override void OnStart()
+		{
+            SpySatelliteLauncherController launcher = FindObjectOfType<SpySatelliteLauncherController>();
+
+			Vector2 parentCruiserPosition = launcher.transform.position;
+			Vector2 enemyCruiserPosition = new Vector2(launcher.transform.position.x + 30, launcher.transform.position.y);
+			IAircraftProvider aircraftProvider = new AircraftProvider(parentCruiserPosition, enemyCruiserPosition);
+
+            Helper helper = new Helper(numOfDrones: 1);
+			helper.InitialiseBuildable(launcher, aircraftProvider: aircraftProvider);
+			launcher.StartConstruction();
+		}
+	}
+}
