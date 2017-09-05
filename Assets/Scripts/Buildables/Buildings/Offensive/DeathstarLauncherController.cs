ca5a3a2d8c7dfb316975eeab63ae297ec05ce59c
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-04 18:26:45 -0700

    Created abstract SatelliteLauncherController, as behaviour between deathstar and spy satellite launchers are almost identical :)

--- Assets/Scripts/Buildables/Buildings/Offensive/DeathstarLauncherController.cs
+++ Assets/Scripts/Buildables/Buildings/Offensive/DeathstarLauncherController.cs
@@ -1,41 +1,10 @@
-﻿using BattleCruisers.Buildables.Units;
-using UnityEngine;
-using UnityEngine.Assertions;
+﻿using UnityEngine;
 
 namespace BattleCruisers.Buildables.Buildings.Offensive
 {
-    public class DeathstarLauncherController : Building
+    public class DeathstarLauncherController : SatelliteLauncherController
 	{
-		private IUnit _deathstar;
-
-		public UnitWrapper deathstarPrefab;
-
-		private static Vector3 DEATHSTAR_SPAWN_POSITION_ADJUSTMENT = new Vector3(0, 0.015f, 0);
-
-		public override TargetValue TargetValue { get { return TargetValue.High; } }
-
-		public override void StaticInitialise()
-		{
-			base.StaticInitialise();
-
-			Assert.IsNotNull(deathstarPrefab);
-		}
-
-		protected override void OnBuildableCompleted()
-		{
-			base.OnBuildableCompleted();
-
-			_deathstar = _factoryProvider.PrefabFactory.CreateUnit(deathstarPrefab);
-			_deathstar.Position = transform.position + DEATHSTAR_SPAWN_POSITION_ADJUSTMENT;
-			_deathstar.Initialise(_parentCruiser, _enemyCruiser, _uiManager, _factoryProvider);
-			_deathstar.StartConstruction();
-		}
-
-		protected override void OnDestroyed()
-		{
-			base.OnDestroyed();
-
-			_deathstar.Destroy();
-		}
+		protected override Vector3 SpawnPositionAdjustment { get { return new Vector3(0, 0.015f, 0); } }
+        public override TargetValue TargetValue { get { return TargetValue.High; } }
 	}
 }
---
+++ Assets/Scripts/Buildables/Buildings/SatelliteLauncherController.cs
@@ -0,0 +1,39 @@
+﻿using BattleCruisers.Buildables.Units;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Offensive
+{
+	public abstract class SatelliteLauncherController : Building
+	{
+        private IUnit _satellite;
+
+        public UnitWrapper satellitePrefab;
+
+        protected abstract Vector3 SpawnPositionAdjustment { get; }
+
+		public override void StaticInitialise()
+		{
+			base.StaticInitialise();
+
+			Assert.IsNotNull(satellitePrefab);
+		}
+
+		protected override void OnBuildableCompleted()
+		{
+			base.OnBuildableCompleted();
+
+			_satellite = _factoryProvider.PrefabFactory.CreateUnit(satellitePrefab);
+			_satellite.Position = transform.position + SpawnPositionAdjustment;
+			_satellite.Initialise(_parentCruiser, _enemyCruiser, _uiManager, _factoryProvider);
+			_satellite.StartConstruction();
+		}
+
+		protected override void OnDestroyed()
+		{
+			base.OnDestroyed();
+
+			_satellite.Destroy();
+		}
+	}
+}
