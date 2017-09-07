bac43cca5c867a75c332aacc0917dab15a4fb356
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-06 21:56:08 -0700

    Created IBooster :)

---
+++ Assets/Scripts/Buildables/Boost/Booster.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BattleCruisers.Buildables.Boost
+{
+    public class Booster : IBooster
+	{
+        private const float DEFAULT_BOOST_MULTIPLIER = 1;
+
+        private float _boostMultiplier;
+		public float BoostMultiplier 
+        { 
+            get { return _boostMultiplier; }
+            set
+            {
+                if (_boostMultiplier != value)
+                {
+                    _boostMultiplier = value;
+
+                    if (BoostChanged != null)
+                    {
+                        BoostChanged.Invoke(this, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+		public event EventHandler BoostChanged;
+
+        public Booster()
+        {
+            _boostMultiplier = DEFAULT_BOOST_MULTIPLIER;
+        }
+	}
+}
---
+++ Assets/Scripts/Buildables/Boost/IBooster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BattleCruisers.Buildables.Boost
+{
+    public interface IBooster
+    {
+		// < 1 to reduce performance, > 1 to improve performance, 1 by default
+		float BoostMultiplier { get; set; }
+
+        event EventHandler BoostChanged;
+	}
+}
