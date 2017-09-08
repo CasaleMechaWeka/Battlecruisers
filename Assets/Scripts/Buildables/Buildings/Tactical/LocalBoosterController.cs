0aed16598a83578f339a8c2001e09bd2dc645e4d
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-07 22:23:00 -0700

    Completed LocalBoosterController :D  (I think :P )

--- Assets/Scripts/Buildables/Buildings/Tactical/LocalBoosterController.cs
+++ Assets/Scripts/Buildables/Buildings/Tactical/LocalBoosterController.cs
@@ -1,7 +1,47 @@
-﻿namespace BattleCruisers.Buildables.Buildings.Tactical
+﻿using BattleCruisers.Buildables.Boost;
+using BattleCruisers.Cruisers.Slots;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Tactical
 {
     public class LocalBoosterController : Building
     {
-        // FELIX
+        private IBoostProvider _boostProvider;
+
+        private const int BOOST_RADIUS_IN_M = 1;
+
+        public float boostMultiplier;
+        public LayerMask slotLayerMask;
+
+        protected override void OnInitialised()
+        {
+            base.OnInitialised();
+
+            _boostProvider = new BoostProvider(boostMultiplier);
+        }
+
+        protected override void OnBuildableCompleted()
+        {
+            base.OnBuildableCompleted();
+
+            // Provide boost to nearby slots
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, BOOST_RADIUS_IN_M, slotLayerMask);
+
+            foreach (Collider2D collider in colliders)
+            {
+                ISlot slot = collider.GetComponent<ISlot>();
+                Assert.IsNotNull(slot, "All colliders in the slots layer should contain an ISlot component :D");
+
+                slot.BoostConsumer.AddBoostProvider(_boostProvider);
+            }
+		}
+
+        protected override void OnDestroyed()
+        {
+            base.OnDestroyed();
+
+            _boostProvider.ClearBoostConsumers();
+        }
     }
 }
