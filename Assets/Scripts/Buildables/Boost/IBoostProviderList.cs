a6167553edc2feb241b019d591297f88a168c96b
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-09 12:18:05 -0700

    Created BoostProviderList :)

---
+++ Assets/Scripts/Buildables/Boost/IBoostProviderList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace BattleCruisers.Buildables.Boost
+{
+	public interface IBoostProviderList
+	{
+        ReadOnlyCollection<IBoostProvider> BoostProviders { get; }
+
+		event EventHandler ProvidersChanged;
+
+		void AddBoostProvider(IBoostProvider boostProvider);
+		void RemoveBoostProvider(IBoostProvider boostProvider);
+	}
+}
--- Assets/Scripts/Utils/IFactoryProvider.cs
+++ Assets/Scripts/Utils/IFactoryProvider.cs
@@ -1,17 +1,14 @@
 ﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
 using BattleCruisers.Buildables.Units.Aircraft.Providers;
-using BattleCruisers.Cruisers;
 using BattleCruisers.Fetchers;
 using BattleCruisers.Movement;
-using BattleCruisers.Movement.Velocity;
 using BattleCruisers.Movement.Predictors;
 using BattleCruisers.Projectiles.FlightPoints;
 using BattleCruisers.Targets;
-using System;
 
 namespace BattleCruisers.Utils
 {
-	public interface IFactoryProvider
+    public interface IFactoryProvider
 	{
 		IPrefabFactory PrefabFactory { get; }
 		ITargetsFactory TargetsFactory { get; }
