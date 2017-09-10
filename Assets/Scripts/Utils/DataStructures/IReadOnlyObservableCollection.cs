71006dab128c88db3fc564137f3eab81a70e3061
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-10 14:13:40 -0700

    Implemented own IReadOnlyObservableCollection :)

---
+++ Assets/Scripts/Utils/DataStructures/IReadOnlyObservableCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace BattleCruisers.Utils.DataStrctures
+{
+    public enum ChangeType
+    {
+        Add, Remove
+    }
+
+    public class CollectionChangedEventArgs<T> : EventArgs
+    {
+        public ChangeType Type { get; private set; }
+        public T Item { get; private set; }
+
+        public CollectionChangedEventArgs(ChangeType type, T item)
+        {
+            Type = type;
+            Item = item;
+        }
+    }
+
+    /// <summary>
+    /// Unity does not have ReadOnlyObservableCollection, so have to implement my own :)
+    /// </summary>
+    public interface IReadOnlyObservableCollection<T>
+    {
+        ReadOnlyCollection<T> Items { get; }
+
+        event EventHandler<CollectionChangedEventArgs<T>> Changed;
+
+        void Add(T item);
+        void Remove(T item);
+    }
+}
---
+++ Assets/Scripts/Utils/DataStructures/ReadOnlyObservableCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.DataStrctures
+{
+    public class ReadOnlyObservableCollection<T> : IReadOnlyObservableCollection<T>
+    {
+        private readonly IList<T> _items;
+        public ReadOnlyCollection<T> Items { get; private set; }
+
+        public event EventHandler<CollectionChangedEventArgs<T>> Changed;
+
+        public ReadOnlyObservableCollection(IList<T> items)
+        {
+            Assert.IsNotNull(items);
+
+            _items = items;
+            Items = new ReadOnlyCollection<T>(_items);
+        }
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+            EmitChangedEvent(ChangeType.Add, item);
+        }
+
+        public void Remove(T item)
+        {
+            bool wasSuccessful = _items.Remove(item);
+
+            if (wasSuccessful)
+            {
+                EmitChangedEvent(ChangeType.Remove, item);
+            }
+        }
+
+        private void EmitChangedEvent(ChangeType type, T item)
+        {
+            if (Changed != null)
+            {
+                Changed.Invoke(this, new CollectionChangedEventArgs<T>(type, item));
+            }
+        }
+    }
+}
