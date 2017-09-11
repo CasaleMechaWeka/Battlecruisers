2fd4e0c1b4921d5236cef43017dc047774dad09a
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-10 18:41:48 -0700

    Created LocalBooster test scene :)

---
+++ Assets/Scripts/Scenes/Test/Tactical/LocalBoosterTestGod.cs
@@ -0,0 +1,50 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Buildings.Tactical;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Targets;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using NSubstitute;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Tactical
+{
+    public class LocalBoosterTestGod : MonoBehaviour
+    {
+        void Start()
+        {
+            Helper helper = new Helper();
+
+
+            // Setup target
+            AirFactory target = FindObjectOfType<AirFactory>();
+            helper.InitialiseBuilding(target, Faction.Reds);
+            target.StartConstruction();
+			
+			
+			// Setup artillery slot
+			ICruiser parentCruiser = Substitute.For<ICruiser>();
+			Slot slotToBoost = FindObjectOfType<Slot>();
+			slotToBoost.Initialise(parentCruiser);
+
+
+            // Setup artillery
+            IExactMatchTargetFilter targetFilter = new ExactMatchTargetFilter();
+            targetFilter.Target = target;
+            ITargetsFactory targetsFactory = helper.CreateTargetsFactory(target.GameObject, targetFilter);
+
+            TurretController turret = FindObjectOfType<TurretController>();
+            helper.InitialiseBuilding(turret, Faction.Blues, targetsFactory: targetsFactory, localBoostProviders: slotToBoost.BoostProviders);
+            turret.StartConstruction();
+
+
+            // Setup local booster
+            LocalBoosterController localBooster = FindObjectOfType<LocalBoosterController>();
+            helper.InitialiseBuilding(localBooster);
+            localBooster.StartConstruction();
+        }
+    }
+}
