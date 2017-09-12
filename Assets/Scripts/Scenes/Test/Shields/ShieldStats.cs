c0905e32b7158f14b1fb3d93bc605a2392c046fe
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-11 20:29:05 -0700

    Created ShieldStats :)

---
+++ Assets/Scripts/Scenes/Test/Shields/ShieldStats.cs
@@ -0,0 +1,15 @@
+﻿namespace BattleCruisers.Buildables.Buildings.Tactical.Shields
+{
+    public class ShieldStats : IShieldStats
+	{
+		public float shieldRadiusInM;
+		public float shieldRechargeDelayInS;
+		public float shieldRechargeRatePerS;
+
+        public float ShieldRadiusInM { get { return shieldRadiusInM; } }
+        public float ShieldRechargeDelayInS { get { return shieldRechargeDelayInS; } }
+        public float ShieldRechargeRatePerS { get { return BoostMultiplier * shieldRechargeRatePerS; } }
+
+        public float BoostMultiplier { private get; set; }
+	}
+}
