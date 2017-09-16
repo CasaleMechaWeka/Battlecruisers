6f469fe267d3e8743b7b8535a9f524898135af21
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-16 16:15:22 -0700

    Created test scene for double barrel turret (not double barrel turret yet :P)

---
+++ Assets/Scripts/Scenes/Test/Turrets/BroadsidesBarrelTestGod.cs
@@ -0,0 +1,37 @@
+﻿using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+using BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers;
+using BattleCruisers.Movement.Predictors;
+using BattleCruisers.Movement.Rotation;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test
+{
+    // FELIX  Avoid duplicate code with ArtilleryBarrelControllerTests
+    public class BroadsidesBarrelTestGod : MonoBehaviour
+	{
+		void Start()
+		{
+            // Initialise target
+            Helper helper = new Helper();
+            Factory target = FindObjectOfType<Factory>();
+            helper.InitialiseBuilding(target);
+
+
+            // Initialise double barrel
+			IAngleCalculator angleCalculator = new ArtilleryAngleCalculator(new TargetPositionPredictorFactory());
+            IExactMatchTargetFilter targetFilter = new ExactMatchTargetFilter()
+            {
+                Target = target
+            };
+
+            BarrelController doubleBarrel = FindObjectOfType<BarrelController>();
+			doubleBarrel.StaticInitialise();
+			IRotationMovementController rotationMovementController = new RotationMovementController(angleCalculator, doubleBarrel.TurretStats.turretRotateSpeedInDegrees, doubleBarrel.transform);
+			doubleBarrel.Target = target;
+			doubleBarrel.Initialise(targetFilter, angleCalculator, rotationMovementController);
+		}
+	}
+}
