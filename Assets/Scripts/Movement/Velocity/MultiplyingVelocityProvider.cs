274b95d7cbdd4f26c325ae91c280de5ae8a46258
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-16 12:11:53 -0700

    Created MultiplyingVelocityProvider :)

---
+++ Assets/Scripts/Movement/Velocity/MultiplyingVelocityProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Movement.Velocity
+{
+    public class MultiplyingVelocityProvider : IVelocityProvider
+    {
+        private readonly IVelocityProvider _providerToWrap;
+        private readonly float _multiplier;
+
+        public float VelocityInMPerS { get { return _providerToWrap.VelocityInMPerS * _multiplier; } }
+
+        public MultiplyingVelocityProvider(IVelocityProvider providerToWrap, float multiplier)
+        {
+            Assert.IsNotNull(providerToWrap);
+
+            _providerToWrap = providerToWrap;
+            _multiplier = multiplier;
+        }
+    }
+}
