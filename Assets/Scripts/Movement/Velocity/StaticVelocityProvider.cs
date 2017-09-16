2f42e74ec2ddb98f538e2628faf5b52819d37725
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-16 12:00:05 -0700

    Created StaticVelocityProvider :D

---
+++ Assets/Scripts/Movement/Velocity/StaticVelocityProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace BattleCruisers.Movement.Velocity
+{
+    public class StaticVelocityProvider : IVelocityProvider
+    {
+        public float VelocityInMPerS { get; private set; }
+
+        public StaticVelocityProvider(float velocityInMPerS)
+        {
+            VelocityInMPerS = velocityInMPerS;
+        }
+    }
+}
