5388e6102784c2848483c77d3550c03e1ba6cdef
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-09-16 15:45:25 -0700

    Created BroadsidesController stub :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BroadsidesController.cs
@@ -0,0 +1,8 @@
+﻿namespace BattleCruisers.Buildables.Buildings.Turrets
+{
+    // FELIX  Avoid duplicate code with TurretController?
+    public class BroadsidesController : Building
+    {
+        
+    }
+}
