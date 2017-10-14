9f4bec1476927557f73f4c325f3abad1cda70988
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-14 15:20:08 -0700

    Created IDroneNumFeedback :)

---
+++ Assets/Scripts/Buildables/Repairables/IDroneNumFeedback.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Drones;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Buildables.Repairables
+{
+    /// <summary>
+    /// Provides feedback to the user about a drone consumer's number of drones.
+    /// </summary>
+    public interface IDroneNumFeedback : IManagedDisposable
+    {
+        IDroneConsumer DroneConsumer { get; }
+    }
+}
