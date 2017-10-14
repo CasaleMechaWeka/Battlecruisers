60bc20b154b9d7c9524dc4db7897095ee8b09290
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-14 15:33:57 -0700

    Created TextMeshWrapper for testability :)

---
+++ Assets/Scripts/Utils/UIWrappers/ITextMesh.cs
@@ -0,0 +1,8 @@
+﻿namespace BattleCruisers.Utils.UIWrappers
+{
+    public interface ITextMesh
+    {
+        string Text { get; set; }
+        void SetActive(bool isActive);
+    }
+}
---
+++ Assets/Scripts/Utils/UIWrappers/TextMeshWrapper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.UIWrappers
+{
+    public class TextMeshWrapper : ITextMesh
+    {
+        private readonly TextMesh _textMesh;
+
+        public string Text
+        {
+            get { return _textMesh.text; }
+            set { _textMesh.text = value; }
+        }
+
+        public TextMeshWrapper(TextMesh textMesh)
+        {
+            Assert.IsNotNull(textMesh);
+            _textMesh = textMesh;
+        }
+
+        public void SetActive(bool isActive)
+        {
+            _textMesh.gameObject.SetActive(isActive);
+        }
+    }
+}
