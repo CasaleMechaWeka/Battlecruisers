239f63b56570d1c6d7e8aa65a5b2bb4d781124ce
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-14 15:19:59 -0700

    Created IManagedDisposable

---
+++ Assets/Scripts/Utils/IManagedDispoasable.cs
@@ -0,0 +1,11 @@
+﻿namespace BattleCruisers.Utils
+{
+    /// <summary>
+    /// Provides a common way for classes to clean themselves up (usually releasing
+    /// event handlers).
+    /// </summary>
+    public interface IManagedDisposable
+    {
+        void DisposeManagedState();
+    }
+}
