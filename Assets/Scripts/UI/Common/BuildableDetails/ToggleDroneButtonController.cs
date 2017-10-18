e588798afd8e41b33499c9ce94ff81a224a6a102
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-17 21:16:59 -0700

    Created ToggleDroneButtonController :)

---
+++ Assets/Scripts/UI/Common/BuildableDetails/ToggleDroneButtonController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using BattleCruisers.Buildables;
+using BattleCruisers.Drones;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.Common.BuildingDetails
+{
+    public class ToggleDroneButtonController : MonoBehaviour
+    {
+        private Button _button;
+        private IDroneManager _droneManager;
+
+        private IBuildable _buildable;
+        public IBuildable Buildable
+        {
+            private get { return _buildable; }
+            set
+            {
+                if (_buildable != null)
+                {
+                    _buildable.CompletedBuildable -= Buildable_CompletedBuildable;
+                }
+
+                _buildable = value;
+				UpdateVisibility();
+
+                if (_buildable != null && _buildable.BuildableState != BuildableState.Completed)
+                {
+                    _buildable.CompletedBuildable += Buildable_CompletedBuildable;
+                }
+            }
+        }
+
+        // FELIX  Units should never have toggle button :/
+        // FELIX  Units should never show number of drones text mesh :/
+        // FELIX  Factory should still be toggleable while producing units!
+        // Should only be visible for player buildings, not AI buildings
+        private bool ShowToggleDroneButton 
+        { 
+            get 
+            { 
+                return 
+                    _buildable != null
+                    && _buildable.Faction == Faction.Blues 
+                    && _buildable.DroneConsumer != null; 
+            } 
+        }
+
+        public void Initialise(IDroneManager droneManager)
+        {
+            Assert.IsNotNull(droneManager);
+            _droneManager = droneManager;
+
+            _button = GetComponent<Button>();
+            Assert.IsNotNull(_button);
+            _button.onClick.AddListener(ToggleDroneButton);
+        }
+
+        private void ToggleDroneButton()
+        {
+            _droneManager.ToggleDroneConsumerFocus(Buildable.DroneConsumer);
+        }
+
+        private void Buildable_CompletedBuildable(object sender, EventArgs e)
+        {
+            _buildable.CompletedBuildable -= Buildable_CompletedBuildable;
+            UpdateVisibility();
+        }
+        
+        private void UpdateVisibility()
+        {
+            gameObject.SetActive(ShowToggleDroneButton);
+        }
+    }
+}
