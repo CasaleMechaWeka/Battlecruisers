26eccb18fc6e082dc6e14937ed1eeca9830d4f86
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-20 20:51:50 -0700

    Created CruiserArgs to hide Cruiser intialisation paramter ugliness :P

---
+++ Assets/Scripts/Cruisers/CruiserArgs.cs
@@ -0,0 +1,40 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Repairables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Drones;
+using BattleCruisers.UI.BattleScene;
+using BattleCruisers.UI.BattleScene.ProgressBars;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Cruisers
+{
+    public class CruiserArgs : ICruiserArgs
+    {
+        public Faction Faction { get; private set; }
+        public ICruiser EnemyCruiser { get; private set; }
+        public HealthBarController HealthBarController { get; private set; }
+        public IUIManager UiManager { get; private set; }
+        public IDroneManager DroneManager { get; private set; }
+        public IDroneConsumerProvider DroneConsumerProvider { get; private set; }
+        public IFactoryProvider FactoryProvider { get; private set; }
+        public Direction FacingDirection { get; private set; }
+        public RepairManager RepairManager { get; private set; }
+        public bool ShouldShowFog { get; private set; }
+
+        public CruiserArgs(Faction faction, ICruiser enemyCruiser, HealthBarController healthBarController,
+            IUIManager uiManager, IDroneManager droneManager, IDroneConsumerProvider droneConsumerProvider,
+            IFactoryProvider factoryProvider, Direction facingDirection, RepairManager repairManager, bool shouldShowFog)
+        {
+            Faction = faction;
+            EnemyCruiser = enemyCruiser;
+            HealthBarController = healthBarController;
+            UiManager = uiManager;
+            DroneManager = droneManager;
+            DroneConsumerProvider = droneConsumerProvider;
+            FactoryProvider = factoryProvider;
+            FacingDirection = facingDirection;
+            RepairManager = repairManager;
+            ShouldShowFog = shouldShowFog;
+        }
+    }
+}
---
+++ Assets/Scripts/Cruisers/ICruiserArgs.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Repairables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Drones;
+using BattleCruisers.UI.BattleScene;
+using BattleCruisers.UI.BattleScene.ProgressBars;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Cruisers
+{
+    public interface ICruiserArgs
+    {
+        Faction Faction { get; }
+        ICruiser EnemyCruiser { get; }
+        HealthBarController HealthBarController { get; }
+        IUIManager UiManager { get; }
+        IDroneManager DroneManager { get; }
+        IDroneConsumerProvider DroneConsumerProvider { get; }
+        IFactoryProvider FactoryProvider { get; }
+        Direction FacingDirection { get; }
+        RepairManager RepairManager { get; }
+        bool ShouldShowFog { get; }
+    }
+}
