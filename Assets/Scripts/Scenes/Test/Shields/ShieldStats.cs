77f7ad9d7520965e112b70700ed199f9985d79e0
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-21 16:43:11 -0700

    Removed FELIX :(

--- Assets/Scripts/Scenes/Test/Shields/ShieldStats.cs
+++ Assets/Scripts/Scenes/Test/Shields/ShieldStats.cs
@@ -2,7 +2,6 @@
 
 namespace BattleCruisers.Buildables.Buildings.Tactical.Shields
 {
-    // FELIX  Also move shield health here, currently using Target.Health : /
     public class ShieldStats : MonoBehaviour, IShieldStats
 	{
 		public float shieldRadiusInM;
