930ccb45b0423d715c480c70a9aec349dfc642f1
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-26 22:00:21 -0700

    Created IDamageApplierFactory :)

---
+++ Assets/Scripts/Projectiles/DamageAppliers/DamageApplierFactory.cs
@@ -0,0 +1,30 @@
+﻿using BattleCruisers.Projectiles.Stats.Wrappers;
+using BattleCruisers.Targets;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Projectiles.DamageAppliers
+{
+    public class DamageApplierFactory : IDamageApplierFactory
+    {
+        private readonly ITargetsFactory _targetsFactory;
+
+        public DamageApplierFactory(ITargetsFactory targetsFactory)
+        {
+            Assert.IsNotNull(targetsFactory);
+            _targetsFactory = targetsFactory;
+        }
+
+        public IDamageApplier CreateSingleDamageApplier(IProjectileStats projectileStats)
+        {
+            return new SingleDamageApplier(projectileStats.Damage);
+        }
+
+        public IDamageApplier CreateAreaOfDamageApplier(IProjectileStats projectileStats)
+        {
+            // All ITargets are susceptible to area of effect damage
+            ITargetFilter damageTargetFilter = _targetsFactory.CreateDummyTargetFilter(isMatchResult: true);
+            return new AreaOfEffectDamageApplier(projectileStats.Damage, projectileStats.DamageRadiusInM, damageTargetFilter);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectiles/DamageAppliers/IDamageApplierFactory.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Projectiles.Stats.Wrappers;
+
+namespace BattleCruisers.Projectiles.DamageAppliers
+{
+    public interface IDamageApplierFactory
+    {
+        IDamageApplier CreateSingleDamageApplier(IProjectileStats projectileStats);
+        IDamageApplier CreateAreaOfDamageApplier(IProjectileStats projectileStats);
+    }
+}
