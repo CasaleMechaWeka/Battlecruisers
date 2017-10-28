e66c82547eafe2607cd04f166bd76f3f382ae423
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-28 12:54:29 -0700

    Created (very) basic explosion, and explosion test scene :)

---
+++ Assets/Scripts/Projectiles/Explosions/Explosion.cs
@@ -0,0 +1,33 @@
+﻿using BattleCruisers.Utils;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Projectiles.Explosions
+{
+    /// <summary>
+    /// Explosion scaled by default to have a radius of 1 meter.  Adjust transform
+    /// scale linearly to achieve desired radius.
+    /// </summary>
+    public class Explosion : MonoBehaviour
+    {
+        private float _radiusToScaleRatio;
+
+        public void StaticInitailise()
+        {
+            RectTransform rectTransfrom = transform.Parse<RectTransform>();
+
+            Assert.AreEqual(rectTransfrom.sizeDelta.x, rectTransfrom.sizeDelta.y);
+            Assert.AreEqual(transform.localScale.x, transform.localScale.y);
+
+            _radiusToScaleRatio = rectTransfrom.sizeDelta.x / transform.localScale.x;
+        }
+
+        public void Show(float radiusInM, float durationInS)
+        {
+            // FELIX  Use duration to fade/disappear
+
+            float newScale = radiusInM / _radiusToScaleRatio;
+            transform.localScale = new Vector3(newScale, newScale, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/Test/Projectiles/ExplosionTestGod.cs
@@ -0,0 +1,15 @@
+﻿using BattleCruisers.Projectiles.Explosions;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Projectiles
+{
+    public class ExplosionTestGod : MonoBehaviour
+    {
+        void Start()
+        {
+            Explosion explosion = FindObjectOfType<Explosion>();
+            explosion.StaticInitailise();
+            explosion.Show(radiusInM: 2, durationInS: 1);
+        }
+    }
+}
