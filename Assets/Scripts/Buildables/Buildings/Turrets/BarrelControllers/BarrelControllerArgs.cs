25e92862462d01ccc17f70908e374f564104ef70
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-29 14:30:50 -0700

    Create BarrelControllerArgs :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/BarrelControllerArgs.cs
@@ -0,0 +1,43 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.AccuracyAdjusters;
+using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+using BattleCruisers.Movement.Predictors;
+using BattleCruisers.Movement.Rotation;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers
+{
+    public class BarrelControllerArgs : IBarrelControllerArgs
+    {
+        public ITargetFilter TargetFilter { get; private set; }
+        public ITargetPositionPredictor TargetPositionPredictor { get; private set; }
+        public IAngleCalculator AngleCalculator { get; private set; }
+        public IAccuracyAdjuster AccuracyAdjuster { get; private set; }
+        public IRotationMovementController RotationMovementController { get; private set; }
+        public IFactoryProvider FactoryProvider { get; private set; }
+
+        public BarrelControllerArgs(
+            ITargetFilter targetFilter,
+            ITargetPositionPredictor targetPositionPredictor,
+            IAngleCalculator angleCalculator,
+            IAccuracyAdjuster accuracyAdjuster,
+            IRotationMovementController rotationMovementController,
+            IFactoryProvider factoryProvider)
+        {
+            Helper.AssertIsNotNull(
+                targetFilter, 
+                targetPositionPredictor, 
+                angleCalculator, 
+                accuracyAdjuster, 
+                rotationMovementController, 
+                factoryProvider);
+
+            TargetFilter = targetFilter;
+            TargetPositionPredictor = targetPositionPredictor;
+            AngleCalculator = angleCalculator;
+            AccuracyAdjuster = accuracyAdjuster;
+            RotationMovementController = rotationMovementController;
+            FactoryProvider = factoryProvider;
+        }
+    }
+}
---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/IBarrelControllerArgs.cs
@@ -0,0 +1,19 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.AccuracyAdjusters;
+using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+using BattleCruisers.Movement.Predictors;
+using BattleCruisers.Movement.Rotation;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers
+{
+    public interface IBarrelControllerArgs
+    {
+        ITargetFilter TargetFilter { get; }
+        ITargetPositionPredictor TargetPositionPredictor { get; }
+        IAngleCalculator AngleCalculator { get; }
+        IAccuracyAdjuster AccuracyAdjuster { get; }
+        IRotationMovementController RotationMovementController { get; }
+        IFactoryProvider FactoryProvider { get; }
+    }
+}
