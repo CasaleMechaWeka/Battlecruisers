c547aaf68f34368e69371ff90215ccbd838db5ef
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-28 22:14:18 -0700

    Override target position predictor for leading barrel wrappers :)

--- Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/LeadingDirectFireBarrelWrapper.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/LeadingDirectFireBarrelWrapper.cs
@@ -1,9 +1,15 @@
 ﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+using BattleCruisers.Movement.Predictors;
 
 namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
 {
 	public class LeadingDirectFireBarrelWrapper : BarrelWrapper
 	{
+        protected override ITargetPositionPredictor CreateTargetPositionPredictor()
+        {
+            return _factoryProvider.TargetPositionPredictorFactory.CreateLinearPredictor();
+        }
+
 		protected override IAngleCalculator CreateAngleCalculator()
 		{
 			return _factoryProvider.AngleCalculatorFactory.CreateLeadingAngleCalcultor(_factoryProvider.TargetPositionPredictorFactory);
--- Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/MortarFireBarrelWrapper.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/MortarFireBarrelWrapper.cs
@@ -1,9 +1,15 @@
 ﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+using BattleCruisers.Movement.Predictors;
 
 namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
 {
     public class MortarFireBarrelWrapper : BarrelWrapper
 	{
+        protected override ITargetPositionPredictor CreateTargetPositionPredictor()
+        {
+            return _factoryProvider.TargetPositionPredictorFactory.CreateMortarPredictor();
+        }
+
 		protected override IAngleCalculator CreateAngleCalculator()
 		{
             return _factoryProvider.AngleCalculatorFactory.CreateMortarAngleCalcultor(_factoryProvider.TargetPositionPredictorFactory);
