d3b5ec34702a2ecef1509c4265cafe3d9146cead
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-29 14:04:32 -0700

    Create null object accuracy adjuster

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AccuracyAdjusters/DummyAccuracyAdjuster.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.AccuracyAdjusters
+{
+    /// <summary>
+    /// Null object.
+    /// </summary>
+    public class DummyAccuracyAdjuster : IAccuracyAdjuster
+    {
+        public float FindAngleInDegrees(float idealFireAngle, Vector2 sourcePosition, Vector2 targetPosition, bool isSourceMirrored)
+        {
+            return idealFireAngle;
+        }
+    }
+}
