d3b9b977a663d5c62119d6300f469099043cdf40
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-28 19:50:58 -0700

    Created TargetBounds :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AccuracyAdjusters/TargetBounds.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.AccuracyAdjusters
+{
+    public class TargetBounds : ITargetBounds
+    {
+        public Vector2 MinPosition { get; private set; }
+        public Vector2 MaxPosition { get; private set; }
+
+        public TargetBounds(Vector2 minPosition, Vector2 maxPosition)
+        {
+            MinPosition = minPosition;
+            MaxPosition = maxPosition;
+        }
+    }
+}
