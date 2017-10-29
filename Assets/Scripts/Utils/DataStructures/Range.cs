c690c3a00c19446ee33926fc798aa9a11aeb4130
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-29 13:37:02 -0700

    Create Range :)

---
+++ Assets/Scripts/Utils/DataStructures/Range.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleCruisers.Utils.DataStrctures
+{
+    public class Range<T> : IRange<T>
+    {
+        public T Min { get; private set; }
+        public T Max { get; private set; }
+
+        public Range(T min, T max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
