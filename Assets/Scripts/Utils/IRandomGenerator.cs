e268b75de8b233f9599d3bcfeb6ce246a68f6096
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-29 13:55:33 -0700

    Create RandomGenerator :)

---
+++ Assets/Scripts/Utils/IRandomGenerator.cs
@@ -0,0 +1,7 @@
+﻿namespace BattleCruisers.Utils
+{
+    public interface IRandomGenerator
+    {
+        float Range(float minInclusive, float maxInclusive);
+    }
+}
---
+++ Assets/Scripts/Utils/RandomGenerator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Utils
+{
+    public class RandomGenerator : IRandomGenerator
+    {
+        public float Range(float minInclusive, float maxInclusive)
+        {
+            return Random.Range(minInclusive, maxInclusive);
+        }
+    }
+}
