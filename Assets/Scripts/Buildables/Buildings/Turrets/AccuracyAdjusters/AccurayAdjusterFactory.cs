b4377f565c5ed735a38bd8bc2a02d050e3ad7827
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-29 14:16:31 -0700

    Create AccuracyAdjusterFactory :D

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AccuracyAdjusters/AccurayAdjusterFactory.cs
@@ -0,0 +1,27 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.AccuracyAdjusters
+{
+    public class AccuracyAdjusterFactory : IAccuracyAdjusterFactory
+    {
+        public IAccuracyAdjuster CreateDummyAdjuster()
+        {
+            return new DummyAccuracyAdjuster();
+        }
+
+        public IAccuracyAdjuster CreateGravityAffectedProjectileAdjuster(
+            IAngleCalculator angleCalculator, 
+            float projectileVelocityInMPerS, 
+            float accuracy)
+        {
+            return
+                new AccuracyAdjuster(
+                    new GravityAffectedTargetBoundsFinder(),
+                    angleCalculator,
+                    new RandomGenerator(),
+                    projectileVelocityInMPerS,
+                    accuracy);
+        }
+    }
+}
---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AccuracyAdjusters/IAccuracyAdjusterFactory.cs
@@ -0,0 +1,14 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.AccuracyAdjusters
+{
+    public interface IAccuracyAdjusterFactory
+    {
+        IAccuracyAdjuster CreateDummyAdjuster();
+
+        IAccuracyAdjuster CreateGravityAffectedProjectileAdjuster(
+            IAngleCalculator angleCalculator,
+            float projectileVelocityInMPerS,
+            float accuracy);
+    }
+}
