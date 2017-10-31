9833328e680df4570214199219a0577f0fd0b439
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-30 22:31:33 -0700

    Created IAngleRangeFinder and LinearRangeFinder implementation

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AccuracyAdjusters/LinearRangeFinder.cs
@@ -0,0 +1,26 @@
+﻿using BattleCruisers.Utils.DataStrctures;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.AccuracyAdjusters
+{
+    public class LinearRangeFinder : IAngleRangeFinder
+    {
+        public IRange<float> FindFireAngleRange(IRange<float> onTargetRange, float accuracy)
+        {
+            Assert.IsTrue(onTargetRange.Max > onTargetRange.Min);
+
+            float onTargetRangeSize = onTargetRange.Max - onTargetRange.Min;
+
+            float fireAngleRangeSize = onTargetRangeSize / accuracy;
+            Assert.IsTrue(fireAngleRangeSize > onTargetRangeSize);
+
+            float totalErrorMargin = fireAngleRangeSize - onTargetRangeSize;
+            float errorMarginEachSide = totalErrorMargin / 2;
+
+            float minFireAngle = onTargetRange.Min - errorMarginEachSide;
+            float maxFireAngle = onTargetRange.Max + errorMarginEachSide;
+
+            return new Range<float>(minFireAngle, maxFireAngle);
+        }
+    }
+}
