42328fe27ee5cbd9ba25891724385f21e279d13b
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-10-31 21:37:56 -0700

    Create OrderedRange

---
+++ Assets/Scripts/Utils/DataStructures/OrderedRange.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Utils.DataStrctures
+{
+    public class OrderedRange : Range<float>
+    {
+        public OrderedRange(float value1, float value2) 
+            : base(Mathf.Min(value1, value2), Mathf.Max(value1, value2))
+        {
+        }
+    }
+}
