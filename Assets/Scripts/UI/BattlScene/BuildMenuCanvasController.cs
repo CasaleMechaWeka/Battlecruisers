ea2ba684bd4a8ec2c8031bbc93a0c65f010cbf3a
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-03 20:52:45 -0700

    Create BuildMenuCanvasController to programmatically retrieve MonoBehaviours :)

---
+++ Assets/Scripts/UI/BattlScene/BuildMenuCanvasController.cs
@@ -0,0 +1,30 @@
+﻿using BattleCruisers.UI.BattleScene.ProgressBars;
+using BattleCruisers.UI.Common.BuildingDetails;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene
+{
+    public class BuildMenuCanvasController : MonoBehaviour
+    {
+        public BuildableDetailsController BuildableDetails { get; private set; }
+        public InBattleCruiserDetailsController CruiserDetails { get; private set; }
+        public HealthBarController PlayerCruiserHealthBar { get; private set; }
+        public HealthBarController AiCruiserHealthBar { get; private set; }
+
+        public void Initialise()
+        {
+            BuildableDetails = GetComponentInChildren<BuildableDetailsController>(includeInactive: true);
+            Assert.IsNotNull(BuildableDetails);
+
+            CruiserDetails = GetComponentInChildren<InBattleCruiserDetailsController>(includeInactive: true);
+            Assert.IsNotNull(CruiserDetails);
+
+            PlayerCruiserHealthBar = GetComponentInChildren<HealthBarController>(includeInactive: true);
+            Assert.IsNotNull(PlayerCruiserHealthBar);
+
+            AiCruiserHealthBar = GetComponentInChildren<HealthBarController>(includeInactive: true);
+            Assert.IsNotNull(AiCruiserHealthBar);
+        }
+    }
+}
