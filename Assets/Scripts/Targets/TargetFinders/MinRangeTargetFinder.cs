74d3a89bf6c9d72c0ca2d415ae6f9c6babf7816e
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-04 19:53:34 -0700

    Create MinRangeTargetFinder, for mortar :)

---
+++ Assets/Scripts/Targets/TargetFinders/MinRangeTargetFinder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Targets.TargetFinders
+{
+    /// <summary>
+    /// Target zone is a donut.  Targets are found as they enter this donut 
+    /// and lost as they exit.
+    /// </summary>
+    public class MinRangeTargetFinder : ITargetFinder
+    {
+        private readonly ITargetDetector _maxRangeDetector, _minRangeDetector;
+        private readonly ITargetFilter _targetFilter;
+
+        public event EventHandler<TargetEventArgs> TargetFound;
+        public event EventHandler<TargetEventArgs> TargetLost;
+
+        public MinRangeTargetFinder(
+            ITargetDetector maxRangeDetector, 
+            ITargetDetector minRangeDetector, 
+            ITargetFilter targetFilter)
+        {
+            Helper.AssertIsNotNull(maxRangeDetector, minRangeDetector, targetFilter);
+
+            _maxRangeDetector = maxRangeDetector;
+            _minRangeDetector = minRangeDetector;
+            _targetFilter = targetFilter;
+        }
+
+        public void StartFindingTargets()
+        {
+            _maxRangeDetector.OnEntered += OnTargetFound;
+            _maxRangeDetector.OnExited += OnTargetLost;
+
+            _minRangeDetector.OnEntered += OnTargetLost;
+            _minRangeDetector.OnExited += OnTargetFound;
+        }
+
+        private void OnTargetFound(object sender, TargetEventArgs args)
+        {
+            if (!args.Target.IsDestroyed
+                && _targetFilter.IsMatch(args.Target) 
+                && TargetFound != null)
+            {
+                TargetFound.Invoke(this, args);
+            }
+        }
+
+        private void OnTargetLost(object sender, TargetEventArgs args)
+        {
+            if (_targetFilter.IsMatch(args.Target) && TargetLost != null)
+            {
+                TargetLost.Invoke(this, args);
+            }
+        }
+
+        public void Dispose()
+        {
+            _maxRangeDetector.OnEntered -= OnTargetFound;
+            _maxRangeDetector.OnExited -= OnTargetLost;
+
+            _minRangeDetector.OnEntered -= OnTargetLost;
+            _minRangeDetector.OnExited -= OnTargetFound;
+        }
+    }
+}
