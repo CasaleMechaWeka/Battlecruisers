77e2034a57f6cfd519bb387cfbb87be7769feb57
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-07 21:43:47 -0800

    Create facing and anti air angle limiters :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AngleLimiters/AntiAirAngleLimiter.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleCruisers.Buildables.Buildings.Turrets.AngleLimiters
+{
+    public class AntiAirAngleLimiter : AngleLimiter
+    {
+        public AntiAirAngleLimiter()
+            : base(minAngle: 30, maxAngle: 150)
+        {
+        }
+    }
+}
