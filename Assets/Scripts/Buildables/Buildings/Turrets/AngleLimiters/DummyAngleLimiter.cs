2f99390442fc41ffc1e88b8cf10688057c6ded4e
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-07 22:03:03 -0800

    Create IAngleLimiter null object :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AngleLimiters/DummyAngleLimiter.cs
@@ -0,0 +1,13 @@
+﻿namespace BattleCruisers.Buildables.Buildings.Turrets.AngleLimiters
+{
+    /// <summary>
+    /// Null object
+    /// </summary>
+    public class DummyAngleLimiter : IAngleLimiter
+    {
+        public float LimitAngle(float desiredAngleInDegrees)
+        {
+            return desiredAngleInDegrees;
+        }
+    }
+}
