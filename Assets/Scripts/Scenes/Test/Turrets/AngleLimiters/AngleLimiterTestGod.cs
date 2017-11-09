e5885814f98448a771cbdf0cf665a30861852b46
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-08 20:20:08 -0800

    Use custom angle limiter test god, instead of trying to reuse mortar test god :)

---
+++ Assets/Scripts/Scenes/Test/Turrets/AngleLimiters/AngleLimiterTestGod.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Turrets.AntiShip
+{
+    public class AngleLimiterTestGod : MonoBehaviour
+    {
+        public List<Vector2> targetPatrolPoints;
+        public TargetType targetType;
+
+        void Start()
+        {
+            Helper helper = new Helper();
+
+
+            // Setup target
+            TestAircraftController target = FindObjectOfType<TestAircraftController>();
+            target.PatrolPoints = targetPatrolPoints;
+            target.SetTargetType(targetType);
+            helper.InitialiseUnit(target, Faction.Blues);
+            target.StartConstruction();
+
+
+            // Setup turret
+            TurretController turret = FindObjectOfType<TurretController>();
+            helper.InitialiseBuilding(turret, Faction.Reds);
+            turret.StartConstruction();
+        }
+    }
+}
