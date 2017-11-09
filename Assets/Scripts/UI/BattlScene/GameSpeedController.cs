e7a90c52dae538307503b6716d5f42bbe8a1a3cc
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-08 21:41:21 -0800

    Can change game speed :)  0.125 - 8x :D

---
+++ Assets/Scripts/UI/BattlScene/GameSpeedController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.BattleScene
+{
+    // TEMP:  For end game, allow chaging speed from settings OR simply lock game speed :)
+    public class GameSpeedController : MonoBehaviour
+    {
+        private Text _gameSpeedText;
+
+        private const float MAX_GAME_SPEED = 8;
+        private const float MIN_GAME_SPEED = 0.125f;
+        private const float SPEED_CHANGE_FACTOR = 2;
+
+        private float GameSpeed
+        {
+            get { return Time.timeScale; }
+            set
+            {
+                Time.timeScale = value;
+                _gameSpeedText.text = value.ToString();
+            }
+        }
+
+        void Start()
+        {
+            _gameSpeedText = GetComponent<Text>();
+            Assert.IsNotNull(_gameSpeedText);
+        }
+
+        void Update()
+        {
+            // Increase game speed
+            if (Input.GetKeyUp(KeyCode.Equals))
+            {
+                if (GameSpeed < MAX_GAME_SPEED)
+                {
+                    GameSpeed *= SPEED_CHANGE_FACTOR;
+                }
+            }
+            // Reduce game speed
+            else if (Input.GetKeyUp(KeyCode.Minus))
+            {
+                if (GameSpeed > MIN_GAME_SPEED)
+                {
+                    GameSpeed /= SPEED_CHANGE_FACTOR;
+                }
+            }
+        }
+    }
+}
