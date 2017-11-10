b757a186dc3f8aa2551a19c9c213f501aa71302d
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-09 21:26:08 -0800

    Create RecursiveProportionAssigner :)

---
+++ Assets/Scripts/Buildables/Units/Aircraft/SpriteChoosers/RecursiveProportionAssigner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Units.Aircraft.SpriteChoosers
+{
+    /// <summary>
+    /// Given:
+    /// numOfOptions = 3
+    /// baseCutoff = 0.5
+    /// 
+    /// Creates cutoffs:
+    /// 0.5
+    /// 0.25
+    /// 0.125
+    /// 
+    /// Assign(int proportion):
+    /// proportion  index
+    /// > 0.5       0
+    /// > 0.25      1
+    /// rest        2
+    /// </summary>
+    public class RecursiveProportionAssigner : IAssigner
+    {
+        private readonly IList<float> _cutoffs;
+
+		private const int MIN_NUM_OF_OPTIONS = 1;
+        private const float DEFAULT_BASE_CUTOFF = 0.67f;
+        private const int MIN_BASE_CUTOFF = 0;
+        private const int MAX_BASE_CUTOFF = 12;
+
+        public RecursiveProportionAssigner(int numOfOptions, float baseCutoff = DEFAULT_BASE_CUTOFF)
+        {
+            Assert.IsTrue(numOfOptions >= MIN_NUM_OF_OPTIONS);
+            Assert.IsTrue(baseCutoff > MIN_BASE_CUTOFF);
+            Assert.IsTrue(baseCutoff < MAX_BASE_CUTOFF);
+
+            _cutoffs = new List<float>(numOfOptions);
+
+            float cumulativeCutoff = baseCutoff;
+
+            for (int i = 0; i < numOfOptions; ++i)
+            {
+                _cutoffs.Add(cumulativeCutoff);
+                cumulativeCutoff *= cumulativeCutoff;
+            }
+        }
+
+        public int Assign(float proportion)
+        {
+            for (int i = 0; i < _cutoffs.Count; ++i)
+            {
+                float cutoff = _cutoffs[i];
+
+                if (proportion > cutoff)
+                {
+                    return i;
+                }
+            }
+
+            return _cutoffs.Count - 1;
+        }
+    }
+}
