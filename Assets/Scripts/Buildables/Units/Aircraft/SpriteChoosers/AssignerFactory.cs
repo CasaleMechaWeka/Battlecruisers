fe49939618fea429b7b71a2a3eac0474b79b8c6a
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-09 21:43:55 -0800

    Create AssignerFactory :)

---
+++ Assets/Scripts/Buildables/Units/Aircraft/SpriteChoosers/AssignerFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace BattleCruisers.Buildables.Units.Aircraft.SpriteChoosers
+{
+    public class AssignerFactory : IAssignerFactory
+    {
+        public IAssigner CreateRecursiveProportionAssigner(
+            int numOfOptions,
+            float baseCutoff = RecursiveProportionAssigner.DEFAULT_BASE_CUTOFF)
+        {
+            return new RecursiveProportionAssigner(numOfOptions, baseCutoff);
+        }
+    }
+}
---
+++ Assets/Scripts/Buildables/Units/Aircraft/SpriteChoosers/IAssignerFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace BattleCruisers.Buildables.Units.Aircraft.SpriteChoosers
+{
+    public interface IAssignerFactory
+    {
+        IAssigner CreateRecursiveProportionAssigner(
+            int numOfOptions,
+            float baseCutoff = RecursiveProportionAssigner.DEFAULT_BASE_CUTOFF);
+    }
+}
