52e70950b96f1418e556fd69ca438f976891782d
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-25 15:00:31 -0800

    Create parent class

--- Assets/Scripts/Scenes/Test/Balancing/AntiAirBalancingTestGod.cs
+++ Assets/Scripts/Scenes/Test/Balancing/AntiAirBalancingTestGod.cs
@@ -1,72 +1,12 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using BattleCruisers.Data.Models.PrefabKeys;
+﻿using BattleCruisers.Data.Models.PrefabKeys;
 using BattleCruisers.Data.Static;
-using BattleCruisers.Fetchers;
-using BattleCruisers.Utils.DataStrctures;
-using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Balancing
 {
-    // FELIX  Avoid duplicate code with AntiAirBalancingTestGod :)
-    public class AntiAirBalancingTestGod : MonoBehaviour
+    public class AntiAirBalancingTestGod : DefenceBuildingBalancingTestGod
     {
-        private ICircularList<Camera> _cameras;
-
-        public Camera overviewCamera;
-
-        private Camera _currentCamera;
-        private Camera CurrentCamera
-        {
-            set
-            {
-                if (_currentCamera != null)
-                {
-                    _currentCamera.enabled = false;
-                }
-
-                _currentCamera = value;
-
-                if (_currentCamera != null)
-                {
-                    _currentCamera.enabled = true;
-                }
-            }
-        }
-
-        void Start()
-        {
-            IList<Camera> cameras = new List<Camera>()
-            {
-                overviewCamera
-            };
-
-            AntiAirBalancingTest[] tests = FindObjectsOfType<AntiAirBalancingTest>();
-
-            IList<AntiAirBalancingTest> orderedTests =
-                tests
-                    .OrderBy(test => test.gameObject.transform.position.x)
-                    .ToList();
-
-            IPrefabFactory prefabFactory = new PrefabFactory(new PrefabFetcher());
-            IPrefabKey bomberKey = StaticPrefabKeys.Units.Bomber;
-            IPrefabKey antiAirKey = StaticPrefabKeys.Buildings.AntiAirTurret;
-            IPrefabKey samSiteKey = StaticPrefabKeys.Buildings.SamSite;
-
-            foreach (AntiAirBalancingTest test in orderedTests)
-            {
-                test.Initialise(prefabFactory, bomberKey, antiAirKey, samSiteKey);
-                cameras.Add(test.Camera);
-            }
-
-            _cameras = new CircularList<Camera>(cameras);
-
-            ToggleCamera();
-        }
-
-        public void ToggleCamera()
-        {
-            CurrentCamera = _cameras.Next();
-        }
-    }
+        protected override IPrefabKey UnitKey { get { return StaticPrefabKeys.Units.Bomber; } }
+        protected override IPrefabKey BasicDefenceBuildingKey { get { return StaticPrefabKeys.Buildings.AntiAirTurret; } }
+        protected override IPrefabKey AdvancedDefenceBuildingKey { get { return StaticPrefabKeys.Buildings.SamSite; }}
+   }
 }
---
+++ Assets/Scripts/Scenes/Test/Balancing/DefenceBuildingBalancingTestGod.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Fetchers;
+using BattleCruisers.Utils.DataStrctures;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Balancing
+{
+    public abstract class DefenceBuildingBalancingTestGod : MonoBehaviour
+    {
+        private ICircularList<Camera> _cameras;
+
+        public Camera overviewCamera;
+
+        protected abstract IPrefabKey UnitKey { get; }
+        protected abstract IPrefabKey BasicDefenceBuildingKey { get; }
+        protected abstract IPrefabKey AdvancedDefenceBuildingKey { get; }
+
+        private Camera _currentCamera;
+        private Camera CurrentCamera
+        {
+            set
+            {
+                if (_currentCamera != null)
+                {
+                    _currentCamera.enabled = false;
+                }
+
+                _currentCamera = value;
+
+                if (_currentCamera != null)
+                {
+                    _currentCamera.enabled = true;
+                }
+            }
+        }
+
+        void Start()
+        {
+            IList<Camera> cameras = new List<Camera>()
+            {
+                overviewCamera
+            };
+
+            AntiAirBalancingTest[] tests = FindObjectsOfType<AntiAirBalancingTest>();
+
+            IList<AntiAirBalancingTest> orderedTests =
+                tests
+                    .OrderBy(test => test.gameObject.transform.position.x)
+                    .ToList();
+
+            IPrefabFactory prefabFactory = new PrefabFactory(new PrefabFetcher());
+
+            foreach (AntiAirBalancingTest test in orderedTests)
+            {
+                test.Initialise(prefabFactory, UnitKey, BasicDefenceBuildingKey, AdvancedDefenceBuildingKey);
+                cameras.Add(test.Camera);
+            }
+
+            _cameras = new CircularList<Camera>(cameras);
+
+            ToggleCamera();
+        }
+
+        public void ToggleCamera()
+        {
+            CurrentCamera = _cameras.Next();
+        }
+    }
+}
