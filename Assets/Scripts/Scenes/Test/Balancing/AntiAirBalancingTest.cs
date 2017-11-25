97cc64e9bf9832c4f1ba32ded9e66a59760fbb85
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-25 14:46:07 -0800

    Extract non-bomber specific code into parent class :)

--- Assets/Scripts/Scenes/Test/Balancing/AntiAirBalancingTest.cs
+++ Assets/Scripts/Scenes/Test/Balancing/AntiAirBalancingTest.cs
@@ -1,58 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using BattleCruisers.Buildables;
-using BattleCruisers.Buildables.Buildings;
 using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Buildables.Units;
 using BattleCruisers.Buildables.Units.Aircraft.Providers;
-using BattleCruisers.Cruisers;
-using BattleCruisers.Data.Models.PrefabKeys;
-using BattleCruisers.Fetchers;
 using BattleCruisers.Targets;
-using BattleCruisers.Utils;
-using BattleCruisers.Utils.UIWrappers;
 using UnityEngine;
-using UnityEngine.Assertions;
-using TestUtils = BattleCruisers.Scenes.Test.Utilities;
 
 namespace BattleCruisers.Scenes.Test.Balancing
 {
-    public class AntiAirBalancingTest : MonoBehaviour, ITargetConsumer
+    public class AntiAirBalancingTest : DefenceBuildingBalancingTest, ITargetConsumer
     {
-        private TestUtils.Helper _helper;
-        private IPrefabFactory _prefabFactory;
-        private IList<ITarget> _antiAirBuildings;
-        private int _numOfAntiAirBuildings;
-        private IPrefabKey _bomberKey;
-        private IFactory _airFactory;
-        private IList<ITarget> _completedBombers;
-        private ITextMesh _bomberKillCountText, _bomberCostText;
-        private float _bomberCostInDroneS;
-
-        public int numOfBomberDrones;
-        public int numOfAntiAirTurrets;
-        public int numOfSamSites;
-
-        private const int ANTI_AIR_BUILDINGS_OFFSET_IN_M = 15;
-        private const int ANTI_AIR_BUILDINGS_GAP_IN_M = 2;
         private const int BOMBER_CRUISING_ALTITUDE_IN_M = 15;
-        private const string BOMBER_KILL_COUNT_PREFIX = "Dead bombers: ";
-        private const string BOMBER_COST_PREFIX = "Bomber cost (drone seconds): ";
-        private const string ANTI_AIR_BUILDINGS_COST_PREFIX = "Building cost (drone secodns): ";
-
-        public Camera Camera { get; private set; }
-
-        private int _bomberKillCount;
-        private int BomberKillCount
-        {
-            get { return _bomberKillCount; }
-            set
-            {
-                _bomberKillCount = value;
-                _bomberKillCountText.Text = BOMBER_KILL_COUNT_PREFIX + _bomberKillCount;
-                _bomberCostText.Text = BOMBER_COST_PREFIX + (_bomberKillCount * _bomberCostInDroneS);
-            }
-        }
 
         public ITarget Target
         {
@@ -60,134 +18,17 @@ namespace BattleCruisers.Scenes.Test.Balancing
             {
                 if (value == null)
                 {
-                    // Bombers have destroyed all targets :D
-					
-                    // Stop producing bombers
-					_airFactory.UnitWrapper = null;
-
-                    int currentBomberKillCount = BomberKillCount;
-
-                    // Destroy all bombers (because they cannot handle not 
-                    // having targets, means the game is won)
-                    foreach (ITarget target in _completedBombers)
-                    {
-                        if (!target.IsDestroyed)
-                        {
-                            target.Destroy();
-                        }
-                    }
-
-                    // Do NOT count bombers destroyed programmatically at scenario end
-                    // towards the kill count.
-                    BomberKillCount = currentBomberKillCount;
+                    OnAllDefenceBuildingsDestroyed();
                 }
             }
         }
 
-        public void Initialise(IPrefabFactory prefabFactory, IPrefabKey bomberKey, IPrefabKey antiAirKey, IPrefabKey samSiteKey)
-        {
-            Assert.IsTrue(numOfBomberDrones > 0);
-            Assert.IsTrue(numOfAntiAirTurrets >= 0);
-            Assert.IsTrue(numOfSamSites >= 0);
-
-
-            _bomberKey = bomberKey;
-            _numOfAntiAirBuildings = numOfAntiAirTurrets + numOfSamSites;
-            _helper = new TestUtils.Helper(numOfDrones: numOfBomberDrones);
-            _prefabFactory = prefabFactory;
-            _antiAirBuildings = new List<ITarget>(_numOfAntiAirBuildings);
-            _completedBombers = new List<ITarget>();
-            _bomberCostInDroneS = FindUnitCost(_bomberKey);
-
-
-            SetupTexts(antiAirKey, samSiteKey);
-
-
-            // Hide camera
-            Camera = GetComponentInChildren<Camera>();
-            Camera.enabled = false;
-
-
-            // Create anti air buildings
-            int originalOffsetInM = (int)transform.position.x + ANTI_AIR_BUILDINGS_OFFSET_IN_M;
-            int currentOffsetInM = CreateBuildings(antiAirKey, numOfAntiAirTurrets, originalOffsetInM);
-            CreateBuildings(samSiteKey, numOfSamSites, currentOffsetInM);
-        }
-
-        private void SetupTexts(IPrefabKey antiAirKey, IPrefabKey samSiteKey)
-        {
-            // Show test case details
-            TextMesh detailsText = transform.FindNamedComponent<TextMesh>("DetailsText");
-            detailsText.text = "Drones: " + numOfBomberDrones + "  AA: " + numOfAntiAirTurrets + "  SS: " + numOfSamSites;
-
-            // Anti air building cost
-            TextMesh buildingCostText = transform.FindNamedComponent<TextMesh>("BuildingDroneSecSpentText");
-            int buildingCost
-                = FindBuildingCost(antiAirKey, numOfAntiAirTurrets)
-                + FindBuildingCost(samSiteKey, numOfSamSites);
-            buildingCostText.text = ANTI_AIR_BUILDINGS_COST_PREFIX + buildingCost;
-
-            // Bomber cost
-            TextMesh bomberCostText = transform.FindNamedComponent<TextMesh>("BomberDroneSecSpentText");
-            _bomberCostText = new TextMeshWrapper(bomberCostText);
-
-			// Bomber kill count
-			TextMesh bomberKillCountText = transform.FindNamedComponent<TextMesh>("BomberKillCountText");
-            _bomberKillCountText = new TextMeshWrapper(bomberKillCountText);
-			BomberKillCount = 0;
-        }
-
-        private int FindBuildingCost(IPrefabKey buildingKey, int numOfBuildings)
-        {
-            IBuildableWrapper<IBuilding> building = _prefabFactory.GetBuildingWrapperPrefab(buildingKey);
-            return building.Buildable.NumOfDronesRequired * (int)building.Buildable.BuildTimeInS * numOfBuildings;
-        }
-
-        private float FindUnitCost(IPrefabKey unitKey)
-        {
-            IBuildableWrapper<IUnit> unit = _prefabFactory.GetUnitWrapperPrefab(unitKey);
-            return unit.Buildable.NumOfDronesRequired * unit.Buildable.BuildTimeInS;
-        }
-
-        /// <returns>Cumulative building offset.</returns>
-        private int CreateBuildings(IPrefabKey buildingKey, int numOfBuildings, int currentOffsetInM)
-        {
-            for (int i = 0; i < numOfBuildings; ++i)
-            {
-                IBuildableWrapper<IBuilding> buildingWrapper = _prefabFactory.GetBuildingWrapperPrefab(buildingKey);
-                IBuilding building = _prefabFactory.CreateBuilding(buildingWrapper);
-                _helper.InitialiseBuilding(building, Faction.Reds, parentCruiserDirection: Direction.Left);
-
-                building.Position = new Vector2(currentOffsetInM, 0);
-
-                // Mirror building, so it is facing left
-                building.Rotation = Helper.MirrorAccrossYAxis(building.Rotation);
-
-                currentOffsetInM += ANTI_AIR_BUILDINGS_GAP_IN_M;
-                building.CompletedBuildable += Building_CompletedBuildable;
-                building.StartConstruction();
-            }
-
-            return currentOffsetInM;
-        }
-
-        private void Building_CompletedBuildable(object sender, EventArgs e)
-        {
-            ITarget completedBuilding = sender.Parse<ITarget>();
-            _antiAirBuildings.Add(completedBuilding);
-
-            if (_antiAirBuildings.Count == _numOfAntiAirBuildings)
-            {
-                _airFactory = CreateAirFactory();
-            }
-        }
-
-        private IFactory CreateAirFactory()
+        protected override IFactory CreateFactory()
         {
             AirFactory factory = GetComponentInChildren<AirFactory>();
             IList<Vector2> bomberPatrolPoints = GetBomberPatrolPoints(factory.transform.position, BOMBER_CRUISING_ALTITUDE_IN_M);
             IAircraftProvider aircraftProvider = _helper.CreateAircraftProvider(bomberPatrolPoints);
-            ITargetsFactory targetsFactory = _helper.CreateBomberTargetsFactory(_antiAirBuildings);
+            ITargetsFactory targetsFactory = _helper.CreateBomberTargetsFactory(_defenceBuildings);
 
             // So we know when (if) the bombers manage to destroy all targets
             targetsFactory.BomberTargetProcessor.AddTargetConsumer(this);
@@ -201,25 +42,7 @@ namespace BattleCruisers.Scenes.Test.Balancing
                     aircraftProvider: aircraftProvider,
                     targetsFactory: targetsFactory);
 
-            factory.CompletedBuildable += (sender, eventArgs) =>
-            {
-                factory.UnitWrapper = _prefabFactory.GetUnitWrapperPrefab(_bomberKey);
-                factory.CompletedBuildingUnit += Factory_CompletedBomber;
-            };
-            factory.StartConstruction();
-
             return factory;
-        }
-
-        private void Factory_CompletedBomber(object sender, CompletedConstructionEventArgs e)
-        {
-            _completedBombers.Add(e.Buildable);
-            e.Buildable.Destroyed += Bomber_Destroyed;
-        }
-
-        private void Bomber_Destroyed(object sender, DestroyedEventArgs e)
-        {
-            BomberKillCount++;
         }
 
         private IList<Vector2> GetBomberPatrolPoints(Vector2 factoryPosition, float bomberCruisingAltitudeInM)
@@ -227,7 +50,7 @@ namespace BattleCruisers.Scenes.Test.Balancing
             return new List<Vector2>()
             {
                 new Vector2(factoryPosition.x, bomberCruisingAltitudeInM),
-                new Vector2(ANTI_AIR_BUILDINGS_OFFSET_IN_M, bomberCruisingAltitudeInM)
+                new Vector2(DEFENCE_BUILDINGS_OFFSET_IN_M, bomberCruisingAltitudeInM)
             };
         }    
     }
---
+++ Assets/Scripts/Scenes/Test/Balancing/DefenceBuildingBalancingTest.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Fetchers;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.UIWrappers;
+using UnityEngine;
+using UnityEngine.Assertions;
+using TestUtils = BattleCruisers.Scenes.Test.Utilities;
+
+namespace BattleCruisers.Scenes.Test.Balancing
+{
+    public abstract class DefenceBuildingBalancingTest : MonoBehaviour
+    {
+        private int _numOfDefenceBuildings;
+        private IPrefabKey _offensiveUnitKey;
+        private IFactory _offensiveFactory;
+        private IList<ITarget> _completedOffensiveUnits;
+        private ITextMesh _unitKillCountText, _totalUnitsCostText;
+        private float _unitCostInDroneS;
+		
+        protected TestUtils.Helper _helper;
+		protected IPrefabFactory _prefabFactory;
+		protected IList<ITarget> _defenceBuildings;
+
+        public int numOfUnitDrones;
+        public int numOfBasicDefenceBuildings;
+        public int numOfAdvancedDefenceBuildings;
+
+        protected const int DEFENCE_BUILDINGS_OFFSET_IN_M = 15;
+        private const int DEFENCE_BUILDINGS_GAP_IN_M = 2;
+
+        private const string UNIT_KILL_COUNT_PREFIX = "Dead units: ";
+        private const string UNIT_COST_PREFIX = "Unit cost (drone seconds): ";
+        private const string DEFENCE_BUILDINGS_COST_PREFIX = "Building cost (drone secodns): ";
+		
+        public Camera Camera { get; private set; }
+
+        private int _unitKillCount;
+        private int UnitKillCount
+        {
+            get { return _unitKillCount; }
+            set
+            {
+                _unitKillCount = value;
+                _unitKillCountText.Text = UNIT_KILL_COUNT_PREFIX + _unitKillCount;
+                _totalUnitsCostText.Text = UNIT_COST_PREFIX + (_unitKillCount * _unitCostInDroneS);
+            }
+        }
+
+        public void Initialise(IPrefabFactory prefabFactory, IPrefabKey unitKey, IPrefabKey basicDefenceBuildingKey, IPrefabKey advancedDefenceBuildingKey)
+        {
+            Helper.AssertIsNotNull(prefabFactory, unitKey, basicDefenceBuildingKey, advancedDefenceBuildingKey);
+            Assert.IsTrue(numOfUnitDrones > 0);
+            Assert.IsTrue(numOfBasicDefenceBuildings >= 0);
+            Assert.IsTrue(numOfAdvancedDefenceBuildings >= 0);
+
+
+            _offensiveUnitKey = unitKey;
+            _numOfDefenceBuildings = numOfBasicDefenceBuildings + numOfAdvancedDefenceBuildings;
+            _helper = new TestUtils.Helper(numOfDrones: numOfUnitDrones);
+            _prefabFactory = prefabFactory;
+            _defenceBuildings = new List<ITarget>(_numOfDefenceBuildings);
+            _completedOffensiveUnits = new List<ITarget>();
+            _unitCostInDroneS = FindUnitCost(_offensiveUnitKey);
+
+
+            SetupTexts(basicDefenceBuildingKey, advancedDefenceBuildingKey);
+
+
+            // Hide camera
+            Camera = GetComponentInChildren<Camera>();
+            Camera.enabled = false;
+
+
+            // Create defence buildings
+            int originalOffsetInM = (int)transform.position.x + DEFENCE_BUILDINGS_OFFSET_IN_M;
+            int currentOffsetInM = CreateBuildings(basicDefenceBuildingKey, numOfBasicDefenceBuildings, originalOffsetInM);
+            CreateBuildings(advancedDefenceBuildingKey, numOfAdvancedDefenceBuildings, currentOffsetInM);
+        }
+
+        private void SetupTexts(IPrefabKey antiAirKey, IPrefabKey samSiteKey)
+        {
+            // Show test case details
+            TextMesh detailsText = transform.FindNamedComponent<TextMesh>("DetailsText");
+            detailsText.text = "Drones: " + numOfUnitDrones + "  AA: " + numOfBasicDefenceBuildings + "  SS: " + numOfAdvancedDefenceBuildings;
+
+            // Anti air building cost
+            TextMesh buildingCostText = transform.FindNamedComponent<TextMesh>("BuildingDroneSecSpentText");
+            int buildingCost
+                = FindBuildingCost(antiAirKey, numOfBasicDefenceBuildings)
+                + FindBuildingCost(samSiteKey, numOfAdvancedDefenceBuildings);
+            buildingCostText.text = DEFENCE_BUILDINGS_COST_PREFIX + buildingCost;
+
+            // Bomber cost
+            // FELIX  Rename UI element
+            TextMesh bomberCostText = transform.FindNamedComponent<TextMesh>("BomberDroneSecSpentText");
+            _totalUnitsCostText = new TextMeshWrapper(bomberCostText);
+
+			// Bomber kill count
+            // FELIX  Rename UI element
+			TextMesh bomberKillCountText = transform.FindNamedComponent<TextMesh>("BomberKillCountText");
+            _unitKillCountText = new TextMeshWrapper(bomberKillCountText);
+			UnitKillCount = 0;
+        }
+
+        private int FindBuildingCost(IPrefabKey buildingKey, int numOfBuildings)
+        {
+            IBuildableWrapper<IBuilding> building = _prefabFactory.GetBuildingWrapperPrefab(buildingKey);
+            return building.Buildable.NumOfDronesRequired * (int)building.Buildable.BuildTimeInS * numOfBuildings;
+        }
+
+        private float FindUnitCost(IPrefabKey unitKey)
+        {
+            IBuildableWrapper<IUnit> unit = _prefabFactory.GetUnitWrapperPrefab(unitKey);
+            return unit.Buildable.NumOfDronesRequired * unit.Buildable.BuildTimeInS;
+        }
+
+        /// <returns>Cumulative building offset.</returns>
+        private int CreateBuildings(IPrefabKey buildingKey, int numOfBuildings, int currentOffsetInM)
+        {
+            for (int i = 0; i < numOfBuildings; ++i)
+            {
+                IBuildableWrapper<IBuilding> buildingWrapper = _prefabFactory.GetBuildingWrapperPrefab(buildingKey);
+                IBuilding building = _prefabFactory.CreateBuilding(buildingWrapper);
+                _helper.InitialiseBuilding(building, Faction.Reds, parentCruiserDirection: Direction.Left);
+
+                building.Position = new Vector2(currentOffsetInM, 0);
+
+                // Mirror building, so it is facing left
+                building.Rotation = Helper.MirrorAccrossYAxis(building.Rotation);
+
+                currentOffsetInM += DEFENCE_BUILDINGS_GAP_IN_M;
+                building.CompletedBuildable += Building_CompletedBuildable;
+                building.StartConstruction();
+            }
+
+            return currentOffsetInM;
+        }
+
+        private void Building_CompletedBuildable(object sender, EventArgs e)
+        {
+            ITarget completedBuilding = sender.Parse<ITarget>();
+            _defenceBuildings.Add(completedBuilding);
+
+            if (_defenceBuildings.Count == _numOfDefenceBuildings)
+            {
+                _offensiveFactory = CreateFactory();
+
+                _offensiveFactory.CompletedBuildable += (factory, eventArgs) =>
+                {
+                    _offensiveFactory.UnitWrapper = _prefabFactory.GetUnitWrapperPrefab(_offensiveUnitKey);
+                    _offensiveFactory.CompletedBuildingUnit += Factory_CompletedUnit;
+                };
+                _offensiveFactory.StartConstruction();
+            }
+        }
+
+        protected abstract IFactory CreateFactory();
+
+        private void Factory_CompletedUnit(object sender, CompletedConstructionEventArgs e)
+        {
+            _completedOffensiveUnits.Add(e.Buildable);
+            e.Buildable.Destroyed += Unit_Destroyed;
+        }
+
+        private void Unit_Destroyed(object sender, DestroyedEventArgs e)
+        {
+            UnitKillCount++;
+        }
+
+        protected void OnAllDefenceBuildingsDestroyed()
+        {
+            // Stop producing units
+            _offensiveFactory.UnitWrapper = null;
+
+            int currentUnitKillCount = UnitKillCount;
+
+            // Destroy all units (because behaviour is undefined they have no more
+            // targets, means the game is won).
+            foreach (ITarget target in _completedOffensiveUnits)
+            {
+                if (!target.IsDestroyed)
+                {
+                    target.Destroy();
+                }
+            }
+
+            // Do NOT count units destroyed programmatically at scenario end
+            // towards the kill count.
+            UnitKillCount = currentUnitKillCount;
+        }
+    }
+}
