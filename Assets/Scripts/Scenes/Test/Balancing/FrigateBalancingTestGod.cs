9d5688434275fe33ab0db6a18d2c6505dfce1dcc
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-25 16:43:53 -0800

    Created mortar v frigate balancing test scene :)

---
+++ Assets/Scripts/Scenes/Test/Balancing/FrigateBalancingTestGod.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Data.Static;
+
+namespace BattleCruisers.Scenes.Test.Balancing
+{
+    public class FrigateBalancingTestGod : AntiSeaBalancingTestGod
+    {
+        protected override IPrefabKey UnitKey { get { return StaticPrefabKeys.Units.Frigate; } }
+    }
+}
