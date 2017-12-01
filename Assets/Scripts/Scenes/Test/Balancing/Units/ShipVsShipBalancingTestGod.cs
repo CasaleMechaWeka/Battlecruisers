0983f4cd6a4e5a5f67d50ee28345f6abc394f621
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-30 21:47:03 -0800

    Refactored ShipVsShipBalancingTestGod to allow lots of different boat vs boat combinations :)

--- Assets/Scripts/Scenes/Test/Balancing/Units/ShipVsShipBalancingTestGod.cs
+++ Assets/Scripts/Scenes/Test/Balancing/Units/ShipVsShipBalancingTestGod.cs
@@ -1,23 +1,37 @@
-﻿using BattleCruisers.Data.Models.PrefabKeys;
+﻿using System.Collections.Generic;
+using BattleCruisers.Data.Static;
 using BattleCruisers.Fetchers;
+using BattleCruisers.Utils.DataStrctures;
 
 namespace BattleCruisers.Scenes.Test.Balancing.Units
 {
-    public abstract class ShipVsShipBalancingTestGod : MultiCameraTestGod<ShipVsShipBalancingTest>
+    public class ShipVsShipBalancingTestGod : MultiCameraTestGod<ShipVsShipBalancingTest>
     {
         private IPrefabFactory _prefabFactory;
-
-        protected abstract IPrefabKey LeftShipKey { get; }
-        protected abstract IPrefabKey RightShipKey { get; }
+        private ICircularList<IPrefabKeyPair> _scenarioPrefabKeys;
 
         protected override void Initialise()
         {
             _prefabFactory = new PrefabFactory(new PrefabFetcher());
+
+            // Have 4 boat units.  Want to see how each fairs against the others.
+            IList<IPrefabKeyPair> scenarioPrefabKeys = new List<IPrefabKeyPair>()
+            {
+                new PrefabKeyPair(StaticPrefabKeys.Units.AttackBoat, StaticPrefabKeys.Units.Frigate),
+                new PrefabKeyPair(StaticPrefabKeys.Units.AttackBoat, StaticPrefabKeys.Units.Destroyer),
+                new PrefabKeyPair(StaticPrefabKeys.Units.AttackBoat, StaticPrefabKeys.Units.ArchonBattleship),
+                new PrefabKeyPair(StaticPrefabKeys.Units.Frigate, StaticPrefabKeys.Units.Destroyer),
+                new PrefabKeyPair(StaticPrefabKeys.Units.Frigate, StaticPrefabKeys.Units.ArchonBattleship),
+                new PrefabKeyPair(StaticPrefabKeys.Units.Destroyer, StaticPrefabKeys.Units.ArchonBattleship),
+            };
+
+            _scenarioPrefabKeys = new CircularList<IPrefabKeyPair>(scenarioPrefabKeys);
         }
 
         protected override void InitialiseScenario(ShipVsShipBalancingTest scenario)
         {
-            scenario.Initialise(_prefabFactory, LeftShipKey, RightShipKey);
+            IPrefabKeyPair keyPair = _scenarioPrefabKeys.Next();
+            scenario.Initialise(_prefabFactory, keyPair.LeftKey, keyPair.RightKey);
         }
     }
 }
