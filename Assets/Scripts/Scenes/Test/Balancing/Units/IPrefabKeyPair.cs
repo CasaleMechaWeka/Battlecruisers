026c6833df3d318f7cc8118bf6f74122b466f9c1
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-11-30 21:38:15 -0800

    Create IPrefabKeyPair and implementation :)

---
+++ Assets/Scripts/Scenes/Test/Balancing/Units/IPrefabKeyPair.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.Scenes.Test.Balancing.Units
+{
+    public interface IPrefabKeyPair
+    {
+        IPrefabKey LeftKey { get; }
+        IPrefabKey RightKey { get; }
+    }
+}
---
+++ Assets/Scripts/Scenes/Test/Balancing/Units/PrefabKeyPair.cs
@@ -0,0 +1,16 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.Scenes.Test.Balancing.Units
+{
+    public class PrefabKeyPair : IPrefabKeyPair
+    {
+        public IPrefabKey LeftKey { get; private set; }
+        public IPrefabKey RightKey { get; private set; }
+
+        public PrefabKeyPair(IPrefabKey leftKey, IPrefabKey rightKey)
+        {
+            LeftKey = leftKey;
+            RightKey = rightKey;
+        }
+    }
+}
