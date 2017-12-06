20ed73243d8bd90cea3cd9448fb87e016c8da954
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-06 12:47:00 -0800

    Create gunship vs frigate, destroyer and archon scenarios :)

---
+++ Assets/Scripts/Scenes/Test/Balancing/Units/Scenarios/ArchonVsGunships.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Data.Static;
+
+namespace BattleCruisers.Scenes.Test.Balancing.Units
+{
+    public class ArchonVsGunships : ShipVsGunshipBalancingTest
+    {
+        protected override IPrefabKey ShipKey { get { return StaticPrefabKeys.Units.ArchonBattleship; } }
+    }
+}
