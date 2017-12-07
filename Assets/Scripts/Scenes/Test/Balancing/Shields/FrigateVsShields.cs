4998f41caa45b650fc3efd09d40c110860b95c9f
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-07 11:00:31 -0800

    Add scenarios for shields vs frigate, destroyer and archon :)

---
+++ Assets/Scripts/Scenes/Test/Balancing/Shields/FrigateVsShields.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Data.Static;
+
+namespace BattleCruisers.Scenes.Test.Balancing.Shields
+{
+    public class FrigateVsShields : ShipsVsShieldsBalancingTest
+    {
+        protected override IPrefabKey ShipKey { get { return StaticPrefabKeys.Units.Frigate; } }
+    }
+}
