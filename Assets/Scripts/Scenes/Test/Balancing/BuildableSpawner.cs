14df6c12f0f11009a7df4e99caaabd4b6fb477e0
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-07 10:08:31 -0800

    Move common code from BuildingSpawner to BuildableSpawner :)

--- Assets/Scripts/Scenes/Test/Balancing/BuildableSpawner.cs
+++ Assets/Scripts/Scenes/Test/Balancing/BuildableSpawner.cs
@@ -1,18 +1,61 @@
-﻿// FELIX
-
-using System.Collections;
-using System.Collections.Generic;
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Fetchers;
+using BattleCruisers.Utils;
 using UnityEngine;
+using UnityEngine.Assertions;
+using TestUtils = BattleCruisers.Scenes.Test.Utilities;
 
-public class BuildableSpawner : MonoBehaviour {
+namespace BattleCruisers.Scenes.Test.Balancing
+{
+    public abstract class BuildableSpawner : IBuildableSpawner
+    {
+        protected readonly IPrefabFactory _prefabFactory;
+        protected readonly TestUtils.Helper _helper;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private const float SPACING_MULTIPLIER = 1.2f;
+
+        public BuildableSpawner(IPrefabFactory prefabFactory, TestUtils.Helper helper)
+        {
+            Helper.AssertIsNotNull(prefabFactory, helper);
+
+            _prefabFactory = prefabFactory;
+            _helper = helper;
+        }
+
+        // FELIX  Take on building completed/started callback :)
+        public void SpawnBuildables(IPrefabKey buildableKey, int numOfBuildables, Direction facingDirection, Vector2 spawnPosition)
+        {
+            Assert.IsTrue(facingDirection == Direction.Left || facingDirection == Direction.Right);
+
+            for (int i = 0; i < numOfBuildables; ++i)
+            {
+                IBuildableWrapper<IBuilding> buildingWrapper = _prefabFactory.GetBuildingWrapperPrefab(buildableKey);
+                IBuilding building = _prefabFactory.CreateBuilding(buildingWrapper);
+                _helper.InitialiseBuilding(building, Faction.Reds, parentCruiserDirection: Direction.Left);
+
+                building.Position = spawnPosition;
+                spawnPosition = IncrementSpawnPosition(spawnPosition, building, facingDirection);
+
+                // Mirror building
+                if (facingDirection == Direction.Left)
+                {
+                    building.Rotation = Helper.MirrorAccrossYAxis(building.Rotation);
+                }
+
+                building.StartConstruction();
+            }
+        }
+
+        protected abstract IBuildable SpawnBuildable(IPrefabKey buildableKey, Direction facingDirection);
+
+        private Vector2 IncrementSpawnPosition(Vector2 currentPosition, IBuildable buildable, Direction facingDirection)
+        {
+            float incrementInM = buildable.Size.x * SPACING_MULTIPLIER;
+            float directionMultiplier = facingDirection == Direction.Right ? -1 : 1;
+            return new Vector2(currentPosition.x + directionMultiplier * incrementInM, currentPosition.y);
+        }
+    }
 }
--- Assets/Scripts/Scenes/Test/Balancing/BuildingSpawner.cs
+++ Assets/Scripts/Scenes/Test/Balancing/BuildingSpawner.cs
@@ -3,59 +3,24 @@ using BattleCruisers.Buildables.Buildings;
 using BattleCruisers.Buildables.Units;
 using BattleCruisers.Data.Models.PrefabKeys;
 using BattleCruisers.Fetchers;
-using BattleCruisers.Utils;
-using UnityEngine;
-using UnityEngine.Assertions;
 using TestUtils = BattleCruisers.Scenes.Test.Utilities;
 
 namespace BattleCruisers.Scenes.Test.Balancing
 {
-    // FELIX  Avoid duplicate code with UnitSpawner
     // FELIX  Use in DefenceBuildingBalancingTest :)
-    public class BuildingSpawner : IBuildableSpawner
+    public class BuildingSpawner : BuildableSpawner
     {
-        private readonly IPrefabFactory _prefabFactory;
-        private readonly TestUtils.Helper _helper;
-
-        private const float SPACING_MULTIPLIER = 1.2f;
-
-        public BuildingSpawner(IPrefabFactory prefabFactory, TestUtils.Helper helper)
+        public BuildingSpawner(IPrefabFactory prefabFactory, TestUtils.Helper helper) 
+            : base(prefabFactory, helper)
         {
-            Helper.AssertIsNotNull(prefabFactory, helper);
-
-            _prefabFactory = prefabFactory;
-            _helper = helper;
         }
 
-        // FELIX  Take on building completed/started callback :)
-        public void SpawnBuildables(IPrefabKey buildableKey, int numOfBuildables, Direction facingDirection, Vector2 spawnPosition)
+        protected override IBuildable SpawnBuildable(IPrefabKey buildableKey, Direction facingDirection)
         {
-            Assert.IsTrue(facingDirection == Direction.Left || facingDirection == Direction.Right);
-
-            for (int i = 0; i < numOfBuildables; ++i)
-            {
-                IBuildableWrapper<IBuilding> buildingWrapper = _prefabFactory.GetBuildingWrapperPrefab(buildableKey);
-                IBuilding building = _prefabFactory.CreateBuilding(buildingWrapper);
-                _helper.InitialiseBuilding(building, Faction.Reds, parentCruiserDirection: Direction.Left);
-
-                building.Position = spawnPosition;
-                spawnPosition = IncrementSpawnPosition(spawnPosition, building, facingDirection);
-
-                // Mirror building
-                if (facingDirection == Direction.Left)
-                {
-                    building.Rotation = Helper.MirrorAccrossYAxis(building.Rotation);
-				}
-
-                building.StartConstruction();
-            }
-        }
-
-        private Vector2 IncrementSpawnPosition(Vector2 currentPosition, IBuildable buildable, Direction facingDirection)
-        {
-            float incrementInM = buildable.Size.x * SPACING_MULTIPLIER;
-            float directionMultiplier = facingDirection == Direction.Right ? -1 : 1;
-            return new Vector2(currentPosition.x + directionMultiplier * incrementInM, currentPosition.y);
+            IBuildableWrapper<IBuilding> buildingWrapper = _prefabFactory.GetBuildingWrapperPrefab(buildableKey);
+            IBuilding building = _prefabFactory.CreateBuilding(buildingWrapper);
+            _helper.InitialiseBuilding(building, Faction.Reds, parentCruiserDirection: facingDirection);
+            return building;
         }
     }
 }
