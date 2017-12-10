ef28f80e3cd55d8534efea8d00ed4a4728ffb879
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-10 12:11:05 -0800

    Make sure destroyer stops just out of mortar range, so that 2 destroyers can attack a mortar at the same time :)  Creat DestroyerOutOfMortarRange test scene to check this.

--- Assets/Scripts/Buildables/Units/Ships/DestroyerController.cs
+++ Assets/Scripts/Buildables/Units/Ships/DestroyerController.cs
@@ -8,18 +8,26 @@ namespace BattleCruisers.Buildables.Units.Ships
 	{
         private IBarrelWrapper _directFireAntiSea, _mortar, _directFireAntiAir, _samSite, _missileLauncher;
 
-        protected override float EnemyDetectionRangeInM 
-        { 
-            get 
-            {
-                return FindEnemyDetectionRange(_missileLauncher);
-            } 
-        }
+        private float _enemyDectionRangeInM;
+        protected override float EnemyDetectionRangeInM { get { return _enemyDectionRangeInM; } }
 
         public override void StaticInitialise()
         {
             base.StaticInitialise();
+
             _attackCapabilities.Add(TargetType.Aircraft);
+            _enemyDectionRangeInM = FindEnemyDetectionRange();
+        }
+
+        /// <summary>
+        /// Want to:
+        /// + Stay out of range of mortars
+        /// + But go close enough for more than one destroyer to attack
+        /// </summary>
+        private float FindEnemyDetectionRange()
+        {
+            // This is the range at which an enemy mortar will be able to attack :)
+            return _mortar.RangeInM + Size.x / 2;
         }
 
         protected override IList<IBarrelWrapper> GetTurrets()
---
+++ Assets/Scripts/Scenes/Test/Balancing/Range/DestroyerOutOfMortarRangeTestGod.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Balancing.Range
+{
+    public class DestroyerOutOfMortarRangeTestGod : MonoBehaviour
+    {
+        void Start()
+        {
+            Helper helper = new Helper();
+
+            // Initialise mortar
+            IBuilding mortar = FindObjectOfType<TurretController>();
+            helper.InitialiseBuilding(mortar, Faction.Reds, parentCruiserDirection: Direction.Left);
+            mortar.StartConstruction();
+
+            // Initialise destroyers
+            IUnit[] destroyers = FindObjectsOfType<ShipController>();
+            foreach (IUnit destroyer in destroyers)
+            {
+                helper.InitialiseUnit(destroyer, Faction.Blues, parentCruiserDirection: Direction.Right);
+                destroyer.StartConstruction();
+            }
+        }
+    }
+}
