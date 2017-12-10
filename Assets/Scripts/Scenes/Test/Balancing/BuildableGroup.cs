72ce02aa3bb7ced935a7d5225083db0e95ca3c51
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-10 14:10:57 -0800

    Create IBuildableGroup and abstract BuildableGroup :)

---
+++ Assets/Scripts/Scenes/Test/Balancing/BuildableGroup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BattleCruisers.Buildables;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Utils;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.Balancing
+{
+    public abstract class BuildableGroup : IBuildableGroup
+    {
+        private readonly IList<IBuildable> _aliveBuildables;
+
+        public IPrefabKey BuildableKey { get; private set; }
+        public int NumOfBuildables { get; private set; }
+
+        public event EventHandler BuildablesDestroyed;
+
+        protected BuildableGroup(IPrefabKey buildableKey, int numOfBuildables)
+        {
+            Assert.IsNotNull(buildableKey);
+            Assert.IsTrue(numOfBuildables > 0);
+
+            BuildableKey = buildableKey;
+            NumOfBuildables = numOfBuildables;
+
+            _aliveBuildables = CreateBuildables(BuildableKey, NumOfBuildables);
+
+            foreach (IBuildable buildable in _aliveBuildables)
+            {
+                buildable.Destroyed += Buildable_Destroyed;
+            }
+        }
+
+        protected abstract IList<IBuildable> CreateBuildables(IPrefabKey buildableKey, int numOfBuildables);
+
+        private void Buildable_Destroyed(object sender, DestroyedEventArgs e)
+        {
+            IBuildable destroyedBuildable = e.DestroyedTarget.Parse<IBuildable>();
+
+            Assert.IsTrue(_aliveBuildables.Contains(destroyedBuildable));
+            _aliveBuildables.Remove(destroyedBuildable);
+
+            if (_aliveBuildables.Count == 0 && BuildablesDestroyed != null)
+            {
+                BuildablesDestroyed.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void DestroyAllBuildables()
+        {
+            IList<IBuildable> aliveBuildablesCopy = _aliveBuildables.ToList();
+
+            foreach (IBuildable buildable in aliveBuildablesCopy)
+            {
+                buildable.Destroy();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/Test/Balancing/IBuildableGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.Scenes.Test.Balancing
+{
+    public interface IBuildableGroup
+    {
+        IPrefabKey BuildableKey { get; }
+        int NumOfBuildables { get; }
+
+        event EventHandler BuildablesDestroyed;
+
+        void DestroyAllBuildables();
+    }
+}
