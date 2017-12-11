bbe47ae213caba28bb93981f73a0297831e52055
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-10 16:23:50 -0800

    Create Building-/Unit- GroupControllers :D

---
+++ Assets/Scripts/Scenes/Test/Balancing/BuildingGroupController.cs
@@ -0,0 +1,26 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Fetchers;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Balancing
+{
+    public class BuildingGroupController : MonoBehaviour 
+    {
+        public PrefabKeyName prefabKeyName;
+        public int numOfBuildables;
+
+        public IBuildableGroup Initialise(
+            IPrefabFactory prefabFactory,
+            Helper helper,
+            Faction faction,
+            Direction facingDirection,
+            Vector2 spawnPosition)
+        {
+            IPrefabKey buildableKey = StaticPrefabKeyHelper.GetPrefabKey(prefabKeyName);
+            return new BuildingGroup(buildableKey, numOfBuildables, prefabFactory, helper, faction, facingDirection, spawnPosition);
+        }
+    }
+}
