fe4111aba644661116ca25318d3653ac9812f7f1
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-10 16:52:45 -0800

    Use generic BuildableVsBuildable classes in offensive vs shields test scene :)

---
+++ Assets/Scripts/Scenes/Test/Balancing/BuildableVsBuildableTestGod.cs
@@ -0,0 +1,22 @@
+﻿using BattleCruisers.Fetchers;
+using TestUtils = BattleCruisers.Scenes.Test.Utilities;
+
+namespace BattleCruisers.Scenes.Test.Balancing
+{
+    public class BuildableVsBuildableTestGod : MultiCameraTestGod<BuildableVsBuildableTest>
+    {
+        private IPrefabFactory _prefabFactory;
+        private TestUtils.Helper _helper;
+
+        protected override void Initialise()
+        {
+            _prefabFactory = new PrefabFactory(new PrefabFetcher());
+            _helper = new TestUtils.Helper();
+        }
+
+        protected override void InitialiseScenario(BuildableVsBuildableTest scenario)
+        {
+            scenario.Initialise(_prefabFactory, _helper);
+        }
+    }
+}
