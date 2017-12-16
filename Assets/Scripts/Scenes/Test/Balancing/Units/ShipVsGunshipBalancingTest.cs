a94e59a3249b089fb60750791442dcfaae8a2766
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-15 16:01:58 -0800

    Remove TODO :P

--- Assets/Scripts/Scenes/Test/Balancing/Units/ShipVsGunshipBalancingTest.cs
+++ Assets/Scripts/Scenes/Test/Balancing/Units/ShipVsGunshipBalancingTest.cs
@@ -14,7 +14,6 @@ using TestUtils = BattleCruisers.Scenes.Test.Utilities;
 
 namespace BattleCruisers.Scenes.Test.Balancing.Units
 {
-    // FELIX  Avoid duplicate code wioth FactoryVsFactoryTest?
     public class ShipVsGunshipBalancingTest : MonoBehaviour, ITestScenario
     {
         private IFactory _navalFactory, _airFactory;
