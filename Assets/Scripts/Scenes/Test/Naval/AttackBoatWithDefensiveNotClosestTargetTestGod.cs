4e1680bf35e6c6a5b28e7bf53f2fcb77b7b04252
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-17 15:03:48 -0800

    Create test scene highlighting issue where attack boat will happily attack naval factory, even if anti ship turret is pounding it :)

---
+++ Assets/Scripts/Scenes/Test/Naval/AttackBoatWithDefensiveNotClosestTargetTestGod.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Naval
+{
+    // FELIX  Avoid duplicate code with:  Frigate vs Defensive not close enough, & then AB vs Elevated target
+    public class AttackBoatWithDefensiveNotClosestTargetTestGod : MonoBehaviour 
+	{
+		void Start()
+		{
+			Helper helper = new Helper();
+
+            TurretController turret = FindObjectOfType<TurretController>();
+            helper.InitialiseBuilding(turret, Faction.Reds);
+            turret.StartConstruction();
+
+            IBuilding blockingCruiserImitiation = FindObjectOfType<NavalFactory>();
+            helper.InitialiseBuilding(blockingCruiserImitiation, Faction.Reds);
+            blockingCruiserImitiation.StartConstruction();
+
+            AttackBoatController boat = FindObjectOfType<AttackBoatController>();
+            helper.InitialiseUnit(boat, Faction.Blues);
+			boat.StartConstruction();
+		}
+	}
+}
