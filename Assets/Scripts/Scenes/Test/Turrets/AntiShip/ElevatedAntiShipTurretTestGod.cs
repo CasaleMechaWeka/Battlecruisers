07b658d1ca3454b74d8829592c0565a7a7f95fd0
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-17 13:21:34 -0800

    Create test scene highlighting problem that attack boat can usually not attack anti ship turret, as the cruiser soaks up the damage :/

---
+++ Assets/Scripts/Scenes/Test/Turrets/AntiShip/ElevatedAntiShipTurretTestGod.cs
@@ -0,0 +1,32 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Targets;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Turrets.AntiShip
+{
+    public class ElevatedAntiShipTurretTestGod : MonoBehaviour 
+	{
+		void Start()
+		{
+			Helper helper = new Helper();
+
+            TurretController turret = FindObjectOfType<TurretController>();
+            helper.InitialiseBuilding(turret, Faction.Reds);
+            turret.StartConstruction();
+
+            IBuilding blockingCruiserImitiation = FindObjectOfType<NavalFactory>();
+            helper.InitialiseBuilding(blockingCruiserImitiation, Faction.Reds);
+            blockingCruiserImitiation.StartConstruction();
+
+            AttackBoatController boat = FindObjectOfType<AttackBoatController>();
+			ITargetsFactory targetsFactory = helper.CreateTargetsFactory(globalTarget: turret.GameObject);
+            helper.InitialiseUnit(boat, Faction.Blues, targetsFactory: targetsFactory);
+			boat.StartConstruction();
+		}
+	}
+}
