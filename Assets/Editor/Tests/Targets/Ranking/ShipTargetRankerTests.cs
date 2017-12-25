b14dbb9715b80e4e542679e0e5a9d23a4f1979b5
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-24 19:31:19 -0800

    Create ShipTargetRanker and tests :)

---
+++ Assets/Editor/Tests/Targets/Ranking/ShipTargetRankerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Buildables;
+using BattleCruisers.Targets.TargetProcessors.Ranking;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.Targets.Ranking
+{
+    /// <summary>
+    /// Note:  Targets are ranked in ascending priority.
+    /// </summary>
+    public class ShipTargetRankerTests : BaseTargetRankerTests
+	{
+		[SetUp]
+		public override void SetuUp()
+		{
+			_targetRanker = new ShipTargetRanker();
+		}
+
+		[Test]
+		public void LowValueAntiShip_TrumpsHighValue()
+		{
+            ITarget lowValueAntiShip = CreateMockTarget(TargetValue.Low, TargetType.Ships);
+			ITarget highValue = CreateMockTarget(TargetValue.High);
+
+			_rankedTargets = new List<ITarget>(new ITarget[] { lowValueAntiShip, highValue });
+			RankTargets();
+
+			_expectedOrder = new List<ITarget>(new ITarget[] { highValue, lowValueAntiShip });
+
+			Assert.AreEqual(_expectedOrder, _rankedTargets);
+		}
+	}
+}
---
+++ Assets/Scripts/Targets/TargetProcessors/Ranking/ShipTargetRanker.cs
@@ -0,0 +1,19 @@
+﻿using BattleCruisers.Buildables;
+
+namespace BattleCruisers.Targets.TargetProcessors.Ranking
+{
+    /// <summary>
+    /// Prioritises targets by:
+    /// 1. Anti sea
+    /// 2. By target value
+    /// </summary>
+    public class ShipTargetRanker : BaseTargetRanker
+    {
+        private const int ANTI_SHIP_BONUS = 30;
+
+        public ShipTargetRanker()
+        {
+            _attackCapabilityToBonus[TargetType.Ships] = ANTI_SHIP_BONUS;
+        }
+    }
+}
