b0e05ce5d0dfe13442026818b8d884dbe68e3bda
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-25 17:39:30 -0800

    Create ITargetProcessorArgs and implementation :)

---
+++ Assets/Scripts/Targets/TargetProcessors/ITargetProcessorArgs.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Buildables;
+
+namespace BattleCruisers.Targets.TargetProcessors
+{
+    public interface ITargetProcessorArgs
+    {
+        ITargetsFactory TargetsFactory { get; }
+        ITargetConsumer TargetConsumer { get; }
+        Faction EnemyFaction { get; }
+        IList<TargetType> AttackCapabilities { get; }
+        float MaxRangeInM { get; }
+        float MinRangeInM { get; }
+    }
+}
---
+++ Assets/Scripts/Targets/TargetProcessors/TargetProcessorArgs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Buildables;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Targets.TargetProcessors
+{
+    public class TargetProcessorArgs : ITargetProcessorArgs
+    {
+        public ITargetsFactory TargetsFactory { get; private set; }
+        public ITargetConsumer TargetConsumer { get; private set; }
+        public Faction EnemyFaction { get; private set; }
+        public IList<TargetType> AttackCapabilities { get; private set; }
+        public float MaxRangeInM { get; private set; }
+        public float MinRangeInM { get; private set; }
+
+        public TargetProcessorArgs(
+        	ITargetsFactory targetsFactory,
+            ITargetConsumer targetConsumer,
+            Faction enemyFaction,
+            IList<TargetType> attackCapabilities,
+            float maxRangeInM,
+            float minRangeInM = 0)
+        {
+            Helper.AssertIsNotNull(targetsFactory, targetConsumer, attackCapabilities);
+
+            TargetsFactory = targetsFactory;
+            TargetConsumer = targetConsumer;
+            EnemyFaction = enemyFaction;
+            AttackCapabilities = attackCapabilities;
+            MaxRangeInM = maxRangeInM;
+            MinRangeInM = minRangeInM;
+        }
+    }
+}
