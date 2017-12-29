8314ef2b833379cf061469907c94979a15985d67
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-29 15:36:24 -0800

    Create LoadoutUnitItem :)

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/LoadoutItems/LoadoutUnitItem.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Buildables.Units;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.ItemDetails;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.LoadoutItems
+{
+    public class LoadoutUnitItem : LoadoutItem<IUnit>
+	{
+        public void Initialise(IUnit building, UnitDetailsManager unitDetailsManager)
+		{
+			InternalInitialise(building, unitDetailsManager);
+		}
+	}
+}
