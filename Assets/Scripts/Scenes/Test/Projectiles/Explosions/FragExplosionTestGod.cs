b7b2dc06c92a330fd7955d19f6b24e7137af0d35
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-28 21:32:50 -0800

    Adjust frag explosion sizing, so explosion max size (bright first light) is the desired radius :)

--- Assets/Scripts/Scenes/Test/Projectiles/Explosions/FragExplosionTestGod.cs
+++ Assets/Scripts/Scenes/Test/Projectiles/Explosions/FragExplosionTestGod.cs
@@ -8,17 +8,21 @@ namespace BattleCruisers.Scenes.Test.Projectiles.Explosions
     {
         void Start()
         {
+            // Reduce game speed
+            Time.timeScale = 0.1f;
+
+            // Setup explosions
             Explosion[] explosions = FindObjectsOfType<Explosion>();
 
-            //Assert.AreEqual(2, explosions.Length);
+            Assert.AreEqual(2, explosions.Length);
 
             Explosion smallExplosion = explosions[0];
             smallExplosion.Initialise(radiusInM: 1, durationInS: 999);
             smallExplosion.Show(new Vector3(-2, 0));
 
-            //Explosion bigExplosion = explosions[0];
-            //bigExplosion.Initialise(radiusInM: 2, durationInS: 999);
-            //bigExplosion.Show(new Vector3(2, 0));
+            Explosion bigExplosion = explosions[1];
+            bigExplosion.Initialise(radiusInM: 2, durationInS: 999);
+            bigExplosion.Show(new Vector3(2, 0));
         }
     }
 }
