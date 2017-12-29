ef9871d5eb3572a1365e2ee332afe505a2f8acc5
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-29 15:37:04 -0800

    Add TODO

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/LoadoutItems/LoadoutBuildingItem.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/LoadoutItems/LoadoutBuildingItem.cs
@@ -5,6 +5,7 @@ namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.LoadoutItems
 {
 	public class LoadoutBuildingItem : LoadoutItem<IBuilding>
 	{
+        // FELIX  Perhaps use interface as parameter instead of BuildingDetailsManager?  (Also for IUnit counterpart)
 		public void Initialise(IBuilding building, BuildingDetailsManager buildingDetailsManager)
 		{
 			InternalInitialise(building, buildingDetailsManager);
