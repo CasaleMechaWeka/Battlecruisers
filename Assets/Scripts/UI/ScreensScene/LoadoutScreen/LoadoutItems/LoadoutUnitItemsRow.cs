0d3a762d688e97e5862153d9ec74029cef3d21bb
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-29 16:25:34 -0800

    Create LoadoutUnitItemsRow :)

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/LoadoutItems/LoadoutUnitItemsRow.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.Buildables.Units;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.LoadoutItems
+{
+    public class LoadoutUnitItemsRow : LoadoutBuildableItemsRow<IUnit>
+    {
+        protected override LoadoutItem<IUnit> CreateItem(IUnit item)
+        {
+            return _uiFactory.CreateLoadoutUnitItem(layoutGroup, item);
+        }
+    }
+}
