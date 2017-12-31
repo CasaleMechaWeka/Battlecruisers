a94cad9532515b5a822e37eeed64198adc1d01c1
Felix Fei Mann <felix.fei.mann@gmail.com>
2017-12-30 16:17:32 -0800

    Tidy up usings

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/IItemsRow.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/IItemsRow.cs
@@ -1,15 +1,8 @@
-﻿using BattleCruisers.Buildables.Buildings;
-using BattleCruisers.UI.ScreensScene.LoadoutScreen.LoadoutItems;
-using BattleCruisers.UI.ScreensScene.LoadoutScreen.UnlockedItems;
-using BattleCruisers.Data;
-using BattleCruisers.Data.Models.PrefabKeys;
-using BattleCruisers.Fetchers;
-using System;
-using System.Collections.Generic;
+﻿using BattleCruisers.UI.ScreensScene.LoadoutScreen.UnlockedItems;
 
 namespace BattleCruisers.UI.ScreensScene.LoadoutScreen
 {
-	public interface IItemsRow<TItem> where TItem : IComparableItem
+    public interface IItemsRow<TItem> where TItem : IComparableItem
 	{
 		/// <returns><c>true</c>, if unlocked item is now in loadout, <c>false</c> otherwise.</returns>
 		bool SelectUnlockedItem(UnlockedItem<TItem> item);
