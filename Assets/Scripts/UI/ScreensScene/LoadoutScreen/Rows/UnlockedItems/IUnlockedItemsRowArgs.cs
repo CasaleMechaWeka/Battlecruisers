0a0045450459b4a8ec06c23b97524ebcb1d8dc60
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-01 11:43:35 -0800

    Create IUnlockedItemsRowArgs :D

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/IUnlockedItemsRowArgs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.ItemDetails;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.UnlockedItems
+{
+    public interface IUnlockedItemsRowArgs<TItem> where TItem : IComparableItem
+    {
+        IUIFactory UIFactory { get; }
+        IList<TItem> UnlockedItems { get; }
+        IItemsRow<TItem> ItemsRow { get; }
+        IItemDetailsManager<TItem> DetailsManager { get; }
+    }
+}
---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/UnlockedItemsRowArgs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.ItemDetails;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.UnlockedItems
+{
+    public class UnlockedItemsRowArgs<TItem> : IUnlockedItemsRowArgs<TItem> where TItem : IComparableItem
+    {
+        public IUIFactory UIFactory { get; private set; }
+        public IList<TItem> UnlockedItems { get; private set; }
+        public IItemsRow<TItem> ItemsRow { get; private set; }
+        public IItemDetailsManager<TItem> DetailsManager { get; private set; }
+
+        public UnlockedItemsRowArgs(IUIFactory uiFactory, IList<TItem> unlockedItems, IItemsRow<TItem> itemsRow, IItemDetailsManager<TItem> detailsManager)
+        {
+            Helper.AssertIsNotNull(uiFactory, unlockedItems, itemsRow, detailsManager);
+
+            UIFactory = uiFactory;
+            UnlockedItems = unlockedItems;
+            ItemsRow = itemsRow;
+            DetailsManager = detailsManager;            
+        }
+    }
+}
