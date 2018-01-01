90c016b85c0ab63a4c6ceb75e3bb4dce373b7c5c
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-01 11:56:42 -0800

    Remove unnecessary method override :)

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/UnlockedBuildingItemsRow.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/UnlockedBuildingItemsRow.cs
@@ -1,17 +1,10 @@
-﻿using System.Collections.Generic;
-using BattleCruisers.Buildables.Buildings;
+﻿using BattleCruisers.Buildables.Buildings;
 using UnityEngine.UI;
 
 namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.UnlockedItems
 {
     public class UnlockedBuildingItemsRow : UnlockedBuildableItemsRow<IBuilding>
 	{
-        // FELIX  Remove method?
-        public override void Initialise(IUnlockedItemsRowArgs<IBuilding> args, IList<IBuilding> loadoutBuildables)
-		{
-            base.Initialise(args, loadoutBuildables);
-		}
-
         protected override UnlockedItem<IBuilding> CreateUnlockedItem(IBuilding item, HorizontalOrVerticalLayoutGroup itemParent, bool isInLoadout)
         {
             return _uiFactory.CreateUnlockedBuildingItem(layoutGroup, _itemsRow, item, isInLoadout);
--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/UnlockedUnitItemsRow.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/UnlockedUnitItemsRow.cs
@@ -1,17 +1,10 @@
-﻿using System.Collections.Generic;
-using BattleCruisers.Buildables.Units;
+﻿using BattleCruisers.Buildables.Units;
 using UnityEngine.UI;
 
 namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.UnlockedItems
 {
     public class UnlockedUnitItemsRow : UnlockedBuildableItemsRow<IUnit>
 	{
-        // FELIX  Remove method?
-        public override void Initialise(IUnlockedItemsRowArgs<IUnit> args, IList<IUnit> loadoutBuildables)
-		{
-            base.Initialise(args, loadoutBuildables);
-		}
-
         protected override UnlockedItem<IUnit> CreateUnlockedItem(IUnit item, HorizontalOrVerticalLayoutGroup itemParent, bool isInLoadout)
         {
             return _uiFactory.CreateUnlockedUnitItem(layoutGroup, _itemsRow, item, isInLoadout);
