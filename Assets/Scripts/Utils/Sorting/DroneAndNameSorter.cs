26c4e79c3951a5cc9f2f3918eff9ae70d9c161b3
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-01 15:24:15 -0800

    Create DroneAndNameSorter :D

---
+++ Assets/Scripts/Utils/Sorting/DroneAndNameSorter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BattleCruisers.Buildables;
+
+namespace BattleCruisers.Utils.Sorting
+{
+    /// <summary>
+    /// Ordery by:
+    /// 1. Drone number (descending)
+    /// 2. Then by name
+    /// </summary>
+    public class DroneAndNameSorter<TBuildable> : IBuildableSorter<TBuildable> 
+        where TBuildable : class, IBuildable
+    {
+        public IList<IBuildableWrapper<TBuildable>> Sort(IList<IBuildableWrapper<TBuildable>> buildables)
+        {
+            return
+                buildables
+                    .OrderBy(wrapper => wrapper.Buildable.NumOfDronesRequired)
+                    .ThenBy(wrapper => wrapper.Buildable.Name)
+                    .ToList();
+        }
+    }
+}
