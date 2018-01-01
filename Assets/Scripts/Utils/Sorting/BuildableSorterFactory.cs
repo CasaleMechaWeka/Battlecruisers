4cabcd2343b8541c0dd07c7a981785a0ce4ad3fe
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-01 15:29:04 -0800

    Create BuildableSorterFactory :)

---
+++ Assets/Scripts/Utils/Sorting/BuildableSorterFactory.cs
@@ -0,0 +1,18 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Units;
+
+namespace BattleCruisers.Utils.Sorting
+{
+    public class BuildableSorterFactory : IBuildableSorterFactory
+    {
+        public IBuildableSorter<IBuilding> CreateBuildingSorter()
+        {
+            return new DroneAndNameSorter<IBuilding>();
+        }
+
+        public IBuildableSorter<IUnit> CreateUnitSorter()
+        {
+            return new DroneAndNameSorter<IUnit>();
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/Sorting/IBuildableSorterFactory.cs
@@ -0,0 +1,11 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Units;
+
+namespace BattleCruisers.Utils.Sorting
+{
+    public interface IBuildableSorterFactory
+    {
+        IBuildableSorter<IBuilding> CreateBuildingSorter();
+        IBuildableSorter<IUnit> CreateUnitSorter();
+    }
+}
