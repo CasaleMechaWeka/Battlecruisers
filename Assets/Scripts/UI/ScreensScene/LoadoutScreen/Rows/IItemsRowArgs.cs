9096bacdbdf0d1f16788e67225dfd606a39f5c3f
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-01 11:23:34 -0800

    Create IItemsRowArgs :D

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/IItemsRowArgs.cs
@@ -0,0 +1,14 @@
+﻿using BattleCruisers.Data.Models;
+using BattleCruisers.Fetchers;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.ItemDetails;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows
+{
+    public interface IItemsRowArgs<TItem> where TItem : IComparableItem
+    {
+        IGameModel GameModel { get; }
+        IPrefabFactory PrefabFactory {  get; }
+        IUIFactory UIFactory { get; }
+        IItemDetailsManager<TItem> DetailsManager { get; }
+    }
+}
---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/ItemsRowArgs.cs
@@ -0,0 +1,29 @@
+﻿using BattleCruisers.Data.Models;
+using BattleCruisers.Fetchers;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.ItemDetails;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows
+{
+    public class ItemsRowArgs<TItem> : IItemsRowArgs<TItem> where TItem : IComparableItem
+    {
+        public IGameModel GameModel { get; private set; }
+        public IPrefabFactory PrefabFactory { get; private set; }
+        public IUIFactory UIFactory { get; private set; }
+        public IItemDetailsManager<TItem> DetailsManager { get; private set; }
+
+        public ItemsRowArgs(
+            IGameModel gameModel,
+            IPrefabFactory prefabFactory,
+            IUIFactory uiFactory,
+            IItemDetailsManager<TItem> detailsManager)
+        {
+            Helper.AssertIsNotNull(gameModel, prefabFactory, uiFactory, detailsManager);
+
+            GameModel = gameModel;
+            PrefabFactory = prefabFactory;
+            UIFactory = uiFactory;
+            DetailsManager = detailsManager;
+        }
+    }
+}
