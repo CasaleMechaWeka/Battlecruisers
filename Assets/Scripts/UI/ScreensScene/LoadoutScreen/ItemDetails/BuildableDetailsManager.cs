2dd6c5b7601042d9471a08c80f0d265b184e8037
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-01 12:31:43 -0800

    Assign detail controllers programamtically :)

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/ItemDetails/BuildableDetailsManager.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/ItemDetails/BuildableDetailsManager.cs
@@ -1,20 +1,23 @@
 ﻿using BattleCruisers.Buildables;
 using BattleCruisers.Fetchers;
 using BattleCruisers.UI.Common.BuildingDetails;
+using BattleCruisers.Utils;
 
 namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.ItemDetails
 {
     public class BuildableDetailsManager<TItem> : ItemDetailsManager<TItem> where TItem : class, IBuildable
 	{
-        // FELIX  Initialise programmatically?
-        public BaseBuildableDetails<TItem> singleItemDetails, leftComparableItemDetails, rightComparableItemDetails;
-
 		public void Initialise()
 		{
             ISpriteProvider spriteProvider = new SpriteProvider(new SpriteFetcher());
-			
+
+            BaseBuildableDetails<TItem> singleItemDetails = transform.FindNamedComponent<BaseBuildableDetails<TItem>>("SingleItemDetails");
 			singleItemDetails.Initialise(spriteProvider);
+
+            BaseBuildableDetails<TItem> leftComparableItemDetails = transform.FindNamedComponent<BaseBuildableDetails<TItem>>("LeftItemDetails");
 			leftComparableItemDetails.Initialise(spriteProvider);
+
+            BaseBuildableDetails<TItem> rightComparableItemDetails = transform.FindNamedComponent<BaseBuildableDetails<TItem>>("RightItemDetails");
 			rightComparableItemDetails.Initialise(spriteProvider);
 
 			Initialise(singleItemDetails, leftComparableItemDetails, rightComparableItemDetails);
