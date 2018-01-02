e065ff6b32a21215d8bc9e70cbacbb0457c269f0
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-01 21:09:31 -0800

    Create SkyMaterials :

---
+++ Assets/Scripts/Data/Static/SkyMaterials.cs
@@ -0,0 +1,13 @@
+﻿namespace BattleCruisers.Data.Static
+{
+    public static class SkyMaterials
+    {
+        public const string Blue = "SkyboxBlue";
+        public const string BlueDeep = "SkyboxBlueDeep";
+        public const string BlueCloudy = "SkyboxBlueCloudy";
+        public const string White = "SkyboxWhite";
+        public const string Sunset = "SkyboxSunset";
+        public const string SunsetCloudy = "SkyboxSunsetCloudy";
+        public const string SunsetWeirdClouds = "SkyboxSunsetWeirdClouds";
+    }
+}
