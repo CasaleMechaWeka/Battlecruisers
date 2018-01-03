f5fb83f2b55ec020d3a0d12c023d12cb39f6216c
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-02 21:47:14 -0800

    Remove unused class :)

--- Assets/Scripts/Buildables/Buildings/Turrets/Stats/TeslaCoilStats.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BattleCruisers.Buildables.Buildings.Turrets.Stats
-{
-    public class TeslaCoilStats : BasicTurretStats
-    {
-        public float Damage { get { return 1; } }
-    }
-}
