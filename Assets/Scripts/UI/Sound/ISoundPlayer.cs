387732dd3a9035848753aa3e5e922c4391517069
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-06 18:14:12 -0800

    Create SoundPlayer :D

---
+++ Assets/Scripts/UI/Sound/ISoundPlayer.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Utils.UIWrappers;
+using UnityEngine;
+
+namespace BattleCruisers.UI.Sound
+{
+    public interface ISoundPlayer
+    {
+        void PlaySound(IAudioClipWrapper soundClip, Vector2 position);
+    }
+}
---
+++ Assets/Scripts/UI/Sound/SoundPlayer.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Utils.UIWrappers;
+using UnityEngine;
+
+namespace BattleCruisers.UI.Sound
+{
+    public class SoundPlayer : ISoundPlayer
+    {
+        public void PlaySound(IAudioClipWrapper soundClip, Vector2 position)
+        {
+            AudioSource.PlayClipAtPoint(soundClip.AudioClip, position);
+        }
+    }
+}
