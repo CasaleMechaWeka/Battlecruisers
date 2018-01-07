ab95a0c3f2fd60b038dcf80285d47d85aa7d8e77
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-06 20:48:33 -0800

    Create SoundKey :)

---
+++ Assets/Scripts/UI/Sound/ISoundKey.cs
@@ -0,0 +1,13 @@
+﻿namespace BattleCruisers.UI.Sound
+{
+    public enum SoundType
+    {
+        Deaths, Engines, Explosions, Firing
+    }
+
+    public interface ISoundKey
+    {
+        SoundType Type { get; }
+        string Name { get; }
+    }
+}
---
+++ Assets/Scripts/UI/Sound/SoundKey.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleCruisers.UI.Sound
+{
+    public class SoundKey : ISoundKey
+    {
+        public SoundType Type { get; private set; }
+        public string Name { get; private set; }
+
+        public SoundKey(SoundType type, string name)
+        {
+            Type = type;
+            Name = name;
+        }
+    }
+}
