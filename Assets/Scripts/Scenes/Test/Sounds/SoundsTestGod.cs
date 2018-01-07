f4d325c0c40c9565b12cdfe5cf2cd471a8fe9211
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-06 16:57:29 -0800

    Create test scene that plays mp3 and wav sounds :)

---
+++ Assets/Scripts/Scenes/Test/Sounds/SoundsTestGod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Sounds
+{
+    public class SoundsTestGod : MonoBehaviour
+    {
+        //public AudioClip audioClip;
+
+        void Start()
+        {
+            string mp3File = "AA-machine-gun";
+            string wavFile = "archon-engine-seamless-loop";
+
+            //AudioClip mp3Audio = GetAudio(mp3File);
+            //AudioSource.PlayClipAtPoint(mp3Audio, default(Vector3));
+
+            AudioClip wavAudio = GetAudio(wavFile);
+            AudioSource.PlayClipAtPoint(wavAudio, default(Vector3));
+
+            //AudioSource.PlayClipAtPoint(audioClip, default(Vector3));
+        }
+
+        private AudioClip GetAudio(string audioName)
+        {
+            AudioClip audioClip = Resources.Load<AudioClip>("Sounds/" + audioName);
+
+            if (audioClip == null)
+            {
+                throw new ArgumentException("Invalid audio name: " + audioName);
+            }
+
+            return audioClip;
+        }
+    }
+}
