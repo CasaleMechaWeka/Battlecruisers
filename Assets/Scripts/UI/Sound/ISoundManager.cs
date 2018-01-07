2b55029dabd2901d6cace45099414ce7b0f0afed
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-06 18:23:21 -0800

    Create SoundManager :)

---
+++ Assets/Scripts/UI/Sound/ISoundManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.UI.Sound
+{
+    public interface ISoundManager
+    {
+        void PlaySound(string soundName, Vector2 position);
+    }
+}
---
+++ Assets/Scripts/UI/Sound/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.Fetchers;
+using BattleCruisers.Utils.UIWrappers;
+using UnityEngine;
+
+namespace BattleCruisers.UI.Sound
+{
+    public class SoundManager : ISoundManager
+    {
+        private readonly ISoundFetcher _soundFetcher;
+        private readonly ISoundPlayer _soundPlayer;
+
+        public SoundManager(ISoundFetcher soundFetcher, ISoundPlayer soundPlayer)
+        {
+            Helper.AssertIsNotNull(soundFetcher, soundPlayer);
+
+            _soundFetcher = soundFetcher;
+            _soundPlayer = soundPlayer;
+        }
+
+        public void PlaySound(string soundName, Vector2 position)
+        {
+            IAudioClipWrapper sound = _soundFetcher.GetSound(soundName);
+            _soundPlayer.PlaySound(sound, position);
+        }
+    }
+}
