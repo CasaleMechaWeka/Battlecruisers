1d5571f25e0c20df3880c684ba72c72406c04ed8
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-06 21:11:47 -0800

    Renamed sound files, and started creating static SoundKeys :)

---
+++ Assets/Scripts/Data/Static/SoundKeys.cs
@@ -0,0 +1,12 @@
+﻿namespace BattleCruisers.Data.Static
+{
+    public static class SoundKeys
+    {
+        public static class Engines
+        {
+            // FELIX  NEXT
+            // Ships
+            public static ISoundKey Archon { get { return new SoundKey(); } }
+        }
+    }
+}
