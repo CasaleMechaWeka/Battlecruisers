47126b3558e62be3331c436dbdd3584478b1d7c0
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-06 20:34:37 -0800

    Creat SoundManagerTests :)

---
+++ Assets/Editor/Tests/UI/Sound/SoundManagerTests.cs
@@ -0,0 +1,42 @@
+﻿using BattleCruisers.UI.Sound;
+using BattleCruisers.Utils.Fetchers;
+using BattleCruisers.Utils.UIWrappers;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityAsserts = UnityEngine.Assertions;
+
+namespace BattleCruisers.Tests.UI.Sound
+{
+    public class SoundManagerTests
+    {
+        private ISoundManager _soundManager;
+        private ISoundFetcher _soundFetcher;
+        private ISoundPlayer _soundPlayer;
+
+        [SetUp]
+        public void SetuUp()
+        {
+            UnityAsserts.Assert.raiseExceptions = true;
+
+            _soundFetcher = Substitute.For<ISoundFetcher>();
+            _soundPlayer = Substitute.For<ISoundPlayer>();
+
+            _soundManager = new SoundManager(_soundFetcher, _soundPlayer);
+        }
+
+        [Test]
+        public void PlaySound()
+        {
+            IAudioClipWrapper audioClip = Substitute.For<IAudioClipWrapper>();
+            Vector2 soundPosition = new Vector2(2, 3);
+            string soundName = "legit as sound";
+
+            _soundFetcher.GetSound(soundName).Returns(audioClip);
+
+            _soundManager.PlaySound(soundName, soundPosition);
+
+            _soundPlayer.Received().PlaySound(audioClip, soundPosition);
+        }
+    }
+}
