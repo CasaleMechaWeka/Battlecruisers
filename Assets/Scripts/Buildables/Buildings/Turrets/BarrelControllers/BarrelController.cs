fd1af29822bd452ff4c815a7f453aa8084d7aa5a
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-15 18:23:51 -0800

    Remove some empty lines in attempt to reduce monster method size :/

--- Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/BarrelController.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/BarrelController.cs
@@ -147,9 +147,7 @@ namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers
             }
 
             float desiredAngleInDegrees = _angleCalculator.FindDesiredAngle(ProjectileSpawnerPosition, predictedTargetPosition, IsSourceMirrored, _projectileStats.MaxVelocityInMPerS);
-
             bool isOnTarget = _rotationMovementController.IsOnTarget(desiredAngleInDegrees);
-
             float limitedDesiredAngle = _angleLimiter.LimitAngle(desiredAngleInDegrees);
 
             if (!isOnTarget)
@@ -169,9 +167,7 @@ namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers
             // the target in this case.  Hence use the actual angle our turret barrel
             // is at, instead of the perfect desired angle.
             float fireAngle = _turretStats.IsInBurst ? transform.rotation.eulerAngles.z : limitedDesiredAngle;
-
             Logging.Log(Tags.BARREL_CONTROLLER, "TryFire()  fireAngle: " + fireAngle + "  transform.rotation.eulerAngles.z: " + transform.rotation.eulerAngles.z);
-
             fireAngle = _accuracyAdjuster.FindAngleInDegrees(fireAngle, ProjectileSpawnerPosition, predictedTargetPosition, IsSourceMirrored);
 
             Fire(fireAngle);
