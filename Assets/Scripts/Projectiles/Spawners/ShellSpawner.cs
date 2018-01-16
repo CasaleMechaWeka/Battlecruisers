eb6e486e25ac08487fcf6775b0f1eccced1fe772
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-15 22:05:28 -0800

    Play firing sound :D

--- Assets/Scripts/Projectiles/Spawners/ShellSpawner.cs
+++ Assets/Scripts/Projectiles/Spawners/ShellSpawner.cs
@@ -7,19 +7,20 @@ namespace BattleCruisers.Projectiles.Spawners
 {
     public class ShellSpawner : BaseShellSpawner
 	{
-        private ISoundKey _spawnSoundKey;
-
-        public SoundKey temp;
+        private ISoundKey _firingSound;
+        private ISoundManager _soundManager;
 
         public ProjectileController shellPrefab;
 		protected override ProjectileController ProjectilePrefab { get { return shellPrefab; } }
 
-        public void Initialise(IProjectileSpawnerArgs args, ITargetFilter targetFilter, ISoundKey spawnSoundKey)
+        public void Initialise(IProjectileSpawnerArgs args, ITargetFilter targetFilter, ISoundKey firingSound)
         {
             base.Initialise(args, targetFilter);
 
-            Assert.IsNotNull(spawnSoundKey);
-            _spawnSoundKey = spawnSoundKey;
+            Assert.IsNotNull(firingSound);
+            _firingSound = firingSound;
+
+            _soundManager = args.FactoryProvider.SoundManager;
         }
 
         public void SpawnShell(float angleInDegrees, bool isSourceMirrored)
@@ -27,6 +28,7 @@ namespace BattleCruisers.Projectiles.Spawners
             ProjectileController shell = Instantiate(shellPrefab, transform.position, new Quaternion());
 			Vector2 shellVelocity = FindProjectileVelocity(angleInDegrees, isSourceMirrored, _projectileStats.MaxVelocityInMPerS);
             shell.Initialise(_projectileStats, shellVelocity, _targetFilter, _factoryProvider, _parent);
+            _soundManager.PlaySound(_firingSound, transform.position);
 		}
 	}
 }
