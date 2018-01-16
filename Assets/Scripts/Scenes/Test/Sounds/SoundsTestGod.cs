aa94734cc291633e0f55044bc23e4603a41191f6
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-15 17:39:09 -0800

    SpatielBlend all the way to 3D makes sound get weaker the further away from an audio listener :)

--- Assets/Scripts/Scenes/Test/Sounds/SoundsTestGod.cs
+++ Assets/Scripts/Scenes/Test/Sounds/SoundsTestGod.cs
@@ -1,46 +1,20 @@
-﻿using BattleCruisers.Data.Static;
-using BattleCruisers.UI.Sound;
-using BattleCruisers.Utils.Fetchers;
-using BattleCruisers.Utils.UIWrappers;
+﻿using System.Collections.Generic;
+using BattleCruisers.Scenes.Test.Utilities;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Sounds
 {
-    // FELIX  Delete this and the corresponding test scene (as soon as I have a better sound test scene :) )
     public class SoundsTestGod : MonoBehaviour
     {
-        private AudioSource _source;
+        public List<Vector2> patrolPoints;
 
         void Start()
         {
-            SoundFetcher fetcher = new SoundFetcher();
-
-            ISoundKey mp3Key = SoundKeys.Firing.AntiAir;
-            ISoundKey wavKey = SoundKeys.Engines.Archon;
-
-            //IAudioClipWrapper audioClip = fetcher.GetSound(mp3Key);
-            IAudioClipWrapper audioClip = fetcher.GetSound(wavKey);
-
-            //AudioSource.PlayClipAtPoint(audioClip.AudioClip, default(Vector3));
-
-            _source = GetComponent<AudioSource>();
-            _source.clip = audioClip.AudioClip;
-            _source.loop = true;
-            _source.Play();
-        }
-
-        public void ToggleSound()
-        {
-            if (_source.isPlaying)
-            {
-                _source.Pause();
-                //gameObject.SetActive(false);
-            }
-            else
-            {
-                //gameObject.SetActive(true);
-                _source.UnPause();
-            }
+            Helper helper = new Helper();
+            TestAircraftController aircraft = FindObjectOfType<TestAircraftController>();
+            aircraft.PatrolPoints = patrolPoints;
+            helper.InitialiseUnit(aircraft);
+            aircraft.StartConstruction();
         }
     }
 }
