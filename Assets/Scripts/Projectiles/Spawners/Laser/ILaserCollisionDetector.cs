bb36edfa5b0e0836e77331027ed8225804e35ba4
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-16 21:42:04 -0800

    Create LaserCollisionDetector :)

---
+++ Assets/Scripts/Projectiles/Spawners/Laser/ILaserCollisionDetector.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Projectiles.Spawners.Laser
+{
+    public interface ILaserCollisionDetector
+    {
+        ILaserCollision FindCollision(Vector2 source, float angleInDegrees, bool isSourceMirrored);
+    }
+}
---
+++ Assets/Scripts/Projectiles/Spawners/Laser/LaserCollisionDetector.cs
@@ -0,0 +1,59 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Projectiles.Spawners.Laser
+{
+    public class LaserCollisionDetector : ILaserCollisionDetector
+    {
+        private readonly ContactFilter2D _contactFilter;
+        private readonly ITargetFilter _targetFilter;
+
+        private const int NUM_OF_COLLIDERS_TO_RAYCAST = 25;
+
+        public LaserCollisionDetector(ContactFilter2D contactFilter, ITargetFilter targetFilter)
+        {
+            Assert.IsNotNull(targetFilter);
+
+            _contactFilter = contactFilter;
+            _targetFilter = targetFilter;
+        }
+
+        public ILaserCollision FindCollision(Vector2 source, float angleInDegrees, bool isSourceMirrored)
+        {
+            Vector2 laserDirection = FindLaserDirection(angleInDegrees, isSourceMirrored);
+
+            RaycastHit2D[] results = new RaycastHit2D[NUM_OF_COLLIDERS_TO_RAYCAST];
+            int numOfResults = Physics2D.Raycast(source, laserDirection, _contactFilter, results);
+
+            return GetMatchingTarget(results, numOfResults);
+        }
+
+        private Vector2 FindLaserDirection(float angleInDegrees, bool isSourceMirrored)
+        {
+            float directionMultiplier = isSourceMirrored ? -1 : 1;
+            float xComponent = Mathf.Cos(Mathf.Deg2Rad * angleInDegrees) * directionMultiplier;
+            float yComponent = Mathf.Sin(Mathf.Deg2Rad * angleInDegrees);
+            return new Vector2(xComponent, yComponent);
+        }
+
+        private ILaserCollision GetMatchingTarget(RaycastHit2D[] results, int numOfResults)
+        {
+            for (int i = 0; i < numOfResults; i++)
+            {
+                RaycastHit2D result = results[i];
+                Assert.IsNotNull(result.collider);
+
+                ITarget target = result.collider.gameObject.GetComponent<ITarget>();
+
+                if (target != null && _targetFilter.IsMatch(target))
+                {
+                    return new LaserCollision(target, result.point);
+                }
+            }
+
+            return null;
+        }
+    }
+}
