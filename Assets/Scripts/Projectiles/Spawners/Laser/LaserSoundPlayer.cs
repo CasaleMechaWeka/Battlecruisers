fceec1c0169f89fdee8b55bf5fd2f4ea84e765de
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-16 21:54:12 -0800

    Craete LaserSoundPlayer :D

---
+++ Assets/Scripts/Projectiles/Spawners/Laser/LaserSoundPlayer.cs
@@ -0,0 +1,42 @@
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.UIWrappers;
+
+namespace BattleCruisers.Projectiles.Spawners.Laser
+{
+    public class LaserSoundPlayer : IManagedDisposable
+    {
+        private readonly ILaserRenderer _laserRenderer;
+        private readonly IAudioSourceWrapper _audioSource;
+        private readonly IAudioClipWrapper _laserSound;
+
+        public LaserSoundPlayer(ILaserRenderer laserRenderer, IAudioSourceWrapper audioSource, IAudioClipWrapper laserSound)
+        {
+            Helper.AssertIsNotNull(laserRenderer, audioSource, laserSound);
+
+            _laserRenderer = laserRenderer;
+            _audioSource = audioSource;
+            _laserSound = laserSound;
+
+            _laserRenderer.LaserVisibilityChanged += _laserRenderer_LaserVisibilityChanged;
+
+            _audioSource.AudioClip = _laserSound;
+        }
+
+        private void _laserRenderer_LaserVisibilityChanged(object sender, LaserVisibilityChangedEventArgs e)
+        {
+            if (e.IsLaserVisible)
+            {
+                _audioSource.Play(isSpatial: true, loop: false);
+            }
+            else
+            {
+                _audioSource.Stop();
+            }
+        }
+
+        public void DisposeManagedState()
+        {
+            _laserRenderer.LaserVisibilityChanged -= _laserRenderer_LaserVisibilityChanged;
+        }
+    }
+}
