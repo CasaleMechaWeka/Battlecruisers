5275d4042c8cfeb18753681994757f7f556f742d
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-17 18:15:32 -0800

    Play bomb sound when bomb hits target :D

---
+++ Assets/Scripts/Projectiles/BombController.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Data.Static;
+using BattleCruisers.UI.Sound;
+
+namespace BattleCruisers.Projectiles
+{
+    public class BombController : ProjectileController
+    {
+        protected override ISoundKey ImpactSoundKey { get { return SoundKeys.Explosions.Bomb; } }
+    }
+}
