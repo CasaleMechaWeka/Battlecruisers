09bca6affccdac5bd52d9d59c1430f2fec1cda0d
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-19 18:00:21 -0800

    Create HullsRowWrapper :)

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/HullsRowWrapper.cs
@@ -0,0 +1,30 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Data.Models;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.ItemDetails;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.LoadoutItems;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.UnlockedItems;
+using BattleCruisers.Utils.Fetchers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows
+{
+    public class HullsRowWrapper : MonoBehaviour
+    {
+        public void Initialise(IGameModel gameModel, IPrefabFactory prefabFactory, IUIFactory uiFactory)
+        {
+            LoadoutHullItem loadoutHullItem = GetComponentInChildren<LoadoutHullItem>();
+            Assert.IsNotNull(loadoutHullItem);
+
+            UnlockedHullItemsRow unlockedHullsRow = GetComponentInChildren<UnlockedHullItemsRow>();
+            Assert.IsNotNull(unlockedHullsRow);
+
+            CruiserDetailsManager cruiserDetailsManager = GetComponentInParent<CruiserDetailsManager>();
+            Assert.IsNotNull(cruiserDetailsManager);
+
+            IItemsRowArgs<ICruiser> args = new ItemsRowArgs<ICruiser>(gameModel, prefabFactory, uiFactory, cruiserDetailsManager);
+            IItemsRow<ICruiser> hullsRow = new HullItemsRow(args, loadoutHullItem, unlockedHullsRow);
+            hullsRow.SetupUI();
+        }
+    }
+}
