ded153d875067e9c4b62c643cc89b1b6e9642b20
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-01-19 19:13:53 -0800

    Create UnitsRowWrapper :D  Ooh, sleeepy

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnitsRowWrapper.cs
@@ -0,0 +1,23 @@
+﻿using BattleCruisers.Buildables.Units;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.LoadoutItems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows
+{
+    public class UnitsRowWrapper : MonoBehaviour
+    {
+        public UnitCategory category;
+
+        public void Initialise(IItemsRowArgs<IUnit> args)
+        {
+            Assert.IsNotNull(args);
+
+            LoadoutUnitItemsRow loadoutRow = GetComponentInChildren<LoadoutUnitItemsRow>();
+            Assert.IsNotNull(loadoutRow);
+
+            IItemsRow<IUnit> buildingsRow = new UnitItemsRow(args, loadoutRow, category);
+            buildingsRow.SetupUI();
+        }
+    }
+}
