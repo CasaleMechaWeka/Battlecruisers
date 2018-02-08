17612f294a5f132838b90753f92ba7f1203037bf
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-02-07 17:41:01 -0800

    Grab stats row references programmatically :)

--- Assets/Scripts/UI/Common/BuildableDetails/Stats/BaseBuildableStatsController.cs
+++ Assets/Scripts/UI/Common/BuildableDetails/Stats/BaseBuildableStatsController.cs
@@ -1,24 +1,35 @@
 ﻿using BattleCruisers.Buildables;
+using BattleCruisers.Utils;
 
 namespace BattleCruisers.UI.Common.BuildingDetails.Stats
 {
     public abstract class BaseBuildableStatsController<TItem> : StatsController<TItem> where TItem : class, IBuildable
 	{
-		public StatsRowNumberController droneRow;
-		public StatsRowNumberController buildTimeRow;
-		public StatsRowStarsController healthRow;
-		public StatsRowStarsController damageRow;
+        private StatsRowNumberController _dronesRow;
+        private StatsRowNumberController _buildTimeRow;
+        private StatsRowStarsController _healthRow;
+        private StatsRowStarsController _damageRow;
 
 		private const string DAMAGE_LABEL = "Damage";
 		private const string BUILD_TIME_LABEL = "BuildTime";
 		private const string BUILD_TIME_SUFFIX = "s";
 
+        public override void Initialise()
+        {
+            base.Initialise();
+
+            _dronesRow = transform.FindNamedComponent<StatsRowNumberController>("DronesRow");
+            _buildTimeRow = transform.FindNamedComponent<StatsRowNumberController>("BuildTimeRow");
+            _healthRow = transform.FindNamedComponent<StatsRowStarsController>("HealthRow");
+            _damageRow = transform.FindNamedComponent<StatsRowStarsController>("DamageRow");
+        }
+
         protected override void InternalShowStats(TItem item, TItem itemToCompareTo)
 		{
-			droneRow.Initialise(DRONES_LABEL, item.NumOfDronesRequired.ToString(), _lowerIsBetterComparer.CompareStats(item.NumOfDronesRequired, itemToCompareTo.NumOfDronesRequired));
-			buildTimeRow.Initialise(BUILD_TIME_LABEL, item.BuildTimeInS.ToString() + BUILD_TIME_SUFFIX, _lowerIsBetterComparer.CompareStats(item.BuildTimeInS, itemToCompareTo.BuildTimeInS));
-			healthRow.Initialise(HEALTH_LABEL, _valueToStarsConverter.HealthValueToStars(item.MaxHealth), _higherIsBetterComparer.CompareStats(item.MaxHealth, itemToCompareTo.MaxHealth));
-			damageRow.Initialise(DAMAGE_LABEL, _valueToStarsConverter.DamageValueToStars(item.Damage), _higherIsBetterComparer.CompareStats(item.Damage, itemToCompareTo.Damage));
+			_dronesRow.Initialise(DRONES_LABEL, item.NumOfDronesRequired.ToString(), _lowerIsBetterComparer.CompareStats(item.NumOfDronesRequired, itemToCompareTo.NumOfDronesRequired));
+			_buildTimeRow.Initialise(BUILD_TIME_LABEL, item.BuildTimeInS.ToString() + BUILD_TIME_SUFFIX, _lowerIsBetterComparer.CompareStats(item.BuildTimeInS, itemToCompareTo.BuildTimeInS));
+			_healthRow.Initialise(HEALTH_LABEL, _valueToStarsConverter.HealthValueToStars(item.MaxHealth), _higherIsBetterComparer.CompareStats(item.MaxHealth, itemToCompareTo.MaxHealth));
+			_damageRow.Initialise(DAMAGE_LABEL, _valueToStarsConverter.DamageValueToStars(item.Damage), _higherIsBetterComparer.CompareStats(item.Damage, itemToCompareTo.Damage));
 		}
 	}
 }
