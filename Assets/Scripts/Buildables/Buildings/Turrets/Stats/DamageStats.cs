a0c62f5ae99ec9f87ff4bb1fc14e918bf9fbb380
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-02-07 18:04:41 -0800

    Create IDamageStats and implementation :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/Stats/DamageStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.Stats
+{
+    public class DamageStats : IDamageStats
+    {
+        public float DamagePerS { get; private set; }
+        public IList<TargetType> AttackCapabilities { get; private set; }
+
+        public DamageStats(float damagePerS, IList<TargetType> attackCapabilities)
+        {
+            Assert.IsTrue(damagePerS > 0);
+            Assert.IsTrue(attackCapabilities.Count > 0);
+
+            DamagePerS = damagePerS;
+            AttackCapabilities = attackCapabilities;
+        }
+    }
+}
---
+++ Assets/Scripts/Buildables/Buildings/Turrets/Stats/IDamageStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.Stats
+{
+    public interface IDamageStats
+    {
+        float DamagePerS { get; }
+        IList<TargetType> AttackCapabilities { get; }
+    }
+}
