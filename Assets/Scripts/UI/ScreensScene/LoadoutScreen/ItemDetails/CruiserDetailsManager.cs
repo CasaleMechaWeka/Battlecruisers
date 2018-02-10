fe71ba25c56fff3e52e1ccd7ab1537b74eec7db1
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-02-09 21:20:56 -0800

    Assign detail controllers programmatically :)

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/ItemDetails/CruiserDetailsManager.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/ItemDetails/CruiserDetailsManager.cs
@@ -1,17 +1,20 @@
 ﻿using BattleCruisers.Cruisers;
 using BattleCruisers.UI.Common.BuildingDetails;
+using BattleCruisers.Utils;
 
 namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.ItemDetails
 {
     public class CruiserDetailsManager : ItemDetailsManager<ICruiser>
 	{
-		public CruiserDetailsController singleItemDetails, leftComparableItemDetails, rightComparableItemDetails;
-
 		public void Initialise()
 		{
-            // FELIX  Assign programmatically like in BuidlableDetailsManager
+            CruiserDetailsController singleItemDetails = transform.FindNamedComponent<CruiserDetailsController>("SingleItemDetails");
             singleItemDetails.Initialise();
+
+            CruiserDetailsController leftComparableItemDetails = transform.FindNamedComponent<CruiserDetailsController>("LeftItemDetails");
             leftComparableItemDetails.Initialise();
+
+            CruiserDetailsController rightComparableItemDetails = transform.FindNamedComponent<CruiserDetailsController>("RightItemDetails");
             rightComparableItemDetails.Initialise();
 
 			Initialise(singleItemDetails, leftComparableItemDetails, rightComparableItemDetails);
