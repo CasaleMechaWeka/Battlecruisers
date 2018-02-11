8a203aa2158b6708627c40f627c285c61cc3619d
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-02-10 19:00:24 -0800

    Create ValueToStarConverterFactory :)

---
+++ Assets/Scripts/Utils/Categorisation/IValueToStarConverterFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace BattleCruisers.Utils.Categorisation
+{
+    public interface IValueToStarConverterFactory
+    {
+        IValueToStarsConverter CreateUnitMovementSpeedConverter();
+
+        // Health
+        IValueToStarsConverter CreateBuildableHealthConverter();
+        IValueToStarsConverter CreateCruiserHealthConverter();
+
+        // Damage
+        IValueToStarsConverter CreateAntiAirDamageConverter();
+        IValueToStarsConverter CreateAntiShipDamageConverter();
+        IValueToStarsConverter CreateAntiCruiserDamageConverter();
+    }
+}
---
+++ Assets/Scripts/Utils/Categorisation/ValueToStarConverterFactory.cs
@@ -0,0 +1,39 @@
+﻿namespace BattleCruisers.Utils.Categorisation
+{
+    public class ValueToStarConverterFactory : IValueToStarConverterFactory
+    {
+        public IValueToStarsConverter CreateUnitMovementSpeedConverter()
+        {
+            return new UnitMovementSpeedToStarsConverter();
+        }
+
+        #region Health
+        public IValueToStarsConverter CreateBuildableHealthConverter()
+        {
+            return new BuildableHealthToStarsConverter();
+        }
+
+        public IValueToStarsConverter CreateCruiserHealthConverter()
+        {
+            return new CruiserHealthToStarsConverter();
+        }
+        #endregion Health
+
+        #region Damage
+        public IValueToStarsConverter CreateAntiAirDamageConverter()
+        {
+            return new AntiAirDamageToStarsConverter();
+        }
+		
+		public IValueToStarsConverter CreateAntiShipDamageConverter()
+		{
+            return new AntiShipDamageToStarsConverter();
+		}
+
+        public IValueToStarsConverter CreateAntiCruiserDamageConverter()
+        {
+            return new AntiCruiserDamageToStarsConverter();
+        }
+        #endregion Damage
+    }
+}
