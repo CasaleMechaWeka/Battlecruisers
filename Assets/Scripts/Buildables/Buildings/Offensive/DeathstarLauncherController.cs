fc2c4be1631ea3e35a4cbae95d6b1ba65e2d6992
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-02-11 20:48:17 -0800

    Grab damage capabilities from death star controller for death star launcher :)

--- Assets/Scripts/Buildables/Buildings/Offensive/DeathstarLauncherController.cs
+++ Assets/Scripts/Buildables/Buildings/Offensive/DeathstarLauncherController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using BattleCruisers.Buildables.Buildings.Turrets.Stats;
+using UnityEngine;
 
 namespace BattleCruisers.Buildables.Buildings.Offensive
 {
@@ -6,5 +7,18 @@ namespace BattleCruisers.Buildables.Buildings.Offensive
 	{
 		protected override Vector3 SpawnPositionAdjustment { get { return new Vector3(0, 0.015f, 0); } }
         public override TargetValue TargetValue { get { return TargetValue.High; } }
+
+        public override void StaticInitialise()
+        {
+            base.StaticInitialise();
+
+            // Need satellite to be initialised to be able to access damage capabilities.
+            satellitePrefab.Buildable.StaticInitialise();
+
+            foreach (IDamageCapability damageCapability in satellitePrefab.Buildable.DamageCapabilities)
+            {
+                AddDamageStats(damageCapability);
+            }
+        }
 	}
 }
