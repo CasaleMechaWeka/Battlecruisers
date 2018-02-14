f7f76cf743c2a3567070b8f361fe66f06f127ab9
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-02-13 21:19:56 -0800

    Create Loot class :)

---
+++ Assets/Scripts/Data/Static/ILoot.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.Data.Static
+{
+    public interface ILoot
+    {
+        int LevelNum { get; }
+        IPrefabKey BuildingKey { get; }
+        IPrefabKey UnitKey { get; }
+        IPrefabKey HullKey { get; }
+    }
+}
