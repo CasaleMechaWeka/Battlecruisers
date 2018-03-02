055c20b6934d934b930c361908d308b62180cc05
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-03-01 22:39:47 -0800

    Add TODO :)

--- Assets/Scripts/Data/Static/LevelLoot/ILootItem.cs
+++ Assets/Scripts/Data/Static/LevelLoot/ILootItem.cs
@@ -6,6 +6,8 @@ namespace BattleCruisers.Data.Static.LevelLoot
 {
     public interface ILootItem
     {
+		// FELIX  Probably need to have method for adding newly unlocked item ti GameModel and player Loadout.
+        // Can then probably remove this getter :)
         IPrefabKey ItemKey { get; }
         void ShowItemDetails(IPrefabFactory prefabFactory, IItemDetailsControllers itemDetailsControllers);
     }
