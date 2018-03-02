f6f7b5042ff3053daa36d60ab76d355a2a30a411
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-03-01 21:56:16 -0800

    Create loot item interfaces

---
+++ Assets/Scripts/Data/Static/LevelLoot/ILootItem.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.UI.Common.BuildingDetails;
+using BattleCruisers.Utils.Fetchers;
+
+namespace BattleCruisers.Data.Static.LevelLoot
+{
+    public interface ILootItem
+    {
+        IPrefabKey ItemKey { get; }
+        void ShowItemDetails(IPrefabFactory prefabFactory, IItemDetailsControllers itemDetailsControllers);
+    }
+}
---
+++ Assets/Scripts/UI/Common/BuildableDetails/IItemDetailsControllers.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Cruisers;
+
+namespace BattleCruisers.UI.Common.BuildingDetails
+{
+    public interface IItemDetailsControllers
+    {
+        IComparableItemDetails<IBuilding> BuildingDetails { get; }
+        IComparableItemDetails<IUnit> UnitDetails { get; }
+        IComparableItemDetails<ICruiser> HullDetails { get; }
+    }
+}
