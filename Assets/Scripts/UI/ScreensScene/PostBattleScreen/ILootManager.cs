51d09bd0d332e352409ab1822aa17ab2228a75c3
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-03-04 12:35:55 -0800

    Create LootManager :D

---
+++ Assets/Scripts/UI/ScreensScene/PostBattleScreen/ILootManager.cs
@@ -0,0 +1,21 @@
+﻿namespace BattleCruisers.UI.ScreensScene.PostBattleScreen
+{
+    public interface ILootManager
+    {
+        /// <summary>
+        /// Shoulds the show loot.
+        /// </summary>
+        /// <returns><c>true</c>, if show loot was shoulded, <c>false</c> otherwise.</returns>
+        /// <param name="levelNum">Level number.</param>
+        bool ShouldShowLoot(int levelNum);
+
+        /// <summary>
+        /// 1. Increments the number of completed levels in the game model
+        /// 2. Adds these items to the:
+        ///     a) Game model unlocked items
+        ///     b) User's loadout
+		/// 3. Displays the loot items for the given level
+        /// </summary>
+        void UnlockLoot(int levelNum);
+    }
+}
---
+++ Assets/Scripts/UI/ScreensScene/PostBattleScreen/LootManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using BattleCruisers.Data;
+using BattleCruisers.Data.Static.LevelLoot;
+using BattleCruisers.UI.Common.BuildingDetails;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Fetchers;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.ScreensScene.PostBattleScreen
+{
+    // FELIX  Test :)
+    public class LootManager : ILootManager
+    {
+        private readonly IDataProvider _dataProvider;
+        private readonly IPrefabFactory _prefabFactory;
+        private readonly IItemDetailsGroup _middleDetailsGroup, _leftDetailsGroup, _rightDetailsGroup;
+
+        public LootManager(
+            IDataProvider dataProvider, 
+            IPrefabFactory prefabFactory, 
+            IItemDetailsGroup middleDetailsGroup,
+            IItemDetailsGroup leftDetailsGroup,
+            IItemDetailsGroup rightDetailsGroup)
+        {
+            Helper.AssertIsNotNull(dataProvider, prefabFactory, middleDetailsGroup, leftDetailsGroup, rightDetailsGroup);
+
+            _dataProvider = dataProvider;
+            _prefabFactory = prefabFactory;
+            _middleDetailsGroup = middleDetailsGroup;
+            _leftDetailsGroup = leftDetailsGroup;
+            _rightDetailsGroup = rightDetailsGroup;
+        }
+
+        public bool ShouldShowLoot(int levelCompleted)
+        {
+            return levelCompleted > _dataProvider.GameModel.NumOfLevelsCompleted;
+        }
+
+        public void UnlockLoot(int levelCompleted)
+        {
+            Assert.IsTrue(ShouldShowLoot(levelCompleted));
+
+            UnlockNextLevel(levelCompleted);
+
+            ILoot unlockedLoot = _dataProvider.StaticData.GetLevelLoot(levelCompleted);
+
+            if (unlockedLoot.Items.Count != 0)
+            {
+                UnlockLootItems(unlockedLoot);
+                ShowLoot(unlockedLoot);
+            }
+
+            _dataProvider.SaveGame();
+        }
+
+        private void UnlockNextLevel(int levelCompleted)
+        {
+            Assert.AreEqual(_dataProvider.GameModel.NumOfLevelsCompleted + 1, levelCompleted);
+            _dataProvider.GameModel.NumOfLevelsCompleted = levelCompleted;
+        }
+
+        private void UnlockLootItems(ILoot unlockedLoot)
+        {
+            // FELIX
+        }
+
+        private void ShowLoot(ILoot unlockedLoot)
+        {
+            switch (unlockedLoot.Items.Count)
+            {
+                case 1:
+                    // Show item details in middle of screen
+                    unlockedLoot.Items[0].ShowItemDetails(_prefabFactory, _middleDetailsGroup);
+                    break;
+                
+                case 2:
+                    // Show item details to left and right sides of screen
+                    unlockedLoot.Items[0].ShowItemDetails(_prefabFactory, _leftDetailsGroup);
+                    unlockedLoot.Items[1].ShowItemDetails(_prefabFactory, _rightDetailsGroup);
+                    break;
+                
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
