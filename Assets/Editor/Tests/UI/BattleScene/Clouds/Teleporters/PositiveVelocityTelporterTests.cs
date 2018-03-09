ccd30cc0ef11fee0ce0e67519786f74b2b3c8b25
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-03-08 21:27:22 -0800

    Create PositiveVelocityTeleporter tests :)

---
+++ Assets/Editor/Tests/UI/BattleScene/Clouds/Teleporters/PositiveVelocityTelporterTests.cs
@@ -0,0 +1,59 @@
+﻿using BattleCruisers.UI.BattleScene.Clouds;
+using BattleCruisers.UI.BattleScene.Clouds.Teleporters;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace BattleCruisers.Tests.UI.BattleScene.Clouds.Teleporters
+{
+    public class PositiveVelocityTeleporterTests
+    {
+        private ICloudTeleporter _teleporter;
+
+        private ICloud _cloud;
+        private ICloudStats _cloudStats;
+        private float _adjustedDisappearLine, _adjustedReappearLine;
+
+        [SetUp]
+        public void SetuUp()
+        {
+            _cloud = Substitute.For<ICloud>();
+            _cloudStats = Substitute.For<ICloudStats>();
+
+            Vector2 cloudSize = new Vector2(4, 2);
+            _cloud.Size.Returns(cloudSize);
+
+            _cloudStats.DisappearLineInM.Returns(10);
+            _cloudStats.ReappaerLineInM.Returns(-10);
+
+            _teleporter = new PositiveVelocityTeleporter(_cloud, _cloudStats);
+
+            _adjustedDisappearLine = 14;
+            _adjustedReappearLine = -14;
+        }
+
+        [Test]
+        public void ShouldTeleportCloud_False()
+        {
+            _cloud.Position = new Vector2(_adjustedDisappearLine, 0);
+            Assert.IsFalse(_teleporter.ShouldTeleportCloud());
+        }
+
+        [Test]
+        public void ShouldTeleportCloud_True()
+        {
+            _cloud.Position = new Vector2(_adjustedDisappearLine + 0.01f, 0);
+            Assert.IsTrue(_teleporter.ShouldTeleportCloud());
+        }
+
+        [Test]
+        public void TeleportCloud()
+        {
+            _cloud.Position = new Vector2(_adjustedDisappearLine + 0.01f, 7);
+
+            _teleporter.TeleportCloud();
+
+            Assert.AreEqual(new Vector2(_adjustedReappearLine, 7), _cloud.Position);
+        }
+    }
+}
