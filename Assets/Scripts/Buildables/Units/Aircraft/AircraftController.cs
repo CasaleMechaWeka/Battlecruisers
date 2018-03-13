6593375b48fbb8aaffb10ced7d897b34eef6b672
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-03-12 17:56:53 -0700

    Add TODO for unused property

--- Assets/Scripts/Buildables/Units/Aircraft/AircraftController.cs
+++ Assets/Scripts/Buildables/Units/Aircraft/AircraftController.cs
@@ -29,6 +29,7 @@ namespace BattleCruisers.Buildables.Units.Aircraft
         protected bool IsInKamikazeMode { get { return _kamikazeController.isActiveAndEnabled; } }
         public override TargetType TargetType { get { return TargetType.Aircraft; } }
 		public override Vector2 Velocity { get { return ActiveMovementController.Velocity; } }
+        // FELIX  MaxPatrollingVelocity is never used :/
         protected virtual float MaxPatrollingVelocity { get { return EffectiveMaxVelocityInMPerS; } }
         protected float EffectiveMaxVelocityInMPerS { get { return _velocityBoostable.BoostMultiplier * maxVelocityInMPerS; } }
         public float VelocityInMPerS { get { return EffectiveMaxVelocityInMPerS; } }
