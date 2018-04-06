7f1c4d9aeb0d9c6e99a1311069157a4ecea36c17
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-05 21:36:38 -0700

    Create CruiserInfoController :D

---
+++ Assets/Scripts/UI/BattlScene/Cruisers/CruiserInfoController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BattleCruisers.Cruisers;
+using BattleCruisers.UI.BattleScene.ProgressBars;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene.Cruisers
+{
+    public class CruiserInfoController : MonoBehaviour
+    {
+        private HealthBarController _cruiserHealthBar;
+        private NumOfDronesController _numOfDrones;
+
+        public void Initialise(ICruiser cruiser)
+        {
+            Assert.IsNotNull(cruiser);
+
+            _cruiserHealthBar = GetComponentInChildren<HealthBarController>();
+            Assert.IsNotNull(_cruiserHealthBar);
+            _cruiserHealthBar.Initialise(cruiser);
+
+            _numOfDrones = GetComponentInChildren<NumOfDronesController>();
+            Assert.IsNotNull(_numOfDrones);
+            _numOfDrones.Initialise(cruiser.DroneManager);
+        }
+    }
+}
