a1734d1d69d6d49bb6cec2b966021e0ed4e9d90e
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-06 20:42:48 -0700

    Create game speed display and initialiser :)

---
+++ Assets/Scripts/UI/BattlScene/GameSpeed/GameSpeedDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Utils;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.BattleScene.GameSpeed
+{
+    public class GameSpeedDisplay
+    {
+        private readonly Text _speedText;
+
+        private const string SPEED_PREFIX = "x";
+        
+        public GameSpeedDisplay(Text speedText, IList<IGameSpeedModifier> gameSpeedModifiers)
+		{
+            Helper.AssertIsNotNull(speedText, gameSpeedModifiers);
+            Assert.IsTrue(gameSpeedModifiers.Count > 0);
+
+            _speedText = speedText;
+
+            foreach (IGameSpeedModifier speedModifier in gameSpeedModifiers)
+            {
+                speedModifier.GameSpeedChanged += SpeedModifier_GameSpeedChanged;
+            }
+		}
+
+        private void SpeedModifier_GameSpeedChanged(object sender, GameSpeedChangedEventArgs e)
+        {
+            _speedText.text = SPEED_PREFIX + e.NewSpeed;
+        }
+	}
+}
---
+++ Assets/Scripts/UI/BattlScene/GameSpeed/GameSpeedInitialiser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Utils;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.BattleScene.GameSpeed
+{
+    public class GameSpeedInitialiser : MonoBehaviour
+    {
+		void Start()
+		{
+            Text speedText = GetComponentInChildren<Text>();
+            Assert.IsNotNull(speedText);
+
+            IList<IGameSpeedModifier> gameSpeedModifiers = new List<IGameSpeedModifier>()
+            {
+				transform.FindNamedComponent<IGameSpeedModifier>("SlowMotionButton"),
+				transform.FindNamedComponent<IGameSpeedModifier>("PlayButton"),
+				transform.FindNamedComponent<IGameSpeedModifier>("FastForwardButton"),
+				transform.FindNamedComponent<IGameSpeedModifier>("DoubleFastForwardButton")
+            };
+
+            new GameSpeedDisplay(speedText, gameSpeedModifiers);
+		}
+	}
+}
