2ca963619eed8cd038586a855b32c40586393288
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-06 20:14:38 -0700

    Create StaticGameSpeedController :)

---
+++ Assets/Scripts/UI/BattlScene/StaticGameSpeedController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene
+{
+    public class StaticGameSpeedController : MonoBehaviour 
+    {
+        private const float MIN_GAME_SPEED = 0.25f;
+        private const float MAX_GAME_SPEED = 4;
+
+        public float gameSpeed;
+
+    	void Start () 
+        {
+            Assert.IsTrue(gameSpeed >= MIN_GAME_SPEED);
+            Assert.IsTrue(gameSpeed <= MAX_GAME_SPEED);
+    	}
+
+        public void ChangeGameSpeed()
+        {
+            Time.timeScale = gameSpeed;
+        }
+    }
+}
