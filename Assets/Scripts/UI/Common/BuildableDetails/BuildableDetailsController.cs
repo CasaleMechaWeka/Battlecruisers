aa345651f7b67432aa0b8095a720027a2d069fb4
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-08 17:55:43 -0700

    Use BottomBarController to simplify BuildableDetailsController :)

--- Assets/Scripts/UI/Common/BuildableDetails/BuildableDetailsController.cs
+++ Assets/Scripts/UI/Common/BuildableDetails/BuildableDetailsController.cs
@@ -1,7 +1,6 @@
 ﻿using BattleCruisers.Buildables;
 using BattleCruisers.Buildables.Repairables;
 using BattleCruisers.Cruisers.Drones;
-using BattleCruisers.UI.BattleScene.ProgressBars;
 using BattleCruisers.UI.Common.BuildingDetails.Buttons;
 using BattleCruisers.Utils;
 using UnityEngine.Assertions;
@@ -10,11 +9,8 @@ namespace BattleCruisers.UI.Common.BuildingDetails
 {
     public abstract class BuildableDetailsController<TItem> : ItemDetails<TItem>, IBuildableDetails<TItem> where TItem : class, IBuildable
     {
-        private IDroneManager _droneManager;
-        private RepairButtonController _repairButton;
-        private ToggleDroneButtonController _toggleDronesButton;
-        private DeleteButtonController _deleteButton;
-        private BuildableProgressBarController _buildProgressController;
+		private DeleteButtonController _deleteButton;
+        private BuildableBottomBarController _bottomBar;
 
         public void Initialise(IDroneManager droneManager, IRepairManager repairManager)
         {
@@ -22,31 +18,22 @@ namespace BattleCruisers.UI.Common.BuildingDetails
 
             Helper.AssertIsNotNull(droneManager, repairManager);
 
-            _droneManager = droneManager;
-
-            _repairButton = GetComponentInChildren<RepairButtonController>(includeInactive: true);
-            Assert.IsNotNull(_repairButton);
-            _repairButton.Initialise(_droneManager, repairManager);
-
-            _toggleDronesButton = GetComponentInChildren<ToggleDroneButtonController>(includeInactive: true);
-            Assert.IsNotNull(_toggleDronesButton);
-            _toggleDronesButton.Initialise();
-
             _deleteButton = GetComponentInChildren<DeleteButtonController>(includeInactive: true);
             Assert.IsNotNull(_deleteButton);
             _deleteButton.Initialise(this);
 
-            _buildProgressController = GetComponentInChildren<BuildableProgressBarController>(includeInactive: true);
-            Assert.IsNotNull(_buildProgressController);
+            _bottomBar = GetComponentInChildren<BuildableBottomBarController>(includeInactive: true);
+            Assert.IsNotNull(_bottomBar);
+            _bottomBar.Initialise(droneManager, repairManager);
         }
 
         public virtual void ShowBuildableDetails(TItem buildable, bool allowDelete)
         {
             base.ShowItemDetails(buildable);
 
-            _buildProgressController.Buildable = buildable;
-            _toggleDronesButton.Buildable = buildable;
-            _repairButton.Repairable = buildable;
+            _bottomBar.Buildable = buildable;
+
+            // FELIX  Update height depending on whether bottom bar is visible!
 
             _deleteButton.gameObject.SetActive(allowDelete);
             _deleteButton.Buildable = buildable;
@@ -56,9 +43,7 @@ namespace BattleCruisers.UI.Common.BuildingDetails
 		{
 			if (_item != null)
 			{
-                _buildProgressController.Buildable = null;
-                _toggleDronesButton.Buildable = null;
-                _repairButton.Repairable = null;
+                _bottomBar.Buildable = null;
                 _deleteButton.Buildable = null;
 			}
 		}
