5baca2e24d75e74bebf94d696bd574746796bbb7
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-10 22:18:48 -0700

    Comment out obsolete files :(

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/UnlockedBuildingItemsRow.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/UnlockedBuildingItemsRow.cs
@@ -3,11 +3,12 @@ using UnityEngine.UI;
 
 namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.UnlockedItems
 {
-    public class UnlockedBuildingItemsRow : UnlockedBuildableItemsRow<IBuilding>
-	{
-        protected override UnlockedItem<IBuilding> CreateUnlockedItem(IBuilding item, HorizontalOrVerticalLayoutGroup itemParent, bool isInLoadout)
-        {
-            return _uiFactory.CreateUnlockedBuildingItem(layoutGroup, _itemsRow, item, isInLoadout);
-        }
-    }
+    // FELIX  Delete :(
+ //   public class UnlockedBuildingItemsRow : UnlockedBuildableItemsRow<IBuilding>
+	//{
+    //    protected override UnlockedItem<IBuilding> CreateUnlockedItem(IBuilding item, HorizontalOrVerticalLayoutGroup itemParent, bool isInLoadout)
+    //    {
+    //        return _uiFactory.CreateUnlockedBuildingItem(layoutGroup, _itemsRow, item, isInLoadout);
+    //    }
+    //}
 }
--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/UnlockedUnitItemsRow.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/UnlockedUnitItemsRow.cs
@@ -3,11 +3,12 @@ using UnityEngine.UI;
 
 namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.UnlockedItems
 {
-    public class UnlockedUnitItemsRow : UnlockedBuildableItemsRow<IUnit>
-	{
-        protected override UnlockedItem<IUnit> CreateUnlockedItem(IUnit item, HorizontalOrVerticalLayoutGroup itemParent, bool isInLoadout)
-        {
-            return _uiFactory.CreateUnlockedUnitItem(layoutGroup, _itemsRow, item, isInLoadout);
-        }
-    }
+    // FELIX  Delete :(
+ //   public class UnlockedUnitItemsRow : UnlockedBuildableItemsRow<IUnit>
+	//{
+    //    protected override UnlockedItem<IUnit> CreateUnlockedItem(IUnit item, HorizontalOrVerticalLayoutGroup itemParent, bool isInLoadout)
+    //    {
+    //        return _uiFactory.CreateUnlockedUnitItem(layoutGroup, _itemsRow, item, isInLoadout);
+    //    }
+    //}
 }
