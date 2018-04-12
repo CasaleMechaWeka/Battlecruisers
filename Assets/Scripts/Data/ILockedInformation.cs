cccedf60615f96c3a40ea4fbc2d451ebeefb8f87
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-11 19:56:19 -0700

    Create ILockedInformation and implementation :)

---
+++ Assets/Scripts/Data/ILockedInformation.cs
@@ -0,0 +1,14 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Units;
+
+namespace BattleCruisers.Data
+{
+    public interface ILockedInformation
+    {
+        int NumOfLevelsUnlocked { get; }
+        int NumOfLockedHulls { get; }
+        int NumOfLockedBuildings(BuildingCategory buildingCategory);
+        int NumOfLockedUnits(UnitCategory unitCategory);
+    }
+}
+    
---
+++ Assets/Scripts/Data/LockedInformation.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Data.Models;
+using BattleCruisers.Data.Static;
+using BattleCruisers.Utils;
+using UnityEngine;
+
+namespace BattleCruisers.Data
+{
+    // FELIX  Test :D
+    public class LockedInformation : ILockedInformation
+    {
+        private readonly IGameModel _gameModel;
+        private readonly IStaticData _staticData;
+
+        public LockedInformation(IGameModel gameModel, IStaticData staticData)
+        {
+            Helper.AssertIsNotNull(gameModel, staticData);
+
+            _gameModel = gameModel;
+            _staticData = staticData;
+        }
+
+        public int NumOfLevelsUnlocked
+        {
+            get
+            {
+                return Mathf.Min(_gameModel.NumOfLevelsCompleted + 1, _staticData.Levels.Count);
+            }
+        }
+
+        public int NumOfLockedHulls
+        {
+            get
+            {
+                return _staticData.HullKeys.Count - _gameModel.UnlockedHulls.Count;
+            }
+        }
+
+        public int NumOfLockedBuildings(BuildingCategory buildingCategory)
+        {
+            int totalNumOfBuildings = NumOfBuildings(buildingCategory);
+            int numOfUnlockedBuildings = _gameModel.GetUnlockedBuildings(buildingCategory).Count;
+            return totalNumOfBuildings - numOfUnlockedBuildings;
+        }
+
+        private int NumOfBuildings(BuildingCategory buildingCategory)
+        {
+            return _staticData.BuildingKeys.Count(key => key.BuildingCategory == buildingCategory);
+        }
+
+        public int NumOfLockedUnits(UnitCategory unitCategory)
+        {
+            int totalNumOfUnits = NumOfUnits(unitCategory);
+            int numOfUnlockedUnits = _gameModel.GetUnlockedUnits(unitCategory).Count;
+            return totalNumOfUnits - numOfUnlockedUnits;
+        }
+
+        private int NumOfUnits(UnitCategory unitCategory)
+        {
+            return _staticData.UnitKeys.Count(key => key.UnitCategory == unitCategory);
+        }
+    }
+}
