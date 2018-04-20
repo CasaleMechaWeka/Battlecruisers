c5fe71910557654bec1edcc95402a164f6d6bf2d
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-19 22:13:36 -0700

    Create unlocked item DisabledState :)

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/States/DisabledState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.UnlockedItems.States
+{
+    public class DisabledState<TItem> : UnlockedItemState<TItem> where TItem : IComparableItem
+    {
+        protected override Color BackgroundColour { get { return BaseItem<TItem>.Colors.DEFAULT; } }
+
+        public DisabledState(UnlockedItem<TItem> item)
+            : base(item)
+        {
+        }
+
+        public override void HandleSelection()
+        {
+            // Disabled, so do nothing :)
+        }
+    }
+}
