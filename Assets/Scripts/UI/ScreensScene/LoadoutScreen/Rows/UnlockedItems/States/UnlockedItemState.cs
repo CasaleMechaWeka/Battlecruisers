92b24285215bc5c9980034bff400c49e402a5182
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-19 22:12:07 -0700

    Make constructor protected and add TODO :)

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/States/UnlockedItemState.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/UnlockedItems/States/UnlockedItemState.cs
@@ -2,13 +2,14 @@
 
 namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.UnlockedItems.States
 {
+    // FELIX  Avoid duplicate code with LoadoutItemState :)
     public abstract class UnlockedItemState<TItem> : IUnlockedItemState<TItem> where TItem : IComparableItem
 	{
 		protected readonly UnlockedItem<TItem> _item;
 
 		protected abstract Color BackgroundColour { get; }
 
-		public UnlockedItemState(UnlockedItem<TItem> item)
+        protected UnlockedItemState(UnlockedItem<TItem> item)
 		{
 			_item = item;
 			_item.backgroundImage.color = BackgroundColour;
