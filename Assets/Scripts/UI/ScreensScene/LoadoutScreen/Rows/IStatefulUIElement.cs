d5400c0c74cdc81eda586ecaed7ce4dff65bd17c
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-19 21:57:57 -0700

    Create IStatefulUIElement :D

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/IStatefulUIElement.cs
@@ -0,0 +1,20 @@
+﻿namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows
+{
+    public interface IStatefulUIElement
+    {
+        /// <summary>
+        /// The element is clickable but not highlighted.
+        /// </summary>
+        void GoToDefaultState();
+
+        /// <summary>
+        /// The element is highlighted and clickable.
+        /// </summary>
+        void GoToHighlightedState();
+
+        /// <summary>
+        /// The element is not clickable and not highlighted.
+        /// </summary>
+        void GoToDisabledState();
+    }
+}
