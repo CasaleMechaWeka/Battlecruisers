47759f5b5f55beb75805f4d6b38fd4e873002d8d
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-19 22:17:23 -0700

    Add TODO :)

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/IItemState.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/IItemState.cs
@@ -1,5 +1,6 @@
 ﻿namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows
 {
+    // FELIX  Create Items namespace?
 	public interface IItemState<TItem> where TItem : IComparableItem
 	{
 		void HandleSelection();
