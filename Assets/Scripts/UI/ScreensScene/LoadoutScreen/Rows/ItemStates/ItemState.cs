d7368312a84f468e75ab8f443dd87d7a82f469fe
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-20 07:54:44 -0700

    Add summary comment :)

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/ItemStates/ItemState.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/ItemStates/ItemState.cs
@@ -4,6 +4,11 @@ using UnityEngine.Assertions;
 
 namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.ItemStates
 {
+    /// <summary>
+    /// Handles:
+    /// 1. What should happen when the item is selected (clicked on).
+    /// 2. What background colour the item should be.
+    /// </summary>
     public abstract class ItemState<TItem> : IItemState<TItem> where TItem : IComparableItem
 	{
         protected readonly IItem<TItem> _item;
