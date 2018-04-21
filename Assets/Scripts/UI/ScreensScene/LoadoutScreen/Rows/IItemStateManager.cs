25acdb1e672958fa3c3fd9faba5c3a14e5939bcb
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-20 20:36:28 -0700

    Create ItemStateManager :)

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/IItemStateManager.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.UI.ScreensScene.LoadoutScreen.ItemDetails;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows
+{
+    public interface IItemStateManager
+    {
+        void AddItem(IStatefulUIElement itemToAdd, ItemType type);
+
+        void HandleDetailsManagerDismissed();
+        void HandleDetailsManagerReadyToCompare(ItemType comparingType);
+        void HandleDetailsManagerComparing();
+    }
+}
