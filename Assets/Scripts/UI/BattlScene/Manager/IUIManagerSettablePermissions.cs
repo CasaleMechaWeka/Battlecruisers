6946368cd00de4a963de44e14d0d42b0b1f6d94e
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-24 22:20:01 -0700

    Create UIManagerPermissions :)

---
+++ Assets/Scripts/UI/BattlScene/Manager/IUIManagerSettablePermissions.cs
@@ -0,0 +1,8 @@
+﻿namespace BattleCruisers.UI.BattleScene.Manager
+{
+    public interface IUIManagerSettablePermissions
+    {
+        bool CanShowItemDetails { set; }
+        bool CanDismissItemDetails { set; }
+    }
+}
