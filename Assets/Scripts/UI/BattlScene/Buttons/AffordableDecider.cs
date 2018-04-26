0f512b031de8f55d93c5b2080ace90d14524cb49
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-25 18:31:15 -0700

    Create normal button activeness decider :)

---
+++ Assets/Scripts/UI/BattlScene/Buttons/AffordableDecider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BattleCruisers.Buildables;
+using BattleCruisers.Cruisers.Drones;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene.Buttons
+{
+    /// <summary>
+    /// Deems a buildable button as enabled if it's buildable can be afforded
+    /// according to the drone manager's current number of drones.
+    /// </summary>
+    public class AffordableDecider : IBuildableButtonActivenessDecider
+    {
+        private readonly IDroneManager _droneManager;
+
+		public event EventHandler PotentialActivenessChange;
+
+        public AffordableDecider(IDroneManager droneManager)
+        {
+            Assert.IsNotNull(droneManager);
+
+            _droneManager = droneManager;
+            _droneManager.DroneNumChanged += _droneManager_DroneNumChanged;
+        }
+
+        private void _droneManager_DroneNumChanged(object sender, DroneNumChangedEventArgs e)
+        {
+            if (PotentialActivenessChange != null)
+            {
+                PotentialActivenessChange.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public bool ShouldBeEnabled(IBuildable buildable)
+        {
+            return _droneManager.CanSupportDroneConsumer(buildable.NumOfDronesRequired);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/BattlScene/Buttons/IBuildableButtonActivenessDecider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BattleCruisers.Buildables;
+
+namespace BattleCruisers.UI.BattleScene.Buttons
+{
+    public interface IBuildableButtonActivenessDecider
+    {
+        /// <summary>
+        /// Emitted when button activeness may have changed (eg, due to the 
+        /// drone manager's number of drones changing).
+        /// </summary>
+        event EventHandler PotentialActivenessChange;
+
+        bool ShouldBeEnabled(IBuildable buildable);
+    }
+}
