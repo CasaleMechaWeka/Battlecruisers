9d25f247947fc79b5528accea9ae8ee86fdb2ff4
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-26 22:08:29 -0700

    Create BuildingCategoryStaticDeciderTests :)

---
+++ Assets/Editor/Tests/UI/BattleScene/Buttons/BuildingCategoryStaticDeciderTests.cs
@@ -0,0 +1,23 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.UI.BattleScene.Buttons;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.UI.BattleScene.Buttons
+{
+    public class BuildingCategoryStaticDeciderTests
+    {
+        [Test]
+        public void ShouldBeEnabled_True()
+        {
+            IActivenessDecider<BuildingCategory> decider = new BuildingCategoryStaticDecider(shouldBeEnabled: true);
+            Assert.IsTrue(decider.ShouldBeEnabled(BuildingCategory.Ultra));
+        }
+
+        [Test]
+        public void ShouldBeEnabled_False()
+        {
+            IActivenessDecider<BuildingCategory> decider = new BuildingCategoryStaticDecider(shouldBeEnabled: false);
+            Assert.IsFalse(decider.ShouldBeEnabled(BuildingCategory.Ultra));
+        }
+    }
+}
