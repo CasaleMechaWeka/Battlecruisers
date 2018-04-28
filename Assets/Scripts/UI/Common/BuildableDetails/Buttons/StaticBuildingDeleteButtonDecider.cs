cda68227b96dca6136450ca8c15c74ad2a0cfe09
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-28 12:52:54 -0700

    Create static delete button decider

---
+++ Assets/Scripts/UI/Common/BuildableDetails/Buttons/StaticBuildingDeleteButtonDecider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.UI.BattleScene.Buttons;
+
+namespace BattleCruisers.UI.Common.BuildableDetails.Buttons
+{
+    // FELIX  Avoid duplicate code with other static decider :)
+    public class StaticBuildingDeleteButtonDecider: IActivenessDecider<IBuilding>
+    {
+        private readonly bool _shouldBeEnabled;
+
+#pragma warning disable 67  // Unused event
+        public event EventHandler PotentialActivenessChange;
+#pragma warning restore 67  // Unused event
+
+        public StaticBuildingDeleteButtonDecider(bool shouldBeEnabled)
+        {
+            _shouldBeEnabled = shouldBeEnabled;
+        }
+
+        public bool ShouldBeEnabled(IBuilding building)
+        {
+            return _shouldBeEnabled;
+        }
+    }
+}
