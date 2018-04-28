21d09ec3ece7df41183292b26771f3ac4301a5bb
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-28 12:08:09 -0700

    Add TODO :)

--- Assets/Scripts/UI/BattlScene/Buttons/IActivenessDecider.cs
+++ Assets/Scripts/UI/BattlScene/Buttons/IActivenessDecider.cs
@@ -2,6 +2,7 @@
 
 namespace BattleCruisers.UI.BattleScene.Buttons
 {
+    // FELIX  Used in two places.  Move up in namespace until both uses are in children namespaces.
     public interface IActivenessDecider<TElement>
     {
         /// <summary>
