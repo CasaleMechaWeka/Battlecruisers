06cfde917cfcb05841defca4c2318e8f0ace56a8
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-28 12:47:45 -0700

    Create delete button activeness decider :)

---
+++ Assets/Scripts/UI/Common/BuildableDetails/Buttons/NormalBuildingDeleteButtonDecider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Cruisers;
+using BattleCruisers.UI.BattleScene.Buttons;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Common.BuildableDetails.Buttons
+{
+    public class NormalBuildingDeleteButtonDecider : IActivenessDecider<IBuilding>
+    {
+        private readonly ICruiser _playerCruiser;
+
+        #pragma warning disable 67  // Unused event
+        public event EventHandler PotentialActivenessChange;
+        #pragma warning restore 67  // Unused event
+
+        public NormalBuildingDeleteButtonDecider(ICruiser playerCruiser)
+        {
+            Assert.IsNotNull(playerCruiser);
+            _playerCruiser = playerCruiser;
+        }
+
+        public bool ShouldBeEnabled(IBuilding building)
+        {
+            return ReferenceEquals(_playerCruiser, building.ParentCruiser);
+        }
+    }
+}
