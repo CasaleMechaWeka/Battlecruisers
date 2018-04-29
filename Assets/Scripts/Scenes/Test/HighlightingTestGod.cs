b6046d4a0dcd289ceaf5aa8d112e36b63a1d6263
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-28 17:23:37 -0700

    Create highlighting test scene.  Currently camera simply moves, no highlighting yet :P

---
+++ Assets/Scripts/Scenes/Test/HighlightingTestGod.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test
+{
+    public class HighlightingTestGod : MonoBehaviour
+    {
+        public Camera camera;
+
+        void Start()
+        {
+
+        }
+
+        // Move camera.  Highlights should stay in place correctly on both
+        // the in game and on canvas objects.
+        void Update()
+        {
+            Vector3 currentPosition = camera.transform.position;
+            float xPosition = currentPosition.x + Time.deltaTime;
+            camera.transform.position = new Vector3(xPosition, currentPosition.y, currentPosition.z);
+        }
+    }
+}
