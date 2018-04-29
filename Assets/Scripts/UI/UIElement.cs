a37a8ef96fd28a6fef0ad8f446cb8d3e27f85569
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-29 12:44:26 -0700

    Create UIElement, which implements IHighlightable :)

---
+++ Assets/Scripts/UI/UIElement.cs
@@ -0,0 +1,20 @@
+﻿using BattleCruisers.Tutorial.Highlighting;
+using BattleCruisers.Utils;
+using UnityEngine;
+
+namespace BattleCruisers.UI
+{
+    public class UIElement : MonoBehaviour, IHighlightable
+    {
+        private RectTransform _rectTransform;
+
+        public Transform Transform { get { return transform; } }
+        public Vector2 Size { get { return _rectTransform.sizeDelta; } }
+        public HighlightableType Type { get { return HighlightableType.OnCanvas; } }
+
+        public void Initialise()
+        {
+            _rectTransform = transform.Parse<RectTransform>();
+        }
+    }
+}
