e6bc466c1117de218aace02e11598522798acecf
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-29 13:19:43 -0700

    Create ITutorialStep and TutorialStepConsumer :)

---
+++ Assets/Scripts/Tutorial/ITutorialStep.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BattleCruisers.Tutorial
+{
+    public interface ITutorialStep
+    {
+        void Start(Action completionCallback);
+    }
+}
---
+++ Assets/Scripts/Tutorial/TutorialStepConsumer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial
+{
+    public class TutorialStepConsumer
+    {
+        private readonly Queue<ITutorialStep> _tutorialSteps;
+
+        public TutorialStepConsumer(Queue<ITutorialStep> tutorialSteps)
+        {
+            Assert.IsNotNull(tutorialSteps);
+            _tutorialSteps = tutorialSteps;
+
+            StartNextTask();
+        }
+
+        private void StartNextTask()
+        {
+            if (_tutorialSteps.Count != 0)
+            {
+                _tutorialSteps.Dequeue().Start(StartNextTask);
+            }
+        }
+    }
+}
