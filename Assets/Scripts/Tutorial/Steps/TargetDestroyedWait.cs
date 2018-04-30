48f2b5264ee368d5127778799445d0be664e75fb
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-29 17:49:07 -0700

    Craete TargetDestroyedWait :)

---
+++ Assets/Scripts/Tutorial/Steps/TargetDestroyedWait.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BattleCruisers.Buildables;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps
+{
+    /// <summary>
+    /// Completed when the target is destroyed.
+    /// </summary>
+    // FELIX  Test :D
+    public class TargetDestroyedWait : TutorialStep
+    {
+        private readonly ITarget _target;
+
+        public TargetDestroyedWait(ITutorialStepArgs args, ITarget target)
+            : base(args)
+        {
+            Assert.IsNotNull(target);
+            _target = target;
+        }
+
+        public override void Start(Action completionCallback)
+        {
+            base.Start(completionCallback);
+            _target.Destroyed += _target_Destroyed;
+        }
+
+        private void _target_Destroyed(object sender, DestroyedEventArgs e)
+        {
+            _target.Destroyed -= _target_Destroyed;
+            base.OnCompleted();
+        }
+    }
+}
