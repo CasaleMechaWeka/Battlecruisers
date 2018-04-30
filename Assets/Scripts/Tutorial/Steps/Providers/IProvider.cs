5daa1af6c3667d6bfc37ec7a1c8424734b6c5b36
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-29 18:05:31 -0700

    Make IProvider interface generic, so it will work for both IBuildables and ITargets :)

---
+++ Assets/Scripts/Tutorial/Steps/Providers/IProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleCruisers.Tutorial.Steps.Providers
+{
+    /// <summary>
+    /// Allows a reference to the provider to be used BEFORE the actual item
+    /// the provider will return is available.
+    /// 
+    /// Ie, the LastFriendlyBuildingStartedProvider would return the last
+    /// building started by the player.
+    /// </summary>
+    public interface IProvider<T>
+    {
+        T Item { get; }
+    }
+}
