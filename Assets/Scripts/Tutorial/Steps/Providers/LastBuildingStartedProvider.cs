e2a4f365e3244eb77b6aab520e35dca2b016a0c1
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-30 17:17:50 -0700

    Create LastBuildingStartedProvider :)

---
+++ Assets/Scripts/Tutorial/Steps/Providers/LastBuildingStartedProvider.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Cruisers;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.Providers
+{
+    // FELIX  Test
+    public class LastBuildingStartedProvider : IProvider<IBuildable>
+    {
+        private readonly ICruiserController _cruiser;
+        private IBuildable _lastBuildingStarted;
+
+        public LastBuildingStartedProvider(ICruiserController cruiser)
+        {
+            Assert.IsNotNull(cruiser);
+            _cruiser = cruiser;
+
+            _cruiser.StartedConstruction += _cruiser_StartedConstruction;
+        }
+
+        private void _cruiser_StartedConstruction(object sender, StartedConstructionEventArgs e)
+        {
+            _lastBuildingStarted = e.Buildable;
+        }
+
+        public IBuildable FindItem()
+        {
+            return _lastBuildingStarted;
+        }
+    }
+}
