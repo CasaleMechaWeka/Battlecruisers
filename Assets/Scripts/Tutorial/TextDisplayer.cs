6c2b88ea068f83da79c47e17c9ce5e7bbdbd2aa9
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-30 19:57:49 -0700

    Create (very) basic TextDisplayer for tutorial :)

---
+++ Assets/Scripts/Tutorial/TextDisplayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BattleCruisers.Tutorial.Steps
+{
+    public class TextDisplayer : MonoBehaviour, ITextDisplayer
+    {
+        private Text _text;
+
+        public void Initialise()
+        {
+            _text = GetComponent<Text>();
+            Assert.IsNotNull(_text);
+            _text.text = string.Empty;
+        }
+
+        public void DisplayText(string textToDisplay)
+        {
+            _text.text = textToDisplay;
+        }
+    }
+}
