7814acbef8543eef0e0fff6a4d0cac5a9d925150
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-04-30 21:00:49 -0700

    Create TutorialManager :D

---
+++ Assets/Scripts/Tutorial/ITutorialManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BattleCruisers.Tutorial
+{
+    public interface ITutorialManager
+    {
+        event EventHandler TutorialCompleted;
+
+        void StartTutorial();
+    }
+}
---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Tutorial.Highlighting;
+using BattleCruisers.Tutorial.Steps;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Tutorial
+{
+    public class TutorialManager : ITutorialManager
+    {
+        private readonly ITutorialStepConsumer _consumer;
+
+        public event EventHandler TutorialCompleted;
+
+        public TutorialManager(
+            ITextDisplayer textDisplayer,
+            ICruiser playerCruiser)
+        {
+            Helper.AssertIsNotNull(textDisplayer, playerCruiser);
+
+            IHighlighter highlighter = new Highlighter(new HighlightFactory());
+
+            ITutorialStepsFactory stepsFactory = new TutorialStepsFactory(highlighter, textDisplayer, playerCruiser);
+            Queue<ITutorialStep> steps = stepsFactory.CreateTutorialSteps();
+            _consumer = new TutorialStepConsumer(steps);
+
+            _consumer.Completed += _consumer_Completed;
+        }
+
+        public void StartTutorial()
+        {
+            _consumer.StartConsuming();
+        }
+
+        private void _consumer_Completed(object sender, EventArgs e)
+        {
+            _consumer.Completed -= _consumer_Completed;
+
+            if (TutorialCompleted != null)
+            {
+                TutorialCompleted.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
