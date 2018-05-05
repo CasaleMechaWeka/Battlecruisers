a4dfebf6781256bd166e4b98f3a176f7bb6b0704
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-04 19:18:48 -0700

    Create BuildingButtonStep :)

---
+++ Assets/Scripts/Tutorial/Steps/ClickSteps/BuildingButtonStep.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.UI.BattleScene.Buttons;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.ClickSteps
+{
+    // FELIX  Test :)
+    public class BuildingButtonStep : ClickStep
+    {
+        private readonly IBuildingPermitter _permitter;
+        private readonly IPrefabKey _buildingToAllow;
+
+        public BuildingButtonStep(
+            ITutorialStepArgs args, 
+            IBuildableButton buttonToHighlight,
+            IBuildingPermitter permitter,
+            IPrefabKey buildingToAllow) 
+            : base(args, buttonToHighlight)
+        {
+            Assert.IsNotNull(permitter);
+
+            _permitter = permitter;
+            _buildingToAllow = buildingToAllow;
+        }
+
+		public override void Start(Action completionCallback)
+		{
+            base.Start(completionCallback);
+
+            _permitter.PermittedBuilding = _buildingToAllow;
+		}
+
+		protected override void OnCompleted()
+		{
+            _permitter.PermittedBuilding = null;
+
+			base.OnCompleted();
+		}
+	}
+}
