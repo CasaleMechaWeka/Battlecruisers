b75eca744352f1103bc23698e94761afa6534018
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-04 21:47:17 -0700

    Create two single deciders :D

---
+++ Assets/Scripts/UI/BattlScene/Buttons/ActivenessDeciders/SingleBuildableButtonDecider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Utils;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene.Buttons.ActivenessDeciders
+{
+    public class SingleBuildableButtonDecider : IActivenessDecider
+    {
+        private readonly IBuildable _buildable;
+        private readonly IActivenessDecider<IBuildable> _generalDecider;
+
+        public bool ShouldBeEnabled { get { return _generalDecider.ShouldBeEnabled(_buildable); } }
+
+        public event EventHandler PotentialActivenessChange
+        {
+            add { _generalDecider.PotentialActivenessChange += value; }
+            remove { _generalDecider.PotentialActivenessChange -= value; }
+        }
+
+        public SingleBuildableButtonDecider(IBuildable buildable, IActivenessDecider<IBuildable> generalDecider)
+        {
+            Helper.AssertIsNotNull(buildable, generalDecider);
+
+            _buildable = buildable;
+            _generalDecider = generalDecider;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/BattlScene/Buttons/ActivenessDeciders/SingleBuildingCategoryButtonDecider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BattleCruisers.Buildables.Buildings;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene.Buttons.ActivenessDeciders
+{
+    public class SingleBuildingCategoryButtonDecider : IActivenessDecider
+    {
+        private readonly BuildingCategory _category;
+        private readonly IActivenessDecider<BuildingCategory> _generalDecider;
+
+        public bool ShouldBeEnabled { get { return _generalDecider.ShouldBeEnabled(_category); } }
+
+        public event EventHandler PotentialActivenessChange
+        {
+            add { _generalDecider.PotentialActivenessChange += value; }
+            remove { _generalDecider.PotentialActivenessChange -= value; }
+        }
+
+        public SingleBuildingCategoryButtonDecider(BuildingCategory category, IActivenessDecider<BuildingCategory> generalDecider)
+        {
+            Assert.IsNotNull(generalDecider);
+
+            _category = category;
+            _generalDecider = generalDecider;
+        }
+    }
+}
