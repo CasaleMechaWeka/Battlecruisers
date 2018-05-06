5c6ccaaef28740a91ca573913f8f80b793c54517
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-06 15:17:32 -0700

    Create LastBuildingStartedHighlightableProvider :)

---
+++ Assets/Scripts/Tutorial/Highlighting/Providers/LastBuildingStartedHighlightableProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Buildables;
+using BattleCruisers.Tutorial.Steps.Providers;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Highlighting.Providers
+{
+    public class LastBuildingStartedHighlightableProvider : IHighlightablesProvider
+    {
+        private readonly IProvider<IBuildable> _lastBuildingStartedProvider;
+
+        public LastBuildingStartedHighlightableProvider(IProvider<IBuildable> lastBuildingStartedProvider)
+        {
+            Assert.IsNotNull(lastBuildingStartedProvider);
+            _lastBuildingStartedProvider = lastBuildingStartedProvider;
+        }
+
+        public IList<IHighlightable> FindHighlightables()
+        {
+            IHighlightable lastBuildingStarted = _lastBuildingStartedProvider.FindItem();
+            Assert.IsNotNull(lastBuildingStarted);
+
+            return new List<IHighlightable>()
+            {
+                lastBuildingStarted
+            };
+        }
+    }
+}
