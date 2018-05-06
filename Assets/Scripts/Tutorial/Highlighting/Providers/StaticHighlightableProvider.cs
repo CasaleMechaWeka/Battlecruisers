8c180884bab1dfae82e29fcc743c8db102ff9aff
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-06 10:52:56 -0700

    Create StaticHighlightableProvider :)

---
+++ Assets/Scripts/Tutorial/Highlighting/Providers/StaticHighlightableProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Highlighting.Providers
+{
+    public class StaticHighlightableProvider : IHighlightablesProvider
+    {
+        private readonly IList<IHighlightable> _highlightables;
+
+        public StaticHighlightableProvider(params IHighlightable[] highlightables)
+        {
+            Assert.IsNotNull(highlightables);
+            Assert.IsTrue(highlightables.Length > 0);
+
+            _highlightables = highlightables;
+        }
+
+        public IList<IHighlightable> FindHighlightables()
+        {
+            return _highlightables;
+        }
+    }
+}
