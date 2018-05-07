d5a5e74bd18d2a8bdf69131ac9d5aaf306e4d35c
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-06 21:45:59 -0700

    Create DelayWaitStepTests :)

---
+++ Assets/Editor/Tests/Tutorial/Steps/DelayWaitStepTests.cs
@@ -0,0 +1,33 @@
+﻿using BattleCruisers.Tutorial.Steps;
+using BattleCruisers.Utils.Threading;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+
+namespace BattleCruisers.Tests.Tutorial.Steps
+{
+    public class DelayWaitStepTests : TutorialStepTestsBase
+    {
+        private ITutorialStep _tutorialStep;
+        private IVariableDelayDeferrer _deferrer;
+        private float _waitTimeInS;
+
+        [SetUp]
+        public override void SetuUp()
+        {
+            base.SetuUp();
+
+            _deferrer = Substitute.For<IVariableDelayDeferrer>();
+            _waitTimeInS = 12;
+            _tutorialStep = new DelayWaitStep(_args, _deferrer, _waitTimeInS);
+        }
+
+        [Test]
+        public void Start_DefersCompletionCallback()
+        {
+            _tutorialStep.Start(_completionCallback);
+            // Protected base class callback is passed
+            _deferrer.Received().Defer(Arg.Is<Action>(callback => true), _waitTimeInS);
+        }
+    }
+}
