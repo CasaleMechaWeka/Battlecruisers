32b196668b3340827954c337ddd345ea7987687b
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-06 19:17:41 -0700

    Remove TODO :)

--- Assets/Scripts/Tutorial/Steps/Providers/IProvider.cs
+++ Assets/Scripts/Tutorial/Steps/Providers/IProvider.cs
@@ -7,7 +7,6 @@
     /// Ie, the LastFriendlyBuildingStartedProvider would return the last
     /// building started by the player.
     /// </summary>
-    /// FELIX  Only used for IBuildable :/  Just make IBuildableProvider interface?
     public interface IProvider<T>
     {
         T FindItem();
