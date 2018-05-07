cc75f73feaa93a76b33c7d71f257f4ebc7de9815
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-06 20:43:20 -0700

    Remove completed TODO :)

--- Assets/Scripts/UI/IFilter.cs
+++ Assets/Scripts/UI/IFilter.cs
@@ -4,7 +4,6 @@ namespace BattleCruisers.UI
 {
     public interface IFilter
     {
-        // FELIX  Rename all event handlers :P
         event EventHandler PotentialMatchChange;
 
         // FELIX  Change to parameterless method, to indicate that some work may be involved (ie, not just returning stored value)
