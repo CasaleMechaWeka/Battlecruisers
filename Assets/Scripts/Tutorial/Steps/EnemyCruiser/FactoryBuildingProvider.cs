ee0c3920e84f3a5a77fb9cf2aeb6bcb30d1e18b6
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-10 18:18:39 -0700

    Rename factory provide to avoid clash with other factory provider :P

---
+++ Assets/Scripts/Tutorial/Steps/EnemyCruiser/FactoryBuildingProvider.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Tutorial.Providers;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.EnemyCruiser
+{
+    public class FactoryBuildingProvider : IProvider<IFactory>
+    {
+        private readonly IProvider<IBuildable> _buildableProvider;
+
+        public FactoryBuildingProvider(IProvider<IBuildable> buildingProvider)
+        {
+            Assert.IsNotNull(buildingProvider);
+            _buildableProvider = buildingProvider;
+        }
+
+        public IFactory FindItem()
+        {
+            IFactory factory = _buildableProvider.FindItem() as IFactory;
+            Assert.IsNotNull(factory);
+            return factory;
+        }
+    }
+}
