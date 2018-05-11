74f36df08738b6ef3c8d24246134404a57fc2ce2
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-10 17:39:02 -0700

    Create StopUnitConstructionStep :)

---
+++ Assets/Scripts/Tutorial/Steps/EnemyCruiser/StopUnitConstructionStep.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Tutorial.Providers;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.EnemyCruiser
+{
+    // FELIX  Write tests
+    public class StopUnitConstructionStep : TutorialStep
+    {
+        private readonly IProvider<IFactory> _factoryProvider;
+
+        public StopUnitConstructionStep(ITutorialStepArgs args, IProvider<IFactory> factoryProvider)
+            : base(args)
+        {
+            Assert.IsNotNull(factoryProvider);
+            _factoryProvider = factoryProvider;
+        }
+
+        public override void Start(Action completionCallback)
+        {
+            base.Start(completionCallback);
+
+            _factoryProvider.FindItem().UnitWrapper = null;
+        }
+    }
+}
