1d129bdb9cb9ff9fa85cf87f9bce75ef59b22cd5
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-10 17:28:59 -0700

    Create FactoryProvider :)

---
+++ Assets/Scripts/Tutorial/Steps/EnemyCruiser/FactoryProvider.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Tutorial.Providers;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.EnemyCruiser
+{
+    public class FactoryProvider : IProvider<IFactory>
+    {
+        private readonly IProvider<IBuilding> _buildingProvider;
+
+        public FactoryProvider(IProvider<IBuilding> buildingProvider)
+        {
+            Assert.IsNotNull(buildingProvider);
+            _buildingProvider = buildingProvider;
+        }
+
+        public IFactory FindItem()
+        {
+            IFactory factory = _buildingProvider.FindItem() as IFactory;
+            Assert.IsNotNull(factory);
+            return factory;
+        }
+    }
+}
