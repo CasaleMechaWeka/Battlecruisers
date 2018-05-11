df24abd40242083db7c2443f504dc687d7681f9c
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-10 21:31:02 -0700

    Create BuildableToTargetProvider :)

---
+++ Assets/Scripts/Tutorial/Steps/Providers/BuildableToTargetProvider.cs
@@ -0,0 +1,22 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Tutorial.Providers;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.Providers
+{
+    public class BuildableToTargetProvider : IProvider<ITarget>
+    {
+        private readonly IProvider<IBuildable> _buildableProvider;
+
+        public BuildableToTargetProvider(IProvider<IBuildable> buildableProvider)
+        {
+            Assert.IsNotNull(buildableProvider);
+            _buildableProvider = buildableProvider;
+        }
+
+        public ITarget FindItem()
+        {
+            return _buildableProvider.FindItem();
+        }
+    }
+}
