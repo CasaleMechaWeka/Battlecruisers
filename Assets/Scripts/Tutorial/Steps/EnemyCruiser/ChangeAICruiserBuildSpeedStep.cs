34192d6e00dbb2c448a0a9115c6c5f5996870997
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-10 17:44:38 -0700

    Create ChangeAICruiserBuildSpeedStep :)

---
+++ Assets/Scripts/Tutorial/Steps/EnemyCruiser/ChangeAICruiserBuildSpeedStep.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BattleCruisers.Buildables.BuildProgress;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.EnemyCruiser
+{
+    // FELIX  Write tests
+    public class ChangeAICruiserBuildSpeedStep : TutorialStep
+    {
+        private readonly IBuildSpeedController _buildSpeedController;
+        private readonly BuildSpeed _buildSpeed;
+
+        public ChangeAICruiserBuildSpeedStep(ITutorialStepArgs args, IBuildSpeedController buildSpeedController, BuildSpeed buildSpeed)
+            : base(args)
+        {
+            Assert.IsNotNull(buildSpeedController);
+
+            _buildSpeedController = buildSpeedController;
+            _buildSpeed = buildSpeed;
+        }
+
+        public override void Start(Action completionCallback)
+        {
+            base.Start(completionCallback);
+
+            _buildSpeedController.BuildSpeed = _buildSpeed;
+        }
+    }
+}
