0ffd4854c17da0bc98fbcb28f437e61015df632f
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-12 13:12:27 -0700

    Create NavigationPermitterStep :)

---
+++ Assets/Scripts/Tutorial/Steps/NavigationPermitterStep.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BattleCruisers.UI;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.EnemyCruiser
+{
+    // FELIX  Test
+    public class NavigationPermitterStep : TutorialStep
+    {
+        private readonly BasicFilter _navigationPermitter;
+        private readonly bool _isNavigationEnabled;
+
+        public NavigationPermitterStep(
+            ITutorialStepArgs args, 
+            BasicFilter navigationPermitter,
+            bool isNavigationEnabled)
+            : base(args)
+        {
+            Assert.IsNotNull(navigationPermitter);
+
+            _navigationPermitter = navigationPermitter;
+            _isNavigationEnabled = isNavigationEnabled;
+        }
+
+        public override void Start(Action completionCallback)
+        {
+            base.Start(completionCallback);
+
+            _navigationPermitter.IsMatch = _isNavigationEnabled;
+
+            OnCompleted();
+        }
+    }
+}
