7ca5615085dd1434e60ffbb12cfa0270930d895d
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-11 18:06:49 -0700

    Move FactoryStepsResult to own file :)

---
+++ Assets/Scripts/Tutorial/FactoryStepsResult.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Tutorial.Providers;
+using BattleCruisers.Tutorial.Steps;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Tutorial
+{
+    public class FactoryStepsResult
+    {
+        public IList<ITutorialStep> Steps { get; private set; }
+        public IProvider<IFactory> FactoryProvider { get; private set; }
+
+        public FactoryStepsResult(IList<ITutorialStep> steps, IProvider<IFactory> factoryProvider)
+        {
+            Helper.AssertIsNotNull(steps, factoryProvider);
+
+            Steps = steps;
+            FactoryProvider = factoryProvider;
+        }
+    }
+}
--- Assets/Scripts/Tutorial/TutorialStepsFactory.cs
+++ Assets/Scripts/Tutorial/TutorialStepsFactory.cs
@@ -397,19 +397,4 @@ namespace BattleCruisers.Tutorial
                     buildSpeed);
         }
     }
-
-    // FELIX  move to own file?
-    public class FactoryStepsResult
-    {
-        public IList<ITutorialStep> Steps { get; private set; }
-        public IProvider<IFactory> FactoryProvider { get; private set; }
-
-        public FactoryStepsResult(IList<ITutorialStep> steps, IProvider<IFactory> factoryProvider)
-        {
-            Helper.AssertIsNotNull(steps, factoryProvider);
-
-            Steps = steps;
-            FactoryProvider = factoryProvider;
-        }
-    }
 }
