4ef2a75946330867d780f5bf01cb01689a2699cf
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-11 18:33:41 -0700

    Remove unused import :P

--- Assets/Scripts/Tutorial/Steps/Providers/ISingleBuildableProvider.cs
+++ Assets/Scripts/Tutorial/Steps/Providers/ISingleBuildableProvider.cs
@@ -1,7 +1,6 @@
 ﻿using BattleCruisers.Buildables;
 using BattleCruisers.Tutorial.Highlighting;
 using BattleCruisers.Tutorial.Providers;
-using BattleCruisers.Tutorial.Steps.ClickSteps;
 
 namespace BattleCruisers.Tutorial.Steps.Providers
 {
