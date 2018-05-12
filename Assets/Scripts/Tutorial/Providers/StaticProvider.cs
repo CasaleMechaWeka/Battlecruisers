3ccf69021ea97721516918f96d788a6a26432fff
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-12 13:12:02 -0700

    Create StaticProvider :)

---
+++ Assets/Scripts/Tutorial/Providers/StaticProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace BattleCruisers.Tutorial.Providers
+{
+    public class StaticProvider<TItem> : IProvider<TItem>
+    {
+        private readonly TItem _item;
+
+        public StaticProvider(TItem item)
+        {
+            _item = item;
+        }
+
+        public TItem FindItem()
+        {
+            return _item;
+        }
+    }
+}
