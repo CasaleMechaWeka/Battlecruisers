5b5a59331ead7ceb0f6844ca239f6ee69323f037
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-13 14:07:36 -0700

    Create SlotProvider, finally :P

---
+++ Assets/Scripts/Tutorial/Steps/Providers/ISlotsProvider.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.Tutorial.Highlighting;
+using BattleCruisers.Tutorial.Providers;
+
+namespace BattleCruisers.Tutorial.Steps.Providers
+{
+    public interface ISlotsProvider :
+        IListProvider<ISlot>,
+        IListProvider<IHighlightable>,
+        IListProvider<IClickableEmitter>
+    {
+    }
+}
---
+++ Assets/Scripts/Tutorial/Steps/Providers/SlotsProvider.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.Tutorial.Highlighting;
+using BattleCruisers.Tutorial.Providers;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.Providers
+{
+    public class SlotsProvider : ISlotsProvider
+    {
+        private readonly ISlotWrapper _slotWrapper;
+        private readonly SlotType _slotType;
+        private readonly bool _preferFrontmostSlot;
+
+        private IList<ISlot> _slots;
+
+        public SlotsProvider(ISlotWrapper slotWrapper, SlotType slotType, bool preferFrontmostSlot)
+        {
+            Assert.IsNotNull(slotWrapper);
+
+            _slotWrapper = slotWrapper;
+            _slotType = slotType;
+            _preferFrontmostSlot = preferFrontmostSlot;
+        }
+
+        IList<ISlot> IListProvider<ISlot>.FindItems()
+        {
+            if (_slots == null)
+            {
+                if (_preferFrontmostSlot)
+                {
+                    ISlot frontMostSlot = _slotWrapper.GetFreeSlot(_slotType, _preferFrontmostSlot);
+                    Assert.IsNotNull(frontMostSlot);
+
+                    _slots = new List<ISlot>()
+                    {
+                        frontMostSlot
+                    };
+                }
+                else
+                {
+                    _slots = _slotWrapper.GetSlotsForType(_slotType);
+                }
+            }
+
+            return _slots;
+        }
+
+        public IList<IHighlightable> FindItems()
+        {
+            return _slots.Cast<IHighlightable>().ToList();
+        }
+
+        IList<IClickableEmitter> IListProvider<IClickableEmitter>.FindItems()
+        {
+            return _slots.Cast<IClickableEmitter>().ToList();
+        }
+    }
+}
