d158800eaabe11bb6e96192310d7ad4396fa664c
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-13 11:50:33 -0700

    Create NavigationPermitterStepTests :)

---
+++ Assets/Editor/Tests/Tutorial/Steps/NavigationPermitterStepTests.cs
@@ -0,0 +1,33 @@
+﻿using BattleCruisers.Tutorial.Steps;
+using BattleCruisers.Tutorial.Steps.EnemyCruiser;
+using BattleCruisers.UI;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.Tutorial.Steps
+{
+    public class NavigationPermitterStepTests : TutorialStepTestsBase
+    {
+        private ITutorialStep _tutorialStep;
+        private BasicFilter _navigationPermitter;
+        private bool _isNavigationEnabled;
+
+        [SetUp]
+        public override void SetuUp()
+        {
+            base.SetuUp();
+
+            _isNavigationEnabled = true;
+            _navigationPermitter = new BasicFilter(!_isNavigationEnabled);
+            _tutorialStep = new NavigationPermitterStep(_args, _navigationPermitter, _isNavigationEnabled);
+        }
+
+        [Test]
+        public void Start_SetNavigationPermission_AndCompletes()
+        {
+            _tutorialStep.Start(_completionCallback);
+            _navigationPermitter.Received().IsMatch = _isNavigationEnabled;
+            Assert.AreEqual(1, _callbackCounter);
+        }
+    }
+}
--- Assets/Scripts/Tutorial/Steps/NavigationPermitterStep.cs
+++ Assets/Scripts/Tutorial/Steps/NavigationPermitterStep.cs
@@ -4,7 +4,6 @@ using UnityEngine.Assertions;
 
 namespace BattleCruisers.Tutorial.Steps.EnemyCruiser
 {
-    // FELIX  Test
     public class NavigationPermitterStep : TutorialStep
     {
         private readonly BasicFilter _navigationPermitter;
