33cdab2869edd61c34916d41c873cad28d4a6c04
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-16 07:31:56 -0700

    Create Rectangle.  Should replace SafeZone :)

---
+++ Assets/Scripts/Utils/DataStructures/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.DataStrctures
+{
+    public class Rectangle
+    {
+        public float MinX { get; private set; }
+        public float MaxX { get; private set; }
+        public float MinY { get; private set; }
+        public float MaxY { get; private set; }
+
+		public Rectangle(float minX, float maxX, float minY, float maxY)
+        {
+            Assert.IsTrue(minX < maxX);
+            Assert.IsTrue(minY < maxY);
+
+            MinX = minX;
+            MaxX = maxX;
+            MinY = minY;
+            MaxY = maxY;
+        }
+    }
+}
