fe47966a7ae3804cdd73722aa5a5a26cf54ac8d7
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-16 19:49:29 -0700

    Create MouseZoomHandlerTests :)

---
+++ Assets/Editor/Tests/UI/Cameras/InputHandlers/MouseZoomHandlerTests.cs
@@ -0,0 +1,64 @@
+﻿using BattleCruisers.Data.Settings;
+using BattleCruisers.UI.Cameras;
+using BattleCruisers.UI.Cameras.InputHandlers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.UI.Cameras.InputHandlers
+{
+	public class MouseZoomHandlerTests
+    {
+		private IMouseZoomHandler _zoomHandler;
+		private ISettingsManager _settingsManager;
+		private float _timeDelta;
+
+        [SetUp]
+        public void SetuUp()
+        {
+			_settingsManager = Substitute.For<ISettingsManager>();
+			_settingsManager.ZoomSpeed.Returns(0.5f);
+
+			_zoomHandler = new MouseZoomHandler(_settingsManager, CameraCalculator.MIN_CAMERA_ORTHOGRAPHIC_SIZE, CameraCalculator.MAX_CAMERA_ORTHOGRAPHIC_SIZE);
+
+			// * ZOOM_SPEED_MULTIPLIER = 1 :)
+			_timeDelta = 0.03333333f;
+        }
+
+        [Test]
+		public void FindCameraOrthographicSize_NonZeroScroll()
+        {
+			float currentSize = 10;
+			float yScroll = -5;
+			float expectedSize = currentSize - _settingsManager.ZoomSpeed * yScroll;
+
+			Assert.AreEqual(expectedSize, _zoomHandler.FindCameraOrthographicSize(currentSize, yScroll, _timeDelta));
+        }
+
+        [Test]
+        public void FindCameraOrthographicSize_NonZeroScroll_TooBigOrthographicSize_Clamped()
+        {
+			float currentSize = CameraCalculator.MAX_CAMERA_ORTHOGRAPHIC_SIZE;
+			float yScroll = -5;
+            
+			Assert.AreEqual(CameraCalculator.MAX_CAMERA_ORTHOGRAPHIC_SIZE, _zoomHandler.FindCameraOrthographicSize(currentSize, yScroll, _timeDelta));
+        }
+
+        [Test]
+        public void FindCameraOrthographicSize_NonZeroScroll_TooSmallOrthographicSize_Clamped()
+        {
+			float currentSize = CameraCalculator.MIN_CAMERA_ORTHOGRAPHIC_SIZE;
+            float yScroll = 5;
+
+            Assert.AreEqual(CameraCalculator.MIN_CAMERA_ORTHOGRAPHIC_SIZE, _zoomHandler.FindCameraOrthographicSize(currentSize, yScroll, _timeDelta));
+        }
+
+        [Test]
+        public void FindCameraOrthographicSize_ZeroScroll()
+        {
+			float currentSize = 10;
+            float yScroll = 0;
+
+            Assert.AreEqual(currentSize, _zoomHandler.FindCameraOrthographicSize(currentSize, yScroll, _timeDelta));
+        }
+    }
+}
