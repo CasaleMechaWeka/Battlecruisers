366ae0e0206deeec7962c5618d0f42d34654b49c
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-17 08:49:09 -0700

    Add ICameraTransitionManager :P

---
+++ Assets/Scripts/UI/Cameras/ICameraTransitionManager.cs
@@ -0,0 +1,13 @@
+﻿namespace BattleCruisers.UI.Cameras
+{
+	public interface ICameraTransitionManager
+	{
+		CameraTarget Target { set; }
+
+        /// <summary>
+        /// Should be called every frame to smoothly move the camera to the 
+		/// target position.
+        /// </summary>
+		void MoveCamera();
+	}
+}
