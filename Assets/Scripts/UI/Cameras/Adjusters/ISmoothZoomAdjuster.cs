c406b73d460c9e25373c48ec8122090808205162
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-16 20:46:13 -0700

    Create ISmoothZoomAdjuster :)

---
+++ Assets/Scripts/UI/Cameras/Adjusters/ISmoothZoomAdjuster.cs
@@ -0,0 +1,11 @@
+﻿namespace BattleCruisers.UI.Cameras.Adjusters
+{
+	public interface ISmoothZoomAdjuster
+	{
+        /// <returns>
+		/// <c>true</c>, if the zoom level is the same as the given 
+		/// <paramref name="targetOrthographicSize"/>, <c>false</c> otherwise.
+		/// </returns>
+		bool AdjustZoom(float targetOrthographicSize);
+	}
+}
