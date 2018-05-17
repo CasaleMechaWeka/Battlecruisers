0631dd685f53ae4c515ccce22cf877bb59c79078
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-17 08:03:09 -0700

    Create SmoothZoomAdjuster :)

---
+++ Assets/Scripts/UI/Cameras/Adjusters/SmoothZoomAdjuster.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.UI.Cameras.Adjusters
+{
+	public class SmoothZoomAdjuster : ISmoothZoomAdjuster
+	{
+		private readonly Camera _camera;
+		private readonly float _smoothTime;
+		private float _cameraOrthographicSizeChangeVelocity;
+        
+		private const float ORTHOGRAPHIC_SIZE_EQUALITY_MARGIN = 0.1f;
+
+		public bool AdjustZoom(float targetOrthographicSize)
+		{
+			bool isRightOrthographicSize = Mathf.Abs(_camera.orthographicSize - targetOrthographicSize) < ORTHOGRAPHIC_SIZE_EQUALITY_MARGIN;
+
+            if (!isRightOrthographicSize)
+            {
+				_camera.orthographicSize = Mathf.SmoothDamp(_camera.orthographicSize, targetOrthographicSize, ref _cameraOrthographicSizeChangeVelocity, _smoothTime);
+            }
+            else if (_camera.orthographicSize != targetOrthographicSize)
+            {
+                _camera.orthographicSize = targetOrthographicSize;
+            }
+
+            return isRightOrthographicSize;
+		}
+	}
+}
