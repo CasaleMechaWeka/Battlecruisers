17e4346507ac843da9e454eef0f1ef50c5253039
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-18 08:39:46 -0700

    Create CameraTransitionManager :D

---
+++ Assets/Scripts/UI/Cameras/CameraTransitionManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using BattleCruisers.UI.Cameras.Adjusters;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.PlatformAbstractions;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Cameras
+{
+	// FELIX  Test?  Pretty please?
+	public class CameraTransitionManager : ICameraTransitionManager
+    {
+		private readonly ICamera _camera;
+		private readonly ISmoothPositionAdjuster _positionAdjuster;
+		private readonly ISmoothZoomAdjuster _zoomAdjuster;
+		private readonly IDictionary<CameraState, ICameraTarget> _stateToTarget;
+		private ICameraTarget _target;
+
+        public CameraState CurrentState { private set; get; }
+
+		public event EventHandler<CameraTransitionArgs> CameraTransitionStarted;
+		public event EventHandler<CameraTransitionArgs> CameraTransitionCompleted;
+
+		public CameraTransitionManager(
+			ICamera camera, 
+			ICameraTargetsFactory cameraTargetsFactory,
+			ISmoothPositionAdjuster positionAdjuster,
+			ISmoothZoomAdjuster zoomAdjuster)
+		{
+			Helper.AssertIsNotNull(camera, cameraTargetsFactory, positionAdjuster, zoomAdjuster);
+
+			_camera = camera;
+			_stateToTarget = cameraTargetsFactory.CreateCameraTargets();
+			_positionAdjuster = positionAdjuster;
+			_zoomAdjuster = zoomAdjuster;
+
+			CurrentState = CameraState.Overview;
+		}
+
+        public bool SetCameraTarget(CameraState targetState)
+        {
+			bool willMoveCamera =
+                CurrentState != CameraState.InTransition
+                && CurrentState != targetState;
+
+            if (willMoveCamera)
+            {
+				Assert.IsTrue(_stateToTarget.ContainsKey(targetState));
+				_target = _stateToTarget[targetState];
+
+                if (CameraTransitionStarted != null)
+                {
+                    CameraTransitionStarted.Invoke(this, new CameraTransitionArgs(CurrentState, targetState));
+                }
+
+                if (_target.IsInstantTransition(CurrentState))
+                {
+					// Move camera instantly
+					_camera.Position = _target.Position;
+                    _camera.OrthographicSize = _target.OrthographicSize;
+                }
+                else
+                {
+					// Move camera via smooth transition, over multiple frames.
+                    CurrentState = CameraState.InTransition;
+                }
+            }
+
+            return willMoveCamera;
+        }
+
+		public void MoveCamera()
+		{
+			if (CurrentState == _target.State)
+			{
+				// Camera is already in the right place.  No need to move the camera.
+				return;
+			}
+
+            bool isInPosition = _positionAdjuster.AdjustPosition(_target.Position);
+			bool isRightOrthographicSize = _zoomAdjuster.AdjustZoom(_target.OrthographicSize);
+
+            if (isInPosition && isRightOrthographicSize)
+            {
+                if (CameraTransitionCompleted != null)
+                {
+                    CameraTransitionCompleted.Invoke(this, new CameraTransitionArgs(CurrentState, _target.State));
+                }
+
+				CurrentState = _target.State;
+            }
+		}
+	}
+}
--- Assets/Scripts/UI/Cameras/ICameraTransitionManager.cs
+++ Assets/Scripts/UI/Cameras/ICameraTransitionManager.cs
@@ -1,8 +1,20 @@
-﻿namespace BattleCruisers.UI.Cameras
+﻿using System;
+
+namespace BattleCruisers.UI.Cameras
 {
 	public interface ICameraTransitionManager
 	{
-		CameraTarget Target { set; }
+		CameraState CurrentState { get; }
+        
+		event EventHandler<CameraTransitionArgs> CameraTransitionStarted;
+        event EventHandler<CameraTransitionArgs> CameraTransitionCompleted;
+
+		/// <returns>
+		/// <c>true</c>, if camera was or will be moved, <c>false</c> otherwise.
+		/// The camera may not be moved if it is currently in a transition, or
+		/// the new target is the same as the current target.
+		/// </returns>
+		bool SetCameraTarget(CameraState targetState);
 
         /// <summary>
         /// Should be called every frame to smoothly move the camera to the 
