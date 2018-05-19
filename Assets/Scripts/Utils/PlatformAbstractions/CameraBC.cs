26e6df96f58b1b4aa41233edbde811202f3d9976
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-19 11:14:31 -0700

    Create camera abstraction

---
+++ Assets/Scripts/Utils/PlatformAbstractions/CameraBC.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.PlatformAbstractions
+{
+	public class CameraBC : ICamera
+    {
+		private readonly Camera _platfromCamera;
+
+		public CameraBC(Camera platformCamera)
+		{
+			Assert.IsNotNull(platformCamera);
+			_platfromCamera = platformCamera;
+		}
+
+		public Vector3 Position 
+		{ 
+			get { return _platfromCamera.transform.position; }
+			set { _platfromCamera.transform.position = value; }
+		}
+
+		public float OrthographicSize
+		{
+			get { return _platfromCamera.orthographicSize; }
+			set { _platfromCamera.orthographicSize = value; }
+		}
+	}
+}
