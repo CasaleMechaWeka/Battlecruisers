c571df142ac431f5f774aeae9fe04128455ede8e
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-19 11:15:11 -0700

    Create ICameraMover :)

---
+++ Assets/Scripts/UI/Cameras/ICameraMover.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BattleCruisers.UI.Cameras
+{
+	public class CameraStateChangedArgs : EventArgs
+    {
+		public CameraState PreviousState { get; private set; }
+		public CameraState NewState { get; private set; }
+
+		public CameraStateChangedArgs(CameraState previousState, CameraState newState)
+        {
+            PreviousState = previousState;
+            NewState = newState;
+        }
+    }
+
+	public interface ICameraMover
+	{
+		CameraState State { get; }
+
+		event EventHandler<CameraStateChangedArgs> StateChanged;
+
+		void MoveCamera(CameraState currentState);
+	}
+}
