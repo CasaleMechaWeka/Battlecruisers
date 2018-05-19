35d126d47b666638c7897bcd586b3a46012f3620
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-19 11:14:15 -0700

    Remove TODOs, these interfaces will be used :)

--- Assets/Scripts/Utils/PlatformAbstractions/ICamera.cs
+++ Assets/Scripts/Utils/PlatformAbstractions/ICamera.cs
@@ -1,6 +1,5 @@
 ﻿namespace BattleCruisers.Utils.PlatformAbstractions
 {
-	// FELIX  Remove if unused :/
 	public interface ICamera : ITransform
 	{
 		float OrthographicSize { get; set; }
--- Assets/Scripts/Utils/PlatformAbstractions/ITransform.cs
+++ Assets/Scripts/Utils/PlatformAbstractions/ITransform.cs
@@ -2,7 +2,6 @@
 
 namespace BattleCruisers.Utils.PlatformAbstractions
 {
-	// FELIX  Remove if unused :/
 	public interface ITransform
 	{
 		Vector3 Position { get; set; }
