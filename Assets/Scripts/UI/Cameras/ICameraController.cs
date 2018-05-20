4d473df5db72fc7946ce690954729c0ec95715ca
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-19 18:00:05 -0700

    Remove unused event args class :)

--- Assets/Scripts/UI/Cameras/ICameraController.cs
+++ Assets/Scripts/UI/Cameras/ICameraController.cs
@@ -2,20 +2,7 @@
 
 namespace BattleCruisers.UI.Cameras
 {
-	// FELIX  Remove
-    public class CameraTransitionArgs : EventArgs
-    {
-        public CameraState Origin { get; private set; }
-        public CameraState Destination { get; private set; }
-
-        public CameraTransitionArgs(CameraState origin, CameraState destination)
-        {
-            Origin = origin;
-            Destination = destination;
-        }
-    }
-
-    public interface ICameraController
+	public interface ICameraController
     {
         CameraState State { get; }
 
