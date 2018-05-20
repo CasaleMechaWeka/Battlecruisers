f3250eca8c81a92255614935f721faea439be2e9
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-19 18:06:08 -0700

    Create abstract CameraMover :)

---
+++ Assets/Scripts/UI/Cameras/CameraMover.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.Cameras
+{
+	// FELIX  Create test file.  Move appropriate tests from CameraTransitionManagerTests over
+	public abstract class CameraMover : ICameraMover
+    {
+        private CameraState _state;
+        public CameraState State 
+        { 
+            get { return _state; }
+            private set
+            {
+                // Event handlers may access this property, so want to update the 
+                // value before emitting the changed event.
+                CameraState oldState = _state;
+                _state = value;
+
+                if (oldState != _state && StateChanged != null)
+                {
+                    Logging.Log(Tags.CAMERA, "CameraMover.State: " + oldState + " > " + value);
+                    StateChanged.Invoke(this, new CameraStateChangedArgs(oldState, _state));
+                }
+            }
+        }
+
+        public event EventHandler<CameraStateChangedArgs> StateChanged;
+
+		protected CameraMover()
+        {
+            _state = CameraState.UserInputControlled;
+        }
+
+		public abstract void MoveCamera(float deltaTime, CameraState currentState);
+
+        public void Reset(CameraState currentState)
+        {
+            Logging.Log(Tags.CAMERA, "CameraMover.Reset(): " + currentState);
+            _state = currentState;
+        }
+	}
+}
