49726119591af364cef4ea070c6068046bf79086
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-05-21 10:00:38 -0700

    Create NavigationSettings :)

---
+++ Assets/Scripts/UI/BattlScene/Navigation/INavigationSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace BattleCruisers.UI.BattleScene.Navigation
+{
+    public enum NavigationPermission
+	{
+		None, TransitionsOnly, UserInputOnly, Both
+	}
+
+	public interface INavigationSettings
+	{
+		bool AreTransitionsEnabled { get; }
+		bool IsUserInputEnabled { get; }
+
+		IFilter AreTransitionsEnabledFilter { get; }
+		IFilter IsUserInputEnabledFilter { get; }
+
+		NavigationPermission Permission { set; }
+	}
+}
---
+++ Assets/Scripts/UI/BattlScene/Navigation/NavigationSettings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace BattleCruisers.UI.BattleScene.Navigation
+{
+	// FELIX  Test :)
+	public class NavigationSettings : INavigationSettings
+	{
+		public bool AreTransitionsEnabled { get { return AreTransitionsEnabledFilter.IsMatch; } }
+		public bool IsUserInputEnabled { get { return IsUserInputEnabledFilter.IsMatch; } }
+
+		private readonly BasicFilter _areTransitionsEnabledFilter;
+		public IFilter AreTransitionsEnabledFilter { get { return _areTransitionsEnabledFilter; } }
+
+		private readonly BasicFilter _isUserInputEnabledFilter;
+		public IFilter IsUserInputEnabledFilter { get { return _isUserInputEnabledFilter; } }
+        
+		public NavigationPermission Permission
+		{
+			set
+			{
+				switch (value)
+				{
+					case NavigationPermission.None:
+						_areTransitionsEnabledFilter.IsMatch = false;
+						_isUserInputEnabledFilter.IsMatch = false;
+						break;
+
+					case NavigationPermission.TransitionsOnly:
+						_areTransitionsEnabledFilter.IsMatch = true;
+                        _isUserInputEnabledFilter.IsMatch = false;
+                        break;
+
+					case NavigationPermission.UserInputOnly:
+                        _areTransitionsEnabledFilter.IsMatch = false;
+                        _isUserInputEnabledFilter.IsMatch = true;
+                        break;
+
+					case NavigationPermission.Both:
+                        _areTransitionsEnabledFilter.IsMatch = true;
+                        _isUserInputEnabledFilter.IsMatch = true;
+                        break;
+
+					default:
+						throw new ArgumentException();
+				}
+			}
+		}
+
+		public NavigationSettings()
+		{
+			_areTransitionsEnabledFilter = new BasicFilter(isMatch: true);
+			_isUserInputEnabledFilter = new BasicFilter(isMatch: true);
+		}
+	}
+}
