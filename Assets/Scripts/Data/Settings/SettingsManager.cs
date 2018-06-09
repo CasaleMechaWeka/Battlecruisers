253eb07ff6cc98f44edc636ef82182dea0f65103
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-06-08 18:46:17 -0700

    Reduce min scroll speed, so scrolling can be noticably slowed down :)

--- Assets/Scripts/Data/Settings/SettingsManager.cs
+++ Assets/Scripts/Data/Settings/SettingsManager.cs
@@ -17,9 +17,9 @@ namespace BattleCruisers.Data.Settings
         public const float MIN_ZOOM_SPEED = 0.1f;
         public const float MAX_ZOOM_SPEED = 3.9f;
 
-		private const float DEFAULT_SCROLL_SPEED = 3;
-		public const float MIN_SCROLL_SPEED = 1;
-		public const float MAX_SCROLL_SPEED = 5;
+		private const float DEFAULT_SCROLL_SPEED = 2;
+		public const float MIN_SCROLL_SPEED = 0.1f;
+		public const float MAX_SCROLL_SPEED = 3.9f;
 
         public Difficulty AIDifficulty
         {
