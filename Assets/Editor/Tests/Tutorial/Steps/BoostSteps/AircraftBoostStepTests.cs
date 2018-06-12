2eb6b51b428f9d91343c1e17c91102a609590515
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-06-11 17:22:54 -0700

    Create AircraftBoostStepTests :)

---
+++ Assets/Editor/Tests/Tutorial/Steps/BoostSteps/AircraftBoostStepTests.cs
@@ -0,0 +1,57 @@
+﻿using BattleCruisers.Buildables.Boost;
+using BattleCruisers.Tutorial.Steps;
+using BattleCruisers.Tutorial.Steps.EnemyCruiser;
+using BattleCruisers.Utils.DataStrctures;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.Tutorial.Steps.BoostSteps
+{
+    public class DummyBoostStep : AircraftBoostStep
+    {
+        public int BoostActionCount { get; private set; }
+
+        public DummyBoostStep(ITutorialStepArgs args, IBoostProvidersManager boostProvidersManager, IBoostProvider boostProvider)
+            : base(args, boostProvidersManager, boostProvider)
+        {
+            BoostActionCount = 0;
+        }
+
+        protected override void BoostProviderAction()
+        {
+            BoostActionCount++;
+        }
+    }
+
+    // FELIX  Create base test class :P
+    public class AircraftBoostStepTests : TutorialStepTestsBase
+    {
+        private DummyBoostStep _boostStep;
+        private IBoostProvidersManager _boostProvidersManager;
+        private IObservableCollection<IBoostProvider> _aircraftBoostProviders;
+        private IBoostProvider _boostProvider;
+
+        [SetUp]
+        public override void SetuUp()
+        {
+            base.SetuUp();
+
+            _aircraftBoostProviders = Substitute.For<IObservableCollection<IBoostProvider>>();
+            _boostProvidersManager = Substitute.For<IBoostProvidersManager>();
+            _boostProvidersManager.AircraftBoostProviders.Returns(_aircraftBoostProviders);
+
+            _boostProvider = Substitute.For<IBoostProvider>();
+
+            _boostStep = new DummyBoostStep(_args, _boostProvidersManager, _boostProvider);
+        }
+
+        [Test]
+        public void Start_RunsBoostAction_AndCompletes()
+        {
+            _boostStep.Start(_completionCallback);
+
+            Assert.AreEqual(1, _boostStep.BoostActionCount);
+            Assert.AreEqual(1, _callbackCounter);
+        }
+    }
+}
