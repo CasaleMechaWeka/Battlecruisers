31b316537560d0bbbca4c591c177c8749af45585
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-06-11 17:32:02 -0700

    Add tests for Adding/Removing aircraft boost :)

---
+++ Assets/Editor/Tests/Tutorial/Steps/BoostSteps/AddAircraftBoostStepTests.cs
@@ -0,0 +1,27 @@
+﻿using BattleCruisers.Tutorial.Steps;
+using BattleCruisers.Tutorial.Steps.EnemyCruiser;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.Tutorial.Steps.BoostSteps
+{
+    public class AddAircraftBoostStepTests : AircraftBoostStepTestsBase
+    {
+        private ITutorialStep _boostStep;
+
+        [SetUp]
+        public override void SetuUp()
+        {
+            base.SetuUp();
+
+            _boostStep = new AddAircraftBoostStep(_args, _boostProvidersManager, _boostProvider);
+        }
+
+        [Test]
+        public void Start_AddsBoost_AndCompletes()
+        {
+            _boostStep.Start(_completionCallback);
+            _aircraftBoostProviders.Received().Add(_boostProvider);
+        }
+    }
+}
