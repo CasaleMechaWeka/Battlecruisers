414882b7ec77bc96c7ab498ac7b3520b71d66be8
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-06-12 18:41:14 -0700

    Create TurretStatsBoostables :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/Stats/ITurretStatsBoostables.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Buildables.Boost;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.Stats
+{
+    public interface ITurretStatsBoostables
+    {
+        IBoostable AccuracyBoostable { get; }
+        IBoostable FireRateBoostable { get; }
+    }
+}
---
+++ Assets/Scripts/Buildables/Buildings/Turrets/Stats/TurretStatsBoostables.cs
@@ -0,0 +1,19 @@
+﻿using BattleCruisers.Buildables.Boost;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.Stats
+{
+    public class TurretStatsBoostables : ITurretStatsBoostables
+    {
+        public IBoostable AccuracyBoostable { get; private set; }
+        public IBoostable FireRateBoostable { get; private set; }
+
+        public TurretStatsBoostables(IBoostFactory boostFactory)
+        {
+            Assert.IsNotNull(boostFactory);
+
+            AccuracyBoostable = boostFactory.CreateBoostable();
+            FireRateBoostable = boostFactory.CreateBoostable();
+        }
+    }
+}
