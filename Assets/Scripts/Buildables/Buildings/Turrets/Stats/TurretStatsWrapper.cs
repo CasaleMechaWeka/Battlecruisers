d567674d7fc45d50ed7d4c32cdac34a07acd304d
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-06-15 18:13:55 -0700

    Create (slightly ugly :( ) TurretStatsWrapper

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/Stats/TurretStatsWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.Stats
+{
+    public class TurretStatsWrapper : ITurretStatsWrapper
+    {
+        private ITurretStats _turretStats;
+        public ITurretStats TurretStats 
+        { 
+            private get { return _turretStats; } 
+            set
+            {
+                Assert.IsNotNull(value);
+                _turretStats = value;
+            }
+        }
+
+        public float Accuracy { get { return TurretStats.Accuracy; } }
+        public float TurretRotateSpeedInDegrees { get { return TurretStats.TurretRotateSpeedInDegrees; } }
+        public bool IsInBurst { get { return TurretStats.IsInBurst; } }
+        public float FireRatePerS { get { return TurretStats.FireRatePerS; } }
+        public float RangeInM { get { return TurretStats.RangeInM; } }
+        public float MinRangeInM { get { return TurretStats.MinRangeInM; } }
+        public float MeanFireRatePerS { get { return TurretStats.MeanFireRatePerS; } }
+        public ReadOnlyCollection<TargetType> AttackCapabilities { get { return TurretStats.AttackCapabilities; } }
+        public float DurationInS { get { return TurretStats.DurationInS; } }
+
+        public TurretStatsWrapper(ITurretStats turretStats)
+        {
+            TurretStats = turretStats;
+        }
+
+        public void MoveToNextDuration()
+        {
+            _turretStats.MoveToNextDuration();
+        }
+    }
+}
