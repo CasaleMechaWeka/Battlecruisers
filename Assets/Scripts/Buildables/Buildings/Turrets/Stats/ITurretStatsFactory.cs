c5db6f9bb519237883b1faf88a95344c3087ca2d
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-06-15 18:34:21 -0700

    Craete TurretStatsFactory :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/Stats/ITurretStatsFactory.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Buildables.Boost;
+using BattleCruisers.Utils.DataStrctures;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.Stats
+{
+    public interface ITurretStatsFactory
+    {
+        // FELIX  Unused?
+        //IBasicTurretStats CreateBoostedBasicTurretStats(IBasicTurretStats baseTurretStats);
+
+        ITurretStats CreateBoostedTurretStats(ITurretStats baseTurretStats);
+    }
+}
---
+++ Assets/Scripts/Buildables/Buildings/Turrets/Stats/TurretStatsFactory.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.Buildables.Boost;
+using BattleCruisers.Buildables.Buildings.Turrets.Stats.Boosted;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.DataStrctures;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.Stats
+{
+    public class TurretStatsFactory : ITurretStatsFactory
+    {
+        private readonly IBoostFactory _boostFactory;
+        private readonly IObservableCollection<IBoostProvider> _localBoostProviders;
+        private readonly IGlobalBoostProviders _globalBoostProviders;
+
+        public TurretStatsFactory(
+            IBoostFactory boostFactory,
+            IObservableCollection<IBoostProvider> localBoostProviders,
+            IGlobalBoostProviders globalBoostProviders)
+        {
+            Helper.AssertIsNotNull(boostFactory, localBoostProviders, globalBoostProviders);
+
+            _boostFactory = boostFactory;
+            _localBoostProviders = localBoostProviders;
+            _globalBoostProviders = globalBoostProviders;
+        }
+
+        public ITurretStats CreateBoostedTurretStats(ITurretStats baseTurretStats)
+        {
+            return new BoostedTurretStats(baseTurretStats, _boostFactory, _localBoostProviders, _globalBoostProviders);
+        }
+    }
+}
