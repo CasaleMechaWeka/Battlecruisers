4a7011a13b4c1dd78f74b86232f11498f2a7a596
Felix Fei Mann <felix.fei.mann@gmail.com>
2018-06-17 15:32:17 -0700

    Create AddTurretRateRateBoostStep :)

---
+++ Assets/Scripts/Tutorial/Steps/BoostSteps/AddTurretFireRateBoostStep.cs
@@ -0,0 +1,21 @@
+﻿using BattleCruisers.Buildables.Boost;
+
+namespace BattleCruisers.Tutorial.Steps.EnemyCruiser
+{
+    // FElIX  Test :P
+    public class AddTurretFireRateBoostStep : BoostStep
+    {
+        public AddTurretFireRateBoostStep(
+            ITutorialStepArgs args,
+            IGlobalBoostProviders globalBoostProviders,
+            IBoostProvider boostProvider)
+            : base(args, globalBoostProviders, boostProvider)
+        {
+        }
+
+        protected override void BoostProviderAction()
+        {
+            _globalBoostProviders.TurretFireRateBoostProviders.Add(_boostProvider);
+        }
+    }
+}
