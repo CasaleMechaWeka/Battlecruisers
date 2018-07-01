0902106501cb3b2d6ff11280cc3cf0a6da13631d
Felix Mann <felix.fei.mann@gmail.com>
2018-07-01 16:36:19 -0700

    Move BarrelAdjustmentResult to own class :)

--- Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/Helpers/IBarrelAdjustmentHelper.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/Helpers/IBarrelAdjustmentHelper.cs
@@ -1,24 +1,5 @@
-﻿using UnityEngine;
-
-namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers.Helpers
+﻿namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers.Helpers
 {
-    public class BarrelAdjustmentResult
-    {
-        public bool IsOnTarget { get; private set; }
-        public float DesiredAngleInDegrees { get; private set; }
-        public Vector2 PredictedTargetPosition { get; private set; }
-
-        public BarrelAdjustmentResult(
-            bool isOnTarget,
-            float desiredAngleInDegrees = default(float),
-            Vector2 predictedTargetPosition = default(Vector2))
-        {
-            this.IsOnTarget = isOnTarget;
-            this.DesiredAngleInDegrees = desiredAngleInDegrees;
-            this.PredictedTargetPosition = predictedTargetPosition;
-        }
-    }
-
     public interface IBarrelAdjustmentHelper
     {
         BarrelAdjustmentResult AdjustTurretBarrel();
