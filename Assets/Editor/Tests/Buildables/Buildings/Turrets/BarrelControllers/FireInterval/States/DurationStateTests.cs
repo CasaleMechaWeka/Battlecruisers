c266945d1f01e3d8cf6474d00fa9a21296fc22e2
Felix Mann <felix.fei.mann@gmail.com>
2018-07-04 09:13:26 -0700

    Create other fire interval state tests :)

---
+++ Assets/Editor/Tests/Buildables/Buildings/Turrets/BarrelControllers/FireInterval/States/DurationStateTests.cs
@@ -0,0 +1,49 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers.FireInterval.States;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.Buildables.Buildings.Turrets.BarrelControllers.FireInterval.States
+{
+    public class DummyState : DurationState
+    {
+        public override bool ShouldFire { get { return true; } }
+    }
+
+    public class DurationStateTests : StatesTestBase
+    {
+        private DurationState _durationState;
+
+        [SetUp]
+        public override void TestSetup()
+        {
+            base.TestSetup();
+
+            _durationState = new DummyState();
+            _durationState.Initialise(_otherState, _durationProvider);
+        }
+
+        [Test]
+        public void OnFired()
+        {
+            IState nextState = _durationState.OnFired();
+
+            Assert.AreSame(_durationState, nextState);
+            _durationProvider.Received().MoveToNextDuration();
+        }
+
+        [Test]
+        public void ProcessTimeInterval_DuringInterval()
+        {
+            _durationProvider.DurationInS.Returns(2);
+            _timePassedInS = 1;
+
+            // 1s < 2s
+            IState nextState = _durationState.ProcessTimeInterval(_timePassedInS);
+            Assert.AreSame(_durationState, nextState);
+
+            // 2s == 2s
+            nextState = _durationState.ProcessTimeInterval(_timePassedInS);
+            Assert.AreSame(_otherState, nextState);
+        }
+    }
+}
---
+++ Assets/Editor/Tests/Buildables/Buildings/Turrets/BarrelControllers/FireInterval/States/WaitingState.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers.FireInterval.States;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.Buildables.Buildings.Turrets.BarrelControllers.FireInterval.States
+{
+    public class WaitingStateTests : StatesTestBase
+    {
+        private WaitingState _waitingState;
+
+        [SetUp]
+        public override void TestSetup()
+        {
+            base.TestSetup();
+
+            _waitingState = new WaitingState();
+            _waitingState.Initialise(_otherState, _durationProvider);
+        }
+
+        [Test]
+        public void ShouldFire()
+        {
+            Assert.IsFalse(_waitingState.ShouldFire);
+        }
+    }
+}
