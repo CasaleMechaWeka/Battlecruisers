fc1418892d8bfeba1b45e36df8c62bf35402e994
Felix Mann <felix.fei.mann@gmail.com>
2018-07-05 18:49:06 -0700

    Defer dealing damage until FixedUpdate() to work around Unity bug, like for ProjectileController :)

--- Assets/Scripts/Buildables/Units/Aircraft/KamikazeController.cs
+++ Assets/Scripts/Buildables/Units/Aircraft/KamikazeController.cs
@@ -15,6 +15,12 @@ namespace BattleCruisers.Buildables.Units.Aircraft
         private IDamageApplier _damageApplier;
         private IExplosion _explosion;
 
+        // Have this to defer damaging the target until the next FixedUpdate(), because
+        // there is a bug in Unity that if the target is destroyed from OnTriggerEnter2D()
+        // the target collider does not trigger OnTriggerExit2D().  I filed a bug with
+        // Unity so *hopefully* this is fixed one day and I can remove this deferral :)
+        private ITarget _targetToDamage;
+
         private const float KAMIKAZE_DAMAGE_MULTIPLIER = 1.5f;
 
         public void Initialise(IUnit parentAircraft, IFactoryProvider factoryProvider, ITarget target)
@@ -22,6 +28,7 @@ namespace BattleCruisers.Buildables.Units.Aircraft
             Helper.AssertIsNotNull(parentAircraft, factoryProvider);
 
             _parentAircraft = parentAircraft;
+            _targetToDamage = null;
 
             List<TargetType> targetTypes = new List<TargetType>() { TargetType.Buildings, TargetType.Cruiser, TargetType.Ships };
             _targetFilter = factoryProvider.TargetsFactory.CreateTargetFilter(target.Faction, targetTypes);
@@ -35,19 +42,28 @@ namespace BattleCruisers.Buildables.Units.Aircraft
             _explosion = factoryProvider.ExplosionFactory.CreateExplosion(kamikazeDamageStats.DamageRadiusInM);
         }
 
-		void OnTriggerEnter2D(Collider2D collider)
+		private void OnTriggerEnter2D(Collider2D collider)
 		{
 			ITarget target = collider.gameObject.GetComponent<ITarget>();
 
 			if (target != null 
                 && _targetFilter.IsMatch(target)
                 && !target.IsDestroyed
-                && !_parentAircraft.IsDestroyed)
+                && !_parentAircraft.IsDestroyed
+                && _targetToDamage == null)
 			{
-                _damageApplier.ApplyDamage(target, _parentAircraft.Position, damageSource: _parentAircraft);
-                _explosion.Show(_parentAircraft.Position);
-                _parentAircraft.Destroy();
+                _targetToDamage = target;
 			}
 		}
+
+        private void FixedUpdate()
+        {
+            if (_targetToDamage != null)
+            {
+                _damageApplier.ApplyDamage(_targetToDamage, _parentAircraft.Position, damageSource: _parentAircraft);
+                _explosion.Show(_parentAircraft.Position);
+                _parentAircraft.Destroy();
+            }
+        }
     }
 }
