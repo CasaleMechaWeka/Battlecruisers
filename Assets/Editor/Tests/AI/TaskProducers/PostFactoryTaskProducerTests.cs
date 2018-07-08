660ff9b24815d4597288bda478516f0d54e58197
Felix Mann <felix.fei.mann@gmail.com>
2018-07-08 16:09:30 -0700

    Create PostFactoryTaskProducerTests :)

---
+++ Assets/Editor/Tests/AI/TaskProducers/PostFactoryTaskProducerTests.cs
@@ -0,0 +1,57 @@
+﻿using BattleCruisers.AI.TaskProducers;
+using BattleCruisers.AI.Tasks;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Cruisers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.AI.TaskProducers
+{
+    public class PostFactoryTaskProducerTests : TaskProducerTestsBase
+    {
+        private PostFactoryTaskProducer _taskProducer;
+        private ITask _waitForUnitsTask;
+        private IFactory _factory;
+
+        [SetUp]
+        public override void SetuUp()
+        {
+            base.SetuUp();
+
+            _factory = Substitute.For<IFactory>();
+            _waitForUnitsTask = Substitute.For<ITask>();
+
+            _taskFactory
+                .CreateWaitForUnitConstructionTask(TaskPriority.Normal, _factory)
+                .Returns(_waitForUnitsTask);
+
+            _taskProducer = new PostFactoryTaskProducer(_tasks, _cruiser, _taskFactory, _prefabFactory);
+        }
+
+        [Test]
+        public void StartedConstruction_Factory_CreatesTask()
+        {
+            StartConstructingBuilding(_factory);
+
+            _taskFactory.Received().CreateWaitForUnitConstructionTask(TaskPriority.Normal, _factory);
+            _tasks.Received().Add(_waitForUnitsTask);
+        }
+
+        [Test]
+        public void StartedConstruction_NonFactory_DoesNotCreatesTask()
+        {
+            IBuilding nonFactoryBuilding = Substitute.For<IBuilding>();
+
+            StartConstructingBuilding(nonFactoryBuilding);
+
+            _taskFactory.DidNotReceiveWithAnyArgs().CreateWaitForUnitConstructionTask(default(TaskPriority), null);
+            _tasks.DidNotReceiveWithAnyArgs().Add(null);
+        }
+
+        private void StartConstructingBuilding(IBuilding building)
+        {
+            _cruiser.StartedConstruction += Raise.EventWith(new StartedConstructionEventArgs(building));
+        }
+    }
+}
--- Assets/Scripts/AI/TaskProducers/PostFactoryTaskProducer.cs
+++ Assets/Scripts/AI/TaskProducers/PostFactoryTaskProducer.cs
@@ -10,8 +10,6 @@ namespace BattleCruisers.AI.TaskProducers
     /// 
     /// This means that after building a factory, the AI cruiser should wait for some
     /// units to be produced before constructing the next building.
-    /// 
-    /// FELIX  Test :D
     /// </summary>
     public class PostFactoryTaskProducer : BaseTaskProducer
     {
