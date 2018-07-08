952d8651abf019dca747d7afec018447087afb83
Felix Mann <felix.fei.mann@gmail.com>
2018-07-07 17:14:31 -0700

    Use IUnitChooser.ChosenUnitChanged to change a factories unit, if it is currently not producing any units :)

--- Assets/Scripts/AI/FactoryManagers/FactoryManager.cs
+++ Assets/Scripts/AI/FactoryManagers/FactoryManager.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using BattleCruisers.Buildables;
+﻿using BattleCruisers.Buildables;
 using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Buildables.Units;
 using BattleCruisers.Cruisers;
 using BattleCruisers.Utils;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
 
 namespace BattleCruisers.AI.FactoryManagers
 {
@@ -14,9 +16,13 @@ namespace BattleCruisers.AI.FactoryManagers
     /// build when that factory:
     /// a) Completes building (itself)
     /// b) Completes building a unit
+    /// c) The unit chooser changes the chosen unit (eg, because the number of drones has changed)
+    ///     AND the factory is not building a unit
     /// </summary>
+    /// FELIX  Update tests :)
     public class FactoryManager : IFactoryManager
     {
+        private readonly HashSet<IFactory> _factories;
         private readonly UnitCategory _factoryUnitCategory;
         private readonly ICruiserController _friendlyCruiser;
         private readonly IUnitChooser _unitChooser;
@@ -28,8 +34,10 @@ namespace BattleCruisers.AI.FactoryManagers
             _factoryUnitCategory = factoryUnitCategory;
             _friendlyCruiser = friendlyCruiser;
             _unitChooser = unitChooser;
+            _factories = new HashSet<IFactory>();
 
             _friendlyCruiser.StartedConstruction += _friendlyCruiser_StartedConstruction;
+            _unitChooser.ChosenUnitChanged += _unitChooser_ChosenUnitChanged;
         }
 
         private void _friendlyCruiser_StartedConstruction(object sender, StartedConstructionEventArgs e)
@@ -38,7 +46,11 @@ namespace BattleCruisers.AI.FactoryManagers
 
             if (factory != null && factory.UnitCategory == _factoryUnitCategory)
             {
+                Assert.IsFalse(_factories.Contains(factory));
+                _factories.Add(factory);
+
                 factory.CompletedBuildable += Factory_CompletedBuildable;
+                factory.CompletedBuildingUnit += Factory_CompletedBuildingUnit;
                 factory.Destroyed += Factory_Destroyed;
             }
         }
@@ -46,29 +58,57 @@ namespace BattleCruisers.AI.FactoryManagers
         private void Factory_CompletedBuildable(object sender, EventArgs e)
         {
 			IFactory factory = sender.Parse<IFactory>();
+            Assert.IsTrue(_factories.Contains(factory));
 
             factory.UnitWrapper = _unitChooser.ChosenUnit;
-            factory.CompletedBuildingUnit += Factory_CompletedBuildingUnit;
         }
 
         private void Factory_CompletedBuildingUnit(object sender, CompletedConstructionEventArgs e)
         {
 			IFactory factory = sender.Parse<IFactory>();
+            Assert.IsTrue(_factories.Contains(factory));
+
             factory.UnitWrapper = _unitChooser.ChosenUnit;
         }
 
         private void Factory_Destroyed(object sender, DestroyedEventArgs e)
         {
             IFactory factory = sender.Parse<IFactory>();
+            Assert.IsTrue(_factories.Contains(factory));
+            _factories.Remove(factory);
 
+            UnsubscribeFromAllEvents(factory);
+        }
+
+        private void UnsubscribeFromAllEvents(IFactory factory)
+        {
             factory.CompletedBuildable -= Factory_CompletedBuildable;
             factory.Destroyed -= Factory_Destroyed;
             factory.CompletedBuildingUnit -= Factory_CompletedBuildingUnit;
+            _unitChooser.ChosenUnitChanged -= _unitChooser_ChosenUnitChanged;
+        }
+
+        private void _unitChooser_ChosenUnitChanged(object sender, EventArgs e)
+        {
+            foreach (IFactory factory in _factories)
+            {
+                if (factory.UnitWrapper == null)
+                {
+                    factory.UnitWrapper = _unitChooser.ChosenUnit;
+                }
+            }
         }
 
         public void Dispose()
         {
             _friendlyCruiser.StartedConstruction -= _friendlyCruiser_StartedConstruction;
+
+            foreach (IFactory factory in _factories)
+            {
+                UnsubscribeFromAllEvents(factory);
+            }
+
+            _factories.Clear();
         }
     }
 }
