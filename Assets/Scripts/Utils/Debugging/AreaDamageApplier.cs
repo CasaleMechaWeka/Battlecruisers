2eef24d0c496ea8103d25760edeb74080c26c4e5
Felix Mann <felix.fei.mann@gmail.com>
2018-07-13 17:06:47 -0700

    Create AreaDamageApplier, so I can have area of effect damage at the mouse location every time I press D :P

---
+++ Assets/Scripts/Utils/Debugging/AreaDamageApplier.cs
@@ -0,0 +1,41 @@
+﻿using BattleCruisers.Projectiles.DamageAppliers;
+using BattleCruisers.Projectiles.Stats.Wrappers;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using UnityEngine;
+
+namespace BattleCruisers.Utils.Debugging
+{
+    /// <summary>
+    /// Applies area of effect damage whenever the "D" key is pressed, 
+    /// at the location of the mouse.  For testing :P
+    /// </summary>
+    /// TEMP  Turn this class off for final game :P
+    public class AreaDamageApplier : MonoBehaviour
+    {
+        private IDamageApplier _areaDamageApplier;
+
+        public float damage;
+        public float damageRadiusInM;
+
+        void Start()
+        {
+            IDamageStats damageStats = new DamageStats(damage, damageRadiusInM);
+            ITargetFilter targetFilter = new DummyTargetFilter(isMatchResult: true);
+            _areaDamageApplier = new AreaOfEffectDamageApplier(damageStats, targetFilter);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                Vector2 collisionPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                _areaDamageApplier
+                    .ApplyDamage(
+                    target: null,
+                    collisionPoint: collisionPoint,
+                    damageSource: null);
+            }
+        }
+    }
+}
