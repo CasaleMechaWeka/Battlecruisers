4a601c21b1393d52e5872fb9b25303229f5b4081
Felix Mann <felix.fei.mann@gmail.com>
2018-07-17 16:52:30 -0700

    Create OffensiveBuildableTargetRankerWrapper :)

---
+++ Assets/Scripts/Targets/TargetProcessors/Ranking/Wrappers/OffensiveBuildableTargetRankerWrapper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Targets.TargetProcessors.Ranking.Wrappers
+{
+    public class OffensiveBuildableTargetRankerWrapper : MonoBehaviour, ITargetRankerWrapper
+    {
+        public ITargetRanker CreateTargetRanker(ITargetsFactory targetsFactory)
+        {
+            return targetsFactory.CreateOffensiveBuildableTargetRanker();
+        }
+    }
+}
