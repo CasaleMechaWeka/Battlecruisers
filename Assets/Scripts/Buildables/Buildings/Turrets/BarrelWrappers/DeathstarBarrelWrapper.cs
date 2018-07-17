28796556395d9511d5cf130f9e547819ba24100c
Felix Mann <felix.fei.mann@gmail.com>
2018-07-17 16:52:51 -0700

    Create DeathstarBarrelWrapper :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/DeathstarBarrelWrapper.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.AngleLimiters;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
+{
+    public class DeathstarBarrelWrapper : DirectFireBarrelWrapper
+    {
+        protected override IAngleLimiter CreateAngleLimiter()
+        {
+            return _factoryProvider.AngleLimiterFactory.CreateDummyLimiter();
+        }
+    }
+}
