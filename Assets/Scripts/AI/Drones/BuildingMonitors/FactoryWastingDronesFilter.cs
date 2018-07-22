b9f8e79e2c27aeb53d6de6be413cf8b02a099f40
Felix Mann <felix.fei.mann@gmail.com>
2018-07-22 10:56:27 -0700

    Create FactoryWastingDronesFilter :)

---
+++ Assets/Scripts/AI/Drones/BuildingMonitors/FactoryWastingDronesFilter.cs
@@ -0,0 +1,16 @@
+﻿using BattleCruisers.Cruisers.Drones;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.AI.Drones.BuildingMonitors
+{
+    public class FactoryWastingDronesFilter : IFilter<IFactoryMonitor>
+    {
+        public bool IsMatch(IFactoryMonitor factoryMonitor)
+        {
+            return
+                 factoryMonitor.HasFactoryBuiltDesiredNumOfUnits
+                 && factoryMonitor.Factory.DroneConsumer != null
+                 && factoryMonitor.Factory.DroneConsumer.State != DroneConsumerState.Idle;
+        }
+    }
+}
