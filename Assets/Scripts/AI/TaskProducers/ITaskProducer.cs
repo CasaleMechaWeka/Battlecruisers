cb0b21cde7c6c64b14d56b17d949cf8bac119fbd
Felix Mann <felix.fei.mann@gmail.com>
2018-07-22 14:46:28 -0700

    Remove TODOs :/

--- Assets/Scripts/AI/TaskProducers/ITaskProducer.cs
+++ Assets/Scripts/AI/TaskProducers/ITaskProducer.cs
@@ -2,8 +2,6 @@
 
 namespace BattleCruisers.AI.TaskProducers
 {
-    // FELIX Remove interface?
-    // FELIX  Do not extend IManagedDisposable :p
     public interface ITaskProducer : IManagedDisposable
     {
         // Empty
