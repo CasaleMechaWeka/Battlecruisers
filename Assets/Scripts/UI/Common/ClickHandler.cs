455f74eb3a7062008f05ae71e2b5d01ce9a8957c
Felix Mann <felix.fei.mann@gmail.com>
2018-07-26 16:53:51 -0700

    Create ClickHandler :)

---
+++ Assets/Scripts/UI/Common/ClickHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Common
+{
+    public class ClickHandler : IClickHandler
+    {
+        private readonly float _doubleClickThresholdInS;
+        private float _lastClickTime;
+
+        public ClickHandler(float doubleClickThresholdInS)
+        {
+            Assert.IsTrue(doubleClickThresholdInS > 0);
+
+            _doubleClickThresholdInS = doubleClickThresholdInS;
+            _lastClickTime = 0;
+        }
+
+        public event EventHandler SingleClick;
+        public event EventHandler DoubleClick;
+
+        public void OnClick(float timeSinceGameStartInS)
+        {
+            if ((timeSinceGameStartInS - _lastClickTime) <= _doubleClickThresholdInS)
+            {
+                // Double click
+                if (DoubleClick != null)
+                {
+                    DoubleClick.Invoke(this, EventArgs.Empty);
+                }
+            }
+            else
+            {
+                // Single click
+                if (SingleClick != null)
+                {
+                    SingleClick.Invoke(this, EventArgs.Empty);
+                }
+            }
+
+            _lastClickTime = timeSinceGameStartInS;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Common/IClickHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BattleCruisers.UI.Common
+{
+    public interface IClickHandler
+    {
+        event EventHandler SingleClick;
+        event EventHandler DoubleClick;
+
+        void OnClick(float timeSinceGameStartInS);
+    }
+}
