486fa94c1d43d169464761104b68e8c9be0f0513
Felix Mann <felix.fei.mann@gmail.com>
2018-07-26 19:09:33 -0700

    Add boost feedabck to slot prefabs :D

--- Assets/Scripts/Scenes/Test/SlotsTestGod.cs
+++ Assets/Scripts/Scenes/Test/SlotsTestGod.cs
@@ -1,4 +1,5 @@
-﻿using BattleCruisers.Cruisers;
+﻿using BattleCruisers.Buildables.Boost;
+using BattleCruisers.Cruisers;
 using BattleCruisers.Cruisers.Slots;
 using BattleCruisers.Cruisers.Slots.BuildingPlacement;
 using NSubstitute;
@@ -15,12 +16,15 @@ namespace BattleCruisers.Scenes.Test
             ICruiser parentCruiser = Substitute.For<ICruiser>();
             ReadOnlyCollection<ISlot> neighbouringSlots = new ReadOnlyCollection<ISlot>(new List<ISlot>());
             IBuildingPlacer buildingPlacer = Substitute.For<IBuildingPlacer>();
+            IBoostProvider boostProvider = Substitute.For<IBoostProvider>();
 
             Slot[] slots = FindObjectsOfType<Slot>();
 
             foreach (Slot slot in slots)
             {
                 slot.Initialise(parentCruiser, neighbouringSlots, buildingPlacer);
+                slot.BoostProviders.Add(boostProvider);
+                slot.BoostProviders.Add(boostProvider);
             }
         }
     }
