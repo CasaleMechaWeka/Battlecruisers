0a548efd01106b3a9750dfda84e57d0e9a93265f
Felix Mann <felix.fei.mann@gmail.com>
2018-07-26 18:22:26 -0700

    Create SlotBoostFeedback :)

---
+++ Assets/Scripts/Cruisers/Slots/SlotBoostFeedback.cs
@@ -0,0 +1,56 @@
+﻿using BattleCruisers.Buildables.Boost;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.DataStrctures;
+using BattleCruisers.Utils.PlatformAbstractions.UI;
+using System;
+
+namespace BattleCruisers.Cruisers.Slots
+{
+    public class SlotBoostFeedback
+    {
+        private readonly ITextMesh _textMesh;
+        private readonly IObservableCollection<IBoostProvider> _boostProviders;
+
+        private int NumOfBoosters
+        {
+            set
+            {
+                switch (value)
+                {
+                    case 0:
+                        _textMesh.SetActive(isActive: false);
+                        break;
+
+                    case 1:
+                        _textMesh.SetActive(isActive: true);
+                        _textMesh.Text = "+";
+                        break;
+
+                    case 2:
+                        _textMesh.SetActive(isActive: true);
+                        _textMesh.Text = "++";
+                        break;
+
+                    default:
+                        throw new ArgumentException("Unsupported number of boosters: " + value);
+                }
+            }
+        }
+
+        public SlotBoostFeedback(ITextMesh textMesh, IObservableCollection<IBoostProvider> boostProviders)
+        {
+            Helper.AssertIsNotNull(textMesh, boostProviders);
+
+            _textMesh = textMesh;
+            _boostProviders = boostProviders;
+
+            NumOfBoosters = 0;
+            _boostProviders.Changed += _boostProviders_Changed;
+        }
+
+        private void _boostProviders_Changed(object sender, CollectionChangedEventArgs<IBoostProvider> e)
+        {
+            NumOfBoosters = _boostProviders.Items.Count;
+        }
+    }
+}
