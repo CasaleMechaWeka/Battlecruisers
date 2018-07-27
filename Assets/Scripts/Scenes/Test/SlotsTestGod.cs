029d8b7976d582e97c9f59aded713724cd67b910
Felix Mann <felix.fei.mann@gmail.com>
2018-07-26 18:41:23 -0700

    Create SlotsTest scene :)

---
+++ Assets/Scripts/Scenes/Test/SlotsTestGod.cs
@@ -0,0 +1,27 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.Cruisers.Slots.BuildingPlacement;
+using NSubstitute;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test
+{
+    public class SlotsTestGod : MonoBehaviour
+    {
+        private void Start()
+        {
+            ICruiser parentCruiser = Substitute.For<ICruiser>();
+            ReadOnlyCollection<ISlot> neighbouringSlots = new ReadOnlyCollection<ISlot>(new List<ISlot>());
+            IBuildingPlacer buildingPlacer = Substitute.For<IBuildingPlacer>();
+
+            Slot[] slots = FindObjectsOfType<Slot>();
+
+            foreach (Slot slot in slots)
+            {
+                slot.Initialise(parentCruiser, neighbouringSlots, buildingPlacer);
+            }
+        }
+    }
+}
