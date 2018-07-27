18ccbdf39a4f4ec9be7e4c30a22a9e759a1f36b8
Felix Mann <felix.fei.mann@gmail.com>
2018-07-26 18:31:32 -0700

    Create SlotBoostFeedbackTests :)

---
+++ Assets/Editor/Tests/Cruisers/Slots/SlotBoostFeedbackTests.cs
@@ -0,0 +1,89 @@
+﻿using BattleCruisers.Buildables.Boost;
+using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.Utils.DataStrctures;
+using BattleCruisers.Utils.PlatformAbstractions.UI;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+
+namespace BattleCruisers.Tests.Cruisers.Slots
+{
+    public class SlotBoostFeedbackTests
+    {
+        private SlotBoostFeedback _slotBoostFeedback;
+        private ITextMesh _textMesh;
+        private IObservableCollection<IBoostProvider> _boostProviders;
+        private IBoostProvider _boostProvider;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _textMesh = Substitute.For<ITextMesh>();
+            _boostProviders = new ObservableCollection<IBoostProvider>();
+
+            _slotBoostFeedback = new SlotBoostFeedback(_textMesh, _boostProviders);
+
+            _boostProvider = Substitute.For<IBoostProvider>();
+        }
+
+        [Test]
+        public void InitialState()
+        {
+            _textMesh.Received().SetActive(isActive: false);
+        }
+
+        [Test]
+        public void OneBoostProvider()
+        {
+            _textMesh.ClearReceivedCalls();
+
+            _boostProviders.Add(_boostProvider);
+
+            _textMesh.Received().SetActive(isActive: true);
+            Assert.AreEqual("+", _textMesh.Text);
+        }
+
+        [Test]
+        public void TwoBoostProviders()
+        {
+            _textMesh.ClearReceivedCalls();
+
+            // First boost provider
+            _boostProviders.Add(_boostProvider);
+
+            _textMesh.Received().SetActive(isActive: true);
+            Assert.AreEqual("+", _textMesh.Text);
+
+            // Second boost provider
+            _boostProviders.Add(_boostProvider);
+
+            _textMesh.Received().SetActive(isActive: true);
+            Assert.AreEqual("++", _textMesh.Text);
+        }
+
+        [Test]
+        public void ThreeBoostProviders_Throws()
+        {
+            TwoBoostProviders();
+
+            Assert.Throws<ArgumentException>(() => _boostProviders.Add(_boostProvider));
+        }
+
+        [Test]
+        public void NoBoostProviders()
+        {
+            _textMesh.ClearReceivedCalls();
+
+            // First boost provider
+            _boostProviders.Add(_boostProvider);
+
+            _textMesh.Received().SetActive(isActive: true);
+            Assert.AreEqual("+", _textMesh.Text);
+
+            // Remove boost provider
+            _boostProviders.Remove(_boostProvider);
+
+            _textMesh.Received().SetActive(isActive: false);
+        }
+    }
+}
