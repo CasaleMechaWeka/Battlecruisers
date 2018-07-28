e69026a7680ccff2bdcfb34f3545d65cbe31e5ad
Felix Mann <felix.fei.mann@gmail.com>
2018-07-28 15:57:02 -0700

    Create UnitClickHandler :)

---
+++ Assets/Scripts/UI/BattlScene/Buttons/IUnitClickHandler.cs
@@ -0,0 +1,11 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Units;
+
+namespace BattleCruisers.UI.BattleScene.Buttons
+{
+    public interface IUnitClickHandler
+    {
+        void HandleUnitClick(IBuildableWrapper<IUnit> unitClicked, IFactory unitFactory);
+    }
+}
---
+++ Assets/Scripts/UI/BattlScene/Buttons/UnitClickHandler.cs
@@ -0,0 +1,33 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.BattleScene.Buttons
+{
+    public class UnitClickHandler : IUnitClickHandler
+    {
+        public void HandleUnitClick(IBuildableWrapper<IUnit> unitClicked, IFactory unitFactory)
+        {
+            Helper.AssertIsNotNull(unitClicked, unitFactory);
+
+            if (ReferenceEquals(unitFactory.UnitWrapper, unitClicked))
+            {
+                // Same unit
+                if (unitFactory.IsUnitPaused)
+                {
+                    unitFactory.ResumeBuildingUnit();
+                }
+                else
+                {
+                    unitFactory.PauseBuildingUnit();
+                }
+            }
+            else
+            {
+                // Different unit
+                unitFactory.StartBuildingUnit(unitClicked);
+            }
+        }
+    }
+}
