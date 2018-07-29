1caa0bdab3eb1089d67e04dd3692a9fe4355535c
Felix Mann <felix.fei.mann@gmail.com>
2018-07-29 11:30:40 -0700

    Create DummyUserChosenTargetManager :)

---
+++ Assets/Scripts/Targets/TargetFinders/DummyUserChosenTargetManager.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Buildables;
+using System;
+
+namespace BattleCruisers.Targets.TargetFinders
+{
+    public class DummyUserChosenTargetManager : IUserChosenTargetManager
+    {
+        public ITarget Target { get; set; }
+
+#pragma warning disable 67  // Unused event
+        public event EventHandler<TargetEventArgs> TargetFound;
+        public event EventHandler<TargetEventArgs> TargetLost;
+#pragma warning restore 67  // Unused event
+
+        public void StartFindingTargets()
+        {
+            // empty
+        }
+
+        public void DisposeManagedState()
+        {
+            // empty
+        }
+    }
+}
