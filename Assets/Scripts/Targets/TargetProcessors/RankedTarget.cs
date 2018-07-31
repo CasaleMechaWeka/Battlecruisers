bb762023ae70f3635d919b99c8469a1236988971
Felix Mann <felix.fei.mann@gmail.com>
2018-07-30 17:22:06 -0700

    Create RankedTarget (POD? :P)

---
+++ Assets/Scripts/Targets/TargetProcessors/RankedTarget.cs
@@ -0,0 +1,16 @@
+﻿using BattleCruisers.Buildables;
+
+namespace BattleCruisers.Targets.TargetProcessors
+{
+    public class RankedTarget
+    {
+        public ITarget Target { get; private set; }
+        public int Rank { get; private set; }
+
+        public RankedTarget(ITarget target, int rank)
+        {
+            Target = target;
+            Rank = rank;
+        }
+    }
+}
