8e763eedc45567d926d91b14ba7565db0d3c1a4b
Felix Mann <felix.fei.mann@gmail.com>
2018-07-31 17:55:30 -0700

    Add TODO

--- Assets/Scripts/Targets/TargetFinders/IUserChosenTargetManager.cs
+++ Assets/Scripts/Targets/TargetFinders/IUserChosenTargetManager.cs
@@ -4,7 +4,7 @@ namespace BattleCruisers.Targets.TargetFinders
 {
     public interface IUserChosenTargetManager : 
         ITargetFinder,      // For TargetProcessor/CompositTargetFinder
-        ITargetConsumer,    // For target rankers
+        ITargetConsumer,    // For target rankers  (FELIX  Remove?  Once target rankers no longer use us :P)
         ITargetProvider     // For UI, so user can set their chosen target
     {
         // empty
