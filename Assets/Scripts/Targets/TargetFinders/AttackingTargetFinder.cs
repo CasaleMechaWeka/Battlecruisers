4601f121e62ebc33135f6b92f607671158adec13
Felix Mann <felix.fei.mann@gmail.com>
2018-08-02 17:17:17 -0700

    Create AttackingTargetFinder :)

---
+++ Assets/Scripts/Targets/TargetFinders/AttackingTargetFinder.cs
@@ -0,0 +1,65 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Utils;
+using System;
+
+namespace BattleCruisers.Targets.TargetFinders
+{
+    /// <summary>
+    /// Tracks the highest priority target that is attacking us.
+    /// </summary>
+    /// FELIX  TEst :D
+    public class AttackingTargetFinder : ITargetFinder
+    {
+        private readonly ITargetFilter _attackingTargetFilter;
+        private readonly IDamagable _parentDamagable;
+
+        public event EventHandler<TargetEventArgs> TargetFound;
+        public event EventHandler<TargetEventArgs> TargetLost;
+
+        public AttackingTargetFinder(ITargetFilter targetFilter, IDamagable parentDamagable)
+        {
+            Helper.AssertIsNotNull(targetFilter, parentDamagable);
+
+            _attackingTargetFilter = targetFilter;
+            _parentDamagable = parentDamagable;
+        }
+
+        public void StartFindingTargets()
+        {
+            _parentDamagable.Damaged += _parentDamagable_Damaged;
+        }
+
+        private void _parentDamagable_Damaged(object sender, DamagedEventArgs e)
+        {
+            Logging.Log(Tags.TARGET_FINDER, "Parent damaged by: " + e.DamageSource);
+
+            if (e.DamageSource != null
+                && !e.DamageSource.IsDestroyed
+                && _attackingTargetFilter.IsMatch(e.DamageSource))
+            {
+                e.DamageSource.Destroyed += AttackingTarget_Destroyed;
+
+                if (TargetFound != null)
+                {
+                    TargetFound.Invoke(this, new TargetEventArgs(e.DamageSource));
+                }
+            }
+        }
+
+        private void AttackingTarget_Destroyed(object sender, DestroyedEventArgs e)
+        {
+            e.DestroyedTarget.Destroyed -= AttackingTarget_Destroyed;
+
+            if (TargetLost != null)
+            {
+                TargetLost.Invoke(this, new TargetEventArgs(e.DestroyedTarget));
+            }
+        }
+
+        public void DisposeManagedState()
+        {
+            _parentDamagable.Damaged -= _parentDamagable_Damaged;
+        }
+    }
+}
