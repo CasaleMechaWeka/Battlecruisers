b41ab2fc42e10d0b756217ae2b8b96a5c51ab371
Felix Mann <felix.fei.mann@gmail.com>
2018-08-02 18:01:33 -0700

    Create RankBooster :)

---
+++ Assets/Scripts/Targets/TargetProcessors/Ranking/RankBooster.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Buildables;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Targets.TargetProcessors.Ranking
+{
+    // FELIX  Test :)
+    public class RankBooster : ITargetRanker
+    {
+        private readonly ITargetRanker _baseRanker;
+        private readonly int _rankBoost;
+
+        public RankBooster(ITargetRanker baseRanker, int rankBoost)
+        {
+            Assert.IsNotNull(baseRanker);
+
+            _baseRanker = baseRanker;
+            _rankBoost = rankBoost;
+        }
+
+        public int RankTarget(ITarget target)
+        {
+            return _baseRanker.RankTarget(target) + _rankBoost);
+        }
+    }
+}
