0b902057339a33ec16ac74c489e0360bd27279d3
Felix Mann <felix.fei.mann@gmail.com>
2018-08-04 16:09:02 -0700

    Create AttackBoatUserChosenTarget test scene.  Currently simply attacks highest priority in range target :)

---
+++ Assets/Scripts/Scenes/Test/Naval/AttackBoatUserChosenTargetTestGod.cs
@@ -0,0 +1,39 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Naval
+{
+    public class AttackBoatUserChosenTargetTestGod : MonoBehaviour 
+	{
+        public TestAircraftController inRangeLowPriorityTarget, inRangeHighPriorityTarget, outOfRangeLowPriorityTarget;
+
+		void Start()
+		{
+			Helper helper = new Helper();
+
+            // Targets
+            IList<TestAircraftController> targets = new List<TestAircraftController>()
+            {
+                inRangeLowPriorityTarget,
+                inRangeHighPriorityTarget,
+                outOfRangeLowPriorityTarget
+            };
+            foreach (TestAircraftController target in targets)
+            {
+                target.UseDummyMovementController = true;
+                target.SetTargetType(TargetType.Buildings);
+                helper.InitialiseUnit(target, Faction.Reds);
+                target.StartConstruction();
+            }
+
+            // Ship
+            // FELIX  Pass in user chosen target manager :)
+            ShipController boat = FindObjectOfType<ShipController>();
+			helper.InitialiseUnit(boat, Faction.Blues);
+            boat.StartConstruction();
+        }
+	}
+}
