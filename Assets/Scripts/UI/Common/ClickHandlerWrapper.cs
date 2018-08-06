9b32312cd0f5883c9c832f789aafc40bf00c7905
Felix Mann <felix.fei.mann@gmail.com>
2018-08-05 17:15:18 -0700

    Create ClickHandlerWrapper :)

---
+++ Assets/Scripts/UI/Common/ClickHandlerWrapper.cs
@@ -0,0 +1,28 @@
+﻿using BattleCruisers.Utils;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace BattleCruisers.UI.Common
+{
+    public class ClickHandlerWrapper : MonoBehaviour, IPointerClickHandler
+    {
+        private IClickHandler _clickHandler;
+
+        public IClickHandler GetClickHandler(float doubleClickThresholdInS = Constants.DEFAULT_DOUBLE_CLICK_THRESHOLD_IN_S)
+        {
+            if (_clickHandler == null)
+            {
+                _clickHandler = new ClickHandler(doubleClickThresholdInS);
+            }
+            return _clickHandler;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (_clickHandler != null)
+            {
+                _clickHandler.OnClick(Time.time);
+            }
+        }
+    }
+}
