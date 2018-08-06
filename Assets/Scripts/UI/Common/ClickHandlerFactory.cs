dd2082dd93ca299ed42e9bdf61cb1a749d6780ab
Felix Mann <felix.fei.mann@gmail.com>
2018-08-05 17:15:23 -0700

    Add TODO

--- Assets/Scripts/UI/Common/ClickHandlerFactory.cs
+++ Assets/Scripts/UI/Common/ClickHandlerFactory.cs
@@ -2,6 +2,7 @@
 
 namespace BattleCruisers.UI.Common
 {
+    // FELIX  Delete factory :/
     public class ClickHandlerFactory : IClickHandlerFactory
     {
         public IClickHandler CreateClickHandler(float doubleClickThresholdInS = Constants.DEFAULT_DOUBLE_CLICK_THRESHOLD_IN_S)
