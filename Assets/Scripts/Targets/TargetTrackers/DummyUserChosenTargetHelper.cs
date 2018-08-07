04ce1b56cac9e8e72095029e0a8d656882fb2bf5
Felix Mann <felix.fei.mann@gmail.com>
2018-08-06 17:00:31 -0700

    Create DummyUserChosenTargetHelper :)

---
+++ Assets/Scripts/Targets/TargetTrackers/DummyUserChosenTargetHelper.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.Buildables;
+
+namespace BattleCruisers.Targets.TargetTrackers
+{
+    public class DummyUserChosenTargetHelper : IUserChosenTargetHelper
+    {
+        public void ToggleChosenTarget(ITarget target)
+        {
+            // empty
+        }
+    }
+}
