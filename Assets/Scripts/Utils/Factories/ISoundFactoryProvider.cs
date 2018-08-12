98e8f6a4274d5355f0ea4ef9f0061e95ec07c419
Felix Mann <felix.fei.mann@gmail.com>
2018-08-12 11:51:16 -0700

    Create ISoundFactoryProvider :)

---
+++ Assets/Scripts/Utils/Factories/ISoundFactoryProvider.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.UI.Sound;
+using BattleCruisers.UI.Sound.ProjectileSpawners;
+using BattleCruisers.Utils.Fetchers;
+
+namespace BattleCruisers.Utils.Factories
+{
+    public interface ISoundFactoryProvider
+    {
+        ISoundFetcher SoundFetcher { get; }
+        ISoundManager SoundManager { get; }
+        ISoundPlayerFactory SoundPlayerFactory { get; }
+    }
+}
---
+++ Assets/Scripts/Utils/Factories/SoundFactoryProvider.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.UI.Sound;
+using BattleCruisers.UI.Sound.ProjectileSpawners;
+using BattleCruisers.Utils.Fetchers;
+using BattleCruisers.Utils.Threading;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.Factories
+{
+    public class SoundFactoryProvider : ISoundFactoryProvider
+    {
+        public ISoundFetcher SoundFetcher { get; private set; }
+        public ISoundManager SoundManager { get; private set; }
+        public ISoundPlayerFactory SoundPlayerFactory { get; private set; }
+
+        public SoundFactoryProvider(IVariableDelayDeferrer deferrer)
+		{
+            Assert.IsNotNull(deferrer);
+
+            SoundFetcher = new SoundFetcher();
+            SoundManager = new SoundManager(SoundFetcher, new SoundPlayer());
+            SoundPlayerFactory = new SoundPlayerFactory(SoundFetcher, deferrer);
+        }
+	}
+}
