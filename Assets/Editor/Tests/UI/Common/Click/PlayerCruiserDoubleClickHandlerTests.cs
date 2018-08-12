c484ee0a950f91cb019f124499a86c94de9b2927
Felix Mann <felix.fei.mann@gmail.com>
2018-08-12 15:28:04 -0700

    Create PlayerCruiserDoubleClickHandlerTests :)

---
+++ Assets/Editor/Tests/UI/Common/Click/PlayerCruiserDoubleClickHandlerTests.cs
@@ -0,0 +1,71 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Repairables;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Cruisers.Drones;
+using BattleCruisers.UI.Common.Click;
+using NSubstitute;
+using NUnit.Framework;
+using UnityAsserts = UnityEngine.Assertions;
+
+namespace BattleCruisers.Tests.UI.Common.Click
+{
+    public class PlayerCruiserDoubleClickHandlerTests
+    {
+        private IDoubleClickHandler<ICruiser> _handler;
+        private ICruiser _clickedCruiser;
+        private IDroneManager _droneManager;
+        private IRepairManager _repairManager;
+        private IDroneConsumer _repairDroneConsumer;
+        private IRepairCommand _repairCommand;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _handler = new PlayerCruiserDoubleClickHandler();
+
+            _droneManager = Substitute.For<IDroneManager>();
+            _repairManager = Substitute.For<IRepairManager>();
+            _repairCommand = Substitute.For<IRepairCommand>();
+
+            _clickedCruiser = Substitute.For<ICruiser>();
+            _clickedCruiser.DroneManager.Returns(_droneManager);
+            _clickedCruiser.RepairManager.Returns(_repairManager);
+            _clickedCruiser.RepairCommand.Returns(_repairCommand);
+
+            _repairDroneConsumer = Substitute.For<IDroneConsumer>();
+            _repairManager.GetDroneConsumer(_clickedCruiser).Returns(_repairDroneConsumer);
+
+            UnityAsserts.Assert.raiseExceptions = true;
+        }
+
+        [Test]
+        public void OnDoubleClick_WrongFaction_Throws()
+        {
+            _clickedCruiser.Faction.Returns(Faction.Reds);
+            Assert.Throws<UnityAsserts.AssertionException>(() => _handler.OnDoubleClick(_clickedCruiser));
+        }
+
+        [Test]
+        public void OnDoubleClick_RightFaction_CanRepair_Repairs()
+        {
+            _clickedCruiser.Faction.Returns(Faction.Blues);
+            _repairCommand.CanExecute.Returns(true);
+
+            _handler.OnDoubleClick(_clickedCruiser);
+
+            _droneManager.Received().ToggleDroneConsumerFocus(_repairDroneConsumer);
+        }
+
+
+        [Test]
+        public void OnDoubleClick_RightFaction_CannotRepair_DoesNothing()
+        {
+            _clickedCruiser.Faction.Returns(Faction.Blues);
+            _repairCommand.CanExecute.Returns(false);
+
+            _handler.OnDoubleClick(_clickedCruiser);
+
+            _droneManager.DidNotReceiveWithAnyArgs().ToggleDroneConsumerFocus(null);
+        }
+    }
+}
--- Assets/Scripts/UI/Common/Click/PlayerCruiserDoubleClickHandler.cs
+++ Assets/Scripts/UI/Common/Click/PlayerCruiserDoubleClickHandler.cs
@@ -5,7 +5,6 @@ using UnityEngine.Assertions;
 
 namespace BattleCruisers.UI.Common.Click
 {
-    // FELIX  Test :)
     public class PlayerCruiserDoubleClickHandler : IDoubleClickHandler<ICruiser>
     {
         public void OnDoubleClick(ICruiser playerCruiser)
