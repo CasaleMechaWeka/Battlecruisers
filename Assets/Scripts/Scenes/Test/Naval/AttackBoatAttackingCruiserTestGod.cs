0df611fa980e03836f74ca1e74de30b2bf835a5f
Felix Mann <felix.fei.mann@gmail.com>
2018-08-12 10:59:30 -0700

    Start creating AttackBoatAttackingCruiser test scene :)

---
+++ Assets/Scripts/Scenes/Test/Naval/AttackBoatAttackingCruiserTestGod.cs
@@ -0,0 +1,20 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Naval
+{
+    public class AttackBoatAttackingCruiserTestGod : MonoBehaviour
+    {
+        void Start()
+        {
+            Helper helper = new Helper();
+
+            // Setup fake cruiser
+            TestTarget fakeCruiser = FindObjectOfType<TestTarget>();
+            fakeCruiser.Initialise(Faction.Reds);
+
+            // Setup attack boat
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/Test/Utilities/TestTarget.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.Buildables;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.Utilities
+{
+    public class TestTarget : Target
+    {
+        public TargetType targetType;
+        public override TargetType TargetType { get { return targetType; } }
+
+        private Vector2 _size;
+        public override Vector2 Size { get { return _size; } }
+
+        public void Initialise(Faction faction)
+        {
+            Faction = faction;
+
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            Assert.IsNotNull(renderer);
+            _size = renderer.bounds.size;
+        }
+    }
+}
