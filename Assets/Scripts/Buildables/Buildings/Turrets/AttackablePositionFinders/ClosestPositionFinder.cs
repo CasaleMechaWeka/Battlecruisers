2335fa05274f488895ee8df8fca4dde38edbe208
Felix Mann <felix.fei.mann@gmail.com>
2018-08-11 17:08:34 -0700

    Create ClosestPositionFinder :D

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AttackablePositionFinders/ClosestPositionFinder.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers.Helpers
+{
+    /// <summary>
+    /// Designed for large targets (cruisers), where the attack boat may be
+    /// in range, but out of range of the cruiser center (default attack
+    /// position).
+    /// 
+    /// Hence want to find a position on the cruiser the attack boat can attack
+    /// (ie, front of cruiser).
+    /// </summary>
+    public class ClosestPositionFinder : IAttackablePositionFinder
+    {
+        private readonly float _targetCutoffWidthInM;
+
+        // Do not want the very front or back of the target.  Want to
+        // be slightly inside the target.  Eg, if the raptor is positioned
+        // from 0 to 6, would want the front attack position to be ~1
+        // and the rear attack position ~5.
+        private readonly float _targetBufferInM;
+
+        // Anti-air turret:     0.4 x 0.3
+        // Raptor cruiser:      9 x 6
+        // Longbow cruiser:     19 x 6
+        private const float DEFAULT_TARGET_CUTOFF_WIDTH_IN_M = 3;
+        private const float DEFAULT_TARGET_BUFFER_IN_M = 1;
+        private const float MAX_BUFFER_TO_CUTOFF_RATIO = 0.4f;  // (1 / 3) > 0.4
+
+        public ClosestPositionFinder(
+            float targetCutoffWidthInM = DEFAULT_TARGET_CUTOFF_WIDTH_IN_M, 
+            float targetBufferInM = DEFAULT_TARGET_BUFFER_IN_M)
+        {
+            Assert.IsTrue((targetBufferInM / targetCutoffWidthInM) <= MAX_BUFFER_TO_CUTOFF_RATIO);
+
+            _targetCutoffWidthInM = targetCutoffWidthInM;
+            _targetBufferInM = targetBufferInM;
+        }
+
+        public Vector2 FindClosestAttackablePosition(Vector2 sourcePosition, ITarget target)
+        {
+            if (target.Size.x <= _targetCutoffWidthInM)
+            {
+                // Target is small, so whether attacking the front or center or back
+                // should not make any difference.
+                return target.Position;
+            }
+
+            float attackPositionX = target.Position.x + (FindXChangeInM(target.Size.x) * FindDirectionMultiplier(sourcePosition, target.Position));
+            return new Vector2(attackPositionX, target.Position.y);
+        }
+
+        private float FindXChangeInM(float targetWidthInM)
+        {
+            return targetWidthInM / 2 - _targetBufferInM;
+        }
+
+        private int FindDirectionMultiplier(Vector2 sourcePosition, Vector2 targetPosition)
+        {
+            if (sourcePosition.x < targetPosition.x)
+            {
+                // Target is to the right, want to reduce target position
+                return -1;
+            }
+            else
+            {
+                // Target is to the left, want to increase target position
+                return 1;
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AttackablePositionFinders/IAttackablePositionFinder.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers.Helpers
+{
+    public interface IAttackablePositionFinder
+    {
+        Vector2 FindClosestAttackablePosition(Vector2 sourcePosition, ITarget target);
+    }
+}
