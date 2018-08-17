9d7c20f6bf370f00dedd41e4fc2f9c08c308a87c
Felix Mann <felix.fei.mann@gmail.com>
2018-08-16 17:27:14 -0700

    Undo temporary logging :)

--- Assets/Scripts/Utils/Logging.cs
+++ Assets/Scripts/Utils/Logging.cs
@@ -138,17 +138,13 @@ namespace BattleCruisers.Utils
 
             // Other
             tagsToActiveness.Add(Tags.CAMERA, false);
-            // FELIX
-            tagsToActiveness.Add(Tags.CRUISER, true);
-            //tagsToActiveness.Add(Tags.CRUISER, false);
+            tagsToActiveness.Add(Tags.CRUISER, false);
             tagsToActiveness.Add(Tags.GENERIC, true);
             tagsToActiveness.Add(Tags.LOCAL_BOOSTER, false);
             tagsToActiveness.Add(Tags.PREDICTORS, false);
             tagsToActiveness.Add(Tags.REPAIR_MANAGER, false);
             tagsToActiveness.Add(Tags.SCENE_NAVIGATION, false);
-            // FELIX
-            tagsToActiveness.Add(Tags.SLOTS, true);
-            //tagsToActiveness.Add(Tags.SLOTS, false);
+            tagsToActiveness.Add(Tags.SLOTS, false);
 
             return tagsToActiveness;
 		}
