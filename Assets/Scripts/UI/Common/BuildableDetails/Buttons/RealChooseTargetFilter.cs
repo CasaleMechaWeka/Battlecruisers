43da3bdaa9f8eaf81223fdcb9354299f290eb20a
Felix Mann <felix.fei.mann@gmail.com>
2018-08-16 17:54:28 -0700

    Create RealChooseTargetFilter :D

---
+++ Assets/Scripts/UI/Common/BuildableDetails/Buttons/RealChooseTargetFilter.cs
@@ -0,0 +1,19 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.Common.BuildableDetails.Buttons
+{
+    // FELIX  Test, use
+    public class RealChooseTargetFilter : IFilter<ITarget>
+    {
+        // AI buildings or cruiser
+        public bool IsMatch(ITarget target)
+        {
+            return
+                target != null
+                && target.Faction == Faction.Reds
+                && (target.TargetType == TargetType.Buildings
+                    || target.TargetType == TargetType.Cruiser);
+        }
+    }
+}
