d69249aa70866988186c09ca2f8fed697a6634a6
Felix Mann <felix.fei.mann@gmail.com>
2018-08-16 18:07:40 -0700

    Create StaticFilter :)

---
+++ Assets/Scripts/UI/Filters/StaticFilter.cs
@@ -0,0 +1,19 @@
+﻿using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.Filters
+{
+    public class StaticFilter<TElement> : IFilter<TElement>
+    {
+        private readonly bool _isMatch;
+
+        public StaticFilter(bool isMatch)
+        {
+            _isMatch = isMatch;
+        }
+
+        public bool IsMatch(TElement element)
+        {
+            return _isMatch;
+        }
+    }
+}
