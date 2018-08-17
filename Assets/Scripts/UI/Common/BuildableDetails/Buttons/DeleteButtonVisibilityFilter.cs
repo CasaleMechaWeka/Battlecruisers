1d946fb2b6c15e3410af315498a522c338f3e429
Felix Mann <felix.fei.mann@gmail.com>
2018-08-17 16:31:49 -0700

    Create DeleteButtonVisibilityFilter :)

---
+++ Assets/Scripts/UI/Common/BuildableDetails/Buttons/DeleteButtonVisibilityFilter.cs
@@ -0,0 +1,18 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.Common.BuildableDetails.Buttons
+{
+    // FELIX  Test :)
+    public class DeleteButtonVisibilityFilter : IFilter<ITarget>
+    {
+        // Player bulidnig
+        public bool IsMatch(ITarget target)
+        {
+            return
+                target != null
+                && target.Faction == Faction.Blues
+                && target.TargetType == TargetType.Buildings;
+        }
+    }
+}
