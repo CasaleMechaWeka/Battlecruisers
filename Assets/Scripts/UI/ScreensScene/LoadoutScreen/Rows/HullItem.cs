ebda35b97bc57706f5cc045df5d5215632246b5f
Felix Mann <felix.fei.mann@gmail.com>
2018-08-18 15:42:22 -0700

    Create revolutionary HullItem class :D

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/HullItem.cs
@@ -0,0 +1,57 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Data.Models;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.ItemDetails;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.LockedItems;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.UnlockedItems;
+using BattleCruisers.Utils;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows
+{
+    // FELIX  Potentially avoid duplicate code with BuildableItems?
+    public class HullItem : MonoBehaviour
+    {
+        // FELIX  Do not cache unused fields :/
+        private IItemDetailsManager<ICruiser> _hullDetailsManager;
+        private IHullItemsRow _hullItemsRow;
+        private IGameModel _gameModel;
+        private ICruiser _hull;
+        private HullKey _hullKey;
+        private UnlockedHullItem _unlockedHullItem;
+        private LockedItem _lockedItem;
+
+        public void Initialise(
+            IItemDetailsManager<ICruiser> hullDetailsManager,
+            IHullItemsRow hullItemsRow,
+            IGameModel gameModel,
+            ICruiser hull,
+            HullKey hullKey)
+        {
+            Helper.AssertIsNotNull(hullDetailsManager, hullItemsRow, gameModel, hull, hullKey);
+
+            _hullDetailsManager = hullDetailsManager;
+            _hullItemsRow = hullItemsRow;
+            _gameModel = gameModel;
+            _hull = hull;
+            _hullKey = hullKey;
+
+            _unlockedHullItem = GetComponentInChildren<UnlockedHullItem>();
+            Assert.IsNotNull(_unlockedHullItem);
+            _unlockedHullItem.Initialise(_hull, _hullDetailsManager);
+
+            _lockedItem = GetComponentInChildren<LockedItem>();
+            Assert.IsNotNull(_lockedItem);
+            _lockedItem.Initialise();
+        }
+
+        public void RefreshLockedStatus()
+        {
+            bool isHullUnlocked = _gameModel.UnlockedHulls.Contains(_hullKey);
+
+            _unlockedHullItem.IsVisible = isHullUnlocked;
+            _lockedItem.IsVisible = !isHullUnlocked;
+        }
+    }
+}
