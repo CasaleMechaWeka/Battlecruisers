eda021b461a3cf150d9655c0a189f466b6eb8a53
Felix Mann <felix.fei.mann@gmail.com>
2018-08-19 14:06:27 -0700

    Add TODO :)

--- Assets/Scripts/UI/BattlScene/Presentables/PresentableComponent.cs
+++ Assets/Scripts/UI/BattlScene/Presentables/PresentableComponent.cs
@@ -2,6 +2,7 @@
 
 namespace BattleCruisers.UI.BattleScene.Presentables
 {
+    // FELIX  Test :)
     public sealed class PresentableComponent : IPresentableComponent
     {
         private readonly IList<IPresentable> _childPresentables;
