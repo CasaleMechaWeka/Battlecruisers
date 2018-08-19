657ffc434023f4bf258b9178a8c18e1005128bf1
Felix Mann <felix.fei.mann@gmail.com>
2018-08-18 19:02:40 -0700

    Add TODO :)

--- Assets/Scripts/UI/BattlScene/IPresentable.cs
+++ Assets/Scripts/UI/BattlScene/IPresentable.cs
@@ -3,6 +3,9 @@
     public interface IPresentable
     {
         // About to be shown
+        // FELIX  I don't like the lack of type saftery of activationParameter :/  
+        // => Create generic interface that takes activationParameter type?  
+        // (Only used by UnitsMenuController?)
         void OnPresenting(object activationParameter);
 
         // About to be hidden
