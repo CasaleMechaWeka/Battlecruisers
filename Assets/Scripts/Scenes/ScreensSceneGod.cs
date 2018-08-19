5249b80a748908b60e2d3c17b9065e88bee74ae9
Felix Mann <felix.fei.mann@gmail.com>
2018-08-19 14:15:02 -0700

    Do not show loadout screen automatically :)

--- Assets/Scripts/Scenes/ScreensSceneGod.cs
+++ Assets/Scripts/Scenes/ScreensSceneGod.cs
@@ -62,8 +62,7 @@ namespace BattleCruisers.Scenes
             // TEMP  Go to specific screen :)
             //GoToSettingsScreen();
             //GoToLevelsScreen();
-            // FELIX  Comment :)
-            GoToLoadoutScreen();
+            //GoToLoadoutScreen();
         }
         
         private void GoToPostBattleScreen()
