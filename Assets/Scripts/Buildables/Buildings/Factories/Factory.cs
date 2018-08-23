d073d01ddfc29a131efc4be29806d7fe46e5fbe8
Felix Mann <felix.fei.mann@gmail.com>
2018-08-22 17:01:02 -0700

    Factory is done focus toggleable even if unit is paused, we simply reactivate the drone consumer :)

--- Assets/Scripts/Buildables/Buildings/Factories/Factory.cs
+++ Assets/Scripts/Buildables/Buildings/Factories/Factory.cs
@@ -42,16 +42,6 @@ namespace BattleCruisers.Buildables.Buildings.Factories
             }
         }
 
-        protected override bool IsDroneConsumerFocusable
-        {
-            get
-            {
-                return
-                    base.IsDroneConsumerFocusable
-                    && !IsUnitPaused;
-            }
-        }
-
         private IBuildableWrapper<IUnit> _unitWrapper;
 		public IBuildableWrapper<IUnit> UnitWrapper 
 		{ 
@@ -283,8 +273,6 @@ namespace BattleCruisers.Buildables.Buildings.Factories
 
         protected override void ToggleDroneConsumerFocusCommandExecute()
         {
-            // FELIX  Should no longer work, because CanExecute checks if unit is paused :/
-            // => Remove this or the CanExecute...
             if (IsUnitPaused)
             {
                 // Cannot focus on drone consumer if they are not activated.
