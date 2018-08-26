77cd39f035902c2cfab4bf71e2b23e940f681100
Felix Mann <felix.fei.mann@gmail.com>
2018-08-26 10:11:41 -0700

    Create ButtonVisibilityFilters class :)

---
+++ Assets/Scripts/UI/BattlScene/Buttons/Filters/ButtonVisibilityFilters.cs
@@ -0,0 +1,32 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.UI.Filters;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.BattleScene.Buttons.Filters
+{
+    public class ButtonVisibilityFilters : IButtonVisibilityFilters
+    {
+        public IBroadcastingFilter<IBuildable> BuildableButtonVisibilityFilter  { get; private set; }
+        public IBroadcastingFilter<BuildingCategory> CategoryButtonVisibilityFilter  { get; private set; }
+        public IFilter<ITarget> ChooseTargetButtonVisiblityFilter  { get; private set; }
+        public IFilter<ITarget> DeletButtonVisiblityFilter  { get; private set; }
+        public BasicFilter BackButtonVisibilityFilter  { get; private set; }
+
+        public ButtonVisibilityFilters(
+            IBroadcastingFilter<IBuildable> buildableButtonVisibilityFilter,
+            IBroadcastingFilter<BuildingCategory> categoryButtonVisibilityFilter,
+            IFilter<ITarget> chooseTargetButtonVisiblityFilter,
+            IFilter<ITarget> deletButtonVisiblityFilter,
+            BasicFilter backButtonVisibilityFilter)
+        {
+            Helper.AssertIsNotNull(buildableButtonVisibilityFilter, categoryButtonVisibilityFilter, chooseTargetButtonVisiblityFilter, deletButtonVisiblityFilter, backButtonVisibilityFilter);
+
+            BuildableButtonVisibilityFilter = buildableButtonVisibilityFilter;
+            CategoryButtonVisibilityFilter = categoryButtonVisibilityFilter;
+            ChooseTargetButtonVisiblityFilter = chooseTargetButtonVisiblityFilter;
+            DeletButtonVisiblityFilter = deletButtonVisiblityFilter;
+            BackButtonVisibilityFilter = backButtonVisibilityFilter;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/BattlScene/Buttons/Filters/IButtonVisibilityFilters.cs
@@ -0,0 +1,16 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.UI.Filters;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.BattleScene.Buttons.Filters
+{
+    public interface IButtonVisibilityFilters
+    {
+        IBroadcastingFilter<IBuildable> BuildableButtonVisibilityFilter { get; }
+        IBroadcastingFilter<BuildingCategory> CategoryButtonVisibilityFilter { get; }
+        IFilter<ITarget> ChooseTargetButtonVisiblityFilter { get; }
+        IFilter<ITarget> DeletButtonVisiblityFilter { get; }
+        BasicFilter BackButtonVisibilityFilter { get; }
+    }
+}
