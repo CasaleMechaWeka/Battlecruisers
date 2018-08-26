46d8b0a4909f9b0d291060ab0d3878cd6fcfb835
Felix Mann <felix.fei.mann@gmail.com>
2018-08-25 17:48:52 -0700

    Create BuildingMenus :D

--- Assets/Scripts/UI/BattlScene/BuildMenus/BuildingMenus.cs
+++ Assets/Scripts/UI/BattlScene/BuildMenus/BuildingMenus.cs
@@ -1,53 +1,53 @@
-﻿//using BattleCruisers.Buildables;
-//using BattleCruisers.Buildables.Units;
-//using BattleCruisers.UI.BattleScene.Buttons;
-//using BattleCruisers.UI.BattleScene.Manager;
-//using BattleCruisers.UI.BattleScene.Presentables;
-//using BattleCruisers.UI.Filters;
-//using BattleCruisers.Utils;
-//using BattleCruisers.Utils.Sorting;
-//using System.Collections.Generic;
-//using System.Linq;
-//using UnityEngine;
-//using UnityEngine.Assertions;
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.UI.BattleScene.Manager;
+using BattleCruisers.UI.BattleScene.Presentables;
+using BattleCruisers.UI.Filters;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Fetchers;
+using BattleCruisers.Utils.Sorting;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
 
-//namespace BattleCruisers.UI.BattleScene.BuildMenus
-//{
-//    // FELIX  Avoid duplicate code with BulidingMenus?
-//    public class UnitMenus : MonoBehaviour
-//    {
-//        private IDictionary<UnitCategory, PresentableController> _unitGroupPanels;
+namespace BattleCruisers.UI.BattleScene.BuildMenus
+{
+    // FELIX  Avoid duplicate code with UnitMenus?
+    public class BuildingMenus : MonoBehaviour
+    {
+        private IDictionary<BuildingCategory, PresentableController> _buildingCategoryPanels;
 
-//        public void Initialise(
-//            IDictionary<UnitCategory, IList<IBuildableWrapper<IUnit>>> units,
-//            IUIManager uiManager,
-//            IBroadcastingFilter<IBuildable> shouldBeEnabledFilter,
-//            IUnitClickHandler unitClickHandler,
-//            IBuildableSorter<IUnit> unitSorter)
-//        {
-//            Helper.AssertIsNotNull(units, uiManager, shouldBeEnabledFilter, unitClickHandler);
+        public void Initialise(
+            IDictionary<BuildingCategory, IList<IBuildableWrapper<IBuilding>>> buildings,
+            IUIManager uiManager,
+            IBroadcastingFilter<IBuildable> shouldBeEnabledFilter,
+            ISpriteProvider spriteProvider,
+            IBuildableSorter<IBuilding> buildingSorter)
+        {
+            Helper.AssertIsNotNull(buildings, uiManager, shouldBeEnabledFilter, spriteProvider, buildingSorter);
 
-//            IList<NEWUnitsMenuController> unitMenus = GetComponentsInChildren<NEWUnitsMenuController>().ToList();
-//            Assert.AreEqual(units.Count, unitMenus.Count);
+            IList<NEWBuildingsMenuController> buildingMenus = GetComponentsInChildren<NEWBuildingsMenuController>().ToList();
+            Assert.AreEqual(buildings.Count, buildingMenus.Count);
 
-//            _unitGroupPanels = new Dictionary<UnitCategory, PresentableController>();
+            _buildingCategoryPanels = new Dictionary<BuildingCategory, PresentableController>();
 
-//            int i = 0;
+            int i = 0;
 
-//            foreach (KeyValuePair<UnitCategory, IList <IBuildableWrapper<IUnit>>> pair in units)
-//            {
-//                NEWUnitsMenuController unitMenu = unitMenus[i];
-//                IList<IBuildableWrapper<IUnit>> sortedUnits = unitSorter.Sort(pair.Value);
-//                unitMenu.Initialise(sortedUnits, uiManager, shouldBeEnabledFilter, unitClickHandler);
-//                _unitGroupPanels.Add(pair.Key, unitMenu);
-//                i++;
-//            }
-//        }
+            foreach (KeyValuePair<BuildingCategory, IList<IBuildableWrapper<IBuilding>>> pair in buildings)
+            {
+                NEWBuildingsMenuController buildingMenu = buildingMenus[i];
+                IList<IBuildableWrapper<IBuilding>> sortedBuildings = buildingSorter.Sort(pair.Value);
+                buildingMenu.Initialise(sortedBuildings, uiManager, shouldBeEnabledFilter, spriteProvider);
+                _buildingCategoryPanels.Add(pair.Key, buildingMenu);
+                i++;
+            }
+        }
 
-//        public PresentableController GetUnitsPanel(UnitCategory unitCategory)
-//        {
-//            Assert.IsTrue(_unitGroupPanels.ContainsKey(unitCategory));
-//            return _unitGroupPanels[unitCategory];
-//        }
-//    }
-//}
+        public PresentableController GetBuildingsPanel(BuildingCategory buildingCategory)
+        {
+            Assert.IsTrue(_buildingCategoryPanels.ContainsKey(buildingCategory));
+            return _buildingCategoryPanels[buildingCategory];
+        }
+    }
+}
