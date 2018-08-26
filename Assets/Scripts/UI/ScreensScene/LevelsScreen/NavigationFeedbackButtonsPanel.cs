9b22af691ccd0d3a30796b90f0e1aec9e7e87300
Felix Mann <felix.fei.mann@gmail.com>
2018-08-26 14:55:29 -0700

    Create NavigationFeedbackButtonsPanel :)

---
+++ Assets/Scripts/UI/ScreensScene/LevelsScreen/NavigationFeedbackButtonsPanel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.ScreensScene.LevelsScreen
+{
+    public class NavigationFeedbackButtonsPanel : MonoBehaviour
+    {
+        public void Initialise(LevelsScreenController levelsScreenController, int numOfSets)
+        {
+            Assert.IsNotNull(levelsScreenController);
+
+            NavigationFeedbackButtonController[] feedbackButtons = GetComponentsInChildren<NavigationFeedbackButtonController>();
+            Assert.AreEqual(numOfSets, feedbackButtons.Length);
+
+            for (int i = 0; i < numOfSets; ++i)
+            {
+                feedbackButtons[i].Initialise(levelsScreenController, i);
+            }
+        }
+    }
+}
