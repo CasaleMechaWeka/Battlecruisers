af7d90771274baa4ad78e6acc930db85bfa936e7
Felix Mann <felix.fei.mann@gmail.com>
2018-08-27 17:40:27 -0700

    Create DummySoundPlayer :P

---
+++ Assets/Scripts/UI/Sound/DummySoundPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.UI.Sound
+{
+    public class DummySoundPlayer : ISoundPlayer
+    {
+        public void PlaySound(ISoundKey soundKey, Vector2 position)
+        {
+            // empty
+        }
+    }
+}
