abff89fbedb92dd49655a7cc5744157108ff2fe6
Felix Mann <felix.fei.mann@gmail.com>
2018-08-28 16:27:43 -0700

    Create Ultralisk class, to override completion sound :)

---
+++ Assets/Scripts/Buildables/Buildings/Factories/Ultralisk.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Data.Static;
+using BattleCruisers.UI.Sound;
+
+namespace BattleCruisers.Buildables.Buildings.Factories
+{
+    public class Ultralisk : DroneStation
+    {
+        protected override ISoundKey ConstructionCompletedSoundKey { get { return SoundKeys.Completed.Ultra; } }
+    }
+}
