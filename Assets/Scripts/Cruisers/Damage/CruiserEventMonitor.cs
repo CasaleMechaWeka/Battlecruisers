17143ebd569a4bdcb7ec32fbb7d86de933b885a2
Felix Mann <felix.fei.mann@gmail.com>
2018-08-28 19:18:19 -0700

    Create CruiserEventMonitor :D

---
+++ Assets/Scripts/Cruisers/Damage/CruiserEventMonitor.cs
@@ -0,0 +1,46 @@
+﻿using BattleCruisers.Data.Static;
+using BattleCruisers.UI.Sound;
+using BattleCruisers.Utils;
+using System;
+
+namespace BattleCruisers.Cruisers.Damage
+{
+    /// <summary>
+    /// Plays sounds to the user when:
+    /// 1. The cruiser (or its buildings) are damaged
+    /// 2. The cruiser reaches critical health (say a third)
+    /// </summary>
+    /// FELIX  Test
+    /// FELIX  Use :)
+    public class CruiserEventMonitor
+    {
+        private readonly IHealthThresholdMonitor _cruiserHealthThresholdMonitor;
+        private readonly ICruiserDamageMonitor _cruiserDamageMonitor;
+        private readonly ISoundPlayer _soundPlayer;
+
+        public CruiserEventMonitor(
+            IHealthThresholdMonitor cruiserHealthThresholdMonitor,
+            ICruiserDamageMonitor cruiserDamageMonitor,
+            ISoundPlayer soundPlayer)
+        {
+            Helper.AssertIsNotNull(cruiserHealthThresholdMonitor, cruiserDamageMonitor, soundPlayer);
+
+            _cruiserHealthThresholdMonitor = cruiserHealthThresholdMonitor;
+            _cruiserDamageMonitor = cruiserDamageMonitor;
+            _soundPlayer = soundPlayer;
+
+            _cruiserHealthThresholdMonitor.ThresholdReached += _cruiserHealthThresholdMonitor_ThresholdReached;
+            _cruiserDamageMonitor.CruiserOrBuildingDamaged += _cruiserDamageMonitor_CruiserOrBuildingDamaged;
+        }
+
+        private void _cruiserHealthThresholdMonitor_ThresholdReached(object sender, EventArgs e)
+        {
+            _soundPlayer.PlaySound(SoundKeys.Events.CruiserSignificantlyDamaged);
+        }
+
+        private void _cruiserDamageMonitor_CruiserOrBuildingDamaged(object sender, EventArgs e)
+        {
+            _soundPlayer.PlaySound(SoundKeys.Events.CruiserUnderAttack);
+        }
+    }
+}
