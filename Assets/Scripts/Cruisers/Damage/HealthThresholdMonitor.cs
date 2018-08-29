2f84a4b246e47a69494b1afd9b9cee612d847ae3
Felix Mann <felix.fei.mann@gmail.com>
2018-08-28 17:34:07 -0700

    Create HealthThresholdMonitor :D

---
+++ Assets/Scripts/Cruisers/Damage/HealthThresholdMonitor.cs
@@ -0,0 +1,44 @@
+﻿using BattleCruisers.Buildables;
+using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Cruisers.Damage
+{
+    public class HealthThresholdMonitor : IHealthThresholdMonitor
+    {
+        private readonly IDamagable _damagable;
+        private readonly float _threshold;
+        private bool _wasAboveThreshold;
+
+        private const float MIN_THRESHOLD = 0;
+        private const float MAX_THRESHOLD = 1;
+
+        public event EventHandler ThresholdReached;
+
+        public HealthThresholdMonitor(IDamagable damagable, float thresholdProportion)
+        {
+            Assert.IsNotNull(damagable);
+            Assert.IsTrue(thresholdProportion > MIN_THRESHOLD);
+            Assert.IsTrue(thresholdProportion < MAX_THRESHOLD);
+            Assert.AreEqual(damagable.MaxHealth, damagable.Health);
+
+            _damagable = damagable;
+            _threshold = thresholdProportion * damagable.MaxHealth;
+            _wasAboveThreshold = true;
+
+            _damagable.HealthChanged += _damagable_HealthChanged;
+        }
+
+        private void _damagable_HealthChanged(object sender, HealthChangedEventArgs e)
+        {
+            if (_wasAboveThreshold
+                && _damagable.Health < _threshold
+                && ThresholdReached != null)
+            {
+                ThresholdReached.Invoke(this, EventArgs.Empty);
+            }
+
+            _wasAboveThreshold = _damagable.Health >= _threshold;
+        }
+    }
+}
---
+++ Assets/Scripts/Cruisers/Damage/IHealthThresholdMonitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BattleCruisers.Cruisers.Damage
+{
+    /// <summary>
+    /// Keeps track of a damagable, and emits an event when that damagable's 
+    /// health drops below a specified threshold.
+    /// </summary>
+    public interface IHealthThresholdMonitor
+    {
+        event EventHandler ThresholdReached;
+    }
+}
