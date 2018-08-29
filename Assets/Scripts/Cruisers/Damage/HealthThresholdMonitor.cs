14364904fa81f52b5ba77eca94812708e959bef5
Felix Mann <felix.fei.mann@gmail.com>
2018-08-28 17:59:06 -0700

    Move comment from interface to class :P

--- Assets/Scripts/Cruisers/Damage/HealthThresholdMonitor.cs
+++ Assets/Scripts/Cruisers/Damage/HealthThresholdMonitor.cs
@@ -4,6 +4,10 @@ using UnityEngine.Assertions;
 
 namespace BattleCruisers.Cruisers.Damage
 {
+    /// <summary>
+    /// Keeps track of a damagable, and emits an event when that damagable's 
+    /// health drops below a specified threshold.
+    /// </summary>
     public class HealthThresholdMonitor : IHealthThresholdMonitor
     {
         private readonly IDamagable _damagable;
--- Assets/Scripts/Cruisers/Damage/IHealthThresholdMonitor.cs
+++ Assets/Scripts/Cruisers/Damage/IHealthThresholdMonitor.cs
@@ -2,10 +2,6 @@
 
 namespace BattleCruisers.Cruisers.Damage
 {
-    /// <summary>
-    /// Keeps track of a damagable, and emits an event when that damagable's 
-    /// health drops below a specified threshold.
-    /// </summary>
     public interface IHealthThresholdMonitor
     {
         event EventHandler ThresholdReached;
