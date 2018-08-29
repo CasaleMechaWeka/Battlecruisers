2ce08b6447fbb7a64b89daecd28ad3573b70d295
Felix Mann <felix.fei.mann@gmail.com>
2018-08-28 18:21:32 -0700

    Create CruiserDamagedMonitorDebouncer :)

---
+++ Assets/Scripts/Cruisers/Damage/CruiserDamagedMonitorDebouncer.cs
@@ -0,0 +1,50 @@
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.PlatformAbstractions;
+using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Cruisers.Damage
+{
+    public class CruiserDamagedMonitorDebouncer : ICruiserDamageMonitor, IManagedDisposable
+    {
+        private readonly ICruiserDamageMonitor _monitor;
+        private readonly ITime _time;
+        private readonly float _debounceTimeInS;
+        private float _lastChangeTimestamp;
+
+        private const float DEFAULT_DEBOUNCE_TIME_IN_S = 20;
+
+        public event EventHandler CruiserOrBuildingDamaged;
+
+        public CruiserDamagedMonitorDebouncer(ICruiserDamageMonitor monitor, ITime time, float debounceTimeInS = DEFAULT_DEBOUNCE_TIME_IN_S)
+        {
+            Helper.AssertIsNotNull(monitor, time);
+            Assert.IsTrue(debounceTimeInS > 0);
+
+            _monitor = monitor;
+            _time = time;
+            _debounceTimeInS = debounceTimeInS;
+            _lastChangeTimestamp = float.MinValue;
+
+            _monitor.CruiserOrBuildingDamaged += _monitor_CruiserOrBuildingDamaged;
+        }
+
+        private void _monitor_CruiserOrBuildingDamaged(object sender, EventArgs e)
+        {
+            if ((_time.TimeSinceGameStartInS - _lastChangeTimestamp) >= _debounceTimeInS)
+            {
+                if (CruiserOrBuildingDamaged != null)
+                {
+                    CruiserOrBuildingDamaged.Invoke(this, EventArgs.Empty);
+                }
+
+                _lastChangeTimestamp = _time.TimeSinceGameStartInS;
+            }
+        }
+        
+        public void DisposeManagedState()
+        {
+            _monitor.CruiserOrBuildingDamaged -= _monitor_CruiserOrBuildingDamaged;
+        }
+    }
+}
