95465fdd2062a9920b57dee01e355b08c37fbe9b
Felix Mann <felix.fei.mann@gmail.com>
2018-08-29 17:56:23 -0700

    Create DroneManagerMonitor :D

---
+++ Assets/Scripts/Cruisers/Drones/DroneManagerMonitor.cs
@@ -0,0 +1,63 @@
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.DataStrctures;
+using System;
+using System.Linq;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Cruisers.Drones
+{
+    // FELIX  test
+    // FELIX  use :D
+    public class DroneManagerMonitor : IDroneManagerMonitor, IManagedDisposable
+    {
+        private readonly IDroneManager _droneManager;
+        private int _previousNumOfDrones;
+
+        public event EventHandler DroneNumIncreased;
+        public event EventHandler IdleDrones;
+
+        public DroneManagerMonitor(IDroneManager droneManager)
+        {
+            Assert.IsNotNull(droneManager);
+
+            _droneManager = droneManager;
+            _previousNumOfDrones = _droneManager.NumOfDrones;
+
+            _droneManager.DroneNumChanged += _droneManager_DroneNumChanged;
+            _droneManager.DroneConsumers.Changed += DroneConsumers_Changed;
+        }
+
+        private void _droneManager_DroneNumChanged(object sender, DroneNumChangedEventArgs e)
+        {
+            if (e.NewNumOfDrones > _previousNumOfDrones
+                && DroneNumIncreased != null)
+            {
+                DroneNumIncreased.Invoke(this, EventArgs.Empty);
+            }
+
+            _previousNumOfDrones = e.NewNumOfDrones;
+        }
+
+        private void DroneConsumers_Changed(object sender, CollectionChangedEventArgs<IDroneConsumer> e)
+        {
+            if (AreAllDroneConsumersIdle()
+                && IdleDrones != null)
+            {
+                IdleDrones.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        private bool AreAllDroneConsumersIdle()
+        {
+            return
+                _droneManager.DroneConsumers.Items
+                    .All(droneConsumer => droneConsumer.State == DroneConsumerState.Idle);
+        }
+
+        public void DisposeManagedState()
+        {
+            _droneManager.DroneNumChanged -= _droneManager_DroneNumChanged;
+            _droneManager.DroneConsumers.Changed -= DroneConsumers_Changed;
+        }
+    }
+}
---
+++ Assets/Scripts/Cruisers/Drones/IDroneManagerMonitor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BattleCruisers.Cruisers.Drones
+{
+    public interface IDroneManagerMonitor
+    {
+        event EventHandler DroneNumIncreased;
+        event EventHandler IdleDrones;
+    }
+}
