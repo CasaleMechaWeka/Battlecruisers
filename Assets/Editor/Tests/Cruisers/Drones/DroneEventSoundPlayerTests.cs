69cc512e431f8b8a83fbfc72122f1c4093348a7b
Felix Mann <felix.fei.mann@gmail.com>
2018-08-30 16:45:03 -0700

    Create DroneEventSoundPlayerTests :)

---
+++ Assets/Editor/Tests/Cruisers/Drones/DroneEventSoundPlayerTests.cs
@@ -0,0 +1,37 @@
+﻿using BattleCruisers.Cruisers.Drones;
+using BattleCruisers.Data.Static;
+using BattleCruisers.UI.Sound;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.Cruisers.Drones
+{
+    public class DroneEventSoundPlayerTests
+    {
+        private DroneEventSoundPlayer _droneEventSoundPlayer;
+        private IDroneManagerMonitor _droneManagerMonitor;
+        private ISoundPlayer _soundPlayer;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _droneManagerMonitor = Substitute.For<IDroneManagerMonitor>();
+            _soundPlayer = Substitute.For<ISoundPlayer>();
+            _droneEventSoundPlayer = new DroneEventSoundPlayer(_droneManagerMonitor, _soundPlayer);
+        }
+
+        [Test]
+        public void DroneNumberIncrease_PlaysSound()
+        {
+            _droneManagerMonitor.DroneNumIncreased += Raise.Event();
+            _soundPlayer.Received().PlaySound(SoundKeys.Events.DronesNewDronesReady);
+        }
+
+        [Test]
+        public void IdleDrones_PlaysSound()
+        {
+            _droneManagerMonitor.IdleDrones += Raise.Event();
+            _soundPlayer.Received().PlaySound(SoundKeys.Events.DronesIdle);
+        }
+    }
+}
