124863e354cbd56f8ff4309834c44fd2239d93f8
Felix Mann <felix.fei.mann@gmail.com>
2018-08-30 17:49:49 -0700

    Create SequentialSoundPlayer :D

--- Assets/Scripts/UI/Sound/ISoundPlayer.cs
+++ Assets/Scripts/UI/Sound/ISoundPlayer.cs
@@ -4,7 +4,9 @@ namespace BattleCruisers.UI.Sound
 {
     public interface ISoundPlayer
     {
+        // FELIX  Only used by SequentialSoundPlayer?  Create 2 interfaces?
         void PlaySound(ISoundKey soundKey);
+        // FELIX  Only used by SoundPlayer?  Create 2 interfaces?
         void PlaySound(ISoundKey soundKey, Vector2 position);
     }
 }
---
+++ Assets/Scripts/UI/Sound/SequentialSoundPlayer.cs
@@ -0,0 +1,55 @@
+﻿using BattleCruisers.Utils.Fetchers;
+using BattleCruisers.Utils.PlatformAbstractions.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Sound
+{
+    // FELIX  Test
+    public class SequentialSoundPlayer : ISoundPlayer
+    {
+        private readonly IAwaitableAudioSource _audioSource;
+        // FELIX  Initialise :P
+        private readonly ISoundFetcher _soundFetcher;
+        private readonly Queue<ISoundKey> _soundsToPlay;
+
+        public SequentialSoundPlayer(IAwaitableAudioSource audioSource)
+        {
+            Assert.IsNotNull(audioSource);
+
+            _audioSource = audioSource;
+            _soundsToPlay = new Queue<ISoundKey>();
+        }
+
+        public void PlaySound(ISoundKey soundKey)
+        {
+            _soundsToPlay.Enqueue(soundKey);
+
+            if (_soundsToPlay.Count == 1)
+            {
+                // Not currently playing any sound, so can play new sound immediately
+                PlayOldestSound();
+            }
+        }
+
+        private void PlayOldestSound()
+        {
+            IAudioClipWrapper audioClip = _soundFetcher.GetSound(_soundsToPlay.Peek());
+            _audioSource.Play(audioClip, OnSoundCompleted);
+
+        }
+
+        private void OnSoundCompleted()
+        {
+            Assert.AreNotEqual(_soundsToPlay.Count, 0);
+            _soundsToPlay.Dequeue();
+            PlayOldestSound();
+        }
+
+        public void PlaySound(ISoundKey soundKey, Vector2 position)
+        {
+            // FELIX
+        }
+    }
+}
