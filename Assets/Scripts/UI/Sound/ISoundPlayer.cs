8fb5ccd1d4c7eedbeaa2e92e7a8a7da1f4518eb5
Felix Mann <felix.fei.mann@gmail.com>
2018-09-05 16:47:17 -0700

    Revert "Create SequentialSoundPlayer :D"
    This reverts commit 124863e354cbd56f8ff4309834c44fd2239d93f8.

--- Assets/Scripts/UI/Sound/ISoundPlayer.cs
+++ Assets/Scripts/UI/Sound/ISoundPlayer.cs
@@ -4,9 +4,7 @@ namespace BattleCruisers.UI.Sound
 {
     public interface ISoundPlayer
     {
-        // FELIX  Only used by SequentialSoundPlayer?  Create 2 interfaces?
         void PlaySound(ISoundKey soundKey);
-        // FELIX  Only used by SoundPlayer?  Create 2 interfaces?
         void PlaySound(ISoundKey soundKey, Vector2 position);
     }
 }
