5d3fa42b3c76d27ec2c2cd01099ce20742aeff99
Felix Mann <felix.fei.mann@gmail.com>
2018-09-05 17:05:02 -0700

    Create PrioritisedSoundKey :)

---
+++ Assets/Scripts/UI/Sound/PrioritisedSoundKey.cs
@@ -0,0 +1,22 @@
+﻿namespace BattleCruisers.UI.Sound
+{
+    public enum SoundPriority
+    {
+        VeryHigh = 4,
+        High = 3,
+        Normal = 2,
+        Low = 1
+    }
+
+    public class PrioritisedSoundKey
+    {
+        public ISoundKey Key { get; private set; }
+        public SoundPriority Priority { get; private set; }
+
+        public PrioritisedSoundKey(ISoundKey key, SoundPriority priority)
+        {
+            Key = key;
+            Priority = priority;
+        }
+    }
+}
