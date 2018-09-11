bde898c3ae3529cecc7a91bca0f9b4841503f593
Felix Mann <felix.fei.mann@gmail.com>
2018-09-11 16:43:44 -0700

    Create UltrasConstructionMonitor :D

---
+++ Assets/Scripts/Cruisers/Construction/UltrasConstructionMonitor.cs
@@ -0,0 +1,50 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Data.Static;
+using BattleCruisers.UI.Sound;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Cruisers.Construction
+{
+    // FELIX  Test :)
+    public class UltrasConstructionMonitor : IManagedDisposable
+    {
+        private readonly ICruiserController _cruiser;
+        private readonly IUnitConstructionMonitor _unitConstructionMonitor;
+        private readonly IPrioritisedSoundPlayer _soundPlayer;
+
+        public UltrasConstructionMonitor(ICruiserController cruiser, IUnitConstructionMonitor unitConstructionMonitor, IPrioritisedSoundPlayer soundPlayer)
+        {
+            Helper.AssertIsNotNull(cruiser, unitConstructionMonitor, soundPlayer);
+
+            _cruiser = cruiser;
+            _unitConstructionMonitor = unitConstructionMonitor;
+            _soundPlayer = soundPlayer;
+
+            _cruiser.BuildingStarted += _cruiser_BuildingStarted;
+            _unitConstructionMonitor.StartedBuildingUnit += _unitConstructionMonitor_StartedBuildingUnit;
+        }
+
+        private void _cruiser_BuildingStarted(object sender, StartedBuildingConstructionEventArgs e)
+        {
+            if (e.Buildable.Category == BuildingCategory.Ultra)
+            {
+                _soundPlayer.PlaySound(PrioritisedSoundKeys.Events.EnemyStartedUltra);
+            }
+        }
+
+        private void _unitConstructionMonitor_StartedBuildingUnit(object sender, StartedUnitConstructionEventArgs e)
+        {
+            if (e.Buildable.IsUltra)
+            {
+                _soundPlayer.PlaySound(PrioritisedSoundKeys.Events.EnemyStartedUltra);
+            }
+        }
+
+        public void DisposeManagedState()
+        {
+            _cruiser.BuildingStarted -= _cruiser_BuildingStarted;
+            _unitConstructionMonitor.StartedBuildingUnit -= _unitConstructionMonitor_StartedBuildingUnit;
+        }
+    }
+}
