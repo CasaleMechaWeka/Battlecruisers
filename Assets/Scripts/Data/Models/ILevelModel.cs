772546506c706131dfa41145a399f9d9e59af3ad
Felix Mann <felix.fei.mann@gmail.com>
2018-09-15 12:14:59 -0700

    Create LevelModel :)

---
+++ Assets/Scripts/Data/Models/ILevelModel.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Data.Settings;
+
+namespace BattleCruisers.Data.Models
+{
+    public interface ILevelModel
+    {
+        int LevelNum { get; set;  }
+        Difficulty HardestCompletedDifficulty { get; set; }
+    }
+}
---
+++ Assets/Scripts/Data/Models/LevelModel.cs
@@ -0,0 +1,50 @@
+﻿using BattleCruisers.Data.Settings;
+using BattleCruisers.Utils;
+using System;
+using UnityEngine;
+
+namespace BattleCruisers.Data.Models
+{
+    // FELIX  Create serialize tests :)
+    [Serializable]
+    public class LevelModel : ILevelModel
+    {
+        [SerializeField]
+        private int _levelNum;
+
+        [SerializeField]
+        private Difficulty _hardestCompletedDifficulty;
+
+        public int LevelNum
+        {
+            get { return _levelNum; }
+            set { _levelNum = value; }
+        }
+
+        public Difficulty HardestCompletedDifficulty
+        {
+            get { return _hardestCompletedDifficulty; }
+            set { _hardestCompletedDifficulty = value; }
+        }
+
+        public LevelModel(int levelNum, Difficulty hardestCompletedDifficulty)
+        {
+            LevelNum = levelNum;
+            HardestCompletedDifficulty = hardestCompletedDifficulty;
+        }
+
+        public override bool Equals(object obj)
+        {
+            LevelModel other = obj as LevelModel;
+            return
+                other != null
+                && LevelNum == other.LevelNum
+                && HardestCompletedDifficulty == other.HardestCompletedDifficulty;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GetHashCode(LevelNum, HardestCompletedDifficulty);
+        }
+    }
+}
