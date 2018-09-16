f270e7532a75bdf1fb4dc4ba4aa3fb65bf8d4af2
Felix Mann <felix.fei.mann@gmail.com>
2018-09-16 16:28:34 -0700

    Create LevelInfo :)

---
+++ Assets/Scripts/UI/ScreensScene/LevelsScreen/LevelInfo.cs
@@ -0,0 +1,18 @@
+﻿using BattleCruisers.Data.Settings;
+
+namespace BattleCruisers.UI.ScreensScene.LevelsScreen
+{
+    public class LevelInfo
+    {
+        public int Num { get; private set; }
+        public string Name { get; private set; }
+        public Difficulty? DifficultyCompleted { get; private set; }
+
+        public LevelInfo(int num, string name, Difficulty? difficultyCompleted)
+        {
+            Num = num;
+            Name = name;
+            DifficultyCompleted = difficultyCompleted;
+        }
+    }
+}
