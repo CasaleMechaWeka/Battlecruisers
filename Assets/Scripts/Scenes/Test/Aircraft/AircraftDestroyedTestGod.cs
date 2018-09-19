24f9461881cf0de6bd92ced6f9e947033a82201d
Felix Mann <felix.fei.mann@gmail.com>
2018-09-18 17:11:51 -0700

    Create test scene for when an aircraft gets destroyed :)

---
+++ Assets/Scripts/Scenes/Test/Aircraft/AircraftDestroyedTestGod.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Aircraft
+{
+    public class AircraftDestroyedTestGod : MonoBehaviour 
+	{
+        private TestAircraftController _aircraft;
+		public List<Vector2> patrolPoints;
+
+		void Start() 
+		{
+			Helper helper = new Helper();
+
+			_aircraft = FindObjectOfType<TestAircraftController>();
+			_aircraft.PatrolPoints = patrolPoints;
+            helper.InitialiseUnit(_aircraft);
+			_aircraft.StartConstruction();
+
+            Invoke("DestroyAircraft", time: 1);
+		}
+
+        private void DestroyAircraft()
+        {
+            _aircraft.TakeDamage(damageAmount: _aircraft.MaxHealth, damageSource: null);
+        }
+	}
+}
