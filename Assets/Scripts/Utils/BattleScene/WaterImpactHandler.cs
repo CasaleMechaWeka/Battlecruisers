d4f7549f641500d1f9e67a6ccd97f0b66750fbb1
Felix Mann <felix.fei.mann@gmail.com>
2018-09-19 19:24:27 -0700

    Destroy aircraft when it hits the water :)

---
+++ Assets/Scripts/Utils/BattleScene/WaterImpactHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Utils.BattleScene
+{
+    public class WaterImpactHandler : MonoBehaviour
+    {
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            Destroy(collision.gameObject);
+
+            // FELIX  Make splash
+        }
+    }
+}
