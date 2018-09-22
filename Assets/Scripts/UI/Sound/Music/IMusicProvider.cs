7b13fb6e211138f20edfb6e95d79821bb5a9be89
Felix Mann <felix.fei.mann@gmail.com>
2018-09-22 15:58:58 -0700

    Create MusicPlayer :)

---
+++ Assets/Scripts/UI/Sound/Music/IMusicProvider.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.UI.Sound;
+
+namespace BattleCruisers.UI.Music
+{
+    public interface IMusicProvider
+    {
+        ISoundKey ScreensSceneKey { get; }
+        ISoundKey BattleSceneKey { get; }
+        ISoundKey DangerKey { get; }
+        ISoundKey VictoryKey { get; }
+    }
+}
---
+++ Assets/Scripts/UI/Sound/Music/MusicPlayer.cs
@@ -0,0 +1,74 @@
+﻿using BattleCruisers.UI.Sound;
+using BattleCruisers.Utils;
+using System;
+
+namespace BattleCruisers.UI.Music
+{
+    // FELIX  Test :)
+    public class MusicPlayer : IMusicPlayer
+    {
+        private readonly IMusicProvider _musicProvider;
+        private readonly ISingleSoundPlayer _soundPlayer;
+        private Music? _currentlyPlaying;
+
+        public MusicPlayer(IMusicProvider musicProvider, ISingleSoundPlayer soundPlayer)
+        {
+            Helper.AssertIsNotNull(musicProvider, soundPlayer);
+
+            _musicProvider = musicProvider;
+            _soundPlayer = soundPlayer;
+            _currentlyPlaying = null;
+        }
+
+        public void PlayScreensSceneMusic()
+        {
+            PlayMusic(Music.ScreensScene);
+        }
+
+        public void PlayBattleSceneMusic()
+        {
+            PlayMusic(Music.BattleScene);
+        }
+
+        public void PlayDangerMusic()
+        {
+            PlayMusic(Music.Danger);
+        }
+
+        public void PlayVictoryMusic()
+        {
+            PlayMusic(Music.Victory);
+        }
+
+        private void PlayMusic(Music musicToPlay)
+        {
+            if (musicToPlay != _currentlyPlaying)
+            {
+                ISoundKey soundKeyToPlay = GetKey(musicToPlay);
+                _soundPlayer.PlaySound(soundKeyToPlay, loop: true);
+                _currentlyPlaying = musicToPlay;
+            }
+        }
+
+        private ISoundKey GetKey(Music musicToPlay)
+        {
+            switch (musicToPlay)
+            {
+                case Music.BattleScene:
+                    return _musicProvider.BattleSceneKey;
+
+                case Music.ScreensScene:
+                    return _musicProvider.ScreensSceneKey;
+
+                case Music.Danger:
+                    return _musicProvider.DangerKey;
+
+                case Music.Victory:
+                    return _musicProvider.VictoryKey;
+
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
