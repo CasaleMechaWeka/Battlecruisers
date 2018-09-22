de5b1b310655ad4f9f285d6589279b1d828aef2e
Felix Mann <felix.fei.mann@gmail.com>
2018-09-22 12:18:27 -0700

    AttackBoat death looks half decent :)

--- Assets/Scripts/Buildables/Units/Ships/ShipController.cs
+++ Assets/Scripts/Buildables/Units/Ships/ShipController.cs
@@ -194,7 +194,13 @@ namespace BattleCruisers.Buildables.Units.Ships
                 turret.DisposeManagedState();
             }
 
-            // FELIX  Torque?
+            // FELIX  Create property to be assigned by parent class
+            rigidBody.gravityScale = 0.5f;
+            rigidBody.AddTorque(0.5f, ForceMode2D.Impulse);
+
+            // Make ship sink stern (back) first
+            rigidBody.velocity = Vector2.zero;
+
         }
     }
 }
---
+++ Assets/Scripts/Scenes/Test/Effects/Deaths/ShipDeathsTestGod.cs
@@ -0,0 +1,34 @@
+﻿using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Utils.Threading;
+using System;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Effects.Deaths
+{
+    public class ShipDeathsTestGod : MonoBehaviour
+    {
+        public VariableDelayDeferrer deferrer;
+
+        void Start()
+        {
+            Helper helper = new Helper();
+            ShipController[] ships = FindObjectsOfType<ShipController>();
+
+            foreach (ShipController ship in ships)
+            {
+                helper.InitialiseUnit(ship);
+                ship.StartConstruction();
+                ship.CompletedBuildable += Ship_CompletedBuildable;
+            }
+        }
+
+        private void Ship_CompletedBuildable(object sender, EventArgs e)
+        {
+            // Ship turrets are initialised when ship completes building, but this
+            // event handler is called first.  Hence wait for turrets to be initialised
+            // before destroying ship.
+            deferrer.Defer(() => (sender as ShipController).Destroy(), delayInS: 0.1f);
+        }
+    }
+}
