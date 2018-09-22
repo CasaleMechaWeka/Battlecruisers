25a6241f416dcaac6156088f741b096b30fbdc30
Felix Mann <felix.fei.mann@gmail.com>
2018-09-21 17:37:57 -0700

    Make WaterImpactHandler create splash :)

--- Assets/Scripts/Utils/BattleScene/WaterImpactHandler.cs
+++ Assets/Scripts/Utils/BattleScene/WaterImpactHandler.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace BattleCruisers.Utils.BattleScene
 {
     public class WaterImpactHandler : MonoBehaviour
     {
+        public ParticleSystem splashPrefab;
+
+        private void Awake()
+        {
+            Assert.IsNotNull(splashPrefab);
+        }
+
         void OnTriggerEnter2D(Collider2D collision)
         {
             Destroy(collision.gameObject);
 
-            // FELIX  Make splash
+            Quaternion rotation = Quaternion.Euler(x: -90, y: 0, z: 0);
+            ParticleSystem splash = Instantiate(splashPrefab, collision.transform.position, rotation);
+            splash.Play();
         }
     }
 }
