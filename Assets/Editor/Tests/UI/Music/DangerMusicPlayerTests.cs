0071836e7942961abbc536c747410e0567bab652
Felix Mann <felix.fei.mann@gmail.com>
2018-09-23 15:39:01 -0700

    Start creating DangerMusicPlayerTests :)

---
+++ Assets/Editor/Tests/UI/Music/DangerMusicPlayerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using UnityEngine;
+using UnityAsserts = UnityEngine.Assertions;
+using BattleCruisers.UI.Music;
+using BattleCruisers.Utils.Threading;
+
+namespace BattleCruisers.Tests.UI.Music
+{
+    public class DangerMusicPlayerTests
+    {
+        private DangerMusicPlayer _dangerMusicPlayer;
+        private IMusicPlayer _musicPlayer;
+        private IDangerMonitor _dangerMonitor;
+        private IVariableDelayDeferrer _deferrer;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _musicPlayer = Substitute.For<IMusicPlayer>();
+            _dangerMonitor = Substitute.For<IDangerMonitor>();
+            _deferrer = Substitute.For<IVariableDelayDeferrer>();
+
+            _dangerMusicPlayer = new DangerMusicPlayer(_musicPlayer, _dangerMonitor, _deferrer);
+        }
+
+        [Test]
+        public void SweetTest()
+        {
+        }
+    }
+}
