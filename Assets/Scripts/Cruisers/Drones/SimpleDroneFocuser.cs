48e32affdd8ee06a0bf0ab5bcb80adca2e8f2641
Felix Mann <felix.fei.mann@gmail.com>
2018-09-28 13:56:13 -0700

    Create SimpleDroneFocuser :)

---
+++ Assets/Scripts/Cruisers/Drones/SimpleDroneFocuser.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Assertions;
+
+namespace BattleCruisers.Cruisers.Drones
+{
+    public class SimpleDroneFocuser : IDroneFocuser
+    {
+        private readonly IDroneManager _droneManager;
+
+        public SimpleDroneFocuser(IDroneManager droneManager)
+        {
+            Assert.IsNotNull(droneManager);
+            _droneManager = droneManager;
+        }
+
+        public void ToggleDroneConsumerFocus(IDroneConsumer droneConsumer, bool isTriggeredByPlayer)
+        {
+            _droneManager.ToggleDroneConsumerFocus(droneConsumer);
+        }
+    }
+}
