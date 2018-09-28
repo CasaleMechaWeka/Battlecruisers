7ff4dbe84a29b437082a5d3dfc4c51e3b175924f
Felix Mann <felix.fei.mann@gmail.com>
2018-09-28 13:29:24 -0700

    Create DroneFocusSoundPicker :)

---
+++ Assets/Scripts/Cruisers/Drones/DroneFocusSoundPicker.cs
@@ -0,0 +1,41 @@
+﻿using BattleCruisers.Data.Static;
+using BattleCruisers.UI.Sound;
+using System;
+
+namespace BattleCruisers.Cruisers.Drones
+{
+    // FELIX  Test :)
+    public class DroneFocusSoundPicker : IDroneFocusSoundPicker
+    {
+        public PrioritisedSoundKey PickSound(DroneConsumerState preFocusState, DroneConsumerState postFocusState)
+        {
+            switch (postFocusState)
+            {
+                case DroneConsumerState.Idle:
+                    return PrioritisedSoundKeys.Events.Drones.NotEnoughDronesToFocus;
+
+                case DroneConsumerState.Active:
+                    switch (preFocusState)
+                    {
+                        case DroneConsumerState.Idle:
+                            return PrioritisedSoundKeys.Events.Drones.Focusing;
+
+                        case DroneConsumerState.Active:
+                            return PrioritisedSoundKeys.Events.Drones.AllFocused;
+
+                        case DroneConsumerState.Focused:
+                            return PrioritisedSoundKeys.Events.Drones.Dispersing;
+
+                        default:
+                            throw new ArgumentException();
+                    }
+
+                case DroneConsumerState.Focused:
+                    return PrioritisedSoundKeys.Events.Drones.AllFocused;
+
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
