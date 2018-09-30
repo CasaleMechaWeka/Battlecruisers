7e36d7f09349ff13164ef8da97dcd1c3ae8c79b4
Felix Mann <felix.fei.mann@gmail.com>
2018-09-30 11:43:18 -0700

    Create test scene for Forged Alliance like zooming :)  I have the formula :D

---
+++ Assets/Scripts/Scenes/Test/CameraDirectionalZoomTestGod.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test
+{
+    public class CameraDirectionalZoomTestGod : MonoBehaviour
+    {
+        public Camera camera;
+        public GameObject zoomTarget;
+
+        private void Start()
+        {
+            //Debug.Log("camera.transform.position: " + camera.transform.position);
+
+            Vector3 zoomTargetViewportPosition = camera.WorldToViewportPoint(zoomTarget.transform.position);
+            Debug.Log("zoomTargetViewportPosition: " + zoomTargetViewportPosition);
+            Debug.Log("Camera size: " + FindSize());
+
+            camera.orthographicSize = 3;
+            camera.transform.position = new Vector3(-2.6f, 0.6f, camera.transform.position.z);
+
+            //Debug.Log("camera.transform.position: " + camera.transform.position);
+
+            zoomTargetViewportPosition = camera.WorldToViewportPoint(zoomTarget.transform.position);
+            Debug.Log("zoomTargetViewportPosition: " + zoomTargetViewportPosition);
+            Debug.Log("Camera size: " + FindSize());
+        }
+
+        private Vector2 FindSize()
+        {
+            float height = 2 * camera.orthographicSize;
+            float width = height * camera.aspect;
+            return new Vector2(width, height);
+        }
+    }
+}
