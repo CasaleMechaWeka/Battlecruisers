95980736243e74526beccbac2ced3a6411bdc835
Felix Mann <felix.fei.mann@gmail.com>
2018-09-30 17:36:20 -0700

    Tidy up usings :)

--- Assets/Scripts/UI/ScreensScene/LevelsScreen/HomeButtonController.cs
+++ Assets/Scripts/UI/ScreensScene/LevelsScreen/HomeButtonController.cs
@@ -1,13 +1,10 @@
-﻿using BattleCruisers.Data;
-using BattleCruisers.Scenes;
-using System.Collections;
-using System.Collections.Generic;
+﻿using BattleCruisers.Scenes;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace BattleCruisers.UI.ScreensScene.LevelsScreen
 {
-	public class HomeButtonController : MonoBehaviour 
+    public class HomeButtonController : MonoBehaviour 
 	{
 		public Button button;
 
