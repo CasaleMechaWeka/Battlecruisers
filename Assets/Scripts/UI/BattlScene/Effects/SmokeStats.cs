305404c557677a4c3d03b0cc6ee489e381d36c28
Felix Mann <felix.fei.mann@gmail.com>
2018-10-04 17:09:33 -0700

    Create SmokeStats :)

---
+++ Assets/Scripts/UI/BattlScene/Effects/SmokeStats.cs
@@ -0,0 +1,22 @@
+﻿namespace BattleCruisers.UI.BattleScene.Effects
+{
+    public class SmokeStats
+    {
+        public float SizeMultiplier { get; private set; }
+        public float StartLifetimeMin { get; private set; }
+        public float StartLifetimeMax { get; private set; }
+        public float EmissionRatePerS { get; private set; }
+
+        public SmokeStats(
+            float sizeMultiplier,
+            float startLifetimeMin,
+            float startLifetimeMax,
+            float emissionRatePerS)
+        {
+            SizeMultiplier = sizeMultiplier;
+            StartLifetimeMin = startLifetimeMin;
+            StartLifetimeMax = startLifetimeMax;
+            EmissionRatePerS = emissionRatePerS;
+        }
+    }
+}
