079ebd167bef15f2eb0c5601184fa46e736316f6
Felix Mann <felix.fei.mann@gmail.com>
2018-10-05 17:47:57 -0700

    Create SmokeInitialiser :)

---
+++ Assets/Scripts/Effects/SmokeInitialiser.cs
@@ -0,0 +1,27 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Cruisers.Damage;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Effects
+{
+    public class SmokeInitialiser : MonoBehaviour
+    {
+        // Keep reference to avoid garbage collection
+#pragma warning disable CS0414  // Variable is assigned but never used
+        private SmokeEmitter _smokeEmitter;
+#pragma warning restore CS0414  // Variable is assigned but never used
+
+        public void Initialise(IDamagable parentDamagable)
+        {
+            Smoke smoke = GetComponent<Smoke>();
+            Assert.IsNotNull(smoke);
+            smoke.Initialise();
+
+            _smokeEmitter
+                = new SmokeEmitter(
+                    new HealthStateMonitor(parentDamagable),
+                    smoke);
+        }
+    }
+}
