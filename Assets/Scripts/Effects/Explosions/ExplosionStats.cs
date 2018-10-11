15e4f7babae7f43e849a856c828f299c2a618baa
Felix Mann <felix.fei.mann@gmail.com>
2018-10-10 17:40:33 -0700

    Create ExplosionStats :D

---
+++ Assets/Scripts/Effects/Explosions/ExplosionStats.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Effects.Explosions
+{
+    // FELIX  Can use similar model to simplify ProjectileStatsWrapper?
+    public class ExplosionStats : MonoBehaviour, IExplosionStats
+    {
+        public ExplosionSize size;
+        public ExplosionSize Size { get { return size; } }
+
+        public bool showTrails;
+        public bool ShowTrails { get { return showTrails; } }
+    }
+}
---
+++ Assets/Scripts/Effects/Explosions/IExplosionStats.cs
@@ -0,0 +1,20 @@
+﻿namespace BattleCruisers.Effects.Explosions
+{
+    public enum ExplosionSize
+    {
+        Small,  // Radius 0.75m => Bomber, mortar, SAM site
+        Medium, // Radius 1m    => Destroyer missiles, archon front launcher missiles
+        Large,  // Radius 1.5m  => Artillery, rocket launcher, broadsides, archon back missile launcher, archon primary cannon
+        Giant   // Radius 10m   => Nuke
+    }
+
+    public interface IExplosionStats
+    {
+        ExplosionSize Size { get; }
+
+        // An explosion consists of 2 parts.  The core explosion, and the trails that
+        // shoot everywhere.  ShowTrails determines whether to show the second explosion
+        // part.
+        bool ShowTrails { get; }
+    }
+}
