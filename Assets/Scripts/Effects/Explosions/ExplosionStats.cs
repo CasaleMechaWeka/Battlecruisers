40adfdd57beed29437e3c3c426494fba7d2ac053
Felix Mann <felix.fei.mann@gmail.com>
2018-10-11 17:42:40 -0700

    Create non-MonoBehaviour ExplosionStats :)

---
+++ Assets/Scripts/Effects/Explosions/ExplosionStats.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleCruisers.Effects.Explosions
+{
+    public class ExplosionStats : IExplosionStats
+    {
+        public ExplosionSize Size { get; private set; }
+        public bool ShowTrails { get; private set; }
+
+        public ExplosionStats(ExplosionSize size, bool showTrails)
+        {
+            Size = size;
+            ShowTrails = showTrails;
+        }
+    }
+}
