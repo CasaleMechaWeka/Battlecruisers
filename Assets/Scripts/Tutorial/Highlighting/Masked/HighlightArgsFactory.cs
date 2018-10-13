67683633f480663e95236bdd5ca3c50d40da868c
Felix Mann <felix.fei.mann@gmail.com>
2018-10-13 11:16:18 -0700

    Create HighlightArgsFactory :)

---
+++ Assets/Scripts/Tutorial/Highlighting/Masked/HighlightArgsFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace BattleCruisers.Tutorial.Highlighting.Masked
+{
+    public class HighlightArgsFactory : IHighlightArgsFactory
+    {
+        public HighlightArgs CreateForOnCanvasObject(RectTransform rectTransform)
+        {
+            Vector3[] corners = new Vector3[4];
+            rectTransform.GetWorldCorners(corners);
+            return new HighlightArgs(corners[0], rectTransform.sizeDelta);
+        }
+
+        public HighlightArgs CreateForInGameObject(Vector2 position, Vector2 size)
+        {
+            // FELIX :D
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorial/Highlighting/Masked/IHighlightArgsFactory.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Tutorial.Highlighting.Masked
+{
+    public interface IHighlightArgsFactory
+    {
+        HighlightArgs CreateForOnCanvasObject(RectTransform rectTransform);
+        HighlightArgs CreateForInGameObject(Vector2 position, Vector2 size);
+    }
+}
