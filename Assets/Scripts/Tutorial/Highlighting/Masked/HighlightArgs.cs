e1dfd80da133618ab80d443a3635e1bfd601d145
Felix Mann <felix.fei.mann@gmail.com>
2018-10-13 11:10:07 -0700

    Create HighlightArgs :)

---
+++ Assets/Scripts/Tutorial/Highlighting/Masked/HighlightArgs.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Tutorial.Highlighting.Masked
+{
+    public class HighlightArgs
+    {
+        public Vector2 BottomLeftPosition { get; private set; }
+        public Vector2 Size { get; private set; }
+
+        public HighlightArgs(Vector2 bottomLeftPosition, Vector2 size)
+        {
+            BottomLeftPosition = bottomLeftPosition;
+            Size = size;
+        }
+    }
+}
