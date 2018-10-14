d3dde983028bc57c3698d21fe7b741ad048e6db2
Felix Mann <felix.fei.mann@gmail.com>
2018-10-14 14:03:55 -0700

    Start experimenting with puzzle slots :)

---
+++ Assets/Scripts/Scenes/Test/Cruisers/ClickHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace BattleCruisers.Scenes.Test.Cruisers
+{
+    public class ClickHandler : MonoBehaviour, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log("ClickHandler.OnPointerClick()");
+        }
+    }
+}
