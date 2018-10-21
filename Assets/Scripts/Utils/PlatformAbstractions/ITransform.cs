161d59d2100a77ead128cddff13b7f422e0acaad
Felix Mann <felix.fei.mann@gmail.com>
2018-10-21 09:56:01 -0700

    Extend ITranform, breaking CameraBC :)

--- Assets/Scripts/Utils/PlatformAbstractions/ITransform.cs
+++ Assets/Scripts/Utils/PlatformAbstractions/ITransform.cs
@@ -5,5 +5,8 @@ namespace BattleCruisers.Utils.PlatformAbstractions
 	public interface ITransform
 	{
 		Vector3 Position { get; set; }
+        Vector3 EulerAngles { get; }
+
+        void Rotate(Vector3 rotationChangeVector);
 	}
 }
