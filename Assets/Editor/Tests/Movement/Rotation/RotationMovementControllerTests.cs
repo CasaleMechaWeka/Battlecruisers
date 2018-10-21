c7e8305f8e22c2fe7f2e206f1b73c9057d6c02a6
Felix Mann <felix.fei.mann@gmail.com>
2018-10-21 11:20:33 -0700

    Add beginnings of RotationMovementController tests :)

---
+++ Assets/Editor/Tests/Movement/Rotation/RotationMovementControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using UnityEngine;
+using UnityAsserts = UnityEngine.Assertions;
+using BattleCruisers.Movement.Rotation;
+using BattleCruisers.Utils.PlatformAbstractions;
+
+namespace BattleCruisers.Tests.Movement.Rotation
+{
+    public class RotationMovementControllerTests
+    {
+        private IRotationMovementController _movementController;
+        private IRotationHelper _rotationHelper;
+        private ITransform _transform;
+        private ITime _time;
+        private float _rotateSpeedInDegreesPerS;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _rotationHelper = Substitute.For<IRotationHelper>();
+            _transform = Substitute.For<ITransform>();
+            _rotateSpeedInDegreesPerS = 10;
+
+            _movementController = new RotationMovementController(_rotationHelper, _transform, _time, _rotateSpeedInDegreesPerS);
+        }
+
+        [Test]
+        public void SweetTest()
+        {
+        }
+    }
+}
