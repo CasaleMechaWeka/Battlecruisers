174fe4535683f0f978ac4626296c8046e2c91c9e
Felix Mann <felix.fei.mann@gmail.com>
2018-10-21 14:50:27 -0700

    Adjust deathstar launcher position :)

--- Assets/Scripts/Buildables/Buildings/Offensive/DeathstarLauncherController.cs
+++ Assets/Scripts/Buildables/Buildings/Offensive/DeathstarLauncherController.cs
@@ -7,7 +7,7 @@ namespace BattleCruisers.Buildables.Buildings.Offensive
 {
     public class DeathstarLauncherController : SatelliteLauncherController
 	{
-		protected override Vector3 SpawnPositionAdjustment { get { return new Vector3(0, 0.015f, 0); } }
+		protected override Vector3 SpawnPositionAdjustment { get { return new Vector3(0.003f, 0.145f, 0); } }
         protected override PrioritisedSoundKey ConstructionCompletedSoundKey { get { return PrioritisedSoundKeys.Completed.Ultra; } }
         public override TargetValue TargetValue { get { return TargetValue.High; } }
 
