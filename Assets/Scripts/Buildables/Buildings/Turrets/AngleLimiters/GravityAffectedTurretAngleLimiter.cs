96edec568238d06dcbd149ad2ba28f0d77deaaa8
Felix Mann <felix.fei.mann@gmail.com>
2018-10-21 11:29:20 -0700

    Create GravityAffectedTurretAngleLimiter :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AngleLimiters/GravityAffectedTurretAngleLimiter.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleCruisers.Buildables.Buildings.Turrets.AngleLimiters
+{
+    public class GravityAffectedTurretAngleLimiter : AngleLimiter
+    {
+        public GravityAffectedTurretAngleLimiter() 
+            : base(minAngle: 0, maxAngle: 85)
+        {
+        }
+    }
+}
