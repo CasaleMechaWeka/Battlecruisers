60b50d11c023b60a8ecb73db4dea58feae2af876
Felix Mann <felix.fei.mann@gmail.com>
2018-10-24 16:30:01 -0700

    Create SlotSpecification :)

---
+++ Assets/Scripts/Cruisers/Slots/SlotSpecification.cs
@@ -0,0 +1,18 @@
+﻿using BattleCruisers.Buildables.Buildings;
+
+namespace BattleCruisers.Cruisers.Slots
+{
+    public class  SlotSpecification
+    {
+        public SlotType SlotType { get; private set; }
+        public BuildingFunction BuildingFunction { get; private set; }
+        public bool PreferFromFront { get; private set; }
+
+        public SlotSpecification(SlotType slotType, BuildingFunction buildingFunction, bool preferCruiserFront)
+        {
+            SlotType = slotType;
+            BuildingFunction = buildingFunction;
+            PreferFromFront = preferCruiserFront;
+        }
+    }
+}
