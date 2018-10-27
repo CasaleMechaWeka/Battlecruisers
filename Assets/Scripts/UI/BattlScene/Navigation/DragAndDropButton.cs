1f1e6ffa14d95d7d0cadc6ff6232403ef2754621
Felix Mann <felix.fei.mann@gmail.com>
2018-10-26 18:10:21 -0700

    I have a drag and drop button :D

---
+++ Assets/Scripts/UI/BattlScene/Navigation/DragAndDropButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace BattleCruisers.UI.BattleScene.Navigation
+{
+    public class DragAndDropButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        private bool _isPointerDown;
+
+        // FELIX  Replace with Initialise() :)
+        private void Start()
+        {
+            _isPointerDown = false;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Debug.Log("OnPointerDown");
+            _isPointerDown = true;
+
+            // FELIX  Handle offset adjustment (ie, mouse position relative to our center position),
+            // otherwise button "jumps" to mouse position :P
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            Debug.Log("OnPointerUp");
+            _isPointerDown = false;
+        }
+
+        private void Update()
+        {
+            if (_isPointerDown)
+            {
+                transform.position = Input.mousePosition;
+            }
+        }
+    }
+}
