50542c1c3d6a8371dea7c37b7b5de6c26417ff13
Felix Mann <felix.fei.mann@gmail.com>
2018-10-27 16:10:50 -0700

    Create and use NavigationWheelInitialiser :D

--- Assets/Scripts/Scenes/Test/NavigationTestGod.cs
+++ Assets/Scripts/Scenes/Test/NavigationTestGod.cs
@@ -1,5 +1,4 @@
 ﻿using BattleCruisers.UI.BattleScene.Navigation;
-using BattleCruisers.Utils.Clamper;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test
@@ -8,16 +7,8 @@ namespace BattleCruisers.Scenes.Test
     {
         void Start()
         {
-            // FELIX  Create NavigationWheelInitialiser (that has access to 3 triangle points, 
-            // instead of hardcoding here :P)
-            IPositionClamper navigationWheelPositionClamper
-                = new TrianglePositionClamper(
-                    bottomLeftVertex: new Vector2(500, 500),
-                    bottomRightVertex: new Vector2(1000, 500),
-                    topCenterVertex: new Vector2(750, 1000));
-
-            NavigationWheel navigationWheel = FindObjectOfType<NavigationWheel>();
-            navigationWheel.Initialise(navigationWheelPositionClamper);
+            NavigationWheelInitialiser navigationWheelInitialiser = FindObjectOfType<NavigationWheelInitialiser>();
+            navigationWheelInitialiser.InitialiseNavigationWheel();
         }
     }
 }
---
+++ Assets/Scripts/UI/BattlScene/Navigation/NavigationWheelInitialiser.cs
@@ -0,0 +1,29 @@
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.Clamper;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene.Navigation
+{
+    public class NavigationWheelInitialiser : MonoBehaviour
+    {
+        public GameObject bottomLeftVertex, bottomRightVertex, topCenterVertex;
+
+        public NavigationWheel InitialiseNavigationWheel()
+        {
+            Helper.AssertIsNotNull(bottomLeftVertex, bottomRightVertex, topCenterVertex);
+
+            IPositionClamper navigationWheelPositionClamper
+                = new TrianglePositionClamper(
+                    bottomLeftVertex.transform.position,
+                    bottomRightVertex.transform.position,
+                    topCenterVertex.transform.position);
+
+            NavigationWheel navigationWheel = GetComponentInChildren<NavigationWheel>();
+            Assert.IsNotNull(navigationWheel);
+            navigationWheel.Initialise(navigationWheelPositionClamper);
+
+            return navigationWheel;
+        }
+    }
+}
