a9ffa5b2eac94ca42f387bd774ad068444e97c81
Felix Mann <felix.fei.mann@gmail.com>
2018-10-27 15:59:10 -0700

    Create test god to initialise DragAndDropButton :)

---
+++ Assets/Scripts/Scenes/Test/NavigationTestGod.cs
@@ -0,0 +1,23 @@
+﻿using BattleCruisers.UI.BattleScene.Navigation;
+using BattleCruisers.Utils.Clamper;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test
+{
+    public class NavigationTestGod : MonoBehaviour
+    {
+        void Start()
+        {
+            // FELIX  Create NavigationWheelInitialiser (that has access to 3 triangle points, 
+            // instead of hardcoding here :P)
+            IPositionClamper navigationWheelPositionClamper
+                = new TrianglePositionClamper(
+                    bottomLeftVertex: new Vector2(500, 500),
+                    bottomRightVertex: new Vector2(1000, 500),
+                    topCenterVertex: new Vector2(750, 1000));
+
+            DragAndDropButton navigationWheel = FindObjectOfType<DragAndDropButton>();
+            navigationWheel.Initialise(navigationWheelPositionClamper);
+        }
+    }
+}
--- Assets/Scripts/UI/BattlScene/Navigation/DragAndDropButton.cs
+++ Assets/Scripts/UI/BattlScene/Navigation/DragAndDropButton.cs
@@ -1,6 +1,7 @@
 ﻿using BattleCruisers.Utils;
 using BattleCruisers.Utils.Clamper;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.EventSystems;
 
 namespace BattleCruisers.UI.BattleScene.Navigation
@@ -10,17 +11,10 @@ namespace BattleCruisers.UI.BattleScene.Navigation
         private IPositionClamper _positionClamper;
         private Vector2 _halfSize;
 
-        // FELIX  Replace with Initialise() :)
-        private void Start()
+        public void Initialise(IPositionClamper positionClamper)
         {
-            // FELIX  Inject :P
-            // FELIX  Don't want validator, want clamper!!  Otherwise won't go perfectly
-            // to area edge if mouse is moved quickly :/
-            _positionClamper
-                = new TrianglePositionClamper(
-                    bottomLeftVertex: new Vector2(500, 500),
-                    bottomRightVertex: new Vector2(1000, 500),
-                    topCenterVertex: new Vector2(750, 1000));
+            Assert.IsNotNull(positionClamper);
+            _positionClamper = positionClamper;
 
             RectTransform rectTransform = transform.Parse<RectTransform>();
             _halfSize = rectTransform.sizeDelta / 2;
@@ -28,14 +22,12 @@ namespace BattleCruisers.UI.BattleScene.Navigation
 
         public void OnDrag(PointerEventData eventData)
         {
-            // FELIX
-            //Debug.Log("OnDrag()  eventData.delta: " + eventData.delta);
-
             Vector2 desiredBottomLeftPosition = (Vector2)transform.position + eventData.delta;
             Vector2 desiredCenterPosition = desiredBottomLeftPosition + _halfSize;
             Vector2 clampedCenterPosition = _positionClamper.Clamp(desiredCenterPosition);
             Vector2 clampedBottomLeftPosition = clampedCenterPosition - _halfSize;
 
+            // FELIX  Remove :)
             Debug.Log("OnDrag()  clampedPosition: " + clampedCenterPosition + "  halfSize: " + _halfSize + "  bottomLeftPosition: " + clampedBottomLeftPosition);
 
             transform.position = clampedBottomLeftPosition;
