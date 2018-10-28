fe930ea8318afa66563d87cb04345048ff1987d2
Felix Mann <felix.fei.mann@gmail.com>
2018-10-28 12:00:06 -0700

    Create INavigationWheelPanel :)

---
+++ Assets/Scripts/UI/BattlScene/Navigation/INavigationWheelPanel.cs
@@ -0,0 +1,11 @@
+﻿namespace BattleCruisers.UI.BattleScene.Navigation
+{
+    public interface INavigationWheelPanel
+    {
+        /// <returns>0-1</returns>
+        float FindNavigationWheelYPositionAsProportionOfMaxHeight();
+
+        /// <returns>0-1</returns>
+        float FindNavigationWheelXPositionAsProportionOfValidWidth();
+    }
+}
