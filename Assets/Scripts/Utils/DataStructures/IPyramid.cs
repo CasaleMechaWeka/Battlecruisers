f03d96aebdc233e169798db30aefabd8189c6ffa
Felix Mann <felix.fei.mann@gmail.com>
2018-10-28 12:05:34 -0700

    Create Pyramid :)

---
+++ Assets/Scripts/Utils/DataStructures/IPyramid.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Utils.DataStrctures
+{
+    /// <summary>
+    /// A tringale with a flat base, with the apex in the middle
+    /// of the two base vertices.
+    /// </summary>
+    public interface IPyramid
+    {
+        Vector2 BottomLeftVertex { get; }
+        Vector2 BottomRightVertex { get; }
+        Vector2 TopCenterVertex { get; }
+    }
+}
---
+++ Assets/Scripts/Utils/DataStructures/Pyramid.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Utils.DataStrctures
+{
+    public class Pyramid : IPyramid
+    {
+        public Vector2 BottomLeftVertex { get; private set; }
+        public Vector2 BottomRightVertex { get; private set; }
+        public Vector2 TopCenterVertex { get; private set; }
+
+        public Pyramid(Vector2 bottomLeftVertex, Vector2 bottomRightVertex, Vector2 topCenterVertex)
+        {
+            BottomLeftVertex = bottomLeftVertex;
+            BottomRightVertex = bottomRightVertex;
+            TopCenterVertex = topCenterVertex;
+        }
+    }
+}
