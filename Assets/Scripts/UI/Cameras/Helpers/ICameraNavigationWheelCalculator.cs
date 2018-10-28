f4b5a9457dda8bed61b50cc1fb970f8df1c62add
Felix Mann <felix.fei.mann@gmail.com>
2018-10-28 11:44:35 -0700

    Create tentative ICameraNavigationWheelCalculator interface :)

---
+++ Assets/Scripts/UI/Cameras/Helpers/ICameraNavigationWheelCalculator.cs
@@ -0,0 +1,30 @@
+﻿using BattleCruisers.Utils.DataStrctures;
+
+namespace BattleCruisers.UI.Cameras.Helpers
+{
+    public interface ICameraNavigationWheelCalculator
+    {
+        float FindOrthographicSize(
+            float navigationWheelYPosition,
+            float navigationWheelPanelHeight,
+            IRange<float> validOrthographicSize);
+
+        float FindCameraYPosition(
+            float orthographicSize,
+            float cameraHeight,
+            float waterSurfaceYPosition);
+
+        // FELIX
+        IRange<float> FindValidNavigationWheelXPositions(
+            float navigationWheelYPosition);
+
+        // FELIX
+        IRange<float> FindValidCameraXPositions(
+            float cameraYPosition);
+
+        float FindCameraXPosition(
+            float navigationWheelXPosition,
+            float navigationWheelPanelWidth,
+            IRange<float> validXPosition);
+    }
+}
