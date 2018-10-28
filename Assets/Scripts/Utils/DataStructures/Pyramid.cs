ee97e1474e550e6fc28e927c43ad7e731966b29e
Felix Mann <felix.fei.mann@gmail.com>
2018-10-28 12:22:30 -0700

    Extend Pryamid :)

--- Assets/Scripts/Utils/DataStructures/Pyramid.cs
+++ Assets/Scripts/Utils/DataStructures/Pyramid.cs
@@ -1,18 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace BattleCruisers.Utils.DataStrctures
 {
     public class Pyramid : IPyramid
     {
+        private readonly float _height;
+        private readonly float _width;
+
         public Vector2 BottomLeftVertex { get; private set; }
         public Vector2 BottomRightVertex { get; private set; }
         public Vector2 TopCenterVertex { get; private set; }
 
+        // FELIX  Test!  (Including asserts)
         public Pyramid(Vector2 bottomLeftVertex, Vector2 bottomRightVertex, Vector2 topCenterVertex)
         {
+            Assert.IsTrue(bottomLeftVertex.x < topCenterVertex.x);
+            Assert.IsTrue(topCenterVertex.x < bottomRightVertex.x);
+            Assert.AreEqual(bottomLeftVertex.y, bottomRightVertex.y);
+            Assert.IsTrue(topCenterVertex.y > bottomLeftVertex.y);
+            Assert.IsTrue(Mathf.Approximately(topCenterVertex.x - bottomLeftVertex.x, bottomRightVertex.x - topCenterVertex.x));
+
             BottomLeftVertex = bottomLeftVertex;
             BottomRightVertex = bottomRightVertex;
             TopCenterVertex = topCenterVertex;
+
+            _height = TopCenterVertex.y - BottomLeftVertex.y;
+            _width = BottomRightVertex.x - BottomLeftVertex.x;
+        }
+
+        public float FindMaxY(float xPosition)
+        {
+            Assert.IsTrue(xPosition >= BottomLeftVertex.x);
+            Assert.IsTrue(xPosition <= BottomRightVertex.x);
+
+            float halfWidth = _width / 2;
+            float proportionOfMaxHeight = halfWidth - Mathf.Abs(TopCenterVertex.x - xPosition);
+            float localMaxY = proportionOfMaxHeight / halfWidth * _height;
+            return BottomLeftVertex.y + localMaxY;
         }
     }
 }
