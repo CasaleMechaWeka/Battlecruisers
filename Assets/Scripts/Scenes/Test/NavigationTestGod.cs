f046608d834371e680141652e6093e006cfbf266
Felix Mann <felix.fei.mann@gmail.com>
2018-10-28 17:06:34 -0700

    Update camera size and position according to navigation wheel :D

--- Assets/Scripts/Scenes/Test/NavigationTestGod.cs
+++ Assets/Scripts/Scenes/Test/NavigationTestGod.cs
@@ -1,4 +1,9 @@
-﻿using BattleCruisers.UI.BattleScene.Navigation;
+﻿using BattleCruisers.Data.Settings;
+using BattleCruisers.UI.BattleScene.Navigation;
+using BattleCruisers.UI.Cameras.Helpers;
+using BattleCruisers.Utils.DataStrctures;
+using BattleCruisers.Utils.PlatformAbstractions;
+using NSubstitute;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test
@@ -6,11 +11,22 @@ namespace BattleCruisers.Scenes.Test
     public class NavigationTestGod : MonoBehaviour
     {
         private INavigationWheelPanel _navigationWheelPanel;
+        private ICameraNavigationWheelCalculator _cameraNavigationWheelCalculator;
+        private ICamera _camera;
 
         private void Start()
         {
             NavigationWheelInitialiser navigationWheelInitialiser = FindObjectOfType<NavigationWheelInitialiser>();
             _navigationWheelPanel = navigationWheelInitialiser.InitialiseNavigationWheel();
+
+            Camera platformCamera = FindObjectOfType<Camera>();
+            _camera = new CameraBC(platformCamera);
+            ISettingsManager settingsManager = Substitute.For<ISettingsManager>();
+            ICameraCalculator cameraCalculator = new CameraCalculator(_camera, settingsManager);
+
+            IRange<float> validOrthographicSizes = new Range<float>(min: 5, max: 33);
+
+            _cameraNavigationWheelCalculator = new CameraNavigationWheelCalculator(_navigationWheelPanel, cameraCalculator, validOrthographicSizes);
         }
 
         private void Update()
@@ -19,8 +35,14 @@ namespace BattleCruisers.Scenes.Test
             //float yProportion = _navigationWheelPanel.FindNavigationWheelYPositionAsProportionOfMaxHeight();
             //Debug.Log("yProportion: " + yProportion);
 
-            float xProportion = _navigationWheelPanel.FindXProportion();
-            Debug.Log("xProportion: " + xProportion);
+            //float xProportion = _navigationWheelPanel.FindXProportion();
+            //Debug.Log("xProportion: " + xProportion);
+
+            float desiredCameraOrthographicSize = _cameraNavigationWheelCalculator.FindOrthographicSize();
+            Vector2 desiredCameraPosition = _cameraNavigationWheelCalculator.FindCameraPosition();
+
+            _camera.OrthographicSize = desiredCameraOrthographicSize;
+            _camera.Transform.Position = new Vector3(desiredCameraPosition.x, desiredCameraPosition.y, _camera.Transform.Position.z);
         }
     }
 }
