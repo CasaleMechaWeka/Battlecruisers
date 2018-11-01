18daeb13a55b5f1e21d6f31575581660129cede7
Felix Mann <felix.fei.mann@gmail.com>
2018-11-01 16:52:03 -0700

    Create SmoothCameraAdjuster :D

---
+++ Assets/Scripts/UI/Cameras/Adjusters/SmoothCameraAdjuster.cs
@@ -0,0 +1,33 @@
+﻿using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.Cameras.Adjusters
+{
+    /// <summary>
+    /// Smoothly moves camera to target postion and orthographic size.
+    /// </summary>
+    /// FELIX  Test :)
+    public class SmoothCameraAdjuster : ICameraAdjuster
+    {
+        private readonly ICameraTargetProvider _cameraTargetProvider;
+        private readonly ISmoothZoomAdjuster _zoomAdjuster;
+        private readonly ISmoothPositionAdjuster _positionAdjuster;
+
+        public SmoothCameraAdjuster(
+            ICameraTargetProvider cameraTargetProvider, 
+            ISmoothZoomAdjuster zoomAdjuster,
+            ISmoothPositionAdjuster positionAdjuster)
+        {
+            Helper.AssertIsNotNull(cameraTargetProvider, zoomAdjuster, positionAdjuster);
+
+            _cameraTargetProvider = cameraTargetProvider;
+            _zoomAdjuster = zoomAdjuster;
+            _positionAdjuster = positionAdjuster;
+        }
+
+        public void AdjustCamera(float deltaTime)
+        {
+            _zoomAdjuster.AdjustZoom(_cameraTargetProvider.Target.OrthographicSize);
+            _positionAdjuster.AdjustPosition(_cameraTargetProvider.Target.Position);
+        }
+    }
+}
