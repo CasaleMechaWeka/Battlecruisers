f8f32da14c51eddf9adc41f47ab6105bd1b73a80
Felix Mann <felix.fei.mann@gmail.com>
2018-10-31 17:49:35 -0700

    Create NavigationWheelCameraTargetProvider :)

---
+++ Assets/Scripts/UI/Cameras/NavigationWheelCameraTargetProvider.cs
@@ -0,0 +1,50 @@
+﻿using BattleCruisers.UI.BattleScene.Navigation;
+using BattleCruisers.Utils;
+using System;
+
+namespace BattleCruisers.UI.Cameras
+{
+    // FELIX  Test :)
+    public class NavigationWheelCameraTargetProvider : ICameraTargetProvider
+    {
+        private readonly INavigationWheel _navigationWheel;
+        private readonly ICameraTargetFinder _navigationWheelCameraTargetFinder;
+
+        private ICameraTarget _target;
+        public ICameraTarget Target
+        {
+            get { return _target; }
+            set
+            {
+                if (_target != value)
+                {
+                    _target = value;
+
+                    if (TargetChanged != null)
+                    {
+                        TargetChanged.Invoke(this, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+        public event EventHandler TargetChanged;
+
+        public NavigationWheelCameraTargetProvider(
+            INavigationWheel navigationWheel,
+            ICameraTargetFinder navigationWheelCameraTargetFinder)
+        {
+            Helper.AssertIsNotNull(navigationWheel, navigationWheelCameraTargetFinder);
+
+            _navigationWheel = navigationWheel;
+            _navigationWheelCameraTargetFinder = navigationWheelCameraTargetFinder;
+
+            _navigationWheel.CenterPositionChanged += _navigationWheel_CenterPositionChanged;
+        }
+
+        private void _navigationWheel_CenterPositionChanged(object sender, EventArgs e)
+        {
+            Target = _navigationWheelCameraTargetFinder.FindCameraTarget();
+        }
+    }
+}
