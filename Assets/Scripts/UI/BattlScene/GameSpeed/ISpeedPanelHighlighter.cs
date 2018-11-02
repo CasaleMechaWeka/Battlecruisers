3e08c1007276dad03f35d4fff40339537fa47c51
Felix Mann <felix.fei.mann@gmail.com>
2018-11-02 16:54:17 -0700

    Create SpeedPanelHighlighter :D

---
+++ Assets/Scripts/UI/BattlScene/GameSpeed/ISpeedPanelHighlighter.cs
@@ -0,0 +1,9 @@
+﻿namespace BattleCruisers.UI.BattleScene.GameSpeed
+{
+    public interface ISpeedPanelHighlighter
+    {
+        void UnhighlightButtons();
+        void HighlightSlowMotionButton();
+        void HighlightFastForwardButton();
+    }
+}
---
+++ Assets/Scripts/UI/BattlScene/GameSpeed/SpeedPanelHighlighter.cs
@@ -0,0 +1,36 @@
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.PlatformAbstractions;
+
+namespace BattleCruisers.UI.BattleScene.GameSpeed
+{
+    public class SpeedPanelHighlighter : ISpeedPanelHighlighter
+    {
+        private readonly IGameObject _slowMotionButtonHighlight, _fastForwardButtonHighlight;
+
+        public SpeedPanelHighlighter(IGameObject slowMotionButtonHighlight, IGameObject fastForwardButtonHighlight)
+        {
+            Helper.AssertIsNotNull(slowMotionButtonHighlight, fastForwardButtonHighlight);
+
+            _slowMotionButtonHighlight = slowMotionButtonHighlight;
+            _fastForwardButtonHighlight = fastForwardButtonHighlight;
+        }
+
+        public void UnhighlightButtons()
+        {
+            _slowMotionButtonHighlight.IsVisible = false;
+            _fastForwardButtonHighlight.IsVisible = false;
+        }
+
+        public void HighlightSlowMotionButton()
+        {
+            UnhighlightButtons();
+            _slowMotionButtonHighlight.IsVisible = true;
+        }
+
+        public void HighlightFastForwardButton()
+        {
+            UnhighlightButtons();
+            _fastForwardButtonHighlight.IsVisible = true;
+        }
+    }
+}
