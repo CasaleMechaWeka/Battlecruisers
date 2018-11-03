24acf67fd7d2166a8664011f51c29cfdfd5f0ea9
Felix Mann <felix.fei.mann@gmail.com>
2018-11-03 10:50:49 -0700

    New game speed buttons seem to work :D

---
+++ Assets/Scripts/Scenes/Test/BattleSceneUITestGod.cs
@@ -0,0 +1,16 @@
+﻿using BattleCruisers.UI.BattleScene.GameSpeed;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test
+{
+    public class BattleSceneUITestGod : MonoBehaviour
+    {
+        private void Start()
+        {
+            SpeedPanelController speedPanelInitialiser = FindObjectOfType<SpeedPanelController>();
+            Assert.IsNotNull(speedPanelInitialiser);
+            speedPanelInitialiser.Initialise();
+        }
+    }
+}
