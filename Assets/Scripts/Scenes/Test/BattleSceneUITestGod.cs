05fb23cb5ba00197c40af945bae006873848b4bc
Felix Mann <felix.fei.mann@gmail.com>
2018-11-03 11:30:55 -0700

    Add navigation wheel to UI test scene, seems to work? :P

--- Assets/Scripts/Scenes/Test/BattleSceneUITestGod.cs
+++ Assets/Scripts/Scenes/Test/BattleSceneUITestGod.cs
@@ -1,4 +1,11 @@
-﻿using BattleCruisers.UI.BattleScene.GameSpeed;
+﻿using BattleCruisers.Data.Settings;
+using BattleCruisers.UI.BattleScene.GameSpeed;
+using BattleCruisers.UI.BattleScene.Navigation;
+using BattleCruisers.UI.Cameras;
+using BattleCruisers.UI.Cameras.Adjusters;
+using BattleCruisers.UI.Cameras.Helpers;
+using BattleCruisers.Utils.PlatformAbstractions;
+using NSubstitute;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -6,11 +13,49 @@ namespace BattleCruisers.Scenes.Test
 {
     public class BattleSceneUITestGod : MonoBehaviour
     {
+        private ICameraAdjuster _cameraAdjuster;
+
+        public float smoothTime;
+
         private void Start()
         {
             SpeedPanelController speedPanelInitialiser = FindObjectOfType<SpeedPanelController>();
             Assert.IsNotNull(speedPanelInitialiser);
             speedPanelInitialiser.Initialise();
+
+            SetupNavigationWheel();
+        }
+
+        // NEWUI  Inject SettingsManager & Camera :D
+        private void SetupNavigationWheel()
+        {
+            NavigationWheelInitialiser navigationWheelInitialiser = FindObjectOfType<NavigationWheelInitialiser>();
+            INavigationWheelPanel navigationWheelPanel = navigationWheelInitialiser.InitialiseNavigationWheel();
+
+            Camera platformCamera = FindObjectOfType<Camera>();
+            ICamera camera = new CameraBC(platformCamera);
+            ICameraCalculatorSettings settings
+                = new CameraCalculatorSettings(
+                    Substitute.For<ISettingsManager>(),
+                    camera.Aspect);
+            ICameraCalculator cameraCalculator = new CameraCalculator(camera, settings);
+
+            ICameraNavigationWheelCalculator cameraNavigationWheelCalculator = new CameraNavigationWheelCalculator(navigationWheelPanel, cameraCalculator, settings.ValidOrthographicSizes);
+            ICameraTargetFinder cameraTargetFinder = new NavigationWheelCameraTargetFinder(cameraNavigationWheelCalculator, camera);
+            ICameraTargetProvider cameraTargetProvider = new NavigationWheelCameraTargetProvider(navigationWheelPanel.NavigationWheel, cameraTargetFinder);
+
+            // Smooth adjuster
+            _cameraAdjuster
+                = new SmoothCameraAdjuster(
+                    cameraTargetProvider,
+                    new SmoothZoomAdjuster(camera, smoothTime),
+                    new SmoothPositionAdjuster(camera.Transform, smoothTime));
+        }
+
+        // NEWUI  Move to CameraController?
+        private void Update()
+        {
+            _cameraAdjuster.AdjustCamera();
         }
     }
 }
