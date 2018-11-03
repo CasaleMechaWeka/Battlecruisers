c3243f65e23aeff4b7e858bc2176e5b697d8ff7c
Felix Mann <felix.fei.mann@gmail.com>
2018-11-03 10:19:43 -0700

    Create toggle buttons functionality :)

---
+++ Assets/Scripts/UI/BattlScene/Buttons/Toggles/ToggleButtonGroup.cs
@@ -0,0 +1,47 @@
+﻿using BattleCruisers.Utils;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene.Buttons.Toggles
+{
+    // FELIX  Test :)
+    public class ToggleButtonGroup
+    {
+        private IToggleButton _selectedButton;
+        private IToggleButton SelectedButton
+        {
+            get { return _selectedButton; }
+            set
+            {
+                if (_selectedButton != null)
+                {
+                    _selectedButton.IsSelected = false;
+                }
+
+                _selectedButton = value;
+
+                if (_selectedButton != null)
+                {
+                    _selectedButton.IsSelected = true;
+                }
+            }
+        }
+
+        public ToggleButtonGroup(IList<IToggleButton> buttons)
+        {
+            Assert.IsNotNull(buttons);
+
+            foreach (IToggleButton button in buttons)
+            {
+                button.Clicked += Button_Clicked;
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            IToggleButton clickedButton = sender.Parse<IToggleButton>();
+            SelectedButton = ReferenceEquals(SelectedButton, clickedButton) ? null : clickedButton;
+        }
+    }
+}
