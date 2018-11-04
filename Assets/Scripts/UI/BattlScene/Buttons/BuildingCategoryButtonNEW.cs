a89ba8275168222bef466e4248ef35e23f85cf38
Felix Mann <felix.fei.mann@gmail.com>
2018-11-04 06:35:40 -0800

    Create slightly simplified BuildingCategoryButtonNEW :)

---
+++ Assets/Scripts/UI/BattlScene/Buttons/BuildingCategoryButtonNEW.cs
@@ -0,0 +1,56 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.UI.BattleScene.Manager;
+using BattleCruisers.UI.Filters;
+using BattleCruisers.Utils;
+using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene.Buttons
+{
+    // NEWUI  Replace old UI :P
+    public class BuildingCategoryButtonNEW : UIElement, IBuildingCategoryButton, IBroadcastingFilter
+	{
+        private IUIManager _uiManager;
+        private IBroadcastingFilter<BuildingCategory> _shouldBeEnabledFilter;
+
+        public event EventHandler Clicked;
+
+        public event EventHandler PotentialMatchChange
+        {
+            add { _shouldBeEnabledFilter.PotentialMatchChange += value; }
+            remove { _shouldBeEnabledFilter.PotentialMatchChange -= value; }
+        }
+
+        public BuildingCategory Category { get; private set; }
+
+        public bool IsMatch { get { return _shouldBeEnabledFilter.IsMatch(Category); } }
+
+        public void Initialise(
+            BuildingCategory buildingCategory,
+            IUIManager uiManager, 
+            IBroadcastingFilter<BuildingCategory> shouldBeEnabledFilter)
+		{
+            base.Initialise();
+
+            Helper.AssertIsNotNull(uiManager, shouldBeEnabledFilter);
+
+            _uiManager = uiManager;
+            _shouldBeEnabledFilter = shouldBeEnabledFilter;
+            Category = buildingCategory;
+
+            ButtonWrapper buttonWrapper = GetComponent<ButtonWrapper>();
+            Assert.IsNotNull(buttonWrapper);
+            buttonWrapper.Initialise(HandleClick, this);
+		}
+
+        private void HandleClick()
+        {
+            _uiManager.SelectBuildingGroup(Category);
+
+            if (Clicked != null)
+            {
+                Clicked.Invoke(this, EventArgs.Empty);
+            }
+        }
+	}
+}
