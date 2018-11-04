e9797fabbe7dbadc17dbcae68d02ea51cb81dd0d
Felix Mann <felix.fei.mann@gmail.com>
2018-11-04 07:08:32 -0800

    Create simplified IBuildMenuNEW :)

---
+++ Assets/Scripts/UI/BattlScene/BuildMenus/IBuildMenuNEW.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Buildings.Factories;
+
+namespace BattleCruisers.UI.BattleScene.BuildMenus
+{
+    public interface IBuildMenuNEW : IBuildMenuButtons
+    {
+        /// <summary>
+        /// Shows the building group menu for the given buildingCategory.
+        /// Hides any currently shown menu.
+        /// </summary>
+        void ShowBuildingGroupMenu(BuildingCategory buildingCategory);
+
+        /// <summary>
+        /// Shows the units menu for the given factory.  Hides any currently 
+        /// shown menu.
+        /// </summary>
+        void ShowUnitsMenu(IFactory factory);
+
+        /// <summary>
+        /// Hides the currently shown menu, if there is one.
+        /// </summary>
+        void HideCurrentlyShownMenu();
+    }
+}
