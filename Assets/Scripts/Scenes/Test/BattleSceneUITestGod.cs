0b646c0cd7dd15324cf40a0fd61be0e29376ef78
Felix Mann <felix.fei.mann@gmail.com>
2018-11-04 07:43:18 -0800

    Extend BattleSceneUITestGod to initiliase BuildMenuControllerNEW (broken :P)

--- Assets/Scripts/Scenes/Test/BattleSceneUITestGod.cs
+++ Assets/Scripts/Scenes/Test/BattleSceneUITestGod.cs
@@ -1,11 +1,26 @@
-﻿using BattleCruisers.Data.Settings;
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Data;
+using BattleCruisers.Data.Models;
+using BattleCruisers.Data.Settings;
+using BattleCruisers.UI.BattleScene;
+using BattleCruisers.UI.BattleScene.BuildMenus;
+using BattleCruisers.UI.BattleScene.Buttons.Filters;
 using BattleCruisers.UI.BattleScene.GameSpeed;
+using BattleCruisers.UI.BattleScene.Manager;
 using BattleCruisers.UI.BattleScene.Navigation;
 using BattleCruisers.UI.Cameras;
 using BattleCruisers.UI.Cameras.Adjusters;
 using BattleCruisers.UI.Cameras.Helpers;
+using BattleCruisers.UI.Sound;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Fetchers;
 using BattleCruisers.Utils.PlatformAbstractions;
+using BattleCruisers.Utils.Sorting;
+using BattleCruisers.Utils.Threading;
 using NSubstitute;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -14,16 +29,38 @@ namespace BattleCruisers.Scenes.Test
     public class BattleSceneUITestGod : MonoBehaviour
     {
         private ICameraAdjuster _cameraAdjuster;
+        private IDataProvider _dataProvider;
 
         public float smoothTime;
+        public BuildMenuControllerNEW buildMenuController;
 
         private void Start()
+        {
+            // FELIX  Extract GetComponents() to separate method?
+            IVariableDelayDeferrer variableDelayDeferrer = GetComponent<IVariableDelayDeferrer>();
+
+            Helper.AssertIsNotNull(buildMenuController, variableDelayDeferrer);
+
+            _dataProvider = ApplicationModel.DataProvider;
+
+            // Common setup
+            IPrefabFactory prefabFactory = new PrefabFactory(new PrefabFetcher());
+            ISpriteProvider spriteProvider = new SpriteProvider(new SpriteFetcher());
+            IBattleSceneHelper helper = CreateHelper(prefabFactory, variableDelayDeferrer);
+
+            // Instantiate player cruiser
+            ILoadout playerLoadout = helper.GetPlayerLoadout();
+
+            InitialiseSpeedPanel();
+            SetupNavigationWheel();
+            InitialiseBuildMenuController(playerLoadout, prefabFactory, spriteProvider);
+        }
+
+        private static void InitialiseSpeedPanel()
         {
             SpeedPanelController speedPanelInitialiser = FindObjectOfType<SpeedPanelController>();
             Assert.IsNotNull(speedPanelInitialiser);
             speedPanelInitialiser.Initialise();
-
-            SetupNavigationWheel();
         }
 
         // NEWUI  Inject SettingsManager & Camera :D
@@ -52,6 +89,53 @@ namespace BattleCruisers.Scenes.Test
                     new SmoothPositionAdjuster(camera.Transform, smoothTime));
         }
 
+        private void InitialiseBuildMenuController(
+            ILoadout playerLoadout,
+            IPrefabFactory prefabFactory,
+            ISpriteProvider spriteProvider)
+        {
+            // FELIX  Create functional UIManager :P
+            IUIManager uiManager = new UIManagerNEW();
+
+            IBuildingGroupFactory buildingGroupFactory = new BuildingGroupFactory();
+            IPrefabOrganiser prefabOrganiser = new PrefabOrganiser(playerLoadout, prefabFactory, buildingGroupFactory);
+            IList<IBuildingGroup> buildingGroups = prefabOrganiser.GetBuildingGroups();
+            IDictionary<UnitCategory, IList<IBuildableWrapper<IUnit>>> units = prefabOrganiser.GetUnits();
+            IBuildableSorterFactory sorterFactory = new BuildableSorterFactory();
+            // FELIX Pass real class :P  (Or create dummy filters that always return true)
+            IButtonVisibilityFilters buttonVisibilityFilters = Substitute.For<IButtonVisibilityFilters>();
+            // FELIX  Pass real class, or remove use :P
+            IPlayerCruiserFocusHelper playerCruiserFocusHelper = Substitute.For<IPlayerCruiserFocusHelper>();
+            IPrioritisedSoundPlayer soundPlayer = Substitute.For<IPrioritisedSoundPlayer>();
+
+            buildMenuController
+                .Initialise(
+                    uiManager,
+                    buildingGroups,
+                    units,
+                    sorterFactory,
+                    buttonVisibilityFilters,
+                    spriteProvider,
+                    playerCruiserFocusHelper,
+                    // FELIX  Pass real sound player :)
+                    soundPlayer);
+                    //helper.GetBuildableButtonSoundPlayer(_playerCruiser));
+        }
+
+        private IBattleSceneHelper CreateHelper(IPrefabFactory prefabFactory, IVariableDelayDeferrer variableDelayDeferrer)
+        {
+            // FELIX  Handle tutorial :)
+            //if (ApplicationModel.IsTutorial)
+            //{
+            //    TutorialHelper helper = new TutorialHelper(_dataProvider, prefabFactory);
+            //    _tutorialProvider = helper;
+            //    return helper;
+            //}
+            //else
+            {
+                return new NormalHelper(_dataProvider, prefabFactory, variableDelayDeferrer);
+            }
+        }
         // NEWUI  Move to CameraController?
         private void Update()
         {
