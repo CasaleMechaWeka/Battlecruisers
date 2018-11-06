7454fb2c5fe5068a3b710d32e0e905b43c667db4
Felix Mann <felix.fei.mann@gmail.com>
2018-11-05 17:02:34 -0800

    Create InformatorPanelController (basically half of HUDCanvasController :)

---
+++ Assets/Scripts/UI/Common/BuildableDetails/InformatorPanelController.cs
@@ -0,0 +1,52 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Targets.TargetTrackers;
+using BattleCruisers.Utils;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Common.BuildableDetails
+{
+    public class InformatorPanelController : MonoBehaviour
+    {
+        private BuildingDetailsController _buildingDetails;
+        public IBuildableDetails<IBuilding> BuildingDetails { get { return _buildingDetails; } }
+
+        private UnitDetailsController _unitDetails;
+        public IBuildableDetails<IUnit> UnitDetails { get { return _unitDetails; } }
+
+        private CruiserDetailsController _cruiserDetails;
+        public ICruiserDetails CruiserDetails { get { return _cruiserDetails; } }
+
+        public void StaticInitialise()
+        {
+            _buildingDetails = GetComponentInChildren<BuildingDetailsController>(includeInactive: true);
+            Assert.IsNotNull(_buildingDetails);
+
+            _unitDetails = GetComponentInChildren<UnitDetailsController>(includeInactive: true);
+            Assert.IsNotNull(_unitDetails);
+
+            _cruiserDetails = GetComponentInChildren<CruiserDetailsController>(includeInactive: true);
+            Assert.IsNotNull(_cruiserDetails);
+        }
+
+        public void Initialise(
+            ICruiser playerCruiser,
+            IUserChosenTargetHelper userChosenTargetHelper,
+            IFilter<ITarget> chooseTargetButtonVisibilityFilter,
+            IFilter<ITarget> deleteButtonVisibilityFilter)
+        {
+            Helper.AssertIsNotNull(
+                playerCruiser,
+                userChosenTargetHelper,
+                chooseTargetButtonVisibilityFilter,
+                deleteButtonVisibilityFilter);
+
+            _buildingDetails.Initialise(playerCruiser.DroneFocuser, playerCruiser.RepairManager, userChosenTargetHelper, chooseTargetButtonVisibilityFilter, deleteButtonVisibilityFilter);
+            _unitDetails.Initialise(playerCruiser.DroneFocuser, playerCruiser.RepairManager, userChosenTargetHelper, chooseTargetButtonVisibilityFilter, deleteButtonVisibilityFilter);
+            _cruiserDetails.Initialise(playerCruiser.DroneFocuser, playerCruiser.RepairManager, userChosenTargetHelper, chooseTargetButtonVisibilityFilter);
+        }
+    }
+}
