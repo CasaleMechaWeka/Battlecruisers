8781b5398c840e4f562c0e4ada1ca5a468f36fa5
Felix Mann <felix.fei.mann@gmail.com>
2018-11-07 17:54:39 -0800

    Create DronesDisplayer :)

---
+++ Assets/Scripts/UI/BattlScene/Cruisers/DronesDisplayer.cs
@@ -0,0 +1,51 @@
+﻿using BattleCruisers.Cruisers.Drones;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.PlatformAbstractions;
+using System;
+
+namespace BattleCruisers.UI.BattleScene.Cruisers
+{
+    // FELIX  test :)
+    public class DronesDisplayer
+    {
+        private readonly IDroneManager _droneManager;
+        private readonly IDroneManagerMonitor _droneManagerMonitor;
+        private readonly INumberDisplay _numberDisplay;
+        private readonly IGameObject _idleFeedback;
+
+        public DronesDisplayer(
+            IDroneManager droneManager, 
+            IDroneManagerMonitor droneManagerMonitor, 
+            INumberDisplay numberDisplay, 
+            IGameObject idleFeedback)
+        {
+            Helper.AssertIsNotNull(droneManager, droneManagerMonitor, numberDisplay, idleFeedback);
+
+            _droneManager = droneManager;
+            _droneManagerMonitor = droneManagerMonitor;
+            _numberDisplay = numberDisplay;
+            _idleFeedback = idleFeedback;
+
+            _numberDisplay.Num = _droneManager.NumOfDrones;
+
+            _droneManager.DroneNumChanged += _droneManager_DroneNumChanged;
+            _droneManagerMonitor.IdleDronesStarted += _droneManagerMonitor_IdleDronesStarted;
+            _droneManagerMonitor.IdleDronesEnded += _droneManagerMonitor_IdleDronesEnded;
+        }
+
+        private void _droneManager_DroneNumChanged(object sender, DroneNumChangedEventArgs e)
+        {
+            _numberDisplay.Num = _droneManager.NumOfDrones;
+        }
+
+        private void _droneManagerMonitor_IdleDronesStarted(object sender, EventArgs e)
+        {
+            _idleFeedback.IsVisible = true;
+        }
+
+        private void _droneManagerMonitor_IdleDronesEnded(object sender, EventArgs e)
+        {
+            _idleFeedback.IsVisible = false;
+        }
+    }
+}
