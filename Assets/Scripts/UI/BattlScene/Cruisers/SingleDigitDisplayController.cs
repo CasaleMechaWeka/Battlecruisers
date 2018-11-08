5a99f98835b9349d66471b7c9c49529cafba09d8
Felix Mann <felix.fei.mann@gmail.com>
2018-11-08 06:10:14 -0800

    Create SingleDigitDisplayController :)

---
+++ Assets/Scripts/UI/BattlScene/Cruisers/SingleDigitDisplayController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.BattleScene.Cruisers
+{
+    public class SingleDigitDisplayController : MonoBehaviour, INumberDisplay
+    {
+        public Text text;
+
+        private const int MIN_VALUE = 0;
+        private const int MAX_VALUE = 9;
+
+        public int Num
+        {
+            set
+            {
+                Assert.IsTrue(value >= MIN_VALUE);
+                Assert.IsTrue(value <= MAX_VALUE);
+
+                text.text = value.ToString();
+            }
+        }
+
+        public void Initialise()
+        {
+            Assert.IsNotNull(text);
+        }
+    }
+}
