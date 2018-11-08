dda08e7d101f38ddc9aa3183946e51b992f25354
Felix Mann <felix.fei.mann@gmail.com>
2018-11-08 06:24:58 -0800

    Create num of drones panel initialisers :)

---
+++ Assets/Scripts/UI/BattlScene/Cruisers/DronesPanelInitialiser.cs
@@ -0,0 +1,29 @@
+﻿using BattleCruisers.Cruisers.Drones;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.PlatformAbstractions;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.BattleScene.Cruisers
+{
+    public class DronesPanelInitialiser : MonoBehaviour
+    {
+        // Hold reference to avoid garbage collection
+#pragma warning disable CS0414  // Variable is assigned but never used
+        private DronesDisplayer _dronesDisplayer;
+#pragma warning restore CS0414  // Variable is assigned but never used
+
+        public void Initialise(IDroneManager droneManager, IDroneManagerMonitor droneManagerMonitor)
+        {
+            Helper.AssertIsNotNull(droneManager, droneManagerMonitor);
+
+            Image highlight = transform.FindNamedComponent<Image>("Highlight");
+            IGameObject highlightGameObject = new GameObjectBC(highlight.gameObject);
+
+            NumOfDronesPanelInitialiser numOfDronesPanel = GetComponentInChildren<NumOfDronesPanelInitialiser>();
+            INumberDisplay twoDigitDisplayer = numOfDronesPanel.CreateTwoDigitDisplay();
+
+            _dronesDisplayer = new DronesDisplayer(droneManager, droneManagerMonitor, twoDigitDisplayer, highlightGameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/BattlScene/Cruisers/NumOfDronesPanelInitialiser.cs
@@ -0,0 +1,15 @@
+﻿using BattleCruisers.Utils;
+using UnityEngine;
+
+namespace BattleCruisers.UI.BattleScene.Cruisers
+{
+    public class NumOfDronesPanelInitialiser : MonoBehaviour
+    {
+        public INumberDisplay CreateTwoDigitDisplay()
+        {
+            SingleDigitDisplayController firstDigit = transform.FindNamedComponent<SingleDigitDisplayController>("FirstDigit");
+            SingleDigitDisplayController secondDigit = transform.FindNamedComponent<SingleDigitDisplayController>("SecondDigit");
+            return new TwoDigitDisplay(firstDigit, secondDigit);
+        }
+    }
+}
