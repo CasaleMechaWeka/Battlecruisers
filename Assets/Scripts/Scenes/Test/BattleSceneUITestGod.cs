b93ea78cc1768a5a35c24ba7ff9e63625e006616
Felix Mann <felix.fei.mann@gmail.com>
2018-11-08 16:49:18 -0800

    Remove unused code :)  (Moved to LeftPanelInitialiser)

--- Assets/Scripts/Scenes/Test/BattleSceneUITestGod.cs
+++ Assets/Scripts/Scenes/Test/BattleSceneUITestGod.cs
@@ -1,5 +1,4 @@
 ﻿using BattleCruisers.Buildables;
-using BattleCruisers.Buildables.Buildings;
 using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Buildables.Units;
 using BattleCruisers.Cruisers;
@@ -13,12 +12,8 @@ using BattleCruisers.UI.BattleScene;
 using BattleCruisers.UI.BattleScene.BuildMenus;
 using BattleCruisers.UI.BattleScene.Buttons;
 using BattleCruisers.UI.BattleScene.Buttons.Filters;
-using BattleCruisers.UI.BattleScene.Cruisers;
 using BattleCruisers.UI.BattleScene.GameSpeed;
 using BattleCruisers.UI.BattleScene.Manager;
-using BattleCruisers.UI.BattleScene.Navigation;
-using BattleCruisers.UI.Cameras;
-using BattleCruisers.UI.Cameras.Adjusters;
 using BattleCruisers.UI.Cameras.Helpers;
 using BattleCruisers.UI.Common.BuildableDetails;
 using BattleCruisers.UI.Sound;
@@ -26,10 +21,8 @@ using BattleCruisers.Utils;
 using BattleCruisers.Utils.BattleScene;
 using BattleCruisers.Utils.Fetchers;
 using BattleCruisers.Utils.PlatformAbstractions;
-using BattleCruisers.Utils.Sorting;
 using BattleCruisers.Utils.Threading;
 using NSubstitute;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -37,7 +30,6 @@ namespace BattleCruisers.Scenes.Test
 {
     public class BattleSceneUITestGod : MonoBehaviour
     {
-        private ICameraAdjuster _cameraAdjuster;
         private ISceneNavigator _sceneNavigator;
         private IApplicationModel _applicationModel;
         private IDataProvider _dataProvider;
@@ -151,32 +143,6 @@ namespace BattleCruisers.Scenes.Test
             speedPanelInitialiser.Initialise();
         }
 
-        // NEWUI  Inject SettingsManager & Camera :D
-        private void SetupNavigationWheel()
-        {
-            NavigationWheelInitialiser navigationWheelInitialiser = FindObjectOfType<NavigationWheelInitialiser>();
-            INavigationWheelPanel navigationWheelPanel = navigationWheelInitialiser.InitialiseNavigationWheel();
-
-            Camera platformCamera = FindObjectOfType<Camera>();
-            ICamera camera = new CameraBC(platformCamera);
-            ICameraCalculatorSettings settings
-                = new CameraCalculatorSettings(
-                    Substitute.For<ISettingsManager>(),
-                    camera.Aspect);
-            ICameraCalculator cameraCalculator = new CameraCalculator(camera, settings);
-
-            ICameraNavigationWheelCalculator cameraNavigationWheelCalculator = new CameraNavigationWheelCalculator(navigationWheelPanel, cameraCalculator, settings.ValidOrthographicSizes);
-            ICameraTargetFinder cameraTargetFinder = new NavigationWheelCameraTargetFinder(cameraNavigationWheelCalculator, camera);
-            ICameraTargetProvider cameraTargetProvider = new NavigationWheelCameraTargetProvider(navigationWheelPanel.NavigationWheel, cameraTargetFinder);
-
-            // Smooth adjuster
-            _cameraAdjuster
-                = new SmoothCameraAdjuster(
-                    cameraTargetProvider,
-                    new SmoothZoomAdjuster(camera, smoothTime),
-                    new SmoothPositionAdjuster(camera.Transform, smoothTime));
-        }
-
         private IUIManager CreateUIManager(ICruiser playerCruiser, ICruiser aiCruiser)
         {
             return
@@ -185,37 +151,6 @@ namespace BattleCruisers.Scenes.Test
                     new BuildableDetailsManager(informator),
                     playerCruiser,
                     aiCruiser);
-        }
-
-        private void SetupBuildMenuController(
-            IUIManager uiManager,
-            ILoadout playerLoadout,
-            IPrefabFactory prefabFactory,
-            ISpriteProvider spriteProvider,
-            IButtonVisibilityFilters buttonVisibilityFilters)
-        {
-
-            IBuildingGroupFactory buildingGroupFactory = new BuildingGroupFactory();
-            IPrefabOrganiser prefabOrganiser = new PrefabOrganiser(playerLoadout, prefabFactory, buildingGroupFactory);
-            IList<IBuildingGroup> buildingGroups = prefabOrganiser.GetBuildingGroups();
-            IDictionary<UnitCategory, IList<IBuildableWrapper<IUnit>>> units = prefabOrganiser.GetUnits();
-            IBuildableSorterFactory sorterFactory = new BuildableSorterFactory();
-            // FELIX  Pass real class, or remove use :P
-            IPlayerCruiserFocusHelper playerCruiserFocusHelper = Substitute.For<IPlayerCruiserFocusHelper>();
-            IPrioritisedSoundPlayer soundPlayer = Substitute.For<IPrioritisedSoundPlayer>();
-
-            buildMenu
-                .Initialise(
-                    uiManager,
-                    buildingGroups,
-                    units,
-                    sorterFactory,
-                    buttonVisibilityFilters,
-                    spriteProvider,
-                    playerCruiserFocusHelper,
-                    // FELIX  Pass real sound player :)
-                    soundPlayer);
-                    //helper.GetBuildableButtonSoundPlayer(_playerCruiser));
         }
 
         private IBattleSceneHelper CreateHelper(IPrefabFactory prefabFactory, IVariableDelayDeferrer variableDelayDeferrer)
@@ -256,19 +191,6 @@ namespace BattleCruisers.Scenes.Test
                     playerCruiser,
                     userChosenTargetHelper,
                     buttonVisibilityFilters);
-        }
-
-        private void SetupDronesPanel(IDroneManager droneManager, IDroneManagerMonitor droneManagerMonitor)
-        {
-            DronesPanelInitialiser dronesPanelInitialiser = FindObjectOfType<DronesPanelInitialiser>();
-            Assert.IsNotNull(dronesPanelInitialiser);
-            dronesPanelInitialiser.Initialise(droneManager, droneManagerMonitor);
-        }
-
-        // NEWUI  Move to CameraController?
-        private void Update()
-        {
-            //_cameraAdjuster.AdjustCamera();
         }
 
         // To test showing unit buttons
