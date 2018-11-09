4089a018037382f0d240566e6b35cf3d81dfe509
Felix Mann <felix.fei.mann@gmail.com>
2018-11-08 16:36:48 -0800

    Create LeftPanelInitialiser :)

---
+++ Assets/Scripts/UI/BattlScene/LeftPanelInitialiser.cs
@@ -0,0 +1,134 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Cruisers.Drones;
+using BattleCruisers.Data.Models;
+using BattleCruisers.Data.Settings;
+using BattleCruisers.UI.BattleScene.BuildMenus;
+using BattleCruisers.UI.BattleScene.Buttons.Filters;
+using BattleCruisers.UI.BattleScene.Cruisers;
+using BattleCruisers.UI.BattleScene.Manager;
+using BattleCruisers.UI.BattleScene.Navigation;
+using BattleCruisers.UI.Cameras;
+using BattleCruisers.UI.Cameras.Adjusters;
+using BattleCruisers.UI.Cameras.Helpers;
+using BattleCruisers.UI.Sound;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Fetchers;
+using BattleCruisers.Utils.PlatformAbstractions;
+using BattleCruisers.Utils.Sorting;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene
+{
+    /// <summary>
+    /// Contains:
+    /// + Drone number display
+    /// + Navigation wheel
+    /// + Cruiser health dial
+    /// + Build menu
+    /// </summary>
+    public class LeftPanelInitialiser : MonoBehaviour
+    {
+        // NEWUI  Move to CameraController?
+        private ICameraAdjuster _cameraAdjuster;
+
+        // FELIX  Group parameters in classes? :P
+        public void Initialise(
+            IDroneManager droneManager, 
+            IDroneManagerMonitor droneManagerMonitor,
+            ICamera camera, 
+            ISettingsManager settingsManager, 
+            float cameraSmoothTime,
+            IUIManager uiManager,
+            ILoadout playerLoadout,
+            IPrefabFactory prefabFactory,
+            ISpriteProvider spriteProvider,
+            IButtonVisibilityFilters buttonVisibilityFilters,
+            IPlayerCruiserFocusHelper playerCruiserFocusHelper,
+            IPrioritisedSoundPlayer soundPlayer)
+        {
+            Helper.AssertIsNotNull(
+                droneManager, 
+                droneManagerMonitor, 
+                camera, 
+                settingsManager,
+                uiManager,
+                playerLoadout,
+                prefabFactory,
+                spriteProvider,
+                buttonVisibilityFilters,
+                playerCruiserFocusHelper,
+                soundPlayer);
+
+            SetupDronesPanel(droneManager, droneManagerMonitor);
+            SetupNavigationWheel(camera, settingsManager, cameraSmoothTime);
+            // FELIX  Setup cruiser health dial :D
+            SetupBuildMenuController(uiManager, playerLoadout, prefabFactory, spriteProvider, buttonVisibilityFilters, playerCruiserFocusHelper, soundPlayer);
+        }
+
+        private void SetupDronesPanel(IDroneManager droneManager, IDroneManagerMonitor droneManagerMonitor)
+        {
+            DronesPanelInitialiser dronesPanelInitialiser = FindObjectOfType<DronesPanelInitialiser>();
+            Assert.IsNotNull(dronesPanelInitialiser);
+            dronesPanelInitialiser.Initialise(droneManager, droneManagerMonitor);
+        }
+
+        private void SetupNavigationWheel(ICamera camera, ISettingsManager settingsManager, float cameraSmoothTime)
+        {
+            NavigationWheelInitialiser navigationWheelInitialiser = FindObjectOfType<NavigationWheelInitialiser>();
+            INavigationWheelPanel navigationWheelPanel = navigationWheelInitialiser.InitialiseNavigationWheel();
+
+            ICameraCalculatorSettings settings = new CameraCalculatorSettings(settingsManager, camera.Aspect);
+            ICameraCalculator cameraCalculator = new CameraCalculator(camera, settings);
+
+            ICameraNavigationWheelCalculator cameraNavigationWheelCalculator = new CameraNavigationWheelCalculator(navigationWheelPanel, cameraCalculator, settings.ValidOrthographicSizes);
+            ICameraTargetFinder cameraTargetFinder = new NavigationWheelCameraTargetFinder(cameraNavigationWheelCalculator, camera);
+            ICameraTargetProvider cameraTargetProvider = new NavigationWheelCameraTargetProvider(navigationWheelPanel.NavigationWheel, cameraTargetFinder);
+
+            _cameraAdjuster
+                = new SmoothCameraAdjuster(
+                    cameraTargetProvider,
+                    new SmoothZoomAdjuster(camera, cameraSmoothTime),
+                    new SmoothPositionAdjuster(camera.Transform, cameraSmoothTime));
+        }
+
+        private void SetupBuildMenuController(
+            IUIManager uiManager,
+            ILoadout playerLoadout,
+            IPrefabFactory prefabFactory,
+            ISpriteProvider spriteProvider,
+            IButtonVisibilityFilters buttonVisibilityFilters,
+            IPlayerCruiserFocusHelper playerCruiserFocusHelper,
+            IPrioritisedSoundPlayer soundPlayer)
+        {
+            IBuildingGroupFactory buildingGroupFactory = new BuildingGroupFactory();
+            IPrefabOrganiser prefabOrganiser = new PrefabOrganiser(playerLoadout, prefabFactory, buildingGroupFactory);
+            IList<IBuildingGroup> buildingGroups = prefabOrganiser.GetBuildingGroups();
+            IDictionary<UnitCategory, IList<IBuildableWrapper<IUnit>>> units = prefabOrganiser.GetUnits();
+            IBuildableSorterFactory sorterFactory = new BuildableSorterFactory();
+
+            BuildMenuControllerNEW buildMenu = FindObjectOfType<BuildMenuControllerNEW>();
+            Assert.IsNotNull(buildMenu);
+
+            buildMenu
+                .Initialise(
+                    uiManager,
+                    buildingGroups,
+                    units,
+                    sorterFactory,
+                    buttonVisibilityFilters,
+                    spriteProvider,
+                    playerCruiserFocusHelper,
+                    soundPlayer);
+        }
+
+        // NEWUI  Move to CameraController?
+        private void Update()
+        {
+            _cameraAdjuster.AdjustCamera();
+        }
+    }
+}
