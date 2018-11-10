434d288b1815ea42ab551d925c75621bb2410ab6
Felix Mann <felix.fei.mann@gmail.com>
2018-11-10 07:29:40 -0800

    Create BattleSceneGodComponents :D

---
+++ Assets/Scripts/Scenes/BattleSceneGodComponents.cs
@@ -0,0 +1,32 @@
+﻿using BattleCruisers.Tutorial.Highlighting;
+using BattleCruisers.Utils.PlatformAbstractions.UI;
+using BattleCruisers.Utils.Threading;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes
+{
+    public class BattleSceneGodComponents : MonoBehaviour, IBattleSceneGodComponents
+    {
+        public IDeferrer Deferrer { get; private set; }
+        public IVariableDelayDeferrer VariableDelayDeferrer { get; private set; }
+        public IHighlightFactory HighlightFactory { get; private set; }
+        public IAudioSource AudioSource { get; private set; }
+
+        public void Initialise()
+        {
+            Deferrer = GetComponent<IDeferrer>();
+            Assert.IsNotNull(Deferrer);
+
+            VariableDelayDeferrer = GetComponent<IVariableDelayDeferrer>();
+            Assert.IsNotNull(VariableDelayDeferrer);
+
+            HighlightFactory = GetComponent<IHighlightFactory>();
+            Assert.IsNotNull(HighlightFactory);
+
+            AudioSource platformAudioSource = GetComponent<AudioSource>();
+            Assert.IsNotNull(platformAudioSource);
+            AudioSource = new AudioSourceBC(platformAudioSource);
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/IBattleSceneGodComponents.cs
@@ -0,0 +1,14 @@
+﻿using BattleCruisers.Tutorial.Highlighting;
+using BattleCruisers.Utils.PlatformAbstractions.UI;
+using BattleCruisers.Utils.Threading;
+
+namespace BattleCruisers.Scenes
+{
+    public interface IBattleSceneGodComponents
+    {
+        IDeferrer Deferrer { get; }
+        IVariableDelayDeferrer VariableDelayDeferrer { get; }
+        IHighlightFactory HighlightFactory { get; }
+        IAudioSource AudioSource { get; }
+    }
+}
