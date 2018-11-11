dedb018d41cb0959646621a2e9b144fe7b56ed2b
Felix Mann <felix.fei.mann@gmail.com>
2018-11-11 12:51:33 -0800

    Initialise health dial :)

--- Assets/Scripts/UI/Common/BuildableDetails/InformatorWidgetManager.cs
+++ Assets/Scripts/UI/Common/BuildableDetails/InformatorWidgetManager.cs
@@ -2,6 +2,7 @@
 using BattleCruisers.Buildables.Repairables;
 using BattleCruisers.Cruisers.Drones;
 using BattleCruisers.Targets.TargetTrackers;
+using BattleCruisers.UI.BattleScene.ProgressBars;
 using BattleCruisers.UI.Common.BuildableDetails.Buttons;
 using BattleCruisers.Utils;
 using UnityEngine;
@@ -14,6 +15,7 @@ namespace BattleCruisers.UI.Common.BuildableDetails
         private RepairButtonController _repairButton;
         private ChooseTargetButtonController _chooseTargetButton;
         private DeleteButtonController _deleteButton;
+        private IHealthDial<IBuildable> _healthDial;
 
         private ToggleDroneButtonController _toggleDronesButton;
         public IButton ToggleDronesButton { get { return _toggleDronesButton; } }
@@ -25,6 +27,7 @@ namespace BattleCruisers.UI.Common.BuildableDetails
                 _toggleDronesButton.Buildable = value;
                 _repairButton.Repairable = value;
                 _chooseTargetButton.Target = value;
+                _healthDial.Damagable = value;
             }
         }
 
@@ -47,6 +50,10 @@ namespace BattleCruisers.UI.Common.BuildableDetails
             _chooseTargetButton = GetComponentInChildren<ChooseTargetButtonController>(includeInactive: true);
             Assert.IsNotNull(_chooseTargetButton);
             _chooseTargetButton.Initialise(userChosenTargetHelper, chooseTargetButtonVisibilityFilter);
+
+            BuildableHealthDialInitialiser healthDialInitialiser = GetComponentInChildren<BuildableHealthDialInitialiser>(includeInactive: true);
+            Assert.IsNotNull(healthDialInitialiser);
+            _healthDial = healthDialInitialiser.Initialise();
         }
     }
 }
