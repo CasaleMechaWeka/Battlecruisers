47266ae5c96bfea0232672b8c69944c6d163718d
Felix Mann <felix.fei.mann@gmail.com>
2018-11-11 12:50:55 -0800

    Create filter :D

---
+++ Assets/Scripts/UI/BattlScene/ProgressBars/BuildableIsInitialisedFilter.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.BattleScene.ProgressBars
+{
+    public class BuildableIsInitialisedFilter : IFilter<IBuildable>
+    {
+        public bool IsMatch(IBuildable element)
+        {
+            return element.IsInitialised;
+        }
+    }
+}
