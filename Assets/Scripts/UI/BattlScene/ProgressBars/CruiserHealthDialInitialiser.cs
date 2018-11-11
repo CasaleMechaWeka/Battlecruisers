21bbb09b3f50f8536495bcca8fd19799fce1f865
Felix Mann <felix.fei.mann@gmail.com>
2018-11-11 12:51:15 -0800

    Create health dial initialisers :D

---
+++ Assets/Scripts/UI/BattlScene/ProgressBars/CruiserHealthDialInitialiser.cs
@@ -0,0 +1,14 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.UI.Filters;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.BattleScene.ProgressBars
+{
+    public class CruiserHealthDialInitialiser : HealthDialInitialiserBase<ICruiser>
+    {
+        protected override IFilter<ICruiser> CreateVisibilityFilter()
+        {
+            return new StaticFilter<ICruiser>(isMatch: true);
+        }
+    }
+}
