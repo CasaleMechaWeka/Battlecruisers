72d267ac3cced6970a1e90d01dd9280a8e488038
Felix Mann <felix.fei.mann@gmail.com>
2018-11-19 06:55:50 -0800

    Create CameraAdjustmentWaitStep :D

---
+++ Assets/Scripts/Tutorial/Steps/WaitSteps/CameraAdjustmentWaitStep.cs
@@ -0,0 +1,34 @@
+﻿using BattleCruisers.UI.Cameras.Adjusters;
+using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.WaitSteps
+{
+    /// <summary>
+    /// Completed when camera completes it's current adjustment.
+    /// </summary>
+    /// FELIX  Test :D
+    public class CameraAdjustmentWaitStep : TutorialStep
+    {
+        private readonly ICameraAdjuster _cameraAdjuster;
+
+        public CameraAdjustmentWaitStep(ITutorialStepArgs args, ICameraAdjuster cameraAdjuster)
+            : base(args)
+        {
+            Assert.IsNotNull(cameraAdjuster);
+            _cameraAdjuster = cameraAdjuster;
+        }
+
+        public override void Start(Action completionCallback)
+        {
+            base.Start(completionCallback);
+            _cameraAdjuster.CompletedAdjustment += _cameraAdjuster_CompletedAdjustment;
+        }
+
+        private void _cameraAdjuster_CompletedAdjustment(object sender, EventArgs e)
+        {
+            _cameraAdjuster.CompletedAdjustment -= _cameraAdjuster_CompletedAdjustment;
+            OnCompleted();
+        }
+    }
+}
