06705ba932f1ff128fb8cc9d5dab750bbb98c571
Felix Mann <felix.fei.mann@gmail.com>
2018-11-19 07:01:59 -0800

    Create new tutorial args, which are a subset of the existing tutorial args :)

---
+++ Assets/Scripts/Tutorial/ITutorialArgsNEW.cs
@@ -0,0 +1,16 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Utils.Fetchers;
+
+namespace BattleCruisers.Tutorial
+{
+    // FELIX  Expand as needed :)
+    public interface ITutorialArgsNEW
+    {
+        ICruiser PlayerCruiser { get; }
+        ICruiser AICruiser { get; }
+        ITutorialProvider TutorialProvider { get; }
+        IPrefabFactory PrefabFactory { get; }
+        // FELIX  Adapt for navigation wheel
+		//INavigationSettings NavigationSettings { get; }
+    }
+}
---
+++ Assets/Scripts/Tutorial/TutorialArgsNEW.cs
@@ -0,0 +1,28 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Fetchers;
+
+namespace BattleCruisers.Tutorial
+{
+    public class TutorialArgsNEW : ITutorialArgsNEW
+    {
+        public ICruiser PlayerCruiser { get; private set; }
+        public ICruiser AICruiser { get; private set; }
+        public ITutorialProvider TutorialProvider { get; private set; }
+        public IPrefabFactory PrefabFactory { get; private set; }
+        
+        public TutorialArgsNEW(
+            ICruiser playerCruiser, 
+            ICruiser aiCruiser, 
+            ITutorialProvider tutorialProvider,
+            IPrefabFactory prefabFactory)
+        {
+            Helper.AssertIsNotNull(playerCruiser, aiCruiser, tutorialProvider, prefabFactory);
+
+            PlayerCruiser = playerCruiser;
+            AICruiser = aiCruiser;
+            TutorialProvider = tutorialProvider;
+            PrefabFactory = prefabFactory;
+        }
+    }
+}
