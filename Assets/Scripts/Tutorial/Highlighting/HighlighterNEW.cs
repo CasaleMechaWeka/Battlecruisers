4f7b8b91c935f4afc84ac28683da0322543cabd6
Felix Mann <felix.fei.mann@gmail.com>
2018-11-19 17:09:21 -0800

    Create HighlighterNEW :)

---
+++ Assets/Scripts/Tutorial/Highlighting/HighlighterNEW.cs
@@ -0,0 +1,34 @@
+﻿using BattleCruisers.Tutorial.Highlighting.Masked;
+using BattleCruisers.Utils;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Highlighting
+{
+    // FELIX  TEst :)
+    public class HighlighterNEW : IHighlighterNEW
+    {
+        private readonly IMaskHighlighter _maskHighlighter;
+        private readonly IHighlightArgsFactory _highlightArgsFactory;
+
+        public HighlighterNEW(IMaskHighlighter maskHighlighter, IHighlightArgsFactory highlightArgsFactory)
+        {
+            Helper.AssertIsNotNull(maskHighlighter, highlightArgsFactory);
+
+            _maskHighlighter = maskHighlighter;
+            _highlightArgsFactory = highlightArgsFactory;
+        }
+
+        public void Highlight(IMaskHighlightable toHighlight)
+        {
+            Assert.IsNotNull(toHighlight);
+
+            HighlightArgs highlightArgs = toHighlight.CreateHighlightArgs(_highlightArgsFactory);
+            _maskHighlighter.Highlight(highlightArgs);
+        }
+
+        public void Unhighlight()
+        {
+            _maskHighlighter.Unhighlight();
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorial/Highlighting/IHighlighterNEW.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Tutorial.Highlighting.Masked;
+
+namespace BattleCruisers.Tutorial.Highlighting
+{
+    public interface IHighlighterNEW
+    {
+        void Highlight(IMaskHighlightable toHighlight);
+        void Unhighlight();
+    }
+}
