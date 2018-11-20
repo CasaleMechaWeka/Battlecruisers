cf652e2f693080cf5ea5b8468bec3d0b62521a10
Felix Mann <felix.fei.mann@gmail.com>
2018-11-19 17:09:10 -0800

    Create IMaskHighlightable :)

---
+++ Assets/Scripts/Tutorial/Highlighting/Masked/IMaskHighlightable.cs
@@ -0,0 +1,8 @@
+﻿namespace BattleCruisers.Tutorial.Highlighting.Masked
+{
+    // FELIX  Implement :P  (InGame & OnCanvas :) )
+    public interface IMaskHighlightable
+    {
+        HighlightArgs CreateHighlightArgs(IHighlightArgsFactory highlightArgsFactory);
+    }
+}
