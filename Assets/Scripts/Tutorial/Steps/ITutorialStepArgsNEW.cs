5f926c502a90a27498cc001a3b8f10b47784b5ea
Felix Mann <felix.fei.mann@gmail.com>
2018-11-19 17:44:07 -0800

    Create TutorialStepArgsNEW :)

---
+++ Assets/Scripts/Tutorial/Steps/ITutorialStepArgsNEW.cs
@@ -0,0 +1,14 @@
+﻿using BattleCruisers.Tutorial.Highlighting;
+using BattleCruisers.Tutorial.Highlighting.Masked;
+using BattleCruisers.Tutorial.Providers;
+
+namespace BattleCruisers.Tutorial.Steps
+{
+    public interface ITutorialStepArgsNEW
+    {
+        IHighlighterNEW Highlighter { get; }
+        string TextToDisplay { get; }
+        ITextDisplayer Displayer { get; }
+		IItemProvider<IMaskHighlightable> HighlightableProvider { get; }
+    }
+}
---
+++ Assets/Scripts/Tutorial/Steps/TutorialStepArgsNEW.cs
@@ -0,0 +1,30 @@
+﻿using BattleCruisers.Tutorial.Highlighting;
+using BattleCruisers.Tutorial.Highlighting.Masked;
+using BattleCruisers.Tutorial.Providers;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Tutorial.Steps
+{
+    public class TutorialStepArgsNEW : ITutorialStepArgsNEW
+    {
+        public IHighlighterNEW Highlighter { get; private set; }
+        public string TextToDisplay { get; private set; }
+        public ITextDisplayer Displayer { get; private set; }
+		public IItemProvider<IMaskHighlightable> HighlightableProvider { get; private set; }
+
+        public TutorialStepArgsNEW(
+            IHighlighterNEW highlighter,
+            string textToDisplay,
+            ITextDisplayer displayer,
+            IItemProvider<IMaskHighlightable> highlightableProvider)
+        {
+            // textToDisplay may be null
+            Helper.AssertIsNotNull(highlighter, displayer, highlightableProvider);
+
+            Highlighter = highlighter;
+            TextToDisplay = textToDisplay;
+            Displayer = displayer;
+            HighlightableProvider = highlightableProvider;
+        }
+    }
+}
