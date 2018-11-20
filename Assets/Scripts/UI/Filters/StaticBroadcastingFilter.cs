867b5559f9edbdddd44f442c987fe63ce47397a0
Felix Mann <felix.fei.mann@gmail.com>
2018-11-20 07:04:03 -0800

    Create static, non-generic broadcasting filter

--- Assets/Scripts/UI/Filters/StaticBroadcastingFilter.cs
+++ Assets/Scripts/UI/Filters/StaticBroadcastingFilter.cs
@@ -2,6 +2,20 @@
 
 namespace BattleCruisers.UI.Filters
 {
+    public class StaticBroadcastingFilter : IBroadcastingFilter
+    {
+        public bool IsMatch { get; private set; }
+
+#pragma warning disable 67  // Unused event
+        public event EventHandler PotentialMatchChange;
+#pragma warning restore 67  // Unused event
+
+        public StaticBroadcastingFilter(bool isMatch)
+        {
+            IsMatch = isMatch;
+        }
+    }
+
     public class StaticBroadcastingFilter<TElement> : IBroadcastingFilter<TElement>
     {
         private readonly bool _isMatch;
