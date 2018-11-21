dbc26d5816369a62027f92954811fcdbfe25fe2c
Felix Mann <felix.fei.mann@gmail.com>
2018-11-21 06:21:15 -0800

    Create Togglable MonoBehaviour :)

---
+++ Assets/Scripts/UI/Togglable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.UI
+{
+    public class Togglable : MonoBehaviour, ITogglable
+    {
+        public bool Enabled { set { enabled = value; } }
+    }
+}
