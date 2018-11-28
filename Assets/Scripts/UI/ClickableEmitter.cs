9f4b09d6cc400ae3915041e4ff3c5fb9ba21dc41
Felix Mann <felix.fei.mann@gmail.com>
2018-11-27 18:36:30 -0800

    Create ClickableEmitter :)

---
+++ Assets/Scripts/UI/ClickableEmitter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace BattleCruisers.UI
+{
+    public class ClickableEmitter : MonoBehaviour, IClickableEmitter, IPointerClickHandler
+    {
+        public event EventHandler Clicked;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (Clicked != null)
+            {
+                Clicked.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
