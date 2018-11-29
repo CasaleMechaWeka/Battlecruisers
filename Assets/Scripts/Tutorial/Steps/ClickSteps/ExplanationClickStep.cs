e058e752f96a7aa6a05c88035854a05099a2c70c
Felix Mann <felix.fei.mann@gmail.com>
2018-11-29 06:38:54 -0800

    Create ExplanationClickStep :D

---
+++ Assets/Scripts/Tutorial/Steps/ClickSteps/ExplanationClickStep.cs
@@ -0,0 +1,35 @@
+﻿using BattleCruisers.Tutorial.Providers;
+using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.ClickSteps
+{
+    // FELIX  Test :)
+    public class ExplanationClickStep : TutorialStepNEW
+    {
+        private readonly IItemProvider<IClickableEmitter> _clickableProvider;
+        private IClickableEmitter _clickableEmitter;
+
+        public ExplanationClickStep(ITutorialStepArgsNEW args, IItemProvider<IClickableEmitter> clickableProvider)
+            : base(args)
+        {
+            Assert.IsNotNull(clickableProvider);
+            _clickableProvider = clickableProvider;
+        }
+
+        public override void Start(Action completionCallback)
+        {
+            base.Start(completionCallback);
+
+            _clickableEmitter = _clickableProvider.FindItem();
+            Assert.IsNotNull(_clickableEmitter);
+            _clickableEmitter.Clicked += _clickableEmitter_Clicked;
+        }
+
+        private void _clickableEmitter_Clicked(object sender, EventArgs e)
+        {
+            _clickableEmitter.Clicked -= _clickableEmitter_Clicked;
+            OnCompleted();
+        }
+    }
+}
