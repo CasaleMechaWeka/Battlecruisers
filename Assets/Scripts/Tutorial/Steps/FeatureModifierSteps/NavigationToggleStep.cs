7a84c6a3b4dae003aad83f60e112e723eb2a07c6
Felix Mann <felix.fei.mann@gmail.com>
2018-12-04 18:34:38 -0800

    Create NavigationToggleStep :D

---
+++ Assets/Scripts/Tutorial/Steps/FeatureModifierSteps/NavigationToggleStep.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.UI.Filters;
+using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.FeatureModifierSteps
+{
+    // FELIX  Test :D
+    public class NavigationToggleStep : TutorialStepNEW
+    {
+        private readonly BasicFilter _isNavigationEnabledFilter;
+
+        public NavigationToggleStep(ITutorialStepArgsNEW args, BasicFilter isNavigationEnabledFilter)
+            : base(args)
+        {
+            Assert.IsNotNull(isNavigationEnabledFilter);
+            _isNavigationEnabledFilter = isNavigationEnabledFilter;
+        }
+
+        public override void Start(Action completionCallback)
+        {
+            base.Start(completionCallback);
+            _isNavigationEnabledFilter.IsMatch = true;
+        }
+
+        protected override void OnCompleted()
+        {
+            base.OnCompleted();
+            _isNavigationEnabledFilter.IsMatch = false;
+        }
+    }
+}
