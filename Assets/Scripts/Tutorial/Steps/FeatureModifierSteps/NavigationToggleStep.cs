1dbe7eea06b665537f30f0dafa224cb925f7e2b0
Felix Mann <felix.fei.mann@gmail.com>
2018-12-04 18:37:21 -0800

    Inject whether navigation should be enabled or not :)

--- Assets/Scripts/Tutorial/Steps/FeatureModifierSteps/NavigationToggleStep.cs
+++ Assets/Scripts/Tutorial/Steps/FeatureModifierSteps/NavigationToggleStep.cs
@@ -8,24 +8,21 @@ namespace BattleCruisers.Tutorial.Steps.FeatureModifierSteps
     public class NavigationToggleStep : TutorialStepNEW
     {
         private readonly BasicFilter _isNavigationEnabledFilter;
+        private readonly bool _enableNavigation;
 
-        public NavigationToggleStep(ITutorialStepArgsNEW args, BasicFilter isNavigationEnabledFilter)
+        public NavigationToggleStep(ITutorialStepArgsNEW args, BasicFilter isNavigationEnabledFilter, bool enableNavigation)
             : base(args)
         {
             Assert.IsNotNull(isNavigationEnabledFilter);
+
             _isNavigationEnabledFilter = isNavigationEnabledFilter;
+            _enableNavigation = enableNavigation;
         }
 
         public override void Start(Action completionCallback)
         {
             base.Start(completionCallback);
-            _isNavigationEnabledFilter.IsMatch = true;
-        }
-
-        protected override void OnCompleted()
-        {
-            base.OnCompleted();
-            _isNavigationEnabledFilter.IsMatch = false;
+            _isNavigationEnabledFilter.IsMatch = _enableNavigation;
         }
     }
 }
