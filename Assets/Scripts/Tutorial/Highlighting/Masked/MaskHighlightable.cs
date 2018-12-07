d1659c1a07ce36e69174fc68341f0fc7da332af3
Felix Mann <felix.fei.mann@gmail.com>
2018-12-07 10:09:02 -0800

    Create MaskHighlightable :)

---
+++ Assets/Scripts/Tutorial/Highlighting/Masked/MaskHighlightable.cs
@@ -0,0 +1,22 @@
+﻿using BattleCruisers.Utils;
+using UnityEngine;
+
+namespace BattleCruisers.Tutorial.Highlighting.Masked
+{
+    public class MaskHighlightable : MonoBehaviour, IMaskHighlightable
+    {
+        private RectTransform _rectTransform;
+
+        public float sizeMultiplier;
+
+        public void Initialise()
+        {
+            _rectTransform = transform.Parse<RectTransform>();
+        }
+
+        public HighlightArgs CreateHighlightArgs(IHighlightArgsFactory highlightArgsFactory)
+        {
+            return highlightArgsFactory.CreateForOnCanvasObject(_rectTransform, sizeMultiplier);
+        }
+    }
+}
