f144224ffa30b84e2b04dc4a71a1f5c6d72d181a
Felix Mann <felix.fei.mann@gmail.com>
2018-12-08 15:51:23 -0800

    Add TODO :)

--- Assets/Scripts/Tutorial/Steps/Providers/ISingleBuildableProvider.cs
+++ Assets/Scripts/Tutorial/Steps/Providers/ISingleBuildableProvider.cs
@@ -4,6 +4,7 @@ using BattleCruisers.Tutorial.Providers;
 
 namespace BattleCruisers.Tutorial.Steps.Providers
 {
+    // FELIX  Remove :)
     public interface ISingleBuildableProvider :
         IItemProvider<IBuildable>,
         IListProvider<IHighlightable>,
