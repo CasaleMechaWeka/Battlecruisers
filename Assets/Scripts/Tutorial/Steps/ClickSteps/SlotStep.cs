8ec0c6b1eb6de32344278faae63a897ca6928bd0
Felix Mann <felix.fei.mann@gmail.com>
2018-12-08 15:27:03 -0800

    Create SlotStep :D

---
+++ Assets/Scripts/Tutorial/Steps/ClickSteps/SlotStep.cs
@@ -0,0 +1,32 @@
+﻿using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.Tutorial.Steps.Providers;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.ClickSteps
+{
+    /// <summary>
+    /// Must be used in tandem with BuildingButtonStep.
+    /// </summary>
+    /// FELIX  Copy tests :)
+    public class SlotStep : ExplanationClickStep
+    {
+        private readonly ISlotPermitter _highlightableSlotPermitter;
+
+        public SlotStep(
+            ITutorialStepArgsNEW args, 
+			ISlotPermitter highlightableSlotPermitter,
+            ISlotProvider slotProvider)
+            : base(args, slotProvider)
+        {
+            Assert.IsNotNull(highlightableSlotPermitter);
+            _highlightableSlotPermitter = highlightableSlotPermitter;
+        }
+
+		protected override void OnCompleted()
+		{
+            _highlightableSlotPermitter.PermittedSlots = null;
+
+			base.OnCompleted();
+		}
+	}
+}
