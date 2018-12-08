5694c422531c72cf62a2d147675bce6d1196fec0
Felix Mann <felix.fei.mann@gmail.com>
2018-12-08 15:20:48 -0800

    Create singular SlotProvider :)  (Compared to existing SlotsProvider)

---
+++ Assets/Scripts/Tutorial/Steps/Providers/ISlotProvider.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.Tutorial.Highlighting;
+using BattleCruisers.Tutorial.Providers;
+
+namespace BattleCruisers.Tutorial.Steps.Providers
+{
+    public interface ISlotProvider :
+        IItemProvider<ISlot>,
+        IItemProvider<IHighlightable>,
+        IItemProvider<IClickableEmitter>
+    {
+    }
+}
---
+++ Assets/Scripts/Tutorial/Steps/Providers/SlotProvider.cs
@@ -0,0 +1,53 @@
+﻿using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.Tutorial.Highlighting;
+using BattleCruisers.Tutorial.Providers;
+using BattleCruisers.Utils;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.Providers
+{
+    public class SlotProvider : ISlotProvider
+    {
+        private readonly ISlotAccessor _slotAccessor;
+        private readonly SlotSpecification _slotSpecification;
+
+        private ISlot _slot;
+        private ISlot Slot
+        {
+            get
+            {
+                if (_slot == null)
+                {
+                    _slot = _slotAccessor.GetFreeSlot(_slotSpecification);
+                    Assert.IsNotNull(_slot);
+                }
+
+                return _slot;
+            }
+        }
+
+        public SlotProvider(ISlotAccessor slotAccessor, SlotSpecification slotSpecification)
+        {
+            Helper.AssertIsNotNull(slotAccessor, slotSpecification);
+
+            _slotAccessor = slotAccessor;
+            _slotSpecification = slotSpecification;
+        }
+
+        ISlot IItemProvider<ISlot>.FindItem()
+        {
+            return _slot;
+        }
+
+        // TUTORIAL  Remove?
+        public IHighlightable FindItem()
+        {
+            return _slot;
+        }
+
+        IClickableEmitter IItemProvider<IClickableEmitter>.FindItem()
+        {
+            return _slot;
+        }
+    }
+}
