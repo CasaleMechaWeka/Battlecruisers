5bc252b4556a33f7d6e94df3805e65448b6b5b2a
Felix Mann <felix.fei.mann@gmail.com>
2018-12-08 16:11:29 -0800

    Create ChangeCruiserBuildSpeedNEW :)

---
+++ Assets/Scripts/Tutorial/Steps/ChangeCruiserBuildSpeedStepNEW.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BattleCruisers.Buildables.BuildProgress;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps
+{
+    // FELIX  Copy tests :)
+    public class ChangeCruiserBuildSpeedStepNEW : TutorialStepNEW
+    {
+        private readonly IBuildSpeedController _buildSpeedController;
+        private readonly BuildSpeed _buildSpeed;
+
+        public ChangeCruiserBuildSpeedStepNEW(ITutorialStepArgsNEW args, IBuildSpeedController buildSpeedController, BuildSpeed buildSpeed)
+            : base(args)
+        {
+            Assert.IsNotNull(buildSpeedController);
+
+            _buildSpeedController = buildSpeedController;
+            _buildSpeed = buildSpeed;
+        }
+
+        public override void Start(Action completionCallback)
+        {
+            base.Start(completionCallback);
+
+            _buildSpeedController.BuildSpeed = _buildSpeed;
+
+            OnCompleted();
+        }
+    }
+}
