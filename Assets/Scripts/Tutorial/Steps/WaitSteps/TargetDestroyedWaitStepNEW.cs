6f16e518723b035d939967aafd745cb238303307
Felix Mann <felix.fei.mann@gmail.com>
2018-12-09 09:24:11 -0800

    Create new TargetDestroyedWaitStep :)

---
+++ Assets/Scripts/Tutorial/Steps/WaitSteps/TargetDestroyedWaitStepNEW.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BattleCruisers.Buildables;
+using BattleCruisers.Tutorial.Providers;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.WaitSteps
+{
+    /// <summary>
+    /// Completed when the target is destroyed.
+    /// </summary>
+    /// FELIX  Copy tests :)
+    public class TargetDestroyedWaitStepNEW : TutorialStepNEW
+    {
+        private readonly IItemProvider<ITarget> _targetProvider;
+        private ITarget _target;
+
+        public TargetDestroyedWaitStepNEW(ITutorialStepArgsNEW args, IItemProvider<ITarget> targetProvider)
+            : base(args)
+        {
+            Assert.IsNotNull(targetProvider);
+            _targetProvider = targetProvider;
+        }
+
+        public override void Start(Action completionCallback)
+        {
+            base.Start(completionCallback);
+
+            _target = _targetProvider.FindItem();
+            Assert.IsNotNull(_target);
+            _target.Destroyed += _target_Destroyed;
+        }
+
+        private void _target_Destroyed(object sender, DestroyedEventArgs e)
+        {
+            _target.Destroyed -= _target_Destroyed;
+            base.OnCompleted();
+        }
+    }
+}
