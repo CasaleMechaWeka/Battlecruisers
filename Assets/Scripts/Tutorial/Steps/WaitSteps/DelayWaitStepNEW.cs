8ad93761acdcc69b638c44ca78ad5ccf25772d9d
Felix Mann <felix.fei.mann@gmail.com>
2018-12-09 09:18:38 -0800

    New version of DelayWaitStep :)

---
+++ Assets/Scripts/Tutorial/Steps/WaitSteps/DelayWaitStepNEW.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BattleCruisers.Utils.Threading;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Steps.WaitSteps
+{
+    /// <summary>
+    /// Completed when the specified amount of time has passed.
+    /// </summary>
+    /// FELIX  Copy tests :)
+    public class DelayWaitStepNEW : TutorialStepNEW
+    {
+        private readonly IVariableDelayDeferrer _defferer;
+        private readonly float _waitTimeInS;
+
+        private const int MIN_WAIT_TIME_IN_S = 0;
+
+        public DelayWaitStepNEW(ITutorialStepArgsNEW args, IVariableDelayDeferrer defferer, float waitTimeInS)
+            : base(args)
+        {
+            Assert.IsNotNull(defferer);
+            Assert.IsTrue(waitTimeInS >= MIN_WAIT_TIME_IN_S);
+
+            _defferer = defferer;
+            _waitTimeInS = waitTimeInS;
+        }
+
+        public override void Start(Action completionCallback)
+        {
+            base.Start(completionCallback);
+            _defferer.Defer(OnCompleted, _waitTimeInS);
+        }
+    }
+}
