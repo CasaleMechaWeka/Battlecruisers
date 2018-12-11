0c689064e3d61a02823bb6678be455250666632c
Felix Mann <felix.fei.mann@gmail.com>
2018-12-10 18:27:07 -0800

    Create TogglableUserChosenTargetHelper :D

---
+++ Assets/Scripts/Targets/TargetTrackers/UserChosen/TogglableUserChosenTargetHelper.cs
@@ -0,0 +1,28 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Targets.TargetTrackers.UserChosen
+{
+    // FELIX  Test :D
+    public class TogglableUserChosenTargetHelper : IUserChosenTargetHelper
+    {
+        private readonly IUserChosenTargetHelper _baseHelper;
+        private readonly IUserChosenTargetHelperPermissions _permissions;
+
+        public TogglableUserChosenTargetHelper(IUserChosenTargetHelper baseHelper, IUserChosenTargetHelperPermissions permissions)
+        {
+            Helper.AssertIsNotNull(baseHelper, permissions);
+
+            _baseHelper = baseHelper;
+            _permissions = permissions;
+        }
+
+        public void ToggleChosenTarget(ITarget target)
+        {
+            if (_permissions.IsEnabled)
+            {
+                _baseHelper.ToggleChosenTarget(target);
+            }
+        }
+    }
+}
