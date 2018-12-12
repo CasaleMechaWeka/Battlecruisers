48b44ce4a714deecf40c11581114a36330014aa0
Felix Mann <felix.fei.mann@gmail.com>
2018-12-11 16:23:56 -0800

    Update TutorialStepTests for new tutorial step :)

--- Assets/Editor/Tests/Tutorial/Steps/TutorialStepTests.cs
+++ Assets/Editor/Tests/Tutorial/Steps/TutorialStepTests.cs
@@ -1,92 +1,104 @@
-﻿// FELIX  Convert or delete :)
-//using BattleCruisers.Tutorial.Steps;
-//using NSubstitute;
-//using NUnit.Framework;
-//using UnityAsserts = UnityEngine.Assertions;
+﻿using BattleCruisers.Tutorial.Highlighting.Masked;
+using BattleCruisers.Tutorial.Steps;
+using NSubstitute;
+using NUnit.Framework;
+using UnityAsserts = UnityEngine.Assertions;
 
-//namespace BattleCruisers.Tests.Tutorial.Steps
-//{
-//    public class DummyTutorialStep : TutorialStep
-//    {
-//        public DummyTutorialStep(ITutorialStepArgs args) : base(args)
-//        {
-//        }
+namespace BattleCruisers.Tests.Tutorial.Steps
+{
+    public class DummyTutorialStep : TutorialStepNEW
+    {
+        public DummyTutorialStep(ITutorialStepArgsNEW args) : base(args)
+        {
+        }
 
-//        public void Complete()
-//        {
-//            base.OnCompleted();
-//        }
-//    }
+        public void Complete()
+        {
+            base.OnCompleted();
+        }
+    }
 
-//    public class TutorialStepTests : TutorialStepTestsBase
-//    {
-//        private DummyTutorialStep _tutorialStep;
+    public class TutorialStepTests : TutorialStepTestsBase
+    {
+        private DummyTutorialStep _tutorialStep;
 
-//        [SetUp]
-//        public override void SetuUp()
-//        {
-//            base.SetuUp();
-//            _tutorialStep = new DummyTutorialStep(_args);
-//        }
+        [SetUp]
+        public override void SetuUp()
+        {
+            base.SetuUp();
+            _tutorialStep = new DummyTutorialStep(_args);
+        }
 
-//        #region Start
-//        [Test]
-//        public void Start_NonNullText()
-//        {
-//            _tutorialStep.Start(_completionCallback);
+        #region Start
+        [Test]
+        public void Start_NonNullText()
+        {
+            _tutorialStep.Start(_completionCallback);
 
-//            _highlightablesProvider.Received().FindItems();
-//            _highlighter.Received().Highlight(_highlightables);
-//            _displayer.Received().DisplayText(_textToDisplay);
-//        }
+            _highlightableProvider.Received().FindItem();
+            _highlighter.Received().Highlight(_highlightable);
+            _displayer.Received().DisplayText(_textToDisplay);
+        }
 
-//        [Test]
-//        public void Start_NullText_DoesNotDisplayText()
-//        {
-//            _textToDisplay = null;
-//            ITutorialStepArgs args = new TutorialStepArgs(_highlighter, _textToDisplay, _displayer, _highlightablesProvider);
-//            _tutorialStep = new DummyTutorialStep(args);
+        [Test]
+        public void Start_NullText_DoesNotDisplayText()
+        {
+            _textToDisplay = null;
+            ITutorialStepArgsNEW args = new TutorialStepArgsNEW(_highlighter, _textToDisplay, _displayer, _highlightableProvider);
+            _tutorialStep = new DummyTutorialStep(args);
 
-//            _tutorialStep.Start(_completionCallback);
+            _tutorialStep.Start(_completionCallback);
 
-//            _highlightablesProvider.Received().FindItems();
-//            _highlighter.Received().Highlight(_highlightables);
-//            _displayer.DidNotReceiveWithAnyArgs().DisplayText(null);
-//        }
+            _highlightableProvider.Received().FindItem();
+            _highlighter.Received().Highlight(_highlightable);
+            _displayer.DidNotReceiveWithAnyArgs().DisplayText(null);
+        }
 
-//        [Test]
-//        public void DoubleStart_Throws()
-//        {
-//            _tutorialStep.Start(_completionCallback);
-//            Assert.Throws<UnityAsserts.AssertionException>(() => _tutorialStep.Start(_completionCallback));
-//        }
-//        #endregion Start
+        [Test]
+        public void Start_NullHighlightable_DoesNotHighlight()
+        {
+            _highlightableProvider.FindItem().Returns((IMaskHighlightable)null);
 
-//        #region OnCompleted
-//        [Test]
-//        public void OnCompleted_Unhighlights_CallsCompletionCallback()
-//        {
-//            _tutorialStep.Start(_completionCallback);
-//            _tutorialStep.Complete();
+            _tutorialStep.Start(_completionCallback);
 
-//            _highlighter.Received().UnhighlightAll();
-//            Assert.AreEqual(1, _callbackCounter);
-//        }
+            _highlightableProvider.Received().FindItem();
+            _highlighter.DidNotReceiveWithAnyArgs().Highlight(null);
+            _displayer.Received().DisplayText(_textToDisplay);
+        }
 
-//        [Test]
-//        public void OnCompleted_NoPreviousStart_Throws()
-//        {
-//            Assert.Throws<UnityAsserts.AssertionException>(_tutorialStep.Complete);
-//        }
+        [Test]
+        public void DoubleStart_Throws()
+        {
+            _tutorialStep.Start(_completionCallback);
+            Assert.Throws<UnityAsserts.AssertionException>(() => _tutorialStep.Start(_completionCallback));
+        }
+        #endregion Start
 
-//        [Test]
-//        public void DoubleOnCompleted_Throws()
-//        {
-//            _tutorialStep.Start(_completionCallback);
-//            _tutorialStep.Complete();
+        #region OnCompleted
+        [Test]
+        public void OnCompleted_Unhighlights_CallsCompletionCallback()
+        {
+            _tutorialStep.Start(_completionCallback);
+            _tutorialStep.Complete();
 
-//            Assert.Throws<UnityAsserts.AssertionException>(_tutorialStep.Complete);
-//        }
-//        #endregion OnCompleted
-//    }
-//}
+            _highlighter.Received().Unhighlight();
+            Assert.AreEqual(1, _callbackCounter);
+        }
+
+        [Test]
+        public void OnCompleted_NoPreviousStart_Throws()
+        {
+            Assert.Throws<UnityAsserts.AssertionException>(_tutorialStep.Complete);
+        }
+
+        [Test]
+        public void DoubleOnCompleted_Throws()
+        {
+            _tutorialStep.Start(_completionCallback);
+            _tutorialStep.Complete();
+
+            Assert.Throws<UnityAsserts.AssertionException>(_tutorialStep.Complete);
+        }
+        #endregion OnCompleted
+    }
+}
