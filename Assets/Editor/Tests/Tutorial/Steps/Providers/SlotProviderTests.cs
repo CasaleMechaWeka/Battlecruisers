561ad5ec2fb0c18fc39feff0272f0712d6aa67b1
Felix Mann <felix.fei.mann@gmail.com>
2018-12-11 17:44:14 -0800

    Complete SlotProviderTests :)

--- Assets/Editor/Tests/Tutorial/Steps/Providers/SlotProviderTests.cs
+++ Assets/Editor/Tests/Tutorial/Steps/Providers/SlotProviderTests.cs
@@ -1,104 +1,57 @@
-﻿//using BattleCruisers.Buildables.Buildings;
-//using BattleCruisers.Cruisers.Slots;
-//using BattleCruisers.Tutorial.Highlighting;
-//using BattleCruisers.Tutorial.Providers;
-//using BattleCruisers.Tutorial.Steps.Providers;
-//using NSubstitute;
-//using NUnit.Framework;
-//using System.Collections.Generic;
-//using System.Collections.ObjectModel;
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.Tutorial.Highlighting.Masked;
+using BattleCruisers.Tutorial.Providers;
+using BattleCruisers.Tutorial.Steps.Providers;
+using NSubstitute;
+using NUnit.Framework;
 
-//namespace BattleCruisers.Tests.Tutorial.Steps.Providers
-//{
-//    public class SlotProviderTests
-//    {
-//        private IItemProvider<ISlot> _slotsProvider;
-//        private IItemProvider<IHighlightable> _highlightablesProvider;
-//        private IItemProvider<IClickableEmitter> _clickablesProvider;
+namespace BattleCruisers.Tests.Tutorial.Steps.Providers
+{
+    public class SlotProviderTests
+    {
+        private IItemProvider<ISlot> _slotProvider;
+        private IItemProvider<IMaskHighlightable> _highlightableProvider;
+        private IItemProvider<IClickableEmitter> _clickableProvider;
 
-//        private ISlotAccessor _slotAccessor;
-//        private SlotSpecification _slotSpecificationPreferFront, _slotSpecificationPreferRear;
+        private ISlotAccessor _slotAccessor;
+        private SlotSpecification _slotSpecification;
 
-//        private ReadOnlyCollection<ISlot> _slots;
-//        private ISlot _slot1, _slot2;
+        private ISlot _slot;
 
-//        [SetUp]
-//        public void SetuUp()
-//        {
-//            _slotAccessor = Substitute.For<ISlotAccessor>();
-//            _slotSpecificationPreferFront = new SlotSpecification(SlotType.Platform, BuildingFunction.Generic, preferCruiserFront: true);
-//            _slotSpecificationPreferRear = new SlotSpecification(SlotType.Platform, BuildingFunction.Generic, preferCruiserFront: false);
+        [SetUp]
+        public void SetuUp()
+        {
+            _slotSpecification = new SlotSpecification(SlotType.Platform, BuildingFunction.Generic, preferCruiserFront: true);
+            _slotAccessor = Substitute.For<ISlotAccessor>();
 
-//            _slot1 = Substitute.For<ISlot>();
-//            _slot2 = Substitute.For<ISlot>();
+            SlotProvider provider = new SlotProvider(_slotAccessor, _slotSpecification);
 
-//            _slots = new ReadOnlyCollection<ISlot>(
-//                new List<ISlot>()
-//                {
-//                    _slot1,
-//                    _slot2
-//                });
-//        }
+            _slotProvider = provider;
+            _highlightableProvider = provider;
+            _clickableProvider = provider;
 
-//        private void CreateProvider(SlotSpecification specification)
-//        {
-//            SlotProvider provider = new SlotProvider(_slotAccessor, specification);
-            
-//            _slotsProvider = provider;
-//            _highlightablesProvider = provider;
-//            _clickablesProvider = provider;
-//        }
+            _slot = Substitute.For<ISlot>();
+            _slotAccessor.GetFreeSlot(_slotSpecification).Returns(_slot);
+        }
 
-//        [Test]
-//        public void PreferFrontmostSlot_ReturnsSingleSlot()
-//        {
-//            CreateProvider(_slotSpecificationPreferFront);
+        [Test]
+        public void ProvidersReturnSlot()
+        {
+            Assert.AreSame(_slot, _slotProvider.FindItem());
+            Assert.AreSame(_slot, _highlightableProvider.FindItem());
+            Assert.AreSame(_slot, _clickableProvider.FindItem());
+        }
 
-//            _slotAccessor.GetFreeSlot(_slotSpecificationPreferFront).Returns(_slot1);
+        [Test]
+        public void SlotsAreCached()
+        {
+            _slotProvider.FindItem();
+            _slotAccessor.Received().GetFreeSlot(_slotSpecification);
+            _slotAccessor.ClearReceivedCalls();
 
-//            Assert.AreEqual(1, _slotsProvider.FindItems().Count);
-//            Assert.IsTrue(_slotsProvider.FindItems().Contains(_slot1));
-
-//            Assert.AreEqual(1, _highlightablesProvider.FindItems().Count);
-//            Assert.IsTrue(_highlightablesProvider.FindItems().Contains(_slot1));
-
-//            Assert.AreEqual(1, _clickablesProvider.FindItems().Count);
-//            Assert.IsTrue(_clickablesProvider.FindItems().Contains(_slot1));
-//        }
-
-//        [Test]
-//        public void DoNotPreferFrontmostSlot_ReturnsMultipleSlots()
-//        {
-//            CreateProvider(_slotSpecificationPreferRear);
-
-//            _slotAccessor.GetFreeSlots(_slotSpecificationPreferRear.SlotType).Returns(_slots);
-
-//            Assert.AreEqual(2, _slotsProvider.FindItems().Count);
-//            Assert.IsTrue(_slotsProvider.FindItems().Contains(_slot1));
-//            Assert.IsTrue(_slotsProvider.FindItems().Contains(_slot2));
-
-//            Assert.AreEqual(2, _highlightablesProvider.FindItems().Count);
-//            Assert.IsTrue(_highlightablesProvider.FindItems().Contains(_slot1));
-//            Assert.IsTrue(_highlightablesProvider.FindItems().Contains(_slot2));
-
-//            Assert.AreEqual(2, _clickablesProvider.FindItems().Count);
-//            Assert.IsTrue(_clickablesProvider.FindItems().Contains(_slot1));
-//            Assert.IsTrue(_clickablesProvider.FindItems().Contains(_slot2));
-//        }
-
-//        [Test]
-//        public void SlotsAreCached()
-//        {
-//            CreateProvider(_slotSpecificationPreferFront);
-
-//            _slotAccessor.GetFreeSlot(_slotSpecificationPreferFront).Returns(_slot1);
-
-//            _slotsProvider.FindItems();
-//            _slotAccessor.Received().GetFreeSlot(_slotSpecificationPreferFront);
-//            _slotAccessor.ClearReceivedCalls();
-
-//            _slotsProvider.FindItems();
-//            _slotAccessor.DidNotReceive().GetFreeSlot(_slotSpecificationPreferFront);
-//        }
-//    }
-//}
+            _slotProvider.FindItem();
+            _slotAccessor.DidNotReceive().GetFreeSlot(_slotSpecification);
+        }
+    }
+}
