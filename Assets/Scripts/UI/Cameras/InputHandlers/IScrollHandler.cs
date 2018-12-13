99f0d9533fcf2a6bb4e623340f1661f9d6662de4
Felix Mann <felix.fei.mann@gmail.com>
2018-12-12 17:50:14 -0800

    More purge :P

--- Assets/Scripts/UI/Cameras/InputHandlers/IScrollHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace BattleCruisers.UI.Cameras.InputHandlers
-{
-	public interface IScrollHandler
-	{
-		Vector3 FindCameraPosition(float cameraOrthographicSize, Vector3 cameraPosition, Vector3 mousePosition);
-	}
-}
