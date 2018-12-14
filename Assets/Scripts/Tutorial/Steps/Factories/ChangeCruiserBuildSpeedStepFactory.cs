6c8d2b1b7d86b4dbc16d46a93f04b32643342d04
Felix Mann <felix.fei.mann@gmail.com>
2018-12-13 18:01:20 -0800

    Create ChangeCruiserBuildSpeedStepFactory :)

---
+++ Assets/Scripts/Tutorial/Steps/Factories/ChangeCruiserBuildSpeedStepFactory.cs
@@ -0,0 +1,22 @@
+﻿using BattleCruisers.Buildables.BuildProgress;
+
+namespace BattleCruisers.Tutorial.Steps.Factories
+{
+    public class ChangeCruiserBuildSpeedStepFactory : TutorialFactoryBase, IChangeCruiserBuildSpeedStepFactory
+    {
+        public ChangeCruiserBuildSpeedStepFactory(ITutorialStepArgsFactory argsFactory, ITutorialArgs tutorialArgs) 
+            : base(argsFactory, tutorialArgs)
+        {
+            // empty
+        }
+
+        public ITutorialStep CreateTutorialStep(IBuildSpeedController speedController, BuildSpeed buildSpeed)
+        {
+            return
+                new ChangeCruiserBuildSpeedStep(
+                    _argsFactory.CreateTutorialStepArgs(),
+                    speedController,
+                    buildSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorial/Steps/Factories/IChangeCruiserBuildSpeedStepFactory.cs
@@ -0,0 +1,9 @@
+﻿using BattleCruisers.Buildables.BuildProgress;
+
+namespace BattleCruisers.Tutorial.Steps.Factories
+{
+    public interface IChangeCruiserBuildSpeedStepFactory
+    {
+        ITutorialStep CreateTutorialStep(IBuildSpeedController speedController, BuildSpeed buildSpeed);
+    }
+}
