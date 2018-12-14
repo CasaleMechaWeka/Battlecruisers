d2084511827a569aade159ef3a9322e9e1fd64e6
Felix Mann <felix.fei.mann@gmail.com>
2018-12-13 18:06:13 -0800

    Create FeaturePermitterStepFactory :)

---
+++ Assets/Scripts/Tutorial/Steps/Factories/FeaturePermitterStepFactory.cs
@@ -0,0 +1,23 @@
+﻿using BattleCruisers.Tutorial.Steps.FeatureModifierSteps;
+using BattleCruisers.UI.Filters;
+
+namespace BattleCruisers.Tutorial.Steps.Factories
+{
+    public class FeaturePermitterStepFactory : TutorialFactoryBase, IFeaturePermitterStepFactory
+    {
+        protected FeaturePermitterStepFactory(ITutorialStepArgsFactory argsFactory, ITutorialArgs tutorialArgs) 
+            : base(argsFactory, tutorialArgs)
+        {
+            // empty
+        }
+
+        public ITutorialStep CreateTutorialStep(BroadcastingFilter featurePermitter, bool enableFeature)
+        {
+            return
+                new FeaturePermitterStep(
+                    _argsFactory.CreateTutorialStepArgs(),
+                    featurePermitter,
+                    enableFeature);
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorial/Steps/Factories/IFeaturePermitterStepFactory.cs
@@ -0,0 +1,9 @@
+﻿using BattleCruisers.UI.Filters;
+
+namespace BattleCruisers.Tutorial.Steps.Factories
+{
+    public interface IFeaturePermitterStepFactory
+    {
+        ITutorialStep CreateTutorialStep(BroadcastingFilter featurePermitter, bool enableFeature);
+    }
+}
