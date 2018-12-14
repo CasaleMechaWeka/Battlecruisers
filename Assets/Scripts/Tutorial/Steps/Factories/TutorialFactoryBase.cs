49dffae329fce2c0f90dbb1bec0258cb8966c55b
Felix Mann <felix.fei.mann@gmail.com>
2018-12-13 17:43:27 -0800

    Add TODO :)

--- Assets/Scripts/Tutorial/Steps/Factories/TutorialFactoryBase.cs
+++ Assets/Scripts/Tutorial/Steps/Factories/TutorialFactoryBase.cs
@@ -12,6 +12,7 @@ namespace BattleCruisers.Tutorial.Steps.Factories
         // FELIX  Make each factory only require what it needs, instead of every factory having access to everything??
         // FELIX  If not, create args class so every implementation doesn't need to list all 4 parameters :P
         protected readonly IVariableDelayDeferrer _deferrer;
+        // FELIX  Even break this down into only waht specific StepsFactory needs!
         protected readonly ITutorialArgs _tutorialArgs;
         protected readonly ISingleBuildableProvider _lastPlayerIncompleteBuildingStartedProvider;
 
