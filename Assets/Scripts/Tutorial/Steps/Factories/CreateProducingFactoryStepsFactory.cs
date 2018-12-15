af7b24aa898f05c43b8ceaf001132198fae61b1c
Felix Mann <felix.fei.mann@gmail.com>
2018-12-14 16:41:33 -0800

    Create CreateProducingFactoryStepsFactory :)

---
+++ Assets/Scripts/Tutorial/Steps/Factories/CreateProducingFactoryStepsFactory.cs
@@ -0,0 +1,83 @@
+﻿using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.BuildProgress;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Tutorial.Providers;
+using BattleCruisers.Tutorial.Steps.EnemyCruiser;
+using BattleCruisers.Tutorial.Steps.Providers;
+using BattleCruisers.Tutorial.Steps.WaitSteps;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Fetchers;
+using System.Collections.Generic;
+
+namespace BattleCruisers.Tutorial.Steps.Factories
+{
+    public class CreateProducingFactoryStepsFactory : TutorialFactoryBase, ICreateProducingFactoryStepsFactory
+    {
+        private readonly IChangeCruiserBuildSpeedStepFactory _changeCruiserBuildSpeedStepFactory;
+        private readonly ITutorialProvider _tutorialProvider;
+        private readonly IPrefabFactory _prefabFactory;
+        private readonly ICruiser _aiCruiser;
+
+        public CreateProducingFactoryStepsFactory(
+            ITutorialStepArgsFactory argsFactory,
+            ITutorialArgs tutorialArgs,
+            IChangeCruiserBuildSpeedStepFactory changeCruiserBuildSpeedStepFactory,
+            ITutorialProvider tutorialProvider,
+            IPrefabFactory prefabFactory,
+            ICruiser aiCruiser)
+            : base(argsFactory, tutorialArgs)
+        {
+            Helper.AssertIsNotNull(changeCruiserBuildSpeedStepFactory, tutorialProvider, prefabFactory, aiCruiser);
+
+            _changeCruiserBuildSpeedStepFactory = changeCruiserBuildSpeedStepFactory;
+            _tutorialProvider = tutorialProvider;
+            _prefabFactory = prefabFactory;
+            _aiCruiser = aiCruiser;
+        }
+
+        public FactoryStepsResult CreateSteps(IPrefabKey factoryKey, IPrefabKey unitKey)
+        {
+            IList<ITutorialStep> factorySteps = new List<ITutorialStep>();
+
+            // These steps should complete very quickly and require no user input.
+            // There is no need to display any text to the user or highlight any
+            // elements.
+            ITutorialStepArgs commonArgs = _argsFactory.CreateTutorialStepArgs();
+
+            // 1. Change build speed to super fast
+            factorySteps.Add(_changeCruiserBuildSpeedStepFactory.CreateTutorialStep(
+                    _tutorialProvider.AICruiserBuildSpeedController,
+                    BuildSpeed.VeryFast));
+
+            // 2. Start building factory
+            StartConstructingBuildingStep startConstructingFactoryStep
+                = new StartConstructingBuildingStep(
+                    commonArgs,
+                    factoryKey,
+                    _tutorialArgs.PrefabFactory,
+                    _tutorialArgs.AICruiser);
+            factorySteps.Add(startConstructingFactoryStep);
+
+            // 3. Wait for factory completion
+            factorySteps.Add(new BuildableCompletedWaitStep(commonArgs, startConstructingFactoryStep));
+
+            // 4. Change build speed to infinitely slow
+            factorySteps.Add(
+                _changeCruiserBuildSpeedStepFactory.CreateTutorialStep(
+                    _tutorialProvider.AICruiserBuildSpeedController,
+                    BuildSpeed.InfinitelySlow));
+
+            // 5. Start building unit
+            IItemProvider<IFactory> factoryProvider = new BuildableToFactoryProvider(startConstructingFactoryStep);
+            factorySteps.Add(
+                new StartConstructingUnitStep(
+                    commonArgs,
+                    unitKey,
+                    _tutorialArgs.PrefabFactory,
+                    factoryProvider));
+
+            return new FactoryStepsResult(factorySteps, factoryProvider);
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorial/Steps/Factories/ICreateProducingFactoryStepsFactory.cs
@@ -0,0 +1,9 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+
+namespace BattleCruisers.Tutorial.Steps.Factories
+{
+    public interface ICreateProducingFactoryStepsFactory
+    {
+        FactoryStepsResult CreateSteps(IPrefabKey factoryKey, IPrefabKey unitKey);
+    }
+}
