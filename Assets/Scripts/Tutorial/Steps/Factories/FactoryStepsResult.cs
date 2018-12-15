a39bee8ae3e27eb7cf2bdc3f4165dc464e658365
Felix Mann <felix.fei.mann@gmail.com>
2018-12-15 15:07:05 -0800

    Move FactoryStepsResult to Factories namespace :)

---
+++ Assets/Scripts/Tutorial/Steps/Factories/FactoryStepsResult.cs
@@ -0,0 +1,21 @@
+﻿using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Tutorial.Providers;
+using BattleCruisers.Utils;
+using System.Collections.Generic;
+
+namespace BattleCruisers.Tutorial.Steps.Factories
+{
+    public class FactoryStepsResult
+    {
+        public IList<ITutorialStep> Steps { get; private set; }
+        public IItemProvider<IFactory> FactoryProvider { get; private set; }
+
+        public FactoryStepsResult(IList<ITutorialStep> steps, IItemProvider<IFactory> factoryProvider)
+        {
+            Helper.AssertIsNotNull(steps, factoryProvider);
+
+            Steps = steps;
+            FactoryProvider = factoryProvider;
+        }
+    }
+}
