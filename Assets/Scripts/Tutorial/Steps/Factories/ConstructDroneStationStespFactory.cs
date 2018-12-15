12837be2595b6082de5582a9522cb700dc94d388
Felix Mann <felix.fei.mann@gmail.com>
2018-12-14 16:30:41 -0800

    Create ConstructDroneStationStespFactory :)

---
+++ Assets/Scripts/Tutorial/Steps/Factories/ConstructDroneStationStespFactory.cs
@@ -0,0 +1,45 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.Data.Static;
+using BattleCruisers.Utils;
+using System.Collections.Generic;
+
+namespace BattleCruisers.Tutorial.Steps.Factories
+{
+    public class ConstructDroneStationStespFactory : TutorialFactoryBase, ITutorialStepsFactory
+    {
+        private readonly IConstructBuildingStepsFactory _constructBuildingStepsFactory;
+        private readonly IExplanationDismissableStepFactory _explanationDismissableStepFactory;
+
+        public ConstructDroneStationStespFactory(
+            ITutorialStepArgsFactory argsFactory,
+            ITutorialArgs tutorialArgs,
+            IConstructBuildingStepsFactory constructBuildingStepsFactory,
+            IExplanationDismissableStepFactory explanationDismissableStepFactory)
+            : base(argsFactory, tutorialArgs)
+        {
+            Helper.AssertIsNotNull(constructBuildingStepsFactory, explanationDismissableStepFactory);
+
+            _constructBuildingStepsFactory = constructBuildingStepsFactory;
+            _explanationDismissableStepFactory = explanationDismissableStepFactory;
+        }
+
+        public IList<ITutorialStep> CreateTutorialSteps()
+        {
+            List<ITutorialStep> steps = new List<ITutorialStep>();
+
+            steps.AddRange(
+                _constructBuildingStepsFactory.CreateSteps(
+                    BuildingCategory.Factory,
+                    new BuildableInfo(StaticPrefabKeys.Buildings.DroneStation, "builder bay"),
+                    new SlotSpecification(SlotType.Utility, BuildingFunction.Generic, preferCruiserFront: true),
+                    "To get more builders construct a builder bay."));
+
+            steps.Add(
+                _explanationDismissableStepFactory.CreateTutorialStep(
+                    _argsFactory.CreateTutorialStepArgs("Nice!  You have gained 2 builders :D")));
+
+            return steps;
+        }
+    }
+}
