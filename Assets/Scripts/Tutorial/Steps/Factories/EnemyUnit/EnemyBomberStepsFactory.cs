5b1853871f03c8e28120fcf20068deecc4badc96
Felix Mann <felix.fei.mann@gmail.com>
2018-12-15 10:36:01 -0800

    Create EnemyBomberStepsFactory :)

--- Assets/Scripts/Tutorial/Steps/Factories/EnemyUnit/EnemyBomberStepsFactory.cs
+++ Assets/Scripts/Tutorial/Steps/Factories/EnemyUnit/EnemyBomberStepsFactory.cs
@@ -1,41 +1,78 @@
-﻿//using BattleCruisers.Cruisers.Slots;
-//using BattleCruisers.Data.Models.PrefabKeys;
-//using BattleCruisers.Tutorial.Steps.Providers;
+﻿using System.Collections.Generic;
+using BattleCruisers.Buildables.Boost;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Data.Static;
+using BattleCruisers.Tutorial.Steps.BoostSteps;
+using BattleCruisers.Tutorial.Steps.Providers;
+using BattleCruisers.Tutorial.Steps.WaitSteps;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Threading;
 
-//namespace BattleCruisers.Tutorial.Steps.Factories.EnemyUnit
-//{
-//    public class EnemyBomberStepsFactory : EnemyUnitDefenceStepsFactory
-//    {
+namespace BattleCruisers.Tutorial.Steps.Factories.EnemyUnit
+{
+    public class EnemyBomberStepsFactory : EnemyUnitDefenceStepsFactory
+    {
+        private readonly ICruiser _aiCruiser;
+        private readonly IVariableDelayDeferrer _deferrer;
 
-//        protected override IPrefabKey FactoryKey
-//        {
-//            get
-//            {
-//                throw new System.NotImplementedException();
-//            }
-//        }
+        protected override IPrefabKey FactoryKey { get { return StaticPrefabKeys.Buildings.AirFactory; } }
+        protected override CameraFocuserTarget UnitCameraFocusTarget { get { return CameraFocuserTarget.AICruiser; } }
 
-//        protected override BuildableInfo UnitToBuild => throw new System.NotImplementedException();
+        private readonly BuildableInfo _unitToBuild;
+        protected override BuildableInfo UnitToBuild { get { return _unitToBuild; } }
 
-//        protected override ISingleBuildableProvider UnitBuiltProvider => throw new System.NotImplementedException();
+        private readonly ISingleBuildableProvider _unitBuiltProvider;
+        protected override ISingleBuildableProvider UnitBuiltProvider { get { return _unitBuiltProvider; } }
 
-//        protected override BuildableInfo DefenceToBuild => throw new System.NotImplementedException();
+        private readonly BuildableInfo _defenceToBuild;
+        protected override BuildableInfo DefenceToBuild { get { return _defenceToBuild; } }
 
-//        protected override SlotSpecification SlotSpecification => throw new System.NotImplementedException();
+        private readonly SlotSpecification _slotSpecification;
+        protected override SlotSpecification SlotSpecification { get { return _slotSpecification; } }
 
-//        protected override CameraFocuserTarget UnitCameraFocusTarget => throw new System.NotImplementedException();
+        public EnemyBomberStepsFactory(
+            ITutorialStepArgsFactory argsFactory,
+            ITutorialArgs tutorialArgs,
+            EnemyUnitArgs enemyUnitArgs,
+            ICruiser aiCruiser,
+            IVariableDelayDeferrer deferrer,
+            ISingleBuildableProvider unitBuiltProvider)
+            : base(argsFactory, tutorialArgs, enemyUnitArgs)
+        {
+            Helper.AssertIsNotNull(aiCruiser, deferrer, unitBuiltProvider);
 
-//        public EnemyBomberStepsFactory(
-//            ITutorialStepArgsFactory argsFactory, 
-//            ITutorialArgs tutorialArgs, 
-//            ICreateProducingFactoryStepsFactory createProducingFactoryStepsFactory, 
-//            IAutoNavigationStepFactory autoNavigationStepFactory, 
-//            IExplanationDismissableStepFactory explanationDismissableStepFactory, 
-//            IConstructBuildingStepsFactory constructBuildingStepsFactory, 
-//            IChangeCruiserBuildSpeedStepFactory changeCruiserBuildSpeedStepFactory, 
-//            ITutorialProvider tutorialProvider) 
-//            : base(argsFactory, tutorialArgs, createProducingFactoryStepsFactory, autoNavigationStepFactory, explanationDismissableStepFactory, constructBuildingStepsFactory, changeCruiserBuildSpeedStepFactory, tutorialProvider)
-//        {
-//        }
-//    }
-//}
+            _aiCruiser = aiCruiser;
+            _deferrer = deferrer;
+            _unitBuiltProvider = unitBuiltProvider;
+            _unitToBuild = new BuildableInfo(StaticPrefabKeys.Units.Bomber, "bomber");
+            _defenceToBuild = new BuildableInfo(StaticPrefabKeys.Buildings.AntiAirTurret, "anti-air turret");
+            _slotSpecification = new SlotSpecification(SlotType.Deck, BuildingFunction.AntiAir, preferCruiserFront: true);
+        }
+
+        protected override IList<ITutorialStep> CreateSpeedBoostSteps(string unitUpcomingText)
+        {
+            IBoostProvider boostProvider = new BoostProvider(boostMultiplier: 8);
+
+            return new List<ITutorialStep>()
+            {
+                new AddAircraftBoostStep(
+                    _argsFactory.CreateTutorialStepArgs(unitUpcomingText),
+                    _aiCruiser.FactoryProvider.GlobalBoostProviders,
+                    boostProvider),
+
+                new DelayWaitStep(
+                    _argsFactory.CreateTutorialStepArgs(),
+                    _deferrer,
+                    waitTimeInS: 3.3f),
+
+                new RemoveAircraftBoostStep(
+                    _argsFactory.CreateTutorialStepArgs(),
+                    _aiCruiser.FactoryProvider.GlobalBoostProviders,
+                    boostProvider)
+            };
+        }
+    }
+}
