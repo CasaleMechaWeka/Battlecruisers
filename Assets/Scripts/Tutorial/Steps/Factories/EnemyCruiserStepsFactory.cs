e9dcdb15e0d0afb996931ce6cddec726561109d4
Felix Mann <felix.fei.mann@gmail.com>
2018-12-14 16:08:04 -0800

    Create EnemyCruiserStepsFactory :)

---
+++ Assets/Scripts/Tutorial/Steps/Factories/EnemyCruiserStepsFactory.cs
@@ -0,0 +1,40 @@
+﻿using BattleCruisers.Utils;
+using System.Collections.Generic;
+
+namespace BattleCruisers.Tutorial.Steps.Factories
+{
+    public class EnemyCruiserStepsFactory : TutorialFactoryBase, ITutorialStepsFactory
+    {
+        private readonly IAutoNavigationStepFactory _autNavigationStepFactory;
+        private readonly IExplanationDismissableStepFactory _explanationDismissableStepFactory;
+
+        public EnemyCruiserStepsFactory(
+            ITutorialStepArgsFactory argsFactory,
+            ITutorialArgs tutorialArgs,
+            IAutoNavigationStepFactory autoNavigationStepFactory,
+            IExplanationDismissableStepFactory explanationDismissableStepFactory) 
+            : base(argsFactory, tutorialArgs)
+        {
+            Helper.AssertIsNotNull(autoNavigationStepFactory, explanationDismissableStepFactory);
+
+            _autNavigationStepFactory = autoNavigationStepFactory;
+            _explanationDismissableStepFactory = explanationDismissableStepFactory;
+        }
+
+        public IList<ITutorialStep> CreateTutorialSteps()
+        {
+            List<ITutorialStep> steps = new List<ITutorialStep>();
+
+            steps.AddRange(_autNavigationStepFactory.CreateSteps(CameraFocuserTarget.AICruiser));
+
+            ITutorialStepArgs args
+                = _argsFactory.CreateTutorialStepArgs(
+                    "This is the enemy cruiser.  You win if you destroy their cruiser before it destroys you.",
+                    _tutorialArgs.AICruiser);
+
+            steps.Add(_explanationDismissableStepFactory.CreateTutorialStep(args));
+
+            return steps;
+        }
+    }
+}
