42d99fffe39f5a8962f0f79f0932c2eb83a2a66b
Felix Mann <felix.fei.mann@gmail.com>
2018-12-19 17:01:20 -0800

    Create Cheater, for W insta-win :D

---
+++ Assets/Scripts/Utils/Debugging/Cheater.cs
@@ -0,0 +1,32 @@
+﻿using BattleCruisers.Cruisers;
+using System.Linq;
+using UnityEngine;
+
+namespace BattleCruisers.Utils.Debugging
+{
+    // TEMP  Turn this class off for final game :P
+    public class Cheater : MonoBehaviour
+    {
+        void Start()
+        {
+            if (!Debug.isDebugBuild)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                Cruiser[] cruisers = FindObjectsOfType<Cruiser>();
+                Cruiser aiCruiser = cruisers.FirstOrDefault(cruiser => cruiser.Faction == Buildables.Faction.Reds);
+
+                if (aiCruiser != null)
+                {
+                    aiCruiser.TakeDamage(aiCruiser.MaxHealth, null);
+                }
+            }
+        }
+    }
+}
