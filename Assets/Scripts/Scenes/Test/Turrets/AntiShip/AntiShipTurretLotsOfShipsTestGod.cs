422cf91d1aa54c4059e1bc008cb2ce94426d8f1a
Felix Mann <felix.fei.mann@gmail.com>
2018-12-19 17:39:57 -0800

    Create test scene to highlight bug of anti-ship turrets sometimes not attacking attack boats :/

---
+++ Assets/Scripts/Scenes/Test/Turrets/AntiShip/AntiShipTurretLotsOfShipsTestGod.cs
@@ -0,0 +1,42 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Scenes.Test.Utilities;
+using System;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Turrets.AntiShip
+{
+    public class AntiShipTurretLotsOfShipsTestGod : MonoBehaviour 
+	{
+        public UnitWrapper unitPrefab;
+
+		void Start()
+		{
+			Helper helper = new Helper();
+
+            unitPrefab.Initialise();
+            unitPrefab.Buildable.StaticInitialise();
+
+            // Factory
+            Factory factory = FindObjectOfType<Factory>();
+            helper.InitialiseBuilding(factory, Faction.Blues, parentCruiserDirection: Direction.Right);
+            factory.CompletedBuildable += Factory_CompletedBuildable;
+            factory.StartConstruction();
+
+            // Turrets
+            TurretController[] turrets = FindObjectsOfType<TurretController>();
+            foreach (TurretController turret in turrets)
+            {
+                helper.InitialiseBuilding(turret, Faction.Reds);
+			    turret.StartConstruction();
+            }
+		}
+
+        private void Factory_CompletedBuildable(object sender, EventArgs e)
+        {
+            ((Factory)sender).StartBuildingUnit(unitPrefab);
+        }
+    }
+}
