76996bdb2b896db2012b5fee5ab71aafcbf745cf
Felix Mann <felix.fei.mann@gmail.com>
2018-12-20 07:00:59 -0800

    Crete TargetColliderHandler :)

---
+++ Assets/Scripts/Targets/TargetDetectors/ITargetDetectorEventEmitter.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Buildables;
+
+namespace BattleCruisers.Targets.TargetDetectors
+{
+    public interface ITargetDetectorEventEmitter
+    {
+        void InvokeTargetEnteredEvent(ITarget target);
+        void InvokeTargetExitedEvent(ITarget target);
+    }
+}
---
+++ Assets/Scripts/Targets/TargetDetectors/TargetColliderHandler.cs
@@ -0,0 +1,38 @@
+﻿using BattleCruisers.Buildables;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Targets.TargetDetectors
+{
+    // FELIX  Test :)
+    public class TargetColliderHandler : ITargetColliderHandler
+    {
+        private readonly ITargetDetectorEventEmitter _eventEmitter;
+
+        public TargetColliderHandler(ITargetDetectorEventEmitter eventEmitter)
+        {
+            Assert.IsNotNull(eventEmitter);
+            _eventEmitter = eventEmitter;
+        }
+
+        public void OnTargetColliderEntered(ITarget target)
+        {
+            target.Destroyed += Target_Destroyed;
+            _eventEmitter.InvokeTargetEnteredEvent(target);
+        }
+
+        public void OnTargetColliderExited(ITarget target)
+        {
+            if (!target.IsDestroyed)
+            {
+                target.Destroyed -= Target_Destroyed;
+                _eventEmitter.InvokeTargetExitedEvent(target);
+            }
+        }
+
+        private void Target_Destroyed(object sender, DestroyedEventArgs e)
+        {
+            e.DestroyedTarget.Destroyed -= Target_Destroyed;
+            _eventEmitter.InvokeTargetExitedEvent(e.DestroyedTarget);
+        }
+    }
+}
