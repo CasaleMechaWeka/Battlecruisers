d0e82575e1875bb6a65fde02f9c2d2cdf8419f02
Felix Mann <felix.fei.mann@gmail.com>
2018-12-23 12:18:28 -0800

    Create HelpButton :)

---
+++ Assets/Scripts/UI/BattlScene/Buttons/HelpButton.cs
@@ -0,0 +1,23 @@
+﻿using BattleCruisers.UI.Filters;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+
+namespace BattleCruisers.UI.BattleScene.Buttons
+{
+    public class HelpButton : MonoBehaviour, IPointerClickHandler
+    {
+        private BroadcastingFilter _helpLabelsVisibilityFilter;
+
+        public void Initialise(BroadcastingFilter helpLabelsVisibilityFilter)
+        {
+            Assert.IsNotNull(helpLabelsVisibilityFilter);
+            _helpLabelsVisibilityFilter = helpLabelsVisibilityFilter;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            _helpLabelsVisibilityFilter.IsMatch = !_helpLabelsVisibilityFilter.IsMatch;
+        }
+    }
+}
