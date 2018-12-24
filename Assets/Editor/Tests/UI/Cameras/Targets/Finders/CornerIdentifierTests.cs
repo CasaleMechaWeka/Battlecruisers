3cd18075995cf9fe044365693f9dd407b1e9453e
Felix Mann <felix.fei.mann@gmail.com>
2018-12-23 16:42:30 -0800

    Fix CornerIdentifierTests :)

--- Assets/Editor/Tests/UI/Cameras/Targets/Finders/CornerIdentifierTests.cs
+++ Assets/Editor/Tests/UI/Cameras/Targets/Finders/CornerIdentifierTests.cs
@@ -9,48 +9,57 @@ namespace BattleCruisers.Tests.UI.Cameras.Targets.Finders
     public class CornerIdentifierTests
     {
         private ICornerIdentifier _cornerIdentifier;
+        private ICornerCutoffProvider _cornerCutoffProvider;
         private ICameraTarget _cameraTarget;
 
-// FELIX  Fix :D
-        //[SetUp]
-        //public void TestSetup()
-        //{
-        //    _cornerIdentifier = new CornerIdentifier();
-        //    _cameraTarget = Substitute.For<ICameraTarget>();
-        //}
+        [SetUp]
+        public void TestSetup()
+        {
+            _cornerCutoffProvider = Substitute.For<ICornerCutoffProvider>();
+            _cornerIdentifier = new CornerIdentifier(_cornerCutoffProvider);
 
-        //[Test]
-        //public void FindCorner_PlayerCruiserCorner()
-        //{
-        //    _cameraTarget.Position.Returns(new Vector3(CornerIdentifier.PLAYER_CRUISER_CORNER_X_POSITION_CUTOFF, 0, 0));
-        //    CameraCorner? cameraCorner = _cornerIdentifier.FindCorner(_cameraTarget);
-        //    Assert.AreEqual(CameraCorner.PlayerCruiser, cameraCorner);
-        //}
+            _cameraTarget = Substitute.For<ICameraTarget>();
 
-        //[Test]
-        //public void FindCorner_AICruiserCorner()
-        //{
-        //    _cameraTarget.Position.Returns(new Vector3(CornerIdentifier.AI_CRUISER_CORNER_X_POSITION_CUTOFF, 0, 0));
-        //    CameraCorner? cameraCorner = _cornerIdentifier.FindCorner(_cameraTarget);
-        //    Assert.AreEqual(CameraCorner.AICruiser, cameraCorner);
-        //}
+            _cornerCutoffProvider.AICruiserCornerXPositionCutoff.Returns(12);
+            _cornerCutoffProvider.PlayerCruiserCornerXPositionCutoff.Returns(-12);
+            _cornerCutoffProvider.OverviewOrthographicSizeCutoff.Returns(77);
+        }
 
-        //[Test]
-        //public void FindCorner_OverviewCorner()
-        //{
-        //    _cameraTarget.Position.Returns(new Vector3(0, 0, 0));
-        //    _cameraTarget.OrthographicSize.Returns(CornerIdentifier.OVERVIEW_ORTHOGRAPHIC_SIZE_CUTOFF);
-        //    CameraCorner? cameraCorner = _cornerIdentifier.FindCorner(_cameraTarget);
-        //    Assert.AreEqual(CameraCorner.Overview, cameraCorner);
-        //}
+        [Test]
+        public void FindCorner_PlayerCruiserCorner()
+        {
+            float xCutoff = _cornerCutoffProvider.PlayerCruiserCornerXPositionCutoff;
+            _cameraTarget.Position.Returns(new Vector3(xCutoff, 0, 0));
+            CameraCorner? cameraCorner = _cornerIdentifier.FindCorner(_cameraTarget);
+            Assert.AreEqual(CameraCorner.PlayerCruiser, cameraCorner);
+        }
 
-        //[Test]
-        //public void FindCorner_NoCorner()
-        //{
-        //    _cameraTarget.Position.Returns(new Vector3(0, 0, 0));
-        //    _cameraTarget.OrthographicSize.Returns(CornerIdentifier.OVERVIEW_ORTHOGRAPHIC_SIZE_CUTOFF - 1);
-        //    CameraCorner? cameraCorner = _cornerIdentifier.FindCorner(_cameraTarget);
-        //    Assert.IsNull(cameraCorner);
-        //}
+        [Test]
+        public void FindCorner_AICruiserCorner()
+        {
+            float xCutoff = _cornerCutoffProvider.AICruiserCornerXPositionCutoff;
+            _cameraTarget.Position.Returns(new Vector3(xCutoff, 0, 0));
+            CameraCorner? cameraCorner = _cornerIdentifier.FindCorner(_cameraTarget);
+            Assert.AreEqual(CameraCorner.AICruiser, cameraCorner);
+        }
+
+        [Test]
+        public void FindCorner_OverviewCorner()
+        {
+            _cameraTarget.Position.Returns(new Vector3(0, 0, 0));
+            float cutoff = _cornerCutoffProvider.OverviewOrthographicSizeCutoff;
+            _cameraTarget.OrthographicSize.Returns(cutoff);
+            CameraCorner? cameraCorner = _cornerIdentifier.FindCorner(_cameraTarget);
+            Assert.AreEqual(CameraCorner.Overview, cameraCorner);
+        }
+
+        [Test]
+        public void FindCorner_NoCorner()
+        {
+            _cameraTarget.Position.Returns(new Vector3(0, 0, 0));
+            _cameraTarget.OrthographicSize.Returns(_cornerCutoffProvider.OverviewOrthographicSizeCutoff - 1);
+            CameraCorner? cameraCorner = _cornerIdentifier.FindCorner(_cameraTarget);
+            Assert.IsNull(cameraCorner);
+        }
     }
 }
