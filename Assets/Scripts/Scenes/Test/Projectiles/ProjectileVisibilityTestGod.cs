abb0cd39ec382e6a6cbf5652b43aab57ae154d25
Felix Mann <felix.fei.mann@gmail.com>
2018-12-25 11:47:53 -0800

    Add navigation wheel to test scene :)

--- Assets/Scripts/Scenes/Test/Projectiles/ProjectileVisibilityTestGod.cs
+++ Assets/Scripts/Scenes/Test/Projectiles/ProjectileVisibilityTestGod.cs
@@ -1,14 +1,24 @@
 ﻿using BattleCruisers.Buildables;
 using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Data.Settings;
 using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.UI.BattleScene.Navigation;
+using BattleCruisers.UI.Cameras.Adjusters;
+using BattleCruisers.UI.Cameras.Helpers;
+using BattleCruisers.UI.Cameras.Targets.Finders;
+using BattleCruisers.UI.Cameras.Targets.Providers;
+using BattleCruisers.UI.Filters;
+using BattleCruisers.Utils.PlatformAbstractions;
+using NSubstitute;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Projectiles
 {
+    // FELIX  Allow changing of skybox :)
     public class ProjectileVisibilityTestGod : MonoBehaviour
     {
-        // FELIX  Allow changing of skybox :)
+        private ICameraAdjuster _cameraAdjuster;
 
         void Start()
         {
@@ -21,11 +31,42 @@ namespace BattleCruisers.Scenes.Test.Projectiles
             AirFactory airFactory = FindObjectOfType<AirFactory>();
             helper.InitialiseBuilding(airFactory, Faction.Reds);
             airFactory.StartConstruction();
+
+            _cameraAdjuster = CreateCameraAdjuster();
+        }
+
+        // FELIX  Avoid duplicate code with NavigationTest scene, if it still exists :P
+        private ICameraAdjuster CreateCameraAdjuster()
+        {
+            NavigationWheelInitialiser navigationWheelInitialiser = FindObjectOfType<NavigationWheelInitialiser>();
+            IBroadcastingFilter navigationWheelEnabledFilter = new StaticBroadcastingFilter(isMatch: true);
+            INavigationWheelPanel navigationWheelPanel = navigationWheelInitialiser.InitialiseNavigationWheel(navigationWheelEnabledFilter);
+
+            Camera platformCamera = FindObjectOfType<Camera>();
+            ICamera camera = new CameraBC(platformCamera);
+            ICameraCalculatorSettings settings
+                = new CameraCalculatorSettings(
+                    Substitute.For<ISettingsManager>(),
+                    camera.Aspect);
+            ICameraCalculator cameraCalculator = new CameraCalculator(camera, settings);
+
+            ICameraNavigationWheelCalculator cameraNavigationWheelCalculator = new CameraNavigationWheelCalculator(navigationWheelPanel, cameraCalculator, settings.ValidOrthographicSizes);
+            ICameraTargetFinder cameraTargetFinder = new NavigationWheelCameraTargetFinder(cameraNavigationWheelCalculator, camera);
+            ICameraTargetProvider cameraTargetProvider = new NavigationWheelCameraTargetProvider(navigationWheelPanel.NavigationWheel, cameraTargetFinder);
+
+            // Smooth adjuster
+            float smoothTime = 0.15f;
+
+            return
+                new SmoothCameraAdjuster(
+                    cameraTargetProvider,
+                    new SmoothZoomAdjuster(camera, smoothTime),
+                    new SmoothPositionAdjuster(camera.Transform, smoothTime));
         }
 
         void Update()
         {
-
+            _cameraAdjuster.AdjustCamera();
         }
     }
 }
