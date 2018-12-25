fa94b9ff8f18f8a5225d479b1a55c5e25864068e
Felix Mann <felix.fei.mann@gmail.com>
2018-12-25 11:36:56 -0800

    Create initial ProjectileVisibilityTest scene :D

---
+++ Assets/Scripts/Scenes/Test/Projectiles/ProjectileVisibilityTestGod.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Projectiles
+{
+    public class ProjectileVisibilityTestGod : MonoBehaviour
+    {
+        // FELIX  Allow changing of skybox :)
+
+        void Start()
+        {
+            Helper helper = new Helper();
+
+            Artillery artillery = FindObjectOfType<Artillery>();
+            helper.InitialiseBuilding(artillery, Faction.Blues);
+            artillery.StartConstruction();
+
+            AirFactory airFactory = FindObjectOfType<AirFactory>();
+            helper.InitialiseBuilding(airFactory, Faction.Reds);
+            airFactory.StartConstruction();
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
