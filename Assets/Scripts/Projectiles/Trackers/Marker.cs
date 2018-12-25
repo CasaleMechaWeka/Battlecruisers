8b6c0ca267101735dd7b36beacf42a9ed562aaa0
Felix Mann <felix.fei.mann@gmail.com>
2018-12-25 13:51:18 -0800

    Create Marker :)

---
+++ Assets/Scripts/Projectiles/Trackers/Marker.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Projectiles.Trackers
+{
+    // FELIX  Show direction as well :D
+    public class Marker : MonoBehaviour, IMarker
+    {
+        public bool IsVisible { set { gameObject.SetActive(value); } }
+        public Vector2 OnScreenPostion { set { transform.position = value; } }
+    }
+}
