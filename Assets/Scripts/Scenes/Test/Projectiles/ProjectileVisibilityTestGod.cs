cea8c8c3bea1be9ee71a33eacb1a9a27bd31d710
Felix Mann <felix.fei.mann@gmail.com>
2018-12-25 12:04:07 -0800

    Allow chaning sky :)

--- Assets/Scripts/Scenes/Test/Projectiles/ProjectileVisibilityTestGod.cs
+++ Assets/Scripts/Scenes/Test/Projectiles/ProjectileVisibilityTestGod.cs
@@ -2,6 +2,7 @@
 using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Buildables.Buildings.Turrets;
 using BattleCruisers.Data.Settings;
+using BattleCruisers.Data.Static;
 using BattleCruisers.Scenes.Test.Utilities;
 using BattleCruisers.UI.BattleScene.Navigation;
 using BattleCruisers.UI.Cameras.Adjusters;
@@ -9,16 +10,21 @@ using BattleCruisers.UI.Cameras.Helpers;
 using BattleCruisers.UI.Cameras.Targets.Finders;
 using BattleCruisers.UI.Cameras.Targets.Providers;
 using BattleCruisers.UI.Filters;
+using BattleCruisers.Utils.DataStrctures;
+using BattleCruisers.Utils.Fetchers;
 using BattleCruisers.Utils.PlatformAbstractions;
 using NSubstitute;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace BattleCruisers.Scenes.Test.Projectiles
 {
-    // FELIX  Allow changing of skybox :)
     public class ProjectileVisibilityTestGod : MonoBehaviour
     {
         private ICameraAdjuster _cameraAdjuster;
+        private Skybox _skybox;
+        private ICircularList<Material> _skies;
 
         void Start()
         {
@@ -33,6 +39,12 @@ namespace BattleCruisers.Scenes.Test.Projectiles
             airFactory.StartConstruction();
 
             _cameraAdjuster = CreateCameraAdjuster();
+
+            _skies = FindSkyMaterials();
+
+            _skybox = FindObjectOfType<Skybox>();
+            Assert.IsNotNull(_skybox);
+            ChangeSky();
         }
 
         // FELIX  Avoid duplicate code with NavigationTest scene, if it still exists :P
@@ -62,6 +74,35 @@ namespace BattleCruisers.Scenes.Test.Projectiles
                     cameraTargetProvider,
                     new SmoothZoomAdjuster(camera, smoothTime),
                     new SmoothPositionAdjuster(camera.Transform, smoothTime));
+        }
+
+        private ICircularList<Material> FindSkyMaterials()
+        {
+            IList<string> skyNames = new List<string>()
+            {
+                SkyMaterials.Blue,
+                SkyMaterials.BlueCloudy,
+                SkyMaterials.BlueDeep,
+                SkyMaterials.Sunset,
+                SkyMaterials.SunsetCloudy,
+                SkyMaterials.SunsetWeirdClouds,
+                SkyMaterials.White
+            };
+
+            IList<Material> skyMaterials = new List<Material>();
+            IMaterialFetcher materialFetcher = new MaterialFetcher();
+
+            foreach (string skyName in skyNames)
+            {
+                skyMaterials.Add(materialFetcher.GetMaterial(skyName));
+            }
+
+            return new CircularList<Material>(skyMaterials);
+        }
+
+        public void ChangeSky()
+        {
+            _skybox.material = _skies.Next();
         }
 
         void Update()
