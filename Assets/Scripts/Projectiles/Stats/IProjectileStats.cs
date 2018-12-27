0e310ee0087f20eccbef4d2883813d2a5dcbf4fa
Felix Mann <felix.fei.mann@gmail.com>
2018-12-27 13:24:20 -0800

    Add TODO :)

--- Assets/Scripts/Projectiles/Stats/IProjectileStats.cs
+++ Assets/Scripts/Projectiles/Stats/IProjectileStats.cs
@@ -2,8 +2,10 @@
 {
     public interface IProjectileStats : IDamageStats
     {
+        // FELIX  Move these 2 to IProjectileFlightStats => Needed by angle calculators :)
         float MaxVelocityInMPerS { get; }
         bool IgnoreGravity { get; }
+
         bool HasAreaOfEffectDamage { get; }
         float InitialVelocityInMPerS { get; }
     }
