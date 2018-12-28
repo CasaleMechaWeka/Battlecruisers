223b876d2d42f0bc1c6068d0296c24dff144249f
Felix Mann <felix.fei.mann@gmail.com>
2018-12-28 11:49:48 -0800

    Add poetry :)

--- Assets/Scripts/Utils/Categorisation/AntiShipDamageToStarsConverter.cs
+++ Assets/Scripts/Utils/Categorisation/AntiShipDamageToStarsConverter.cs
@@ -5,8 +5,12 @@
         private static readonly float[] CATEGORY_THRESHOLDS =
         {
             1,
+            // 20, Anti-ship turret
             24,     // Mortar
+            // 25, Attack boat
+            // 26, Gunship
             50,     // Railgun
+            // 73, Frigate
             87,     // Destroyer
             119     // Archon
         };
