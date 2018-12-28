4292b88a3c73fdff34653ed6e493e0581b8d7478
Felix Mann <felix.fei.mann@gmail.com>
2018-12-28 11:54:45 -0800

    Add more comments :)

--- Assets/Scripts/Utils/Categorisation/AntiAirDamageToStarsConverter.cs
+++ Assets/Scripts/Utils/Categorisation/AntiAirDamageToStarsConverter.cs
@@ -5,9 +5,12 @@
         private static readonly float[] CATEGORY_THRESHOLDS =
         {
             1,
+            // 9, SAM site
+            // 13, Anti-air turret
             15,     // Fighter
-            25,     // Destroyer
-            32,     // Archon
+            // 16, Frigate
+            23,     // Destroyer
+            27,     // Archon
             50      // No buildables achieve this :P
         };
 
