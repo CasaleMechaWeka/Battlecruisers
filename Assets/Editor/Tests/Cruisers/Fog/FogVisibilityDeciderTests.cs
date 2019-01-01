fc76b7ef37664b6088ac4b8420126b96334dd08c
Felix Mann <felix.fei.mann@gmail.com>
2019-01-01 11:11:53 -0800

    Create FogVisibilityDeciderTests :)

---
+++ Assets/Editor/Tests/Cruisers/Fog/FogVisibilityDeciderTests.cs
@@ -0,0 +1,40 @@
+﻿using BattleCruisers.Cruisers.Fog;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.Cruisers.Fog
+{
+    public class FogVisibilityDeciderTests
+    {
+        private IFogVisibilityDecider _decider;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _decider = new FogVisibilityDecider();
+        }
+
+        [Test]
+        public void ShouldFogBeVisible_0StealthGenerators_0SpySatellites_ReturnsFalse()
+        {
+            Assert.IsFalse(_decider.ShouldFogBeVisible(numOfFriendlyStealthGenerators: 0, numOfEnemySpySatellites: 0));
+        }
+
+        [Test]
+        public void ShouldFogBeVisible_1StealthGenerators_0SpySatellites_ReturnsTrue()
+        {
+            Assert.IsTrue(_decider.ShouldFogBeVisible(numOfFriendlyStealthGenerators: 1, numOfEnemySpySatellites: 0));
+        }
+
+        [Test]
+        public void ShouldFogBeVisible_0StealthGenerators_1SpySatellites_ReturnsFalse()
+        {
+            Assert.IsFalse(_decider.ShouldFogBeVisible(numOfFriendlyStealthGenerators: 0, numOfEnemySpySatellites: 1));
+        }
+
+        [Test]
+        public void ShouldFogBeVisible_1StealthGenerators_1SpySatellites_ReturnsFalse()
+        {
+            Assert.IsFalse(_decider.ShouldFogBeVisible(numOfFriendlyStealthGenerators: 1, numOfEnemySpySatellites: 1));
+        }
+    }
+}
--- Assets/Scripts/Cruisers/Fog/FogVisibilityDecider.cs
+++ Assets/Scripts/Cruisers/Fog/FogVisibilityDecider.cs
@@ -1,6 +1,5 @@
 ﻿namespace BattleCruisers.Cruisers.Fog
 {
-    // FELIX  Test :)
     public class FogVisibilityDecider : IFogVisibilityDecider
     {
         public bool ShouldFogBeVisible(int numOfFriendlyStealthGenerators, int numOfEnemySpySatellites)
