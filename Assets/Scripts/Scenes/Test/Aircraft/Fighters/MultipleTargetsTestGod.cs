0d004080e1455733fde0d9b9a81febe33d49d4e1
Felix Mann <felix.fei.mann@gmail.com>
2019-01-01 18:02:50 -0800

    Create test scene highlighting bug where fighter never fires on a target after destroying the first :)

---
+++ Assets/Scripts/Scenes/Test/Aircraft/Fighters/MultipleTargetsTestGod.cs
@@ -0,0 +1,37 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units.Aircraft;
+using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Aircraft.Fighters
+{
+    public class MultipleTargetsTestGod : MonoBehaviour 
+	{
+		void Start() 
+		{
+            Helper helper = new Helper();
+
+            IList<Vector2> patrolPoints = new List<Vector2>()
+            {
+                new Vector2(-12, 5),
+                new Vector2(12, 5)
+            };
+
+            // Fighter
+            FighterController fighter = FindObjectOfType<FighterController>();
+            helper.InitialiseUnit(fighter, Faction.Blues);
+            fighter.StartConstruction();
+
+            // Targets
+            TestAircraftController[] targets = FindObjectsOfType<TestAircraftController>();
+
+            foreach (TestAircraftController target in targets)
+            {
+                target.PatrolPoints = patrolPoints;
+                helper.InitialiseUnit(target, Faction.Reds);
+                target.StartConstruction();
+            }
+		}
+	}
+}
