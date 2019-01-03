a80e27b3fb44c15ceb2aac92a05ad18bb9ec322f
Felix Mann <felix.fei.mann@gmail.com>
2019-01-02 16:50:53 -0800

    Create TargetFinderFactory :)

---
+++ Assets/Scripts/Targets/Factories/ITargetFinderFactory.cs
@@ -0,0 +1,14 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Targets.TargetDetectors;
+using BattleCruisers.Targets.TargetFinders;
+using BattleCruisers.Targets.TargetFinders.Filters;
+
+namespace BattleCruisers.Targets.Factories
+{
+    public interface ITargetFinderFactory
+    {
+        ITargetFinder CreateRangedTargetFinder(ITargetDetector targetDetector, ITargetFilter targetFilter);
+        ITargetFinder CreateMinRangeTargetFinder(ITargetDetector maxRangeTargetDetector, ITargetDetector minRangeTargetDetector, ITargetFilter targetFilter);
+        ITargetFinder CreateAttackingTargetFinder(IDamagable parentDamagable, ITargetFilter targetFilter);
+    }
+}
