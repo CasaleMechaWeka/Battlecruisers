8f8794e3bf68e1e7bb9589eea9ca34286a455c82
Felix Mann <felix.fei.mann@gmail.com>
2019-01-02 16:56:59 -0800

    Create TargetRankerFactory :)

---
+++ Assets/Scripts/Targets/Factories/TargetRankerFactory.cs
@@ -0,0 +1,23 @@
+﻿using BattleCruisers.Targets.TargetTrackers.Ranking;
+
+namespace BattleCruisers.Targets.Factories
+{
+    public class TargetRankerFactory : ITargetRankerFactory
+    {
+        public TargetRankerFactory()
+		{
+            EqualTargetRanker = new EqualTargetRanker();
+            ShipTargetRanker = new ShipTargetRanker();
+            OffensiveBuildableTargetRanker = new OffensiveBuildableTargetRanker();
+		}
+
+        public ITargetRanker EqualTargetRanker { get; private set; }
+        public ITargetRanker ShipTargetRanker { get; private set; }
+        public ITargetRanker OffensiveBuildableTargetRanker { get; private set; }
+
+        public ITargetRanker CreateBoostedRanker(ITargetRanker baseRanker, int rankBoost)
+        {
+            return new BoostedRanker(baseRanker, rankBoost);
+        }
+    }
+}
