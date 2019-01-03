b1bbe00e1481931b069ca6d0aea5defa0585d900
Felix Mann <felix.fei.mann@gmail.com>
2019-01-02 16:55:26 -0800

    Create TargetFilterFactory :)

---
+++ Assets/Scripts/Targets/Factories/ITargetFilterFactory.cs
@@ -0,0 +1,18 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using System.Collections.Generic;
+
+namespace BattleCruisers.Targets.Factories
+{
+    public interface ITargetFilterFactory
+    {
+        ITargetFilter CreateTargetFilter(Faction faction);
+        ITargetFilter CreateTargetFilter(Faction faction, IList<TargetType> targetTypes);
+        ITargetFilter CreateDummyTargetFilter(bool isMatchResult);
+        ITargetFilter CreateTargetInFrontFilter(IUnit source);
+        IExactMatchTargetFilter CreateExactMatchTargetFilter();
+        IExactMatchTargetFilter CreateExactMatchTargetFilter(ITarget targetToMatch);
+        IExactMatchTargetFilter CreateMulitpleExactMatchTargetFilter();
+    }
+}
