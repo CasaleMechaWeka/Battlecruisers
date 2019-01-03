41d5b8352f17c969459e2b0ec8d7eafe1c8c3da7
Felix Mann <felix.fei.mann@gmail.com>
2019-01-02 17:58:07 -0800

    Remove ITargetsFactory use :)

--- Assets/Scripts/Scenes/Test/BuildProgressTestsGod.cs
+++ Assets/Scripts/Scenes/Test/BuildProgressTestsGod.cs
@@ -1,8 +1,6 @@
 ﻿using BattleCruisers.Buildables.Buildings;
 using BattleCruisers.Cruisers;
 using BattleCruisers.Scenes.Test.Utilities;
-using BattleCruisers.Targets.Factories;
-using NSubstitute;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test
@@ -15,13 +13,12 @@ namespace BattleCruisers.Scenes.Test
 		{
             Helper helper = new Helper(buildSpeedMultiplier: 5);
 			
-            ITargetsFactory targetsFactory = Substitute.For<ITargetsFactory>();
             ICruiser enemyCruiser = helper.CreateCruiser(dummyEnemyCruiser);
 
             IBuilding[] buildings = FindObjectsOfType<Building>();
             foreach (IBuilding building in buildings)
             {
-                helper.InitialiseBuilding(building, targetsFactory: targetsFactory, enemyCruiser: enemyCruiser);
+                helper.InitialiseBuilding(building, enemyCruiser: enemyCruiser);
                 building.CompletedBuildable += (sender, e) => building.InitiateDelete();
                 building.StartConstruction();
             }
