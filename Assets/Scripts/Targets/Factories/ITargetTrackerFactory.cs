e0508233d69c8a82f7e448de63bc03f1d7b4de5b
Felix Mann <felix.fei.mann@gmail.com>
2019-01-02 16:53:49 -0800

    Create TargetTrackerFactory :)

---
+++ Assets/Scripts/Targets/Factories/ITargetTrackerFactory.cs
@@ -0,0 +1,18 @@
+﻿using BattleCruisers.Targets.TargetFinders;
+using BattleCruisers.Targets.TargetTrackers;
+using BattleCruisers.Targets.TargetTrackers.Ranking;
+
+namespace BattleCruisers.Targets.Factories
+{
+    public interface ITargetTrackerFactory
+    {
+        // Highest priority trackers
+        IRankedTargetTracker UserChosenTargetTracker { get; }
+        IRankedTargetTracker CreateUserChosenInRangeTargetTracker(ITargetTracker inRangeTargetTracker);
+        IRankedTargetTracker CreateRankedTargetTracker(ITargetFinder targetFinder, ITargetRanker targetRanker);
+        IRankedTargetTracker CreateCompositeTracker(params IRankedTargetTracker[] targetTrackers);
+
+        // Trackers
+        ITargetTracker CreateTargetTracker(ITargetFinder targetFinder);
+    }
+}
---
+++ Assets/Scripts/Targets/Factories/TargetTrackerFactory.cs
@@ -0,0 +1,40 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Targets.TargetFinders;
+using BattleCruisers.Targets.TargetTrackers;
+using BattleCruisers.Targets.TargetTrackers.Ranking;
+using BattleCruisers.Targets.TargetTrackers.UserChosen;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Targets.Factories
+{
+    public class TargetTrackerFactory : ITargetTrackerFactory
+    {
+        public IRankedTargetTracker UserChosenTargetTracker { get; private set; }
+
+        public TargetTrackerFactory(ICruiser enemyCruiser, IRankedTargetTracker userChosenTargetTracker)
+        {
+            Assert.IsNotNull(userChosenTargetTracker);
+            UserChosenTargetTracker = userChosenTargetTracker;
+        }
+
+        public ITargetTracker CreateTargetTracker(ITargetFinder targetFinder)
+        {
+            return new TargetTracker(targetFinder);
+        }
+
+        public IRankedTargetTracker CreateUserChosenInRangeTargetTracker(ITargetTracker inRangeTargetTracker)
+        {
+            return new UserChosenInRangeTargetTracker(inRangeTargetTracker, UserChosenTargetTracker);
+        }
+
+        public IRankedTargetTracker CreateRankedTargetTracker(ITargetFinder targetFinder, ITargetRanker targetRanker)
+        {
+            return new RankedTargetTracker(targetFinder, targetRanker);
+        }
+
+        public IRankedTargetTracker CreateCompositeTracker(params IRankedTargetTracker[] targetTrackers)
+        {
+            return new CompositeTracker(targetTrackers);
+        }
+    }
+}
