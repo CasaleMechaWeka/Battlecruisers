4f6b59a1f517e420171e29b33241d5142c6312cd
Felix Mann <felix.fei.mann@gmail.com>
2019-01-02 17:07:27 -0800

    Create TargetHelperFactory :)

---
+++ Assets/Scripts/Targets/Factories/ITargetHelperFactory.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Targets.Helpers;
+
+namespace BattleCruisers.Targets.Factories
+{
+    public interface ITargetHelperFactory
+    {
+        ITargetRangeHelper CreateShipRangeHelper(IShip ship);
+    }
+}
