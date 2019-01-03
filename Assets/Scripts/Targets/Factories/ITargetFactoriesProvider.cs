16214dbddba28446fcf711ab5f7b4a7dd51d36e6
Felix Mann <felix.fei.mann@gmail.com>
2019-01-02 17:12:16 -0800

    Create TargetFactoriesProvider :)

---
+++ Assets/Scripts/Targets/Factories/ITargetFactoriesProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace BattleCruisers.Targets.Factories
+{
+    public interface ITargetFactoriesProvider
+    {
+        ITargetProcessorFactory ProcessorFactory { get; }
+        ITargetFinderFactory FinderFactory { get; }
+        ITargetTrackerFactory TrackerFactory { get; }
+        ITargetFilterFactory FilterFactory { get; }
+        ITargetRankerFactory RankerFactory { get; }
+        ITargetProviderFactory ProviderFactory { get; }
+        ITargetHelperFactory HelperFactory { get; }
+    }
+}
---
+++ Assets/Scripts/Targets/Factories/TargetFactoriesProvider.cs
@@ -0,0 +1,27 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Targets.TargetTrackers;
+
+namespace BattleCruisers.Targets.Factories
+{
+    public class TargetFactoriesProvider : ITargetFactoriesProvider
+    {
+        public ITargetProcessorFactory ProcessorFactory { get; private set; }
+        public ITargetFinderFactory FinderFactory { get; private set; }
+        public ITargetTrackerFactory TrackerFactory { get; private set; }
+        public ITargetFilterFactory FilterFactory { get; private set; }
+        public ITargetRankerFactory RankerFactory { get; private set; }
+        public ITargetProviderFactory ProviderFactory { get; private set; }
+        public ITargetHelperFactory HelperFactory { get; private set; }
+
+        public TargetFactoriesProvider(ICruiser enemyCruiser, IRankedTargetTracker userChosenTargetTracker)
+        {
+            ProcessorFactory = new TargetProcessorFactory(enemyCruiser, userChosenTargetTracker);
+            FinderFactory = new TargetFinderFactory();
+            TrackerFactory = new TargetTrackerFactory(enemyCruiser, userChosenTargetTracker);
+            FilterFactory = new TargetFilterFactory();
+            RankerFactory = new TargetRankerFactory();
+            ProviderFactory = new TargetProviderFactory();
+            HelperFactory = new TargetHelperFactory();
+        }
+    }
+}
