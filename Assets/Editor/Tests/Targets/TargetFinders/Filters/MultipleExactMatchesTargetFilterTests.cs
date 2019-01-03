dc30aa970a3d86bd3c91b6b652425b9f39281e92
Felix Mann <felix.fei.mann@gmail.com>
2019-01-02 16:39:56 -0800

    Create MultipleExactMatchesTargetFilterTests :)

---
+++ Assets/Editor/Tests/Targets/TargetFinders/Filters/MultipleExactMatchesTargetFilterTests.cs
@@ -0,0 +1,48 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.Targets.TargetFinders.Filters
+{
+    public class MultipleExactMatchesTargetFilterTests
+    {
+        private IExactMatchTargetFilter _filter;
+        private ITarget _target1, _target2;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _filter = new MultipleExactMatchesTargetFilter();
+
+            _target1 = Substitute.For<ITarget>();
+            _target2 = Substitute.For<ITarget>();
+        }
+
+        [Test]
+        public void NoMatches()
+        {
+            Assert.IsFalse(_filter.IsMatch(_target1));
+            Assert.IsFalse(_filter.IsMatch(_target2));
+        }
+
+        [Test]
+        public void SingleMatch()
+        {
+            _filter.Target = _target1;
+
+            Assert.IsTrue(_filter.IsMatch(_target1));
+            Assert.IsFalse(_filter.IsMatch(_target2));
+        }
+
+        [Test]
+        public void MultipleMatches()
+        {
+            _filter.Target = _target1;
+            _filter.Target = _target2;
+
+            Assert.IsTrue(_filter.IsMatch(_target1));
+            Assert.IsTrue(_filter.IsMatch(_target2));
+        }
+    }
+}
--- Assets/Scripts/Targets/TargetFinders/Filters/MultipleExactMatchesTargetFilter.cs
+++ Assets/Scripts/Targets/TargetFinders/Filters/MultipleExactMatchesTargetFilter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 namespace BattleCruisers.Targets.TargetFinders.Filters
 {
-    // FELIX  Test :)
 	public class MultipleExactMatchesTargetFilter : IExactMatchTargetFilter
 	{
         private readonly HashSet<ITarget> _matches;
