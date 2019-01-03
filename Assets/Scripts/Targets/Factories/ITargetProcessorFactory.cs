3990c7712140c913b2c2911701b37bf17df0ea75
Felix Mann <felix.fei.mann@gmail.com>
2019-01-02 16:49:08 -0800

    Create TargetProcessorFactory :)

---
+++ Assets/Scripts/Targets/Factories/ITargetProcessorFactory.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.Targets.TargetProcessors;
+using BattleCruisers.Targets.TargetTrackers;
+
+namespace BattleCruisers.Targets.Factories
+{
+    public interface ITargetProcessorFactory
+    {
+        ITargetProcessor BomberTargetProcessor { get; }
+        ITargetProcessor OffensiveBuildableTargetProcessor { get; }
+        ITargetProcessor CreateTargetProcessor(IRankedTargetTracker rankedTargetTracker);
+    }
+}
---
+++ Assets/Scripts/Targets/Factories/TargetProcessorFactory.cs
@@ -0,0 +1,45 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Targets.TargetFinders;
+using BattleCruisers.Targets.TargetProcessors;
+using BattleCruisers.Targets.TargetTrackers;
+using BattleCruisers.Targets.TargetTrackers.Ranking;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Targets.Factories
+{
+    public class TargetProcessorFactory : ITargetProcessorFactory
+    {
+        public TargetProcessorFactory(ICruiser enemyCruiser, IRankedTargetTracker userChosenTargetTracker)
+		{
+            Helper.AssertIsNotNull(enemyCruiser, userChosenTargetTracker);
+
+            GlobalTargetFinder globalTargetFinder = new GlobalTargetFinder(enemyCruiser);
+
+            BomberTargetProcessor 
+                = new TargetProcessor(
+                    new CompositeTracker(
+                        userChosenTargetTracker,
+                        new RankedTargetTracker(
+                            globalTargetFinder, 
+                            new BomberTargetRanker())));
+
+            OffensiveBuildableTargetProcessor
+                = new TargetProcessor(
+                    new CompositeTracker(
+                        userChosenTargetTracker,
+                        new RankedTargetTracker(
+                            globalTargetFinder,
+                            new OffensiveBuildableTargetRanker())));
+
+            globalTargetFinder.EmitCruiserAsGlobalTarget();
+		}
+
+        public ITargetProcessor BomberTargetProcessor { get; private set; }
+        public ITargetProcessor OffensiveBuildableTargetProcessor { get; private set; }
+
+        public ITargetProcessor CreateTargetProcessor(IRankedTargetTracker rankedTargetTracker)
+		{
+			return new TargetProcessor(rankedTargetTracker);
+		}
+    }
+}
