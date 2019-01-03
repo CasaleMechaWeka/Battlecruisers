c25e6de57b8dc336d012767b018d487365f5c1a4
Felix Mann <felix.fei.mann@gmail.com>
2019-01-02 16:59:48 -0800

    Create TargetProviderFactory :)

---
+++ Assets/Scripts/Targets/Factories/TargetProviderFactory.cs
@@ -0,0 +1,27 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Targets.TargetDetectors;
+using BattleCruisers.Targets.TargetProviders;
+
+namespace BattleCruisers.Targets.Factories
+{
+    public class TargetProviderFactory : ITargetProviderFactory
+    {
+        public ITargetProvider CreateStaticTargetProvider(ITarget target)
+		{
+            return new StaticTargetProvider(target);
+		}
+
+        public IBroadcastingTargetProvider CreateShipBlockingEnemyProvider(ITargetDetector enemyDetector, IUnit parentUnit)
+        {
+            // FELIX  Pass ITargetFactoryProvider :)
+            return new ShipBlockingEnemyProvider(null, enemyDetector, parentUnit);
+        }
+
+        public IBroadcastingTargetProvider CreateShipBlockingFriendlyProvider(ITargetDetector friendlyDetector, IUnit parentUnit)
+        {
+            // FELIX  Pass ITargetFactoryProvider :)
+            return new ShipBlockingFriendlyProvider(null, friendlyDetector, parentUnit);
+        }
+    }
+}
