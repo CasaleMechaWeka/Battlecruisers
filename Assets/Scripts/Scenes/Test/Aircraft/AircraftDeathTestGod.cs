75be9dca4a1708f70f5ab1a89709bc7d6acfd507
Felix Mann <felix.fei.mann@gmail.com>
2019-01-03 16:59:46 -0800

    Create AircraftDeathTest scene :)

---
+++ Assets/Scripts/Scenes/Test/Aircraft/AircraftDeathTestGod.cs
@@ -0,0 +1,33 @@
+﻿using BattleCruisers.Buildables.Units.Aircraft;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Aircraft
+{
+    public class AircraftDeathTestGod : MonoBehaviour
+    {
+        private AircraftController[] _aircraftList;
+
+        void Start()
+        {
+            Helper helper = new Helper();
+            _aircraftList = FindObjectsOfType<AircraftController>();
+
+            foreach (AircraftController aircraft in _aircraftList)
+            {
+                helper.InitialiseUnit(aircraft);
+                aircraft.StartConstruction();
+            }
+
+            Invoke("DestroyAircraft", 1);
+        }
+
+        private void DestroyAircraft()
+        {
+            foreach (AircraftController aircraft in _aircraftList)
+            {
+                aircraft.Destroy();
+            }
+        }
+    }
+}
