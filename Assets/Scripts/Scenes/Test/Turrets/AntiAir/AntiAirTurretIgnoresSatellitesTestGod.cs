06df86c89396a4fee6a1b3ed4553099ea6204aa1
Felix Mann <felix.fei.mann@gmail.com>
2019-01-03 17:25:34 -0800

    Complete AntiAirTurretIgnoresSatellites test scene, to check AA turrets ignore satellites :)

---
+++ Assets/Scripts/Scenes/Test/Turrets/AntiAir/AntiAirTurretIgnoresSatellitesTestGod.cs
@@ -0,0 +1,29 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Buildables.Units.Aircraft;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Turrets.AntiAir
+{
+    public class AntiAirTurretIgnoresSatellitesTestGod : MonoBehaviour
+    {
+        void Start()
+        {
+            Helper helper = new Helper();
+
+            // Aircraft
+            AircraftController[] aircraftList = FindObjectsOfType<AircraftController>();
+            foreach (AircraftController aircraft in aircraftList)
+            {
+                helper.InitialiseUnit(aircraft, Faction.Blues);
+                aircraft.StartConstruction();
+            }
+
+            // Turret
+            TurretController turret = FindObjectOfType<TurretController>();
+            helper.InitialiseBuilding(turret, Faction.Reds);
+            turret.StartConstruction();
+        }
+    }
+}
