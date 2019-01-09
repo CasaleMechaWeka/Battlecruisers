5da132cd8366b50d08193fab1963c132aad5e977
Felix Mann <felix.fei.mann@gmail.com>
2019-01-09 09:23:31 -0800

    Create AirFactorySpawnPositionFinder :)

---
+++ Assets/Scripts/Buildables/Buildings/Factories/Spawning/AirFactorySpawnPositionFinder.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.Buildables.Units;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Factories.Spawning
+{
+    // FELIX  Test :)
+    public class AirFactorySpawnPositionFinder : ISpawnPositionFinder
+    {
+        private readonly IFactory _factory;
+
+        public AirFactorySpawnPositionFinder(IFactory factory)
+        {
+            Assert.IsNotNull(factory);
+            _factory = factory;
+        }
+
+        public Vector3 FindSpawnPosition(IUnit unitToSpawn)
+        {
+            float verticalChange = (_factory.Size.y * 0.6f) + (unitToSpawn.Size.y * 0.5f);
+            return _factory.Transform.Position + (_factory.Transform.Up * verticalChange);
+        }
+    }
+}
