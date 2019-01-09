be4279ef074e7a68439d8b6dcbf921e11d9623a7
Felix Mann <felix.fei.mann@gmail.com>
2019-01-09 10:42:10 -0800

    Create hot spawning test scene.  Broken, but unsure if real bug :/

---
+++ Assets/Scripts/Scenes/Test/Factories/HotSpawningTestGod.cs
@@ -0,0 +1,33 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Factories
+{
+    public class HotSpawningTestGod : MonoBehaviour
+    {
+        public UnitWrapper unitPrefab;
+
+        void Start()
+        {
+            unitPrefab.Initialise();
+            unitPrefab.Buildable.StaticInitialise();
+
+            Helper helper = new Helper();
+
+            // Factory
+            Factory factory = FindObjectOfType<Factory>();
+            helper.InitialiseBuilding(factory, Faction.Blues, parentCruiserDirection: Direction.Right);
+            factory.CompletedBuildable += (sender, e) => factory.StartBuildingUnit(unitPrefab);
+            factory.StartConstruction();
+
+            // Turret
+            TurretController turret = FindObjectOfType<TurretController>();
+            helper.InitialiseBuilding(turret, Faction.Reds);
+            turret.StartConstruction();
+        }
+    }
+}
