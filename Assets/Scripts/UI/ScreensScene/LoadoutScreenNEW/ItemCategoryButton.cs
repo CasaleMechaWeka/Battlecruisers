509ac93ba4508b43ababd584c1a8c7e7d5e8a34b
Felix Mann <felix.fei.mann@gmail.com>
2019-01-24 17:07:21 -0800

    Create ItemCategoryButtons :D

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/ItemCategoryButton.cs
@@ -0,0 +1,26 @@
+﻿using BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.Items;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW
+{
+    // FELIX  Handle being disabled during comparison.  Follow BuildableButtonController's use of IBroadcastingFilter??
+    public class ItemCategoryButton : MonoBehaviour, IPointerClickHandler
+    {
+        private IItemPanelsController _itemPanels;
+
+        public ItemType itemType;
+
+        public void Initialise(IItemPanelsController itemPanels)
+        {
+            Assert.IsNotNull(itemPanels);
+            _itemPanels = itemPanels;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            _itemPanels.ShowItemsPanel(itemType);
+        }
+    }
+}
