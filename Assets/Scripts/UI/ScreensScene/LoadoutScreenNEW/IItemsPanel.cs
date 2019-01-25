dcc873fa3fb57362ba0b57569806b83fc725effe
Felix Mann <felix.fei.mann@gmail.com>
2019-01-24 16:43:57 -0800

    Create ItemsPanel :)

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/IItemsPanel.cs
@@ -0,0 +1,16 @@
+﻿namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW
+{
+    public enum ItemType
+    {
+        Hull,
+        // Buildings
+        Factory, Defense, Offensive, Tactical, Ultra,
+        // Units
+        Ship, Aircraft
+    }
+
+    public interface IItemsPanel : IPanel
+    {
+        ItemType ItemType { get; }
+    }
+}
---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/ItemsPanel.cs
@@ -0,0 +1,8 @@
+﻿namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW
+{
+    public class ItemsPanel : Panel, IItemsPanel
+    {
+        public ItemType itemType;
+        public ItemType ItemType { get { return ItemType; } }
+    }
+}
