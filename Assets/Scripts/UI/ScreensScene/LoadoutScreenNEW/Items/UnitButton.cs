86ffc5f02fccefe2786f44d08ad53b2c6bb57e1e
Felix Mann <felix.fei.mann@gmail.com>
2019-01-24 20:57:07 -0800

    Create UnitButton (and unit prefabs get worked over :/ )

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/Items/UnitButton.cs
@@ -0,0 +1,26 @@
+﻿using BattleCruisers.Buildables.Units;
+using BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.ItemDetails;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW
+{
+    public class UnitButton : ItemButton
+    {
+        public UnitWrapper unit;
+
+        public override void Initialise(IItemDetailsDisplayer itemDetailsDisplayer)
+        {
+            base.Initialise(itemDetailsDisplayer);
+
+            Assert.IsNotNull(unit);
+
+            unit.Initialise();
+            unit.Buildable.StaticInitialise();
+        }
+
+        protected override void ShowItemDetails()
+        {
+            _itemDetailsDisplayer.ShowDetails(unit.Buildable);
+        }
+    }
+}
