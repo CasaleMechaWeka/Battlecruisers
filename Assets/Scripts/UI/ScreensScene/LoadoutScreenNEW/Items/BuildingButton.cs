3965c5bfbcafbd67d560ddf0111f734c86b64ad8
Felix Mann <felix.fei.mann@gmail.com>
2019-01-24 20:34:34 -0800

    Create BuildingButton script :)

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/Items/BuildingButton.cs
@@ -0,0 +1,22 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.ItemDetails;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW
+{
+    public class BuildingButton : ItemButton
+    {
+        public IBuilding building;
+
+        public void Initialise(IItemDetailsDisplayer itemDetailsDisplayer)
+        {
+            base.Initailise(itemDetailsDisplayer);
+            Assert.IsNotNull(building);
+        }
+
+        protected override void ShowItemDetails()
+        {
+            _itemDetailsDisplayer.ShowDetails(building);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/Items/ItemButton.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.ItemDetails;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW
+{
+    public abstract class ItemButton : MonoBehaviour, IPointerClickHandler
+    {
+        protected IItemDetailsDisplayer _itemDetailsDisplayer;
+
+        protected void Initailise(IItemDetailsDisplayer itemDetailsDisplayer)
+        {
+            Assert.IsNotNull(itemDetailsDisplayer);
+            _itemDetailsDisplayer = itemDetailsDisplayer;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            ShowItemDetails();
+        }
+
+        protected abstract void ShowItemDetails();
+    }
+}
