50318ee7c8f2b65d845a980a4eee8c13df198be2
Felix Mann <felix.fei.mann@gmail.com>
2019-01-25 19:27:12 -0800

    Rename interface :P

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/IItemToCompareTracker.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.Buildables;
+using System;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW
+{
+    public interface IItemToCompareTracker
+    {
+        TargetType? ItemToCompare { get; }
+
+        event EventHandler ItemToCompareChanged;
+    }
+}
