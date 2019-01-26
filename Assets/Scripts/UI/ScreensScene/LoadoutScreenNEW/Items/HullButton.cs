5fe73ce6f0787c7a8e00f20ce95e722b8d36c470
Felix Mann <felix.fei.mann@gmail.com>
2019-01-25 16:27:30 -0800

    Show hull details :D

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/Items/HullButton.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.ItemDetails;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW
+{
+    public class HullButton : ItemButton
+    {
+        public Cruiser cruiser;
+
+        public override void Initialise(IItemDetailsDisplayer itemDetailsDisplayer)
+        {
+            base.Initialise(itemDetailsDisplayer);
+
+            Assert.IsNotNull(cruiser);
+            cruiser.StaticInitialise();
+        }
+
+        protected override void ShowItemDetails()
+        {
+            _itemDetailsDisplayer.ShowDetails(cruiser);
+        }
+    }
+}
