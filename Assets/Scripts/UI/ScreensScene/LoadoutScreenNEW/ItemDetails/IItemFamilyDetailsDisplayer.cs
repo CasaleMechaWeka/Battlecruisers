67e1d6989abaf82f58d7db44106b319694182556
Felix Mann <felix.fei.mann@gmail.com>
2019-01-26 17:34:19 -0800

    Create ItemFamilyDetailsDisplayer :)

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/ItemDetails/IItemFamilyDetailsDisplayer.cs
@@ -0,0 +1,11 @@
+﻿using BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW
+{
+    public interface IItemFamilyDetailsDisplayer<TItem> where TItem : IComparableItem
+    {
+        void SelectItem(TItem item);
+        void CompareWithSelectedItem(TItem item);
+        void HideDetails();
+    }
+}
---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/ItemDetails/ItemFamilyDetailsDisplayer.cs
@@ -0,0 +1,49 @@
+﻿using BattleCruisers.UI.Common.BuildableDetails;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows;
+using BattleCruisers.Utils;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW
+{
+    // FELIX  Test :D
+    // FELIX  Rename to ItemDetailsDisplayer?
+    public class ItemFamilyDetailsDisplayer<TItem> : IItemFamilyDetailsDisplayer<TItem> where TItem : class, IComparableItem
+    {
+        private readonly IComparableItemDetails<TItem> _leftDetails, _rightDetails;
+        private TItem _selectedItem;
+
+        public ItemFamilyDetailsDisplayer(IComparableItemDetails<TItem> leftDetails, IComparableItemDetails<TItem> rightDetails)
+        {
+            Helper.AssertIsNotNull(leftDetails, rightDetails);
+
+            _leftDetails = leftDetails;
+            _rightDetails = rightDetails;
+            _selectedItem = null;
+        }
+
+        public void SelectItem(TItem item)
+        {
+            Assert.IsNotNull(item);
+
+            HideDetails();
+            _selectedItem = item;
+            _leftDetails.ShowItemDetails(item);
+        }
+
+        public void CompareWithSelectedItem(TItem item)
+        {
+            Assert.IsNotNull(item);
+            Assert.IsNotNull(_selectedItem);
+
+            _leftDetails.ShowItemDetails(_selectedItem, item);
+            _rightDetails.ShowItemDetails(item, _selectedItem);
+        }
+
+        public void HideDetails()
+        {
+            _leftDetails.Hide();
+            _rightDetails.Hide();
+            _selectedItem = null;
+        }
+    }
+}
