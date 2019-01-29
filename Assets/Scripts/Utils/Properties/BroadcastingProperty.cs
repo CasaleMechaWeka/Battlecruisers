fca6b0f57693d48eb69fe9afc6323bfd17e5012e
Felix Mann <felix.fei.mann@gmail.com>
2019-01-28 16:16:26 -0800

    Create IBroadcastingProperty, that does not expose public setter :D

---
+++ Assets/Scripts/Utils/Properties/BroadcastingProperty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.Properties
+{
+    // FELIX  Test :D
+    public class BroadcastingProperty<T> : IBroadcastingProperty<T>
+    {
+        private readonly ISettableBroadcastingProperty<T> _baseProperty;
+
+        public T Value { get { return _baseProperty.Value; } }
+
+        public event EventHandler ValueChanged
+        {
+            add { _baseProperty.ValueChanged += value; }
+            remove { _baseProperty.ValueChanged -= value; }
+        }
+
+        public BroadcastingProperty(ISettableBroadcastingProperty<T> baseProperty)
+        {
+            Assert.IsNotNull(baseProperty);
+            _baseProperty = baseProperty;
+        }
+    }
+}
