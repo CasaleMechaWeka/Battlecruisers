1d04acdeef7e10835dc88f8924da05953bfe82cd
Felix Mann <felix.fei.mann@gmail.com>
2019-01-28 16:46:56 -0800

    Create ComparingItemFamilyTracker :)

---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/Comparisons/ComparingItemFamilyTracker.cs
@@ -0,0 +1,22 @@
+﻿using BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.Items;
+using BattleCruisers.Utils.Properties;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.Comparisons
+{
+    public class ComparingItemFamilyTracker : IComparingItemFamilyTracker
+    {
+        private ISettableBroadcastingProperty<ItemFamily?> _comparingFamily;
+        public IBroadcastingProperty<ItemFamily?> ComparingFamily { get; private set; }
+
+        public ComparingItemFamilyTracker()
+        {
+            _comparingFamily = new SettableBroadcastingProperty<ItemFamily?>(initialValue: null);
+            ComparingFamily = new BroadcastingProperty<ItemFamily?>(_comparingFamily);
+        }
+
+        public void SetComparingFamily(ItemFamily? comparingItemFamily)
+        {
+            _comparingFamily.Value = comparingItemFamily;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/Comparisons/IComparingItemFamilyTracker.cs
@@ -0,0 +1,12 @@
+﻿using BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.Items;
+using BattleCruisers.Utils.Properties;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.Comparisons
+{
+    public interface IComparingItemFamilyTracker
+    {
+        IBroadcastingProperty<ItemFamily?> ComparingFamily { get; }
+
+        void SetComparingFamily(ItemFamily? comparingItemFamily);
+    }
+}
