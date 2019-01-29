c97ced08bf478ab7275396c70f7298b4e8735c0d
Felix Mann <felix.fei.mann@gmail.com>
2019-01-28 16:06:41 -0800

    Remove unused generic FilterToggler, sniff :/

--- Assets/Scripts/UI/FilterToggler.cs
+++ Assets/Scripts/UI/FilterToggler.cs
@@ -2,7 +2,6 @@
 using BattleCruisers.Utils;
 using System;
 
-// FELIX  Create Toggle namespace?
 namespace BattleCruisers.UI
 {
     public class FilterToggler
@@ -24,30 +23,6 @@ namespace BattleCruisers.UI
         private void _shouldBeEnabledFilter_PotentialMatchChange(object sender, EventArgs e)
         {
             _togglable.Enabled = _shouldBeEnabledFilter.IsMatch;
-        }
-    }
-
-    // FELIX  Remove if unused :)
-    // FELIX  Test :P
-    public class FilterToggler<TTogglable> where TTogglable : ITogglable
-    {
-        private readonly TTogglable _togglable;
-        private readonly IBroadcastingFilter<TTogglable> _shouldBeEnabledFilter;
-
-        public FilterToggler(TTogglable togglable, IBroadcastingFilter<TTogglable> shouldBeEnabledFilter)
-        {
-            Helper.AssertIsNotNull(togglable, shouldBeEnabledFilter);
-
-            _togglable = togglable;
-            _shouldBeEnabledFilter = shouldBeEnabledFilter;
-
-            _shouldBeEnabledFilter.PotentialMatchChange += _shouldBeEnabledFilter_PotentialMatchChange;
-            _togglable.Enabled = _shouldBeEnabledFilter.IsMatch(_togglable);
-        }
-
-        private void _shouldBeEnabledFilter_PotentialMatchChange(object sender, EventArgs e)
-        {
-            _togglable.Enabled = _shouldBeEnabledFilter.IsMatch(_togglable);
         }
     }
 }
