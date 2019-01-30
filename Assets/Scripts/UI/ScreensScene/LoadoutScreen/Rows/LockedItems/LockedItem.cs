c881c802db25a5aa4be4cc2a29d29a375fcf2a3d
Felix Mann <felix.fei.mann@gmail.com>
2019-01-29 16:53:02 -0800

    Create ItemContainer :)

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/LockedItems/LockedItem.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Rows/LockedItems/LockedItem.cs
@@ -1,5 +1,6 @@
 ﻿namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.LockedItems
 {
+    // FELIX  Used in NEW loadout screen, don't delete :P
     public class LockedItem : MonoBehaviourWrapper
     {
     }
---
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreenNEW/Items/ItemContainer.cs
@@ -0,0 +1,42 @@
+﻿using BattleCruisers.Data.Models;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.Rows.LockedItems;
+using BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.Comparisons;
+using BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.ItemDetails;
+using BattleCruisers.Utils;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.ScreensScene.LoadoutScreenNEW.Items
+{
+    /// <summary>
+    /// Contains:
+    /// + Item button
+    /// + Locked item placeholder
+    /// 
+    /// Shows the item button if the item has been unlocked, otherwise shows
+    /// the locked placholder.
+    /// </summary>
+    public abstract class ItemContainer : MonoBehaviour
+    {
+        public void Initialise(
+            IItemDetailsManager itemDetailsManager,
+            IComparingItemFamilyTracker comparingFamiltyTracker,
+            IGameModel gameModel)
+        {
+            Helper.AssertIsNotNull(itemDetailsManager, comparingFamiltyTracker, gameModel);
+
+            LockedItem lockedItem = GetComponentInChildren<LockedItem>(includeInactive: true);
+            Assert.IsNotNull(lockedItem);
+
+            ItemButton itemButton = GetComponentInChildren<ItemButton>(includeInactive: true);
+            Assert.IsNotNull(itemButton);
+            itemButton.Initialise(itemDetailsManager, comparingFamiltyTracker);
+
+            bool isItemUnlocked = IsUnlocked(gameModel);
+            lockedItem.IsVisible = !isItemUnlocked;
+            itemButton.IsVisible = isItemUnlocked;
+        }
+
+        protected abstract bool IsUnlocked(IGameModel gameModel);
+    }
+}
