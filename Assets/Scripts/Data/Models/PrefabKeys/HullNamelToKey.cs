c547b424c60db13ba2b47a8d47dff6a46f15b62e
Felix Mann <felix.fei.mann@gmail.com>
2019-01-30 17:08:37 -0800

    Create HullNamelToKey :)

---
+++ Assets/Scripts/Data/Models/PrefabKeys/HullNamelToKey.cs
@@ -0,0 +1,32 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Fetchers;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Data.Models.PrefabKeys
+{
+    public class HullNamelToKey : IHullNameToKey
+    {
+        private readonly IDictionary<string, HullKey> _hullNameToKey;
+
+        public HullNamelToKey(IList<HullKey> keys, IPrefabFactory prefabFactory)
+        {
+            Helper.AssertIsNotNull(keys, prefabFactory);
+
+            _hullNameToKey = new Dictionary<string, HullKey>();
+
+            foreach (HullKey key in keys)
+            {
+                ICruiser hullPrefab = prefabFactory.GetCruiserPrefab(key);
+                _hullNameToKey.Add(hullPrefab.Name, key);
+            }
+        }
+
+        public HullKey GetKey(string hullName)
+        {
+            Assert.IsTrue(_hullNameToKey.ContainsKey(hullName));
+            return _hullNameToKey[hullName];
+        }
+    }
+}
