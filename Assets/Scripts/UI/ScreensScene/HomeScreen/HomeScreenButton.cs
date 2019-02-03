e429d01243ae5e2317d164e1cbc5ddb2ffca3932
Felix Mann <felix.fei.mann@gmail.com>
2019-02-03 14:28:03 -0800

    Create PlayButton script :D

---
+++ Assets/Scripts/UI/ScreensScene/HomeScreen/HomeScreenButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+
+namespace BattleCruisers.UI.ScreensScene.HomeScreen
+{
+    public abstract class HomeScreenButton : MonoBehaviour, IPointerClickHandler
+    {
+        protected IHomeScreen _homeScreen;
+
+        public void Initialise(IHomeScreen homeScreen)
+        {
+            Assert.IsNotNull(homeScreen);
+            _homeScreen = homeScreen;
+        }
+
+        public abstract void OnPointerClick(PointerEventData eventData);
+    }
+}
---
+++ Assets/Scripts/UI/ScreensScene/HomeScreen/PlayButton.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.Data.Models;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+
+namespace BattleCruisers.UI.ScreensScene.HomeScreen
+{
+    public class PlayButton : HomeScreenButton
+    {
+        private IGameModel _gameModel;
+
+        public void Initialise(IHomeScreen homeScreen, IGameModel gameModel)
+        {
+            base.Initialise(homeScreen);
+
+            Assert.IsNotNull(gameModel);
+            _gameModel = gameModel;
+        }
+
+        public override void OnPointerClick(PointerEventData eventData)
+        {
+            if (_gameModel.HasAttemptedTutorial)
+            {
+                _homeScreen.StartLevel1();
+            }
+            else
+            {
+                _homeScreen.StartTutorial();
+            }
+        }
+    }
+}
