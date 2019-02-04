faceb41e7a704aa306faa8075fc45248eb1666c1
Felix Mann <felix.fei.mann@gmail.com>
2019-02-03 19:44:37 -0800

    Refactory LevelButtonController slightly :)

--- Assets/Scripts/UI/ScreensScene/LevelsScreen/LevelButtonController.cs
+++ Assets/Scripts/UI/ScreensScene/LevelsScreen/LevelButtonController.cs
@@ -1,36 +1,44 @@
 ﻿using BattleCruisers.Scenes;
 using BattleCruisers.Utils;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace BattleCruisers.UI.ScreensScene.LevelsScreen
 {
-    // FELIX  Extend Togglable :P
-    // FELIX  Implement IPointerClickHandler instead of public Button field :)
-    public class LevelButtonController : MonoBehaviour 
-	{
-		public Button button;
+    public class LevelButtonController : Togglable, IPointerClickHandler
+    {
+        private LevelInfo _level;
+        private IScreensSceneGod _screensSceneGod;
+
         public Text levelNumberText;
 		public Text levelNameText;
         public LevelStatsController levelStatsController;
-		public CanvasGroup canvasGroup;
 
-		public void Initialise(LevelInfo level, IScreensSceneGod screensSceneGod, int numOfLevelsUnlocked)
+		private CanvasGroup _canvasGroup;
+        protected override CanvasGroup CanvasGroup { get { return _canvasGroup; } }
+
+        public void Initialise(LevelInfo level, IScreensSceneGod screensSceneGod, int numOfLevelsUnlocked)
 		{
-            Helper.AssertIsNotNull(button, levelNumberText, levelNameText, levelStatsController, canvasGroup, level, screensSceneGod);
+            Helper.AssertIsNotNull(levelNumberText, levelNameText, levelStatsController, level, screensSceneGod);
+
+            _level = level;
+            _screensSceneGod = screensSceneGod;
 
             levelNumberText.text = level.Num.ToString();
             levelNameText.text = level.Name;
             levelStatsController.Initialise(level.DifficultyCompleted);
 
-			if (numOfLevelsUnlocked >= level.Num)
-			{
-                button.onClick.AddListener(() => screensSceneGod.LoadLevel(level.Num));
-			}
-			else
-			{
-				canvasGroup.alpha = Constants.DISABLED_UI_ALPHA;
-			}
+            _canvasGroup = GetComponent<CanvasGroup>();
+            Assert.IsNotNull(_canvasGroup);
+
+            Enabled = numOfLevelsUnlocked >= level.Num;
 		}
-	}
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            _screensSceneGod.LoadLevel(_level.Num);
+        }
+    }
 }
