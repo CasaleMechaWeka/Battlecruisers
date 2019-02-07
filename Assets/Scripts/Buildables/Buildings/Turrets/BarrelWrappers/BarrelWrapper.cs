e1b06da72c9593ba77ec79ddc2d273ca32c4b1a0
Felix Mann <felix.fei.mann@gmail.com>
2019-02-06 19:21:29 -0800

    Remove TODO :)

--- Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/BarrelWrapper.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/BarrelWrapper.cs
@@ -98,7 +98,6 @@ namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
             }
         }
 
-        // FELIX Create BarrelWrapperArgs class?
         public void Initialise(
             ITarget parent, 
             IFactoryProvider factoryProvider, 
