64cf3b7880ede351c811a929d509f0b64888bfb5
Felix Mann <felix.fei.mann@gmail.com>
2019-02-07 15:50:18 -0800

    Add a whole bunch of build rate boost providers :D

--- Assets/Scripts/Buildables/Boost/GlobalBoostProviders.cs
+++ Assets/Scripts/Buildables/Boost/GlobalBoostProviders.cs
@@ -5,20 +5,46 @@ namespace BattleCruisers.Buildables.Boost
     public class GlobalBoostProviders : IGlobalBoostProviders
     {
         public IObservableCollection<IBoostProvider> DummyBoostProviders { get; private set; }
+
         public IObservableCollection<IBoostProvider> AircraftBoostProviders { get; private set; }
+
         public IObservableCollection<IBoostProvider> TurretAccuracyBoostProviders { get; private set; }
         public IObservableCollection<IBoostProvider> DefenseFireRateBoostProviders { get; private set; }
         public IObservableCollection<IBoostProvider> OffenseFireRateBoostProviders { get; private set; }
+        
         public IObservableCollection<IBoostProvider> ShieldRechargeRateBoostProviders { get; private set; }
+
+        // Build rate
+        public IObservableCollection<IBoostProvider> DefensivesBuildRateBoostProviders { get; private set; }
+        public IObservableCollection<IBoostProvider> OffensivesBuildRateBoostProviders { get; private set; }
+        public IObservableCollection<IBoostProvider> TacticalsBuildRateBoostProviders { get; private set; }
+        public IObservableCollection<IBoostProvider> ShieldsBuildRateBoostProviders { get; private set; }
+        public IObservableCollection<IBoostProvider> UltrasBuildRateBoostProviders { get; private set; }
+        public IObservableCollection<IBoostProvider> AirFactoryBuildRateBoostProviders { get; private set; }
+        public IObservableCollection<IBoostProvider> NavalFactoryBuildRateBoostProviders { get; private set; }
+        public IObservableCollection<IBoostProvider> DroneStationBuildRateBoostProviders { get; private set; }
 
         public GlobalBoostProviders()
         {
             DummyBoostProviders = new DummyObservableCollection<IBoostProvider>();
+
             AircraftBoostProviders = new ObservableCollection<IBoostProvider>();
-            TurretAccuracyBoostProviders = new ObservableCollection<IBoostProvider>();
+
             DefenseFireRateBoostProviders = new ObservableCollection<IBoostProvider>();
             OffenseFireRateBoostProviders = new ObservableCollection<IBoostProvider>();
+            TurretAccuracyBoostProviders = new ObservableCollection<IBoostProvider>();
+
             ShieldRechargeRateBoostProviders = new ObservableCollection<IBoostProvider>();
+
+            // Build rate
+            DefenseFireRateBoostProviders = new ObservableCollection<IBoostProvider>();
+            OffensivesBuildRateBoostProviders = new ObservableCollection<IBoostProvider>();
+            TacticalsBuildRateBoostProviders = new ObservableCollection<IBoostProvider>();
+            ShieldsBuildRateBoostProviders = new ObservableCollection<IBoostProvider>();
+            UltrasBuildRateBoostProviders = new ObservableCollection<IBoostProvider>();
+            AircraftBoostProviders = new ObservableCollection<IBoostProvider>();
+            NavalFactoryBuildRateBoostProviders = new ObservableCollection<IBoostProvider>();
+            DroneStationBuildRateBoostProviders = new ObservableCollection<IBoostProvider>();
         }
 	}
 }
--- Assets/Scripts/Buildables/Boost/IGlobalBoostProviders.cs
+++ Assets/Scripts/Buildables/Boost/IGlobalBoostProviders.cs
@@ -5,13 +5,25 @@ namespace BattleCruisers.Buildables.Boost
     public interface IGlobalBoostProviders
     {
         IObservableCollection<IBoostProvider> DummyBoostProviders { get; }
+
         IObservableCollection<IBoostProvider> AircraftBoostProviders { get; }
+
         IObservableCollection<IBoostProvider> DefenseFireRateBoostProviders { get; }
         IObservableCollection<IBoostProvider> OffenseFireRateBoostProviders { get; }
-        IObservableCollection<IBoostProvider> ShieldRechargeRateBoostProviders { get; }
-
         // Currently affects ALL turrets (ships, gunship, buildings).  
         // That's ok though, because only used in tutorial to improve artillery accuracy :)
         IObservableCollection<IBoostProvider> TurretAccuracyBoostProviders { get; }
+
+        IObservableCollection<IBoostProvider> ShieldRechargeRateBoostProviders { get; }
+
+        // Build rate
+        IObservableCollection<IBoostProvider> DefensivesBuildRateBoostProviders { get; }
+        IObservableCollection<IBoostProvider> OffensivesBuildRateBoostProviders { get; }
+        IObservableCollection<IBoostProvider> TacticalsBuildRateBoostProviders { get; }
+        IObservableCollection<IBoostProvider> ShieldsBuildRateBoostProviders { get; }
+        IObservableCollection<IBoostProvider> UltrasBuildRateBoostProviders { get; }
+        IObservableCollection<IBoostProvider> AirFactoryBuildRateBoostProviders { get; }
+        IObservableCollection<IBoostProvider> NavalFactoryBuildRateBoostProviders { get; }
+        IObservableCollection<IBoostProvider> DroneStationBuildRateBoostProviders { get; }
     }
 }
