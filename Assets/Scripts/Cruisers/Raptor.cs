b59430c13f065ae2ecb66585deb62f660f5d6150
Felix Mann <felix.fei.mann@gmail.com>
2019-02-07 15:39:30 -0800

    Raptor doubles shield recharge rate :D

---
+++ Assets/Scripts/Cruisers/Raptor.cs
@@ -0,0 +1,21 @@
+﻿using BattleCruisers.Buildables.Boost;
+
+namespace BattleCruisers.Cruisers
+{
+    /// <summary>
+    /// Perks:
+    /// + Increases shield recharge rate
+    /// </summary>
+    public class Raptor : Cruiser
+    {
+        public float shieldRechargeRateBoost;
+
+        public override void Initialise(ICruiserArgs args)
+        {
+            base.Initialise(args);
+
+            IBoostProvider boostProvider = FactoryProvider.BoostFactory.CreateBoostProvider(shieldRechargeRateBoost);
+            FactoryProvider.GlobalBoostProviders.ShieldRechargeRateBoostProviders.Add(boostProvider);
+        }
+    }
+}
