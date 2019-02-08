897ff4b721f2fe9bb07f374e92145e25bc18cc9d
Felix Mann <felix.fei.mann@gmail.com>
2019-02-07 19:31:54 -0800

    Rename Cancel to Home button :)

--- Assets/Scripts/Scenes/ScreensSceneGod.cs
+++ Assets/Scripts/Scenes/ScreensSceneGod.cs
@@ -80,7 +80,7 @@ namespace BattleCruisers.Scenes
             // TEMP  Go to specific screen :)
             //GoToSettingsScreen();
             //GoToLevelsScreen();
-            GoToLoadoutScreen();
+            //GoToLoadoutScreen();
         }
         
         private void GoToPostBattleScreen()
