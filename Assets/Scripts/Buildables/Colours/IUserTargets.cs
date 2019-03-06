9fa3eeda021f8dcbbe6f8496f0f008e7410a7bd7
Felix Mann <felix.fei.mann@gmail.com>
2019-03-05 20:29:39 -0800

    Move TargetColours to IUserTargets :)

--- Assets/Scripts/Buildables/Colours/IUserTargets.cs
+++ Assets/Scripts/Buildables/Colours/IUserTargets.cs
@@ -1,5 +1,14 @@
-﻿namespace BattleCruisers.Buildables.Colours
+﻿using UnityEngine;
+
+namespace BattleCruisers.Buildables.Colours
 {
+    public static class TargetColours
+    {
+        public static Color Default = Color.black;
+        public static Color Selected = new Color(186f / 255f, 56f / 255f, 32f / 255f);    // Orange
+        public static Color Targetted = Color.red;
+    }
+
     public interface IUserTargets
     {
         /// <summary>
