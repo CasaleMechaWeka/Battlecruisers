11626ccc7bc875a9db7bd82c1e2525700929f208
Felix Mann <felix.fei.mann@gmail.com>
2019-03-06 18:19:09 -0800

    Create test scene highlighting archon spawning when not enough space bug :P

---
+++ Assets/Scripts/Scenes/Test/Factories/EnemyBlockingSpawningTestGod.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Factories
+{
+    public class EnemyBlockingSpawningTestGod : MonoBehaviour 
+	{
+		public Factory leftFactory, rightFactory;
+		public UnitWrapper archonPrefab, attackBoatPrefab;
+
+		void Start()
+		{
+			archonPrefab.Initialise();
+            attackBoatPrefab.Initialise();
+
+			Helper helper = new Helper();
+
+            // Factory building archon
+            helper.InitialiseBuilding(leftFactory, Faction.Reds, parentCruiserDirection: Direction.Right);
+			leftFactory.CompletedBuildable += LeftFactory_CompletedBuildable;
+			leftFactory.StartConstruction();
+
+            // Factory building attack boats
+            helper.InitialiseBuilding(rightFactory, Faction.Blues, parentCruiserDirection: Direction.Left);
+			rightFactory.CompletedBuildable += RightFactory_CompletedBuildable;
+			rightFactory.StartConstruction();
+		}
+
+		private void LeftFactory_CompletedBuildable(object sender, EventArgs e)
+        {
+            Invoke("StartBuildingArchon", 5);
+        }
+
+        private void StartBuildingArchon()
+        {
+            leftFactory.StartBuildingUnit(archonPrefab);
+        }
+
+		private void RightFactory_CompletedBuildable(object sender, EventArgs e)
+        {
+            rightFactory.StartBuildingUnit(attackBoatPrefab);
+		}
+	}
+}
