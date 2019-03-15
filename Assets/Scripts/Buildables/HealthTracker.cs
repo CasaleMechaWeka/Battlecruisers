484aca3b0c75124bb56bd6ce9298654af7548eb3
Felix Mann <felix.fei.mann@gmail.com>
2019-03-14 19:52:10 -0700

    Create HealthTracker :D

---
+++ Assets/Scripts/Buildables/HealthTracker.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables
+{
+    // FELIX  Use :)
+    // FELIX  Test :)
+    public class HealthTracker : IHealthTracker
+    {
+        public float MaxHealth { get; private set; }
+        public DamagableState DamagableState { private get; set; }
+
+        private float _health;
+        public float Health
+        {
+            get { return _health; }
+            protected set
+            {
+                if (value >= MaxHealth)
+                {
+                    _health = MaxHealth;
+                }
+                else if (value <= 0)
+                {
+                    _health = 0;
+                    
+                    if (HealthGone != null)
+                    {
+                        HealthGone.Invoke(this, EventArgs.Empty);
+                    }
+                }
+                else
+                {
+                    _health = value;
+                }
+
+                if (HealthChanged != null)
+                {
+                    HealthChanged.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public event EventHandler HealthChanged;
+        public event EventHandler HealthGone;
+
+        public HealthTracker(float maxHealth)
+        {
+            MaxHealth = maxHealth;
+        }
+
+        public bool TakeDamage(float damageAmount)
+        {
+            Assert.IsTrue(damageAmount > 0);
+
+            if (Health > 0
+                && DamagableState == DamagableState.Damagable)
+            {
+                Health -= damageAmount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Repair(float repairAmount)
+        {
+            Assert.IsTrue(repairAmount > 0);
+            Health += repairAmount;
+        }
+    }
+}
---
+++ Assets/Scripts/Buildables/IHealthTracker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BattleCruisers.Buildables
+{
+    public enum DamagableState
+    {
+        Damagable, Invincible
+    }
+
+    public interface IHealthTracker
+    {
+        float Health { get; }
+        float MaxHealth { get; }
+        DamagableState DamagableState { set; }
+
+        event EventHandler HealthChanged;
+        event EventHandler HealthGone;
+
+        /// <returns>True if the target was damaged, false otherwise (eg: due to being in invincible state).</returns>
+        bool TakeDamage(float damageAmount);
+
+        void Repair(float repairAmount);
+    }
+}
