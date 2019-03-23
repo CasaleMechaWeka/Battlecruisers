c861c50609b7bfe9288284c8f8413e0ee038b4ec
Felix Mann <felix.fei.mann@gmail.com>
2019-03-23 11:06:20 -0700

    Initialise cruiser in test scene without the world exploding :D

--- Assets/Scripts/Scenes/Test/Effects/Deaths/CruiserDeathTestGod.cs
+++ Assets/Scripts/Scenes/Test/Effects/Deaths/CruiserDeathTestGod.cs
@@ -1,4 +1,6 @@
-﻿using BattleCruisers.Buildables.BuildProgress;
+﻿using BattleCruisers.Buildables.Boost.GlobalProviders;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.BuildProgress;
 using BattleCruisers.Buildables.Repairables;
 using BattleCruisers.Buildables.Units;
 using BattleCruisers.Cruisers;
@@ -6,12 +8,12 @@ using BattleCruisers.Cruisers.Drones;
 using BattleCruisers.Cruisers.Helpers;
 using BattleCruisers.Cruisers.Slots;
 using BattleCruisers.UI.BattleScene.Manager;
+using BattleCruisers.UI.Common.Click;
 using BattleCruisers.Utils.Factories;
 using NSubstitute;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
+using BCUtils = BattleCruisers.Utils;
 
 namespace BattleCruisers.Scenes.Test.Effects.Deaths
 {
@@ -22,25 +24,31 @@ namespace BattleCruisers.Scenes.Test.Effects.Deaths
             Cruiser cruiser = FindObjectOfType<Cruiser>();
             Assert.IsNotNull(cruiser);
 
-            //ICruiserArgs cruiserArgs
-            //    = new CruiserArgs(
-            //        Buildables.Faction.Reds,
-            //        enemyCruiser: Substitute.For<ICruiser>(),
-            //        uiManager: Substitute.For<IUIManager>(),
-            //        droneManager: Substitute.For<IDroneManager>(),
-            //        droneFocuser: Substitute.For<IDroneFocuser>(),
-            //        droneConsumerProvider: Substitute.For<IDroneConsumerProvider>(),
-            //        factoryProvider: Substitute.For<IFactoryProvider>(),
-            //        facingDirection: Direction.Right,
-            //        // FELIX  NEXT  Doesn't use interface, uses RepairManager concrete type :/
-            //        repairManager: Substitute.For<IRepairManager>(),
-            //        fogStrength: BattleCruisers.Cruisers.Fog.FogStrength.Weak,
-            //        helper: Substitute.For<ICruiserHelper>(),
-            //        highlightableFilter: Substitute.For<ISlotFilter>(),
-            //        buildProgressCalculator: Substitute.For<IBuildProgressCalculator>(),
-            //        buildingDoubleClickHandler,
-            //        cruiserDoubleClickHandler,
-            //        fogOfWarManager);
+            IFactoryProvider factoryProvider = Substitute.For<IFactoryProvider>();
+            GlobalBoostProviders globalBoostProviders = new GlobalBoostProviders();
+            factoryProvider.GlobalBoostProviders.Returns(globalBoostProviders);
+
+            ICruiserArgs cruiserArgs
+                = new CruiserArgs(
+                    Buildables.Faction.Reds,
+                    enemyCruiser: Substitute.For<ICruiser>(),
+                    uiManager: Substitute.For<IUIManager>(),
+                    droneManager: Substitute.For<IDroneManager>(),
+                    droneFocuser: Substitute.For<IDroneFocuser>(),
+                    droneConsumerProvider: Substitute.For<IDroneConsumerProvider>(),
+                    factoryProvider: factoryProvider,
+                    facingDirection: Direction.Right,
+                    repairManager: Substitute.For<IRepairManager>(),
+                    fogStrength: BattleCruisers.Cruisers.Fog.FogStrength.Weak,
+                    helper: Substitute.For<ICruiserHelper>(),
+                    highlightableFilter: Substitute.For<ISlotFilter>(),
+                    buildProgressCalculator: Substitute.For<IBuildProgressCalculator>(),
+                    buildingDoubleClickHandler: Substitute.For<IDoubleClickHandler<IBuilding>>(),
+                    cruiserDoubleClickHandler: Substitute.For<IDoubleClickHandler<ICruiser>>(),
+                    fogOfWarManager: Substitute.For<BCUtils.IManagedDisposable>());
+
+            cruiser.StaticInitialise();
+            cruiser.Initialise(cruiserArgs);
         }
     }
 }
