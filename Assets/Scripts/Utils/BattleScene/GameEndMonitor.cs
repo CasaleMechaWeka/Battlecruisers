03fdf280457488c2d69de2a9f730795ac5abc9d5
Felix Mann <felix.fei.mann@gmail.com>
2019-03-23 15:28:25 -0700

    Create GameEndMonitor :)

---
+++ Assets/Scripts/Utils/BattleScene/GameEndMonitor.cs
@@ -0,0 +1,48 @@
+﻿using BattleCruisers.Cruisers;
+using System;
+
+namespace BattleCruisers.Utils.BattleScene
+{
+    /// <summary>
+    /// The game can be ended in two ways:
+    /// 1. A cruiser is destroyed
+    /// 2. The user quits
+    /// </summary>
+    /// FELIX  Test
+    public class GameEndMonitor : IGameEndMonitor
+    {
+        private readonly ICruiserDestroyedMonitor _cruiserDestroyedMonitor;
+        private readonly IBattleCompletionHandler _battleCompletionHandler;
+
+        public event EventHandler GameEnded;
+
+        public GameEndMonitor(ICruiserDestroyedMonitor cruiserDestroyedMonitor, IBattleCompletionHandler battleCompletionHandler)
+        {
+            Helper.AssertIsNotNull(cruiserDestroyedMonitor, battleCompletionHandler);
+
+            _cruiserDestroyedMonitor = cruiserDestroyedMonitor;
+            _cruiserDestroyedMonitor.CruiserDestroyed += _cruiserDestroyedMonitor_CruiserDestroyed;
+
+            _battleCompletionHandler = battleCompletionHandler;
+            _battleCompletionHandler.BattleCompleted += _battleCompletionHandler_BattleCompleted;
+        }
+
+        private void _cruiserDestroyedMonitor_CruiserDestroyed(object sender, EventArgs e)
+        {
+            OnGameEnded();
+        }
+
+        private void _battleCompletionHandler_BattleCompleted(object sender, EventArgs e)
+        {
+            OnGameEnded();
+        }
+
+        private void OnGameEnded()
+        {
+            _cruiserDestroyedMonitor.CruiserDestroyed -= _cruiserDestroyedMonitor_CruiserDestroyed;
+            _battleCompletionHandler.BattleCompleted -= _battleCompletionHandler_BattleCompleted;
+
+            GameEnded?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/BattleScene/IGameEndMonitor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BattleCruisers.Utils.BattleScene
+{
+    public interface IGameEndMonitor
+    {
+        event EventHandler GameEnded;
+    }
+}
