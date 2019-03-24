3bb7ad18af281ede1abc258bc305b5fcfb1f4e25
Felix Mann <felix.fei.mann@gmail.com>
2019-03-24 15:38:56 -0700

    Do not remove cruiser if it sinks to the remover level :)

--- Assets/Scripts/Utils/BattleScene/Remover.cs
+++ Assets/Scripts/Utils/BattleScene/Remover.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace BattleCruisers.Utils.BattleScene
 {
-	public class Remover : MonoBehaviour 
+    public class Remover : MonoBehaviour 
 	{
 		void OnTriggerEnter2D(Collider2D collider)
 		{
             IRemovable removable = collider.GetComponent<IRemovable>();
-            Assert.IsNotNull(removable);
-            removable.RemoveFromScene();
+
+            if (removable != null)
+            {
+                removable.RemoveFromScene();
+            }
 		}
 	}
 }
