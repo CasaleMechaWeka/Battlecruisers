291b1a1f8141e024b01a7b1dfddc0faf9718856a
Felix Mann <felix.fei.mann@gmail.com>
2019-03-24 17:11:49 -0700

    Create SpaceSpawnDecider :D

--- Assets/Scripts/Buildables/Buildings/Factories/Spawning/SpaceSpawnDecider.cs
+++ Assets/Scripts/Buildables/Buildings/Factories/Spawning/SpaceSpawnDecider.cs
@@ -4,43 +4,28 @@ using UnityEngine;
 
 namespace BattleCruisers.Buildables.Buildings.Factories.Spawning
 {
-    // FELIX  Replace with CompositeSpawnDecider :)
-    public class UnitSpawnDecider : IUnitSpawnDecider
+    /// <summary>
+    /// Checks whether there is enough room to spawn a unit, without
+    /// hitting any other unit.
+    /// </summary>
+    public class SpaceSpawnDecider : IUnitSpawnDecider
     {
         private readonly IFactory _factory;
         private readonly IUnitSpawnPositionFinder _unitSpawnPositionFinder;
-        private readonly IUnitSpawnTimer _unitSpawnTimer;
 
-        public const float MIN_BUILD_BREAK_IN_S = 0.5f;
         public const float SPAWN_RADIUS_MULTIPLIER = 1.2f;
 
-        public UnitSpawnDecider(IFactory factory, IUnitSpawnPositionFinder unitSpawnPositionFinder, IUnitSpawnTimer unitSpawnTimer)
+        public SpaceSpawnDecider(IFactory factory, IUnitSpawnPositionFinder unitSpawnPositionFinder)
         {
-            Helper.AssertIsNotNull(factory, unitSpawnPositionFinder, unitSpawnTimer);
+            Helper.AssertIsNotNull(factory, unitSpawnPositionFinder);
 
             _factory = factory;
             _unitSpawnPositionFinder = unitSpawnPositionFinder;
-            _unitSpawnTimer = unitSpawnTimer;
         }
 
         public bool CanSpawnUnit(IUnit unitToSpawn)
         {
             Helper.AssertIsNotNull(unitToSpawn);
-
-            // If the unit under construction is destroyed, do not want to immediately
-            // start buliding the next unit.  This avoids the factory being "protected"
-            // by instantly respawning in progress units.  Ignore if the unit under 
-            // construction was recently changed.
-            Logging.Verbose(Tags.FACTORY, $"{unitToSpawn}  Time since chosen: {_unitSpawnTimer.TimeSinceUnitWasChosenInS}  " +
-                $"Time since clear:  {_unitSpawnTimer.TimeSinceFactoryWasClearInS}");
-
-            if (_unitSpawnTimer.TimeSinceUnitWasChosenInS >= MIN_BUILD_BREAK_IN_S
-                && _unitSpawnTimer.TimeSinceFactoryWasClearInS <= MIN_BUILD_BREAK_IN_S)
-            {
-                Logging.Verbose(Tags.FACTORY, "Times mean false :)");
-
-                return false;
-            }
 
             Vector3 spawnPositionV3 = _unitSpawnPositionFinder.FindSpawnPosition(unitToSpawn);
             Vector2 spawnPositionV2 = new Vector2(spawnPositionV3.x, spawnPositionV3.y);
