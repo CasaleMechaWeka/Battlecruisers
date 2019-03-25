0fb1532049efa8e1b1633600cd0573e02c2f21b2
Felix Mann <felix.fei.mann@gmail.com>
2019-03-24 17:45:20 -0700

    Create PopulationLimitSpawnDecider :)

---
+++ Assets/Scripts/Buildables/Buildings/Factories/Spawning/PopulationLimitSpawnDecider.cs
@@ -0,0 +1,27 @@
+﻿using BattleCruisers.Buildables.Units;
+using BattleCruisers.Cruisers.Construction;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Factories.Spawning
+{
+    // FELIX  Test :)
+    public class PopulationLimitSpawnDecider : IUnitSpawnDecider
+    {
+        private readonly ICruiserBuildingMonitor _buildingMonitor;
+        private readonly int _populationLimit;
+
+        public PopulationLimitSpawnDecider(ICruiserBuildingMonitor buildingMonitor, int populationLimit)
+        {
+            Assert.IsNotNull(buildingMonitor);
+            Assert.IsTrue(populationLimit > 0);
+
+            _buildingMonitor = buildingMonitor;
+            _populationLimit = populationLimit;
+        }
+
+        public bool CanSpawnUnit(IUnit unitToSpawn)
+        {
+            return _buildingMonitor.AliveBuildings.Count < _populationLimit;
+        }
+    }
+}
