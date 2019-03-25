701a1ed13f9332859f5a6f0a20b3e0ba7e609f87
Felix Mann <felix.fei.mann@gmail.com>
2019-03-24 17:15:59 -0700

    Create CompositeSpawnDecider :)

---
+++ Assets/Scripts/Buildables/Buildings/Factories/Spawning/CompositeSpawnDecider.cs
@@ -0,0 +1,32 @@
+﻿using BattleCruisers.Buildables.Units;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Factories.Spawning
+{
+    // FELIX  Test :)
+    public class CompositeSpawnDecider : IUnitSpawnDecider
+    {
+        private readonly IUnitSpawnDecider[] _spawnDeciders;
+
+        public CompositeSpawnDecider(params IUnitSpawnDecider[] spawnDeciders)
+        {
+            Assert.IsNotNull(spawnDeciders);
+            Assert.IsTrue(spawnDeciders.Length > 0);
+
+            _spawnDeciders = spawnDeciders;
+        }
+
+        public bool CanSpawnUnit(IUnit unitToSpawn)
+        {
+            foreach (IUnitSpawnDecider decider in _spawnDeciders)
+            {
+                if (!decider.CanSpawnUnit(unitToSpawn))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
