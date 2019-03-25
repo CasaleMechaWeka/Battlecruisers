da400dae3f0f6fcf68b60076cbbecba958c35b25
Felix Mann <felix.fei.mann@gmail.com>
2019-03-24 17:24:57 -0700

    Create SpawnDeciderFactory :)

---
+++ Assets/Scripts/Buildables/Buildings/Factories/Spawning/ISpawnDeciderFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace BattleCruisers.Buildables.Buildings.Factories.Spawning
+{
+    public interface ISpawnDeciderFactory
+    {
+        IUnitSpawnDecider CreateAircraftSpawnDecider(IFactory factory);
+        IUnitSpawnDecider CreateNavalSpawnDecider(IFactory factory);
+    }
+}
---
+++ Assets/Scripts/Buildables/Buildings/Factories/Spawning/SpawnDeciderFactory.cs
@@ -0,0 +1,30 @@
+﻿using BattleCruisers.Utils.PlatformAbstractions;
+
+namespace BattleCruisers.Buildables.Buildings.Factories.Spawning
+{
+    public class SpawnDeciderFactory : ISpawnDeciderFactory
+    {
+        public IUnitSpawnDecider CreateAircraftSpawnDecider(IFactory factory)
+        {
+            return CreateSpawnDecider(factory, new AirFactorySpawnPositionFinder(factory));
+        }
+
+        public IUnitSpawnDecider CreateNavalSpawnDecider(IFactory factory)
+        {
+            return CreateSpawnDecider(factory, new NavalFactorySpawnPositionFinder(factory));
+        }
+
+        private IUnitSpawnDecider CreateSpawnDecider(IFactory factory, IUnitSpawnPositionFinder spawnPositionFinder)
+        {
+            return
+                new CompositeSpawnDecider(
+                    new SpaceSpawnDecider(
+                        factory,
+                        spawnPositionFinder),
+                    new CooldownSpawnDecider(
+                        new UnitSpawnTimer(
+                            factory,
+                            new TimeBC())));
+        }
+    }
+}
