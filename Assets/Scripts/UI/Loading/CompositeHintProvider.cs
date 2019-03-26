8ebcc0b127fd3be59749151679464946ff7d8dc7
Felix Mann <felix.fei.mann@gmail.com>
2019-03-25 17:54:24 -0700

    Start creating hint providers :)

---
+++ Assets/Scripts/UI/Loading/CompositeHintProvider.cs
@@ -0,0 +1,35 @@
+﻿using BattleCruisers.Data.Models;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.UI.Loading
+{
+    public class CompositeHintProvider : IHintProvider
+    {
+        private readonly IHintProvider _basicHints, _advancedHints;
+        private readonly IGameModel _gameModel;
+
+        public const int ADVANCED_HINT_LEVEL_REQUIREMENT = 7;
+
+        public CompositeHintProvider(IHintProvider basicHints, IHintProvider advancedHints, IGameModel gameModel)
+        {
+            Helper.AssertIsNotNull(basicHints, advancedHints, gameModel);
+
+            _basicHints = basicHints;
+            _advancedHints = advancedHints;
+            _gameModel = gameModel;
+        }
+
+        public string GetHint()
+        {
+            if (_gameModel.NumOfLevelsCompleted > ADVANCED_HINT_LEVEL_REQUIREMENT)
+            {
+                // FELIX :D  Randomly choose between basic and advanced hints
+                return "";
+            }
+            else
+            {
+                return _basicHints.GetHint();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Loading/HintProvider.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Utils;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Loading
+{
+    public class HintProvider : IHintProvider
+    {
+        private readonly IList<string> _hints;
+
+        public HintProvider(IList<string> hints)
+        {
+            Assert.IsNotNull(hints);
+            Assert.IsTrue(hints.Count > 0);
+
+            _hints = hints;
+        }
+
+        public string GetHint()
+        {
+            return _hints.Shuffle().First();
+        }
+    }
+}
