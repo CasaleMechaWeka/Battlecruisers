acf86d920d6844d4549c747dcdbdb3dc2ce6da1b
Felix Mann <felix.fei.mann@gmail.com>
2019-03-30 16:29:22 -0700

    Create ISystemInfo :)

---
+++ Assets/Scripts/Utils/PlatformAbstractions/ISystemInfo.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Utils.PlatformAbstractions
+{
+    public interface ISystemInfo
+    {
+        DeviceType DeviceType { get; }
+    }
+}
---
+++ Assets/Scripts/Utils/PlatformAbstractions/SystemInfoBC.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Utils.PlatformAbstractions
+{
+    public class SystemInfoBC : ISystemInfo
+    {
+        public DeviceType DeviceType => SystemInfo.deviceType;
+    }
+}
