b70b9feac7d57b2d44738dc5e849fcc167bb616f
Felix Mann <felix.fei.mann@gmail.com>
2019-03-30 17:37:36 -0700

    Create CompositeBroadcastingFilter :)

---
+++ Assets/Scripts/UI/Filters/CompositeBroadcastingFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Filters
+{
+    // FELIX  Test :)
+    public class CompositeBroadcastingFilter : IBroadcastingFilter
+    {
+        private readonly BroadcastingFilter[] _filters;
+
+        private bool _isMatch;
+        public bool IsMatch
+        {
+            get { return _isMatch; }
+            set
+            {
+                if (_isMatch != value)
+                {
+                    _isMatch = value;
+
+                    foreach (BroadcastingFilter filter in _filters)
+                    {
+                        filter.IsMatch = value;
+                    }
+
+                    PotentialMatchChange?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public event EventHandler PotentialMatchChange;
+
+        public CompositeBroadcastingFilter(bool isMatch, params BroadcastingFilter[] filters)
+        {
+            Assert.IsNotNull(filters);
+            Assert.IsTrue(filters.Length > 1);
+
+            IsMatch = isMatch;
+        }
+    }
+}
