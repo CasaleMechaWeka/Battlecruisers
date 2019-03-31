beb435ede336ef657e9572cdb725e9de3ec96d98
Felix Mann <felix.fei.mann@gmail.com>
2019-03-30 17:41:36 -0700

    Create CompositeBroadcastingFilter :/

---
+++ Assets/Scripts/UI/BattlScene/Navigation/NavigationPermitters.cs
@@ -0,0 +1,18 @@
+﻿using BattleCruisers.UI.Filters;
+
+namespace BattleCruisers.UI.BattleScene.Navigation
+{
+    public class NavigationPermitters
+    {
+        public CompositeBroadcastingFilter NavigationFilter { get; }
+        public BroadcastingFilter NavigationWheelFilter { get; }
+        public BroadcastingFilter ScrollWheelFilter { get; }
+
+        public NavigationPermitters()
+        {
+            NavigationWheelFilter = new BroadcastingFilter(isMatch: true);
+            ScrollWheelFilter = new BroadcastingFilter(isMatch: true);
+            NavigationFilter = new CompositeBroadcastingFilter(true, NavigationWheelFilter, ScrollWheelFilter);
+        }
+    }
+}
