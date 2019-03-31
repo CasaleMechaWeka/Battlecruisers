00c1c0a7ab6c467c29d4a3ca37759116560621db
Felix Mann <felix.fei.mann@gmail.com>
2019-03-31 08:04:05 -0700

    Create ScrollWheelStepsFactory :)

---
+++ Assets/Scripts/Tutorial/Steps/Factories/ScrollWheelStepsFactory.cs
@@ -0,0 +1,49 @@
+﻿using BattleCruisers.UI.Cameras;
+using BattleCruisers.UI.Filters;
+using BattleCruisers.Utils;
+using System.Collections.Generic;
+
+namespace BattleCruisers.Tutorial.Steps.Factories
+{
+    public class ScrollWheelStepsFactory : TutorialFactoryBase, ITutorialStepsFactory
+    {
+        private readonly IFeaturePermitterStepFactory _featurePermitterStepFactory;
+        private readonly IPermitter _scrollWheelPermitter;
+        private readonly IExplanationDismissableStepFactory _explanationDismissableStepFactory;
+        private readonly ICameraComponents _cameraComponents;
+
+        public ScrollWheelStepsFactory(
+            ITutorialStepArgsFactory argsFactory,
+            IFeaturePermitterStepFactory featurePermitterStepFactory,
+            IPermitter scrollWheelPermitter,
+            IExplanationDismissableStepFactory explanationDismissableStepFactory,
+            ICameraComponents cameraComponents) 
+            : base(argsFactory)
+        {
+            Helper.AssertIsNotNull(featurePermitterStepFactory, scrollWheelPermitter, explanationDismissableStepFactory, cameraComponents);
+
+            _featurePermitterStepFactory = featurePermitterStepFactory;
+            _scrollWheelPermitter = scrollWheelPermitter;
+            _explanationDismissableStepFactory = explanationDismissableStepFactory;
+            _cameraComponents = cameraComponents;
+        }
+
+        public IList<ITutorialStep> CreateSteps()
+        {
+            IList<ITutorialStep> steps = new List<ITutorialStep>();
+
+            // Enable scroll wheel
+            steps.Add(_featurePermitterStepFactory.CreateStep(_scrollWheelPermitter, enableFeature: true));
+
+            // Explain scroll wheel and encourage user to experiment
+            steps.Add(
+                _explanationDismissableStepFactory.CreateStepWithSecondaryButton(
+                    _argsFactory.CreateTutorialStepArgs("You can also use your mouse scroll wheel to look around.  (Click \"Done\" when you have had enough.)")));
+
+            // Disable scroll wheel
+            steps.Add(_featurePermitterStepFactory.CreateStep(_scrollWheelPermitter, enableFeature: false));
+
+            return steps;
+        }
+    }
+}
