8952e99a2b33041ed880dfb6b52c9540ce8f85d9
Felix Mann <felix.fei.mann@gmail.com>
2019-04-06 10:50:32 -0700

    Don't go to loadout screen by default :)

--- Assets/Scripts/Scenes/ScreensSceneGod.cs
+++ Assets/Scripts/Scenes/ScreensSceneGod.cs
@@ -84,7 +84,7 @@ namespace BattleCruisers.Scenes
             // TEMP  Go to specific screen :)
             //GoToSettingsScreen();
             //GoToLevelsScreen();
-            GoToLoadoutScreen();
+            //GoToLoadoutScreen();
         }
         
         private void GoToPostBattleScreen()
