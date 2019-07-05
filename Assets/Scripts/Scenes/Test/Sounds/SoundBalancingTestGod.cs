2d1423b32e891572364bf9b50f3ef4e8a54c23e6
Felix Mann <felix.fei.mann@gmail.com>
2019-07-05 11:20:17 -0700

    Create sound balancing test scene, to try and get sound effects sounding right (ie, you can actually hear them).  Flipping Unity update :P

---
+++ Assets/Scripts/Scenes/Test/Sounds/SoundBalancingTestGod.cs
@@ -0,0 +1,46 @@
+﻿using BattleCruisers.Data.Static;
+using BattleCruisers.UI.Music;
+using BattleCruisers.UI.Sound;
+using BattleCruisers.Utils.Fetchers;
+using BattleCruisers.Utils.PlatformAbstractions.UI;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.Sounds
+{
+    public class SoundBalancingTestGod : NavigationTestGod
+    {
+        private IMusicPlayer _musicPlayer;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            _musicPlayer = CreateMusicPlayer();
+            _musicPlayer.LevelMusicKey = SoundKeys.Music.Background.Kentient;
+        }
+
+        private IMusicPlayer CreateMusicPlayer()
+        {
+            AudioSource platformAudioSource = GetComponent<AudioSource>();
+            Assert.IsNotNull(platformAudioSource);
+            IAudioSource audioSource = new AudioSourceBC(platformAudioSource);
+
+            return
+                new MusicPlayer(
+                    new SingleSoundPlayer(
+                        new SoundFetcher(),
+                        audioSource));
+        }
+
+        public void PlayMusic()
+        {
+            _musicPlayer.PlayBattleSceneMusic();
+        }
+
+        public void StopMusic()
+        {
+            _musicPlayer.Stop();
+        }
+    }
+}
