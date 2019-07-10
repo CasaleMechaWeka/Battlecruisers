19ddf417c97be9b0a07a78167dccc46654bbd7ef
Felix Mann <felix.fei.mann@gmail.com>
2019-07-09 17:55:46 -0700

    Do not make clicking player cruiser navigate to cruiser in tutorial :)

---
+++ Assets/Scripts/Cruisers/Helpers/TutorialPlayerCruiserHelper.cs
@@ -0,0 +1,23 @@
+﻿using BattleCruisers.UI.BattleScene.Manager;
+using BattleCruisers.UI.BattleScene.Navigation;
+
+namespace BattleCruisers.Cruisers.Helpers
+{
+    public class TutorialPlayerCruiserHelper : CruiserHelper
+    {
+        public TutorialPlayerCruiserHelper(IUIManager uIManager, ICameraFocuser cameraFocuser)
+            : base(uIManager, cameraFocuser)
+        {
+        }
+
+        public override void FocusCameraOnCruiser()
+        {
+             // Disabled for tutorial
+        }
+
+        public override void ShowBuildingGroupButtons()
+        {
+            _uiManager.HideCurrentlyShownMenu();
+        }
+    }
+}
