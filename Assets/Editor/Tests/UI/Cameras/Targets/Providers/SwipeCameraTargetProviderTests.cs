28ee858953edfc8a7ac338af6b17724ad575a487
Felix Mann <felix.fei.mann@gmail.com>
2019-07-16 18:05:01 -0700

    Create test stub :P

---
+++ Assets/Editor/Tests/UI/Cameras/Targets/Providers/SwipeCameraTargetProviderTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using UnityEngine;
+using UnityAsserts = UnityEngine.Assertions;
+
+namespace BattleCruisers.Tests.UI.Cameras.Targets.Providers
+{
+    public class SwipeCameraTargetProviderTests
+    {
+        // FELIX
+        //private readonly IDragTracker _dragTracker;
+        //private readonly IScrollCalculator _scrollCalculator;
+        //private readonly IZoomCalculator _zoomCalculator;
+        //private readonly ICamera _camera;
+        //private readonly ICameraCalculator _cameraCalculator;
+        //private readonly IDirectionalZoom _directionalZoom;
+        //private readonly IScrollRecogniser _scrollRecogniser;
+
+
+        [SetUp]
+        public void TestSetup()
+        {
+            UnityAsserts.Assert.raiseExceptions = true;
+        }
+
+        [Test]
+        public void SweetTest()
+        {
+        }
+    }
+}
