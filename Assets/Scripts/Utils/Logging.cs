b9ae0aaa4b6a6bad1c83c7859dcf8ad1a33700da
Felix Mann <felix.fei.mann@gmail.com>
2019-07-19 17:28:57 -0700

    Enable all logging by default, so the Player.log has something useful :)

--- Assets/Scripts/Utils/Logging.cs
+++ Assets/Scripts/Utils/Logging.cs
@@ -88,9 +88,10 @@ namespace BattleCruisers.Utils
 	
 	public static class Logging
 	{
-		private const bool LOG_ALL = false;
-		//private const LoggingLevel LOG_LEVEL = LoggingLevel.Normal;
-		private const LoggingLevel LOG_LEVEL = LoggingLevel.Verbose;
+		private const bool LOG_ALL = true;
+        //private const bool LOG_ALL = false;
+        private const LoggingLevel LOG_LEVEL = LoggingLevel.Normal;
+        //private const LoggingLevel LOG_LEVEL = LoggingLevel.Verbose;
 
 		private static Dictionary<string, bool> _tagsToActiveness;
 		private static Dictionary<string, bool> TagsToActiveness
@@ -241,7 +242,7 @@ namespace BattleCruisers.Utils
 		private static void Log(LoggingLevel logLevel, string tag, string message)
 		{
 			if (LOG_LEVEL >= logLevel
-				&& (TagsToActiveness[tag] || LOG_ALL))
+				&& (LOG_ALL || TagsToActiveness[tag]))
 			{
 				string timestamp = DateTime.Now.ToString("hh:mm:ss.fff");
 				string fullMsg = $"{timestamp} - {tag}:  {message}";
