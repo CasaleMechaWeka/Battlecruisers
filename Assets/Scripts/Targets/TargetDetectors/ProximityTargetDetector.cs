d96e691bd765b30d9f7ca0e6545d2b326261f640
Felix Mann <felix.fei.mann@gmail.com>
2019-07-22 17:28:11 -0700

    Create ProximityTargetDetector :)

--- Assets/Scripts/Targets/TargetDetectors/ProximityTargetDetector.cs
+++ Assets/Scripts/Targets/TargetDetectors/ProximityTargetDetector.cs
@@ -1,6 +1,11 @@
-﻿using BattleCruisers.Cruisers.Construction;
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Cruisers.Construction;
+using BattleCruisers.Utils;
 using System;
+using System.Collections.Generic;
 using UnityCommon.PlatformAbstractions;
+using UnityEngine;
 
 namespace BattleCruisers.Targets.TargetDetectors
 {
@@ -11,13 +16,65 @@ namespace BattleCruisers.Targets.TargetDetectors
         private readonly ITransform _parentTransform;
         private readonly IUnitProvider _potentialTargets;
         private readonly float _detectionRange;
+        private readonly ISet<ITarget> _currentInRangeTargets, _newInRangeTargets;
 
         public event EventHandler<TargetEventArgs> TargetEntered;
         public event EventHandler<TargetEventArgs> TargetExited;
 
+        public ProximityTargetDetector(ITransform parentTransform, IUnitProvider potentialTargets, float detectionRange)
+        {
+            Helper.AssertIsNotNull(parentTransform, potentialTargets);
+
+            _parentTransform = parentTransform;
+            _potentialTargets = potentialTargets;
+            _detectionRange = detectionRange;
+            _currentInRangeTargets = new HashSet<ITarget>();
+            _newInRangeTargets = new HashSet<ITarget>();
+        }
+
         public void Detect()
         {
-            throw new NotImplementedException();
+            ISet<ITarget> newInRangeTargets = FindInRangeTargets();
+            Logging.Verbose(Tags.TARGET_DETECTOR, $"Current targets: {_currentInRangeTargets.Count}  new targets: {newInRangeTargets.Count}");
+
+            // Find targets that have left
+            foreach (ITarget currentInRangeTarget in _currentInRangeTargets)
+            {
+                if (!newInRangeTargets.Contains(currentInRangeTarget))
+                {
+                    Logging.Log(Tags.TARGET_DETECTOR, $"Lost target: {currentInRangeTarget}");
+
+                    _currentInRangeTargets.Remove(currentInRangeTarget);
+                    TargetExited?.Invoke(this, new TargetEventArgs(currentInRangeTarget));
+                }
+            }
+
+            // Find targets that are new
+            foreach (ITarget newInRangeTarget in newInRangeTargets)
+            {
+                if (!_currentInRangeTargets.Contains(newInRangeTarget))
+                {
+                    Logging.Log(Tags.TARGET_DETECTOR, $"Found new target: {newInRangeTarget}");
+
+                    _currentInRangeTargets.Add(newInRangeTarget);
+                    TargetEntered?.Invoke(this, new TargetEventArgs(newInRangeTarget));
+                }
+            }
+        }
+
+        private ISet<ITarget> FindInRangeTargets()
+        {
+            _newInRangeTargets.Clear();
+
+            foreach (IUnit potentialTarget in _potentialTargets.AliveUnits)
+            {
+                if (Vector2.Distance(potentialTarget.Transform.Position, _parentTransform.Position) <= _detectionRange)
+                {
+                    _newInRangeTargets.Add(potentialTarget);
+                }
+            }
+
+            return _newInRangeTargets;
         }
 
         public void StartDetecting()
