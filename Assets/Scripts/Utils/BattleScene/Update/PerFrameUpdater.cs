0f1758d79545f8ee3a5193d9783552dc145e3bc6
Felix Mann <felix.fei.mann@gmail.com>
2019-07-24 16:58:48 -0700

    Create PerFrameUpdater :)

---
+++ Assets/Scripts/Utils/BattleScene/Update/PerFrameUpdater.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace BattleCruisers.Utils.BattleScene.Update
+{
+    public class PerFrameUpdater : MonoBehaviour, IUpdater
+    {
+        public event EventHandler Updated;
+
+        void Update()
+        {
+            Updated?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
