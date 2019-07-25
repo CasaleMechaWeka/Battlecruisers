7d9c719d986b4257020a874ff384125ef1b16f44
Felix Mann <felix.fei.mann@gmail.com>
2019-07-24 17:06:27 -0700

    Create and test MultiFrameUpdater :)

---
+++ Assets/Editor/Tests/Utils/BattleScene/Update/MultiFrameUpdaterTests.cs
@@ -0,0 +1,56 @@
+﻿using BattleCruisers.Utils.BattleScene.Update;
+using NSubstitute;
+using NUnit.Framework;
+using UnityCommon.PlatformAbstractions;
+
+namespace BattleCruisers.Tests.Utils.BattleScene.Update
+{
+    public class MultiFrameUpdaterTests
+    {
+        private IUpdater _multiFrameUpdater, _perFrameUpdater;
+        private IDeltaTimeProvider _timeProvider;
+        private float _intervalInS = 0.2f;
+        private int _updatedCount;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _perFrameUpdater = Substitute.For<IUpdater>();
+            _timeProvider = Substitute.For<IDeltaTimeProvider>();
+
+            _multiFrameUpdater = new MultiFrameUpdater(_perFrameUpdater, _timeProvider, _intervalInS);
+
+            _updatedCount = 0;
+            _multiFrameUpdater.Updated += (sender, e) => _updatedCount++;
+
+            _timeProvider.DeltaTime.Returns(0.1f);
+        }
+
+        [Test]
+        public void Updated_NotEnoughTimeHasPassed_DoesNothing()
+        {
+            _perFrameUpdater.Updated += Raise.Event();
+            Assert.AreEqual(0, _updatedCount);
+        }
+
+        [Test]
+        public void Updated_EnoughTimeHasPassed_EmitsEvent()
+        {
+            _perFrameUpdater.Updated += Raise.Event();
+            _perFrameUpdater.Updated += Raise.Event();
+            Assert.AreEqual(1, _updatedCount);
+        }
+
+        [Test]
+        public void Updated_ResetsTimeWaited()
+        {
+            _perFrameUpdater.Updated += Raise.Event();
+            _perFrameUpdater.Updated += Raise.Event();
+            Assert.AreEqual(1, _updatedCount);
+
+            // Internal count is reset, another event is not raised
+            _perFrameUpdater.Updated += Raise.Event();
+            Assert.AreEqual(1, _updatedCount);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/BattleScene/Update/MultiFrameUpdater.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityCommon.PlatformAbstractions;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.BattleScene.Update
+{
+    /// <summary>
+    /// Emits the Updated event during the first frame after the specified amount of time.
+    /// </summary>
+    /// FELIX  Use
+    public class MultiFrameUpdater : IUpdater
+    {
+        private readonly IDeltaTimeProvider _timeProvider;
+        private readonly float _intervalInS;
+        private float _timeSinceUpdateInS;
+
+        public event EventHandler Updated;
+
+        public MultiFrameUpdater(IUpdater perFrameUpdater, IDeltaTimeProvider timeProvider, float intervalInS)
+        {
+            Helper.AssertIsNotNull(perFrameUpdater, timeProvider);
+            Assert.IsTrue(intervalInS > 0);
+
+            _timeProvider = timeProvider;
+            _intervalInS = intervalInS;
+            _timeSinceUpdateInS = 0;
+
+            perFrameUpdater.Updated += PerFrameUpdater_Updated;
+        }
+
+        private void PerFrameUpdater_Updated(object sender, EventArgs e)
+        {
+            _timeSinceUpdateInS += _timeProvider.DeltaTime;
+
+            if (_timeSinceUpdateInS >= _intervalInS)
+            {
+                Updated?.Invoke(this, EventArgs.Empty);
+                _timeSinceUpdateInS = 0;
+            }
+        }
+    }
+}
