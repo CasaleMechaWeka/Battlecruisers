b959dfd12bbcc1ffbc90305727ad5d7d86faa9bc
Felix Mann <felix.fei.mann@gmail.com>
2019-07-25 16:27:04 -0700

    Create ManualDetectorPoller :)

---
+++ Assets/Editor/Tests/Targets/TargetDetectors/ManualDetectorPollerTests.cs
@@ -0,0 +1,30 @@
+﻿using BattleCruisers.Targets.TargetDetectors;
+using BattleCruisers.Utils.BattleScene.Update;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.Targets.TargetDetectors
+{
+    public class ManualDetectorPollerTests
+    {
+        private ManualDetectorPoller _detectorPoller;
+        private IManualDetector _manualDetector;
+        private IUpdater _updater;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _manualDetector = Substitute.For<IManualDetector>();
+            _updater = Substitute.For<IUpdater>();
+
+            _detectorPoller = new ManualDetectorPoller(_manualDetector, _updater);
+        }
+
+        [Test]
+        public void Updated_TriggersDetect()
+        {
+            _updater.Updated += Raise.Event();
+            _manualDetector.Received().Detect();
+        }
+    }
+}
---
+++ Assets/Scripts/Targets/TargetDetectors/ManualDetectorPoller.cs
@@ -0,0 +1,27 @@
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.BattleScene.Update;
+using System;
+
+namespace BattleCruisers.Targets.TargetDetectors
+{
+    public class ManualDetectorPoller
+    {
+        private readonly IManualDetector _manualDetector;
+        private readonly IUpdater _updater;
+
+        public ManualDetectorPoller(IManualDetector manualDetector, IUpdater updater)
+        {
+            Helper.AssertIsNotNull(manualDetector, updater);
+
+            _manualDetector = manualDetector;
+            _updater = updater;
+
+            _updater.Updated += _updater_Updated;
+        }
+
+        private void _updater_Updated(object sender, EventArgs e)
+        {
+            _manualDetector.Detect();
+        }
+    }
+}
