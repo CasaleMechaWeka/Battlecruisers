8280204fe90e30d9b3de14691201a24dc34dd656
Felix Mann <felix.fei.mann@gmail.com>
2019-07-25 16:56:52 -0700

    Create UpdaterProvider :)

---
+++ Assets/Scripts/Utils/BattleScene/Update/UpdaterProvider.cs
@@ -0,0 +1,22 @@
+﻿using UnityCommon.PlatformAbstractions;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.BattleScene.Update
+{
+    public class UpdaterProvider : MonoBehaviour
+    {
+        private IUpdater _perFrameUpdater;
+
+        private const float SLOWER_UPDATER_INTERVAL_IN_S = 0.2f;
+        public IUpdater SlowerUpdater { get; private set; }
+        
+        public void Initialise()
+        {
+            _perFrameUpdater = GetComponent<PerFrameUpdater>();
+            Assert.IsNotNull(_perFrameUpdater);
+
+            SlowerUpdater = new MultiFrameUpdater(_perFrameUpdater, new TimeBC(), SLOWER_UPDATER_INTERVAL_IN_S);
+        }
+    }
+}
