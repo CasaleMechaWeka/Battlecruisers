3ffbd82e52bea021e678d56da913fd3433b3e3f3
Felix Mann <felix.fei.mann@gmail.com>
2019-07-28 14:22:07 -0700

    Trying to fix FactoryTestGod after switching to manual target detectors :/

--- Assets/Scripts/Scenes/Test/Factories/FactoryTestGod.cs
+++ Assets/Scripts/Scenes/Test/Factories/FactoryTestGod.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using BattleCruisers.Buildables;
+﻿using BattleCruisers.Buildables;
 using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Buildables.Units;
+using BattleCruisers.Cruisers;
 using BattleCruisers.Scenes.Test.Utilities;
-using UnityEngine;
+using System;
 
 namespace BattleCruisers.Scenes.Test.Factories
 {
-    public class FactoryTestGod : MonoBehaviour 
+    public class FactoryTestGod : TestGodBase
 	{
 		public Factory factoryFacingRight, factoryFacingLeft;
 		public UnitWrapper unitPrefab;
@@ -15,14 +15,19 @@ namespace BattleCruisers.Scenes.Test.Factories
 		protected virtual Faction FactoryFacingLeftFaction => Faction.Blues;
 		protected virtual Faction FactoryFacingRightFaction => Faction.Blues;
 
-		void Start()
+		protected override void Start()
 		{
+            base.Start();
+
 			unitPrefab.Initialise();
 
-			Helper helper = new Helper();
+			Helper helper = new Helper(updaterProvider: _updaterProvider);
 
-            helper.InitialiseBuilding(factoryFacingRight, FactoryFacingRightFaction, parentCruiserDirection: Direction.Right);
-            helper.InitialiseBuilding(factoryFacingLeft, FactoryFacingLeftFaction, parentCruiserDirection: Direction.Left);
+            ICruiser leftCruiser = helper.CreateCruiser(Direction.Right, FactoryFacingRightFaction);
+            ICruiser rightCruiser = helper.CreateCruiser(Direction.Left, FactoryFacingLeftFaction);
+
+            helper.InitialiseBuilding(factoryFacingRight, FactoryFacingRightFaction, parentCruiserDirection: leftCruiser.Direction, enemyCruiser: rightCruiser);
+            helper.InitialiseBuilding(factoryFacingLeft, FactoryFacingLeftFaction, parentCruiserDirection: rightCruiser.Direction, enemyCruiser: leftCruiser);
 
 			factoryFacingRight.CompletedBuildable += Factory_CompletedBuildable;
 			factoryFacingLeft.CompletedBuildable += Factory_CompletedBuildable;
@@ -30,7 +35,10 @@ namespace BattleCruisers.Scenes.Test.Factories
 			factoryFacingRight.StartConstruction();
 			factoryFacingLeft.StartConstruction();
 
-			OnStart();
+            Helper.SetupFactoryForUnitMonitor(factoryFacingRight, rightCruiser);
+            Helper.SetupFactoryForUnitMonitor(factoryFacingLeft, leftCruiser);
+
+            OnStart();
 		}
 
 		protected virtual void OnStart() { }
