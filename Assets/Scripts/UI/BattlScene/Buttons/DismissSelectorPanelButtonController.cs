84fc9bfc1a3f762025b35eb0fa32af04f5ef7845
Felix Mann <felix.fei.mann@gmail.com>
2019-07-30 19:09:02 -0700

    Add close image to selector panel :)

--- Assets/Scripts/UI/BattlScene/Buttons/DismissSelectorPanelButtonController.cs
+++ Assets/Scripts/UI/BattlScene/Buttons/DismissSelectorPanelButtonController.cs
@@ -1,24 +1,31 @@
 ﻿using BattleCruisers.UI.BattleScene.Manager;
 using BattleCruisers.UI.Filters;
 using BattleCruisers.Utils;
-using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace BattleCruisers.UI.BattleScene.Buttons
 {
-    public class DismissSelectorPanelButtonController : Togglable, IPointerClickHandler
+    public class DismissSelectorPanelButtonController : ClickableTogglable
 	{
         private IUIManager _uiManager;
         private FilterToggler _isEnabledToggler;
 
+        private Image _closeImage;
+        protected override MaskableGraphic Graphic => _closeImage;
+
         public void Initialise(IUIManager uiManager, IBroadcastingFilter shouldBeEnabledFilter)
 		{
+            base.Initialise();
+
             Helper.AssertIsNotNull(uiManager, shouldBeEnabledFilter);
 
             _uiManager = uiManager;
             _isEnabledToggler = new FilterToggler(this, shouldBeEnabledFilter);
+
+            _closeImage = transform.FindNamedComponent<Image>("CloseImage");
 		}
 
-        public void OnPointerClick(PointerEventData eventData)
+        protected override void OnClicked()
         {
             _uiManager.HideCurrentlyShownMenu();
         }
