b063ed5cb5b5be575349ada90c960afd3f1ba1e0
Felix Mann <felix.fei.mann@gmail.com>
2019-08-03 11:03:48 -0700

    Create bomber performance test scene :D

---
+++ Assets/Scripts/Scenes/Test/Performance/BomberPerformanceTestGod.cs
@@ -0,0 +1,58 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Buildables.Units.Aircraft.Providers;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Scenes.Test.Balancing.Groups;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Targets.Factories;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Utils.Fetchers;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Performance
+{
+    public class BomberPerformanceTestGod : TestGodBase
+    {
+        public List<Vector2> bomberPatrolPoints;
+        public Vector2 spawnPosition;
+        public UnitWrapper aircraftPrefab;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            aircraftPrefab.Initialise();
+
+            Helper helper = new Helper(updaterProvider: _updaterProvider);
+
+            ICruiser redCruiser = helper.CreateCruiser(Direction.Left, Faction.Reds);
+            ICruiser blueCruiser = helper.CreateCruiser(Direction.Right, Faction.Blues);
+
+            // Setup target
+            AirFactory factory = FindObjectOfType<AirFactory>();
+            helper.InitialiseBuilding(factory, Faction.Reds);
+
+            IList<TargetType> targetTypes = new List<TargetType>() { factory.TargetType };
+            ITargetFilter targetFilter = new FactionAndTargetTypeFilter(factory.Faction, targetTypes);
+            ITargetFactoriesProvider targetFactories = helper.CreateTargetFactories(factory.GameObject, targetFilter);
+
+            // Setup bombers
+            IAircraftProvider aircraftProvider = helper.CreateAircraftProvider(bomberPatrolPoints: bomberPatrolPoints);
+            IPrefabFactory prefabFactory = new PrefabFactory(new PrefabFetcher());
+
+            BuildableGroupController bombersGroup = FindObjectOfType<BuildableGroupController>();
+            BuildableInitialisationArgs groupArgs
+                = new BuildableInitialisationArgs(
+                    helper,
+                    Faction.Blues,
+                    aircraftProvider: aircraftProvider,
+                    updaterProvider: _updaterProvider,
+                    enemyCruiser: redCruiser,
+                    parentCruiser: blueCruiser,
+                    targetFactories: targetFactories);
+            bombersGroup.Initialise(prefabFactory, helper, groupArgs, spawnPosition);
+        }
+    }
+}
