153fa94f8c702df488bfc9c50e1ced4e8fe665d9
Felix Mann <felix.fei.mann@gmail.com>
2019-08-11 11:35:56 -0700

    Create Pool :)

---
+++ Assets/Scripts/Utils/BattleScene/Pools/Pool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.BattleScene.Pools
+{
+    // FELIX  Use
+    // FELIX  Test
+    // FELIX  Have initial capacity?  Need to deactivate items...  But they have not been Activated/Initialised?
+    public class Pool<TArgs> : IPool<TArgs>
+    {
+        private readonly Stack<IPoolable<TArgs>> _items;
+        private readonly IPoolableFactory<TArgs> _itemFactory;
+
+        public Pool(IPoolableFactory<TArgs> itemFactory)
+        {
+            Assert.IsNotNull(itemFactory);
+
+            _itemFactory = itemFactory;
+            _items = new Stack<IPoolable<TArgs>>();
+        }
+
+        public IPoolable<TArgs> GetItem(TArgs initialisationArgs)
+        {
+            IPoolable<TArgs> item = _items.Count != 0 ? _items.Pop() : _itemFactory.CreateItem();
+            item.Activate(initialisationArgs);
+            return item;
+        }
+
+        public void ReleaseItem(IPoolable<TArgs> itemToRelease)
+        {
+            itemToRelease.Deactivate();
+            _items.Push(itemToRelease);
+        }
+    }
+}
