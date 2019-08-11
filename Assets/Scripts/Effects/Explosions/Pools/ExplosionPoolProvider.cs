6c784408e4612a390f9d2aa47414009204b4850c
Felix Mann <felix.fei.mann@gmail.com>
2019-08-11 16:16:31 -0700

    Implement ExplosionPoolProvider :)

--- Assets/Scripts/Effects/Explosions/Pools/ExplosionPoolProvider.cs
+++ Assets/Scripts/Effects/Explosions/Pools/ExplosionPoolProvider.cs
@@ -1,17 +1,48 @@
-﻿using BattleCruisers.Utils.BattleScene.Pools;
+﻿using BattleCruisers.Data.Static;
+using BattleCruisers.Utils.BattleScene.Pools;
+using BattleCruisers.Utils.Fetchers;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace BattleCruisers.Effects.Explosions.Pools
 {
-    // FELIX  Implement
     public class ExplosionPoolProvider : IExplosionPoolProvider
     {
-        public IPool<Vector3> SmallExplosionsPool => throw new System.NotImplementedException();
+        public IPool<Vector3> SmallExplosionsPool { get; }
 
-        public IPool<Vector3> MediumExplosionsPool => throw new System.NotImplementedException();
+        public IPool<Vector3> MediumExplosionsPool { get; }
 
-        public IPool<Vector3> LargeExplosionsPool => throw new System.NotImplementedException();
+        public IPool<Vector3> LargeExplosionsPool { get; }
 
-        public IPool<Vector3> HugeExplosionsPool => throw new System.NotImplementedException();
+        public IPool<Vector3> HugeExplosionsPool { get; }
+
+        public ExplosionPoolProvider(IPrefabFactory prefabFactory)
+        {
+            Assert.IsNotNull(prefabFactory);
+
+            SmallExplosionsPool
+                = new Pool<Vector3>(
+                    new ExplosionFactory(
+                        prefabFactory,
+                        StaticPrefabKeys.Explosions.HDExplosion75));
+
+            MediumExplosionsPool
+                = new Pool<Vector3>(
+                    new ExplosionFactory(
+                        prefabFactory,
+                        StaticPrefabKeys.Explosions.HDExplosion100));
+
+            LargeExplosionsPool
+                = new Pool<Vector3>(
+                    new ExplosionFactory(
+                        prefabFactory,
+                        StaticPrefabKeys.Explosions.HDExplosion150));
+
+            HugeExplosionsPool
+                = new Pool<Vector3>(
+                    new ExplosionFactory(
+                        prefabFactory,
+                        StaticPrefabKeys.Explosions.HDExplosion500));
+        }
     }
 }
