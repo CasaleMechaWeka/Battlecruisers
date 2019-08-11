676a7e1cde818e35bf04531d232adfb1d2f1e18c
Felix Mann <felix.fei.mann@gmail.com>
2019-08-11 16:55:46 -0700

    Create IExplosionPoolChooser and implementations :)

---
+++ Assets/Scripts/Effects/Explosions/Pools/IExplosionPoolChooser.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Utils.BattleScene.Pools;
+using UnityEngine;
+
+namespace BattleCruisers.Effects.Explosions.Pools
+{
+    public interface IExplosionPoolChooser
+    {
+        IPool<Vector3> ChoosePool(IExplosionPoolProvider explosionPoolProvider);
+    }
+}
---
+++ Assets/Scripts/Effects/Explosions/Pools/LargeExplosionPoolChooser.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Utils.BattleScene.Pools;
+using UnityEngine;
+
+namespace BattleCruisers.Effects.Explosions.Pools
+{
+    public class LargeExplosionPoolChooser : MonoBehaviour, IExplosionPoolChooser
+    {
+        public IPool<Vector3> ChoosePool(IExplosionPoolProvider explosionPoolProvider)
+        {
+            return explosionPoolProvider.LargeExplosionsPool;
+        }
+    }
+}
