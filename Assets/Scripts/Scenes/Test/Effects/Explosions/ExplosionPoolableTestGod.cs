addf871a0bf525ebba8e7983249aa7856a9fb276
Felix Mann <felix.fei.mann@gmail.com>
2019-08-11 15:37:19 -0700

    Explosion Activate() and Deactivated event seem to work O_o

---
+++ Assets/Scripts/Scenes/Test/Effects/Explosions/ExplosionPoolableTestGod.cs
@@ -0,0 +1,22 @@
+﻿using BattleCruisers.Effects.Explosions;
+using BattleCruisers.Utils;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Effects.Explosions
+{
+    public class ExplosionPoolableTestGod : MonoBehaviour
+    {
+        void Start()
+        {
+            AdvancedExplosion[] explosions = FindObjectsOfType<AdvancedExplosion>();
+            IRandomGenerator random = new RandomGenerator();
+
+            foreach (AdvancedExplosion explosion in explosions)
+            {
+                explosion.Initialise(random);
+                explosion.Activate(explosion.transform.position);
+                explosion.Deactivated += (sender, e) => explosion.Activate(explosion.transform.position);
+            }
+        }
+    }
+}
