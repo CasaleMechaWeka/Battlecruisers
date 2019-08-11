ecfcf175170e916debc3b85a34d1ae9c554a6d63
Felix Mann <felix.fei.mann@gmail.com>
2019-08-11 16:04:24 -0700

    Create ExplosionPoolProvider stub :)

---
+++ Assets/Scripts/Effects/Explosions/Pools/ExplosionPoolProvider.cs
@@ -0,0 +1,17 @@
+﻿using BattleCruisers.Utils.BattleScene.Pools;
+using UnityEngine;
+
+namespace BattleCruisers.Effects.Explosions
+{
+    // FELIX  Implement
+    public class ExplosionPoolProvider : IExplosionPoolProvider
+    {
+        public IPool<Vector3> SmallExplosionsPool => throw new System.NotImplementedException();
+
+        public IPool<Vector3> MediumExplosionsPool => throw new System.NotImplementedException();
+
+        public IPool<Vector3> LargeExplosionsPool => throw new System.NotImplementedException();
+
+        public IPool<Vector3> HugeExplosionsPool => throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Effects/Explosions/Pools/IExplosionPoolProvider.cs
@@ -0,0 +1,20 @@
+﻿using BattleCruisers.Utils.BattleScene.Pools;
+using UnityEngine;
+
+namespace BattleCruisers.Effects.Explosions
+{
+    public interface IExplosionPoolProvider
+    {
+        // Radius 0.75m => Bomber, mortar, SAM site
+        IPool<Vector3> SmallExplosionsPool { get; }
+        
+        // Radius 1m    => Destroyer missiles, archon front launcher missiles
+        IPool<Vector3> MediumExplosionsPool { get; }
+        
+        // Radius 1.5m  => Artillery, rocket launcher, broadsides, archon back missile launcher, archon primary cannon
+        IPool<Vector3> LargeExplosionsPool { get; }
+        
+        // Radius 5m    => Nuke
+        IPool<Vector3> HugeExplosionsPool { get; }
+    }
+}
