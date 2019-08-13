d1ee4d92352699d35e6c0cf1cedbe9c48d3544fe
Felix Mann <felix.fei.mann@gmail.com>
2019-08-12 17:01:09 -0700

    Create PoolTests :)

---
+++ Assets/Editor/Tests/Utils/BattleScene/Pools/PoolTests.cs
@@ -0,0 +1,53 @@
+﻿using BattleCruisers.Utils.BattleScene.Pools;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BattleCruisers.Tests.Utils.BattleScene.Pools
+{
+    public class PoolTests
+    {
+        private IPool<int> _pool;
+        private IPoolableFactory<int> _itemFactory;
+        private IPoolable<int> _item;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _itemFactory = Substitute.For<IPoolableFactory<int>>();
+
+            _pool = new Pool<int>(_itemFactory);
+
+            _item = Substitute.For<IPoolable<int>>();
+            _itemFactory.CreateItem().Returns(_item);
+        }
+
+        [Test]
+        public void GetItem_NoStoredItems_CreatesItem()
+        {
+            IPoolable<int> returnedItem = _pool.GetItem(7);
+
+            Assert.AreSame(_item, returnedItem);
+            _itemFactory.Received().CreateItem();
+            _item.Received().Activate(7);
+        }
+
+        [Test]
+        public void GetItem_HasStoredItems_ReturnsStoredItem()
+        {
+            // First retrieval => create item
+            IPoolable<int> firstItem = _pool.GetItem(7);
+
+            // Item deactivated => store item in pool
+            firstItem.Deactivated += Raise.Event();
+
+            // Second retrieval => returns stored item
+            _itemFactory.ClearReceivedCalls();
+
+            IPoolable<int> secondItem = _pool.GetItem(12);
+
+            Assert.AreSame(firstItem, secondItem);
+            _itemFactory.DidNotReceive().CreateItem();
+            _item.Received().Activate(12);
+        }
+    }
+}
