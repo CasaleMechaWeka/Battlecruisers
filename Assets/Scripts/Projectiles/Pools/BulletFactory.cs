9dd24d2885e830b13762761b85c628304c8464a6
Felix Mann <felix.fei.mann@gmail.com>
2019-08-14 16:46:07 -0700

    Dang it, PoolableFactory needs to take parameters :/

---
+++ Assets/Scripts/Projectiles/Pools/BulletFactory.cs
@@ -0,0 +1,20 @@
+﻿using BattleCruisers.Projectiles.ActivationArgs;
+using BattleCruisers.Projectiles.Stats;
+using BattleCruisers.Utils.BattleScene.Pools;
+
+namespace BattleCruisers.Projectiles.Pools
+{
+    public class BulletFactory : IPoolableFactory<ProjectileActivationArgs<ProjectileStats>>
+    {
+        private readonly IProjectileFactory _projectileFactory;
+
+        // FELIX  Constructor :/
+
+        public IPoolable<ProjectileActivationArgs<ProjectileStats>> CreateItem()
+        {
+            // FELIX  Dang :/
+            //_projectileFactory.CreateBullet()
+                return null;
+        }
+    }
+}
