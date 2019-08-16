1ee243d55ee01902e50279fa430a32c08ada33fe
Felix Mann <felix.fei.mann@gmail.com>
2019-08-16 16:38:43 -0700

    Undo TODO, more pain than it's worth :P

--- Assets/Scripts/Projectiles/DamageAppliers/IDamageApplier.cs
+++ Assets/Scripts/Projectiles/DamageAppliers/IDamageApplier.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 namespace BattleCruisers.Projectiles.DamageAppliers
 {
-    // FELIX  Make both implementations have no state, so the same damage applier
-    // can be reused by a projectile, even if the projectile stats (damage, range, etc) have changed :)
 	public interface IDamageApplier
 	{
         void ApplyDamage(ITarget target, Vector2 collisionPoint, ITarget damageSource);
