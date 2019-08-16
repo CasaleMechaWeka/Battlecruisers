815a0712fe590b03d5d87b6a865d5456fda4c3fb
Felix Mann <felix.fei.mann@gmail.com>
2019-08-15 17:51:33 -0700

    Reorder methods :P

--- Assets/Scripts/Projectiles/Pools/IProjectilePoolProvider.cs
+++ Assets/Scripts/Projectiles/Pools/IProjectilePoolProvider.cs
@@ -6,13 +6,13 @@ namespace BattleCruisers.Projectiles.Pools
 {
     public interface IProjectilePoolProvider
     {
-        IPool<ProjectileActivationArgs<IProjectileStats>> BombsPool { get; }
         IPool<ProjectileActivationArgs<IProjectileStats>> BulletsPool { get; }
-        IPool<TargetProviderActivationArgs<IProjectileStats>> MissilesLargePool { get; }
-        IPool<TargetProviderActivationArgs<IProjectileStats>> MissilesMediumPool { get; }
-        IPool<TargetProviderActivationArgs<IProjectileStats>> MissilesSmallPool { get; }
-        IPool<TargetProviderActivationArgs<ICruisingProjectileStats>> RocketsPool { get; }
         IPool<ProjectileActivationArgs<IProjectileStats>> ShellsLargePool { get; }
         IPool<ProjectileActivationArgs<IProjectileStats>> ShellsSmallPool { get; }
+        IPool<ProjectileActivationArgs<IProjectileStats>> BombsPool { get; }
+        IPool<TargetProviderActivationArgs<ICruisingProjectileStats>> RocketsPool { get; }
+        IPool<TargetProviderActivationArgs<IProjectileStats>> MissilesSmallPool { get; }
+        IPool<TargetProviderActivationArgs<IProjectileStats>> MissilesMediumPool { get; }
+        IPool<TargetProviderActivationArgs<IProjectileStats>> MissilesLargePool { get; }
     }
 }
