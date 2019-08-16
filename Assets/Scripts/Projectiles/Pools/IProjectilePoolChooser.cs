2ed5ac2551a75c1fcede32f4da91c18b4c147194
Felix Mann <felix.fei.mann@gmail.com>
2019-08-15 18:04:09 -0700

    Create first projectile pool chooser :)

---
+++ Assets/Scripts/Projectiles/Pools/IProjectilePoolChooser.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Projectiles.ActivationArgs;
+using BattleCruisers.Projectiles.Stats;
+using BattleCruisers.Utils.BattleScene.Pools;
+
+namespace BattleCruisers.Projectiles.Pools
+{
+    public interface IProjectilePoolChooser<TActivationArgs, TStats>
+        where TActivationArgs : ProjectileActivationArgs<TStats>
+        where TStats : IProjectileStats
+    {
+        IPool<TActivationArgs> ChoosePool(IProjectilePoolProvider projectilePoolProvider);
+    }
+}
