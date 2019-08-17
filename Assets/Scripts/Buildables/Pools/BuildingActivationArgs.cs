461a0b9f1a8921907c1cdb526c6473b7e98107ee
Felix Mann <felix.fei.mann@gmail.com>
2019-08-17 16:48:04 -0700

    Create BuildingActivationArgs :)

---
+++ Assets/Scripts/Buildables/Pools/BuildingActivationArgs.cs
@@ -0,0 +1,29 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Cruisers.Slots;
+using BattleCruisers.UI.Common.Click;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Factories;
+
+namespace BattleCruisers.Buildables.ActivationArgs
+{
+    public class BuildingActivationArgs : BuildableActivationArgs
+    {
+        public ISlot ParentSlot { get; }
+        public IDoubleClickHandler<IBuilding> DoubleClickHandler { get; }
+
+        public BuildingActivationArgs(
+            ICruiser parentCruiser,
+            ICruiser enemyCruiser,
+            ICruiserSpecificFactories cruiserSpecificFactories,
+            ISlot parentSlot,
+            IDoubleClickHandler<IBuilding> doubleClickHandler)
+            : base(parentCruiser, enemyCruiser, cruiserSpecificFactories)
+        {
+            Helper.AssertIsNotNull(parentSlot, doubleClickHandler);
+
+            ParentSlot = parentSlot;
+            DoubleClickHandler = doubleClickHandler;
+        }
+    }
+}
