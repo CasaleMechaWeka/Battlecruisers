166c73b5e1b39def57d8b493434f16b4a98b8670
Felix Mann <felix.fei.mann@gmail.com>
2019-08-17 16:08:54 -0700

    Create BuildableActivationArgs :)

---
+++ Assets/Scripts/Buildables/Pools/BuildableActivationArgs.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Factories;
+
+namespace BattleCruisers.Buildables.ActivationArgs
+{
+    public class BuildableActivationArgs
+    {
+        public ICruiser ParentCruiser { get; }
+        public ICruiser EnemyCruiser { get; }
+        public ICruiserSpecificFactories CruiserSpecificFactories { get; }
+
+        public BuildableActivationArgs(
+            ICruiser parentCruiser,
+            ICruiser enemyCruiser,
+            ICruiserSpecificFactories cruiserSpecificFactories)
+        {
+            Helper.AssertIsNotNull(parentCruiser, enemyCruiser, cruiserSpecificFactories);
+
+            ParentCruiser = parentCruiser;
+            EnemyCruiser = enemyCruiser;
+            CruiserSpecificFactories = cruiserSpecificFactories;
+        }
+    }
+}
