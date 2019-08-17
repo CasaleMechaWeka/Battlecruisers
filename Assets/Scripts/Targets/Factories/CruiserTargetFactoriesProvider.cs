b59b02df245348dd4aca258e3fa7e8687d6d9cfe
Felix Mann <felix.fei.mann@gmail.com>
2019-08-17 15:33:15 -0700

    Create ICruiserTargetFactoriesProvider :D

---
+++ Assets/Scripts/Targets/Factories/CruiserTargetFactoriesProvider.cs
@@ -0,0 +1,30 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Targets.TargetTrackers;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Factories;
+
+namespace BattleCruisers.Targets.Factories
+{
+    public class CruiserTargetFactoriesProvider : ICruiserTargetFactoriesProvider
+    {
+        public ITargetProcessorFactory ProcessorFactory { get; }
+        public ITargetTrackerFactory TrackerFactory { get; }
+        public ITargetDetectorFactory TargetDetectorFactory { get; }  // FELIX  Remove Target prefix once part of sub provider :)
+        public ITargetProviderFactory TargetProviderFactory { get; }  // FELIX  Remove Target prefix once part of sub provider :)
+
+        public CruiserTargetFactoriesProvider(
+            IFactoryProvider factoryProvider,
+            ICruiserSpecificFactories cruiserSpecificFactories,
+            ICruiser parentCruiser,
+            ICruiser enemyCruiser,
+            IRankedTargetTracker userChosenTargetTracker)
+        {
+            Helper.AssertIsNotNull(factoryProvider, cruiserSpecificFactories, parentCruiser, enemyCruiser, userChosenTargetTracker);
+
+            ProcessorFactory = new TargetProcessorFactory(enemyCruiser, userChosenTargetTracker);
+            TrackerFactory = new TargetTrackerFactory(userChosenTargetTracker);
+            TargetDetectorFactory = new TargetDetectorFactory(enemyCruiser.UnitTargets, parentCruiser.UnitTargets, factoryProvider.UpdaterProvider);
+            TargetProviderFactory = new TargetProviderFactory(cruiserSpecificFactories, factoryProvider.TargetFactories);
+        }
+    }
+}
---
+++ Assets/Scripts/Targets/Factories/ICruiserTargetFactoriesProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleCruisers.Targets.Factories
+{
+    public interface ICruiserTargetFactoriesProvider
+    {
+        ITargetProcessorFactory ProcessorFactory { get; }
+        ITargetDetectorFactory TargetDetectorFactory { get; }
+        ITargetProviderFactory TargetProviderFactory { get; }
+        ITargetTrackerFactory TrackerFactory { get; }
+    }
+}
