9f0b28be680018880313377f7df4c597f8268feb
Felix Mann <felix.fei.mann@gmail.com>
2019-08-18 11:05:32 -0700

    Fix multiple targets test scene :)

--- Assets/Scripts/Scenes/Test/Aircraft/Fighters/MultipleTargetsTestGod.cs
+++ Assets/Scripts/Scenes/Test/Aircraft/Fighters/MultipleTargetsTestGod.cs
@@ -1,16 +1,20 @@
 ﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
 using BattleCruisers.Buildables.Units.Aircraft;
+using BattleCruisers.Cruisers;
 using BattleCruisers.Scenes.Test.Utilities;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Aircraft.Fighters
 {
-    public class MultipleTargetsTestGod : MonoBehaviour 
+    public class MultipleTargetsTestGod : TestGodBase 
 	{
-		void Start() 
-		{
-            Helper helper = new Helper();
+        protected override void Start()
+        {
+            base.Start();
+
+            Helper helper = new Helper(updaterProvider: _updaterProvider);
 
             IList<Vector2> patrolPoints = new List<Vector2>()
             {
@@ -18,9 +22,11 @@ namespace BattleCruisers.Scenes.Test.Aircraft.Fighters
                 new Vector2(12, 5)
             };
 
+            ICruiser redCruiser = helper.CreateCruiser(Direction.Left, Faction.Reds);
+
             // Fighter
             FighterController fighter = FindObjectOfType<FighterController>();
-            helper.InitialiseUnit(fighter, Faction.Blues);
+            helper.InitialiseUnit(fighter, Faction.Blues, enemyCruiser: redCruiser);
             fighter.StartConstruction();
 
             // Targets
@@ -31,6 +37,7 @@ namespace BattleCruisers.Scenes.Test.Aircraft.Fighters
                 target.PatrolPoints = patrolPoints;
                 helper.InitialiseUnit(target, Faction.Reds);
                 target.StartConstruction();
+                Helper.SetupUnitForUnitMonitor(target, redCruiser);
             }
 		}
 	}
