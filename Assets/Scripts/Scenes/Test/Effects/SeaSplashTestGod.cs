4d56867d2222ff6b27805ca5d1be59b6a08ed4be
Felix Mann <felix.fei.mann@gmail.com>
2019-08-24 11:30:09 -0700

    Fix sea splash test scene, ish.  Water asset has a bug, bomb creates a splash when at position 15, but not at position 0 :/

--- Assets/Scripts/Scenes/Test/Effects/SeaSplashTestGod.cs
+++ Assets/Scripts/Scenes/Test/Effects/SeaSplashTestGod.cs
@@ -1,9 +1,24 @@
-﻿using BattleCruisers.Scenes.Test.Utilities;
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Projectiles;
+using BattleCruisers.Projectiles.ActivationArgs;
+using BattleCruisers.Projectiles.Stats;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Utils.Factories;
+using NSubstitute;
 using System;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Effects
 {
+    /// <summary>
+    /// NOTE:
+    /// Bullet and bomb don't create a splash.
+    /// 
+    /// Bomb creates a splash if I move it to x position 15 :/
+    /// 
+    /// The problem is with the Game2DWater asset we bought, and I'm sick of debugging it :P
+    /// </summary>
     public class SeaSplashTestGod : MonoBehaviour
     {
         private void Start()
@@ -13,6 +28,25 @@ namespace BattleCruisers.Scenes.Test.Effects
             helper.InitialiseUnit(aircraft);
             aircraft.StartConstruction();
             aircraft.CompletedBuildable += Aircraft_CompletedBuildable;
+
+            IFactoryProvider factoryProvider = Substitute.For<IFactoryProvider>();
+            IProjectileStats projectileStats = Substitute.For<IProjectileStats>();
+            projectileStats.GravityScale.Returns(1);
+            ITarget parent = Substitute.For<ITarget>();
+            ITargetFilter targetFilter = Substitute.For<ITargetFilter>();
+
+            ProjectileController[] projectiles = FindObjectsOfType<ProjectileController>();
+            foreach (ProjectileController projectile in projectiles)
+            {
+                projectile.Initialise(factoryProvider);
+                projectile.Activate(
+                    new ProjectileActivationArgs<IProjectileStats>(
+                        projectile.Position,
+                        projectileStats,
+                        Vector2.zero,
+                        targetFilter,
+                        parent));
+            }
         }
 
         private void Aircraft_CompletedBuildable(object sender, EventArgs e)
