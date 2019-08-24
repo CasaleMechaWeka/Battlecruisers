f5ea3349755f5ffc0a02239981d31ab2c8beb85c
Felix Mann <felix.fei.mann@gmail.com>
2019-08-24 10:25:21 -0700

    Fix last balancing test scene :)

--- Assets/Scripts/Scenes/Test/Balancing/Range/DestroyerOutOfMortarRangeTestGod.cs
+++ Assets/Scripts/Scenes/Test/Balancing/Range/DestroyerOutOfMortarRangeTestGod.cs
@@ -3,16 +3,20 @@ using BattleCruisers.Buildables.Buildings;
 using BattleCruisers.Buildables.Buildings.Turrets;
 using BattleCruisers.Buildables.Units;
 using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Cruisers;
 using BattleCruisers.Scenes.Test.Utilities;
-using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Balancing.Range
 {
-    public class DestroyerOutOfMortarRangeTestGod : MonoBehaviour
+    public class DestroyerOutOfMortarRangeTestGod : TestGodBase
     {
-        void Start()
+        protected override void Start()
         {
-            Helper helper = new Helper();
+            base.Start();
+
+            Helper helper = new Helper(updaterProvider: _updaterProvider);
+
+            ICruiser blueCruiser = helper.CreateCruiser(Direction.Right, Faction.Blues);
 
             // Initialise mortar
             IBuilding mortar = FindObjectOfType<TurretController>();
@@ -23,8 +27,9 @@ namespace BattleCruisers.Scenes.Test.Balancing.Range
             IUnit[] destroyers = FindObjectsOfType<ShipController>();
             foreach (IUnit destroyer in destroyers)
             {
-                helper.InitialiseUnit(destroyer, Faction.Blues, parentCruiserDirection: Direction.Right);
+                helper.InitialiseUnit(destroyer, Faction.Blues, parentCruiserDirection: Direction.Right, parentCruiser: blueCruiser);
                 destroyer.StartConstruction();
+                Helper.SetupUnitForUnitMonitor(destroyer, blueCruiser);
             }
         }
     }
