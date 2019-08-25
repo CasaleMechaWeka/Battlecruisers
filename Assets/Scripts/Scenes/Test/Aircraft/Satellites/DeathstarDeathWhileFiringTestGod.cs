90a1100f0cde59887444a6aae383507b77fc62cb
Felix Mann <felix.fei.mann@gmail.com>
2019-08-25 12:15:18 -0700

    Complete DeathstarDeathWhileFiringTest, is all good :)

--- Assets/Scripts/Scenes/Test/Aircraft/Satellites/DeathstarDeathWhileFiringTestGod.cs
+++ Assets/Scripts/Scenes/Test/Aircraft/Satellites/DeathstarDeathWhileFiringTestGod.cs
@@ -1,4 +1,6 @@
-﻿using BattleCruisers.Buildables.Buildings;
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Buildables.Units.Aircraft.Providers;
 using BattleCruisers.Scenes.Test.Utilities;
 using UnityEngine;
@@ -16,14 +18,20 @@ namespace BattleCruisers.Scenes.Test.Aircraft.Satellites
 
             Helper helper = new Helper(updaterProvider: _updaterProvider);
 
+            // Satellite
             _launcher = FindObjectOfType<SatelliteLauncherController>();
 
             Vector2 parentCruiserPosition = _launcher.transform.position;
             Vector2 enemyCruiserPosition = new Vector2(_launcher.transform.position.x + 30, _launcher.transform.position.y);
             IAircraftProvider aircraftProvider = new AircraftProvider(parentCruiserPosition, enemyCruiserPosition, new BCUtils.RandomGenerator());
 
-            helper.InitialiseBuilding(_launcher, aircraftProvider: aircraftProvider);
+            helper.InitialiseBuilding(_launcher, Faction.Blues, aircraftProvider: aircraftProvider);
             _launcher.StartConstruction();
+
+            // Target
+            AirFactory target = FindObjectOfType<AirFactory>();
+            helper.InitialiseBuilding(target, Faction.Reds);
+            target.StartConstruction();
         }
 
         public void DestroyLaunchers()
