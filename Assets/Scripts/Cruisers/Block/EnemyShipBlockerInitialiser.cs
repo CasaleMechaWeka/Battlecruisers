5e912e97e6c58578b8432f25f5fd0b3923bb87c7
Felix Mann <felix.fei.mann@gmail.com>
2019-08-24 17:50:30 -0700

    Create EnemyShipBlockerInitialiser :)

---
+++ Assets/Scripts/Cruisers/Block/EnemyShipBlockerInitialiser.cs
@@ -0,0 +1,40 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Targets.Factories;
+using BattleCruisers.Targets.TargetDetectors;
+using BattleCruisers.Targets.TargetFinders;
+using BattleCruisers.Targets.TargetTrackers;
+using BattleCruisers.Utils;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+// FELIX  Remove namespace if only for one class?
+namespace BattleCruisers.Cruisers.Block
+{
+    public class EnemyShipBlockerInitialiser : MonoBehaviour
+    {
+        public ITargetTracker Initialise(
+            ITargetFactoriesProvider targetFactoriesProvider, 
+            ITargetTrackerFactory targetTrackerFactory,
+            Faction enemyFaction)
+        {
+            Helper.AssertIsNotNull(targetFactoriesProvider, targetTrackerFactory);
+
+            TargetDetectorController targetDetectorController = GetComponent<TargetDetectorController>();
+            Assert.IsNotNull(targetDetectorController);
+            targetDetectorController.Initialise();
+
+            IList<TargetType> targetTypesToFind = new List<TargetType>()
+            {
+                TargetType.Ships
+            };
+
+            ITargetFinder targetFinder
+                = targetFactoriesProvider.FinderFactory.CreateRangedTargetFinder(
+                    targetDetectorController,
+                    targetFactoriesProvider.FilterFactory.CreateTargetFilter(enemyFaction, targetTypesToFind));
+
+            return targetTrackerFactory.CreateTargetTracker(targetFinder);
+        }
+    }
+}
