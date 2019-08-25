79398a38247b173e4a0d0a3601ba5a244158afbc
Felix Mann <felix.fei.mann@gmail.com>
2019-08-25 09:58:11 -0700

    Complete ShipStopsForEnemyCruiserTest scene :)

--- Assets/Scripts/Scenes/Test/Naval/ShipStopsForEnemyCruiserTestGod.cs
+++ Assets/Scripts/Scenes/Test/Naval/ShipStopsForEnemyCruiserTestGod.cs
@@ -1,5 +1,11 @@
-﻿using BattleCruisers.Buildables.Units.Ships;
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Cruisers.Block;
 using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Targets.TargetTrackers;
+using NSubstitute;
 using UnityEngine.Assertions;
 
 namespace BattleCruisers.Scenes.Test.Naval
@@ -12,9 +18,22 @@ namespace BattleCruisers.Scenes.Test.Naval
 
             Helper helper = new Helper(updaterProvider: _updaterProvider);
 
+            BuildableInitialisationArgs redArgs = new BuildableInitialisationArgs(helper, faction: Faction.Reds);
+
+            EnemyShipBlockerInitialiser enemyShipBlockerInitialiser = FindObjectOfType<EnemyShipBlockerInitialiser>();
+            Assert.IsNotNull(enemyShipBlockerInitialiser);
+            ITargetTracker enemyShipBlockerTargetTracker
+                = enemyShipBlockerInitialiser.Initialise(
+                    redArgs.FactoryProvider.Targets,
+                    redArgs.CruiserSpecificFactories.Targets.TrackerFactory,
+                    Faction.Blues);
+
+            ICruiser redCruiser = helper.CreateCruiser(Direction.Left, Faction.Reds);
+            redCruiser.BlockedShipsTracker.Returns(enemyShipBlockerTargetTracker);
+
             ShipController ship = FindObjectOfType<ShipController>();
             Assert.IsNotNull(ship);
-            helper.InitialiseUnit(ship);
+            helper.InitialiseUnit(ship, enemyCruiser: redCruiser);
             ship.StartConstruction();
         }
     }
