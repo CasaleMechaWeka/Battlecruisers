85b7726f2be94b6621821cc75055ce241ee5be02
Felix Mann <felix.fei.mann@gmail.com>
2019-08-25 09:45:20 -0700

    Create new test scene stub.  Attack boat moves :D

---
+++ Assets/Scripts/Scenes/Test/Naval/ShipStopsForEnemyCruiserTestGod.cs
@@ -0,0 +1,21 @@
+﻿using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.Naval
+{
+    public class ShipStopsForEnemyCruiserTestGod : TestGodBase
+    {
+        protected override void Start()
+        {
+            base.Start();
+
+            Helper helper = new Helper(updaterProvider: _updaterProvider);
+
+            ShipController ship = FindObjectOfType<ShipController>();
+            Assert.IsNotNull(ship);
+            helper.InitialiseUnit(ship);
+            ship.StartConstruction();
+        }
+    }
+}
