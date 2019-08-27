ab2fb08b125e51c647c6da61f0594a4976a4b849
Felix Mann <felix.fei.mann@gmail.com>
2019-08-26 17:18:06 -0700

    Create (ugly :P) UnitToPoolMap :)

---
+++ Assets/Scripts/Buildables/Pools/IUnitToPoolMap.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Buildables.Units;
+using BattleCruisers.Utils.BattleScene.Pools;
+
+namespace BattleCruisers.Buildables.Pools
+{
+    public interface IUnitToPoolMap
+    {
+        IPool<Unit, BuildableActivationArgs> GetPool(IUnit unit);
+    }
+}
---
+++ Assets/Scripts/Buildables/Pools/UnitToPoolMap.cs
@@ -0,0 +1,75 @@
+﻿using BattleCruisers.Buildables.Units;
+using BattleCruisers.Utils.BattleScene.Pools;
+using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Pools
+{
+    // FELIX  Test???
+    public class UnitToPoolMap : IUnitToPoolMap
+    {
+        private readonly IUnitPoolProvider _unitPoolProvider;
+
+        public UnitToPoolMap(IUnitPoolProvider unitPoolProvider)
+        {
+            Assert.IsNotNull(unitPoolProvider);
+            _unitPoolProvider = unitPoolProvider;
+        }
+
+        public IPool<Unit, BuildableActivationArgs> GetPool(IUnit unit)
+        {
+            Assert.IsNotNull(unit);
+
+            switch (unit.Category)
+            {
+                case UnitCategory.Aircraft:
+                    return GetAircraftPool(unit);
+
+                case UnitCategory.Naval:
+                    return GetShipPool(unit);
+
+                default:
+                    throw new ArgumentException($"Unsupported unit category: {unit.Category}");
+            }
+        }
+
+        private IPool<Unit, BuildableActivationArgs> GetAircraftPool(IUnit aircraft)
+        {
+            switch (aircraft.Name)
+            {
+                case "Bomber":
+                    return _unitPoolProvider.BomberPool;
+
+                case "Fighter":
+                    return _unitPoolProvider.FighterPool;
+
+                case "Gunship":
+                    return _unitPoolProvider.GunshipPool;
+
+                default:
+                    throw new ArgumentException($"Unsupported aircraft: {aircraft.Name}");
+            }
+        }
+
+        private IPool<Unit, BuildableActivationArgs> GetShipPool(IUnit ship)
+        {
+            switch (ship.Name)
+            {
+                case "Attack Boat":
+                    return _unitPoolProvider.AttackBoatPool;
+
+                case "Frigate":
+                    return _unitPoolProvider.FrigatePool;
+
+                case "Destroyer":
+                    return _unitPoolProvider.DestroyerPool;
+
+                case "Archon Battleship":
+                    return _unitPoolProvider.ArchonPool;
+
+                default:
+                    throw new ArgumentException($"Unsupported ship: {ship.Name}");
+            }
+        }
+    }
+}
