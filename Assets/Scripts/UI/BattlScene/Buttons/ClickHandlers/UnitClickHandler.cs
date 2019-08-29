ddfc49c7f78fd90c3ca5b1c880de0320541b85e7
Felix Mann <felix.fei.mann@gmail.com>
2019-08-28 17:03:38 -0700

    Pseudo code for playing "Population limit reached" sound when user tries to build unit :)

--- Assets/Scripts/UI/BattlScene/Buttons/ClickHandlers/UnitClickHandler.cs
+++ Assets/Scripts/UI/BattlScene/Buttons/ClickHandlers/UnitClickHandler.cs
@@ -7,6 +7,7 @@ using BattleCruisers.Utils;
 
 namespace BattleCruisers.UI.BattleScene.Buttons.ClickHandlers
 {
+    // FELIX  Update tests :)
     public class UnitClickHandler : BuildableClickHandler, IUnitClickHandler
     {
         public UnitClickHandler(IUIManager uiManager, IPrioritisedSoundPlayer soundPlayer)
@@ -20,8 +21,13 @@ namespace BattleCruisers.UI.BattleScene.Buttons.ClickHandlers
 
             if (canAffordBuildable)
             {
-                HandleFactory(unitClicked, unitFactory);
+                bool tryingToBuildUnit = HandleFactory(unitClicked, unitFactory);
 			    _uiManager.ShowUnitDetails(unitClicked.Buildable);
+
+                if (tryingToBuildUnit)
+                {
+                    // FELIX  Check for population limit, play sound
+                }
             }
             else if (unitFactory.BuildableState == BuildableState.Completed)
             {
@@ -29,7 +35,10 @@ namespace BattleCruisers.UI.BattleScene.Buttons.ClickHandlers
             }
         }
 
-        private void HandleFactory(IBuildableWrapper<IUnit> unitClicked, IFactory unitFactory)
+        /// <returns>
+        /// True if trying to build a unit, false otherwise.
+        /// </returns>
+        private bool HandleFactory(IBuildableWrapper<IUnit> unitClicked, IFactory unitFactory)
         {
             if (ReferenceEquals(unitFactory.UnitWrapper, unitClicked))
             {
@@ -37,16 +46,19 @@ namespace BattleCruisers.UI.BattleScene.Buttons.ClickHandlers
                 if (unitFactory.IsUnitPaused.Value)
                 {
                     unitFactory.ResumeBuildingUnit();
+                    return true;
                 }
                 else
                 {
                     unitFactory.PauseBuildingUnit();
+                    return false;
                 }
             }
             else
             {
                 // Different unit
                 unitFactory.StartBuildingUnit(unitClicked);
+                return true;
             }
         }
     }
