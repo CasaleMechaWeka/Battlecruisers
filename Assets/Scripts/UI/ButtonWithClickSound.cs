df9e38abdb925193e086ab0c11b811b68b29af55
Felix Mann <felix.fei.mann@gmail.com>
2019-08-31 14:51:56 -0700

    Create ButtonWithClickSound :)

---
+++ Assets/Scripts/UI/ButtonWithClickSound.cs
@@ -0,0 +1,27 @@
+﻿using BattleCruisers.UI.Sound;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI
+{
+    public class ButtonWithClickSound : ClickableTogglable
+    {
+        private ISoundPlayer _soundPlayer;
+        protected virtual ISoundKey ClickSound => null;
+
+        public virtual void Initialise(ISoundPlayer soundPlayer)
+        {
+            base.Initialise();
+
+            Assert.IsNotNull(soundPlayer);
+            _soundPlayer = soundPlayer;
+        }
+
+        protected override void OnClicked()
+        {
+            if (ClickSound != null)
+            {
+                _soundPlayer.PlaySound(ClickSound);
+            }
+        }
+    }
+}
