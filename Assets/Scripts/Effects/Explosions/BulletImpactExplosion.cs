dfbe6b546cb6c9da54616b10b275c8f74f37be1d
Felix Mann <felix.fei.mann@gmail.com>
2019-09-01 13:31:52 -0700

    Create BulletImpactExplosion script :)

---
+++ Assets/Scripts/Effects/Explosions/BulletImpactExplosion.cs
@@ -0,0 +1,40 @@
+﻿using BattleCruisers.Utils;
+using System;
+using UnityEngine;
+
+namespace BattleCruisers.Effects.Explosions
+{
+    public class BulletImpactExplosion : MonoBehaviour, IExplosion
+    {
+        private BroadcastingParticleSystem _sparks;
+        private ParticleSystem _blast;
+
+        public event EventHandler Deactivated;
+
+        public void Initialise()
+        {
+            _sparks = transform.FindNamedComponent<BroadcastingParticleSystem>("sparks");
+            _sparks.Initialise();
+
+            _blast = transform.FindNamedComponent<ParticleSystem>("blast");
+
+            // Sparks is longer particle system, so wait for it to complete
+            _sparks.Stopped += _sparks_Stopped;
+        }
+
+        private void _sparks_Stopped(object sender, EventArgs e)
+        {
+            gameObject.SetActive(false);
+            Deactivated?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Activate(Vector3 position)
+        {
+            gameObject.SetActive(true);
+            gameObject.transform.position = position;
+
+            _blast.Play();
+            _sparks.ParticleSystem.Play();
+        }
+    }
+}
