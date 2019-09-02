d1af02947469bd9408d2d13a15f79c32219454f9
Felix Mann <felix.fei.mann@gmail.com>
2019-09-02 16:08:34 -0700

    Created test scene to show that unit healthbar is missing :(

---
+++ Assets/Scripts/Scenes/Test/Factories/SlowFactoryTestGod.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.Factories
+{
+    public class SlowFactoryTestGod : MonoBehaviour
+    {
+        public UnitWrapper unitPrefab;
+
+        void Start()
+        {
+            Helper helper = new Helper(buildSpeedMultiplier: 5);
+
+            Factory factory = FindObjectOfType<Factory>();
+            Assert.IsNotNull(factory);
+            helper.InitialiseBuilding(factory);
+            factory.StartConstruction();
+            factory.CompletedBuildable += (sender, e) => factory.StartBuildingUnit(unitPrefab);
+        }
+    }
+}
