3cb47c2d7735b465b6eb85fdab213f0d6bc0c54b
Felix Mann <felix.fei.mann@gmail.com>
2019-09-02 16:03:22 -0700

    Oh dear, it's because unit was in parent game object that was at 0.5 :(

---
+++ Assets/Scripts/Scenes/Test/SandboxTestGod.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test
+{
+    public class SandboxTestGod : MonoBehaviour
+    {
+        void Start()
+        {
+            Artillery artillery = FindObjectOfType<Artillery>();
+            Assert.IsNotNull(artillery);
+
+            //BuildingWrapper wrapper = artillery.GetComponentInParent<BuildingWrapper>();
+            //Assert.IsNotNull(wrapper);
+            //wrapper.Initialise();
+
+            Helper helper = new Helper(buildSpeedMultiplier: 5);
+            helper.InitialiseBuilding(artillery);
+            artillery.StartConstruction();
+        }
+    }
+}
