5faf6d72c92c5a92f4b0f2460b525e28aa0ea7d8
Felix Mann <felix.fei.mann@gmail.com>
2019-09-03 18:41:46 -0700

    Create UnitRecyclingTestGod to simplify unit recycled test scenes :)

--- Assets/Scripts/Scenes/Test/Factories/FactoryTestGod.cs
+++ Assets/Scripts/Scenes/Test/Factories/FactoryTestGod.cs
@@ -4,7 +4,6 @@ using BattleCruisers.Buildables.Units;
 using BattleCruisers.Cruisers;
 using BattleCruisers.Scenes.Test.Utilities;
 using System;
-using BCUtils = BattleCruisers.Utils;
 
 namespace BattleCruisers.Scenes.Test.Factories
 {
@@ -12,7 +11,6 @@ namespace BattleCruisers.Scenes.Test.Factories
 	{
 		public Factory factoryFacingRight, factoryFacingLeft;
 		public UnitWrapper unitPrefab;
-        public float buildSpeedMultiplier = BCUtils.BuildSpeedMultipliers.VERY_FAST;
 
         protected virtual Faction FactoryFacingLeftFaction => Faction.Blues;
 		protected virtual Faction FactoryFacingRightFaction => Faction.Blues;
@@ -23,7 +21,7 @@ namespace BattleCruisers.Scenes.Test.Factories
 
 			unitPrefab.Initialise();
 
-			Helper helper = new Helper(buildSpeedMultiplier: buildSpeedMultiplier, updaterProvider: _updaterProvider);
+			Helper helper = new Helper(updaterProvider: _updaterProvider);
 
             ICruiser leftCruiser = helper.CreateCruiser(Direction.Right, FactoryFacingRightFaction);
             ICruiser rightCruiser = helper.CreateCruiser(Direction.Left, FactoryFacingLeftFaction);
---
+++ Assets/Scripts/Scenes/Test/Performance/ObjectPooling/UnitRecyclingTestGod.cs
@@ -0,0 +1,34 @@
+﻿using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Utils.Threading;
+using UnityEngine.Assertions;
+using BCUtils = BattleCruisers.Utils;
+
+namespace BattleCruisers.Scenes.Test.Performance.ObjectPooling
+{
+    public class UnitRecyclingTestGod : TestGodBase
+    {
+        public UnitWrapper unitPrefab;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            Assert.IsNotNull(unitPrefab);
+            unitPrefab.Initialise();
+
+            Helper helper = new Helper(buildSpeedMultiplier: BCUtils.BuildSpeedMultipliers.FAST, updaterProvider: _updaterProvider);
+
+            TimeScaleDeferrer deferrer = GetComponent<TimeScaleDeferrer>();
+            Assert.IsNotNull(deferrer);
+
+            Factory factory = FindObjectOfType<Factory>();
+            Assert.IsNotNull(factory);
+            helper.InitialiseBuilding(factory);
+            factory.StartConstruction();
+            factory.CompletedBuildable += (sender, e) => factory.StartBuildingUnit(unitPrefab);
+            factory.UnitCompleted += (sender, e) => deferrer.Defer(e.CompletedUnit.Destroy, delayInS: 1);
+        }
+    }
+}
