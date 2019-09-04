09c1579d463515d531fb11f8e82c741b55184c26
Felix Mann <felix.fei.mann@gmail.com>
2019-09-04 16:55:26 -0700

    Generate 10 drones and randomise start position

---
+++ Assets/Scripts/Scenes/Test/Effects/DronesTestGod.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.Utils;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Effects
+{
+    public class DronesTestGod : MonoBehaviour
+    {
+        public GameObject parentObject;
+        public GameObject dronePrefab;
+        public int numOfDrones = 10;
+        public float spawnRadiusInM = 0.5f;
+
+        void Start()
+        {
+            for (int i = 0; i < numOfDrones; ++i)
+            {
+                GameObject newDrone = Instantiate(dronePrefab, parentObject.transform);
+                newDrone.transform.position = RandomisePosition(parentObject.transform.position);
+
+                Debug.Log($"Created rone #{i} at position: {newDrone.transform.position}");
+            }
+        }
+
+        private Vector2 RandomisePosition(Vector2 originalPosition)
+        {
+            float x = RandomGenerator.Instance.Range(-spawnRadiusInM, spawnRadiusInM);
+            float y = RandomGenerator.Instance.Range(-spawnRadiusInM, spawnRadiusInM);
+            return new Vector2(originalPosition.x + x, originalPosition.y + y);
+        }
+    }
+}
