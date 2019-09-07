1c16bc928513b4555d3a66552acf79a3cc07d724
Felix Mann <felix.fei.mann@gmail.com>
2019-09-07 10:23:53 -0700

    Create DroneFactory :)

---
+++ Assets/Scripts/Cruisers/Drones/Feedback/DroneFactory.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.Effects;
+using BattleCruisers.Utils.BattleScene.Pools;
+using BattleCruisers.Utils.Fetchers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Cruisers.Drones.Feedback
+{
+    public class DroneFactory : IPoolableFactory<IDroneController, Vector2>
+    {
+        private readonly IPrefabFactory _prefabFactory;
+
+        public DroneFactory(IPrefabFactory prefabFactory)
+        {
+            Assert.IsNotNull(prefabFactory);
+            _prefabFactory = prefabFactory;
+        }
+
+        public IDroneController CreateItem()
+        {
+            return _prefabFactory.CreateDrone();
+        }
+    }
+}
