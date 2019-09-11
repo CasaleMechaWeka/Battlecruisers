fa51e5ff290bc33aad5ece2311e66d45e01a7111
Felix Mann <felix.fei.mann@gmail.com>
2019-09-10 17:58:25 -0700

    Use level music keys instead of hardcoding :)

--- Assets/Scripts/Scenes/BattleScene/BattleSceneGod.cs
+++ Assets/Scripts/Scenes/BattleScene/BattleSceneGod.cs
@@ -171,11 +171,10 @@ namespace BattleCruisers.Scenes.BattleScene
 
             // Audio
             ILevel currentLevel = applicationModel.DataProvider.GetLevel(applicationModel.SelectedLevel);
-            // FELIX  Use music keys provided by level instead of hardcoding :)
             ILayeredMusicPlayer layeredMusicPlayer
                 = components.MusicPlayerInitialiser.CreatePlayer(
                     factoryProvider.Sound.SoundFetcher,
-                    SoundKeys.Music.Background.Kentient);
+                    currentLevel.MusicKeys);
             _audioInitialiser
                 = new AudioInitialiser(
                     helper,
