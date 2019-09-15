ee47d8396458fc0e91e24121ef9459008669d81f
Felix Mann <felix.fei.mann@gmail.com>
2019-09-15 09:17:53 -0700

    Create ExplosionController :)

---
+++ Assets/Scripts/Effects/Explosions/ExplosionController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Assertions;
+
+namespace BattleCruisers.Effects.Explosions
+{
+    public class ExplosionController : MonoBehaviourWrapper
+    {
+        public virtual IExplosion Initialise()
+        {
+            IBroadcastingParticleSystem[] particleSystems = GetParticleSystems();
+            return new Explosion(this, particleSystems);
+        }
+
+        protected virtual IBroadcastingParticleSystem[] GetParticleSystems()
+        {
+            BroadcastingParticleSystem[] particleSystems = GetComponentsInChildren<BroadcastingParticleSystem>();
+            Assert.IsTrue(particleSystems.Length != 0);
+
+            foreach (BroadcastingParticleSystem particleSystem in particleSystems)
+            {
+                particleSystem.Initialise();
+            }
+
+            return particleSystems;
+        }
+    }
+}
