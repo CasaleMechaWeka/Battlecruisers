588799e16e0958f7f65e2be50d02bc8094e644e3
Felix Mann <felix.fei.mann@gmail.com>
2019-09-15 11:09:16 -0700

    Make 1.5 explosion use ExplosionController :)

--- Assets/Scripts/Scenes/Test/Effects/Explosions/ExplosionSizeTestGod.cs
+++ Assets/Scripts/Scenes/Test/Effects/Explosions/ExplosionSizeTestGod.cs
@@ -7,7 +7,6 @@ namespace BattleCruisers.Scenes.Test.Effects.Explosions
 {
     public class ExplosionSizeTestGod : MonoBehaviour
     {
-        // FELIX
         void Start()
         {
             TimeScaleDeferrer deferrer = GetComponent<TimeScaleDeferrer>();
@@ -21,14 +20,6 @@ namespace BattleCruisers.Scenes.Test.Effects.Explosions
                 explosion.Deactivated += (sender, e) => deferrer.Defer(() => explosion.Activate(explosionController.Position), delayInS: 0.25f);
                 explosion.Activate(explosionController.Position);
             }
-
-            //AdvancedExplosion[] explosions = GetComponentsInChildren<AdvancedExplosion>();
-
-            //foreach (AdvancedExplosion explosion in explosions)
-            //{
-            //    explosion.Initialise(RandomGenerator.Instance);
-            //    explosion.Activate(explosion.transform.position);
-            //}
         }
     }
 }
