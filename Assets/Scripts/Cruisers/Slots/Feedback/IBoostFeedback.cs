46c858563e62c853b5d1e9d65c0c61325bd38dff
Felix Mann <felix.fei.mann@gmail.com>
2019-09-16 17:23:37 -0700

    Create IBoostFeedback

---
+++ Assets/Scripts/Cruisers/Slots/Feedback/IBoostFeedback.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleCruisers.Cruisers.Slots.Feedback
+{
+    public enum BoostState
+    {
+        Off,    // No boost
+        Single, // Single boost
+        Double  // Double boost
+    }
+
+    public interface IBoostFeedback
+    {
+        BoostState State { get; set; }
+    }
+}
