5c46b83d97d8095de44498a9cde950eb2272b0c9
Felix Mann <felix.fei.mann@gmail.com>
2019-09-16 17:33:43 -0700

    Create BoostStateFinder :)

---
+++ Assets/Scripts/Cruisers/Slots/Feedback/BoostStateFinder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Assertions;
+
+namespace BattleCruisers.Cruisers.Slots.Feedback
+{
+    // FELIX  Use, test
+    public class BoostStateFinder : IBoostStateFinder
+    {
+        public BoostState FindState(int numOfLocalBoosters, bool isBuildingBoostable)
+        {
+            Assert.IsTrue(numOfLocalBoosters >= 0);
+
+            if (!isBuildingBoostable
+                || numOfLocalBoosters == 0)
+            {
+                return BoostState.Off;
+            }
+            else if (numOfLocalBoosters == 1)
+            {
+                return BoostState.Single;
+            }
+            else
+            {
+                return BoostState.Double;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Cruisers/Slots/Feedback/IBoostStateFinder.cs
@@ -0,0 +1,7 @@
+﻿namespace BattleCruisers.Cruisers.Slots.Feedback
+{
+    public interface IBoostStateFinder
+    {
+        BoostState FindState(int numOfLocalBoosters, bool isBuildingBoostable);
+    }
+}
