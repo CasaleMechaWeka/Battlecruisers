1f928e0cf89bc677d62a74d81417d36526d4ebdb
Felix Mann <felix.fei.mann@gmail.com>
2019-09-17 17:09:20 -0700

    Create SlotBoostFeedbackMonitorInitialiser :)

---
+++ Assets/Scripts/Cruisers/Slots/Feedback/SlotBoostFeedbackMonitorInitialiser.cs
@@ -0,0 +1,23 @@
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.PlatformAbstractions;
+using UnityEngine;
+
+namespace BattleCruisers.Cruisers.Slots.Feedback
+{
+    public class SlotBoostFeedbackMonitorInitialiser : MonoBehaviour
+    {
+        public SlotBoostFeedbackMonitor CreateFeedbackMonitor(ISlot slot)
+        {
+            ParticleSystem singleBoostEffect = transform.FindNamedComponent<ParticleSystem>("SingleBoostEffect");
+            ParticleSystem doubleBoostEffect = transform.FindNamedComponent<ParticleSystem>("DoubleBoostEffect");
+
+            return
+                new SlotBoostFeedbackMonitor(
+                    slot,
+                    new BoostStateFinder(),
+                    new BoostFeedback(
+                        new GameObjectBC(singleBoostEffect.gameObject),
+                        new GameObjectBC(doubleBoostEffect.gameObject)));
+        }
+    }
+}
