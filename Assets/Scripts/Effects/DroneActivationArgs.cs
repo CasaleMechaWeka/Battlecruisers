32866cf284879a350ac681b0d31958400ab67609
Felix Mann <felix.fei.mann@gmail.com>
2019-09-22 13:37:38 -0700

    Create DroneActivationArgs

---
+++ Assets/Scripts/Effects/DroneActivationArgs.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Effects
+{
+    // FELIX  Create Drones namespace?
+    public class DroneActivationArgs
+    {
+        public Vector2 Position { get; }
+        public bool PlayAudio { get; }
+
+        public DroneActivationArgs(Vector2 position, bool playAudio)
+        {
+            Position = position;
+            PlayAudio = playAudio;
+        }
+    }
+}
