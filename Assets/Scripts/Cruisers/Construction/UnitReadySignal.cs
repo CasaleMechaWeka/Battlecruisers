73209f36dad41137b8b2114e35f85bccfbacb216
Felix Mann <felix.fei.mann@gmail.com>
2019-09-22 17:56:08 -0700

    Create UnitReadySignal :)

---
+++ Assets/Scripts/Cruisers/Construction/UnitReadySignal.cs
@@ -0,0 +1,47 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.PlatformAbstractions.UI;
+using System;
+
+namespace BattleCruisers.Cruisers.Construction
+{
+    // FELIX  Use, test
+    public class UnitReadySignal : IManagedDisposable
+    {
+        private readonly ICruiserUnitMonitor _unitMonitor;
+        private readonly IAudioSource _navalAudioSource, _aircraftAudioSource;
+
+        public UnitReadySignal(ICruiserUnitMonitor unitMonitor, IAudioSource navalAudioSource, IAudioSource aircraftAudioSource)
+        {
+            Helper.AssertIsNotNull(unitMonitor, navalAudioSource, aircraftAudioSource);
+
+            _unitMonitor = unitMonitor;
+            _navalAudioSource = navalAudioSource;
+            _aircraftAudioSource = aircraftAudioSource;
+
+            _unitMonitor.UnitCompleted += _unitMonitor_UnitCompleted;
+        }
+
+        private void _unitMonitor_UnitCompleted(object sender, UnitCompletedEventArgs e)
+        {
+            switch (e.CompletedUnit.TargetType)
+            {
+                case TargetType.Aircraft:
+                    _aircraftAudioSource.Play();
+                    break;
+
+                case TargetType.Ships:
+                    _navalAudioSource.Play();
+                    break;
+
+                default:
+                    throw new ArgumentException($"Unsupported target type: {e.CompletedUnit.TargetType}");
+            }
+        }
+
+        public void DisposeManagedState()
+        {
+            _unitMonitor.UnitCompleted -= _unitMonitor_UnitCompleted;
+        }
+    }
+}
