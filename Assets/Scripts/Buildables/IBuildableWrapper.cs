7c60f64608b40b877a0c99d2aa704410124a8640
Felix Mann <felix.fei.mann@gmail.com>
2019-09-28 10:44:13 -0700

    Add comment so I don't spend 5h refactoring IBuildableWrapper away only to remember why I had them in the first place :P

--- Assets/Scripts/Buildables/IBuildableWrapper.cs
+++ Assets/Scripts/Buildables/IBuildableWrapper.cs
@@ -1,5 +1,13 @@
 ﻿namespace BattleCruisers.Buildables
 {
+    /// <summary>
+    /// BuildableWrappers contain:
+    /// 1. Buildable
+    /// 2. Healthbar
+    /// 
+    /// This is so that as the buildable's rotation changes (eg, fighter), the healthbar
+    /// rotation remains the same.
+    /// </summary>
     public interface IBuildableWrapper<TBuildable> where TBuildable : class, IBuildable
 	{
         TBuildable Buildable { get; }
