8416c3879a5a76287e54aca4593a89e93aa7b806
Felix Mann <felix.fei.mann@gmail.com>
2019-09-28 12:06:17 -0700

    Create AngleConverter :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AngleCalculators/AngleConverter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators
+{
+    // FELIX  use, test
+    public class AngleConverter : IAngleConverter
+    {
+        private const float MIN_UNSIGNED_ANGLE_IN_DEGREES = 0;
+        private const float MAX_UNSIGNED_ANGLE_IN_DEGREES = 360;
+        private const float MIN_SIGNED_ANGLE_IN_DEGREES = -180;
+        private const float MAX_SIGNED_ANGLE_IN_DEGREES = 180;
+
+        public float ConvertToSigned(float unsignedAngleInDegrees)
+        {
+            Assert.IsTrue(unsignedAngleInDegrees >= MIN_UNSIGNED_ANGLE_IN_DEGREES);
+            Assert.IsTrue(unsignedAngleInDegrees <= MAX_UNSIGNED_ANGLE_IN_DEGREES);
+
+            if (unsignedAngleInDegrees > 180)
+            {
+                unsignedAngleInDegrees -= 360;
+            }
+
+            return unsignedAngleInDegrees;
+        }
+
+        public float ConvertToUnsigned(float signedAngleInDegrees)
+        {
+            Assert.IsTrue(signedAngleInDegrees >= MIN_SIGNED_ANGLE_IN_DEGREES);
+            Assert.IsTrue(signedAngleInDegrees <= MAX_SIGNED_ANGLE_IN_DEGREES);
+
+            if (signedAngleInDegrees < 0)
+            {
+                signedAngleInDegrees += 360;
+            }
+
+            return signedAngleInDegrees;
+        }
+    }
+}
---
+++ Assets/Scripts/Buildables/Buildings/Turrets/AngleCalculators/IAngleConverter.cs
@@ -0,0 +1,13 @@
+﻿namespace BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators
+{
+    public interface IAngleConverter
+    {
+        /// <param name="unsignedAngleInDegrees">0* to 360*</param>
+        /// <returns>-180* to 180*</returns>
+        float ConvertToSigned(float unsignedAngleInDegrees);
+
+        /// <param name="signedAngleInDegrees">-180* to 180*</param>
+        /// <returns>0* to 360*</returns>
+        float ConvertToUnsigned(float signedAngleInDegrees);
+    }
+}
