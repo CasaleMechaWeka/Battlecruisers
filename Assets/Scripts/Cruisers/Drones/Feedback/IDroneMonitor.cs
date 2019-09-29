11d40ea05f7dabcb1829c5a195530fc8ecad104b
Felix Mann <felix.fei.mann@gmail.com>
2019-09-29 14:25:03 -0700

    Remove TODO :)

--- Assets/Scripts/Cruisers/Drones/Feedback/IDroneMonitor.cs
+++ Assets/Scripts/Cruisers/Drones/Feedback/IDroneMonitor.cs
@@ -6,9 +6,7 @@ namespace BattleCruisers.Cruisers.Drones.Feedback
 {
     public interface IDroneMonitor
     {
-        // FELIX  Remove?
         IReadOnlyDictionary<Faction, int> FactionToActiveDroneNum { get; }
-
         IBroadcastingProperty<bool> PlayerCruiserHasActiveDrones { get; }
         IBroadcastingProperty<bool> AICruiserHasActiveDrones { get; }
     }
