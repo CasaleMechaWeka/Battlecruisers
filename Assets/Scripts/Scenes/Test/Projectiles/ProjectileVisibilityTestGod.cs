7c72167ae26c7e8d4856062f937fb1e48105e310
Felix Mann <felix.fei.mann@gmail.com>
2019-10-08 17:40:16 -0700

    Create SkyChanger test prefab :)

--- Assets/Scripts/Scenes/Test/Projectiles/ProjectileVisibilityTestGod.cs
+++ Assets/Scripts/Scenes/Test/Projectiles/ProjectileVisibilityTestGod.cs
@@ -1,21 +1,12 @@
 ﻿using BattleCruisers.Buildables;
 using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Buildables.Buildings.Turrets;
-using BattleCruisers.Data.Static;
 using BattleCruisers.Scenes.Test.Utilities;
-using BattleCruisers.Utils.DataStrctures;
-using BattleCruisers.Utils.Fetchers;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace BattleCruisers.Scenes.Test.Projectiles
 {
     public class ProjectileVisibilityTestGod : NavigationTestGod
     {
-        private Skybox _skybox;
-        private ICircularList<Material> _skies;
-
         protected override void Start()
         {
             base.Start();
@@ -29,41 +20,6 @@ namespace BattleCruisers.Scenes.Test.Projectiles
             AirFactory airFactory = FindObjectOfType<AirFactory>();
             helper.InitialiseBuilding(airFactory, Faction.Reds);
             airFactory.StartConstruction();
-
-            _skies = FindSkyMaterials();
-
-            _skybox = FindObjectOfType<Skybox>();
-            Assert.IsNotNull(_skybox);
-            ChangeSky();
-        }
-
-        private ICircularList<Material> FindSkyMaterials()
-        {
-            IList<string> skyNames = new List<string>()
-            {
-                SkyMaterials.Blue,
-                SkyMaterials.BlueCloudy,
-                SkyMaterials.BlueDeep,
-                SkyMaterials.Sunset,
-                SkyMaterials.SunsetCloudy,
-                SkyMaterials.SunsetWeirdClouds,
-                SkyMaterials.White
-            };
-
-            IList<Material> skyMaterials = new List<Material>();
-            IMaterialFetcher materialFetcher = new MaterialFetcher();
-
-            foreach (string skyName in skyNames)
-            {
-                skyMaterials.Add(materialFetcher.GetMaterial(skyName));
-            }
-
-            return new CircularList<Material>(skyMaterials);
-        }
-
-        public void ChangeSky()
-        {
-            _skybox.material = _skies.Next();
         }
     }
 }
---
+++ Assets/Scripts/Scenes/Test/Utilities/SkyChanger.cs
@@ -0,0 +1,53 @@
+﻿using BattleCruisers.Data.Static;
+using BattleCruisers.Utils.DataStrctures;
+using BattleCruisers.Utils.Fetchers;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.Utilities
+{
+    public class SkyChanger : MonoBehaviour
+    {
+        private Skybox _skybox;
+        private ICircularList<Material> _skies;
+
+        void Start()
+        {
+            _skies = FindSkyMaterials();
+
+            _skybox = FindObjectOfType<Skybox>();
+            Assert.IsNotNull(_skybox);
+            ChangeSky();
+        }
+
+        private ICircularList<Material> FindSkyMaterials()
+        {
+            IList<string> skyNames = new List<string>()
+            {
+                SkyMaterials.Blue,
+                SkyMaterials.BlueCloudy,
+                SkyMaterials.BlueDeep,
+                SkyMaterials.Sunset,
+                SkyMaterials.SunsetCloudy,
+                SkyMaterials.SunsetWeirdClouds,
+                SkyMaterials.White
+            };
+
+            IList<Material> skyMaterials = new List<Material>();
+            IMaterialFetcher materialFetcher = new MaterialFetcher();
+
+            foreach (string skyName in skyNames)
+            {
+                skyMaterials.Add(materialFetcher.GetMaterial(skyName));
+            }
+
+            return new CircularList<Material>(skyMaterials);
+        }
+
+        public void ChangeSky()
+        {
+            _skybox.material = _skies.Next();
+        }
+    }
+}
