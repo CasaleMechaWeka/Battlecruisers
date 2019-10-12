4734dc83af7f684f67a71299213d539512f4014c
Felix Mann <felix.fei.mann@gmail.com>
2019-10-12 11:25:05 -0700

    Remove unused method :)

--- Assets/Scripts/Utils/Fetchers/ISpriteFetcher.cs
+++ Assets/Scripts/Utils/Fetchers/ISpriteFetcher.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections.Generic;
-using BattleCruisers.Utils.PlatformAbstractions.UI;
+﻿using BattleCruisers.Utils.PlatformAbstractions.UI;
+using System.Collections.Generic;
 
 namespace BattleCruisers.Utils.Fetchers
 {
     public interface ISpriteFetcher
 	{
-		ISpriteWrapper GetSprite(string spritePath);
         IList<ISpriteWrapper> GetMultiSprites(string spritePath);
 	}
 }
--- Assets/Scripts/Utils/Fetchers/SpriteFetcher.cs
+++ Assets/Scripts/Utils/Fetchers/SpriteFetcher.cs
@@ -1,25 +1,12 @@
-﻿using System;
+﻿using BattleCruisers.Utils.PlatformAbstractions.UI;
 using System.Collections.Generic;
 using System.Linq;
-using BattleCruisers.Utils.PlatformAbstractions.UI;
 using UnityEngine;
 
 namespace BattleCruisers.Utils.Fetchers
 {
     public class SpriteFetcher : ISpriteFetcher
 	{
-        public ISpriteWrapper GetSprite(string spritePath)
-        {
-            Sprite sprite = Resources.Load<Sprite>(spritePath);
-
-            if (sprite == null)
-            {
-                throw new ArgumentException("Invalid sprite path: " + spritePath);
-            }
-
-            return new SpriteWrapper(sprite);
-        }
-		
         public IList<ISpriteWrapper> GetMultiSprites(string spritePath)
 		{
             Sprite[] sprites = Resources.LoadAll<Sprite>(spritePath);
