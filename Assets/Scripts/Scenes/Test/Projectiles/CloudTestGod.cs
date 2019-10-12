ac5e49b467066741ce7520c3d9585144c3a37384
Felix Mann <felix.fei.mann@gmail.com>
2019-10-12 11:22:57 -0700

    Use SkyChanger prefab to avoid duplicate code :)

--- Assets/Scripts/Scenes/Test/Projectiles/CloudTestGod.cs
+++ Assets/Scripts/Scenes/Test/Projectiles/CloudTestGod.cs
@@ -1,9 +1,6 @@
-﻿using BattleCruisers.Data.Static;
-using BattleCruisers.Scenes.Test.Utilities;
+﻿using BattleCruisers.Scenes.Test.Utilities;
 using BattleCruisers.UI.BattleScene.Clouds;
 using BattleCruisers.Utils.DataStrctures;
-using BattleCruisers.Utils.Fetchers;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 using BCUtils = BattleCruisers.Utils;
@@ -12,20 +9,11 @@ namespace BattleCruisers.Scenes.Test.Projectiles
 {
     public class CloudTestGod : NavigationTestGod
     {
-        private Skybox _skybox;
-        private ICircularList<Material> _skies;
-
         protected override void Start()
         {
             base.Start();
 
             Helper helper = new Helper();
-
-            _skies = FindSkyMaterials();
-
-            _skybox = FindObjectOfType<Skybox>();
-            Assert.IsNotNull(_skybox);
-            ChangeSky();
 
             CloudStatsController cloudStatsController = GetComponentInChildren<CloudStatsController>();
             Assert.IsNotNull(cloudStatsController);
@@ -42,37 +30,6 @@ namespace BattleCruisers.Scenes.Test.Projectiles
             CloudInitialiser cloudInitialiser = GetComponentInChildren<CloudInitialiser>();
             Assert.IsNotNull(cloudInitialiser);
             cloudInitialiser.Initialise(cloudStats, BCUtils.RandomGenerator.Instance);
-        }
-
-        // FELIX  Use sky changer :)
-        private ICircularList<Material> FindSkyMaterials()
-        {
-            //IList<string> skyNames = new List<string>()
-            //{
-            //    SkyMaterials.Blue,
-            //    SkyMaterials.BlueCloudy,
-            //    SkyMaterials.BlueDeep,
-            //    SkyMaterials.Sunset,
-            //    SkyMaterials.SunsetCloudy,
-            //    SkyMaterials.SunsetWeirdClouds,
-            //    SkyMaterials.White
-            //};
-
-            //IList<Material> skyMaterials = new List<Material>();
-            //IMaterialFetcher materialFetcher = new MaterialFetcher();
-
-            //foreach (string skyName in skyNames)
-            //{
-            //    skyMaterials.Add(materialFetcher.GetMaterialAsync(skyName));
-            //}
-
-            //return new CircularList<Material>(skyMaterials);
-            return null;
-        }
-
-        public void ChangeSky()
-        {
-            _skybox.material = _skies.Next();
         }
     }
 }
