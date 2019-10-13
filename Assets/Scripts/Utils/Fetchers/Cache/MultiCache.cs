e395aa161cbc0f824faa62134c971a872a15115a
Felix Mann <felix.fei.mann@gmail.com>
2019-10-13 10:51:05 -0700

    MultiCache, a glorified dictionary :P

---
+++ Assets/Scripts/Utils/Fetchers/Cache/MultiCache.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.Fetchers.Cache
+{
+    public class MultiCache<TPrefab> : IMultiCache<TPrefab> where TPrefab : class
+    {
+        private readonly IDictionary<IPrefabKey, TPrefab> _prefabs;
+
+        public MultiCache(IDictionary<IPrefabKey, TPrefab> prefabs)
+        {
+            Assert.IsNotNull(prefabs);
+            _prefabs = prefabs;
+        }
+
+        public TPrefab GetPrefab(IPrefabKey prefabKey)
+        {
+            Assert.IsNotNull(prefabKey);
+            Assert.IsTrue(_prefabs.ContainsKey(prefabKey));
+            return _prefabs[prefabKey];
+        }
+    }
+}
