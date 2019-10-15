29ba26d8706deb91958d01a313f398c864b02254
Felix Mann <felix.fei.mann@gmail.com>
2019-10-15 15:56:44 -0700

    Fix BombingSpriteChooser test scene :)

--- Assets/Scripts/Scenes/Test/Aircraft/BombingSpriteChooserTestGod.cs
+++ Assets/Scripts/Scenes/Test/Aircraft/BombingSpriteChooserTestGod.cs
@@ -4,7 +4,6 @@ using BattleCruisers.Buildables.Units;
 using BattleCruisers.Buildables.Units.Aircraft;
 using BattleCruisers.Buildables.Units.Aircraft.Providers;
 using BattleCruisers.Scenes.Test.Utilities;
-using BattleCruisers.Targets.Factories;
 using BattleCruisers.Targets.TargetFinders.Filters;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,21 +14,29 @@ namespace BattleCruisers.Scenes.Test.Aircraft
     {
         public List<Vector2> patrolPoints;
 
-        void Start()
+        async void Start()
         {
-            Helper helper = new Helper();
-
             AirFactory factory = FindObjectOfType<AirFactory>();
-            helper.InitialiseBuilding(factory, Faction.Blues);
+            factory.GameObject.SetActive(false);
 
+            BomberController bomber = FindObjectOfType<BomberController>();
+            bomber.GameObject.SetActive(false);
+
+            Helper helper = await HelperFactory.CreateHelperAsync();
+
+            // Factory
+            helper.InitialiseBuilding(factory, Faction.Blues);
+            factory.GameObject.SetActive(true);
+
+            // Bomber
             IList<TargetType> targetTypes = new List<TargetType>() { factory.TargetType };
             ITargetFilter targetFilter = new FactionAndTargetTypeFilter(factory.Faction, targetTypes);
             ITargetFactories targetFactories = helper.CreateTargetFactories(factory.GameObject, targetFilter: targetFilter);
 
-            BomberController bomber = FindObjectOfType<BomberController>();
             IAircraftProvider aircraftProvider = helper.CreateAircraftProvider(bomberPatrolPoints: patrolPoints);
             helper.InitialiseUnit(bomber, Faction.Reds, aircraftProvider: aircraftProvider, targetFactories: targetFactories, parentCruiserDirection: Direction.Right);
             bomber.StartConstruction();
+            bomber.GameObject.SetActive(true);
         }
     }
 }
