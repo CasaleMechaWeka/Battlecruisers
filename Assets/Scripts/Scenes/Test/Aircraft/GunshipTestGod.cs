de11b0933392b15472b1c1fa9cf688fa840268fb
Felix Mann <felix.fei.mann@gmail.com>
2019-10-15 16:56:36 -0700

    Fix yet another test scene :)

--- Assets/Scripts/Scenes/Test/Aircraft/GunshipTestGod.cs
+++ Assets/Scripts/Scenes/Test/Aircraft/GunshipTestGod.cs
@@ -6,31 +6,40 @@ using BattleCruisers.Buildables.Units.Ships;
 using BattleCruisers.Cruisers;
 using BattleCruisers.Scenes.Test.Utilities;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Aircraft
 {
     public class GunshipTestGod : TestGodBase
     {
+        private AttackBoatController[] _ships;
+        private GunShipController _gunship;
+
         public List<Vector2> gunshipPatrolPoints;
 
-        protected override void Start()
+        protected override IList<GameObject> GetGameObjects()
         {
-            base.Start();
+            _ships = FindObjectsOfType<AttackBoatController>();
+            IList<GameObject> gameObjects = _ships.Select(ship => ship.GameObject).ToList();
 
-            Helper helper = new Helper(updaterProvider: _updaterProvider);
+            _gunship = FindObjectOfType<GunShipController>();
+            gameObjects.Add(_gunship.GameObject);
 
+            return gameObjects;
+        }
+
+        protected override void Setup(Helper helper)
+        {
             ICruiser redCruiser = helper.CreateCruiser(Direction.Left, Faction.Reds);
 
             // Setup gunship
             IAircraftProvider aircraftProvider = helper.CreateAircraftProvider(gunshipPatrolPoints: gunshipPatrolPoints);
-            GunShipController gunship = FindObjectOfType<GunShipController>();
-            helper.InitialiseUnit(gunship, Faction.Blues, aircraftProvider: aircraftProvider, parentCruiserDirection: Direction.Left, enemyCruiser: redCruiser);
-            gunship.StartConstruction();
+            helper.InitialiseUnit(_gunship, Faction.Blues, aircraftProvider: aircraftProvider, parentCruiserDirection: Direction.Left, enemyCruiser: redCruiser);
+            _gunship.StartConstruction();
 
             // Setup target attack boats
-            AttackBoatController[] ships = FindObjectsOfType<AttackBoatController>();
-            foreach (AttackBoatController ship in ships)
+            foreach (AttackBoatController ship in _ships)
             {
                 helper.InitialiseUnit(ship, Faction.Reds);
                 ship.StartConstruction();
