d43800942390b44761520ba72ffde54d1048d486
Felix Mann <felix.fei.mann@gmail.com>
2019-10-15 16:58:41 -0700

    Fix yet another test scene :)

--- Assets/Scripts/Scenes/Test/Aircraft/PatrollingTestsGod.cs
+++ Assets/Scripts/Scenes/Test/Aircraft/PatrollingTestsGod.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Aircraft
 {
-    public class PatrollingTestsGod : MonoBehaviour 
+    public class PatrollingTestsGod : TestGodBase
 	{
-		public List<GameObject> patrolPoints;
+        private TestAircraftController _aircraft;
 
-		void Start() 
-		{
-			Helper helper = new Helper();
+        public List<GameObject> patrolPoints;
 
-			TestAircraftController aircraft = FindObjectOfType<TestAircraftController>();
+        protected override IList<GameObject> GetGameObjects()
+        {
+			_aircraft = FindObjectOfType<TestAircraftController>();
+
+            return new List<GameObject>()
+            {
+                _aircraft.GameObject
+            };
+        }
+
+        protected override void Setup(Helper helper)
+        {
 			IList<Vector2> patrolPointsAsVectors = patrolPoints.ConvertAll(gameObject => new Vector2(gameObject.transform.position.x, gameObject.transform.position.y));
-			aircraft.PatrolPoints = patrolPointsAsVectors;
-            helper.InitialiseUnit(aircraft);
-			aircraft.StartConstruction();
+			_aircraft.PatrolPoints = patrolPointsAsVectors;
+            helper.InitialiseUnit(_aircraft);
+			_aircraft.StartConstruction();
 		}
 	}
 }
