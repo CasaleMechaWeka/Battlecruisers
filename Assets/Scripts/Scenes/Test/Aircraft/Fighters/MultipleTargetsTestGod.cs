44f57220e9f8d6ac58984e116b8cc577f06a424f
Felix Mann <felix.fei.mann@gmail.com>
2019-10-15 17:03:28 -0700

    Fix yet another test scene :)

--- Assets/Scripts/Scenes/Test/Aircraft/Fighters/MultipleTargetsTestGod.cs
+++ Assets/Scripts/Scenes/Test/Aircraft/Fighters/MultipleTargetsTestGod.cs
@@ -4,18 +4,29 @@ using BattleCruisers.Buildables.Units.Aircraft;
 using BattleCruisers.Cruisers;
 using BattleCruisers.Scenes.Test.Utilities;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Aircraft.Fighters
 {
     public class MultipleTargetsTestGod : TestGodBase 
 	{
-        protected override void Start()
+        private FighterController _fighter;
+        private TestAircraftController[] _targets;
+
+        protected override IList<GameObject> GetGameObjects()
         {
-            base.Start();
+            _targets = FindObjectsOfType<TestAircraftController>();
+            IList<GameObject> gameObjects = _targets.Select(target => target.GameObject).ToList();
 
-            Helper helper = new Helper(updaterProvider: _updaterProvider);
+            _fighter = FindObjectOfType<FighterController>();
+            gameObjects.Add(_fighter.GameObject);
 
+            return gameObjects;
+        }
+
+        protected override void Setup(Helper helper)
+        {
             IList<Vector2> patrolPoints = new List<Vector2>()
             {
                 new Vector2(-12, 5),
@@ -25,14 +36,11 @@ namespace BattleCruisers.Scenes.Test.Aircraft.Fighters
             ICruiser redCruiser = helper.CreateCruiser(Direction.Left, Faction.Reds);
 
             // Fighter
-            FighterController fighter = FindObjectOfType<FighterController>();
-            helper.InitialiseUnit(fighter, Faction.Blues, enemyCruiser: redCruiser);
-            fighter.StartConstruction();
+            helper.InitialiseUnit(_fighter, Faction.Blues, enemyCruiser: redCruiser);
+            _fighter.StartConstruction();
 
             // Targets
-            TestAircraftController[] targets = FindObjectsOfType<TestAircraftController>();
-
-            foreach (TestAircraftController target in targets)
+            foreach (TestAircraftController target in _targets)
             {
                 target.PatrolPoints = patrolPoints;
                 helper.InitialiseUnit(target, Faction.Reds);
