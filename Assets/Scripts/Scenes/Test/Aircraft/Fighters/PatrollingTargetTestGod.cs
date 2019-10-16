7768d710fa2dad8330fc69587df32dbb32d976b5
Felix Mann <felix.fei.mann@gmail.com>
2019-10-15 17:07:41 -0700

    Fix yet another test scene :)

--- Assets/Scripts/Scenes/Test/Aircraft/Fighters/PatrollingTargetTestGod.cs
+++ Assets/Scripts/Scenes/Test/Aircraft/Fighters/PatrollingTargetTestGod.cs
@@ -11,39 +11,46 @@ namespace BattleCruisers.Scenes.Test.Aircraft.Fighters
 {
     public class PatrollingTargetTestGod : TestGodBase
 	{
-		private Helper _helper;
-
 		public FighterController fighter1, fighter2, fighter3;
 		public TestAircraftController targetAircraft1, targetAircraft2, targetAircraft3;
 		public List<Vector2> patrolPoints1, patrolPoints2, patrolPoints3;
 
-        protected override void Start()
+        protected override IList<GameObject> GetGameObjects()
         {
-            base.Start();
+            return new List<GameObject>()
+            {
+                fighter1.GameObject,
+                targetAircraft1.GameObject,
+                fighter2.GameObject,
+                targetAircraft2.GameObject,
+                fighter3.GameObject,
+                targetAircraft3.GameObject
+            };
+        }
 
-            _helper = new Helper(updaterProvider: _updaterProvider);
-
-            SetupPair(fighter1, targetAircraft1, patrolPoints1);
-			SetupPair(fighter2, targetAircraft2, patrolPoints2);
-			SetupPair(fighter3, targetAircraft3, patrolPoints3);
+        protected override void Setup(Helper helper)
+        {
+            SetupPair(helper, fighter1, targetAircraft1, patrolPoints1);
+			SetupPair(helper, fighter2, targetAircraft2, patrolPoints2);
+			SetupPair(helper, fighter3, targetAircraft3, patrolPoints3);
 		}
 
-		private void SetupPair(FighterController fighter, TestAircraftController target, IList<Vector2> patrolPoints)
+		private void SetupPair(Helper helper, FighterController fighter, TestAircraftController target, IList<Vector2> patrolPoints)
 		{
-            ICruiser blueCruiser = _helper.CreateCruiser(Direction.Right, Faction.Blues);
-            ICruiser redCruiser = _helper.CreateCruiser(Direction.Left, Faction.Reds);
+            ICruiser blueCruiser = helper.CreateCruiser(Direction.Right, Faction.Blues);
+            ICruiser redCruiser = helper.CreateCruiser(Direction.Left, Faction.Reds);
 
             // Target
             target.PatrolPoints = patrolPoints;
-            _helper.InitialiseUnit(target, Faction.Blues);
+            helper.InitialiseUnit(target, Faction.Blues);
 			target.StartConstruction();
             Helper.SetupUnitForUnitMonitor(target, blueCruiser);
 
 			// Fighter
 			IList<TargetType> targetTypes = new List<TargetType>() { target.TargetType };
             ITargetFilter targetFilter = new FactionAndTargetTypeFilter(target.Faction, targetTypes);
-            ITargetFactories targetFactories = _helper.CreateTargetFactories(target.GameObject, redCruiser, blueCruiser, _updaterProvider, targetFilter);
-            _helper.InitialiseUnit(fighter, Faction.Reds, targetFactories: targetFactories);
+            ITargetFactories targetFactories = helper.CreateTargetFactories(target.GameObject, redCruiser, blueCruiser, _updaterProvider, targetFilter);
+            helper.InitialiseUnit(fighter, Faction.Reds, targetFactories: targetFactories);
 			fighter.StartConstruction();
 		}
 	}
