962ab7b293b8f7d0647e33ec7ecbbcf1c154107c
Felix Mann <felix.fei.mann@gmail.com>
2019-10-27 12:47:41 -0700

    Fix another test scene :P

--- Assets/Scripts/Scenes/Test/Factories/SlowFactoryTestGod.cs
+++ Assets/Scripts/Scenes/Test/Factories/SlowFactoryTestGod.cs
@@ -1,24 +1,40 @@
 ﻿using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Buildables.Units;
 using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Utils.BattleScene.Update;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace BattleCruisers.Scenes.Test.Factories
 {
-    public class SlowFactoryTestGod : MonoBehaviour
+    public class SlowFactoryTestGod : TestGodBase
     {
+        private Factory _factory;
         public UnitWrapper unitPrefab;
 
-        void Start()
+        protected override async Task<Helper> CreateHelperAsync(IUpdaterProvider updaterProvider)
         {
-            Helper helper = new Helper(buildSpeedMultiplier: 10);
+            return await HelperFactory.CreateHelperAsync(buildSpeedMultiplier: 5, updaterProvider: updaterProvider);
+        }
 
-            Factory factory = FindObjectOfType<Factory>();
-            Assert.IsNotNull(factory);
-            helper.InitialiseBuilding(factory);
-            factory.StartConstruction();
-            factory.CompletedBuildable += (sender, e) => factory.StartBuildingUnit(unitPrefab);
+        protected override IList<GameObject> GetGameObjects()
+        {
+            _factory = FindObjectOfType<Factory>();
+
+            return new List<GameObject>()
+            {
+                _factory.GameObject
+            };
+        }
+
+        protected override void Setup(Helper helper)
+        {
+            Assert.IsNotNull(_factory);
+            helper.InitialiseBuilding(_factory);
+            _factory.StartConstruction();
+            _factory.CompletedBuildable += (sender, e) => _factory.StartBuildingUnit(unitPrefab);
         }
     }
 }
