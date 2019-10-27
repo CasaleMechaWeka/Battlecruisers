cffce7305e780944a6b54fefa56b80304e5bc5a4
Felix Mann <felix.fei.mann@gmail.com>
2019-10-27 13:55:31 -0700

    Fix another test scene :P

--- Assets/Scripts/Scenes/Test/Projectiles/DamageApplierTestGod.cs
+++ Assets/Scripts/Scenes/Test/Projectiles/DamageApplierTestGod.cs
@@ -5,38 +5,57 @@ using BattleCruisers.Projectiles.DamageAppliers;
 using BattleCruisers.Projectiles.Stats;
 using BattleCruisers.Scenes.Test.Utilities;
 using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.Utils.Threading;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Projectiles
 {
-    public class DamageApplierTestGod : MonoBehaviour 
+    public class DamageApplierTestGod : TestGodBase
     {
-	    void Start() 
+        private IBuilding _baseTarget;
+        private TestAircraftController _aircraft;
+        private IBuilding[] _targets;
+
+        protected override List<GameObject> GetGameObjects()
         {
-            Helper helper = new Helper();
+            _baseTarget = FindObjectOfType<AirFactory>();
+			_aircraft = FindObjectOfType<TestAircraftController>();
+            _targets = FindObjectsOfType<Building>();
+
+            List<GameObject> gameObjects
+                = _targets
+                    .Select(target => target.GameObject)
+                    .ToList();
+            gameObjects.Add(_aircraft.GameObject);
+            return gameObjects;
+        }
+
+        protected override void Setup(Helper helper)
+        {
             Faction factionToTarget = Faction.Blues;
 
-
             // Setup targets
-            IBuildable baseTarget = FindObjectOfType<AirFactory>();
-			
-			TestAircraftController aircraft = FindObjectOfType<TestAircraftController>();
-			aircraft.UseDummyMovementController = true;
-            helper.InitialiseUnit(aircraft, factionToTarget);
+			_aircraft.UseDummyMovementController = true;
+            helper.InitialiseUnit(_aircraft, factionToTarget);
+            _aircraft.StartConstruction();
 
-            IBuilding[] targets = FindObjectsOfType<Building>();
-            foreach (IBuilding target in targets)
+            foreach (IBuilding target in _targets)
             {
                 helper.InitialiseBuilding(target, factionToTarget);
+                target.StartConstruction();
             }
-
 
             // Setup damage applier
             IDamageStats damageStats = new DamageStats(damage: 50, damageRadiusInM: 5);
             ITargetFilter targetFilter = new DummyTargetFilter(isMatchResult: true);
             IDamageApplier damageApplier = new AreaOfEffectDamageApplier(damageStats, targetFilter);
 
-            damageApplier.ApplyDamage(baseTarget, baseTarget.Position, damageSource: null);
+            TimeScaleDeferrer deferrer = GetComponent<TimeScaleDeferrer>();
+            deferrer.Defer(
+                () => damageApplier.ApplyDamage(_baseTarget, _baseTarget.Position, damageSource: null),
+                delayInS: 1);
 	    }
     }
 }
