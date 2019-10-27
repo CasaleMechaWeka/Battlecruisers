8cae110f1f9034dd7c2df32f1f17e5d4284e610b
Felix Mann <felix.fei.mann@gmail.com>
2019-10-27 13:35:00 -0700

    Fix another test scene :P

--- Assets/Scripts/Scenes/Test/Turrets/BarrelControllerTestGod.cs
+++ Assets/Scripts/Scenes/Test/Turrets/BarrelControllerTestGod.cs
@@ -4,25 +4,32 @@ using BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers;
 using BattleCruisers.Projectiles.Stats;
 using BattleCruisers.Scenes.Test.Utilities;
 using NSubstitute;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test
 {
     public abstract class BarrelControllerTestGod : TestGodBase
 	{
-		public GameObject targetGameObject;
+        private BarrelController[] _turretBarrels;
+        public GameObject targetGameObject;
 
-        protected override void Start()
+        protected override List<GameObject> GetGameObjects()
         {
-            base.Start();
+            _turretBarrels = FindObjectsOfType<BarrelController>();
 
-            Helper helper = new Helper();
+            return
+                _turretBarrels
+                    .Select(barrel => barrel.gameObject)
+                    .ToList();
+        }
 
+        protected override void Setup(Helper helper)
+        {
             ITarget target = CreateTarget(targetGameObject.transform.position);
 
-            BarrelController[] turretBarrels = FindObjectsOfType<BarrelController>();
-
-            foreach (BarrelController barrel in turretBarrels)
+            foreach (BarrelController barrel in _turretBarrels)
 			{
 				barrel.StaticInitialise();
 				barrel.Target = target;
