f9c43e0a4e40269c64d8561eda3066888a90bcfa
Felix Mann <felix.fei.mann@gmail.com>
2019-10-27 13:41:57 -0700

    Fix another test scene :P

--- Assets/Scripts/Scenes/Test/Performance/ObjectPooling/UnitRecyclingTestGod.cs
+++ Assets/Scripts/Scenes/Test/Performance/ObjectPooling/UnitRecyclingTestGod.cs
@@ -1,7 +1,11 @@
 ﻿using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Buildables.Units;
 using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Utils.BattleScene.Update;
 using BattleCruisers.Utils.Threading;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.Assertions;
 using BCUtils = BattleCruisers.Utils;
 
@@ -9,26 +13,37 @@ namespace BattleCruisers.Scenes.Test.Performance.ObjectPooling
 {
     public class UnitRecyclingTestGod : TestGodBase
     {
+        private Factory _factory;
         public UnitWrapper unitPrefab;
 
-        protected override void Start()
+        protected override async Task<Helper> CreateHelperAsync(IUpdaterProvider updaterProvider)
         {
-            base.Start();
+            return await HelperFactory.CreateHelperAsync(buildSpeedMultiplier: BCUtils.BuildSpeedMultipliers.FAST, updaterProvider: updaterProvider);
+        }
 
+        protected override List<GameObject> GetGameObjects()
+        {
+            _factory = FindObjectOfType<Factory>();
+            Assert.IsNotNull(_factory);
+
+            return new List<GameObject>()
+            {
+                _factory.GameObject
+            };
+        }
+
+        protected override void Setup(Helper helper)
+        {
             Assert.IsNotNull(unitPrefab);
             unitPrefab.StaticInitialise();
-
-            Helper helper = new Helper(buildSpeedMultiplier: BCUtils.BuildSpeedMultipliers.FAST, updaterProvider: _updaterProvider);
 
             TimeScaleDeferrer deferrer = GetComponent<TimeScaleDeferrer>();
             Assert.IsNotNull(deferrer);
 
-            Factory factory = FindObjectOfType<Factory>();
-            Assert.IsNotNull(factory);
-            helper.InitialiseBuilding(factory);
-            factory.StartConstruction();
-            factory.CompletedBuildable += (sender, e) => factory.StartBuildingUnit(unitPrefab);
-            factory.UnitCompleted += (sender, e) => deferrer.Defer(e.CompletedUnit.Destroy, delayInS: 1);
+            helper.InitialiseBuilding(_factory);
+            _factory.StartConstruction();
+            _factory.CompletedBuildable += (sender, e) => _factory.StartBuildingUnit(unitPrefab);
+            _factory.UnitCompleted += (sender, e) => deferrer.Defer(e.CompletedUnit.Destroy, delayInS: 1);
         }
     }
 }
