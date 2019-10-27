7d393ef2fcbf23f3e953122a9e4e674138ad97d8
Felix Mann <felix.fei.mann@gmail.com>
2019-10-27 13:20:14 -0700

    Fix another test scene :P

--- Assets/Scripts/Scenes/Test/Offensive/NukeVsCruiserTestGod.cs
+++ Assets/Scripts/Scenes/Test/Offensive/NukeVsCruiserTestGod.cs
@@ -5,33 +5,53 @@ using BattleCruisers.Cruisers;
 using BattleCruisers.Scenes.Test.Utilities;
 using BattleCruisers.Targets.TargetFinders.Filters;
 using NSubstitute;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace BattleCruisers.Scenes.Test.Offensive
 {
     public class NukeVsCruiserTestGod : CameraToggleTestGod
 	{
+        private Cruiser _cruiser;
+        private ShieldGenerator _shield;
+        private NukeLauncherController _launcher;
+
+        protected override List<GameObject> GetGameObjects()
+        {
+            _cruiser = FindObjectOfType<Cruiser>();
+            Assert.IsNotNull(_cruiser);
+
+            _shield = FindObjectOfType<ShieldGenerator>();
+            Assert.IsNotNull(_shield);
+
+            _launcher = FindObjectOfType<NukeLauncherController>();
+            Assert.IsNotNull(_launcher);
+
+            return new List<GameObject>()
+            {
+                _cruiser.GameObject,
+                _shield.GameObject,
+                _launcher.GameObject
+            };
+        }
+
         protected override void Setup(Helper helper)
         {
             // Setup cruiser
-            Cruiser cruiser = FindObjectOfType<Cruiser>();
-            Assert.IsNotNull(cruiser);
-            helper.SetupCruiser(cruiser);
+            helper.SetupCruiser(_cruiser);
 
             // Setup shield
-            ShieldGenerator shield = FindObjectOfType<ShieldGenerator>();
-            Assert.IsNotNull(shield);
-            helper.InitialiseBuilding(shield, Faction.Reds);
-            shield.StartConstruction();
+            helper.InitialiseBuilding(_shield, Faction.Reds);
+            _shield.StartConstruction();
 
 			// Setup nuke launcher
 			IExactMatchTargetFilter targetFilter = Substitute.For<IExactMatchTargetFilter>();
-			targetFilter.IsMatch(cruiser).Returns(true);
-            ITargetFactories targetFactories = helper.CreateTargetFactories(cruiser.GameObject, exactMatchTargetFilter: targetFilter);
+			targetFilter.IsMatch(_cruiser).Returns(true);
+            ITargetFactories targetFactories = helper.CreateTargetFactories(_cruiser.GameObject, exactMatchTargetFilter: targetFilter);
 
-			NukeLauncherController launcher = FindObjectOfType<NukeLauncherController>();
-            helper.InitialiseBuilding(launcher, Faction.Blues, enemyCruiser: cruiser, targetFactories: targetFactories);
-			launcher.StartConstruction();
+            helper.InitialiseBuilding(_launcher, Faction.Blues, enemyCruiser: _cruiser, targetFactories: targetFactories);
+			_launcher.StartConstruction();
 		}
 	}
 }
