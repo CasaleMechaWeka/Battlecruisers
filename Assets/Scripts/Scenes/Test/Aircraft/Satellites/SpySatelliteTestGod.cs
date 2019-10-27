76f6f3e9c03170bad89ad184c6e04ce9998e0061
Felix Mann <felix.fei.mann@gmail.com>
2019-10-26 18:34:53 -0700

    Fix another test scene :P

--- Assets/Scripts/Scenes/Test/Aircraft/Satellites/SpySatelliteTestGod.cs
+++ Assets/Scripts/Scenes/Test/Aircraft/Satellites/SpySatelliteTestGod.cs
@@ -1,24 +1,33 @@
 ﻿using BattleCruisers.Buildables.Units.Aircraft;
 using BattleCruisers.Buildables.Units.Aircraft.Providers;
 using BattleCruisers.Scenes.Test.Utilities;
-using BCUtils = BattleCruisers.Utils;
+using System.Collections.Generic;
 using UnityEngine;
+using BCUtils = BattleCruisers.Utils;
 
 namespace BattleCruisers.Scenes.Test.Aircraft.Satellites
 {
-    public class SpySatelliteTestGod : MonoBehaviour
+    public class SpySatelliteTestGod : TestGodBase
     {
-        void Start()
-        {
-            Helper helper = new Helper();
+        private SpySatelliteController _satellite;
 
+        protected override IList<GameObject> GetGameObjects()
+        {
+            _satellite = FindObjectOfType<SpySatelliteController>();
+            return new List<GameObject>()
+            {
+                _satellite.GameObject
+            };
+        }
+
+        protected override void Setup(Helper helper)
+        {
             Vector2 parentCruiserPosition = new Vector2(-35, 0);
             Vector2 enemyCruiserPosition = new Vector2(35, 0);
             IAircraftProvider aircraftProvider = new AircraftProvider(parentCruiserPosition, enemyCruiserPosition, BCUtils.RandomGenerator.Instance);
 
-            SpySatelliteController satellite = FindObjectOfType<SpySatelliteController>();
-            helper.InitialiseUnit(satellite, aircraftProvider: aircraftProvider);
-            satellite.StartConstruction();
+            helper.InitialiseUnit(_satellite, aircraftProvider: aircraftProvider);
+            _satellite.StartConstruction();
         }
     }
 }
