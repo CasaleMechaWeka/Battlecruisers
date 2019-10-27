7b5d1066ea2f304a60e2f41dafc5c699dcfcbcf4
Felix Mann <felix.fei.mann@gmail.com>
2019-10-26 18:05:13 -0700

    Test runs, but laser stays when deathstar dies :P

--- Assets/Scripts/Scenes/Test/Aircraft/Satellites/DeathstarDeathWhileFiringTestGod.cs
+++ Assets/Scripts/Scenes/Test/Aircraft/Satellites/DeathstarDeathWhileFiringTestGod.cs
@@ -3,6 +3,7 @@ using BattleCruisers.Buildables.Buildings;
 using BattleCruisers.Buildables.Buildings.Factories;
 using BattleCruisers.Buildables.Units.Aircraft.Providers;
 using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections.Generic;
 using UnityEngine;
 using BCUtils = BattleCruisers.Utils;
 
@@ -11,16 +12,23 @@ namespace BattleCruisers.Scenes.Test.Aircraft.Satellites
     public class DeathstarDeathWhileFiringTestGod : TestGodBase
     {
         private SatelliteLauncherController _launcher;
+        private AirFactory _target;
 
-        protected override void Start()
+        protected override IList<GameObject> GetGameObjects()
         {
-            base.Start();
-
-            Helper helper = new Helper(updaterProvider: _updaterProvider);
-
-            // Satellite
             _launcher = FindObjectOfType<SatelliteLauncherController>();
+            _target = FindObjectOfType<AirFactory>();
 
+            return new List<GameObject>()
+            {
+                _launcher.GameObject,
+                _target.GameObject
+            };
+        }
+
+        protected override void Setup(Helper helper)
+        {
+            // Satellite
             Vector2 parentCruiserPosition = _launcher.transform.position;
             Vector2 enemyCruiserPosition = new Vector2(_launcher.transform.position.x + 30, _launcher.transform.position.y);
             IAircraftProvider aircraftProvider = new AircraftProvider(parentCruiserPosition, enemyCruiserPosition, BCUtils.RandomGenerator.Instance);
@@ -29,9 +37,8 @@ namespace BattleCruisers.Scenes.Test.Aircraft.Satellites
             _launcher.StartConstruction();
 
             // Target
-            AirFactory target = FindObjectOfType<AirFactory>();
-            helper.InitialiseBuilding(target, Faction.Reds);
-            target.StartConstruction();
+            helper.InitialiseBuilding(_target, Faction.Reds);
+            _target.StartConstruction();
         }
 
         public void DestroyLaunchers()
