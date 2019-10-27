0e6180643b4fbe6653e783c8c0b7ea56832bf266
Felix Mann <felix.fei.mann@gmail.com>
2019-10-27 15:44:58 -0700

    Fix another test scene :P

--- Assets/Scripts/Scenes/Test/Turrets/MissileBarrelTestGod.cs
+++ Assets/Scripts/Scenes/Test/Turrets/MissileBarrelTestGod.cs
@@ -5,41 +5,48 @@ using BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers;
 using BattleCruisers.Scenes.Test.Utilities;
 using BattleCruisers.Targets.TargetFinders.Filters;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Turrets
 {
     public class MissileBarrelTestGod : TestGodBase 
     {
-	    protected override void Start() 
+        private AirFactory _target;
+        private MissileBarrelController _missileBarrel;
+
+        protected override List<GameObject> GetGameObjects()
         {
-            base.Start();
+            _target = FindObjectOfType<AirFactory>();
+            _missileBarrel = FindObjectOfType<MissileBarrelController>();
 
+            return new List<GameObject>()
+            {
+                _target.GameObject,
+                _missileBarrel.gameObject
+            };
+        }
 
-            Helper helper = new Helper();
-
-
+        protected override void Setup(Helper helper)
+        {
             // Setup target
-            AirFactory target = FindObjectOfType<AirFactory>();
-            helper.InitialiseBuilding(target, Faction.Reds);
-            target.StartConstruction();
-
+            helper.InitialiseBuilding(_target, Faction.Reds);
+            _target.StartConstruction();
 
             // Setup missile barrel controller
             IList<TargetType> targetTypes = new List<TargetType>() { TargetType.Buildings };
             ITargetFilter targetFilter = new FactionAndTargetTypeFilter(Faction.Reds, targetTypes);
 
-            MissileBarrelController missileBarrel = FindObjectOfType<MissileBarrelController>();
-            missileBarrel.StaticInitialise();
-			missileBarrel.Target = target;
+            _missileBarrel.StaticInitialise();
+			_missileBarrel.Target = _target;
 
             IBarrelControllerArgs barrelControllerArgs
                 = helper.CreateBarrelControllerArgs(
-                    missileBarrel, 
+                    _missileBarrel, 
                     _updaterProvider.PerFrameUpdater,
                     targetFilter: targetFilter,
                     angleCalculator: new StaticAngleCalculator(new AngleHelper(), desiredAngleInDegrees: 90));
 
-            missileBarrel.InitialiseAsync(barrelControllerArgs);
+            _missileBarrel.InitialiseAsync(barrelControllerArgs);
 	    }
 	}
 }
