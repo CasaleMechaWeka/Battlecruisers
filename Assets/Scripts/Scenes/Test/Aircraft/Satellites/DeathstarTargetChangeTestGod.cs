952203b28bb6b960e791c7bd89f7fc45b22b6ff1
Felix Mann <felix.fei.mann@gmail.com>
2019-10-26 18:27:08 -0700

    Fix another test scene :P

--- Assets/Scripts/Scenes/Test/Aircraft/Satellites/DeathstarTargetChangeTestGod.cs
+++ Assets/Scripts/Scenes/Test/Aircraft/Satellites/DeathstarTargetChangeTestGod.cs
@@ -3,7 +3,9 @@ using BattleCruisers.Buildables.Buildings;
 using BattleCruisers.Buildables.Units.Aircraft;
 using BattleCruisers.Buildables.Units.Aircraft.Providers;
 using BattleCruisers.Scenes.Test.Utilities;
-using BattleCruisers.Utils.Fetchers;
+using BattleCruisers.Utils.BattleScene.Update;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using BCUtils = BattleCruisers.Utils;
 
@@ -11,15 +13,23 @@ namespace BattleCruisers.Scenes.Test.Aircraft.Satellites
 {
     public class DeathstarTargetChangeTestGod : TestGodBase
     {
+        private DeathstarController _deathstar;
         private Helper _helper;
 
         public BuildingWrapper targetPrefab;
 
-        protected override void Start()
+        protected override IList<GameObject> GetGameObjects()
         {
-            base.Start();
+            _deathstar = FindObjectOfType<DeathstarController>();
+            return new List<GameObject>()
+            {
+                _deathstar.GameObject
+            };
+        }
 
-            _helper = new Helper(buildSpeedMultiplier: BCUtils.BuildSpeedMultipliers.DEFAULT, updaterProvider: _updaterProvider);
+        protected override void Setup(Helper helper)
+        {
+            _helper = helper;
 
             // Setup target 1
             Vector2 target1Position = new Vector2(-3, 0);
@@ -34,9 +44,8 @@ namespace BattleCruisers.Scenes.Test.Aircraft.Satellites
             Vector2 enemyCruiserPosition = new Vector2(0, 0);
             IAircraftProvider aircraftProvider = new AircraftProvider(parentCruiserPosition, enemyCruiserPosition, BCUtils.RandomGenerator.Instance);
 
-            DeathstarController deathstar = FindObjectOfType<DeathstarController>();
-            _helper.InitialiseUnit(deathstar, Faction.Blues, aircraftProvider: aircraftProvider);
-            deathstar.StartConstruction();
+            _helper.InitialiseUnit(_deathstar, Faction.Blues, aircraftProvider: aircraftProvider);
+            _deathstar.StartConstruction();
         }
 
         private void ConstructBuilding(Vector2 position)
@@ -52,6 +61,11 @@ namespace BattleCruisers.Scenes.Test.Aircraft.Satellites
 
             // Rebuild building instantly :D
             building.Destroyed += (sender, e) => ConstructBuilding(position);
+        }
+
+        protected async override Task<Helper> CreateHelperAsync(IUpdaterProvider updaterProvider)
+        {
+            return await HelperFactory.CreateHelperAsync(buildSpeedMultiplier: BCUtils.BuildSpeedMultipliers.DEFAULT, updaterProvider: _updaterProvider);
         }
     }
 }
