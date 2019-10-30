886d9a959f87cde708b19074e62cf793e0f6245c
Felix Mann <felix.fei.mann@gmail.com>
2019-10-29 17:31:51 -0700

    Fix another test scene :)

--- Assets/Scripts/Scenes/Test/Turrets/AntiAir/AntiAirVsPatrollingAircraftTestGod.cs
+++ Assets/Scripts/Scenes/Test/Turrets/AntiAir/AntiAirVsPatrollingAircraftTestGod.cs
@@ -1,8 +1,8 @@
 ﻿using BattleCruisers.Buildables;
 using BattleCruisers.Buildables.Buildings.Turrets;
 using BattleCruisers.Scenes.Test.Utilities;
-using BattleCruisers.Utils.Threading;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -15,23 +15,28 @@ namespace BattleCruisers.Scenes.Test.Turrets.AnitAir
 
 		public Vector2[] aircraftPatrolPoints;
 
-		protected override void Start () 
-		{
-            base.Start();
+        protected override List<GameObject> GetGameObjects()
+        {
+			_aircraft = FindObjectsOfType<TestAircraftController>();
+			Assert.IsTrue(_aircraft.Length > 0);
 
-            TimeScaleDeferrer timeScaleDeferrer = GetComponent<TimeScaleDeferrer>();
-            Assert.IsNotNull(timeScaleDeferrer);
-
-            Helper helper = new Helper(deferrer: timeScaleDeferrer, updaterProvider: _updaterProvider);
-
-            // Setup turret
             _turret = FindObjectOfType<TurretController>();
 			Assert.IsNotNull(_turret);
+
+            List<GameObject> gameObjects
+                = _aircraft
+                    .Select(aircraft => aircraft.GameObject)
+                    .ToList();
+            gameObjects.Add(_turret.GameObject);
+            return gameObjects;
+        }
+
+        protected override void Setup(Helper helper)
+        {
+            // Setup turret
             helper.InitialiseBuilding(_turret, faction: Faction.Reds);
 			_turret.StartConstruction();
 
-			_aircraft = FindObjectsOfType<TestAircraftController>();
-			Assert.IsTrue(_aircraft.Length > 0);
 			InitialisePlanes(helper, _aircraft, Faction.Blues);
 		}
 
