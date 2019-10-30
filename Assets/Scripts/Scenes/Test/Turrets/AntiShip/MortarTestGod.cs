5479bf12df66572f7374e8c080ad0396545524b4
Felix Mann <felix.fei.mann@gmail.com>
2019-10-29 17:40:36 -0700

    Fix another test scene :)

--- Assets/Scripts/Scenes/Test/Turrets/AntiShip/MortarTestGod.cs
+++ Assets/Scripts/Scenes/Test/Turrets/AntiShip/MortarTestGod.cs
@@ -1,33 +1,42 @@
-﻿using System.Collections.Generic;
-using BattleCruisers.Buildables;
+﻿using BattleCruisers.Buildables;
 using BattleCruisers.Buildables.Buildings.Turrets;
 using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Turrets.AntiShip
 {
     public abstract class MortarTestGod : TestGodBase
     {
+        private TurretController[] _mortars;
+        private TestAircraftController _target;
+
         protected abstract List<Vector2> TargetPatrolPoints { get; }
 
-        protected override void Start()
+        protected override List<GameObject> GetGameObjects()
         {
-            base.Start();
+            _mortars = FindObjectsOfType<TurretController>();
+            _target = FindObjectOfType<TestAircraftController>();
 
-            Helper helper = new Helper(updaterProvider: _updaterProvider);
+            List<GameObject> gameObjects
+                = _mortars
+                    .Select(mortar => mortar.GameObject)
+                    .ToList();
+            gameObjects.Add(_target.GameObject);
+            return gameObjects;
+        }
 
-
+        protected override void Setup(Helper helper)
+        {
             // Setup target
-            TestAircraftController target = FindObjectOfType<TestAircraftController>();
-            target.PatrolPoints = TargetPatrolPoints;
-            target.SetTargetType(TargetType.Ships);  // So mortars will attack this
-            helper.InitialiseUnit(target, Faction.Blues);
-            target.StartConstruction();
-
+            _target.PatrolPoints = TargetPatrolPoints;
+            _target.SetTargetType(TargetType.Ships);  // So mortars will attack this
+            helper.InitialiseUnit(_target, Faction.Blues);
+            _target.StartConstruction();
 
             // Setup mortars
-            TurretController[] mortars = FindObjectsOfType<TurretController>();
-            foreach (TurretController mortar in mortars)
+            foreach (TurretController mortar in _mortars)
             {
                 helper.InitialiseBuilding(mortar, Faction.Reds);
                 mortar.StartConstruction();
