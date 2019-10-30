79aabc5651bfb9fc5d7da0899a6d93f0b49f5175
Felix Mann <felix.fei.mann@gmail.com>
2019-10-29 18:09:27 -0700

    Remove TODO :P

--- Assets/Scripts/Scenes/Test/Balancing/Units/FactoryVsFactoryTestGod.cs
+++ Assets/Scripts/Scenes/Test/Balancing/Units/FactoryVsFactoryTestGod.cs
@@ -2,7 +2,6 @@
 {
     public class FactoryVsFactoryTestGod : MultiCameraTestGod<FactoryVsFactoryTest>
     {
-        // FELIX  Seems to have same implementation as all sibling classes?  Move to parent?
         protected override void InitialiseScenario(FactoryVsFactoryTest scenario)
         {
             scenario.Initialise(_baseHelper);
