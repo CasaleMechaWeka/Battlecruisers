c3b66147a8107214b2907d5239f9b7ac21644dc0
Felix Mann <felix.fei.mann@gmail.com>
2019-10-29 17:24:50 -0700

    Fix another test scene :)

--- Assets/Scripts/Scenes/Test/Turrets/AngleLimiters/AngleLimiterTestGod.cs
+++ Assets/Scripts/Scenes/Test/Turrets/AngleLimiters/AngleLimiterTestGod.cs
@@ -8,28 +8,36 @@ namespace BattleCruisers.Scenes.Test.Turrets.AntiShip
 {
     public class AngleLimiterTestGod : TestGodBase
     {
+        private TestAircraftController _target;
+        private TurretController _turret;
+
         public List<Vector2> targetPatrolPoints;
         public TargetType targetType;
 
-        protected override void Start()
+        protected override List<GameObject> GetGameObjects()
         {
-            base.Start();
+            _target = FindObjectOfType<TestAircraftController>();
+            _turret = FindObjectOfType<TurretController>();
 
-            Helper helper = new Helper(updaterProvider: _updaterProvider);
+            return new List<GameObject>()
+            {
+                _target.GameObject,
+                _turret.GameObject
+            };
+        }
 
-
+        protected override void Setup(Helper helper)
+        {
             // Setup target
-            TestAircraftController target = FindObjectOfType<TestAircraftController>();
-            target.PatrolPoints = targetPatrolPoints;
-            target.SetTargetType(targetType);
-            helper.InitialiseUnit(target, Faction.Blues);
-            target.StartConstruction();
+            _target.PatrolPoints = targetPatrolPoints;
+            _target.SetTargetType(targetType);
+            helper.InitialiseUnit(_target, Faction.Blues);
+            _target.StartConstruction();
 
 
             // Setup turret
-            TurretController turret = FindObjectOfType<TurretController>();
-            helper.InitialiseBuilding(turret, Faction.Reds);
-            turret.StartConstruction();
+            helper.InitialiseBuilding(_turret, Faction.Reds);
+            _turret.StartConstruction();
         }
     }
 }
