43a9ce1b4cab0a3aba3a6b0a926f8a34c15d1ea9
Felix Mann <felix.fei.mann@gmail.com>
2019-10-29 17:38:33 -0700

    Fix another test scene :)

--- Assets/Scripts/Scenes/Test/Turrets/AntiShip/AntiShipTurretLotsOfShipsTestGod.cs
+++ Assets/Scripts/Scenes/Test/Turrets/AntiShip/AntiShipTurretLotsOfShipsTestGod.cs
@@ -5,32 +5,45 @@ using BattleCruisers.Buildables.Units;
 using BattleCruisers.Cruisers;
 using BattleCruisers.Scenes.Test.Utilities;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Turrets.AntiShip
 {
     public class AntiShipTurretLotsOfShipsTestGod : TestGodBase 
 	{
+        private Factory _factory;
+        private TurretController[] _turrets;
+
         public UnitWrapper unitPrefab;
 
-		protected override void Start()
-		{
-            base.Start();
+        protected override List<GameObject> GetGameObjects()
+        {
+            _factory = FindObjectOfType<Factory>();
+            _turrets = FindObjectsOfType<TurretController>();
 
-			Helper helper = new Helper(updaterProvider: _updaterProvider);
+            List<GameObject> gameObjects
+                = _turrets
+                    .Select(turret => turret.GameObject)
+                    .ToList();
+            gameObjects.Add(_factory.GameObject);
+            return gameObjects;
+        }
 
+        protected override void Setup(Helper helper)
+        {
             unitPrefab.StaticInitialise();
 
             // Factory
-            Factory factory = FindObjectOfType<Factory>();
             ICruiser blueCruiser = helper.CreateCruiser(Direction.Right, Faction.Blues);
-            helper.InitialiseBuilding(factory, Faction.Blues, parentCruiserDirection: Direction.Right, parentCruiser: blueCruiser);
-            factory.CompletedBuildable += Factory_CompletedBuildable;
-            factory.StartConstruction();
-            Helper.SetupFactoryForUnitMonitor(factory, blueCruiser);
+            helper.InitialiseBuilding(_factory, Faction.Blues, parentCruiserDirection: Direction.Right, parentCruiser: blueCruiser);
+            _factory.CompletedBuildable += Factory_CompletedBuildable;
+            _factory.StartConstruction();
+            Helper.SetupFactoryForUnitMonitor(_factory, blueCruiser);
 
             // Turrets
-            TurretController[] turrets = FindObjectsOfType<TurretController>();
-            foreach (TurretController turret in turrets)
+            foreach (TurretController turret in _turrets)
             {
                 helper.InitialiseBuilding(turret, Faction.Reds);
 			    turret.StartConstruction();
