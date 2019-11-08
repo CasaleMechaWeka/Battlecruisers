80916cbb6807e512c4821f65c04ffdb629a64544
Felix Mann <felix.fei.mann@gmail.com>
2019-11-07 16:39:53 -0800

    Show level name and number on post battle screen :)
    (cherry picked from commit 011abc242fd4d199ad1404049d0ef1d10103d938)

---
+++ Assets/Scripts/UI/ScreensScene/PostBattleScreen/LevelNameController.cs
@@ -0,0 +1,35 @@
+﻿using BattleCruisers.Data;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.ScreensScene.PostBattleScreen
+{
+    public class LevelNameController : MonoBehaviour
+    {
+        public void Initialise(IApplicationModel appModel)
+        {
+            Assert.IsNotNull(appModel);
+
+            if (appModel.IsTutorial)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(true);
+
+                Text levelName = GetComponent<Text>();
+                Assert.IsNotNull(levelName);
+                levelName.text = CreateLevelName(appModel);
+            }
+        }
+
+        private string CreateLevelName(IApplicationModel appModel)
+        {
+            int levelNum = appModel.DataProvider.GameModel.LastBattleResult.LevelNum;
+            ILevel level = appModel.DataProvider.GetLevel(levelNum);
+            return $"#{levelNum} {level.Name}";
+        }
+    }
+}
