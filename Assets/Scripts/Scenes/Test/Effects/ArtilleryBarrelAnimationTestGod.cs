999dfff092d06ce4a65e4a6eb6753340de5ecb54
Felix Mann <felix.fei.mann@gmail.com>
2019-11-30 12:15:10 -0800

    Hot the blazes does Animator work?!?

---
+++ Assets/Scripts/Scenes/Test/Effects/ArtilleryBarrelAnimationTestGod.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Effects
+{
+    public class ArtilleryBarrelAnimationTestGod : MonoBehaviour
+    {
+        public Animator artilleryAnimator;
+
+        void Start()
+        {
+            Debug.Log("Yo");
+
+            artilleryAnimator.enabled = false;
+            Debug.Log($"artilleryAnimator.enabled: {artilleryAnimator.enabled}");
+        }
+    }
+}
