a8871a2b78034b1d2f4418f2873bed1261895490
Felix Mann <felix.fei.mann@gmail.com>
2019-11-30 12:56:06 -0800

    Can play animation :D  Had to set normalized time (time of animation to start at) :)

--- Assets/Scripts/Scenes/Test/Effects/ArtilleryBarrelAnimationTestGod.cs
+++ Assets/Scripts/Scenes/Test/Effects/ArtilleryBarrelAnimationTestGod.cs
@@ -10,8 +10,19 @@ namespace BattleCruisers.Scenes.Test.Effects
         {
             Debug.Log("Yo");
 
+            // FELIX  NEXT  Look at animator properties while debugging?  Find state names?
             //artilleryAnimator.enabled = false;
             Debug.Log($"artilleryAnimator.enabled: {artilleryAnimator.enabled}");
+        }
+
+        public void PlayAnimation()
+        {
+            Debug.Log("PlayAnimation");
+
+            artilleryAnimator.enabled = true;
+            //artilleryAnimator.Play("GIBBERISH");
+            //artilleryAnimator.Play("ArtilleryBarrel");
+            artilleryAnimator.Play("ArtilleryBarrel", layer: -1, normalizedTime: 0);
         }
     }
 }
