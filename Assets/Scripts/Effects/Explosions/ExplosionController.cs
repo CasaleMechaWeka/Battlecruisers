e1c0ec3aaa4503e58e949763b321539922e5be6f
Felix Mann <felix.fei.mann@gmail.com>
2019-12-01 10:51:43 -0800

    Create ParticleSystemGroupController :)

--- Assets/Scripts/Effects/Explosions/ExplosionController.cs
+++ Assets/Scripts/Effects/Explosions/ExplosionController.cs
@@ -1,26 +1,11 @@
-﻿using UnityEngine.Assertions;
-
-namespace BattleCruisers.Effects.Explosions
+﻿namespace BattleCruisers.Effects.Explosions
 {
-    public class ExplosionController : MonoBehaviourWrapper
+    public class ExplosionController : ParticleSystemGroupController
     {
         public virtual IExplosion Initialise()
         {
             IBroadcastingParticleSystem[] particleSystems = GetParticleSystems();
             return new Explosion(this, particleSystems);
-        }
-
-        protected virtual IBroadcastingParticleSystem[] GetParticleSystems()
-        {
-            BroadcastingParticleSystem[] particleSystems = GetComponentsInChildren<BroadcastingParticleSystem>();
-            Assert.IsTrue(particleSystems.Length != 0);
-
-            foreach (BroadcastingParticleSystem particleSystem in particleSystems)
-            {
-                particleSystem.Initialise();
-            }
-
-            return particleSystems;
         }
     }
 }
---
+++ Assets/Scripts/Effects/ParticleSystemGroupController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Assertions;
+
+namespace BattleCruisers.Effects
+{
+    public class ParticleSystemGroupController : MonoBehaviourWrapper
+    {
+        public IParticleSystemGroup CreateParticleSystemGroup()
+        {
+            IBroadcastingParticleSystem[] particleSystems = GetParticleSystems();
+            return new ParticleSystemGroup(particleSystems);
+        }
+
+        protected virtual IBroadcastingParticleSystem[] GetParticleSystems()
+        {
+            BroadcastingParticleSystem[] particleSystems = GetComponentsInChildren<BroadcastingParticleSystem>();
+            Assert.IsTrue(particleSystems.Length != 0);
+
+            foreach (BroadcastingParticleSystem particleSystem in particleSystems)
+            {
+                particleSystem.Initialise();
+            }
+
+            return particleSystems;
+        }
+    }
+}
