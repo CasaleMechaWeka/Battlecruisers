4ba239b4dd61ad52a05c82793c333c7bb808c578
Felix Mann <felix.fei.mann@gmail.com>
2019-12-04 17:06:29 -0800

    Add aircraft smoke to test scene :)

--- Assets/Scripts/Scenes/Test/Effects/Smokes/SmokeStatsTestGod.cs
+++ Assets/Scripts/Scenes/Test/Effects/Smokes/SmokeStatsTestGod.cs
@@ -8,28 +8,38 @@ namespace BattleCruisers.Scenes.Test.Effects.Smokes
     {
         private SmokeChanger _smokeChanger;
 
-        public ParticleSystem smoke;
+        public ParticleSystem shipSmoke, aircraftSmoke, buildingSmoke;
 
         void Start()
         {
-            Assert.IsNotNull(smoke);
-            smoke.Play();
+            Assert.IsNotNull(shipSmoke);
+            Assert.IsNotNull(aircraftSmoke);
+            // FELIX
+            //Assert.IsNotNull(buildingSmoke);
+
+            shipSmoke.Play();
+            aircraftSmoke.Play();
+            shipSmoke.Play();
+
             _smokeChanger = new SmokeChanger();
         }
 
         public void WeakSmoke()
         {
-            _smokeChanger.Change(smoke, StaticSmokeStats.Ship.Weak);
+            _smokeChanger.Change(shipSmoke, StaticSmokeStats.Ship.Weak);
+            _smokeChanger.Change(aircraftSmoke, StaticSmokeStats.Aircraft.Weak);
         }
 
         public void NormalSmoke()
         {
-            _smokeChanger.Change(smoke, StaticSmokeStats.Ship.Normal);
+            _smokeChanger.Change(shipSmoke, StaticSmokeStats.Ship.Normal);
+            _smokeChanger.Change(aircraftSmoke, StaticSmokeStats.Aircraft.Normal);
         }
 
         public void StrongSmoke()
         {
-            _smokeChanger.Change(smoke, StaticSmokeStats.Ship.Strong);
+            _smokeChanger.Change(shipSmoke, StaticSmokeStats.Ship.Strong);
+            _smokeChanger.Change(aircraftSmoke, StaticSmokeStats.Aircraft.Strong);
         }
     }
 }
