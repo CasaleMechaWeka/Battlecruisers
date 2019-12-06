7319782c0745f9cf28e3d2d3fd7d3912cf3e9e58
Felix Mann <felix.fei.mann@gmail.com>
2019-12-05 17:27:18 -0800

    Create ConstantDeferrer :)

---
+++ Assets/Scripts/Utils/Threading/ConstantDeferrer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.Threading
+{
+    public class ConstantDeferrer : IConstantDeferrer
+    {
+        private readonly IDeferrer _deferrer;
+        private readonly float _delayInMs;
+
+        public ConstantDeferrer(IDeferrer deferrer, float delayInMs)
+        {
+            Assert.IsNotNull(deferrer);
+
+            _deferrer = deferrer;
+            _delayInMs = delayInMs;
+        }
+
+        public void Defer(Action action)
+        {
+            _deferrer.Defer(action, _delayInMs);
+        }
+    }
+}
