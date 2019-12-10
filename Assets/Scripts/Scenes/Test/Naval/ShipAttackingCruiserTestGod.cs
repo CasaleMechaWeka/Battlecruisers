67df7512b067f1920ed9dc56c8af510a4b241234
Felix Mann <felix.fei.mann@gmail.com>
2019-12-09 17:22:39 -0800

    Add planes to frigate test scene :)

--- Assets/Scripts/Scenes/Test/Naval/ShipAttackingCruiserTestGod.cs
+++ Assets/Scripts/Scenes/Test/Naval/ShipAttackingCruiserTestGod.cs
@@ -2,33 +2,49 @@
 using BattleCruisers.Buildables.Units.Ships;
 using BattleCruisers.Scenes.Test.Utilities;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace BattleCruisers.Scenes.Test.Naval
 {
     public class ShipAttackingCruiserTestGod : TestGodBase
     {
+        private TestAircraftController[] _planes;
         private ShipController _ship;
+
+        public List<Vector2> aircraftPatorlPoints;
 
         protected override List<GameObject> GetGameObjects()
         {
-            _ship = FindObjectOfType<ShipController>();
+            _planes = FindObjectsOfType<TestAircraftController>();
+            List<GameObject> gameObjects
+                = _planes
+                    .Select(plane => plane.GameObject)
+                    .ToList();
 
-            return new List<GameObject>()
-            {
-                _ship.GameObject
-            };
+            _ship = FindObjectOfType<ShipController>();
+            gameObjects.Add(_ship.GameObject);
+
+            return gameObjects;
         }
 
         protected override void Setup(Helper helper)
         {
+            // Setup ship
+            helper.InitialiseUnit(_ship, Faction.Blues);
+            _ship.StartConstruction();
+
             // Setup fake cruiser
             TestTarget fakeCruiser = FindObjectOfType<TestTarget>();
             fakeCruiser.Initialise(Faction.Reds);
 
-            // Setup ship
-            helper.InitialiseUnit(_ship, Faction.Blues);
-            _ship.StartConstruction();
+            // Setup planes
+            foreach (TestAircraftController plane in _planes)
+            {
+                plane.PatrolPoints = aircraftPatorlPoints;
+                helper.InitialiseUnit(plane, Faction.Reds);
+                plane.StartConstruction();
+            }
         }
     }
 }
