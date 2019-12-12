c6d4eb6cd001f4bb09d0350991aa0391f59847cb
Felix Mann <felix.fei.mann@gmail.com>
2019-12-11 16:12:49 -0800

    Have both cruisers die (no explosions yet :P)

---
+++ Assets/Scripts/Scenes/Test/Effects/Deaths/CruiserDeathManagerTestGod.cs
@@ -0,0 +1,36 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections.Generic;
+using UnityEngine;
+using BCUtils = BattleCruisers.Utils;
+
+namespace BattleCruisers.Scenes.Test.Effects.Deaths
+{
+    public class CruiserDeathManagerTestGod : TestGodBase
+    {
+        public Cruiser playerCruiser, aiCruiser;
+
+        protected override List<GameObject> GetGameObjects()
+        {
+            BCUtils.Helper.AssertIsNotNull(playerCruiser, aiCruiser);
+
+            return new List<GameObject>()
+            {
+                playerCruiser.GameObject,
+                aiCruiser.GameObject
+            };
+        }
+
+        protected override void Setup(Helper helper)
+        {
+            CreateAndDestroyCruiser(playerCruiser, helper);
+            CreateAndDestroyCruiser(aiCruiser, helper);
+        }
+
+        private void CreateAndDestroyCruiser(Cruiser cruiser, Helper helper)
+        {
+            helper.SetupCruiser(cruiser);
+            cruiser.Destroy();
+        }
+    }
+}
