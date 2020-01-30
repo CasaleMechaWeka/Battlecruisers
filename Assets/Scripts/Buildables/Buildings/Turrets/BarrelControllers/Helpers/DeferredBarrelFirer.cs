53fd7a3ba3ff61d48b3b9ab10d42e4bf9a214d8a
Felix Mann <felix.fei.mann@gmail.com>
2020-01-29 17:28:34 -0800

    Create DeferredBarrelFirer :)

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/Helpers/DeferredBarrelFirer.cs
@@ -0,0 +1,26 @@
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.Threading;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers.Helpers
+{
+    // FELIX  Use, Test
+    public class DeferredBarrelFirer : IBarrelFirer
+    {
+        private readonly IBarrelFirer _coreFirer;
+        private readonly IConstantDeferrer _deferrer;
+
+        public DeferredBarrelFirer(IBarrelFirer coreFirer, IConstantDeferrer deferrer)
+        {
+            Helper.AssertIsNotNull(coreFirer, deferrer);
+
+            _coreFirer = coreFirer;
+            _deferrer = deferrer;
+        }
+
+        public void Fire(float fireAngleInDegrees)
+        {
+            Logging.Verbose(Tags.BARREL_CONTROLLER, $"fireAngleInDegrees: {fireAngleInDegrees}");
+            _deferrer.Defer(() => _coreFirer.Fire(fireAngleInDegrees));
+        }
+    }
+}
