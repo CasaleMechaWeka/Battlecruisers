8ccb754eff46400370eebb680af8a48a7390a2b5
Felix Mann <felix.fei.mann@gmail.com>
2020-01-30 16:44:25 -0800

    Create custom test scene for lascannon flap animation :P

---
+++ Assets/Scripts/Scenes/Test/Effects/TurretBarrels/LascannonFlapAnimationTestGod.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.Effects.TurretBarrels
+{
+    public class LascannonFlapAnimationTestGod : MonoBehaviour
+    {
+        public Animator flapAnimator;
+
+        private const string CLOSED_STATE = "ClosedFlap";
+        private const string OPENED_STATE = "OpenedFlap";
+
+        void Start()
+        {
+            Assert.IsNotNull(flapAnimator);
+
+        }
+    }
+}
