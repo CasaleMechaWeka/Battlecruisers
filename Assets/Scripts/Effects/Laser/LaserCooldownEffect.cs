9c288490913525e70e0417edaba7542a08f6ffed
Felix Mann <felix.fei.mann@gmail.com>
2020-02-03 17:04:27 -0800

    Implement LaserCooldownEffect :)

--- Assets/Scripts/Effects/Laser/LaserCooldownEffect.cs
+++ Assets/Scripts/Effects/Laser/LaserCooldownEffect.cs
@@ -1,8 +1,44 @@
-﻿namespace BattleCruisers.Effects.Laser
-{
-    // FELIX  Implement, use, test :)
-    public class LaserCooldownEffect
-    {
+﻿using BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers.FireInterval;
+using BattleCruisers.Effects.ParticleSystems;
+using BattleCruisers.Utils;
+using System;
 
+namespace BattleCruisers.Effects.Laser
+{
+    // FELIX  use, test :)
+    public class LaserCooldownEffect : IManagedDisposable
+    {
+        private readonly IFireIntervalManager _fireIntervalManager;
+        private readonly ILaserFlap _laserFlap;
+        private readonly IParticleSystemGroup _overheatingSmoke;
+
+        public LaserCooldownEffect(IFireIntervalManager fireIntervalManager, ILaserFlap laserFlap, IParticleSystemGroup overheatingSmoke)
+        {
+            Helper.AssertIsNotNull(fireIntervalManager, laserFlap, overheatingSmoke);
+
+            _fireIntervalManager = fireIntervalManager;
+            _laserFlap = laserFlap;
+            _overheatingSmoke = overheatingSmoke;
+
+            _fireIntervalManager.ShouldFire.ValueChanged += ShouldFire_ValueChanged;
+        }
+
+        private void ShouldFire_ValueChanged(object sender, EventArgs e)
+        {
+            if (_fireIntervalManager.ShouldFire.Value)
+            {
+                _laserFlap.CloseFlap();
+            }
+            else
+            {
+                _laserFlap.OpenFlap();
+                _overheatingSmoke.Play();
+            }
+        }
+
+        public void DisposeManagedState()
+        {
+            _fireIntervalManager.ShouldFire.ValueChanged -= ShouldFire_ValueChanged;
+        }
     }
 }
