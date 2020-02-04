c969c3cecd2af199d4b817d938acebe92f20b3d4
Felix Mann <felix.fei.mann@gmail.com>
2020-02-03 16:58:45 -0800

    Create ILaserFlap and implementation :)

---
+++ Assets/Scripts/Effects/Laser/ILaserFlap.cs
@@ -0,0 +1,8 @@
+﻿namespace BattleCruisers.Effects.Laser
+{
+    public interface ILaserFlap
+    {
+        void CloseFlap();
+        void OpenFlap();
+    }
+}
---
+++ Assets/Scripts/Effects/Laser/LaserFlapController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Effects.Laser
+{
+    // FELIX  use :)
+    public class LaserFlapController : MonoBehaviour, ILaserFlap
+    {
+        public Animator _flapAnimator;
+
+        private const string CLOSED_STATE = "ClosedFlap";
+        private const string OPENED_STATE = "OpenedFlap";
+
+        public void Initialise()
+        {
+            _flapAnimator = GetComponent<Animator>();
+            Assert.IsNotNull(_flapAnimator);
+        }
+
+        public void OpenFlap()
+        {
+            _flapAnimator.Play(OPENED_STATE, layer: -1, normalizedTime: 0);
+        }
+
+        public void CloseFlap()
+        {
+            _flapAnimator.Play(CLOSED_STATE, layer: -1, normalizedTime: 0);
+        }
+    }
+}
