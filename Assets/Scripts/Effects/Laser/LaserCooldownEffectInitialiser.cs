78961ae093d2cce8b6f1e4fc449bef6679fdcd7c
Felix Mann <felix.fei.mann@gmail.com>
2020-02-03 17:19:55 -0800

    Create LaserCooldownEffectInitialiser :)

---
+++ Assets/Scripts/Effects/Laser/LaserCooldownEffectInitialiser.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers.FireInterval;
+using BattleCruisers.Effects.ParticleSystems;
+using BattleCruisers.Utils;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Effects.Laser
+{
+    public class LaserCooldownEffectInitialiser : MonoBehaviour
+    {
+        public IManagedDisposable CreateLaserCooldownEffect(IFireIntervalManager fireIntervalManager)
+        {
+            Assert.IsNotNull(fireIntervalManager);
+
+            LaserFlapController laserFlap = GetComponentInChildren<LaserFlapController>();
+            Assert.IsNotNull(laserFlap);
+            laserFlap.Initialise();
+
+            IParticleSystemGroupInitialiser smokeDischargeInitialiser = transform.FindNamedComponent<IParticleSystemGroupInitialiser>("SmokeDischarge");
+            IParticleSystemGroup smokeDischarge = smokeDischargeInitialiser.CreateParticleSystemGroup();
+
+            return new LaserCooldownEffect(fireIntervalManager, laserFlap, smokeDischarge);
+        }
+    }
+}
