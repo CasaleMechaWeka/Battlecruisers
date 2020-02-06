40b75595a081dbc9dac5a93482290c54155d4916
Felix Mann <felix.fei.mann@gmail.com>
2020-02-05 18:00:16 -0800

    Create ShipDeath stub :)

---
+++ Assets/Scripts/Effects/Deaths/ShipDeath.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BattleCruisers.Effects.Explosions;
+using BattleCruisers.Utils.BattleScene.Pools;
+using BattleCruisers.Utils.PlatformAbstractions;
+using UnityEngine;
+
+namespace BattleCruisers.Effects.Deaths
+{
+    // FELIX  Use, test
+    public class ShipDeath : IPoolable<Vector3>
+    {
+        private readonly IGameObject _shipDeathController;
+        private readonly IList<IExplosion> _explosions;
+        // FELIX  IBroadcastingAnimation?  Knows when it's done?
+        private readonly IAnimation _sinkingAnimation;
+
+        // FELIX  Handle :P
+        public event EventHandler Deactivated;
+
+        // FELIX  Constructor
+
+        public void Activate(Vector3 activationArgs)
+        {
+            _shipDeathController.IsVisible = true;
+            _shipDeathController.Position = activationArgs;
+
+            _sinkingAnimation.Play();
+
+            // FELIX  Explosions are abstracted...  Don't expose position :(  => Use existing pools?
+            //foreach (IExplosion explosion in _explosions)
+            //{
+            //    explosion.Activate(explosion.)
+            //}
+        }
+    }
+}
