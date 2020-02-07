09978fe6d4e65c096401e9b45b3e28e106471d4d
Felix Mann <felix.fei.mann@gmail.com>
2020-02-06 16:45:12 -0800

    Use IBroadcastingAnimation in ShipDeath :)

--- Assets/Scripts/Effects/Deaths/ShipDeath.cs
+++ Assets/Scripts/Effects/Deaths/ShipDeath.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using BattleCruisers.Effects.Explosions;
+using BattleCruisers.Utils;
 using BattleCruisers.Utils.BattleScene.Pools;
 using BattleCruisers.Utils.PlatformAbstractions;
 using UnityEngine;
@@ -12,14 +13,28 @@ namespace BattleCruisers.Effects.Deaths
     public class ShipDeath : IPoolable<Vector3>
     {
         private readonly IGameObject _shipDeathController;
-        private readonly IList<IExplosion> _explosions;
-        // FELIX  IBroadcastingAnimation?  Knows when it's done?
-        private readonly IAnimation _sinkingAnimation;
+        // FELIX  Add explosions!
+        //private readonly IList<IExplosion> _explosions;
+        private readonly IBroadcastingAnimation _sinkingAnimation;
 
-        // FELIX  Handle :P
         public event EventHandler Deactivated;
 
-        // FELIX  Constructor
+        public ShipDeath(IGameObject shipDeathController, IBroadcastingAnimation sinkingAnimation)
+        {
+            Helper.AssertIsNotNull(shipDeathController, sinkingAnimation);
+
+            _shipDeathController = shipDeathController;
+            _sinkingAnimation = sinkingAnimation;
+
+            // Assume sinking animaion takes longer than other effects.
+            _sinkingAnimation.AnimationDone += _sinkingAnimation_AnimationDone;
+        }
+
+        private void _sinkingAnimation_AnimationDone(object sender, EventArgs e)
+        {
+            _shipDeathController.IsVisible = false;
+            Deactivated?.Invoke(this, EventArgs.Empty);
+        }
 
         public void Activate(Vector3 activationArgs)
         {
