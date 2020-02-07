526c541259c5d655cc28e3e7950930b084e2d1fa
Felix Mann <felix.fei.mann@gmail.com>
2020-02-06 19:10:54 -0800

    Create ArchonDeathPoolChooser :)

---
+++ Assets/Scripts/Effects/Deaths/Pools/ArchonDeathPoolChooser.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Utils.BattleScene.Pools;
+using UnityEngine;
+
+namespace BattleCruisers.Effects.Deaths.Pools
+{
+    public class ArchonDeathPoolChooser : MonoBehaviour, IArchonDeathPoolChooser
+    {
+        public IPool<IShipDeath, Vector3> ChoosePool(IShipDeathPoolProvider shipDeathPoolProvider)
+        {
+            return shipDeathPoolProvider.ArchonPool;
+        }
+    }
+}
---
+++ Assets/Scripts/Effects/Deaths/Pools/IArchonDeathPoolChooser.cs
@@ -0,0 +1,10 @@
+﻿using BattleCruisers.Utils.BattleScene.Pools;
+using UnityEngine;
+
+namespace BattleCruisers.Effects.Deaths.Pools
+{
+    public interface IArchonDeathPoolChooser
+    {
+        IPool<IShipDeath, Vector3> ChoosePool(IShipDeathPoolProvider shipDeathPoolProvider);
+    }
+}
