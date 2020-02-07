7512a191915eb835757e2437f99508c87365d528
Felix Mann <felix.fei.mann@gmail.com>
2020-02-06 18:55:44 -0800

    Create ShipDeathFactory :)

---
+++ Assets/Scripts/Effects/Deaths/Pools/ShipDeathFactory.cs
@@ -0,0 +1,33 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.BattleScene.Pools;
+using BattleCruisers.Utils.Fetchers;
+using UnityEngine;
+
+namespace BattleCruisers.Effects.Deaths.Pools
+{
+    // FELIX  Test :)
+    public class ShipDeathFactory : IPoolableFactory<IShipDeath, Vector3>
+    {
+        private readonly IPrefabFactory _prefabFactory;
+        private readonly ShipDeathKey _shipDeathKey;
+
+        public ShipDeathFactory(IPrefabFactory prefabFactory, ShipDeathKey shipDeathKey)
+        {
+            Helper.AssertIsNotNull(prefabFactory, shipDeathKey);
+
+            _prefabFactory = prefabFactory;
+            _shipDeathKey = shipDeathKey;
+        }
+
+        public IShipDeath CreateItem()
+        {
+            return _prefabFactory.CreateShipDeath(_shipDeathKey);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(ShipDeathFactory)} {_shipDeathKey}";
+        }
+    }
+}
