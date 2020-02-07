94d58d1896875110c8137f7bc2cd06a723306b34
Felix Mann <felix.fei.mann@gmail.com>
2020-02-06 16:50:55 -0800

    Base test scene doesn't crash :P  (because everything's inactive...)

---
+++ Assets/Scripts/Scenes/Test/Effects/Deaths/ShipDeathAnimationTestGod.cs
@@ -0,0 +1,50 @@
+﻿using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Utils.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.Effects.Deaths
+{
+    public class ShipDeathAnimationTestGod : TestGodBase
+    {
+        private ShipController[] _ships;
+        private IDeferrer _deferrer;
+
+            // FELIX
+        //protected override List<GameObject> GetGameObjects()
+        //{
+        //    _ships = FindObjectsOfType<ShipController>();
+        //    return
+        //        _ships
+        //            .Select(ship => ship.GameObject)
+        //            .ToList();
+        //}
+
+        protected override void Setup(Helper helper)
+        {
+            // FELIX
+            //_deferrer = GetComponent<TimeScaleDeferrer>();
+            //Assert.IsNotNull(_deferrer);
+
+            //foreach (ShipController ship in _ships)
+            //{
+            //    helper.InitialiseUnit(ship);
+            //    ship.StartConstruction();
+            //    ship.CompletedBuildable += Ship_CompletedBuildable;
+            //}
+        }
+
+        // FELIX
+        //private void Ship_CompletedBuildable(object sender, EventArgs e)
+        //{
+        //    // Ship turrets are initialised when ship completes building, but this
+        //    // event handler is called first.  Hence wait for turrets to be initialised
+        //    // before destroying ship.
+        //    _deferrer.Defer(() => (sender as ShipController).Destroy(), delayInS: 0.1f);
+        //}
+    }
+}
