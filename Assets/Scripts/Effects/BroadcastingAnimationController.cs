be50f1d9157812cbbf1581e5b632f9cb3e61aec4
Felix Mann <felix.fei.mann@gmail.com>
2020-02-06 17:05:11 -0800

    Script gets informed when animation ends :)

---
+++ Assets/Scripts/Effects/BroadcastingAnimationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Effects
+{
+    public class BroadcastingAnimationController : MonoBehaviour, IBroadcastingAnimation
+    {
+        private Animator _animator;
+
+        private const string BARREL_ANIMATION_STATE = "BarrelAnimation";
+        private const int DEFAULT_ANIMATION_LAYER_INDEX = 0;
+
+        public event EventHandler AnimationDone;
+
+        public void Done()
+        {
+            Debug.Log("Done");
+        }
+
+        public void Play()
+        {
+            //CheckAnimationState();
+            //_animator.enabled = true;
+            //_animator.Play(BARREL_ANIMATION_STATE, layer: -1, normalizedTime: 0);
+        }
+
+        ///// <summary>
+        ///// If the game object is inactive, Animator.HasState() always returns false.
+        ///// Hence cannot perform this check at construction in case the game object is inactive.
+        ///// </summary>
+        //private void CheckAnimationState()
+        //{
+        //    int stateId = Animator.StringToHash(BARREL_ANIMATION_STATE);
+        //    Assert.IsTrue(_animator.HasState(DEFAULT_ANIMATION_LAYER_INDEX, stateId));
+        //}
+    }
+}
