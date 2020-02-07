c62a33beac0b1160e721eafe85cd38d2857cc8d8
Felix Mann <felix.fei.mann@gmail.com>
2020-02-06 17:23:22 -0800

    Create ShipDeathInitialiser :D

---
+++ Assets/Scripts/Effects/Deaths/ShipDeathInitialiser.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.Effects.ParticleSystems;
+using BattleCruisers.Utils.BattleScene.Pools;
+using BattleCruisers.Utils.PlatformAbstractions;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Effects.Deaths
+{
+    public class ShipDeathInitialiser : MonoBehaviour
+    {
+        public IPoolable<Vector3> CreateShipDeath()
+        {
+            BroadcastingAnimationController sinkingAnimation = GetComponent<BroadcastingAnimationController>();
+            Assert.IsNotNull(sinkingAnimation);
+
+            ParticleSystemGroupInitialiser[] particleSystemGroupInitialisers = GetComponentsInChildren<ParticleSystemGroupInitialiser>();
+            IList<IParticleSystemGroup> effects
+                = particleSystemGroupInitialisers
+                    .Select(initialiser => initialiser.CreateParticleSystemGroup())
+                    .ToList();
+
+            return
+                new ShipDeath(
+                    new GameObjectBC(gameObject),
+                    sinkingAnimation,
+                    effects);
+        }
+    }
+}
