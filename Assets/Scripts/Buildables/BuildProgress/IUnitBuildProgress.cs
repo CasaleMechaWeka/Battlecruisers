13e1f8dc4f5a23689c855b5898200cca588e7ccf
Felix Mann <felix.fei.mann@gmail.com>
2020-02-09 14:19:03 -0800

    Create UnitBuildProgress :)

---
+++ Assets/Scripts/Buildables/BuildProgress/IUnitBuildProgress.cs
@@ -0,0 +1,9 @@
+﻿using BattleCruisers.Buildables.Buildings.Factories;
+
+namespace BattleCruisers.Buildables.BuildProgress
+{
+    public interface IUnitBuildProgress
+    {
+        IFactory Factory { set; }
+    }
+}
---
+++ Assets/Scripts/Buildables/BuildProgress/UnitBuildProgress.cs
@@ -0,0 +1,78 @@
+﻿using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Cruisers.Construction;
+using System;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.BuildProgress
+{
+    // FELIX  Interface, use, test :)
+    public class UnitBuildProgress : IUnitBuildProgress
+    {
+        private readonly string _unitName;
+        private readonly IBuildProgressFeedback _buildProgressFeedback;
+
+        private IFactory _factory;
+        public IFactory Factory
+        {
+            private get => _factory;
+            set
+            {
+                if (_factory != null)
+                {
+                    _factory.UnitStarted -= _factory_StartedBuildingUnit;
+                    _factory.NewUnitChosen -= _factory_NewUnitChosen;
+                    _factory.UnitUnderConstructionDestroyed -= _factory_UnitUnderConstructionDestroyed;
+                }
+
+                _factory = value;
+
+                if (_factory != null)
+                {
+                    ShowBuildProgressIfNecessary(_factory.UnitUnderConstruction);
+
+                    _factory.UnitStarted += _factory_StartedBuildingUnit;
+                    _factory.NewUnitChosen += _factory_NewUnitChosen;
+                    _factory.UnitUnderConstructionDestroyed += _factory_UnitUnderConstructionDestroyed;
+                }
+            }
+        }
+
+        public UnitBuildProgress(string unitName, IBuildProgressFeedback buildProgressFeedback)
+        {
+            Assert.IsFalse(string.IsNullOrWhiteSpace(unitName));
+            Assert.IsNotNull(buildProgressFeedback);
+
+            _unitName = unitName;
+            _buildProgressFeedback = buildProgressFeedback;
+        }
+
+        private void _factory_StartedBuildingUnit(object sender, UnitStartedEventArgs e)
+        {
+            ShowBuildProgressIfNecessary(e.StartedUnit);
+        }
+
+        private void _factory_NewUnitChosen(object sender, EventArgs e)
+        {
+            ShowBuildProgressIfNecessary(_factory.UnitWrapper?.Buildable);
+        }
+
+        private void _factory_UnitUnderConstructionDestroyed(object sender, EventArgs e)
+        {
+            ShowBuildProgressIfNecessary(_factory.UnitWrapper?.Buildable);
+        }
+
+        private void ShowBuildProgressIfNecessary(IUnit unitUnderConstruction)
+        {
+            if (unitUnderConstruction != null
+                && unitUnderConstruction.Name == _unitName)
+            {
+                _buildProgressFeedback.ShowBuildProgress(unitUnderConstruction, _factory);
+            }
+            else
+            {
+                _buildProgressFeedback.HideBuildProgress();
+            }
+        }
+    }
+}
