2aaac89b0a4832d495ab11d6f4b22a7f9058a020
Felix Mann <felix.fei.mann@gmail.com>
2020-02-10 17:04:49 -0800

    Create (start of) TeslaCoilBalancingTest scene :)

---
+++ Assets/Scripts/Scenes/Test/Balancing/TeslaCoilBalancingTestGod.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Turrets;
+using BattleCruisers.Scenes.Test.Offensive;
+using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Balancing
+{
+    public class TeslaCoilBalancingTestGod : OffensiveTurretTestGod
+	{
+        private TeslaCoil _tesla;
+
+        protected override List<GameObject> GetGameObjects()
+        {
+            _tesla = FindObjectOfType<TeslaCoil>();
+
+            List<GameObject> gameObjects = base.GetGameObjects();
+            gameObjects.Add(_tesla.GameObject);
+            return gameObjects;
+        }
+
+        protected override void Setup(Helper helper)
+        {
+            base.Setup(helper);
+
+            helper.InitialiseBuilding(_tesla, Faction.Blues);
+			_tesla.StartConstruction();
+		}
+	}
+}
