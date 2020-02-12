29713404be6219d6818f24423208e2f8036e5aff
Felix Mann <felix.fei.mann@gmail.com>
2020-02-11 17:30:14 -0800

    Turn off logging :)

--- Assets/Scripts/Utils/Logging.cs
+++ Assets/Scripts/Utils/Logging.cs
@@ -179,9 +179,7 @@ namespace BattleCruisers.Utils
             tagsToActiveness.Add(Tags.DRONE_CONSUMER_PROVIDER, false);
 
             // Movement
-            // FELIX
-            tagsToActiveness.Add(Tags.MOVEMENT, true);
-            //tagsToActiveness.Add(Tags.MOVEMENT, false);
+            tagsToActiveness.Add(Tags.MOVEMENT, false);
             tagsToActiveness.Add(Tags.ROTATION_HELPER, false);
             tagsToActiveness.Add(Tags.ROTATION_MOVEMENT_CONTROLLER, false);
             tagsToActiveness.Add(Tags.SHIP_MOVEMENT_DECIDER, false);
