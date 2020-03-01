ed7edf660d529a78219eb091c9a13e9d9a15f79b
Felix Mann <felix.fei.mann@gmail.com>
2020-03-01 14:02:43 -0800

    Create ArchonBlocksFriendliesTest scene :)

---
+++ Assets/Scripts/Scenes/Test/Naval/ArchonBlocksFriendliesTestGod.cs
@@ -0,0 +1,41 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections.Generic;
+using UnityEngine;
+using BCUtils = BattleCruisers.Utils;
+
+namespace BattleCruisers.Scenes.Test.Naval
+{
+    public class ArchonBlocksFriendliesTestGod : TestGodBase
+    {
+        public ArchonBattleshipController archon;
+        public AttackBoatController attackBoat;
+
+        protected override List<GameObject> GetGameObjects()
+        {
+            BCUtils.Helper.AssertIsNotNull(archon, attackBoat);
+
+            return new List<GameObject>()
+            {
+                archon.GameObject,
+                attackBoat.GameObject
+            };
+        }
+
+        protected override void Setup(Helper helper)
+        {
+            ICruiser cruiser = helper.CreateCruiser(Direction.Right, Faction.Blues);
+
+            helper.InitialiseUnit(archon, parentCruiser: cruiser);
+            archon.StartConstruction();
+            Helper.SetupUnitForUnitMonitor(archon, cruiser);
+
+            helper.InitialiseUnit(attackBoat, parentCruiser: cruiser);
+            attackBoat.StartConstruction();
+            Helper.SetupUnitForUnitMonitor(attackBoat, cruiser);
+        }
+    }
+}
