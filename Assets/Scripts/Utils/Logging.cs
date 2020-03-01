b77bf5579919dfc89daa32bb72aba83595c43445
Felix Mann <felix.fei.mann@gmail.com>
2020-03-01 13:11:00 -0800

    Turn off logging :)

--- Assets/Scripts/Utils/Logging.cs
+++ Assets/Scripts/Utils/Logging.cs
@@ -143,13 +143,9 @@ namespace BattleCruisers.Utils
             // Projectiles
             tagsToActiveness.Add(Tags.ACCURACY_ADJUSTERS, false);
 			tagsToActiveness.Add(Tags.ANGLE_CALCULATORS, false);
-            // FELIX
-            tagsToActiveness.Add(Tags.BARREL_CONTROLLER, true);
-            //tagsToActiveness.Add(Tags.BARREL_CONTROLLER, false);
+            tagsToActiveness.Add(Tags.BARREL_CONTROLLER, false);
             tagsToActiveness.Add(Tags.BARREL_WRAPPER, false);
-            // FELIX
-            tagsToActiveness.Add(Tags.LASER, true);
-            //tagsToActiveness.Add(Tags.LASER, false);
+            tagsToActiveness.Add(Tags.LASER, false);
             tagsToActiveness.Add(Tags.PROJECTILE_SPAWNER, false);
             tagsToActiveness.Add(Tags.SHELLS, false);
 
