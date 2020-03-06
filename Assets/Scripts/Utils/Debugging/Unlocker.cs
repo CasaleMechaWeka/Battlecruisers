ed817a886008b0b4461c3a996b2272100ea15d85
Felix Mann <felix.fei.mann@gmail.com>
2020-03-05 16:40:13 -0800

    Add clickable script to top left corner of screens scene :P

---
+++ Assets/Scripts/Utils/Debugging/Unlocker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace BattleCruisers.Utils.Debugging
+{
+    public class Unlocker : MonoBehaviour, IPointerClickHandler
+    {
+        private int _numOfClicks = 0;
+
+        public int numOfClicksToUnlock = 7;
+
+        void Start()
+        {
+            Debug.Log("yo");
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            _numOfClicks++;
+            Debug.Log($"{_numOfClicks}/{numOfClicksToUnlock}");
+
+            if (_numOfClicks == numOfClicksToUnlock)
+            {
+                UnlockEverything();
+                enabled = false;
+            }
+        }
+
+        private void UnlockEverything()
+        {
+
+        }
+    }
+}
