ccb871e8d4d518f8a202bb60ef048c55fda053f8
Felix Mann <felix.fei.mann@gmail.com>
2020-03-21 13:34:39 -0700

    Remove unused constant :)

--- Assets/Scripts/Movement/Deciders/ShipMovementDecider.cs
+++ Assets/Scripts/Movement/Deciders/ShipMovementDecider.cs
@@ -25,10 +25,6 @@ namespace BattleCruisers.Movement.Deciders
         private readonly ITargetTracker _inRangeTargetTracker, _shipBlockerTargetTracker;
         private readonly ITargetRangeHelper _rangeHelper;
 
-        // Frigate would have optimal range of 18.63 but target was at 18.6305.
-        // Hence provide a tiny bit of leeway, so target is counted as in range.
-        private const float IN_RANGE_LEEWAY_IN_M = 0.01f;
-
         private ITarget _highestPriorityTarget;
         public ITarget Target
         {
