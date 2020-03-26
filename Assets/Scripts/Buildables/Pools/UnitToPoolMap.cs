575276441ab8cfa2c004c9e00a5b331af906bed0
Felix Mann <felix.fei.mann@gmail.com>
2020-03-26 15:50:29 -0700

    Change Archon name to Mann o' War :P

--- Assets/Scripts/Buildables/Pools/UnitToPoolMap.cs
+++ Assets/Scripts/Buildables/Pools/UnitToPoolMap.cs
@@ -66,7 +66,7 @@ namespace BattleCruisers.Buildables.Pools
                 case "Destroyer":
                     return _unitPoolProvider.DestroyerPool;
 
-                case "Archon Battleship":
+                case "Mann o' War":
                     return _unitPoolProvider.ArchonPool;
 
                 default:
