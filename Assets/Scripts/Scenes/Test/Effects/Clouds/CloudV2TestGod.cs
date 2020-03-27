a81aa90a8bbcc7bcad80806b36bb2fde8a18057a
Felix Mann <felix.fei.mann@gmail.com>
2020-03-27 15:45:37 -0700

    Duplicate test god :)

---
+++ Assets/Scripts/Scenes/Test/Effects/Clouds/CloudV2TestGod.cs
@@ -0,0 +1,32 @@
+﻿using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.UI.BattleScene.Clouds;
+using BattleCruisers.Utils.DataStrctures;
+using UnityEngine;
+using UnityEngine.Assertions;
+using BCUtils = BattleCruisers.Utils;
+
+namespace BattleCruisers.Scenes.Test.Effects.Clouds
+{
+    public class CloudV2TestGod : NavigationTestGod
+    {
+        protected override void Setup(Helper helper)
+        {
+            base.Setup(helper);
+
+            CloudStatsController cloudStatsController = GetComponentInChildren<CloudStatsController>();
+            Assert.IsNotNull(cloudStatsController);
+            // -110 <= x <= 110
+            //  10 <= y <= 60
+            Rect cloudSpawnArea = new Rect(x: -110, y: 10, width: 220, height: 50);
+            ICloudGenerationStats cloudStats
+                = new CloudGenerationStats(
+                    cloudSpawnArea,
+                    cloudStatsController.density,
+                    cloudStatsController.movementSpeed);
+
+            CloudInitialiser cloudInitialiser = GetComponentInChildren<CloudInitialiser>();
+            Assert.IsNotNull(cloudInitialiser);
+            cloudInitialiser.Initialise(cloudStats, BCUtils.RandomGenerator.Instance);
+        }
+    }
+}
