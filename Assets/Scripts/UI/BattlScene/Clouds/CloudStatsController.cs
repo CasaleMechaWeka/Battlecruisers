af383c4bdc1525b83ee4d584710e7d640e4138ad
Felix Mann <felix.fei.mann@gmail.com>
2020-03-28 12:03:26 -0700

    Move legacy field to end

--- Assets/Scripts/UI/BattlScene/Clouds/CloudStatsController.cs
+++ Assets/Scripts/UI/BattlScene/Clouds/CloudStatsController.cs
@@ -7,9 +7,9 @@ namespace BattleCruisers.UI.BattleScene.Clouds
         [Tooltip("Cloud density is the total area of all clouds divided by the area clouds are allowed to spawn in.  0 means no clouds.  1 means some clouds.  2 means more clouds, etc")]
         public float cloudDensityAsFraction = 0.5f;
 
-        public CloudDensity legacyDensity = CloudDensity.VeryHigh;
         public CloudMovementSpeed movementSpeed = CloudMovementSpeed.Fast;
         public Color frontCloudColor = Color.white;
         public Color backCloudColor = Color.white;
+        public CloudDensity legacyDensity = CloudDensity.VeryHigh;
     }
 }
