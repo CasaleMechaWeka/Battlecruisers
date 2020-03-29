33cde7140fcdb5b3f9f87f9c2bf9faf4b2363d82
Felix Mann <felix.fei.mann@gmail.com>
2020-03-29 09:00:21 -0700

    Create nuke trail persists test scene.  Same effects as rocket, so everything works :)

--- Assets/Scripts/Scenes/Test/Offensive/CameraToggleTestGod.cs
+++ Assets/Scripts/Scenes/Test/Offensive/CameraToggleTestGod.cs
@@ -6,11 +6,12 @@ namespace BattleCruisers.Scenes.Test.Offensive
     public class CameraToggleTestGod : TestGodBase
 	{
 		public Camera overviewCamera, closeUpCamera;
+		public bool showCloseupFirst = true;
 
         protected override void Setup(Helper helper)
         {
-			closeUpCamera.enabled = true;
-			overviewCamera.enabled = false;
+			closeUpCamera.enabled = showCloseupFirst;
+			overviewCamera.enabled = !showCloseupFirst;
         }
 
         public void ToggleCamera()
