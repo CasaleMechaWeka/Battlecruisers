901a8ea96feba45628662e05309e4a3acd96ac86
Felix Mann <felix.fei.mann@gmail.com>
2020-03-29 09:50:30 -0700

    Create BombTrailController :D

---
+++ Assets/Scripts/Effects/Trails/BombTrailController.cs
@@ -0,0 +1,35 @@
+﻿using BattleCruisers.Utils;
+using UnityEngine;
+
+namespace BattleCruisers.Effects.Trails
+{
+    public class BombTrailController : MonoBehaviour, IProjectileTrail
+    {
+        public SpriteRenderer glow, bullet;
+        public TrailRenderer trail;
+
+        public void Initialise()
+        {
+            Helper.AssertIsNotNull(glow, bullet, trail);
+        }
+
+        public void ShowAllEffects()
+        {
+            glow.enabled = true;
+            bullet.enabled = true;
+            trail.Clear();
+        }
+
+        public void HideAliveEffects()
+        {
+            glow.enabled = false;
+            bullet.enabled = false;
+        }
+
+        // FELIX  Remove?
+        public void HideAllEffects()
+        {
+            // empty
+        }
+    }
+}
