b50bad6683636fd47d0fa219cda4ea4988c70aa5
Felix Mann <felix.fei.mann@gmail.com>
2020-04-03 15:13:12 -0700

    Create new CloudController :D

---
+++ Assets/Scripts/UI/BattlScene/Clouds/CloudControllerNEW.cs
@@ -0,0 +1,37 @@
+﻿using BattleCruisers.Utils;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene.Clouds
+{
+    public class CloudControllerNEW : MonoBehaviour, ICloudNEW
+    {
+        public SpriteRenderer frontCloud;
+        public SpriteRenderer backCloud;
+
+        public Vector2 Size { get; private set; }
+
+        public Vector2 Position
+        {
+            get { return transform.position; }
+            set { transform.position = value; }
+        }
+
+        public void Initialise(ICloudStats cloudStats)
+        {
+            Assert.IsNotNull(cloudStats);
+            Assert.IsTrue(cloudStats.HorizontalMovementSpeedInMPerS > 0, "Only support clouds moving from left to right.");
+            Helper.AssertIsNotNull(frontCloud, backCloud);
+
+            frontCloud.color = cloudStats.FrontCloudColour;
+            backCloud.color = cloudStats.BackCloudColour;
+
+            Size = frontCloud.size;
+
+            // Start moving
+            Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+            Assert.IsNotNull(rigidBody);
+            rigidBody.velocity = new Vector2(cloudStats.HorizontalMovementSpeedInMPerS, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/BattlScene/Clouds/ICloudNEW.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.UI.BattleScene.Clouds
+{
+    public interface ICloudNEW
+    {
+        Vector2 Size { get; }
+        Vector2 Position { get; set; }
+
+        void Initialise(ICloudStats cloudStats);
+    }
+}
