a3d4ad7ee1821852f63b4b8528be1bb11747d127
Felix Mann <felix.fei.mann@gmail.com>
2020-04-04 14:00:34 -0700

    Default to cloud stats for main camera material

--- Assets/Scripts/Scenes/Test/Effects/Clouds/CloudV2TestGod.cs
+++ Assets/Scripts/Scenes/Test/Effects/Clouds/CloudV2TestGod.cs
@@ -1,5 +1,6 @@
 ﻿using BattleCruisers.Scenes.Test.Utilities;
 using BattleCruisers.UI.BattleScene.Clouds;
+using UnityEngine;
 using UnityEngine.Assertions;
 using BCUtils = BattleCruisers.Utils;
 
@@ -7,41 +8,23 @@ namespace BattleCruisers.Scenes.Test.Effects.Clouds
 {
     public class CloudV2TestGod : NavigationTestGod
     {
+        public Skybox skybox;
         public SkyStatsGroup skyStatsGroup;
         public SkyButtonGroup skyButtonGroup;
 
         protected override void Setup(Helper helper)
         {
             base.Setup(helper);
-            BCUtils.Helper.AssertIsNotNull(skyStatsGroup, skyButtonGroup);
-
-            CloudStatsController cloudStatsController = GetComponentInChildren<CloudStatsController>();
-            Assert.IsNotNull(cloudStatsController);
-
-            ICloudStats cloudStats 
-                = new CloudStats(
-                    ConvertMovementSpeed(cloudStatsController.movementSpeed),
-                    cloudStatsController.frontCloudColor,
-                    cloudStatsController.backCloudColor);
-
-            CloudInitialiser cloudInitialiser = GetComponentInChildren<CloudInitialiser>();
-            Assert.IsNotNull(cloudInitialiser);
-            cloudInitialiser.Initialise(cloudStats, _updaterProvider.SlowerUpdater);
+            BCUtils.Helper.AssertIsNotNull(skybox, skyStatsGroup, skyButtonGroup);
 
             skyStatsGroup.Initialise();
             skyButtonGroup.Initialise(skyStatsGroup.SkyStats);
-        }
 
-        private float ConvertMovementSpeed(CloudMovementSpeed movementSpeed)
-        {
-            switch (movementSpeed)
-            {
-                case CloudMovementSpeed.Fast:
-                    return 0.75f;
-                case CloudMovementSpeed.Slow:
-                default:
-                    return 0.5f;
-            }
+            ISkyStats skyStats = skyStatsGroup.GetSkyStats(skybox.material.name);
+
+            CloudInitialiser cloudInitialiser = GetComponentInChildren<CloudInitialiser>();
+            Assert.IsNotNull(cloudInitialiser);
+            cloudInitialiser.Initialise(skyStats, _updaterProvider.SlowerUpdater);
         }
     }
 }
