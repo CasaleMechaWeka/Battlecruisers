ebfa3e2334dd9a5b797727a12bd56a2fa722b3a6
Felix Mann <felix.fei.mann@gmail.com>
2020-04-04 13:17:55 -0700

    Create (unused) SkyButtonController :D

---
+++ Assets/Scripts/Scenes/Test/Utilities/SkyButtonController.cs
@@ -0,0 +1,46 @@
+﻿using BattleCruisers.UI.BattleScene.Clouds;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using BCUtils = BattleCruisers.Utils;
+
+namespace BattleCruisers.Scenes.Test.Utilities
+{
+    // FELIX  Move to where used?
+    public interface ISkyProvider
+    {
+        ISkyStats GetSkyStats(Material skyMaterial);
+    }
+
+    public class SkyButtonController : MonoBehaviour
+    {
+        private Skybox _skybox;
+        private ISkyStats _skyStats;
+        private IList<ICloud> _clouds;
+
+        public Text skyName;
+
+        public void Initialise(Skybox skybox, ISkyStats skyStats, IList<ICloud> clouds)
+        {
+            BCUtils.Helper.AssertIsNotNull(skybox, skyStats, clouds);
+            Assert.IsNotNull(skyName);
+
+            _skybox = skybox;
+            _skyStats = skyStats;
+            _clouds = clouds;
+
+            skyName.text = _skyStats.SkyMaterial.name;
+        }
+
+        public void SelectSky()
+        {
+            _skybox.material = _skyStats.SkyMaterial;
+
+            foreach (ICloud cloud in _clouds)
+            {
+                cloud.Initialise(_skyStats);
+            }
+        }
+    }
+}
