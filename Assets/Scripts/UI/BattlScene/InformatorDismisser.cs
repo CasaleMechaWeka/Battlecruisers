50f0d356a766e57cb24aeef1157c603e6c208117
Felix Mann <felix.fei.mann@gmail.com>
2020-04-05 08:53:33 -0700

    Create InformatorDismisser, trying to make clicking background dismiss informator :)

---
+++ Assets/Scripts/UI/BattlScene/InformatorDismisser.cs
@@ -0,0 +1,29 @@
+﻿using BattleCruisers.UI.BattleScene.Manager;
+using BattleCruisers.Utils;
+using System;
+
+// FELIX  Togglable for tutorial?
+// FELIX  Test?
+namespace BattleCruisers.UI.BattleScene
+{
+    public class InformatorDismisser
+    {
+        private readonly IClickableEmitter _background;
+        private readonly IUIManager _uiManager;
+
+        public InformatorDismisser(IClickableEmitter background, IUIManager uiManager)
+        {
+            Helper.AssertIsNotNull(background, uiManager);
+
+            _background = background;
+            _uiManager = uiManager;
+
+            _background.Clicked += _background_Clicked;
+        }
+
+        private void _background_Clicked(object sender, EventArgs e)
+        {
+            _uiManager.HideItemDetails();
+        }
+    }
+}
