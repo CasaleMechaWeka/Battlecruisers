b8c25c0ed46d4e3708aa12230601d64ebeb7e58d
Felix Mann <felix.fei.mann@gmail.com>
2020-04-15 16:01:08 -0700

    Create MistController :D

---
+++ Assets/Scripts/UI/BattlScene/Clouds/MistController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.UI.BattleScene.Clouds
+{
+    public class MistController : MonoBehaviour
+    {
+        public void Initialse(Color mistColour)
+        {
+            SpriteRenderer[] mistSprites = GetComponentsInChildren<SpriteRenderer>();
+            
+            foreach (SpriteRenderer mistSprite in mistSprites)
+            {
+                mistSprite.color = mistColour;
+            }
+        }
+    }
+}
