936ff6a3aff60f8e0782e44f3df7a5c36591ceac
Felix Mann <felix.fei.mann@gmail.com>
2020-04-16 15:01:05 -0700

    Fix laser impact particle systems

--- Assets/Scripts/Effects/Laser/LaserImpact.cs
+++ Assets/Scripts/Effects/Laser/LaserImpact.cs
@@ -1,27 +1,30 @@
-﻿using UnityEngine;
+﻿using BattleCruisers.Effects.ParticleSystems;
+using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace BattleCruisers.Effects.Laser
 {
     public class LaserImpact : MonoBehaviour, ILaserImpact
     {
-        private ParticleSystem _glow;
+        private IParticleSystemGroup _effects;
 
         public void Initialise()
         {
-            _glow = GetComponent<ParticleSystem>();
-            Assert.IsNotNull(_glow);
+            ParticleSystemGroupInitialiser effectsInitialiser = GetComponent<ParticleSystemGroupInitialiser>();
+            Assert.IsNotNull(effectsInitialiser);
+            _effects = effectsInitialiser.CreateParticleSystemGroup();
+            _effects.Stop();
         }
 
         public void Show(Vector3 postion)
         {
             transform.position = postion;
-            _glow.Play();
+            _effects.Play();
         }
 
         public void Hide()
         {
-            _glow.Stop();
+            _effects.Stop();
         }
     }
 }
