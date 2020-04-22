a4c297d450373ce3b9dd7076fa4b80ee8f68fc1d
Felix Mann <felix.fei.mann@gmail.com>
2020-04-21 19:05:14 -0700

    Create CruiserRegionController to avoid duplicate code for different cruisers :)

---
+++ Assets/Scripts/Scenes/Test/Cruisers/CruiserRegionController.cs
@@ -0,0 +1,31 @@
+﻿using BattleCruisers.Buildables.Buildings;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+using UnityEngine.Assertions;
+using BCUtils = BattleCruisers.Utils;
+
+namespace BattleCruisers.Scenes.Test.Cruisers
+{
+    public class CruiserRegionController : MonoBehaviour
+    {
+        public Cruiser cruiser;
+        public ICruiser Cruiser => cruiser;
+
+        public void StaticInitialise()
+        {
+            Assert.IsNotNull(cruiser);
+        }
+
+        // FELIX  Add buildings for all slots :)
+        public void Initialise(Helper helper, BuildingWrapper deckSlotBuilding)
+        {
+            BCUtils.Helper.AssertIsNotNull(helper, deckSlotBuilding);
+
+            helper.SetupCruiser(cruiser);
+
+            // FELIX  Build deck slot building
+            // FELIX  On all slots!
+        }
+    }
+}
--- Assets/Scripts/Scenes/Test/Cruisers/SmartSlotPlacementTestGod.cs
+++ Assets/Scripts/Scenes/Test/Cruisers/SmartSlotPlacementTestGod.cs
@@ -1,6 +1,7 @@
-﻿using BattleCruisers.Cruisers;
+﻿using BattleCruisers.Buildables.Buildings;
 using BattleCruisers.Scenes.Test.Utilities;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -8,21 +9,35 @@ namespace BattleCruisers.Scenes.Test.Cruisers
 {
     public class SmartSlotPlacementTestGod : TestGodBase
     {
-        public Cruiser cruiser;
+        private IList<CruiserRegionController> _cruiserRegions;
+
+        // FELIX  Add other slot buildings :)
+        public BuildingWrapper deckSlotBuilding;
 
         protected override List<GameObject> GetGameObjects()
         {
-            Assert.IsNotNull(cruiser);
-            return new List<GameObject>()
+            Assert.IsNotNull(deckSlotBuilding);
+
+            _cruiserRegions = GetComponentsInChildren<CruiserRegionController>();
+            Assert.IsTrue(_cruiserRegions.Count > 0);
+
+            foreach (CruiserRegionController cruiserRegion in _cruiserRegions)
             {
-                cruiser.GameObject
-            };
+                cruiserRegion.StaticInitialise();
+            }
+
+            return
+                _cruiserRegions
+                    .Select(region => region.Cruiser.GameObject)
+                    .ToList();
         }
 
         protected override void Setup(Helper helper)
         {
-            // FELIX  Avoid duplicate code for muiltiple cruisers => CruiserRegion class :)
-            helper.SetupCruiser(cruiser);
+            foreach (CruiserRegionController cruiserRegion in _cruiserRegions)
+            {
+                cruiserRegion.Initialise(helper, deckSlotBuilding);
+            }
         }
     }
 }
