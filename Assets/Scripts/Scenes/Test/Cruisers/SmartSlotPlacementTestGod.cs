a773c4c4c526ed71d5851681ddf2b54ef01dbefe
Felix Mann <felix.fei.mann@gmail.com>
2020-04-21 17:21:54 -0700

    Create new smart slot placement test scene stub :D

---
+++ Assets/Scripts/Scenes/Test/Cruisers/SmartSlotPlacementTestGod.cs
@@ -0,0 +1,28 @@
+﻿using BattleCruisers.Cruisers;
+using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.Cruisers
+{
+    public class SmartSlotPlacementTestGod : TestGodBase
+    {
+        public Cruiser cruiser;
+
+        protected override List<GameObject> GetGameObjects()
+        {
+            Assert.IsNotNull(cruiser);
+            return new List<GameObject>()
+            {
+                cruiser.GameObject
+            };
+        }
+
+        protected override void Setup(Helper helper)
+        {
+            // FELIX  Avoid duplicate code for muiltiple cruisers => CruiserRegion class :)
+            helper.SetupCruiser(cruiser);
+        }
+    }
+}
