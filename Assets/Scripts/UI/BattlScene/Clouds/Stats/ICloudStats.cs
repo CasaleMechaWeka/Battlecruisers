754779dd605f051c3fce0b0539132041c7156e95
Felix Mann <felix.fei.mann@gmail.com>
2020-05-13 16:08:04 -0700

    Expand sky stats for z position

--- Assets/Scripts/UI/BattlScene/Clouds/Stats/ICloudStats.cs
+++ Assets/Scripts/UI/BattlScene/Clouds/Stats/ICloudStats.cs
@@ -4,10 +4,15 @@ namespace BattleCruisers.UI.BattleScene.Clouds.Stats
 {
     public interface ICloudStats
     {
-        float Height { get; }
         float HorizontalMovementSpeedInMPerS { get; }
         bool FlipClouds { get; }
+        
         Color CloudColour { get; }
+        float Height { get; }
+        float CloudZPosition { get; }
+        
         Color MistColour { get; }
+        float MistYPosition { get; }
+        float MistZPosiiton { get; }
     }
 }
--- Assets/Scripts/UI/BattlScene/Clouds/Stats/SkyStatsController.cs
+++ Assets/Scripts/UI/BattlScene/Clouds/Stats/SkyStatsController.cs
@@ -5,25 +5,34 @@ namespace BattleCruisers.UI.BattleScene.Clouds.Stats
 {
     public class SkyStatsController : MonoBehaviour, ISkyStats
     {
+        public IMoonStats MoonStats { get; private set; }
+        
         public Material skyboxMaterial;
         public Material SkyMaterial => skyboxMaterial;
 
         public float movementSpeedInMPerS = 5;
         public float HorizontalMovementSpeedInMPerS => movementSpeedInMPerS;
 
+        public bool flipClouds = false;
+        public bool FlipClouds => flipClouds;
+
         public Color cloudColour = Color.white;
         public Color CloudColour => cloudColour;
-
-        public Color mistColour = Color.white;
-        public Color MistColour => mistColour;
 
         public float cloudHeight = 28;
         public float Height => cloudHeight;
 
-        public bool flipClouds = false;
-        public bool FlipClouds => flipClouds;
+        public float cloudZPosition = 290;
+        public float CloudZPosition => cloudZPosition;
 
-        public IMoonStats MoonStats { get; private set; }
+        public Color mistColour = Color.white;
+        public Color MistColour => mistColour;
+
+        public float mistYPosition = 0;
+        public float MistYPosition => mistYPosition;
+
+        public float mistZPosition = 0;
+        public float MistZPosiiton => mistZPosition;
 
         public void Initialise()
         {
