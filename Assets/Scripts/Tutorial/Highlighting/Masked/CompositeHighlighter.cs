3cfebf6c27081060ef10d43efb94537699d9cd60
Felix Mann <felix.fei.mann@gmail.com>
2020-05-15 10:42:18 -0700

    Make highlighter more generic, allowing arrow highlighter to also be easily added :)

---
+++ Assets/Scripts/Tutorial/Highlighting/Masked/CompositeHighlighter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.Assertions;
+
+namespace BattleCruisers.Tutorial.Highlighting.Masked
+{
+    public class CompositeHighlighter : IMaskHighlighter
+    {
+        private readonly IMaskHighlighter[] _highlighters;
+
+        public CompositeHighlighter(params IMaskHighlighter[] highlighters)
+        {
+            Assert.IsTrue(highlighters.Length > 0);
+            _highlighters = highlighters;
+        }
+
+        public void Highlight(HighlightArgs args)
+        {
+            foreach (IMaskHighlighter highlighter in _highlighters)
+            {
+                highlighter.Highlight(args);
+            }
+        }
+
+        public void Unhighlight()
+        {
+            foreach (IMaskHighlighter highlighter in _highlighters)
+            {
+                highlighter.Unhighlight();
+            }
+        }
+    }
+}
--- Assets/Scripts/Tutorial/Highlighting/Masked/HighlighterInitialiser.cs
+++ Assets/Scripts/Tutorial/Highlighting/Masked/HighlighterInitialiser.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 
 namespace BattleCruisers.Tutorial.Highlighting.Masked
 {
+    /// <summary>
+    /// Uses two types of masking:
+    /// 1. UI inverse masking to show highlight to user
+    /// 2. Original manual masking (placing 4 rectangles around highlight point), 
+    ///     to block raycasts (user input) to anything outside of the highlight zone.
+    /// </summary>
     public class HighlighterInitialiser : MonoBehaviour
     {
         public InverseMaskHighlighter inverseHighlighter;
@@ -15,7 +21,7 @@ namespace BattleCruisers.Tutorial.Highlighting.Masked
             inverseHighlighter.Initialise();
             maskHighlighter.Initialise();
 
-            return new DualMaskHighlighter(inverseHighlighter, maskHighlighter);
+            return new CompositeHighlighter(inverseHighlighter, maskHighlighter);
         }
     }
 }
