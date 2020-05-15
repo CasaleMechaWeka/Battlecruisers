17e9950dbf72c502948490ee9a9250d03cb8747d
Felix Mann <felix.fei.mann@gmail.com>
2020-05-15 10:20:35 -0700

    Extract method

--- Assets/Scripts/Tutorial/Highlighting/Masked/HighlightArgsFactory.cs
+++ Assets/Scripts/Tutorial/Highlighting/Masked/HighlightArgsFactory.cs
@@ -20,25 +20,24 @@ namespace BattleCruisers.Tutorial.Highlighting.Masked
         {
             Vector2 bottomLeftPosition = FindBottomLeftPosition(rectTransform, sizeMultiplier);
             Vector2 size = sizeMultiplier * rectTransform.sizeDelta * rectTransform.lossyScale;
+            Vector2 center = FindCenterPosition(rectTransform);
 
-            // FELIX  Extract method
+            HighlightArgs noBorderArgs = new HighlightArgs(center, bottomLeftPosition, size);
+            return AddBorder(noBorderArgs);
+        }
+
+        private static Vector2 FindCenterPosition(RectTransform rectTransform)
+        {
             Vector3[] corners = new Vector3[4];
             rectTransform.GetWorldCorners(corners);
-            
+
             Vector2 bottomLeftCorner = corners[0];
             Vector2 topRightCorner = corners[2];
 
-            Vector2 center
-                = new Vector2(
+            return
+                new Vector2(
                     (bottomLeftCorner.x + topRightCorner.x) / 2,
                     (bottomLeftCorner.y + topRightCorner.y) / 2);
-
-            // FELIX
-            HighlightArgs noBorderArgs = new HighlightArgs(center, bottomLeftPosition, size);
-            //HighlightArgs noBorderArgs = new HighlightArgs(rectTransform.position, bottomLeftPosition, size);
-            //HighlightArgs noBorderArgs = new HighlightArgs(rectTransform.localPosition, bottomLeftPosition, size);
-            //HighlightArgs noBorderArgs = new HighlightArgs(rectTransform.anchoredPosition, bottomLeftPosition, size);
-            return AddBorder(noBorderArgs);
         }
 
         private Vector2 FindBottomLeftPosition(RectTransform rectTransform, float sizeMultiplier)
