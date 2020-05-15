603d8f2ae472756b26e4546b5702b617a8016b39
Felix Mann <felix.fei.mann@gmail.com>
2020-05-15 09:36:38 -0700

    Create HighlighterInitialiser :D

---
+++ Assets/Scripts/Tutorial/Highlighting/Masked/HighlighterInitialiser.cs
@@ -0,0 +1,17 @@
+﻿using BattleCruisers.Utils;
+using UnityEngine;
+
+namespace BattleCruisers.Tutorial.Highlighting.Masked
+{
+    public class HighlighterInitialiser : MonoBehaviour
+    {
+        public InverseMaskHighlighter inverseHighlighter;
+        public MaskHighlighter maskHighlighter;
+
+        public IMaskHighlighter CreateHighlighter()
+        {
+            Helper.AssertIsNotNull(inverseHighlighter, maskHighlighter);
+            return new DualMaskHighlighter(inverseHighlighter, maskHighlighter);
+        }
+    }
+}
