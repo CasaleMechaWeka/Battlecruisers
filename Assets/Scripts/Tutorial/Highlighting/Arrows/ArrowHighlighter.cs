01dce0eada81ee177aa093aad175cb9d33066864
Felix Mann <felix.fei.mann@gmail.com>
2020-05-15 10:38:55 -0700

    Create ArrowHighlighter stub

---
+++ Assets/Scripts/Tutorial/Highlighting/Arrows/ArrowHighlighter.cs
@@ -0,0 +1,17 @@
+﻿using BattleCruisers.Tutorial.Highlighting.Masked;
+
+namespace BattleCruisers.Tutorial.Highlighting.Arrows
+{
+    public class ArrowHighlighter : IMaskHighlighter
+    {
+        public void Highlight(HighlightArgs args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Unhighlight()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
