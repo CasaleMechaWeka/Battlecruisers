b79a951150bd87bde8f9eec5def457f1de6c18c4
Felix Mann <felix.fei.mann@gmail.com>
2020-05-18 07:35:08 -0700

    Add summaries :)

--- Assets/Scripts/UI/Sound/ISoundPlayer.cs
+++ Assets/Scripts/UI/Sound/ISoundPlayer.cs
@@ -6,8 +6,16 @@ namespace BattleCruisers.UI.Sound
 {
     public interface ISoundPlayer
     {
+        /// <summary>
+        /// Play sound at the current audio listener position, for full volume.
+        /// Note this breaks if the camera is moved after the sound  has started playing.
+        /// </summary>
         Task PlaySoundAsync(ISoundKey soundKey);
         Task PlaySoundAsync(ISoundKey soundKey, Vector2 position);
+        /// <summary>
+        /// Play sound at the current audio listener position, for full volume.
+        /// Note this breaks if the camera is moved after the sound  has started playing.
+        /// </summary>
         void PlaySound(IAudioClipWrapper sound);
         void PlaySound(IAudioClipWrapper sound, Vector2 position);
     }
