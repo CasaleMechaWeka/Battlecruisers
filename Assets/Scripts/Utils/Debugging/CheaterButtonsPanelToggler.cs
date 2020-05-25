f2adabd4b740732aadcb8fdea4ef4da9e8f6ae1b
FelixMann <felix.fei.mann@gmail.com>
2020-05-25 09:17:14 -0700

    4 touch points toggle debug UI :)

---
+++ Assets/Scripts/Utils/Debugging/CheaterButtonsPanelToggler.cs
@@ -0,0 +1,35 @@
+﻿using BattleCruisers.Utils.Timers;
+using UnityCommon.PlatformAbstractions.Time;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Utils.Debugging
+{
+    public class CheaterButtonsPanelToggler : MonoBehaviour
+    {
+        private IDebouncer _debouncer;
+
+        public float debounceTimeInS = 1;
+        public int numOfContactPoints = 4;
+        public GameObject cheaterButtonsPanel;
+
+        void Start()
+        {
+            Assert.IsNotNull(cheaterButtonsPanel);
+            _debouncer = new Debouncer(TimeBC.Instance.RealTimeSinceGameStartProvider, debounceTimeInS);
+        }
+
+        void Update()
+        {
+            if (Input.touchCount == numOfContactPoints)
+            {
+                _debouncer.Debounce(ToggleCheatersUI);
+            }
+        }
+
+        private void ToggleCheatersUI()
+        {
+            cheaterButtonsPanel.SetActive(!cheaterButtonsPanel.activeSelf);
+        }
+    }
+}
