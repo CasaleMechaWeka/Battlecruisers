7c682658b38867d3abdc648441f6382effccec2b
FelixMann <felix.fei.mann@gmail.com>
2020-05-27 16:00:55 -0700

    Use full blown camera unitializer, so can access CameraFocuser :)

--- Assets/Scripts/Scenes/Test/Effects/Deaths/CruiserDeathCameraTestGod.cs
+++ Assets/Scripts/Scenes/Test/Effects/Deaths/CruiserDeathCameraTestGod.cs
@@ -1,10 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Data;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.UI.BattleScene.Navigation;
+using BattleCruisers.UI.Cameras;
 using UnityEngine;
+using UnityEngine.Assertions;
+using BCUtils = BattleCruisers.Utils;
 
 namespace BattleCruisers.Scenes.Test.Effects.Deaths
 {
-    public class CruiserDeathCameraTestGod : NavigationTestGod
+    public class CruiserDeathCameraTestGod : TestGodBase
     {
+        public Cruiser playerCruiser, aiCruiser;
+
+        protected override List<GameObject> GetGameObjects()
+        {
+            BCUtils.Helper.AssertIsNotNull(playerCruiser, aiCruiser);
+
+            return new List<GameObject>()
+            {
+                playerCruiser.GameObject,
+                aiCruiser.GameObject
+            };
+        }
+
+        protected override void Setup(Helper helper)
+        {
+            helper.SetupCruiser(playerCruiser);
+            helper.SetupCruiser(aiCruiser);
+
+            NavigationPermitters navigationPermitters = new NavigationPermitters();
+
+            CameraInitialiser cameraInitialiser = FindObjectOfType<CameraInitialiser>();
+            Assert.IsNotNull(cameraInitialiser);
+            cameraInitialiser
+                .Initialise(
+                    ApplicationModelProvider.ApplicationModel.DataProvider.SettingsManager,
+                    playerCruiser,
+                    aiCruiser,
+                    navigationPermitters,
+                    _updaterProvider.SwitchableUpdater);
+        }
     }
 }
