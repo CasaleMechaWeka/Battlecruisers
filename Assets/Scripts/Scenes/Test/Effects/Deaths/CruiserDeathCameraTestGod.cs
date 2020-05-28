6266ffaccdbd5c1d2b3cdf73d9dd83e4d16b5ef8
FelixMann <felix.fei.mann@gmail.com>
2020-05-27 17:49:17 -0700

    Add nuke buttons :)

--- Assets/Scripts/Scenes/Test/Effects/Deaths/CruiserDeathCameraTestGod.cs
+++ Assets/Scripts/Scenes/Test/Effects/Deaths/CruiserDeathCameraTestGod.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using BattleCruisers.Cruisers;
 using BattleCruisers.Data;
 using BattleCruisers.Scenes.Test.Utilities;
 using BattleCruisers.UI.BattleScene.Navigation;
 using BattleCruisers.UI.Cameras;
+using BattleCruisers.Utils.Factories;
 using UnityEngine;
 using UnityEngine.Assertions;
 using BCUtils = BattleCruisers.Utils;
@@ -14,6 +14,7 @@ namespace BattleCruisers.Scenes.Test.Effects.Deaths
     public class CruiserDeathCameraTestGod : TestGodBase
     {
         private ICameraFocuser _cameraFocuser;
+        private IFactoryProvider _factoryProvider;
 
         public Cruiser playerCruiser, aiCruiser;
 
@@ -45,6 +46,15 @@ namespace BattleCruisers.Scenes.Test.Effects.Deaths
                     navigationPermitters,
                     _updaterProvider.SwitchableUpdater);
             _cameraFocuser = cameraComponents.CameraFocuser;
+
+            BuildableInitialisationArgs args = new BuildableInitialisationArgs(helper);
+            _factoryProvider = args.FactoryProvider;
+        }
+
+        public void PlayerNuke()
+        {
+            _factoryProvider.PoolProviders.ExplosionPoolProvider.HugeExplosionsPool.GetItem(playerCruiser.Position);
+
         }
 
         public void PlayerCruiser()
@@ -55,6 +65,11 @@ namespace BattleCruisers.Scenes.Test.Effects.Deaths
         public void PlayerCruiserNuke()
         {
             _cameraFocuser.FocusOnPlayerCruiserNuke();
+        }
+
+        public void AINuke()
+        {
+            _factoryProvider.PoolProviders.ExplosionPoolProvider.HugeExplosionsPool.GetItem(aiCruiser.Position);
         }
 
         public void AICruiser()
