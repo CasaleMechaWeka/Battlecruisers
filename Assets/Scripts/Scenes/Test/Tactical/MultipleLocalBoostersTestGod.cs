98fb133e54e6c348346d7dc117f658ec385e31ad
FelixMann <felix.fei.mann@gmail.com>
2020-05-29 14:54:33 -0700

    Create test scene stub for boosters

---
+++ Assets/Scripts/Scenes/Test/Tactical/MultipleLocalBoostersTestGod.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.Tactical
+{
+    public class MultipleLocalBoostersTestGod : TestGodBase
+    {
+        public Cruiser cruiser;
+
+        protected override List<GameObject> GetGameObjects()
+        {
+            Assert.IsNotNull(cruiser);
+            return new List<GameObject>()
+            {
+                cruiser.GameObject
+            };
+        }
+
+        protected override void Setup(Helper helper)
+        {
+            helper.SetupCruiser(cruiser);
+        }
+    }
+}
