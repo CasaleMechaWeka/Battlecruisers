4bd9247b11863ace790f537e8648afe3f8a63f5d
Felix Mann <felix.fei.mann@gmail.com>
2020-06-02 17:03:00 -0700

    Create ToggleController :)

---
+++ Assets/Scripts/UI/ScreensScene/SettingsScreen/ToggleController.cs
@@ -0,0 +1,28 @@
+﻿using UnityCommon.Properties;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.ScreensScene.SettingsScreen
+{
+    public class ToggleController : MonoBehaviour
+    {
+        private ISettableBroadcastingProperty<bool> _isChecked;
+        public IBroadcastingProperty<bool> IsChecked { get; private set; }
+
+        public void Initialise(bool isChecked)
+        {
+            Toggle toggle = GetComponentInChildren<Toggle>();
+            Assert.IsNotNull(toggle);
+            toggle.onValueChanged.AddListener(OnValueChanged);
+
+            _isChecked = new SettableBroadcastingProperty<bool>(isChecked);
+            IsChecked = new BroadcastingProperty<bool>(_isChecked);
+        }
+
+        private void OnValueChanged(bool value)
+        {
+            _isChecked.Value = value;
+        }
+    }
+}
