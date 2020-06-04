3bdc30339d0bf6fba8dbe07c2f20e021fde17df5
Felix Mann <felix.fei.mann@gmail.com>
2020-06-04 16:28:35 -0700

    Create lightning emitter test scene :D

---
+++ Assets/Scripts/Scenes/Test/Projectiles/LightningEmitterTestGod.cs
@@ -0,0 +1,37 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Projectiles.Spawners.Beams.Lightning;
+using BattleCruisers.Scenes.Test.Utilities;
+using BattleCruisers.Targets.TargetFinders.Filters;
+using NSubstitute;
+using BCUtils = BattleCruisers.Utils;
+
+namespace BattleCruisers.Scenes.Test.Projectiles
+{
+    public class LightningEmitterTestGod : TestGodBase
+    {
+        public LightningEmitter lightningEmitter;
+        public AirFactory target;
+
+        protected override void Setup(Helper helper)
+        {
+            BCUtils.Helper.AssertIsNotNull(lightningEmitter, target);
+
+            // Setup lightning emitter
+            ITargetFilter targetFilter = new DummyTargetFilter(isMatchResult: true);
+            ITarget parent = Substitute.For<ITarget>();
+
+            lightningEmitter.Initialise(targetFilter, damage: 1, parent);
+            InvokeRepeating(nameof(FireLightning), time: 0.5f, repeatRate: 1);
+
+            // Setup target
+            helper.InitialiseBuilding(target);
+            target.StartConstruction();
+        }
+
+        private void FireLightning()
+        {
+            lightningEmitter.FireBeam(angleInDegrees: 0, isSourceMirrored: false);
+        }
+    }
+}
