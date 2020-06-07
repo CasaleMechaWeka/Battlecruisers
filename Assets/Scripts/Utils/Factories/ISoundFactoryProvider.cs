d01ed9f1d7224c1576cef6d84b63254c08050b0c
Felix Mann <felix.fei.mann@gmail.com>
2020-06-07 09:45:50 -0700

    Add summary comment

--- Assets/Scripts/Utils/Factories/ISoundFactoryProvider.cs
+++ Assets/Scripts/Utils/Factories/ISoundFactoryProvider.cs
@@ -8,6 +8,11 @@ namespace BattleCruisers.Utils.Factories
     {
         ISoundFetcher SoundFetcher { get; }
         ISoundPlayer SoundPlayer { get; }
+
+        /// <summary>
+        /// Plays a single sound at a time.  Used by in game events, such as
+        /// building completed or cruiser events.
+        /// </summary>
         IPrioritisedSoundPlayer PrioritisedSoundPlayer { get; }
         IPrioritisedSoundPlayer DummySoundPlayer { get; }
         ISoundPlayerFactory SoundPlayerFactory { get; }
