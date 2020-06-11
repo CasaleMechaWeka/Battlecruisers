386b9c7e656a1637c857a49a75791eb52fef3d64
FelixMann <felix.fei.mann@gmail.com>
2020-06-10 17:17:33 -0700

    Create new methods to avoid duplicate code in child classes :)

--- Assets/Scripts/UI/Cameras/Targets/Providers/UserInputCameraTargetProvider.cs
+++ Assets/Scripts/UI/Cameras/Targets/Providers/UserInputCameraTargetProvider.cs
@@ -2,18 +2,46 @@
 
 namespace BattleCruisers.UI.Cameras.Targets.Providers
 {
+    // FELIX  Test :)
     public abstract class UserInputCameraTargetProvider : CameraTargetProvider, IUserInputCameraTargetProvider
     {
+        private bool _duringUserInput;
+
         public abstract int Priority { get; }
 
         public event EventHandler UserInputStarted;
         public event EventHandler UserInputEnded;
 
+        public UserInputCameraTargetProvider()
+        {
+            _duringUserInput = false;
+        }
+
+        protected void ReceivedUserInput()
+        {
+            if (!_duringUserInput)
+            {
+                _duringUserInput = true;
+                UserInputStarted?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        protected void UserInputEnd()
+        {
+            if (_duringUserInput)
+            {
+                _duringUserInput = false;
+                UserInputEnded?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        // FELIX  Remove :)
         protected void RaiseUserInputStarted()
         {
             UserInputStarted?.Invoke(this, EventArgs.Empty);
         }
 
+        // FELIX  Remove :)
         protected void RaiseUserInputEnded()
         {
             UserInputEnded?.Invoke(this, EventArgs.Empty);
