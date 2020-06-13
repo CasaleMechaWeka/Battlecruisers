282e35eb2e148d90ddc38e6596e5902dd708f5f2
Felix Mann <felix.fei.mann@gmail.com>
2020-06-13 11:37:14 -0700

    Create WindInitialiser :)

---
+++ Assets/Scripts/UI/Sound/Wind/WindInitialiser.cs
@@ -0,0 +1,26 @@
+﻿using BattleCruisers.UI.Cameras.Helpers.Calculators;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.PlatformAbstractions;
+using BattleCruisers.Utils.PlatformAbstractions.UI;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Sound.Wind
+{
+    public class WindInitialiser : MonoBehaviour
+    {
+        public AudioSource audioSource;
+
+        public IWindManager Initialise(ICamera camera, ICameraCalculatorSettings cameraCalculatorSettings)
+        {
+            Assert.IsNotNull(audioSource);
+            Helper.AssertIsNotNull(camera, cameraCalculatorSettings);
+
+            return
+                new WindManager(
+                    new AudioSourceBC(audioSource),
+                    camera,
+                    cameraCalculatorSettings.ValidOrthographicSizes);
+        }
+    }
+}
