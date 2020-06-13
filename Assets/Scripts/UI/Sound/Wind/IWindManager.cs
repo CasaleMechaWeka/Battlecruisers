8ce3ece55d603f529526b7bcca97671559b2b957
Felix Mann <felix.fei.mann@gmail.com>
2020-06-13 11:24:37 -0700

    Create WindManager :)

---
+++ Assets/Scripts/UI/Sound/Wind/IWindManager.cs
@@ -0,0 +1,8 @@
+﻿namespace BattleCruisers.UI.Sound.Wind
+{
+    public interface IWindManager
+    {
+        void Play();
+        void Stop();
+    }
+}
---
+++ Assets/Scripts/UI/Sound/Wind/WindManager.cs
@@ -0,0 +1,60 @@
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.DataStrctures;
+using BattleCruisers.Utils.PlatformAbstractions;
+using BattleCruisers.Utils.PlatformAbstractions.UI;
+using System;
+
+namespace BattleCruisers.UI.Sound.Wind
+{
+    // FELIX  Create tests :)
+    public class WindManager : IWindManager
+    {
+        private readonly IAudioSource _audioSource;
+        private readonly ICamera _camera;
+        private readonly IRange<float> _validOrthographicSizes;
+
+        public WindManager(IAudioSource audioSource, ICamera camera, IRange<float> validOrthographicSizes)
+        {
+            Helper.AssertIsNotNull(audioSource, camera, validOrthographicSizes);
+
+            _audioSource = audioSource;
+            _camera = camera;
+            _validOrthographicSizes = validOrthographicSizes;
+
+            _camera.OrthographicSizeChanged += _camera_OrthographicSizeChanged;
+        }
+
+        private void _camera_OrthographicSizeChanged(object sender, EventArgs e)
+        {
+            _audioSource.Volume = FindVolume(_camera.OrthographicSize);
+        }
+
+        // FELIX  Extract :)
+        private float FindVolume(float cameraOrthographicSize)
+        {
+            float orthographicSizeProportion = cameraOrthographicSize / _validOrthographicSizes.Max;
+
+            // Don't play wind if less than halfway zoomed out
+            if (orthographicSizeProportion < 0.5)
+            {
+                return 0;
+            }
+
+            // OS proportion    Wind volume
+            // 0.5              0
+            // 0.75             0.5
+            // 1                1
+            return 2 * orthographicSizeProportion - 1;
+        }
+
+        public void Play()
+        {
+            _audioSource.Play(isSpatial: false, loop: true);
+        }
+
+        public void Stop()
+        {
+            _audioSource.Stop();
+        }
+    }
+}
