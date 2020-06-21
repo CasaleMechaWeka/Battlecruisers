f3539235283c9080078ffee5f277b6f1f8568d7b
Felix Mann <felix.fei.mann@gmail.com>
2020-06-21 14:06:25 -0700

    Create stub :)

---
+++ Assets/Scripts/UI/Sound/Pools/AudioSourcePoolableFactory.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Utils.BattleScene.Pools;
+
+namespace BattleCruisers.UI.Sound.Pools
+{
+    public class AudioSourcePoolableFactory : IPoolableFactory<AudioSourcePoolable, AudioSourceActivationArgs>
+    {
+        public AudioSourcePoolable CreateItem()
+        {
+            // FELIX :D
+            throw new System.NotImplementedException();
+        }
+    }
+}
