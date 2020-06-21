d36b10519e112896fb23c5b2314efc7e28c87022
Felix Mann <felix.fei.mann@gmail.com>
2020-06-21 12:23:48 -0700

    Start dreaming of poolable audio sources :)

---
+++ Assets/Scripts/UI/Sound/Pools/AudioSourceActivationArgs.cs
@@ -0,0 +1,20 @@
+﻿using BattleCruisers.Utils.PlatformAbstractions.UI;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Sound.Pools
+{
+    public class AudioSourceActivationArgs
+    {
+        public IAudioClipWrapper Sound { get; }
+        public Vector2 Position { get; }
+
+        public AudioSourceActivationArgs(IAudioClipWrapper sound, Vector2 position)
+        {
+            Assert.IsNotNull(sound);
+
+            Sound = sound;
+            Position = position;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Sound/Pools/AudioSourcePoolable.cs
@@ -0,0 +1,15 @@
+﻿using BattleCruisers.Utils.BattleScene.Pools;
+using System;
+
+namespace BattleCruisers.UI.Sound.Pools
+{
+    public class AudioSourcePoolable : IPoolable<AudioSourceActivationArgs>
+    {
+        public event EventHandler Deactivated;
+
+        public void Activate(AudioSourceActivationArgs activationArgs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
