b9a8be7f75f209605f837737f521cb47690af6ba
Felix Mann <felix.fei.mann@gmail.com>
2020-06-21 13:49:30 -0700

    Complete AudioSourcePoolable :)

--- Assets/Scripts/UI/Sound/Pools/AudioSourcePoolable.cs
+++ Assets/Scripts/UI/Sound/Pools/AudioSourcePoolable.cs
@@ -1,15 +1,44 @@
-﻿using BattleCruisers.Utils.BattleScene.Pools;
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.BattleScene.Pools;
+using BattleCruisers.Utils.PlatformAbstractions.UI;
+using BattleCruisers.Utils.Threading;
 using System;
+using UnityEngine.Assertions;
 
 namespace BattleCruisers.UI.Sound.Pools
 {
+    // FELIX  Use, test
     public class AudioSourcePoolable : IPoolable<AudioSourceActivationArgs>
     {
+        private readonly IAudioSource _source;
+        private readonly IDeferrer _deferrer;
+
         public event EventHandler Deactivated;
+
+        public AudioSourcePoolable(IAudioSource source, IDeferrer deferrer)
+        {
+            Helper.AssertIsNotNull(source, deferrer);
+
+            _source = source;
+            _deferrer = deferrer;
+        }
 
         public void Activate(AudioSourceActivationArgs activationArgs)
         {
-            throw new NotImplementedException();
+            Assert.IsNotNull(activationArgs);
+
+            _source.IsActive = true;
+            _source.AudioClip = activationArgs.Sound;
+            _source.Position = activationArgs.Position;
+            _source.Play();
+
+            _deferrer.Defer(CleanUp, activationArgs.Sound.AudioClip.length);
+        }
+
+        private void CleanUp()
+        {
+            _source.IsActive = false;
+            Deactivated?.Invoke(this, EventArgs.Empty);
         }
     }
 }
