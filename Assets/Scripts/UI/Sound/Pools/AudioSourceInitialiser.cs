4533e019ddb7e08e8b0a75ea2fd6b6f59b4796e1
Felix Mann <felix.fei.mann@gmail.com>
2020-06-21 14:16:57 -0700

    Create AudioSourceInitialiser :)

---
+++ Assets/Scripts/UI/Sound/Pools/AudioSourceInitialiser.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.Utils.PlatformAbstractions.UI;
+using BattleCruisers.Utils.Threading;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Sound.Pools
+{
+    public class AudioSourceInitialiser : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource _audioSource;
+
+        public AudioSourcePoolable Initialise(IDeferrer realTimeDeferrer)
+        {
+            Assert.IsNotNull(_audioSource);
+            Assert.IsNotNull(realTimeDeferrer);
+
+            return 
+                new AudioSourcePoolable(
+                    new AudioSourceBC(_audioSource), 
+                    realTimeDeferrer);
+        }
+    }
+}
