bdc87c44dbfbdf2d228f2d292a25b90a61578125
Felix Mann <felix.fei.mann@gmail.com>
2020-06-21 13:04:26 -0700

    Create test scene to see how deferrals work with game speed

---
+++ Assets/Scripts/Scenes/Test/InvokeTestGod.cs
@@ -0,0 +1,37 @@
+﻿using BattleCruisers.Utils;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test
+{
+    public class InvokeTestGod : MonoBehaviour
+    {
+        private float _previousGameSpeed = 1;
+
+        public async void Invoke5s()
+        {
+            Logging.LogMethod(Tags.ALWAYS);
+
+            Invoke(nameof(InvokeDone), 5);
+
+            await Task.Delay(5000);
+            Logging.Log(Tags.ALWAYS, "await Task.Delay() done :)");
+        }
+
+        private void InvokeDone()
+        {
+            Logging.Log(Tags.ALWAYS, "Invoke done :D");
+        }
+
+        public void PauseGame()
+        {
+            _previousGameSpeed = Time.timeScale;
+            Time.timeScale = 0;
+        }
+
+        public void ResumeGame()
+        {
+            Time.timeScale = _previousGameSpeed;
+        }
+    }
+}
