15bff53cc7f9c8bd9f68088d24247fd5ddedb57f
Felix Mann <felix.fei.mann@gmail.com>
2020-06-23 16:09:30 -0700

    Tweak capactiy from playtest

--- Assets/Scripts/Effects/Explosions/Pools/InitialCapacity.cs
+++ Assets/Scripts/Effects/Explosions/Pools/InitialCapacity.cs
@@ -4,9 +4,9 @@
     {
         public const int BULLET_IMPACT = 40;
         public const int BOMB = 10;
-        public const int SMALL = 3;
+        public const int SMALL = 10;
         public const int MEDIUM = 10;
-        public const int LARGE = 10;
+        public const int LARGE = 5;
         public const int HUGE = 1;
     }
 }
