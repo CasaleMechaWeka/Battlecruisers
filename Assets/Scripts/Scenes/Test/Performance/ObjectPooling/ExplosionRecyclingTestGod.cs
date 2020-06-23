fb0b567f7e2205f282c965834c2d0908b682a360
Felix Mann <felix.fei.mann@gmail.com>
2020-06-23 08:09:48 -0700

    Create test scene highlighting issue that explosions are not recycled :)

---
+++ Assets/Scripts/Scenes/Test/Performance/ObjectPooling/ExplosionRecyclingTestGod.cs
@@ -0,0 +1,35 @@
+﻿using BattleCruisers.Effects.Explosions.Pools;
+using BattleCruisers.Scenes.Test.Utilities;
+using UnityEngine;
+using BCUtils = BattleCruisers.Utils;
+
+namespace BattleCruisers.Scenes.Test.Performance.ObjectPooling
+{
+    public class ExplosionRecyclingTestGod : TestGodBase
+    {
+        private IExplosionPoolProvider _pools;
+
+        public float delayInS = 0.5f;
+        public float xRadiusInM = 2.5f;
+        public float yRadiusInM = 2.5f;
+
+        protected override void Setup(Helper helper)
+        {
+            base.Setup(helper);
+
+            BuildableInitialisationArgs args = helper.CreateBuildableInitialisationArgs();
+            _pools = args.FactoryProvider.PoolProviders.ExplosionPoolProvider;
+
+            InvokeRepeating(nameof(ShowExplosion), time: 0, repeatRate: delayInS);
+        }
+
+        private void ShowExplosion()
+        {
+            Vector2 spawnPosition = new Vector2(
+                BCUtils.RandomGenerator.Instance.Range(-xRadiusInM, xRadiusInM),
+                BCUtils.RandomGenerator.Instance.Range(-yRadiusInM, yRadiusInM));
+
+            _pools.SmallExplosionsPool.GetItem(spawnPosition);
+        }
+    }
+}
