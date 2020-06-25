0e72a8d12ab6fa2c8cac9901c045a853b493b868
Felix Mann <felix.fei.mann@gmail.com>
2020-06-25 16:47:17 -0700

    Create NonRepeatingHintDisplayer :)

---
+++ Assets/Scripts/UI/BattlScene/InGameHints/NonRepeatingHintDisplayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene.InGameHints
+{
+    // FELIX  Use, test :)
+    public class NonRepeatingHintDisplayer : IHintDisplayer
+    {
+        private readonly HashSet<string> _hints;
+        private readonly IHintDisplayer _coreDisplayer;
+
+        public NonRepeatingHintDisplayer(IHintDisplayer coreDisplayer)
+        {
+            Assert.IsNotNull(coreDisplayer);
+            _coreDisplayer = coreDisplayer;
+
+            _hints = new HashSet<string>();
+        }
+
+        public void ShowHint(string hint)
+        {
+            Assert.IsNotNull(hint);
+
+            if (_hints.Contains(hint))
+            {
+                return;
+            }
+
+            _hints.Add(hint);
+            _coreDisplayer.ShowHint(hint);
+        }
+    }
+}
