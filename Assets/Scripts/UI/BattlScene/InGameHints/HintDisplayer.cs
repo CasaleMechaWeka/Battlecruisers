e0ce28a19d8f1829de6e9bc9cdd229e536a3eeed
Felix Mann <felix.fei.mann@gmail.com>
2020-06-26 15:05:51 -0700

    Hide explanation panel when user clicks ok button :P

--- Assets/Scripts/UI/BattlScene/InGameHints/HintDisplayer.cs
+++ Assets/Scripts/UI/BattlScene/InGameHints/HintDisplayer.cs
@@ -1,4 +1,5 @@
 ﻿using BattleCruisers.Tutorial.Explanation;
+using System;
 using UnityEngine.Assertions;
 
 namespace BattleCruisers.UI.BattleScene.InGameHints
@@ -10,13 +11,21 @@ namespace BattleCruisers.UI.BattleScene.InGameHints
         public HintDisplayer(IExplanationPanel explanationPanel)
         {
             Assert.IsNotNull(explanationPanel);
+
             _explanationPanel = explanationPanel;
+            _explanationPanel.OkButton.Clicked += OkButton_Clicked;
+        }
+
+        private void OkButton_Clicked(object sender, EventArgs e)
+        {
+            _explanationPanel.IsVisible = false;
         }
 
         public void ShowHint(string hint)
         {
             Assert.IsFalse(string.IsNullOrEmpty(hint));
 
+            _explanationPanel.IsVisible = true;
             _explanationPanel.TextDisplayer.DisplayText(hint);
             _explanationPanel.OkButton.Enabled = true;
         }
