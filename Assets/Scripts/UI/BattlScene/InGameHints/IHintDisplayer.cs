8e6623057cdf76d56bd94b35ffb821883ea97d3b
Felix Mann <felix.fei.mann@gmail.com>
2020-06-26 07:36:49 -0700

    Remove completed TODO :)

--- Assets/Scripts/UI/BattlScene/InGameHints/IHintDisplayer.cs
+++ Assets/Scripts/UI/BattlScene/InGameHints/IHintDisplayer.cs
@@ -4,7 +4,6 @@ namespace BattleCruisers.UI.BattleScene.InGameHints
 {
     public interface IHintDisplayer
     {
-        // FELIX  Only show hint of have not shown this hint before :)
         void ShowHint(string hint);
     }
 }
