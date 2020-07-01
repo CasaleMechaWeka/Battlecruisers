b464b74c89a99cb061f918f3619f42044a41104b
Felix Mann <felix.fei.mann@gmail.com>
2020-06-30 17:08:10 -0700

    Make aircraft smoke :)

--- Assets/Scripts/Scenes/Test/Effects/Smokes/AircraftSmokeIntensityTestGod.cs
+++ Assets/Scripts/Scenes/Test/Effects/Smokes/AircraftSmokeIntensityTestGod.cs
@@ -1,4 +1,5 @@
 ﻿using BattleCruisers.Scenes.Test.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -9,6 +10,7 @@ namespace BattleCruisers.Scenes.Test.Effects.Smokes
 {
     public class AircraftSmokeIntensityTestGod : TestGodBase
     {
+        private int _numOfCompletedAircraft;
         private TestAircraftController[] _aircraft;
         public List<Vector2> patrolPoints;
 
@@ -18,7 +20,9 @@ namespace BattleCruisers.Scenes.Test.Effects.Smokes
 
             _aircraft = FindObjectsOfType<TestAircraftController>();
             Assert.IsTrue(_aircraft.Length != 0);
-            
+
+            _numOfCompletedAircraft = 0;
+
             return
                 _aircraft
                     .Select(aircraft => aircraft.GameObject)
@@ -33,18 +37,42 @@ namespace BattleCruisers.Scenes.Test.Effects.Smokes
             {
                 float fuzzedCruisingAltitute = FuzzCruisingAltitude(patrolPoints[0].y);
                 aircraft.PatrolPoints = new List<Vector2>()
-                { 
+                {
                     new Vector2(patrolPoints[0].x, fuzzedCruisingAltitute),
                     new Vector2(patrolPoints[1].x, fuzzedCruisingAltitute)
                 };
                 helper.InitialiseUnit(aircraft);
                 aircraft.StartConstruction();
+                aircraft.CompletedBuildable += Aircraft_CompletedBuildable;
+            }
+        }
+
+        private void Aircraft_CompletedBuildable(object sender, EventArgs e)
+        {
+            _numOfCompletedAircraft++;
+
+            if (_numOfCompletedAircraft == _aircraft.Length)
+            {
+                DamagePlanes(_aircraft[0].MaxHealth - 1);
             }
         }
 
         private float FuzzCruisingAltitude(float cruisingAltitudeInM)
         {
             return BCUtils.RandomGenerator.Instance.RangeFromCenter(cruisingAltitudeInM, radius: 1);
+        }
+
+        public void KillPlanes()
+        {
+            DamagePlanes(_aircraft[0].MaxHealth);
+        }
+
+        private void DamagePlanes(float damage)
+        {
+            foreach (TestAircraftController aircraft in _aircraft)
+            {
+                aircraft.TakeDamage(damage, damageSource: null);
+            }
         }
     }
 }
