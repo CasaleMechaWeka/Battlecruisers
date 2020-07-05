53844667f4f675c69698f8d6e932dc32422c1272
Felix Mann <felix.fei.mann@gmail.com>
2020-07-05 10:57:42 -0700

    Create CameraTargetTracker :)

---
+++ Assets/Scripts/UI/Cameras/Targets/CameraTargetTracker.cs
@@ -0,0 +1,49 @@
+﻿using BattleCruisers.Utils;
+using BattleCruisers.Utils.PlatformAbstractions;
+using System;
+using UnityCommon.Properties;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.Cameras.Targets
+{
+    // FELIX  use, test
+    public class CameraTargetTracker : ICameraTargetTracker
+    {
+        private readonly ICamera _camera;
+        private readonly ICameraTarget _target;
+        private readonly float _positionEqualityMarginInM;
+        private readonly float _orthographicSizeEqualityMargin;
+
+        private readonly ISettableBroadcastingProperty<bool> _isOnTarget;
+        public IBroadcastingProperty<bool> IsOnTarget { get; }
+
+        public CameraTargetTracker(ICamera camera, ICameraTarget target, float positionEqualityMarginInM, float orthographicSizeEqualityMargin)
+        {
+            Helper.AssertIsNotNull(camera, target);
+            Assert.IsTrue(positionEqualityMarginInM > 0);
+            Assert.IsTrue(orthographicSizeEqualityMargin > 0);
+
+            _camera = camera;
+            _target = target;
+            _positionEqualityMarginInM = positionEqualityMarginInM;
+            _orthographicSizeEqualityMargin = orthographicSizeEqualityMargin;
+
+            _isOnTarget = new SettableBroadcastingProperty<bool>(initialValue: FindIfOnTarget());
+
+            _camera.PositionChanged += CameraChanged;
+            _camera.OrthographicSizeChanged += CameraChanged;
+        }
+
+        private void CameraChanged(object sender, EventArgs e)
+        {
+            _isOnTarget.Value = FindIfOnTarget();
+        }
+
+        private bool FindIfOnTarget()
+        {
+            return
+                _camera.OrthographicSize - _target.OrthographicSize < _orthographicSizeEqualityMargin
+                && (_camera.Position - _target.Position).magnitude < _positionEqualityMarginInM;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Cameras/Targets/ICameraTargetTracker.cs
@@ -0,0 +1,9 @@
+﻿using UnityCommon.Properties;
+
+namespace BattleCruisers.UI.Cameras.Targets
+{
+    public interface ICameraTargetTracker
+    {
+        IBroadcastingProperty<bool> IsOnTarget { get; }
+    }
+}
