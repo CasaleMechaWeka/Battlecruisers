84a717a0f96d54d1a8e707dbbfa952453c8a0ff0
FelixMann <felix.fei.mann@gmail.com>
2020-07-16 07:36:07 -0700

    Unsubscribe from desttroyed event, so it does not get triggered after we've cleaned up :)

--- Assets/Scripts/Buildables/Units/Aircraft/KamikazeController.cs
+++ Assets/Scripts/Buildables/Units/Aircraft/KamikazeController.cs
@@ -1,21 +1,21 @@
-﻿using BattleCruisers.Effects.Explosions.Pools;
+﻿using System.Collections.Generic;
+using BattleCruisers.Effects.Explosions.Pools;
 using BattleCruisers.Projectiles.DamageAppliers;
 using BattleCruisers.Projectiles.Stats;
 using BattleCruisers.Targets.TargetFinders.Filters;
 using BattleCruisers.Utils;
-using BattleCruisers.Utils.BattleScene;
 using BattleCruisers.Utils.Factories;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace BattleCruisers.Buildables.Units.Aircraft
 {
-    public class KamikazeController : MonoBehaviour, IRemovable
+    public class KamikazeController : MonoBehaviour
     {
         private IUnit _parentAircraft;
         private ITargetFilter _targetFilter;
         private IDamageApplier _damageApplier;
         private IExplosionPoolProvider _explosionPoolProvider;
+        private ITarget _initialTarget;
 
         // Have this to defer damaging the target until the next FixedUpdate(), because
         // there is a bug in Unity that if the target is destroyed from OnTriggerEnter2D()
@@ -30,28 +30,29 @@ namespace BattleCruisers.Buildables.Units.Aircraft
             Helper.AssertIsNotNull(parentAircraft, factoryProvider);
 
             _parentAircraft = parentAircraft;
+            _initialTarget = target;
             _targetToDamage = null;
 
             List<TargetType> targetTypes = new List<TargetType>() { TargetType.Buildings, TargetType.Cruiser, TargetType.Ships };
-            _targetFilter = factoryProvider.Targets.FilterFactory.CreateTargetFilter(target.Faction, targetTypes);
+            _targetFilter = factoryProvider.Targets.FilterFactory.CreateTargetFilter(_initialTarget.Faction, targetTypes);
 
             IDamageStats kamikazeDamageStats
                 = factoryProvider.DamageApplierFactory.CreateDamageStats(
                     damage: parentAircraft.MaxHealth * KAMIKAZE_DAMAGE_MULTIPLIER,
                     damageRadiusInM: parentAircraft.Size.x);
-            _damageApplier = factoryProvider.DamageApplierFactory.CreateFactionSpecificAreaOfDamageApplier(kamikazeDamageStats, target.Faction);
+            _damageApplier = factoryProvider.DamageApplierFactory.CreateFactionSpecificAreaOfDamageApplier(kamikazeDamageStats, _initialTarget.Faction);
 
             _explosionPoolProvider = factoryProvider.PoolProviders.ExplosionPoolProvider;
 
-            target.Destroyed += Target_Destroyed;
+            _initialTarget.Destroyed += Target_Destroyed;
         }
 
         private void Target_Destroyed(object sender, DestroyedEventArgs e)
         {
             if (!_parentAircraft.IsDestroyed)
             {
-                RemoveFromScene();
                 _explosionPoolProvider.SmallExplosionsPool.GetItem(transform.position);
+                CleanUp();
             }
         }
 
@@ -74,14 +75,15 @@ namespace BattleCruisers.Buildables.Units.Aircraft
             if (_targetToDamage != null
                 && !_parentAircraft.IsDestroyed)
             {
-                RemoveFromScene();
                 _damageApplier.ApplyDamage(_targetToDamage, _parentAircraft.Position, damageSource: _parentAircraft);
                 _explosionPoolProvider.SmallExplosionsPool.GetItem(transform.position);
+                CleanUp();
             }
         }
 
-        public void RemoveFromScene()
+        public void CleanUp()
         {
+            _initialTarget.Destroyed -= Target_Destroyed;
             _targetToDamage = null;
             _parentAircraft.Destroy();
         }
