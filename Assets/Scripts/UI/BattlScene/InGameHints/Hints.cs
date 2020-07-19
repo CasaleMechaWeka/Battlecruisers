246812a0fe6fb0d14ed05bf3200e8a50770456fe
Felix Mann <felix.fei.mann@gmail.com>
2020-07-19 11:06:43 -0700

    Update factory hint with how to pause

--- Assets/Scripts/UI/BattlScene/InGameHints/Hints.cs
+++ Assets/Scripts/UI/BattlScene/InGameHints/Hints.cs
@@ -5,7 +5,7 @@
         public const string AIR_FACTORY_RESPONSE_HINT = "The enemy is building an air factory!  Maybe build some air turrets :D";
         public const string NAVAL_FACTORY_RESPONSE_HINT = "The enemy is building a naval factory!  Maybe build some ship turrets :D";
         public const string OFFENSIVE_RESPONSE_HINT = "The enemy is building an offensive!  Maybe build some shields :D";
-        public const string FACTORY_COMPLETED_HINT = "You have build a factory :D  To start building units, select the factory and choose a unit.";
+        public const string FACTORY_COMPLETED_HINT = "You have build a factory :D  To start or pause building units, select the factory and choose a unit.";
         public const string UNIT_CHOSEN_HINT = "You have started building a unit :D  The factory will keep building units as long as it has builders.";
         public const string PLAYER_DAMAGED_HINT = "You are under attack!  Repairs happen automatically as long as there are free builders.  To force repairs select the building and press the \"REPAIR\" button (or double click it).";
     }
