3e4d8a4a6482127283ba40f333d7f43295a4e201
Felix Mann <felix.fei.mann@gmail.com>
2020-09-11 07:18:39 -0700

    Add stub

---
+++ Assets/Scripts/Scenes/Test/Utilities/LevelButtonController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using BattleCruisers.Data.Static;
+using BattleCruisers.UI.BattleScene.Clouds.Stats;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using BattleCruisers.Data;
+
+namespace Assets.Scripts.Scenes.Test.Utilities
+{
+    public class LevelButtonController : MonoBehaviour
+    {
+        public Text levelNumText;
+
+        public void Initialise(int levelNum, SkyStatsGroup skyStatsGroup, IStaticData staticData)
+        {
+            Assert.IsNotNull(levelNumText);
+
+            levelNumText.text = levelNum.ToString();
+
+            ILevel level = staticData.Levels[levelNum - 1];
+            // FELIX  TODO :)
+        }
+    }
+}
