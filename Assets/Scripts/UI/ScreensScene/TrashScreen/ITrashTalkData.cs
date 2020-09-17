0059519e95cd34cfa393983b848c5957a15eb491
Felix Mann <felix.fei.mann@gmail.com>
2020-09-17 07:40:40 -0700

    Create TrashTalkData :)

---
+++ Assets/Scripts/UI/ScreensScene/TrashScreen/ITrashTalkData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.UI.ScreensScene.TrashScreen
+{
+    public interface ITrashTalkData
+    {
+        // FELIX  Avoid duplication (enemy image, enemy name) with levels screen
+        Sprite EnemyImage { get; }
+        string EnemyName { get; }
+        bool PlayerTalksFirst { get; }
+        string PlayerText { get; }
+        string EnemyText { get; }
+    }
+}
---
+++ Assets/Scripts/UI/ScreensScene/TrashScreen/TrashTalkData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.UI.ScreensScene.TrashScreen
+{
+    public class TrashTalkData : MonoBehaviour, ITrashTalkData
+    {
+        public Sprite enemyImage;
+        public Sprite EnemyImage => enemyImage;
+
+        public string enemyName = "Bill";
+        public string EnemyName => enemyName;
+
+        public bool playerTalksFirst;
+        public bool PlayerTalksFirst => playerTalksFirst;
+
+        public string playerText = "Sup";
+        public string PlayerText => playerText;
+
+        public string enemyText = "Kia Ora";
+        public string EnemyText => enemyText;
+    }
+}
