c0409f189cb01a58149fbc2619f92c02ce34340d
Peter Casale <casalepeter@gmail.com>
2020-09-25 08:28:58 +1200

    Fixed typo in directory name "Battlscene" - if everything catastrophically breaks, this is why!

---
+++ Assets/Scripts/UI/BattleScene/Cruisers/NumOfDronesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using BattleCruisers.Cruisers.Drones;
+using BattleCruisers.Utils;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.BattleScene.Cruisers
+{
+    public class NumOfDronesController : Togglable, IManagedDisposable, IButton, IPointerClickHandler
+    {
+        private IDroneManager _droneManager;
+
+        public Text numOfDronesText;
+
+        public event EventHandler Clicked;
+
+        private int NumOfDrones
+        {
+            set
+            {
+                numOfDronesText.text = value.ToString();
+            }
+        }
+
+        public void Initialise(IDroneManager droneManager)
+        {
+            base.Initialise();
+
+            Helper.AssertIsNotNull(numOfDronesText, droneManager);
+
+            _droneManager = droneManager;
+            _droneManager.DroneNumChanged += _droneManager_DroneNumChanged;
+            NumOfDrones = _droneManager.NumOfDrones;
+        }
+
+        private void _droneManager_DroneNumChanged(object sender, DroneNumChangedEventArgs e)
+        {
+            NumOfDrones = e.NewNumOfDrones;
+        }
+
+        public void DisposeManagedState()
+        {
+            _droneManager.DroneNumChanged -= _droneManager_DroneNumChanged;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Clicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
