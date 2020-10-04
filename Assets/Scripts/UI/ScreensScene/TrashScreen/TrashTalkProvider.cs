8d9bce3d932e0f419db7c5c91c9b31094cf276b5
Felix Mann <felix.fei.mann@gmail.com>
2020-10-04 11:39:43 -0700

    Create TrashTalkProvider :)

---
+++ Assets/Scripts/UI/ScreensScene/TrashScreen/TrashTalkProvider.cs
@@ -0,0 +1,17 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Utils.Fetchers;
+using System.Threading.Tasks;
+
+namespace BattleCruisers.UI.ScreensScene.TrashScreen
+{
+    public class TrashTalkProvider : ITrashTalkProvider
+    {
+        private readonly IPrefabFetcher _prefabFetcher;
+
+        public async Task<ITrashTalkData> GetTrashTalkAsync(int levelNum)
+        {
+            IPrefabKey key = new TrashTalkKey(levelNum);
+            return await _prefabFetcher.GetPrefabAsync<TrashTalkData>(key);
+        }
+    }
+}
