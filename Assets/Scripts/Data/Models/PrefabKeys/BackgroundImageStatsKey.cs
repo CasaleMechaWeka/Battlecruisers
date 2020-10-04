25f770117a5ee416baaa5e3eabd49f46d77d11e7
Felix Mann <felix.fei.mann@gmail.com>
2020-10-04 08:46:14 -0700

    Create BackgroundImageStatsKey :)

---
+++ Assets/Scripts/Data/Models/PrefabKeys/BackgroundImageStatsKey.cs
@@ -0,0 +1,29 @@
+﻿using BattleCruisers.Data.Static;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Data.Models.PrefabKeys
+{
+    public class BackgroundImageStatsKey : PrefabKey
+    {
+        private const string BACKGROUND_PATH = "Clouds/Background";
+
+        protected override string PrefabPathPrefix
+        {
+            get
+            {
+                return base.PrefabPathPrefix + BACKGROUND_PATH + PATH_SEPARATOR;
+            }
+        }
+
+        public BackgroundImageStatsKey(int levelNum)
+            : base(CreatePrefabName(levelNum)) { }
+
+        private static string CreatePrefabName(int levelNum)
+        {
+            Assert.IsTrue(levelNum > 0);
+            Assert.IsTrue(levelNum <= StaticData.NUM_OF_LEVELS);
+
+            return $"BackgroundStatsLevel ({levelNum}) Variant";
+        }
+    }
+}
