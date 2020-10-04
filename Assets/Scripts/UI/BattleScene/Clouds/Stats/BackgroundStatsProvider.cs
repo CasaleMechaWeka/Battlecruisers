8f15fb69c54a26ffe69cb37f69f642c823cdc497
Felix Mann <felix.fei.mann@gmail.com>
2020-10-04 08:50:00 -0700

    Create BackgroundStatsProvider :)

---
+++ Assets/Scripts/UI/BattleScene/Clouds/Stats/BackgroundStatsProvider.cs
@@ -0,0 +1,25 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Utils.Fetchers;
+using System.Threading.Tasks;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.UI.BattleScene.Clouds.Stats
+{
+    // FELIX  Use
+    public class BackgroundStatsProvider : IBackgroundStatsProvider
+    {
+        private readonly IPrefabFetcher _prefabFetcher;
+
+        public BackgroundStatsProvider(IPrefabFetcher prefabFetcher)
+        {
+            Assert.IsNotNull(prefabFetcher);
+            _prefabFetcher = prefabFetcher;
+        }
+
+        public async Task<IBackgroundImageStats> GetStatsAsync(int levelNum)
+        {
+            IPrefabKey key = new BackgroundImageStatsKey(levelNum);
+            return await _prefabFetcher.GetPrefabAsync<BackgroundImageStats>(key);
+        }
+    }
+}
