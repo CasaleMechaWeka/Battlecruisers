cd59d0b4ca7adeacf80e350e71960c01600f6ed8
Felix Mann <felix.fei.mann@gmail.com>
2020-10-04 11:31:43 -0700

    Create TrashTalkKey :)

---
+++ Assets/Scripts/Data/Models/PrefabKeys/TrashTalkKey.cs
@@ -0,0 +1,21 @@
+﻿using BattleCruisers.Data.Static;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Data.Models.PrefabKeys
+{
+    public class TrashTalkKey : PrefabKey
+    {
+        protected override string PrefabPathPrefix => "Prefabs/ScreensScene/TrashScreen/";
+
+        public TrashTalkKey(int levelNum)
+            : base(CreatePrefabName(levelNum)) { }
+
+        private static string CreatePrefabName(int levelNum)
+        {
+            Assert.IsTrue(levelNum > 0);
+            Assert.IsTrue(levelNum <= StaticData.NUM_OF_LEVELS);
+
+            return $"TrashTalkData ({levelNum}) Variant";
+        }
+    }
+}
