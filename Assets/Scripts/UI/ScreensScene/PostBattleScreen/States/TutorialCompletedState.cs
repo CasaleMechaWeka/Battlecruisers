884d45aa07e9fa9054c905307956e5bf0208e3b0
Felix Mann <felix.fei.mann@gmail.com>
2020-10-08 07:41:06 -0700

    Add line break to tutorial completed text :)

--- Assets/Scripts/UI/ScreensScene/PostBattleScreen/States/TutorialCompletedState.cs
+++ Assets/Scripts/UI/ScreensScene/PostBattleScreen/States/TutorialCompletedState.cs
@@ -9,7 +9,9 @@ namespace BattleCruisers.UI.ScreensScene.PostBattleScreen.States
     public class TutorialCompletedState
     {
 		private const string TUTORIAL_TITLE = "Tutorial Completed :D";
-		private const string TUTORIAL_APPRAISAL_DRONE_TEXT = "Your 10 minute tutorial simulation is complete! You are now a qualified Battlecruiser Captain, certified by the UAC. On behalf of the crew of builder drones, I’d like to thank you for choosing to steal a Trident-class battlecruiser! We hope your joyride is comfortable.";
+		private const string TUTORIAL_APPRAISAL_DRONE_TEXT =
+@"Your 10 minute How-to simulation is complete!
+You are now a qualified Battlecruiser Captain, certified by the UAC. On behalf of us crew of builder drones, I’d like to thank you for choosing to steal a Trident-class Battlecruiser! We hope your joyride is comfortable.";
 
         public void Initialise(
             PostBattleScreenController postBattleScreen,
