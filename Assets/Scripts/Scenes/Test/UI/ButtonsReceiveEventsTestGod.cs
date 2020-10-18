d62bd30c97c362bf6018261971db798aceabd2fa
Felix Mann <felix.fei.mann@gmail.com>
2020-10-18 13:50:16 -0700

    Create test scene with action button, seems to work fine :/

---
+++ Assets/Scripts/Scenes/Test/UI/ButtonsReceiveEventsTestGod.cs
@@ -0,0 +1,26 @@
+﻿using BattleCruisers.UI;
+using BattleCruisers.UI.Sound;
+using NSubstitute;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Scenes.Test.UI
+{
+    public class ButtonsReceiveEventsTestGod : MonoBehaviour
+    {
+        public ActionButton actionButton;
+
+        void Start()
+        {
+            Assert.IsNotNull(actionButton);
+
+            ISingleSoundPlayer soundPlayer = Substitute.For<ISingleSoundPlayer>();
+            actionButton.Initialise(soundPlayer, SweetAction);
+        }
+
+        private void SweetAction()
+        {
+            Debug.Log("SweetAction");
+        }
+    }
+}
