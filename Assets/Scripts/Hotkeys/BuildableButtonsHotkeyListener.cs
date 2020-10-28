cc81b90de2b7f4d4c2f380902b44b0a6387d3543
Felix Mann <felix.fei.mann@gmail.com>
2020-10-28 07:07:44 -0700

    Create ShipButtonsHotkeyListener stub

---
+++ Assets/Scripts/Hotkeys/BuildableButtonsHotkeyListener.cs
@@ -0,0 +1,37 @@
+﻿using BattleCruisers.UI.BattleScene.Buttons;
+using BattleCruisers.UI.BattleScene.Navigation;
+using BattleCruisers.Utils;
+using System;
+
+// FELIX  Test
+namespace BattleCruisers.Hotkeys
+{
+    public class ShipButtonsHotkeyListener : IManagedDisposable
+    {
+        private readonly IHotkeyDetector _hotkeyDetector;
+        private readonly IBuildableButton _attackBoatButton, _frigateButton, _destroyerButton, _archonButton;
+
+        public ShipButtonsHotkeyListener(
+            IHotkeyDetector hotkeyDetector,
+            IBuildableButton attackBoatButton,
+            IBuildableButton frigateButton,
+            IBuildableButton destroyerButton,
+            IBuildableButton archonButton)
+        {
+            Helper.AssertIsNotNull(hotkeyDetector, attackBoatButton, frigateButton, destroyerButton, archonButton);
+
+            _hotkeyDetector = hotkeyDetector;
+
+            _attackBoatButton = attackBoatButton;
+            _frigateButton = frigateButton;
+            _destroyerButton = destroyerButton;
+            _archonButton = archonButton;
+
+            // FELIX
+        }
+
+        public void DisposeManagedState()
+        {
+        }
+    }
+}
