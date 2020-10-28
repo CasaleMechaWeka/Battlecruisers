4e23f0dee2cecd547e27d49f4086316717eb543c
Felix Mann <felix.fei.mann@gmail.com>
2020-10-28 07:27:51 -0700

    Complete ShipButtonsHotkeyListener :)

---
+++ Assets/Scripts/Hotkeys/ShipButtonsHotkeyListener.cs
@@ -0,0 +1,72 @@
+﻿using BattleCruisers.UI.BattleScene.Buttons;
+using BattleCruisers.Utils;
+using System;
+
+// FELIX  Test
+// FELIX  Create class for single hotkey to avoid duplicate code?  Takes event and button?
+namespace BattleCruisers.Hotkeys
+{
+    public class ShipButtonsHotkeyListener : IManagedDisposable
+    {
+        private readonly IHotkeyDetector _hotkeyDetector;
+        private readonly IBuildableButton _attackBoatButton, _frigateButton, _destroyerButton, _archonButton;
+
+        public ShipButtonsHotkeyListener(
+            IHotkeyDetector hotkeyDetector,
+            IBuildableButton attackBoatButton,
+            IBuildableButton frigateButton,
+            IBuildableButton destroyerButton,
+            IBuildableButton archonButton)
+        {
+            Helper.AssertIsNotNull(hotkeyDetector, attackBoatButton, frigateButton, destroyerButton, archonButton);
+
+            _hotkeyDetector = hotkeyDetector;
+
+            _attackBoatButton = attackBoatButton;
+            _frigateButton = frigateButton;
+            _destroyerButton = destroyerButton;
+            _archonButton = archonButton;
+
+            _hotkeyDetector.AttackBoat += _hotkeyDetector_AttackBoat;
+            _hotkeyDetector.Frigate += _hotkeyDetector_Frigate;
+            _hotkeyDetector.Destroyer += _hotkeyDetector_Destroyer;
+            _hotkeyDetector.Archon += _hotkeyDetector_Archon;
+        }
+
+        private void _hotkeyDetector_AttackBoat(object sender, EventArgs e)
+        {
+            ClickIfPresented(_attackBoatButton);
+        }
+
+        private void _hotkeyDetector_Frigate(object sender, EventArgs e)
+        {
+            ClickIfPresented(_frigateButton);
+        }
+
+        private void _hotkeyDetector_Destroyer(object sender, EventArgs e)
+        {
+            ClickIfPresented(_destroyerButton);
+        }
+
+        private void _hotkeyDetector_Archon(object sender, EventArgs e)
+        {
+            ClickIfPresented(_archonButton);
+        }
+
+        private void ClickIfPresented(IBuildableButton button)
+        {
+            if (button.IsPresented)
+            {
+                button.TriggerClick();
+            }
+        }
+
+        public void DisposeManagedState()
+        {
+            _hotkeyDetector.AttackBoat -= _hotkeyDetector_AttackBoat;
+            _hotkeyDetector.Frigate -= _hotkeyDetector_Frigate;
+            _hotkeyDetector.Destroyer -= _hotkeyDetector_Destroyer;
+            _hotkeyDetector.Archon -= _hotkeyDetector_Archon;
+        }
+    }
+}
--- Assets/Scripts/UI/BattleScene/Buttons/IBuildableButton.cs
+++ Assets/Scripts/UI/BattleScene/Buttons/IBuildableButton.cs
@@ -1,9 +1,10 @@
 ﻿using BattleCruisers.Buildables;
+using BattleCruisers.UI.BattleScene.Presentables;
 using UnityEngine;
 
 namespace BattleCruisers.UI.BattleScene.Buttons
 {
-    public interface IBuildableButton : IButton
+    public interface IBuildableButton : IButton, IPresentable
     {
         IBuildable Buildable { get; }
         Color Color { set; }
