23ed300fd5fba1418e87339ee9b7d85a060acbc8
Felix Mann <felix.fei.mann@gmail.com>
2020-11-01 09:56:23 -0800

    All buildable hotkeys work :D

--- Assets/Scripts/Hotkeys/BuildableButtons/BuildableButtonsHotkeyInitialiser.cs
+++ Assets/Scripts/Hotkeys/BuildableButtons/BuildableButtonsHotkeyInitialiser.cs
@@ -10,28 +10,54 @@ namespace Assets.Scripts.Hotkeys.BuildableButtons
     public class BuildableButtonsHotkeyInitialiser : MonoBehaviour
     {
         // Keep references to avoid garbage collection
-        private IManagedDisposable _shipButtonsHotkeyListener, _factoryButtonsHotkeyListener, _defensiveButtonsHotkeyListener;
+        private IManagedDisposable _factoriesListener, _defensivesListener, _offensivesListener, _tacticalsListener, _ultrasListener, _aircraftListener, _shipsListener;
 
         [Header("Factories")]
         public BuildingButtonController droneStationButton;
         public BuildingButtonController airFactoryButton, navalFactoryButton;
+        
         [Header("Defensives")]
         public BuildingButtonController shipTurretButton;
         public BuildingButtonController airTurretButton, mortarButton, samSiteButton, teslaCoilButton;
+
+        [Header("Offensives")]
+        public BuildingButtonController artilleryButton;
+        public BuildingButtonController railgunButton, rocketLauncherButton;
+
+        [Header("Tacticals")]
+        public BuildingButtonController shieldButton;
+        public BuildingButtonController boosterButton, stealthGeneratorButton, spySatelliteButton, controlTowerButton;
+
+        [Header("Ultras")]
+        public BuildingButtonController deathstarButton;
+        public BuildingButtonController nukeLauncherButton, ultraliskButton, kamikazeSignalButton, broadsidesButton;
+
+        [Header("Aircraft")]
+        public UnitButtonController bomberButton;
+        public UnitButtonController gunshipButton, fighterButton;
+
         [Header("Ships")]
         public UnitButtonController attackBoatButton;
         public UnitButtonController frigateButton, destroyerButton, archonButton;
 
         public void Initialise(IHotkeyDetector hotkeyDetector)
         {
-            Helper.AssertIsNotNull(attackBoatButton, frigateButton, destroyerButton, archonButton);
             Helper.AssertIsNotNull(droneStationButton, airFactoryButton, navalFactoryButton);
             Helper.AssertIsNotNull(shipTurretButton, airTurretButton, mortarButton, samSiteButton, teslaCoilButton);
+            Helper.AssertIsNotNull(artilleryButton, railgunButton, rocketLauncherButton);
+            Helper.AssertIsNotNull(shieldButton, boosterButton, stealthGeneratorButton, spySatelliteButton, controlTowerButton);
+            Helper.AssertIsNotNull(deathstarButton, nukeLauncherButton, ultraliskButton, kamikazeSignalButton, broadsidesButton);
+            Helper.AssertIsNotNull(bomberButton, gunshipButton, fighterButton);
+            Helper.AssertIsNotNull(attackBoatButton, frigateButton, destroyerButton, archonButton);
             Assert.IsNotNull(hotkeyDetector);
 
-            _factoryButtonsHotkeyListener = new FactoryButtonsHotkeyListener(hotkeyDetector, droneStationButton, airFactoryButton, navalFactoryButton);
-            _defensiveButtonsHotkeyListener = new DefensiveButtonsHotkeyListener(hotkeyDetector, shipTurretButton, airTurretButton, mortarButton, samSiteButton, teslaCoilButton);
-            _shipButtonsHotkeyListener = new ShipButtonsHotkeyListener(hotkeyDetector, attackBoatButton, frigateButton, destroyerButton, archonButton);
+            _factoriesListener = new FactoryButtonsHotkeyListener(hotkeyDetector, droneStationButton, airFactoryButton, navalFactoryButton);
+            _defensivesListener = new DefensiveButtonsHotkeyListener(hotkeyDetector, shipTurretButton, airTurretButton, mortarButton, samSiteButton, teslaCoilButton);
+            _offensivesListener = new OffensiveButtonsHotkeyListener(hotkeyDetector, artilleryButton, railgunButton, rocketLauncherButton);
+            _tacticalsListener = new TacticalButtonsHotkeyListener(hotkeyDetector, shieldButton, boosterButton, stealthGeneratorButton, spySatelliteButton, controlTowerButton);
+            _ultrasListener = new UltraButtonsHotkeyListener(hotkeyDetector, deathstarButton, nukeLauncherButton, ultraliskButton, kamikazeSignalButton, broadsidesButton);
+            _aircraftListener = new AircraftButtonsHotkeyListener(hotkeyDetector, bomberButton, gunshipButton, fighterButton);
+            _shipsListener = new ShipButtonsHotkeyListener(hotkeyDetector, attackBoatButton, frigateButton, destroyerButton, archonButton);
         }
     }
 }
