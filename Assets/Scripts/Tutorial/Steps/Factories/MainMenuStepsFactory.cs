8aa49eb1815f44075ae6705174c76b8e6ef089fb
Felix Mann <felix.fei.mann@gmail.com>
2020-11-03 07:06:51 -0800

    Create MainMenuStepsFactory :)

---
+++ Assets/Scripts/Tutorial/Steps/Factories/MainMenuStepsFactory.cs
@@ -0,0 +1,37 @@
+﻿using BattleCruisers.Tutorial.Highlighting;
+using BattleCruisers.Utils;
+using System.Collections.Generic;
+
+namespace BattleCruisers.Tutorial.Steps.Factories
+{
+    public class MainMenuStepsFactory : TutorialFactoryBase, ITutorialStepsFactory
+    {
+        private readonly IHighlightable _mainMenuButton;
+        private readonly IExplanationDismissableStepFactory _explanationDismissableStepFactory;
+
+        public MainMenuStepsFactory(
+            ITutorialStepArgsFactory argsFactory,
+            IHighlightable mainMenuButton,
+            IExplanationDismissableStepFactory explanationDismissableStepFactory) 
+            : base(argsFactory)
+        {
+            Helper.AssertIsNotNull(mainMenuButton, explanationDismissableStepFactory);
+
+            _mainMenuButton = mainMenuButton;
+            _explanationDismissableStepFactory = explanationDismissableStepFactory;
+        }
+
+        public IList<ITutorialStep> CreateSteps()
+        {
+            IList<ITutorialStep> steps = new List<ITutorialStep>();
+
+            ITutorialStepArgs args
+                = _argsFactory.CreateTutorialStepArgs(
+                    "This is the main menu button",
+                    _mainMenuButton);
+            steps.Add(_explanationDismissableStepFactory.CreateStep(args));
+
+            return steps;
+        }
+    }
+}
