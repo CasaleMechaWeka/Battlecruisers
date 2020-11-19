9718699be1575abb5008057e1d62074d0c0fd5f7
Felix Mann <felix.fei.mann@gmail.com>
2020-11-19 07:53:41 -0800

    Reduce default tutorial build speed, so buildings don't insta build :P

--- Assets/Scripts/Utils/Constants.cs
+++ Assets/Scripts/Utils/Constants.cs
@@ -32,7 +32,7 @@
         public const float DEFAULT = 2;
         public const float ONE_AND_A_QUARTER_DEFAULT = 2.5f;
         public const float ONE_AND_A_HALF_DEFAULT = 3;
-        public const float DEFAULT_TUTORIAL = 8;
+        public const float DEFAULT_TUTORIAL = 3;
         public const float FAST = 50;
 		public const float VERY_FAST = 500;
     }
