02d9d44e1a6d5f152b1373f7d6b1e8e05a4ad367
Felix Mann <felix.fei.mann@gmail.com>
2020-11-29 07:58:00 -0800

    Create DummyProjectileSoundPlayerInitialiser :)

---
+++ Assets/Scripts/UI/Sound/ProjectileSpawners/DummyProjectileSoundPlayerInitialiser.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BattleCruisers.UI.Sound.ProjectileSpawners
+{
+    public class DummyProjectileSoundPlayerInitialiser : MonoBehaviour, IProjectileSoundPlayerInitialiser
+    {
+        public Task<IProjectileSpawnerSoundPlayer> CreateSoundPlayerAsync(ISoundPlayerFactory soundPlayerFactory, ISoundKey firingSound, int burstSize)
+        {
+            return Task.FromResult(soundPlayerFactory.DummyPlayer);
+        }
+    }
+}
