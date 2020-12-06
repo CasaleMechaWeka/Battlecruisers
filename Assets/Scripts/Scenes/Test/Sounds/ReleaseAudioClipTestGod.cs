f6a1906a5bb39db0db0a6360de6a703f31b6b29a
Felix Mann <felix.fei.mann@gmail.com>
2020-12-06 10:00:31 -0800

    Create test scene to try and get audio clip release working :P

---
+++ Assets/Scripts/Scenes/Test/Sounds/ReleaseAudioClipTestGod.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace BattleCruisers.Scenes.Test.Sounds
+{
+    public class ReleaseAudioClipTestGod : MonoBehaviour
+    {
+
+        public string audioClipPath = "Assets/Resources_moved/Sounds/Music/confusion-danger";
+
+        void Start()
+        {
+            TempAsync();
+        }
+
+        private AsyncOperationHandle<AudioClip> _handle;
+
+        private async Task TempAsync()
+        {
+            AsyncOperationHandle<AudioClip> handle = Addressables.LoadAssetAsync<AudioClip>(audioClipPath);
+            await handle.Task;
+            // Expected ref count: 1  Actual:  7
+
+            //Addressables.Release(handle);
+            // Expected ref count: 0  Actual:  3
+        }
+
+        public void LoadClip()
+        {
+            _handle = Addressables.LoadAssetAsync<AudioClip>(audioClipPath);
+        }
+
+        public void ReleaseClip()
+        {
+            Addressables.Release(_handle);
+        }
+    }
+}
