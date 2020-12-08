5005505337ad4fc0fd0525c060a7322be9fe243c
Felix Mann <felix.fei.mann@gmail.com>
2020-12-08 07:24:03 -0800

    Fix cruiser deaths test scene :)

---
+++ Assets/Scripts/Scenes/Test/Cruisers/AllCruiserDeathsTestGod.cs
@@ -0,0 +1,19 @@
+﻿using BattleCruisers.Effects.Explosions;
+using UnityEngine;
+
+namespace Assets.Scripts.Scenes.Test.Cruisers
+{
+    public class AllCruiserDeathsTestGod : MonoBehaviour
+    {
+        void Start()
+        {
+            ExplosionController[] cruiserDeaths = FindObjectsOfType<ExplosionController>();
+
+            foreach (ExplosionController death in cruiserDeaths)
+            {
+                IExplosion deathExplosion = death.Initialise();
+                deathExplosion.Activate(death.Position);
+            }
+        }
+    }
+}
