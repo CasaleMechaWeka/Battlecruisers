01772bdcdaef6ff280d34b84f6c49115072ca2d1
Felix Mann <felix.fei.mann@gmail.com>
2020-12-19 06:56:13 -0800

    F4 toggles UI :)

--- Assets/Scripts/Utils/Debugging/KeyboardCheater.cs
+++ Assets/Scripts/Utils/Debugging/KeyboardCheater.cs
@@ -14,52 +14,58 @@ namespace BattleCruisers.Utils.Debugging
 
         void Update()
         {
-            // W = Win
-            if (Input.GetKeyUp(KeyCode.W))
-            {
-                cheater.Win();
-            }
-            // L = Loss
-            else if (Input.GetKeyUp(KeyCode.L))
-            {
-                cheater.Lose();
-            }
-            // B = Builders
-            else if (Input.GetKeyUp(KeyCode.B))
-            {
-                cheater.AddBuilders();
-            }
-            // T = Toggle UI
-            else if (Input.GetKeyUp(KeyCode.T))
+            if (Input.GetKeyUp(KeyCode.F4))
             {
                 cheater.ToggleUI();
                 cheater.ToggleCursor();
             }
-            // N = Nuke
-            else if (Input.GetKeyUp(KeyCode.N))
-            {
-                cheater.ShowNuke();
-            }
-            // P = Pause
-            else if (Input.GetKeyUp(KeyCode.P))
-            {
-                cheater.TogglePause();
-            }
-            // 1 = Normal build speed
-            else if (Input.GetKeyUp(KeyCode.Alpha1))
-            {
-                cheater.SetSpeedNormal();
-            }
-            // 2 = Fast build speed (50)
-            else if (Input.GetKeyUp(KeyCode.Alpha2))
-            {
-                cheater.SetSpeedFast();
-            }
-            // 3 = Very fast build speed (500)
-            else if (Input.GetKeyUp(KeyCode.Alpha3))
-            {
-                cheater.SetSpeedVeryFast();
-            }
+
+            //// W = Win
+            //if (Input.GetKeyUp(KeyCode.W))
+            //{
+            //    cheater.Win();
+            //}
+            //// L = Loss
+            //else if (Input.GetKeyUp(KeyCode.L))
+            //{
+            //    cheater.Lose();
+            //}
+            //// B = Builders
+            //else if (Input.GetKeyUp(KeyCode.B))
+            //{
+            //    cheater.AddBuilders();
+            //}
+            //// T = Toggle UI
+            //else if (Input.GetKeyUp(KeyCode.T))
+            //{
+            //    cheater.ToggleUI();
+            //    cheater.ToggleCursor();
+            //}
+            //// N = Nuke
+            //else if (Input.GetKeyUp(KeyCode.N))
+            //{
+            //    cheater.ShowNuke();
+            //}
+            //// P = Pause
+            //else if (Input.GetKeyUp(KeyCode.P))
+            //{
+            //    cheater.TogglePause();
+            //}
+            //// 1 = Normal build speed
+            //else if (Input.GetKeyUp(KeyCode.Alpha1))
+            //{
+            //    cheater.SetSpeedNormal();
+            //}
+            //// 2 = Fast build speed (50)
+            //else if (Input.GetKeyUp(KeyCode.Alpha2))
+            //{
+            //    cheater.SetSpeedFast();
+            //}
+            //// 3 = Very fast build speed (500)
+            //else if (Input.GetKeyUp(KeyCode.Alpha3))
+            //{
+            //    cheater.SetSpeedVeryFast();
+            //}
         }
     }
 }
