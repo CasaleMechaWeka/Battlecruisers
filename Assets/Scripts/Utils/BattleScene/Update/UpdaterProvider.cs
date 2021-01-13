d1e129b56bf6fab8d68af2e3f8f91b25cb94ba6f
Felix Mann <felix.fei.mann@gmail.com>
2021-01-13 06:43:16 -0800

    Make barrel controllers update every physics frame.  Makes AA more accurate vs bombers, especially on fast forward :)  Will reduce performance :/

--- Assets/Scripts/Utils/BattleScene/Update/UpdaterProvider.cs
+++ Assets/Scripts/Utils/BattleScene/Update/UpdaterProvider.cs
@@ -13,6 +13,7 @@ namespace BattleCruisers.Utils.BattleScene.Update
         private const float SLOWER_UPDATER_INTERVAL_IN_S = 0.2f;
         public IUpdater SlowerUpdater { get; private set; }
 
+        // FELIX  Remove :D
         private const float BARREL_CONTROLLER_UPDATER_INTERVAL_IN_S = 0.1f;
         public IUpdater BarrelControllerUpdater { get; private set; }
 
@@ -28,7 +29,9 @@ namespace BattleCruisers.Utils.BattleScene.Update
             Assert.IsNotNull(SwitchableUpdater);
 
             SlowerUpdater = new MultiFrameUpdater(PhysicsUpdater, TimeBC.Instance, SLOWER_UPDATER_INTERVAL_IN_S);
-            BarrelControllerUpdater = new MultiFrameUpdater(PhysicsUpdater, TimeBC.Instance, BARREL_CONTROLLER_UPDATER_INTERVAL_IN_S);
+            // FELIX  TEMP
+            BarrelControllerUpdater = PhysicsUpdater;
+            //BarrelControllerUpdater = new MultiFrameUpdater(PhysicsUpdater, TimeBC.Instance, BARREL_CONTROLLER_UPDATER_INTERVAL_IN_S);
         }
     }
 }
