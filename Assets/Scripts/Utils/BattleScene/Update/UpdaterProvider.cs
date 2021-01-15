38afe18b9a84a11383f9ff5202d0bf4394a241ad
Felix Mann <felix.fei.mann@gmail.com>
2021-01-15 07:28:53 -0800

    Remove old TODOs

--- Assets/Scripts/Utils/BattleScene/Update/UpdaterProvider.cs
+++ Assets/Scripts/Utils/BattleScene/Update/UpdaterProvider.cs
@@ -13,8 +13,6 @@ namespace BattleCruisers.Utils.BattleScene.Update
         private const float SLOWER_UPDATER_INTERVAL_IN_S = 0.2f;
         public IUpdater SlowerUpdater { get; private set; }
 
-        // FELIX  Remove :D
-        private const float BARREL_CONTROLLER_UPDATER_INTERVAL_IN_S = 0.1f;
         public IUpdater BarrelControllerUpdater { get; private set; }
 
         public void Initialise()
@@ -29,9 +27,7 @@ namespace BattleCruisers.Utils.BattleScene.Update
             Assert.IsNotNull(SwitchableUpdater);
 
             SlowerUpdater = new MultiFrameUpdater(PhysicsUpdater, TimeBC.Instance, SLOWER_UPDATER_INTERVAL_IN_S);
-            // FELIX  TEMP
             BarrelControllerUpdater = PhysicsUpdater;
-            //BarrelControllerUpdater = new MultiFrameUpdater(PhysicsUpdater, TimeBC.Instance, BARREL_CONTROLLER_UPDATER_INTERVAL_IN_S);
         }
     }
 }
