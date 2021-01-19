07205055c4b49a5a99f1f53f5d783574489fc019
Felix Mann <felix.fei.mann@gmail.com>
2021-01-19 06:48:00 -0800

    Create PrefabContainer  :)

---
+++ Assets/Scripts/Utils/Fetchers/IPrefabContainer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace BattleCruisers.Utils.Fetchers
+{
+    public interface IPrefabContainer<TPrefab> where TPrefab : class
+    {
+        AsyncOperationHandle<TPrefab> Handle { get; }
+        TPrefab Prefab { get; }
+    }
+}
---
+++ Assets/Scripts/Utils/Fetchers/PrefabContainer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace BattleCruisers.Utils.Fetchers
+{
+    public class PrefabContainer<TPrefab> : IPrefabContainer<TPrefab> where TPrefab : class
+    {
+        public AsyncOperationHandle<TPrefab> Handle { get; }
+        public TPrefab Prefab { get; }
+
+        public PrefabContainer(AsyncOperationHandle<TPrefab> handle, TPrefab prefab)
+        {
+            Helper.AssertIsNotNull(handle, prefab);
+
+            Handle = handle;
+            Prefab = prefab;
+        }
+    }
+}
