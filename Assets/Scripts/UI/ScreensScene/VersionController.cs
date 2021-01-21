e04a154732d78115aafe289bf0597c72956b65cd
Felix Mann <felix.fei.mann@gmail.com>
2021-01-21 07:35:31 -0800

    Show app version on home screen (also if is demo) :)

---
+++ Assets/Scripts/UI/ScreensScene/VersionController.cs
@@ -0,0 +1,26 @@
+﻿using BattleCruisers.Data;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.ScreensScene
+{
+    public class VersionController : MonoBehaviour
+    {
+        public Text versionText;
+
+        void Start()
+        {
+            Assert.IsNotNull(versionText);
+
+            string value = "v" + Application.version;
+
+            if (ApplicationModelProvider.ApplicationModel.DataProvider.StaticData.IsDemo)
+            {
+                value += " - DEMO";
+            }
+
+            versionText.text = value;
+        }
+    }
+}
