a7a0b947964c363ed65752268dd7aa334a44952e
Felix Mann <felix.fei.mann@gmail.com>
2021-01-24 09:25:23 -0800

    Prevent trash talk screen music finishing halfway through loading scene :P

--- Assets/Scripts/Scenes/LandingSceneGod.cs
+++ Assets/Scripts/Scenes/LandingSceneGod.cs
@@ -81,6 +81,7 @@ namespace BattleCruisers.Scenes
                 hint = _hintProvider.GetHint();
             }
             LoadingScreenHint = hint;
+            MusicPlayer.Stop();
 
             StartCoroutine(LoadSceneWithLoadingScreen(sceneName));
         }
