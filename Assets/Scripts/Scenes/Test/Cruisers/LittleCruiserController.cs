a2fa8a1b204063eef1664ea05f4129cd96764de9
Felix Mann <felix.fei.mann@gmail.com>
2021-01-29 17:40:51 -0800

    Create test scene highlighting that a composite collider is unclickable :D

---
+++ Assets/Scripts/Scenes/Test/Cruisers/LittleCruiserController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+namespace BattleCruisers.Scenes.Test.Cruisers
+{
+    public class LittleCruiserController : MonoBehaviour, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log($"Clicked {eventData.position}");
+        }
+    }
+}
