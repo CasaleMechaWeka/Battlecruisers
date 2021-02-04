e92b64968bae3500fd42b23b3630eae8e5a08123
Felix Mann <felix.fei.mann@gmail.com>
2021-02-04 07:23:34 -0800

    Create test scene highlighting bomber build progress problem

---
+++ Assets/Scripts/Scenes/Test/Factories/SingleFactoryTestGod.cs
@@ -0,0 +1,50 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Buildings.Factories;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Scenes.Test.Utilities;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using BCUtils = BattleCruisers.Utils;
+
+namespace BattleCruisers.Scenes.Test.Factories
+{
+    public class SingleFactoryTestGod : TestGodBase
+	{
+		public Factory factory;
+		public UnitWrapper unitPrefab;
+
+        protected override List<GameObject> GetGameObjects()
+        {
+            BCUtils.Helper.AssertIsNotNull(factory, unitPrefab);
+
+            return new List<GameObject>()
+            {
+                factory.GameObject
+            };
+        }
+
+        protected override void Setup(Helper helper)
+        {
+			unitPrefab.StaticInitialise();
+
+            ICruiser leftCruiser = helper.CreateCruiser(Direction.Right, Faction.Blues);
+
+            helper.InitialiseBuilding(
+                factory, 
+                parentCruiserDirection: leftCruiser.Direction, 
+                parentCruiser: leftCruiser);
+
+			factory.CompletedBuildable += Factory_CompletedBuildable;
+			factory.StartConstruction();
+
+            Helper.SetupFactoryForUnitMonitor(factory, leftCruiser);
+		}
+
+		private void Factory_CompletedBuildable(object sender, EventArgs e)
+		{
+            ((Factory)sender).StartBuildingUnit(unitPrefab);
+		}
+	}
+}
