cc139024a8a58273f2cfaa80b208b05b59339bb8
Felix Mann <felix.fei.mann@gmail.com>
2021-02-05 16:13:02 -0800

    Show legal text on first time loading screen

--- Assets/Scripts/UI/Loading/LoadingScreenController.cs
+++ Assets/Scripts/UI/Loading/LoadingScreenController.cs
@@ -11,15 +11,31 @@ namespace BattleCruisers.UI.Loading
         public Text loadingText;
 
         private const string DEFAULT_LOADING_TEXT = "Loading";
+        private const string LEGAL_TEXT = "Any unauthorized exhibition, distribution, or copying of this video game or any part thereof (including soundtrack) may result in civil liability and criminal prosecution. The story, names, characters, and incidents portrayed in this production are fictitious. No identification with actual persons (living or deceased), places, buildings, and products is intended or should be inferred.";
 
+        private static bool IsFirstTime = true;
         public static LoadingScreenController Instance { get; private set; }
 
         void Start()
         {
             Helper.AssertIsNotNull(root, loadingText);
 
-            loadingText.text = LandingSceneGod.LoadingScreenHint ?? DEFAULT_LOADING_TEXT;
+            loadingText.text = FindLoadingText();
             Instance = this;
+
+            IsFirstTime = false;
+        }
+
+        private string FindLoadingText()
+        {
+            if (IsFirstTime)
+            {
+                return LEGAL_TEXT;
+            }
+            else
+            {
+                return LandingSceneGod.LoadingScreenHint ?? DEFAULT_LOADING_TEXT;
+            }
         }
 
         public void Destroy()
