db9aaaa368f47f5efcd3f6402aeed6e6f86d4fe8
Felix Mann <felix.fei.mann@gmail.com>
2021-02-17 06:50:47 -0800

    Create Skirmish :)

---
+++ Assets/Scripts/Data/Skirmishes/ISkirmish.cs
@@ -0,0 +1,13 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Data.Settings;
+using BattleCruisers.Data.Static.Strategies;
+
+namespace BattleCruisers.Data.Skirmishes
+{
+    public interface ISkirmish
+    {
+        IPrefabKey AICruiser { get; }
+        StrategyType AIStrategy { get; }
+        Difficulty Difficulty { get; }
+    }
+}
---
+++ Assets/Scripts/Data/Skirmishes/Skirmish.cs
@@ -0,0 +1,23 @@
+﻿using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.Data.Settings;
+using BattleCruisers.Data.Static.Strategies;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Data.Skirmishes
+{
+    public class Skirmish : ISkirmish
+    {
+        public Difficulty Difficulty { get; }
+        public IPrefabKey AICruiser { get; }
+        public StrategyType AIStrategy { get; }
+
+        public Skirmish(Difficulty difficulty, IPrefabKey aICruiser, StrategyType aIStrategy)
+        {
+            Assert.IsNotNull(AICruiser);
+
+            Difficulty = difficulty;
+            AICruiser = aICruiser;
+            AIStrategy = aIStrategy;
+        }
+    }
+}
