884b5caba255355cad598d4739574367a9914fe9
Felix Mann <felix.fei.mann@gmail.com>
2021-02-17 19:19:58 -0800

    Add TODO`

--- Assets/Scripts/Data/Static/Strategies/IStrategyProvider.cs
+++ Assets/Scripts/Data/Static/Strategies/IStrategyProvider.cs
@@ -3,6 +3,7 @@
 // FELIX  Fix namespcae :P
 namespace Assets.Scripts.Data.Static.Strategies
 {
+    // FELIX  Rename to factory?  (And implementations...)
     public interface IStrategyProvider
     {
         IStrategy GetAdaptiveStrategy();
