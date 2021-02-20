63b9b03fcbf9fd3f5299559c316d37b43d357e9d
Felix Mann <felix.fei.mann@gmail.com>
2021-02-19 16:52:49 -0800

    Make SkirmishModel serialisable :)

--- Assets/Scripts/Data/Models/SkirmishModel.cs
+++ Assets/Scripts/Data/Models/SkirmishModel.cs
@@ -1,24 +1,51 @@
 ﻿using BattleCruisers.Data.Models.PrefabKeys;
 using BattleCruisers.Data.Settings;
 using BattleCruisers.Data.Static.Strategies.Helper;
+using BattleCruisers.Utils;
+using System;
+using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace BattleCruisers.Data.Models
 {
+    // FELIX  Add to GameModel :)
     // FELIX  Preserve sky & background for retry => Have here?
+    [Serializable]
     public class SkirmishModel : ISkirmishModel
     {
-        public Difficulty Difficulty { get; }
-        public HullKey AICruiser { get; }
-        public StrategyType AIStrategy { get; }
+        [SerializeField]
+        private Difficulty _difficulty;
+        public Difficulty Difficulty => _difficulty;
+
+        [SerializeField]
+        private HullKey _aiCruiser;
+        public HullKey AICruiser => _aiCruiser;
+
+        [SerializeField]
+        private StrategyType _aiStrategy;
+        public StrategyType AIStrategy => _aiStrategy;
 
         public SkirmishModel(Difficulty difficulty, HullKey aiCruiser, StrategyType aIStrategy)
         {
             Assert.IsNotNull(aiCruiser);
 
-            Difficulty = difficulty;
-            AICruiser = aiCruiser;
-            AIStrategy = aIStrategy;
+            _difficulty = difficulty;
+            _aiCruiser = aiCruiser;
+            _aiStrategy = aIStrategy;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return
+                obj is SkirmishModel other
+                && Difficulty == other.Difficulty
+                && AICruiser.SmartEquals(other.AICruiser)
+                && AIStrategy == other.AIStrategy;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GetHashCode(Difficulty, AICruiser, AIStrategy);
         }
 
         public override string ToString()
