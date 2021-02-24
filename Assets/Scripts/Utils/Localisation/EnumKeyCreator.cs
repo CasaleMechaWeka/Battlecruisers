219a7c90cecf1e77195d3ef6b7cd56de023ab3a7
Felix Mann <felix.fei.mann@gmail.com>
2021-02-23 16:27:12 -0800

    Create EnumKeyCreator :)

---
+++ Assets/Scripts/Utils/Localisation/EnumKeyCreator.cs
@@ -0,0 +1,15 @@
+﻿namespace BattleCruisers.Utils.Localisation
+{
+    public class EnumKeyCreator
+    {
+        public const string ENUM_KEY_PREFIX = "Enums";
+
+        public static string CreateKey<TEnum>(TEnum enumValue)
+        {
+            string enumType = typeof(TEnum).Name;
+            string enumValueString = enumValue.ToString();
+
+            return $"{ENUM_KEY_PREFIX}/{enumType}/{enumValueString}";
+        }
+    }
+}
