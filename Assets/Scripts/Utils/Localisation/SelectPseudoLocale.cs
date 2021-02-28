86a9b3444722f859a94a06b77f26b47b948c467c
Felix Mann <felix.fei.mann@gmail.com>
2021-02-28 09:29:04 -0800

    Use pseudo locale in BattleScene :)

---
+++ Assets/Scripts/Utils/Localisation/SelectPseudoLocale.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+namespace Assets.Scripts.Utils.Localisation
+{
+    public class SelectPseudoLocale : MonoBehaviour
+    {
+        public Locale pseudoLocale;
+
+        private void Awake()
+        {
+            Assert.IsNotNull(pseudoLocale);
+            LocalizationSettings.SelectedLocale = pseudoLocale;
+        }
+    }
+}
