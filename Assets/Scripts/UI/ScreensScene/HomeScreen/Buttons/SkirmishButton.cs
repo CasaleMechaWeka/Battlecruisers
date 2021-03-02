2f686cc8781e131fecac6c8ac909f56be9280669
Felix Mann <felix.fei.mann@gmail.com>
2021-03-01 18:14:17 -0800

    Always enable skirmish (don't wait to clock game), also don't disable for Steam builds

--- Assets/Scripts/UI/ScreensScene/HomeScreen/Buttons/SkirmishButton.cs
+++ Assets/Scripts/UI/ScreensScene/HomeScreen/Buttons/SkirmishButton.cs
@@ -1,25 +1,7 @@
-﻿using BattleCruisers.Data;
-using BattleCruisers.Data.Models;
-using BattleCruisers.Data.Static;
-using BattleCruisers.UI.Sound.Players;
-
-namespace BattleCruisers.UI.ScreensScene.HomeScreen.Buttons
+﻿namespace BattleCruisers.UI.ScreensScene.HomeScreen.Buttons
 {
     public class SkirmishButton : HomeScreenButton
     {
-        public override void Initialise(ISingleSoundPlayer soundPlayer, IHomeScreen homeScreen, IGameModel gameModel)
-        {
-            base.Initialise(soundPlayer, homeScreen, gameModel);
-
-            // Only enable skirmish screen once user has completed campaign
-            if (gameModel.NumOfLevelsCompleted != StaticData.NUM_OF_LEVELS
-                // TEMP  Disable skirmish on release builds, until big update announcement
-                || !ApplicationModelProvider.ApplicationModel.DataProvider.StaticData.HasAsserts)
-            {
-                DestroySelf();
-            }
-        }
-
         protected override void OnClicked()
         {
             base.OnClicked();
