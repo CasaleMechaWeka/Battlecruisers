0ce90e94c69682e0b505ed40ffbe9e3b75ee377a
Felix Mann <felix.fei.mann@gmail.com>
2021-03-04 07:52:22 -0800

    Create AllCaps stub

---
+++ Assets/Scripts/Utils/Localisation/AllCaps.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Localization.Components;
+using UnityEngine.UI;
+
+namespace BattleCruisers.Utils.Localisation
+{
+    public class AllCaps : MonoBehaviour
+    {
+        private Text _text;
+        private LocalizeStringEvent _localizeStringEvent;
+
+        void Start()
+        {
+            _text = GetComponent<Text>();
+            Assert.IsNotNull(_text);
+
+            _localizeStringEvent = GetComponent<LocalizeStringEvent>();
+            Assert.IsNotNull(_localizeStringEvent);
+        }
+    }
+}
