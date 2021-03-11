b2e892a14635101a0bf65b16f6d71544aa298baa
Felix Mann <felix.fei.mann@gmail.com>
2021-03-11 06:53:20 -0800

    Create IonCannonTargetProcessorWrapper :)

---
+++ Assets/Scripts/Targets/TargetProcessors/IonCannonTargetProcessorWrapper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BattleCruisers.Targets.TargetProcessors
+{
+    public class IonCannonTargetProcessorWrapper : MonoBehaviour
+    {
+        public ITargetProcessor CreateTargetProcessor(ITargetProcessorArgs args)
+        {
+            return args.CruiserSpecificFactories.Targets.ProcessorFactory.IonCannonTargetProcessor;
+        }
+    }
+}
