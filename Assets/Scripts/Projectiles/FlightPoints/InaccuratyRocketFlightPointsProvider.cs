f3c1c1ffb0e66b746f60bef44eb52fa5ec4efaab
Felix Mann <felix.fei.mann@gmail.com>
2021-03-10 19:11:02 -0800

    Reduce duplicate code :)

--- Assets/Scripts/Projectiles/FlightPoints/InaccuratyRocketFlightPointsProvider.cs
+++ Assets/Scripts/Projectiles/FlightPoints/InaccuratyRocketFlightPointsProvider.cs
@@ -1,22 +1,16 @@
 ﻿using BattleCruisers.Utils;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace BattleCruisers.Projectiles.FlightPoints
 {
-	// FELIX  Test
-	// FELIX  Avoid duplicat code with RocketFlightPointsProvider
-	/// <summary>
-	/// Similar to RocketFlightPointsProvider, but introduces inaccuracy.
-	/// </summary>
-	public class InaccuratyRocketFlightPointsProvider : IFlightPointsProvider
+    /// <summary>
+    /// Similar to RocketFlightPointsProvider, but introduces inaccuracy.
+    /// </summary>
+    public class InaccuratyRocketFlightPointsProvider : RocketFlightPointsProvider
 	{
 		private readonly FlightPointStats _stats;
 		private readonly IRandomGenerator _random;
-
-		private const float ROCKET_CRUISING_POINTS_OFFSET_PROPORTION = 0.25f;
-		private const float ROCKET_MIN_HORIZONTAL_DISTANCE_IN_M = 10;
 
 		// Because may miss target on the x axis, need to aim lower to hit cruiser or water
 		// FELIX Test rocket hitting water :P
@@ -30,37 +24,19 @@ namespace BattleCruisers.Projectiles.FlightPoints
 			_random = RandomGenerator.Instance;
         }
 
-        public Queue<Vector2> FindFlightPoints(Vector2 sourcePosition, Vector2 targetPosition, float cruisingAltitudeInM)
-		{
-			Queue<Vector2> flightPoints = new Queue<Vector2>();
+        protected override Vector2 CreateAscendPoint(Vector2 sourcePosition, Vector2 targetPosition, float cruisingPointsXOffset, float cruisingAltitudeInM)
+        {
+            Vector2 idealPoint = base.CreateAscendPoint(sourcePosition, targetPosition, cruisingPointsXOffset, cruisingAltitudeInM);
+			return FuzzCruisingPoint(idealPoint, _stats.AscendPointRadiusVariationM);
+        }
 
-			float horizontalDistanceToTarget = Mathf.Abs(sourcePosition.x - targetPosition.x);
-			Assert.IsTrue(horizontalDistanceToTarget >= ROCKET_MIN_HORIZONTAL_DISTANCE_IN_M);
+        protected override Vector2 CreateDescendPoint(Vector2 sourcePosition, Vector2 targetPosition, float cruisingPointsXOffset, float cruisingAltitudeInM)
+        {
+            Vector2 idealPoint = base.CreateDescendPoint(sourcePosition, targetPosition, cruisingPointsXOffset, cruisingAltitudeInM);
+			return FuzzCruisingPoint(idealPoint, _stats.DescendPointRadiusVariationM);
+        }
 
-			float cruisingPointsXOffset = ROCKET_CRUISING_POINTS_OFFSET_PROPORTION * horizontalDistanceToTarget;
-
-			Vector2 ascendPoint, descendPoint;
-
-			if (sourcePosition.x < targetPosition.x)
-			{
-				ascendPoint = new Vector2(sourcePosition.x + cruisingPointsXOffset, cruisingAltitudeInM);
-				descendPoint = new Vector2(targetPosition.x - cruisingPointsXOffset, cruisingAltitudeInM);
-			}
-			else
-			{
-				ascendPoint = new Vector2(sourcePosition.x - cruisingPointsXOffset, cruisingAltitudeInM);
-				descendPoint = new Vector2(targetPosition.x + cruisingPointsXOffset, cruisingAltitudeInM);
-			}
-
-			flightPoints.Enqueue(FuzzCruisingPoint(ascendPoint, _stats.AscendPointRadiusVariationM));
-			flightPoints.Enqueue(FuzzCruisingPoint(descendPoint, _stats.DescendPointRadiusVariationM));
-			flightPoints.Enqueue(FuzzTargetPoint(targetPosition, _stats.TargetPointXRadiusVariationM));
-
-			return flightPoints;
-		}
-		
-		// FELIX Abstract :)
-		private Vector2 FuzzCruisingPoint(Vector2 idealPoint, float fuzzRadiusM)
+        private Vector2 FuzzCruisingPoint(Vector2 idealPoint, float fuzzRadiusM)
         {
 			return
 				new Vector2(
@@ -68,7 +44,13 @@ namespace BattleCruisers.Projectiles.FlightPoints
 					_random.RangeFromCenter(idealPoint.y, fuzzRadiusM));
         }
 
-		private Vector2 FuzzTargetPoint(Vector2 targetPoint, float xFuzzRadiusM)
+        protected override Vector2 CreateTargetPoint(Vector2 targetPosition)
+        {
+            Vector2 idealPoint = base.CreateTargetPoint(targetPosition);
+			return FuzzTargetPoint(idealPoint, _stats.TargetPointXRadiusVariationM);
+        }
+
+        private Vector2 FuzzTargetPoint(Vector2 targetPoint, float xFuzzRadiusM)
         {
 			return
 				new Vector2(
--- Assets/Scripts/Projectiles/FlightPoints/RocketFlightPointsProvider.cs
+++ Assets/Scripts/Projectiles/FlightPoints/RocketFlightPointsProvider.cs
@@ -16,27 +16,46 @@ namespace BattleCruisers.Projectiles.FlightPoints
 		/// </summary>
 		public Queue<Vector2> FindFlightPoints(Vector2 sourcePosition, Vector2 targetPosition, float cruisingAltitudeInM)
 		{
-			Queue<Vector2> flightPoints = new Queue<Vector2>();
-
 			float horizontalDistanceToTarget = Mathf.Abs(sourcePosition.x - targetPosition.x);
 			Assert.IsTrue(horizontalDistanceToTarget >= ROCKET_MIN_HORIZONTAL_DISTANCE_IN_M);
-
 			float cruisingPointsXOffset = ROCKET_CRUISING_POINTS_OFFSET_PROPORTION * horizontalDistanceToTarget;
 
-			if (sourcePosition.x < targetPosition.x)
-			{
-				flightPoints.Enqueue(new Vector2(sourcePosition.x + cruisingPointsXOffset, cruisingAltitudeInM));
-				flightPoints.Enqueue(new Vector2(targetPosition.x - cruisingPointsXOffset, cruisingAltitudeInM));
-			}
-			else
-			{
-				flightPoints.Enqueue(new Vector2(sourcePosition.x - cruisingPointsXOffset, cruisingAltitudeInM));
-				flightPoints.Enqueue(new Vector2(targetPosition.x + cruisingPointsXOffset, cruisingAltitudeInM));
-			}
+			Queue<Vector2> flightPoints = new Queue<Vector2>();
 
-			flightPoints.Enqueue(targetPosition);
+			flightPoints.Enqueue(CreateAscendPoint(sourcePosition, targetPosition, cruisingPointsXOffset, cruisingAltitudeInM));
+			flightPoints.Enqueue(CreateDescendPoint(sourcePosition, targetPosition, cruisingPointsXOffset, cruisingAltitudeInM));
+			flightPoints.Enqueue(CreateTargetPoint(targetPosition));
 
 			return flightPoints;
 		}
+
+        protected virtual Vector2 CreateAscendPoint(Vector2 sourcePosition, Vector2 targetPosition, float cruisingPointsXOffset, float cruisingAltitudeInM)
+        {
+			if (sourcePosition.x < targetPosition.x)
+            {
+				return new Vector2(sourcePosition.x + cruisingPointsXOffset, cruisingAltitudeInM);
+            }
+			else
+            {
+				return new Vector2(sourcePosition.x - cruisingPointsXOffset, cruisingAltitudeInM);
+			}
+        }
+
+		protected virtual Vector2 CreateDescendPoint(Vector2 sourcePosition, Vector2 targetPosition, float cruisingPointsXOffset, float cruisingAltitudeInM)
+		{
+			if (sourcePosition.x < targetPosition.x)
+			{
+				return new Vector2(targetPosition.x - cruisingPointsXOffset, cruisingAltitudeInM);
+			}
+			else
+			{
+				return new Vector2(targetPosition.x + cruisingPointsXOffset, cruisingAltitudeInM);
+			}
+		}
+
+		protected virtual Vector2 CreateTargetPoint(Vector2 targetPosition)
+        {
+			return targetPosition;
+        }
 	}
 }
