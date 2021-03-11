c833c2ff561e8d37ef134534652527552277c125
Felix Mann <felix.fei.mann@gmail.com>
2021-03-10 19:31:43 -0800

    Create StaticTargetProcessor for IonCannon :D

---
+++ Assets/Scripts/Targets/TargetProcessors/StaticTargetProcessor.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.Buildables;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Targets.TargetProcessors
+{
+    public class StaticTargetProcessor : ITargetProcessor
+    {
+        private readonly ITarget _target;
+
+        public StaticTargetProcessor(ITarget target)
+        {
+            Assert.IsNotNull(target);
+            _target = target;
+        }
+
+        public void AddTargetConsumer(ITargetConsumer targetConsumer)
+        {
+            targetConsumer.Target = _target;
+        }
+
+        public void RemoveTargetConsumer(ITargetConsumer targetConsumer) { }
+        public void DisposeManagedState() { }
+    }
+}
