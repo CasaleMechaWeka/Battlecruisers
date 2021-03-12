c36ba095748796c80de359390569774c152bad8f
Felix Mann <felix.fei.mann@gmail.com>
2021-03-12 12:58:12 -0800

    Finish SmartMissileSpawner? :D

--- Assets/Scripts/Projectiles/Spawners/SmartMissileSpawner.cs
+++ Assets/Scripts/Projectiles/Spawners/SmartMissileSpawner.cs
@@ -1,23 +1,36 @@
-﻿using BattleCruisers.Buildables;
-using BattleCruisers.Projectiles.ActivationArgs;
+﻿using BattleCruisers.Projectiles.ActivationArgs;
 using BattleCruisers.Projectiles.Stats;
 using BattleCruisers.Targets.TargetFinders.Filters;
+using BattleCruisers.UI.Sound;
 using BattleCruisers.Utils;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace BattleCruisers.Projectiles.Spawners
 {
     public class SmartMissileSpawner : ProjectileSpawner<SmartMissileController, SmartMissileActivationArgs<ISmartProjectileStats>, ISmartProjectileStats>
 	{
-		public void SpawnMissile(float angleInDegrees, bool isSourceMirrored, ITarget target, ITargetFilter targetFilter)
+        private ISmartProjectileStats _smartProjectileStats;
+
+        public async Task InitialiseAsync(
+            IProjectileSpawnerArgs args,
+            ISoundKey firingSound,
+            ISmartProjectileStats smartProjectileStats)
+        {
+            Assert.IsNotNull(smartProjectileStats);
+            _smartProjectileStats = smartProjectileStats;
+
+            await base.InitialiseAsync(args, firingSound);
+        }
+
+        public void SpawnMissile(float angleInDegrees, bool isSourceMirrored, ITargetFilter targetFilter)
 		{
             Vector2 missileVelocity = FindProjectileVelocity(angleInDegrees, isSourceMirrored, _projectileStats.InitialVelocityInMPerS);
             SmartMissileActivationArgs<ISmartProjectileStats> activationArgs
                 = new SmartMissileActivationArgs<ISmartProjectileStats>(
                     transform.position,
-                    // FELIX 
-                    null,
-                    //_projectileStats,
+                    _smartProjectileStats,
                     missileVelocity,
                     targetFilter,
                     _parent,
