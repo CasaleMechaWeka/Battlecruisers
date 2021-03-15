c90727610081a73703b8b70f68783ed225ddc5f3
Felix Mann <felix.fei.mann@gmail.com>
2021-03-15 06:49:59 -0700

    Create MissileLauncher :D

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/MissileLauncher.cs
@@ -0,0 +1,24 @@
+﻿using BattleCruisers.Data.Static;
+using BattleCruisers.UI.Sound;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets
+{
+    public class MissileLauncher : DefenseTurret
+	{
+        // DLC  Have own sound
+        protected override PrioritisedSoundKey ConstructionCompletedSoundKey => PrioritisedSoundKeys.Completed.Buildings.TeslaCoil;
+
+        // FELIX  Avoid duplicate code with TeslaCoil
+        protected override SpriteRenderer[] GetBaseRenderers()
+        {
+            SpriteRenderer mainRenderer = GetComponent<SpriteRenderer>();
+            Assert.IsNotNull(mainRenderer);
+            return new SpriteRenderer[]
+            {
+                mainRenderer
+            };
+        }
+	}
+}
--- Assets/Scripts/Scenes/Test/Turrets/MissileLauncherTestGod.cs
+++ Assets/Scripts/Scenes/Test/Turrets/MissileLauncherTestGod.cs
@@ -1,22 +1,27 @@
 ﻿using BattleCruisers.Buildables;
 using BattleCruisers.Buildables.Buildings.Factories;
-using BattleCruisers.Buildables.Buildings.Turrets;
 using BattleCruisers.Buildables.Units;
 using BattleCruisers.Buildables.Units.Ships;
 using BattleCruisers.Cruisers;
+using BattleCruisers.Data.Static;
+using BattleCruisers.Projectiles.Spawners;
 using BattleCruisers.Projectiles.Stats;
 using BattleCruisers.Scenes.Test.Utilities;
 using BattleCruisers.Targets.TargetFinders.Filters;
+using NSubstitute;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using BCUtils = BattleCruisers.Utils;
 
-namespace BattleCruisers.Scenes.Test
+namespace BattleCruisers.Scenes.Test.Turrets
 {
-    public class SmartMissileSpawnerTestGod : TestGodBase
+	// FELIX  Avoid duplicate code with SmartMissileSpawnerTestGod
+    public class MissileLauncherTestGod : TestGodBase
 	{
-		public MissileLauncher missileLauncher;
+		private ITargetFilter _targetFilter;
+
+		public SmartMissileSpawner missileSpawner;
 		public SmartProjectileStats projectileStats;
         public TestTarget enemyCruiserTarget;
         public ShipController enemyShip;
@@ -26,7 +31,7 @@ namespace BattleCruisers.Scenes.Test
 
         protected override List<GameObject> GetGameObjects()
         {
-			BCUtils.Helper.AssertIsNotNull(enemyAircraft, enemyShip, enemyFactory, enemyCruiserTarget, missileLauncher, projectileStats);
+			BCUtils.Helper.AssertIsNotNull(enemyAircraft, enemyShip, enemyFactory, enemyCruiserTarget, missileSpawner, projectileStats);
 
             return new List<GameObject>()
             {
@@ -40,10 +45,18 @@ namespace BattleCruisers.Scenes.Test
         protected override async Task SetupAsync(Helper helper)
         {
 			ICruiser redCruiser = helper.CreateCruiser(Direction.Left, Faction.Reds);
+			
+			// Setup missile spawner
+			_targetFilter = new FactionAndTargetTypeFilter(Faction.Reds, projectileStats.AttackCapabilities);
 
-			// Setup missile launcher
-			helper.InitialiseBuilding(missileLauncher, Faction.Blues, enemyCruiser: redCruiser);
-			missileLauncher.StartConstruction();
+			ITarget parent = Substitute.For<ITarget>();
+            int burstSize = 1;
+			BuildableInitialisationArgs args = helper.CreateBuildableInitialisationArgs(enemyCruiser: redCruiser);
+            IProjectileSpawnerArgs spawnerArgs = new ProjectileSpawnerArgs(parent, projectileStats, burstSize, args.FactoryProvider, args.CruiserSpecificFactories, args.EnemyCruiser);
+
+            await missileSpawner.InitialiseAsync(spawnerArgs, SoundKeys.Firing.Missile, projectileStats);
+
+			InvokeRepeating("FireMissile", time: 0.5f, repeatRate: 2);
 
 			// Setup enemies
 			enemyAircraft.PatrolPoints = aircraftPatrolPoints;
@@ -60,5 +73,10 @@ namespace BattleCruisers.Scenes.Test
 
 			enemyCruiserTarget.Initialise(helper.CommonStrings, Faction.Reds);
         }
+
+		private void FireMissile()
+		{
+			missileSpawner.SpawnMissile(angleInDegrees: 90, isSourceMirrored: false, _targetFilter);
+		}
 	}
 }
