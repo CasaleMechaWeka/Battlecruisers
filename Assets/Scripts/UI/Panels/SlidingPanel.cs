708a3327ada7e96961a11c92618ffa9c117a2b71
Felix Mann <felix.fei.mann@gmail.com>
2021-03-24 07:48:43 -0700

    Handle scale change :D

--- Assets/Scripts/UI/Panels/SlidingPanel.cs
+++ Assets/Scripts/UI/Panels/SlidingPanel.cs
@@ -12,12 +12,13 @@ namespace BattleCruisers.UI.Panels
 
     public class SlidingPanel : Panel, ISlidingPanel
     {
-		private Vector2 _slideVelocity;
+		private Vector2 _slidePositionVelocity, _sliderScaleVelocity;
         private bool _isInitialised = false;
-        private Vector2 _hiddenPosition, _shownPosition;
+        private Vector2 _hiddenPosition, _shownPosition, _hiddenScale;
+        private bool _positionDone, _scaleDone;
 
         private float _smoothTimeinS;
-        private Vector2 _targetPosition;
+        private Vector2 _targetPosition, _targetScale;
         private PanelState _targetState;
         private PanelState TargetState
         {
@@ -28,16 +29,20 @@ namespace BattleCruisers.UI.Panels
 
                 _targetState = value;
                 _state.Value = PanelState.Sliding;
+                _positionDone = false;
+                _scaleDone = false;
 
                 if (_targetState == PanelState.Shown)
                 {
                     _smoothTimeinS = showSmoothTimeInS;
                     _targetPosition = _shownPosition;
+                    _targetScale = shownScale;
                 }
                 else
                 {
                     _smoothTimeinS = hideSmoothTimeInS;
                     _targetPosition = _hiddenPosition;
+                    _targetScale = _hiddenScale;
                 }
             }
         }
@@ -50,11 +55,17 @@ namespace BattleCruisers.UI.Panels
         public float hideSmoothTimeInS = 0.2f;
         public float positionEqualityMarginInPixels = 2;
 
+        public bool changeScale = false;
+        public Vector2 shownScale = new Vector2(1, 1);
+        public float scaleEqualityMargin = 0.005f;
+
         public void Initialise()
         {
             _hiddenPosition = transform.position;
             float yDelta = transform.lossyScale.y * shownPositionYDelta;
             _shownPosition = new Vector2(transform.position.x, transform.position.y + yDelta);
+
+            _hiddenScale = transform.localScale;
 
             _state = new SettableBroadcastingProperty<PanelState>(initialValue: PanelState.Sliding);
             State = new BroadcastingProperty<PanelState>(_state);
@@ -71,20 +82,53 @@ namespace BattleCruisers.UI.Panels
                 return;
             }
 
-            if (Vector2.Distance(transform.position, _targetPosition) <= positionEqualityMarginInPixels)
+            if (_positionDone && _scaleDone)
             {
                 _state.Value = TargetState;
                 return;
+            }
+
+            _positionDone = AdjustPosition();
+            _scaleDone = AdjustScale();
+        }
+
+        private bool AdjustPosition()
+        {
+            if (_positionDone
+                || Vector2.Distance(transform.position, _targetPosition) <= positionEqualityMarginInPixels)
+            {
+                return true;
             }
 
             transform.position
                 = Vector2.SmoothDamp(
                     transform.position,
                     _targetPosition,
-                    ref _slideVelocity,
+                    ref _slidePositionVelocity,
                     _smoothTimeinS,
                     maxSpeed: float.MaxValue,
                     deltaTime: TimeBC.Instance.UnscaledDeltaTime);
+            return false;
+        }
+
+        private bool AdjustScale()
+        { 
+            if (!changeScale
+                || _scaleDone
+                || Vector2.Distance(transform.localScale, _targetScale) <= scaleEqualityMargin)
+            {
+                return true;
+            }
+
+            transform.localScale
+                = Vector2.SmoothDamp(
+                    transform.localScale,
+                    _targetScale,
+                    ref _sliderScaleVelocity,
+                    _smoothTimeinS,
+                    maxSpeed: float.MaxValue,
+                    deltaTime: TimeBC.Instance.UnscaledDeltaTime);
+            return false;
         }
 
         public override void Show()
