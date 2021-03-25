801132724894f522916728d37f447d83562eebb5
Felix Mann <felix.fei.mann@gmail.com>
2021-03-24 17:52:03 -0700

    Create ShowDetailsButtonController :)

---
+++ Assets/Scripts/UI/Common/BuildableDetails/Buttons/ShowDetailsButtonController.cs
@@ -0,0 +1,40 @@
+﻿using BattleCruisers.UI.Panels;
+using BattleCruisers.UI.Sound.Players;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BattleCruisers.UI.Common.BuildableDetails.Buttons
+{
+    // FELIX  Hide for tutorial?
+    public class ShowDetailsButtonController : CanvasGroupButton, IButton
+    {
+        private ISlidingPanel _informatorPanel;
+
+        public Image activeFeedback;
+
+        public void Initialise(
+            ISingleSoundPlayer soundPlayer,
+            ISlidingPanel informatorPanel)
+        {
+            base.Initialise(soundPlayer);
+
+            Assert.IsNotNull(activeFeedback);
+            Assert.IsNotNull(informatorPanel);
+            _informatorPanel = informatorPanel;
+        }
+
+        protected override void OnClicked()
+        {
+            base.OnClicked();
+
+            if (_informatorPanel.TargetState == PanelState.Shown)
+            {
+                _informatorPanel.Hide();
+            }
+            else
+            {
+                _informatorPanel.Show();
+            }
+        }
+    }
+}
