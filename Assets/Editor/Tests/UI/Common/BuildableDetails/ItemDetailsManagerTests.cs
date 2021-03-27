adeb30815c6fbf0c11c0af79811a2377e57d1b68
Felix Mann <felix.fei.mann@gmail.com>
2021-03-27 12:40:22 -0700

    Remove unused interfaces :)

--- Assets/Editor/Tests/UI/Common/BuildableDetails/ItemDetailsManagerTests.cs
+++ Assets/Editor/Tests/UI/Common/BuildableDetails/ItemDetailsManagerTests.cs
@@ -1,5 +1,4 @@
-﻿
-using BattleCruisers.Buildables.Buildings;
+﻿using BattleCruisers.Buildables.Buildings;
 using BattleCruisers.Buildables.Units;
 using BattleCruisers.Cruisers;
 using BattleCruisers.UI.Common.BuildableDetails;
@@ -12,9 +11,9 @@ namespace BattleCruisers.Tests.UI.Common.BuildableDetails
     {
         private IItemDetailsManager _detailsManager;
         private IInformatorPanel _informatorPanel;
-        private IBuildableDetails<IBuilding> _buildingDetails;
-        private IBuildableDetails<IUnit> _unitDetails;
-        private ICruiserDetails _cruiserDetails;
+        private IComparableItemDetails<IBuilding> _buildingDetails;
+        private IComparableItemDetails<IUnit> _unitDetails;
+        private IComparableItemDetails<ICruiser> _cruiserDetails;
         private IBuilding _building;
         private IUnit _unit;
         private ICruiser _cruiser;
@@ -22,9 +21,9 @@ namespace BattleCruisers.Tests.UI.Common.BuildableDetails
         [SetUp]
         public void SetuUp()
         {
-            _buildingDetails = Substitute.For<IBuildableDetails<IBuilding>>();
-            _unitDetails = Substitute.For<IBuildableDetails<IUnit>>();
-            _cruiserDetails = Substitute.For<ICruiserDetails>();
+            _buildingDetails = Substitute.For<IComparableItemDetails<IBuilding>>();
+            _unitDetails = Substitute.For<IComparableItemDetails<IUnit>>();
+            _cruiserDetails = Substitute.For<IComparableItemDetails<ICruiser>>();
 
             _informatorPanel = Substitute.For<IInformatorPanel>();
             _informatorPanel.BuildingDetails.Returns(_buildingDetails);
@@ -44,8 +43,8 @@ namespace BattleCruisers.Tests.UI.Common.BuildableDetails
             _detailsManager.ShowDetails(_building);
 
             ReceivedHideEverything();
-            _buildingDetails.Received().ShowBuildableDetails(_building);
-            _informatorPanel.Received().Show();
+            _buildingDetails.Received().ShowItemDetails(_building);
+            _informatorPanel.Received().Show(_building);
             Assert.AreSame(_building, _detailsManager.SelectedItem.Value);
         }
 
@@ -55,8 +54,8 @@ namespace BattleCruisers.Tests.UI.Common.BuildableDetails
             _detailsManager.ShowDetails(_unit);
 
             ReceivedHideEverything();
-            _unitDetails.Received().ShowBuildableDetails(_unit);
-            _informatorPanel.Received().Show();
+            _unitDetails.Received().ShowItemDetails(_unit);
+            _informatorPanel.Received().Show(_unit);
             Assert.AreSame(_unit, _detailsManager.SelectedItem.Value);
         }
 
@@ -66,8 +65,8 @@ namespace BattleCruisers.Tests.UI.Common.BuildableDetails
             _detailsManager.ShowDetails(_cruiser);
 
             ReceivedHideEverything();
-            _cruiserDetails.Received().ShowCruiserDetails(_cruiser);
-            _informatorPanel.Received().Show();
+            _cruiserDetails.Received().ShowItemDetails(_cruiser);
+            _informatorPanel.Received().Show(_cruiser);
             Assert.AreSame(_cruiser, _detailsManager.SelectedItem.Value);
         }
 
