b98edcdd45e47d807989e8af13fd9c00350d0b7d
Felix Mann <felix.fei.mann@gmail.com>
2021-04-07 18:16:13 -0700

    Tidy up item panels :)

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/Items/ItemButton.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/Items/ItemButton.cs
@@ -12,7 +12,6 @@ namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Items
     public abstract class ItemButton : ElementWithClickSound, IItemButton
     {
         private Image _itemImage;
-        private Text _itemName;
 
         protected IItemDetailsManager _itemDetailsManager;
         protected IComparingItemFamilyTracker _comparingFamiltyTracker;
@@ -28,7 +27,6 @@ namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Items
             set
             {
                 _itemImage.color = value;
-                _itemName.color = value;
             }
         }
 
@@ -43,9 +41,6 @@ namespace BattleCruisers.UI.ScreensScene.LoadoutScreen.Items
             _itemDetailsManager = itemDetailsManager;
             _comparingFamiltyTracker = comparingFamiltyTracker;
             _itemImage = transform.FindNamedComponent<Image>("ItemImage");
-
-            _itemName = GetComponentInChildren<Text>();
-            Assert.IsNotNull(_itemName);
 
             _canvasGroup = GetComponent<CanvasGroup>();
             Assert.IsNotNull(_canvasGroup);
