278804c211226b05c05021fcbaa1260a58bfc3b4
Felix Mann <felix.fei.mann@gmail.com>
2021-04-11 16:04:35 -0700

    Add colour :)

--- Assets/Scripts/UI/Common/BuildableDetails/Stats/ComparisonResult.cs
+++ Assets/Scripts/UI/Common/BuildableDetails/Stats/ComparisonResult.cs
@@ -1,40 +1,52 @@
-﻿namespace BattleCruisers.UI.Common.BuildableDetails.Stats
+﻿using UnityEngine;
+
+namespace BattleCruisers.UI.Common.BuildableDetails.Stats
 {
     public interface IComparisonResult
 	{
         float RowAlpha { get; }
+		Color RowColour { get; }
 	}
 
 	public abstract class ComparisonResult : IComparisonResult
 	{
         protected class Alpha
         {
-		    public static float NEUTRAL = 1;
-            public static float BETTER = 1;
-            public static float WORSE = 0.5f;
+		    public const float NEUTRAL = 1;
+            public const float BETTER = 1;
+            public const float WORSE = 0.5f;
+        }
+
+		protected class Hue
+        {
+			public static Color NEUTRAL = Color.white;
+			public static Color BETTER = Color.white;
+			public static Color WORSE = Color.black;
         }
 
         public float RowAlpha { get; }
+        public Color RowColour { get; }
 
-        protected ComparisonResult(float rowAlpha)
+        protected ComparisonResult(float rowAlpha, Color rowColour)
 		{
             RowAlpha = rowAlpha;
+			RowColour = rowColour;
 		}
 	}
 
 	public class NeutralResult : ComparisonResult
 	{
-		public NeutralResult() : base(Alpha.NEUTRAL) { }
+		public NeutralResult() : base(Alpha.NEUTRAL, Hue.NEUTRAL) { }
 	}
 
 	public class BetterResult : ComparisonResult
 	{
-		public BetterResult() : base(Alpha.BETTER) { }
+		public BetterResult() : base(Alpha.BETTER, Hue.BETTER) { }
 	}
 
 	public class WorseResult : ComparisonResult
 	{
-		public WorseResult() : base(Alpha.WORSE) { }
+		public WorseResult() : base(Alpha.WORSE, Hue.WORSE) { }
 	}
 }
 
