24c20124c33e032f6b907238b7fc5e03b6587541
Felix Mann <felix.fei.mann@gmail.com>
2021-04-13 13:52:27 -0700

    Remove legacy scripts :)

--- Assets/Scripts/UI/Common/BuildableDetails/Stats/BuildingStatsController.cs
+++
@@ -1,8 +0,0 @@
-﻿using BattleCruisers.Buildables.Buildings;
-
-namespace BattleCruisers.UI.Common.BuildableDetails.Stats
-{
-    public class BuildingStatsController : BuildableStatsController<IBuilding>
-	{
-	}
-}
