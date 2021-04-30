f332ec846472febb1bb52f3d319ebf9d6f03b5c2
Felix Mann <felix.fei.mann@gmail.com>
2021-04-30 15:16:17 -0700

    Add TODO

--- Assets/Scripts/UI/BattleScene/HelpLabels/IHelpLabels.cs
+++ Assets/Scripts/UI/BattleScene/HelpLabels/IHelpLabels.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 namespace BattleCruisers.UI.BattleScene.HelpLabels
 {
+    // FELIX  Remove all old help labels :P
     public interface IHelpLabels
     {
         // FELIX :D
