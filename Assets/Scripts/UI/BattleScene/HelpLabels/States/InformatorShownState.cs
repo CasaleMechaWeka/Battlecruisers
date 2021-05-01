e82798386a846a65839ec3156ca0c2b7e4db18b5
Felix Mann <felix.fei.mann@gmail.com>
2021-05-01 08:49:00 -0700

    Create the 4 help label states :)

---
+++ Assets/Scripts/UI/BattleScene/HelpLabels/States/InformatorShownState.cs
@@ -0,0 +1,18 @@
+﻿using BattleCruisers.UI.Panels;
+
+namespace BattleCruisers.UI.BattleScene.HelpLabels.States
+{
+    public class InformatorShownState : HelpState
+    {
+        public InformatorShownState(IPanel helpLabelCanvas, ISlidingPanel informatorExtendedPanel, IHelpLabels helpLabels) 
+            : base(helpLabelCanvas, informatorExtendedPanel, helpLabels)
+        {
+        }
+
+        public override void ShowHelpLabels()
+        {
+            base.ShowHelpLabels();
+            _helpLabels.Informator.Show();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/BattleScene/HelpLabels/States/SelectorShownState.cs
@@ -0,0 +1,21 @@
+﻿using BattleCruisers.UI.Panels;
+
+namespace BattleCruisers.UI.BattleScene.HelpLabels.States
+{
+    public class SelectorShownState : HelpState
+    {
+        public SelectorShownState(IPanel helpLabelCanvas, ISlidingPanel informatorExtendedPanel, IHelpLabels helpLabels) 
+            : base(helpLabelCanvas, informatorExtendedPanel, helpLabels)
+        {
+        }
+
+        public override void ShowHelpLabels()
+        {
+            base.ShowHelpLabels();
+
+            _helpLabels.LeftBottom.Show();
+            _helpLabels.RightBottom.Show();
+            _helpLabels.BuildMenu.Show();
+        }
+    }
+}
