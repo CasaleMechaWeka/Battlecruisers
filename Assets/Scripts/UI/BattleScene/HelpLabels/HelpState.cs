dc2750ef70a481c7758b80587092dbf6ab54ba16
Felix Mann <felix.fei.mann@gmail.com>
2021-05-01 08:41:41 -0700

    Implement base HelpState :)

--- Assets/Scripts/UI/BattleScene/HelpLabels/HelpState.cs
+++ Assets/Scripts/UI/BattleScene/HelpLabels/HelpState.cs
@@ -5,26 +5,34 @@ namespace BattleCruisers.UI.BattleScene.HelpLabels
 {
     public abstract class HelpState : IHelpState
     {
+        private readonly IPanel _helpLabelCanvas;
         protected readonly ISlidingPanel _informatorExtendedPanel;
         protected readonly IHelpLabels _helpLabels;
 
-        protected HelpState(ISlidingPanel informatorExtendedPanel, IHelpLabels helpLabels)
+        protected HelpState(IPanel helpLabelCanvas, ISlidingPanel informatorExtendedPanel, IHelpLabels helpLabels)
         {
-            Helper.AssertIsNotNull(informatorExtendedPanel, helpLabels);
+            Helper.AssertIsNotNull(helpLabelCanvas, informatorExtendedPanel, helpLabels);
 
+            _helpLabelCanvas = helpLabelCanvas;
             _informatorExtendedPanel = informatorExtendedPanel;
             _helpLabels = helpLabels;
         }
 
         public virtual void ShowHelpLabels()
         {
-            // FELIX  Show help label canvas
-            // FELIX  Common to all 4 stats (cruiser health bars?)
+            _helpLabelCanvas.Show();
+            _helpLabels.CruiserHealth.Show();
         }
 
         public void HideHelpLables()
         {
-            // FELIX  Hide everything
+            _helpLabelCanvas.Hide();
+            _helpLabels.CruiserHealth.Hide();
+            _helpLabels.LeftBottom.Hide();
+            _helpLabels.BuildingCategories.Hide();
+            _helpLabels.RightBottom.Hide();
+            _helpLabels.Informator.Hide();
+            _helpLabels.BuildMenu.Hide();
         }
     }
 }
