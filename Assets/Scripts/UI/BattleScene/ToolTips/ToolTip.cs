e6f0830a787c6d15324a2c9cfce94bfac5c980af
ArcaneWinter <deanwilliams.arcanewinter@gmail.com>
2022-01-18 00:27:05 +0200

    Added tooltips to left panel of UI

---
+++ Assets/Scripts/UI/BattleScene/ToolTips/ToolTip.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ToolTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject ToolTipTextObject;
+    private Text toolTipText;
+    public Text textForLoc;
+
+    public void Start()
+    {
+        textForLoc.enabled = false;
+        toolTipText = ToolTipTextObject.GetComponent<Text>();
+        toolTipText.enabled = false;
+    }
+
+    public void Update()
+    {
+        if (toolTipText.enabled)
+        {
+            ToolTipTextObject.transform.position = Input.mousePosition;
+            ToolTipTextObject.transform.position = new Vector3(ToolTipTextObject.transform.position.x, ToolTipTextObject.transform.position.y + 60, ToolTipTextObject.transform.position.z);
+            
+        }
+    }
+    
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        toolTipText.text = textForLoc.text;
+        toolTipText.enabled = true;
+        ToolTipTextObject.transform.position = Input.mousePosition;
+        ToolTipTextObject.transform.position = new Vector3(ToolTipTextObject.transform.position.x, ToolTipTextObject.transform.position.y + 60, ToolTipTextObject.transform.position.z);
+        if (ToolTipTextObject.transform.position.x < Screen.width/2)
+        {
+            toolTipText.alignment = TextAnchor.LowerLeft;
+        }
+        else{
+            toolTipText.alignment = TextAnchor.LowerRight;
+        }
+        Debug.Log("WOW");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        toolTipText.enabled = false;
+        toolTipText.text = "";
+        Debug.Log("WOW2");
+    }
+}
---
+++ Assets/Scripts/UI/BattleScene/ToolTips/ToolTipManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTipManager : MonoBehaviour
+{
+    public static ToolTipManager _instance;
+    public Text textComponent;
+
+    public void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else{
+            _instance = this;
+        }
+    }
+
+    public void Start()
+    {
+        textComponent.text = "";
+        gameObject.SetActive(false);
+    }
+
+    //may have issues with touch versus mouse
+    public void SetAndShowToolTip(string message)
+    {
+        gameObject.SetActive(true);
+        textComponent.text = message;
+        transform.position = Input.mousePosition;
+        if (transform.position.x < Screen.width/2)
+        {
+            textComponent.alignment = TextAnchor.LowerRight;
+        }
+        else{
+            textComponent.alignment = TextAnchor.LowerLeft;
+        }
+    }
+
+    public void HideToolTip()
+    {
+        gameObject.SetActive(false);
+        textComponent.text = "";
+    }
+    
+}
