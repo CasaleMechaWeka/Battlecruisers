bd26f5a276bc252a8e6fcc77638f0ca2fdc3b2fb
ArcaneWinter <deanwilliams.arcanewinter@gmail.com>
2022-01-19 00:29:14 +0200

    Final tooltip changes

--- Assets/Scripts/UI/BattleScene/ToolTips/ToolTip.cs
+++ Assets/Scripts/UI/BattleScene/ToolTips/ToolTip.cs
@@ -8,21 +8,35 @@ public class ToolTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject ToolTipTextObject;
     private Text toolTipText;
+    private Image background;
     public Text textForLoc;
+    private float xDelta;
 
     public void Start()
     {
         textForLoc.enabled = false;
-        toolTipText = ToolTipTextObject.GetComponent<Text>();
+        toolTipText = ToolTipTextObject.GetComponentInChildren<Text>();
+        background = ToolTipTextObject.GetComponent<Image>();
         toolTipText.enabled = false;
+        background.enabled = false;
     }
 
     public void Update()
     {
         if (toolTipText.enabled)
         {
-            ToolTipTextObject.transform.position = Input.mousePosition;
-            ToolTipTextObject.transform.position = new Vector3(ToolTipTextObject.transform.position.x, ToolTipTextObject.transform.position.y + 60, ToolTipTextObject.transform.position.z);
+            if (Input.mousePosition.x < Screen.width/2)
+            {
+                xDelta = (((RectTransform)ToolTipTextObject.transform).rect.width)/3;
+            }
+            else{
+                xDelta = -(((RectTransform)ToolTipTextObject.transform).rect.width)/3;
+            }
+            if (Mathf.Abs(xDelta) > 10)
+            {
+                ToolTipTextObject.transform.position = Input.mousePosition;
+                ToolTipTextObject.transform.position = new Vector3(ToolTipTextObject.transform.position.x + xDelta, ToolTipTextObject.transform.position.y + 60, ToolTipTextObject.transform.position.z);
+            } 
         }
     }
     
@@ -31,20 +45,14 @@ public class ToolTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         toolTipText.text = textForLoc.text;
         toolTipText.enabled = true;
-        ToolTipTextObject.transform.position = Input.mousePosition;
-        ToolTipTextObject.transform.position = new Vector3(ToolTipTextObject.transform.position.x, ToolTipTextObject.transform.position.y + 60, ToolTipTextObject.transform.position.z);
-        if (ToolTipTextObject.transform.position.x < Screen.width/2)
-        {
-            toolTipText.alignment = TextAnchor.LowerLeft;
-        }
-        else{
-            toolTipText.alignment = TextAnchor.LowerRight;
-        }
+        background.enabled = true;
+        //ToolTipTextObject.transform.position = Input.mousePosition;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         toolTipText.enabled = false;
+        background.enabled = false;
         toolTipText.text = "";
     }
 }
