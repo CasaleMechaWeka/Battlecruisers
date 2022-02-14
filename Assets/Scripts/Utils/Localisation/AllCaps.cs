82f860850855a54479d077ccb78d625bc9c9e54a
ArcaneWinter <deanwilliams.arcanewinter@gmail.com>
2022-02-14 23:23:37 +0200

    Fixed Loc issues with AllCaps.

--- Assets/Scripts/Utils/Localisation/AllCaps.cs
+++ Assets/Scripts/Utils/Localisation/AllCaps.cs
@@ -13,17 +13,20 @@ namespace BattleCruisers.Utils.Localisation
         void Start()
         {
             _text = GetComponent<Text>();
+            Debug.Log(_text.text + "- Start");
             Assert.IsNotNull(_text, $"{gameObject.name}: {nameof(AllCaps)} should only be attached to a game object that has a {nameof(Text)} element.");
 
             _localizeStringEvent = GetComponent<LocalizeStringEvent>();
             Assert.IsNotNull(_localizeStringEvent, $"{gameObject.name}: {nameof(AllCaps)} should only be attached to a game object that has a {nameof(LocalizeStringEvent)} element.");
 
             _localizeStringEvent.OnUpdateString.AddListener(OnUpdateString);
+            _localizeStringEvent.RefreshString();
         }
 
         private void OnUpdateString(string localisedString)
         {
             _text.text = localisedString.ToUpper();
+            Debug.Log(_text.text + "- OnUpdateString");
         }
     }
 }
