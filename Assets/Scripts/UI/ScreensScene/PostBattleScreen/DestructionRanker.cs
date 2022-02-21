e82dbc81f049dbe049ca519538ff2746e666e33a
ArcaneWinter <deanwilliams.arcanewinter@gmail.com>
2022-02-21 03:04:23 +0200

    More work on DestructionScene

---
+++ Assets/Scripts/UI/ScreensScene/PostBattleScreen/DestructionRanker.cs
@@ -0,0 +1,35 @@
+using BattleCruisers.Data;
+using BattleCruisers.UI.Loading;
+using BattleCruisers.UI.Music;
+using BattleCruisers.UI.Sound.AudioSources;
+using BattleCruisers.UI.Sound.Players;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Fetchers;
+using BattleCruisers.Utils.Localisation;
+using BattleCruisers.Utils.PlatformAbstractions.Audio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace BattleCruisers.PostBattleScreen
+{
+    public class DestructionRanker : MonoBehaviour
+    {
+        public GameObject[] destructionRanks;
+
+        public void DisplayRank(long score)
+        {
+
+        }
+
+        private void CalculateRank(long score)
+        {
+            
+        }
+
+    }
+
+}
