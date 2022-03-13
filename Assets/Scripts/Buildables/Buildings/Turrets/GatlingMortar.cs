a637db64ae6ad5688c5cd43e274cc7cf89c00252
ArcaneWinter <deanwilliams.arcanewinter@gmail.com>
2022-03-13 20:20:22 +0200

    Gatling mortar now has same firing sound as huntress' version

--- Assets/Scripts/Buildables/Buildings/Turrets/GatlingMortar.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/GatlingMortar.cs
@@ -6,7 +6,7 @@ namespace BattleCruisers.Buildables.Buildings.Turrets
     public class GatlingMortar : OffenseTurret
 	{
         // DLC  Have own sound
-        protected override ISoundKey FiringSound => SoundKeys.Firing.Artillery;
+        protected override ISoundKey FiringSound => SoundKeys.Firing.AttackBoat;
         protected override PrioritisedSoundKey ConstructionCompletedSoundKey => PrioritisedSoundKeys.Completed.Buildings.Artillery;
     }
 }
