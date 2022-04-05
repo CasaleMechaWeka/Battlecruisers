f2c0cfcf6bbd55abeb933b3d5b2f26c1ee3c9b39
ArcaneWinter <deanwilliams.arcanewinter@gmail.com>
2022-04-05 22:02:50 +0200

    Added a discord button in the bottom right of the screen.

---
+++ Assets/Scripts/OpenLink.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public string linkString;
+    
+    public void Open()
+    {
+        Application.OpenURL(linkString);
+    }
+}
