b7e3b1d415e3c82512ddcd4b904ce268a788e9a0
Peter Casale <casalepeter@gmail.com>
2022-10-23 17:27:55 +1300

    GodCopter test scene created, still works :)

---
+++ Assets/Scripts/Scenes/Test/Aircraft/GodCopterTestGod.cs
@@ -0,0 +1,50 @@
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Buildables.Units;
+using BattleCruisers.Buildables.Units.Aircraft;
+using BattleCruisers.Buildables.Units.Aircraft.Providers;
+using BattleCruisers.Buildables.Units.Ships;
+using BattleCruisers.Cruisers;
+using BattleCruisers.Scenes.Test.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BattleCruisers.Scenes.Test.Aircraft
+{
+    public class GodCopterTestGod : TestGodBase
+    {
+        private AttackBoatController[] _ships;
+        private GunShipController _gunship;
+
+        public List<Vector2> gunshipPatrolPoints;
+
+        protected override List<GameObject> GetGameObjects()
+        {
+            _ships = FindObjectsOfType<AttackBoatController>();
+            List<GameObject> gameObjects = _ships.Select(ship => ship.GameObject).ToList();
+
+            _gunship = FindObjectOfType<GunShipController>();
+            gameObjects.Add(_gunship.GameObject);
+
+            return gameObjects;
+        }
+
+        protected override void Setup(Helper helper)
+        {
+            ICruiser redCruiser = helper.CreateCruiser(Direction.Left, Faction.Reds);
+
+            // Setup gunship
+            IAircraftProvider aircraftProvider = helper.CreateAircraftProvider(gunshipPatrolPoints: gunshipPatrolPoints);
+            helper.InitialiseUnit(_gunship, Faction.Blues, aircraftProvider: aircraftProvider, parentCruiserDirection: Direction.Left, enemyCruiser: redCruiser);
+            _gunship.StartConstruction();
+
+            // Setup target attack boats
+            foreach (AttackBoatController ship in _ships)
+            {
+                helper.InitialiseUnit(ship, Faction.Reds);
+                ship.StartConstruction();
+                Helper.SetupUnitForUnitMonitor(ship, redCruiser);
+			}
+        }
+    }
+}
