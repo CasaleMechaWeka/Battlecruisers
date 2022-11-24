13b8e64ae042aff29c421d24aa767463e830eb07
David D <bravegamestudio.kiwi@gmail.com>
2022-11-25 11:55:03 +1300

    in range check fix up

--- Assets/Scripts/Targets/Helpers/SizeInclusiveCalculator.cs
+++ Assets/Scripts/Targets/Helpers/SizeInclusiveCalculator.cs
@@ -8,6 +8,9 @@ namespace BattleCruisers.Targets.Helpers
     {
         public bool IsInRange(ITransform parentTransform, ITarget target, float rangeInM)
         {
+            if (target == null || parentTransform == null) {//if either have been distoryed then it's not in range
+                return false;
+            }
             float distanceCenterToCenter = Vector2.Distance(target.Position, parentTransform.Position);
             float distanceCenterToEdge = distanceCenterToCenter - target.Size.x / 2;
             return distanceCenterToEdge <= rangeInM;
