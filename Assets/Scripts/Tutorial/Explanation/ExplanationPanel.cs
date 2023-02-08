0fc28943ba1ddf87e8d2a8d715d0b67d8e987a78
Nycolas <nycolas@mechaweka.co.nz>
2023-02-08 18:22:51 -0300

    Changed the behavior of the tutorial Explanation Panel in the Battle Scene to automate text resizing and prevent character overflow.

--- Assets/Scripts/Tutorial/Explanation/ExplanationPanel.cs
+++ Assets/Scripts/Tutorial/Explanation/ExplanationPanel.cs
@@ -8,6 +8,7 @@ namespace BattleCruisers.Tutorial.Explanation
     public class ExplanationPanel : MonoBehaviourWrapper, IExplanationPanel
     {
         private RectTransform _transform;
+        private RectTransform _textDisplayerTransform;
 
         private const float LARGE_HEIGHT = 770;
         private const float EXPANDED_HEIGHT = 570; 
@@ -15,6 +16,9 @@ namespace BattleCruisers.Tutorial.Explanation
         private const float MID_HEIGHT = 430;
         private const float MID_HEIGHT_NO_BUTTONS = 320;
         private const float SHRUNK_HEIGHT = 220;
+        
+        private const float TEXT_RECT_BOTTOM_NO_BUTTON = 0;
+        private const float TEXT_RECT_BOTTOM_BUTTON = 150;
 
         public TextDisplayer textDisplayer;
         public ITextDisplayer TextDisplayer => textDisplayer;
@@ -39,25 +43,37 @@ namespace BattleCruisers.Tutorial.Explanation
             DoneButton.Enabled = false;
 
             _transform = transform.Parse<RectTransform>();
+            _textDisplayerTransform = textDisplayer.transform.Parse<RectTransform>();
         }
 
         public void ExpandHeight()
         {
             Logging.LogMethod(Tags.TUTORIAL_EXPLANATION_PANEL);
-            //change height depending on the length of the text (number of characters)
+            //change height depending on the length of the text (number of characters) and change the bottom of the textDisplayer rect to match the window
             if (textDisplayer.Text.Length < 39)
             {
-                _transform.sizeDelta = new Vector2(_transform.sizeDelta.x, MID_HEIGHT_NO_BUTTONS);
+                if (!doneButton.Enabled && !okButton.Enabled)
+                {
+                    _transform.sizeDelta = new Vector2(_transform.sizeDelta.x, MID_HEIGHT_NO_BUTTONS);
+                    _textDisplayerTransform.offsetMin = new Vector2(_textDisplayerTransform.offsetMin.x, TEXT_RECT_BOTTOM_NO_BUTTON);
+                }
+                else
+                {
+                    _transform.sizeDelta = new Vector2(_transform.sizeDelta.x, MID_HEIGHT);
+                    _textDisplayerTransform.offsetMin = new Vector2(_textDisplayerTransform.offsetMin.x, TEXT_RECT_BOTTOM_BUTTON);
+                }
             }
             else if (textDisplayer.Text.Length < 60)
             {
                 if (!doneButton.Enabled && !okButton.Enabled) //if no buttons then reduce height further
                 {
                     _transform.sizeDelta = new Vector2(_transform.sizeDelta.x, MID_HEIGHT_NO_BUTTONS);
+                    _textDisplayerTransform.offsetMin = new Vector2(_textDisplayerTransform.offsetMin.x, TEXT_RECT_BOTTOM_NO_BUTTON);
                 }
                 else
                 {
                     _transform.sizeDelta = new Vector2(_transform.sizeDelta.x, MID_HEIGHT);
+                    _textDisplayerTransform.offsetMin = new Vector2(_textDisplayerTransform.offsetMin.x, TEXT_RECT_BOTTOM_BUTTON);
                 }
             }
             else if (textDisplayer.Text.Length < 140)
@@ -65,15 +81,18 @@ namespace BattleCruisers.Tutorial.Explanation
                 if (!doneButton.Enabled && !okButton.Enabled) //if no buttons then reduce height further
                 {
                     _transform.sizeDelta = new Vector2(_transform.sizeDelta.x, EXPANDED_HEIGHT_NO_BUTTONS);
+                    _textDisplayerTransform.offsetMin = new Vector2(_textDisplayerTransform.offsetMin.x, TEXT_RECT_BOTTOM_NO_BUTTON);
                 }
                 else
                 {
                     _transform.sizeDelta = new Vector2(_transform.sizeDelta.x, EXPANDED_HEIGHT);
+                    _textDisplayerTransform.offsetMin = new Vector2(_textDisplayerTransform.offsetMin.x, TEXT_RECT_BOTTOM_BUTTON);
                 }
             }
             else 
             {
                 _transform.sizeDelta = new Vector2(_transform.sizeDelta.x, LARGE_HEIGHT);
+                _textDisplayerTransform.offsetMin = new Vector2(_textDisplayerTransform.offsetMin.x, TEXT_RECT_BOTTOM_BUTTON);
             }
             
         }
@@ -82,6 +101,7 @@ namespace BattleCruisers.Tutorial.Explanation
         {
             Logging.LogMethod(Tags.TUTORIAL_EXPLANATION_PANEL);
             _transform.sizeDelta = new Vector2(_transform.sizeDelta.x, SHRUNK_HEIGHT);
+            _textDisplayerTransform.offsetMin = new Vector2(_textDisplayerTransform.offsetMin.x, TEXT_RECT_BOTTOM_NO_BUTTON);
         }
     }
 }
