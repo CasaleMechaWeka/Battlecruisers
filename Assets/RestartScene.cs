a0ac63476abdf68a4deb1c0f1e9f3b1353f21f66
Peter Casale <casalepeter@gmail.com>
2023-02-12 01:39:07 +1300

    Moved to test god

--- Assets/RestartScene.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class RestartScene : MonoBehaviour
-{
-    public void Restart()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
-    }
-}
