6938e6578cd07bfa5341a8a3338756b5ca27a147
Peter Casale <casalepeter@gmail.com>
2023-02-24 16:48:08 +1300

    All cruisers work :) Adjusted stars

--- Assets/Scripts/Utils/Categorisation/CruiserHealthToStarsConverter.cs
+++ Assets/Scripts/Utils/Categorisation/CruiserHealthToStarsConverter.cs
@@ -4,12 +4,12 @@
     {
         private static readonly float[] CATEGORY_THRESHOLDS =
         {
-            1200,   // Raptor
+            1,   // Raptor
             2800,   // Eagle
             // 3000, Trident
             3600,   // Bullshark, Hammerhead
-            3800,   // Rockjaw
-            4000    // Longbow, Megalodon
+            4000,   // Rockjaw
+            6000    // Longbow, Megalodon
         };
 
         public CruiserHealthToStarsConverter() : base(CATEGORY_THRESHOLDS)
