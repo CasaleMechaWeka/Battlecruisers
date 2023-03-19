a017ba2114b2cf864257c0a5360f6f7e5c0767ca
Peter Casale <casalepeter@gmail.com>
2023-03-19 18:50:26 +1300

    Lowered naval spawn position :)

--- Assets/Scripts/Buildables/Buildings/Factories/Spawning/NavalFactorySpawnPositionFinder.cs
+++ Assets/Scripts/Buildables/Buildings/Factories/Spawning/NavalFactorySpawnPositionFinder.cs
@@ -19,11 +19,16 @@ namespace BattleCruisers.Buildables.Buildings.Factories.Spawning
             float horizontalChange = (_factory.Size.x * 0.6f) + (unitToSpawn.Size.x * 0.5f);
 
             // If the factory is facing left it has been mirrored (rotated
-            // around the y-axis by 180*).  So its right is an unmirrored
+            // around the y-axis by 180*). So its right is an unmirrored
             // factory's left :/
             Vector3 direction = _factory.Transform.Right;
 
-            return _factory.Transform.Position + (direction * horizontalChange);
+            Vector3 spawnPosition = _factory.Transform.Position + (direction * horizontalChange);
+
+            float yOffset = -0.35f; // Adjust this value based on your requirements
+            spawnPosition.y += yOffset; // Apply the vertical offset
+
+            return spawnPosition;
         }
     }
 }
