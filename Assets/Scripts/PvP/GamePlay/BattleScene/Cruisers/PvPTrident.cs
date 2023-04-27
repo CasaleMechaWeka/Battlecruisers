037ead76ef53c4e27ceada5948e796828206f352
sava <sava@mechaweka.co.nz>
2023-04-28 02:15:23 +0900

    pvp: built prefab of PvPTrident

---
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Cruisers/PvPTrident.cs
@@ -0,0 +1,24 @@
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Boost;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Cruisers
+{
+    /// <summary>
+    /// Perks:
+    /// + Increases tacticals build speed
+    /// </summary>
+    public class PvPTrident : PvPCruiser
+    {
+        public float tacticalsBuildRateBoost;
+
+        public override void Initialise(IPvPCruiserArgs args)
+        {
+            base.Initialise(args);
+
+            Assert.IsTrue(tacticalsBuildRateBoost > 0);
+
+            IPvPBoostProvider boostProvider = FactoryProvider.BoostFactory.CreateBoostProvider(tacticalsBuildRateBoost);
+            CruiserSpecificFactories.GlobalBoostProviders.BuildingBuildRate.TacticalsProviders.Add(boostProvider);
+        }
+    }
+}
