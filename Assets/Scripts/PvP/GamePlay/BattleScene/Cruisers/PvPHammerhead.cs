ef233bd8825a6f7130ca17f627edad420197d78a
sava <sava@mechaweka.co.nz>
2023-04-30 06:02:02 +0900

    pvp: built prefab of PvPHammerhead

---
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Cruisers/PvPHammerhead.cs
@@ -0,0 +1,30 @@
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Boost;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Cruisers
+{
+    /// <summary>
+    /// Perks:
+    /// + Increases naval factory build speed
+    /// + Increases ship build speed
+    /// </summary>
+    public class PvPHammerhead : PvPCruiser
+    {
+        public float navalFactoryBuildRateBoost;
+        public float shipBuildRateBoost;
+
+        public override void Initialise(IPvPCruiserArgs args)
+        {
+            base.Initialise(args);
+
+            Assert.IsTrue(navalFactoryBuildRateBoost > 0);
+            Assert.IsTrue(shipBuildRateBoost > 0);
+
+            IPvPBoostProvider factoryBoostProvider = FactoryProvider.BoostFactory.CreateBoostProvider(navalFactoryBuildRateBoost);
+            CruiserSpecificFactories.GlobalBoostProviders.BuildingBuildRate.NavalFactoryProviders.Add(factoryBoostProvider);
+
+            IPvPBoostProvider shipBoostProvider = FactoryProvider.BoostFactory.CreateBoostProvider(shipBuildRateBoost);
+            CruiserSpecificFactories.GlobalBoostProviders.UnitBuildRate.ShipProviders.Add(shipBoostProvider);
+        }
+    }
+}
