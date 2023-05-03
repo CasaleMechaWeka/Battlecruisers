909557ab501dbedc38b95c3852057ccdb56097c5
sava <sava@mechaweka.co.nz>
2023-05-04 06:35:50 +0900

    pvp: built prefab of PvPArtillery

---
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/PvPArtillery.cs
@@ -0,0 +1,11 @@
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Data.Static;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.UI.Sound;
+
+namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Turrets
+{
+    public class PvPArtillery : PvPOffenseTurret
+    {
+        protected override IPvPSoundKey FiringSound => PvPSoundKeys.PvPFiring.Artillery;
+        protected override PvPPrioritisedSoundKey ConstructionCompletedSoundKey => PvPPrioritisedSoundKeys.PvPCompleted.PvPBuildings.Artillery;
+    }
+}
---
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Projectiles/Pools/PvPShellLargePoolChooser.cs
@@ -0,0 +1,24 @@
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Projectiles.ActivationArgs;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Projectiles.Stats;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils.Pools;
+using UnityEngine;
+
+namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Projectiles.Pools
+{
+    public class PvPShellLargePoolChooser : MonoBehaviour, IPvPProjectilePoolChooser<PvPProjectileController, PvPProjectileActivationArgs<IPvPProjectileStats>, IPvPProjectileStats>
+    {
+
+        public bool novaShell = false;
+
+        public IPvPPool<PvPProjectileController, PvPProjectileActivationArgs<IPvPProjectileStats>>
+            ChoosePool(IPvPProjectilePoolProvider projectilePoolProvider)
+        {
+            if (novaShell)
+            {
+                return projectilePoolProvider.NovaShellPool;
+            }
+
+            return projectilePoolProvider.ShellsLargePool;
+        }
+    }
+}
