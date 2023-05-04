0e73232bb3d4a14a3f1f14cdd9536b5ee7cd2efc
sava <sava@mechaweka.co.nz>
2023-05-05 06:00:29 +0900

    pvp: built prefab of PvPKamikazeSignal

---
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Tactical/PvPKamikazeSignal.cs
@@ -0,0 +1,45 @@
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Boost;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Boost.GlobalProviders;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Units.Aircraft;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Data.Static;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.UI.Sound;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Tactical
+{
+    public class PvPKamikazeSignal : PvPBuilding
+    {
+        protected override PvPPrioritisedSoundKey ConstructionCompletedSoundKey => PvPPrioritisedSoundKeys.PvPCompleted.Ultra;
+
+        protected override void AddBuildRateBoostProviders(
+            IPvPGlobalBoostProviders globalBoostProviders,
+            IList<ObservableCollection<IPvPBoostProvider>> buildRateBoostProvidersList)
+        {
+            base.AddBuildRateBoostProviders(globalBoostProviders, buildRateBoostProvidersList);
+            buildRateBoostProvidersList.Add(_cruiserSpecificFactories.GlobalBoostProviders.BuildingBuildRate.UltrasProviders);
+        }
+
+        protected override void OnBuildableCompleted()
+        {
+            base.OnBuildableCompleted();
+
+            GameObject[] aircraftGameObjects = GameObject.FindGameObjectsWithTag(GameObjectTags.AIRCRAFT);
+
+            foreach (GameObject aircraftGameObject in aircraftGameObjects)
+            {
+                PvPAircraftController aircraftController = aircraftGameObject.GetComponentInChildren<PvPAircraftController>();
+                Assert.IsNotNull(aircraftController);
+
+                if (aircraftController.BuildableState == PvPBuildableState.Completed
+                    && !aircraftController.IsDestroyed)
+                {
+                    aircraftController.Kamikaze(EnemyCruiser);
+                }
+            }
+        }
+    }
+}
