cd91ec1ada52812ee87a9929fd7c3f8f8aa7ac66
Icewave <cole@mechaweka.co.nz>
2023-05-07 15:32:34 -0700

    SFX fix

--- Assets/Scripts/Effects/Explosions/ExplosionController.cs
+++ Assets/Scripts/Effects/Explosions/ExplosionController.cs
@@ -1,17 +1,33 @@
 ﻿using BattleCruisers.Effects.ParticleSystems;
+using BattleCruisers.Data.Settings;
+using BattleCruisers.Data;
 using UnityEngine;
+
 namespace BattleCruisers.Effects.Explosions
 {
     public class ExplosionController : ParticleSystemGroupInitialiser
     {
+        AudioSource audioSource;
+        private ISettingsManager _settingsManager;
         public virtual IExplosion Initialise()
         {
-            return 
+
+            return
                 new Explosion(
-                    this, 
+                    this,
                     GetParticleSystems(),
                     GetSynchronizedSystems());
         }
+
+        private void Awake()
+        {
+            _settingsManager = ApplicationModelProvider.ApplicationModel.DataProvider.SettingsManager;
+            audioSource = GetComponentInChildren<AudioSource>();
+            if (audioSource != null)
+                audioSource.volume = _settingsManager.EffectVolume * _settingsManager.MasterVolume;
+        }
+
+
     }
 
 }
