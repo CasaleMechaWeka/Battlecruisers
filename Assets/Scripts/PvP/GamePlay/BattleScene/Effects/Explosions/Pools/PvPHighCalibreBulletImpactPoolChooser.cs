d52b579721ddf94a722c68d95fac69e12482fc4f
sava <sava@mechaweka.co.nz>
2023-05-08 17:09:45 +0900

    pvp: built prefab of PvPHighCalibreBullet

---
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Effects/Explosions/Pools/PvPHighCalibreBulletImpactPoolChooser.cs
@@ -0,0 +1,13 @@
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils.Pools;
+using UnityEngine;
+
+namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Effects.Explosions.Pools
+{
+    public class PvPHighCalibreBulletImpactPoolChooser : MonoBehaviour, IPvPExplosionPoolChooser
+    {
+        public IPvPPool<IPvPExplosion, Vector3> ChoosePool(IPvPExplosionPoolProvider explosionPoolProvider)
+        {
+            return explosionPoolProvider.HighCalibreBulletImpactPool;
+        }
+    }
+}
