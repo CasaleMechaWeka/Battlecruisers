13096070bbc5d7104e7864980213763e0484c997
sava <sava@mechaweka.co.nz>
2023-05-08 17:49:56 +0900

    pvp: built prefab of PvPMissileMedium

---
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Effects/Explosions/Pools/PvPMediumExplosionPoolChooser.cs
@@ -0,0 +1,13 @@
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils.Pools;
+using UnityEngine;
+
+namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Effects.Explosions.Pools
+{
+    public class PvPMediumExplosionPoolChooser : MonoBehaviour, IPvPExplosionPoolChooser
+    {
+        public IPvPPool<IPvPExplosion, Vector3> ChoosePool(IPvPExplosionPoolProvider explosionPoolProvider)
+        {
+            return explosionPoolProvider.MediumExplosionsPool;
+        }
+    }
+}
