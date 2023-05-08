43439c360a0fbac6e713b6a8ca36ad6eb4b2fe98
sava <sava@mechaweka.co.nz>
2023-05-08 11:17:19 +0900

    pvp: built prefab of PvPBomb

---
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Effects/Trails/PvPBombTrailController.cs
@@ -0,0 +1,29 @@
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils;
+using UnityEngine;
+
+namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Effects.Trails
+{
+    public class PvPBombTrailController : MonoBehaviour, IPvPProjectileTrail
+    {
+        public SpriteRenderer glow, bullet;
+        public TrailRenderer trail;
+
+        public void Initialise()
+        {
+            PvPHelper.AssertIsNotNull(glow, bullet, trail);
+        }
+
+        public void ShowAllEffects()
+        {
+            glow.enabled = true;
+            bullet.enabled = true;
+            trail.Clear();
+        }
+
+        public void HideEffects()
+        {
+            glow.enabled = false;
+            bullet.enabled = false;
+        }
+    }
+}
