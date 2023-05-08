fd44f75cf6b17246a4b79d612959a75d1426a147
sava <sava@mechaweka.co.nz>
2023-05-08 10:30:10 +0900

    pvp: built prefab of PvPBuilderDrone

---
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Effects/Drones/PvPDroneController.cs
@@ -0,0 +1,54 @@
+using System;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils;
+using BattleCruisers.Utils.Localisation;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Effects.Drones
+{
+    public class PvPDroneController : PvPPrefab, IPvPDroneController
+    {
+        private Animation _animation;
+
+        public PvPFaction Faction { get; private set; }
+
+        public event EventHandler Activated;
+        public event EventHandler Deactivated;
+
+        public override void StaticInitialise(ILocTable commonStrings)
+        {
+            base.StaticInitialise(commonStrings);
+
+            _animation = GetComponentInChildren<Animation>();
+            Assert.IsNotNull(_animation);
+
+            gameObject.SetActive(false);
+        }
+
+        public void Activate(PvPDroneActivationArgs activationArgs)
+        {
+            gameObject.transform.position = activationArgs.Position;
+            gameObject.SetActive(true);
+
+            Faction = activationArgs.Faction;
+
+            AnimationState state = _animation["BuilderDrone"];
+            Assert.IsNotNull(state);
+            state.normalizedTime = PvPRandomGenerator.Instance.Value;
+            _animation.Play();
+
+            Activated?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Activate(PvPDroneActivationArgs activationArgs, PvPFaction faction)
+        {
+        }
+
+        public void Deactivate()
+        {
+            gameObject.SetActive(false);
+            Deactivated?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
