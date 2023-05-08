657b6f111cd808abd529a28b817ae45dbd948172
sava <sava@mechaweka.co.nz>
2023-05-08 09:00:07 +0900

    pvp: built prefab of PvPFrigate

---
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Units/Ships/PvPFrigateController.cs
@@ -0,0 +1,64 @@
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Turrets.BarrelWrappers;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Data.Static;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.UI.BattleScene.ProgressBars;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils;
+using BattleCruisers.Utils.Localisation;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Units.Ships
+{
+    public class PvPFrigateController : PvPShipController
+    {
+        private IPvPBarrelWrapper _directFireAntiSea, _mortar, _samSite;// _directFireAntiAir;
+
+        private float _optimalArmamentRangeInM;
+        public override float OptimalArmamentRangeInM => _optimalArmamentRangeInM;
+
+        public override void StaticInitialise(GameObject parent, PvPHealthBarController healthBar, ILocTable commonStrings)
+        {
+            base.StaticInitialise(parent, healthBar, commonStrings);
+            _optimalArmamentRangeInM = FindOptimalArmamentRangeInM();
+        }
+
+        /// <summary>
+        /// Enemy detector is in ship center, but longest range barrel (mortar) is behind
+        /// ship center.  Want to only stop once barrel is in range, so make optimal 
+        /// armament range be less than the longest range barrel.
+        private float FindOptimalArmamentRangeInM()
+        {
+            return _mortar.RangeInM - (Mathf.Abs(transform.position.x - _mortar.Position.x));
+        }
+
+        protected override IList<IPvPBarrelWrapper> GetTurrets()
+        {
+            IList<IPvPBarrelWrapper> turrets = new List<IPvPBarrelWrapper>();
+
+            // Anti ship turret
+            _directFireAntiSea = transform.FindNamedComponent<IPvPBarrelWrapper>("DirectFireAntiSea");
+            turrets.Add(_directFireAntiSea);
+
+            // Mortar
+            _mortar = transform.FindNamedComponent<IPvPBarrelWrapper>("Mortar");
+            turrets.Add(_mortar);
+
+            // Anti air turret
+            //_directFireAntiAir = transform.FindNamedComponent<IBarrelWrapper>("DirectBurstFireAntiAir");
+            //turrets.Add(_directFireAntiAir);
+
+            // SAM site
+            _samSite = transform.FindNamedComponent<IPvPBarrelWrapper>("SamSite");
+            turrets.Add(_samSite);
+
+            return turrets;
+        }
+
+        protected override void InitialiseTurrets()
+        {
+            _directFireAntiSea.Initialise(this, _factoryProvider, _cruiserSpecificFactories, PvPSoundKeys.PvPFiring.BigCannon);
+            _mortar.Initialise(this, _factoryProvider, _cruiserSpecificFactories, PvPSoundKeys.PvPFiring.BigCannon);
+            //_directFireAntiAir.Initialise(this, _factoryProvider, _cruiserSpecificFactories, SoundKeys.Firing.AntiAir);
+            _samSite.Initialise(this, _factoryProvider, _cruiserSpecificFactories, PvPSoundKeys.PvPFiring.Missile);
+        }
+    }
+}
