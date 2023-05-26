bfc33f870ec113bb66a92299551d6cc5e1fc5709
Peter Casale <casalepeter@gmail.com>
2023-05-26 16:51:27 +1200

    update: New shell and turrel controllers to handle ranked targets

--- Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/ShellTurretBarrelController.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelControllers/ShellTurretBarrelController.cs
@@ -1,5 +1,6 @@
 ﻿using BattleCruisers.Projectiles.Spawners;
 using BattleCruisers.Utils;
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Assertions;
@@ -7,12 +8,12 @@ using UnityEngine.Assertions;
 namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers
 {
     /// <summary>
-    /// Supports mutliple shell spawners (barrels).  The turret stats damage
-    /// is spread accross the barrels (ie, if there are 2 barrels, each barrel
+    /// Supports multiple shell spawners (barrels). The turret stats damage
+    /// is spread across the barrels (i.e., if there are 2 barrels, each barrel
     /// receives half the turret stats' damage).
     /// </summary>
     public class ShellTurretBarrelController : BarrelController
-	{
+    {
         private ShellSpawner[] _shellSpawners;
         private ShellSpawner _middleSpawner;
 
@@ -22,33 +23,32 @@ namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers
         public override bool CanFireWithoutTarget => true;
 
         public override void StaticInitialise()
-		{
-			base.StaticInitialise();
+        {
+            base.StaticInitialise();
 
-			_shellSpawners = gameObject.GetComponentsInChildren<ShellSpawner>();
-			Assert.IsNotNull(_shellSpawners);
+            _shellSpawners = gameObject.GetComponentsInChildren<ShellSpawner>();
+            Assert.IsNotNull(_shellSpawners);
             Assert.IsTrue(_shellSpawners.Length != 0);
 
             _middleSpawner = _shellSpawners.Middle();
         }
 
         protected override async Task InternalInitialiseAsync(IBarrelControllerArgs args)
-		{
+        {
             IProjectileSpawnerArgs spawnerArgs = new ProjectileSpawnerArgs(args, _projectileStats, TurretStats.BurstSize);
 
             foreach (ShellSpawner spawner in _shellSpawners)
             {
-                await spawner.InitialiseAsync(spawnerArgs, args.SpawnerSoundKey, args.TargetFilter);
+                await spawner.InitialiseAsync(spawnerArgs, args.SpawnerSoundKey, args.TargetFilter, TurretStats.AttackCapabilities.ToList());
             }
-		}
+        }
 
         public override void Fire(float angleInDegrees)
-		{
-			foreach (ShellSpawner spawner in _shellSpawners)
+        {
+            foreach (ShellSpawner spawner in _shellSpawners)
             {
                 spawner.SpawnShell(angleInDegrees, IsSourceMirrored);
-			}
-		}
-	}
+            }
+        }
+    }
 }
-
--- Assets/Scripts/Projectiles/ProjectileController.cs
+++ Assets/Scripts/Projectiles/ProjectileController.cs
@@ -1,10 +1,14 @@
-﻿using BattleCruisers.Projectiles.ActivationArgs;
+﻿using BattleCruisers.Buildables;
+using BattleCruisers.Projectiles.ActivationArgs;
 using BattleCruisers.Projectiles.Stats;
+using System.Collections.Generic;
 
 namespace BattleCruisers.Projectiles
 {
     public class ProjectileController : ProjectileControllerBase<ProjectileActivationArgs<IProjectileStats>, IProjectileStats>
     {
+        public List<TargetType> AttackCapabilities { get; set; }
+
         // empty
     }
 }
