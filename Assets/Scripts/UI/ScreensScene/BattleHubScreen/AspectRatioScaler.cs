05b1a9d40baae0ebaed3059897828bf027906b22
Peter Casale <casalepeter@gmail.com>
2023-06-20 04:45:49 +1200

    design: all Main panel elements respond

--- Assets/Scripts/UI/ScreensScene/BattleHubScreen/AspectRatioScaler.cs
+++ Assets/Scripts/UI/ScreensScene/BattleHubScreen/AspectRatioScaler.cs
@@ -6,31 +6,41 @@ using UnityEngine.UI;
 namespace BattleCruisers.UI.ScreensScene.BattleHubScreen
 {
     [RequireComponent(typeof(RectTransform))]
+    [AddComponentMenu("BattleCruisers/UI/AspectRatioScaler")]
     public class AspectRatioScaler : MonoBehaviour
     {
+        [Header("General Function:")]
+        [TextArea]
+        [SerializeField]
+        private string info = "This script adjusts the scale of the attached GameObject based on the current aspect ratio of the screen.";
+
         [Header("Aspect Ratio Settings")]
-        [Tooltip("The aspect ratio (width) that your game is designed for.")]
+        [Tooltip("The width part of the aspect ratio that your game is optimized for. For an aspect ratio of 16:9, this would be 16.")]
         [SerializeField]
         private float targetRatioX = 16;
 
-        [Tooltip("The aspect ratio (height) that your game is designed for.")]
+        [Tooltip("The height part of the aspect ratio that your game is optimized for. For an aspect ratio of 16:9, this would be 9.")]
         [SerializeField]
-        private float targetRatioY = 10;
+        private float targetRatioY = 9;
 
         [Header("Scale Settings")]
-        [Tooltip("The maximum scale of the object. The object's scale will increase as the screen's aspect ratio narrows, up to this maximum.")]
-        [SerializeField, Range(0, 2)]
-        private float maxScale = 1.5f;
+        [Tooltip("The minimum scale of the object when the screen's aspect ratio is the same as or narrower than 'Minimum Scale Ratio'. This scale is relative to the object's original size.")]
+        [SerializeField, Range(-2, 2)]
+        private float minScale = 0.5f;
+
+        [Tooltip("The aspect ratio (width/height) at which the object reaches its minimum scale. When the screen's aspect ratio is the same as or narrower than this value, the object will have the 'Min Scale'.")]
+        [SerializeField]
+        private float minScaleRatio = 5f / 4f;
 
         private void Update()
         {
             float currentAspect = (float)Screen.width / Screen.height;
             float targetAspect = targetRatioX / targetRatioY;
 
-            if (currentAspect < targetAspect)
+            if (currentAspect <= targetAspect)
             {
-                float ratioDifference = (targetAspect - currentAspect) / (targetAspect - 1);
-                float adjustedScale = Mathf.Clamp(1 + ratioDifference, 1, maxScale);
+                float ratioDifference = Mathf.Clamp((targetAspect - currentAspect) / (targetAspect - minScaleRatio), 0, 1);
+                float adjustedScale = 1 + ratioDifference * (minScale - 1);
 
                 transform.localScale = Vector3.one * adjustedScale;
             }
