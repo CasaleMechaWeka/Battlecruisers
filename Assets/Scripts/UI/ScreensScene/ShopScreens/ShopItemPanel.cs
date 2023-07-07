69d760fb299165ef2ae640c3a22cb7623b8c0d3f
Aaron Maniyakku <aaron@mechaweka.co.nz>
2023-07-07 14:17:24 -0400

    update: added shopItempanel script

---
+++ Assets/Scripts/UI/ScreensScene/ShopScreens/ShopItemPanel.cs
@@ -0,0 +1,20 @@
+using BattleCruisers.Data.Models;
+using BattleCruisers.Data.Models.PrefabKeys;
+using BattleCruisers.UI.Panels;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.Comparisons;
+using BattleCruisers.UI.ScreensScene.LoadoutScreen.ItemDetails;
+using BattleCruisers.UI.Sound.Players;
+using BattleCruisers.Utils;
+using BattleCruisers.Utils.Fetchers;
+using System.Collections.Generic;
+using BattleCruisers.Utils.Properties;
+using UnityEngine;
+
+public class ShopItemPanel : Panel
+{
+    public void Initialise(
+        ISingleSoundPlayer soundPlayer)
+    {
+        
+    }
+}
