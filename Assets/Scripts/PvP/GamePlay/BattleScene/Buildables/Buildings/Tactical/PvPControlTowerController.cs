b328b0e25f6bff2b5417d0fefdcc61dd91013ff2
Sava Spasic <sava@mechaweka.co.nz>
2023-07-21 19:40:52 +0100

    pvp: PvPControlTower

--- Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Tactical/PvPControlTowerController.cs
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Tactical/PvPControlTowerController.cs
@@ -1,8 +1,11 @@
 using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Boost;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Cruisers.Drones;
 using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Data.Static;
 using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.UI.BattleScene.Manager;
 using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.UI.Sound;
 using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils.Factories;
+using Unity.Netcode;
+using UnityEngine;
 
 namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Tactical
 {
@@ -16,7 +19,7 @@ namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Builda
 
         public override void Initialise(IPvPFactoryProvider factoryProvider)
         {
-            // base.Initialise(uiManager, factoryProvider);
+             base.Initialise(/*uiManager,*/ factoryProvider);
             _boostProvider = _factoryProvider.BoostFactory.CreateBoostProvider(boostMultiplier);
         }
 
@@ -30,6 +33,234 @@ namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Builda
         {
             base.OnDestroyed();
             _cruiserSpecificFactories.GlobalBoostProviders.AircraftBoostProviders.Remove(_boostProvider);
+        }
+
+        // sava added code
+        // BuildProgress 
+        public NetworkVariable<float> PvP_BuildProgress = new NetworkVariable<float>();
+
+
+        // Visibility
+        protected override void OnValueChangedIsEnableRenderes(bool isEnabled)
+        {
+            if (IsClient)
+                base.OnValueChangedIsEnableRenderes(isEnabled);
+            if (IsServer)
+                OnValueChangedIsEnabledRendersClientRpc(isEnabled);
+        }
+
+        // Healthbar offset
+        protected override void CallRpc_SetHealthbarOffset(Vector2 offset)
+        {
+            OnSetHealthbarOffsetClientRpc(offset);
+        }
+
+        // set Position of PvPBuildable
+        protected override void CallRpc_SetPosition(Vector3 pos)
+        {
+            OnSetPositionClientRpc(pos);
+        }
+
+
+        // Set Rotation of PvPBuildable
+        protected override void CallRpc_SetRotation(Quaternion rotation)
+        {
+            OnSetRotationClientRpc(rotation);
+        }
+
+        protected override void ShareIsDroneConsumerFocusableValueWithClient(bool isFocusable)
+        {
+            OnShareIsDroneConsumerFocusableValueWithClientRpc(isFocusable);
+        }
+
+        protected override void CallRpc_ToggleDroneConsumerFocusCommandExecute()
+        {
+            base.CallRpc_ToggleDroneConsumerFocusCommandExecute();
+            if (IsClient)
+                OnToggleDroneConsumerFocusCommandExecuteServerRpc();
+        }
+
+        protected override void PlayPlacementSound()
+        {
+            base.PlayPlacementSound();
+
+            if (IsServer)
+                PlayPlacementSoundClientRpc();
+        }
+
+        protected override void DestroyMe()
+        {
+            if (IsServer)
+                base.DestroyMe();
+            if (IsClient)
+                OnDestroyMeServerRpc();
+        }
+
+        protected override void CallRpc_PlayDeathSound()
+        {
+            if (IsClient)
+                base.CallRpc_PlayDeathSound();
+            if (IsServer)
+                OnPlayDeathSoundClientRpc();
+        }
+
+        protected override void PlayBuildableConstructionCompletedSound()
+        {
+            if (IsClient)
+                base.PlayBuildableConstructionCompletedSound();
+            if (IsServer)
+                PlayBuildableConstructionCompletedSoundClientRpc();
+        }
+
+        protected override void CallRpc_ProgressControllerVisible(bool isEnabled)
+        {
+            OnProgressControllerVisibleClientRpc(isEnabled);
+        }
+
+        protected override void OnBuildableStateValueChanged(PvPBuildableState state)
+        {
+            OnBuildableStateValueChangedClientRpc(state);
+        }
+
+        protected override void CallRpc_ClickedRepairButton()
+        {
+            PvP_RepairableButtonClickedServerRpc();
+        }
+        protected override void CallRpc_SyncFaction(PvPFaction faction)
+        {
+            OnSyncFationClientRpc(faction);
+        }
+
+
+        protected override void OnDestroyedEvent()
+        {
+            if (IsClient)
+                base.OnDestroyedEvent();
+            if (IsServer)
+                OnDestroyedEventClientRpc();
+        }
+
+
+        private void LateUpdate()
+        {
+            if (IsServer)
+            {
+                if (PvP_BuildProgress.Value != BuildProgress)
+                    PvP_BuildProgress.Value = BuildProgress;
+            }
+            if (IsClient)
+            {
+                BuildProgress = PvP_BuildProgress.Value;
+            }
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            if (IsServer)
+                pvp_Health.Value = maxHealth;
+        }
+
+        [ClientRpc]
+        private void OnShareIsDroneConsumerFocusableValueWithClientRpc(bool isFocusable)
+        {
+            IsDroneConsumerFocusable_PvPClient = isFocusable;
+        }
+
+        [ServerRpc]
+        private void OnToggleDroneConsumerFocusCommandExecuteServerRpc()
+        {
+            CallRpc_ToggleDroneConsumerFocusCommandExecute();
+        }
+
+        [ClientRpc]
+        private void PlayPlacementSoundClientRpc()
+        {
+            PlayPlacementSound();
+        }
+
+        [ServerRpc(RequireOwnership = true)]
+        private void OnDestroyMeServerRpc()
+        {
+            DestroyMe();
+        }
+
+        [ClientRpc]
+        private void OnPlayDeathSoundClientRpc()
+        {
+            CallRpc_PlayDeathSound();
+        }
+
+        [ClientRpc]
+        private void PlayBuildableConstructionCompletedSoundClientRpc()
+        {
+            PlayBuildableConstructionCompletedSound();
+        }
+
+        [ClientRpc]
+        private void OnBuildableCompletedClientRpc()
+        {
+            OnBuildableCompleted();
+        }
+
+        [ClientRpc]
+        private void OnSetPositionClientRpc(Vector3 pos)
+        {
+            Position = pos;
+        }
+
+        [ClientRpc]
+        private void OnSetRotationClientRpc(Quaternion rotation)
+        {
+            Rotation = rotation;
+        }
+
+        [ClientRpc]
+        private void OnSetHealthbarOffsetClientRpc(Vector2 offset)
+        {
+            HealthBar.Offset = offset;
+        }
+
+        [ClientRpc]
+        private void OnValueChangedIsEnabledRendersClientRpc(bool isEnabled)
+        {
+            OnValueChangedIsEnableRenderes(isEnabled);
+        }
+
+        [ClientRpc]
+        private void OnProgressControllerVisibleClientRpc(bool isEnabled)
+        {
+            _buildableProgress.gameObject.SetActive(isEnabled);
+        }
+
+        [ClientRpc]
+        protected void OnBuildableStateValueChangedClientRpc(PvPBuildableState state)
+        {
+            BuildableState = state;
+        }
+
+        [ServerRpc(RequireOwnership = true)]
+        private void PvP_RepairableButtonClickedServerRpc()
+        {
+            IPvPDroneConsumer repairDroneConsumer = ParentCruiser.RepairManager.GetDroneConsumer(this);
+            ParentCruiser.DroneFocuser.ToggleDroneConsumerFocus(repairDroneConsumer, isTriggeredByPlayer: true);
+        }
+
+        [ClientRpc]
+        private void OnSyncFationClientRpc(PvPFaction faction)
+        {
+            Faction = faction;
+        }
+
+        [ClientRpc]
+        private void OnEnableBoosterGlowClientRpc(bool enabled)
+        {
+          //  _boosterGlow.gameObject.SetActive(enabled);
+        }
+
+        [ClientRpc]
+        private void OnDestroyedEventClientRpc()
+        {
+            OnDestroyedEvent();
         }
     }
 }
