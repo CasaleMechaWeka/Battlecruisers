6d171be44033213405c4854f688cff668222c0f3
Sava Spasic <sava@mechaweka.co.nz>
2023-08-15 16:47:34 +0100

    RankImage is displayed in RightTop PlayerInfoPanel

--- Assets/Scripts/UI/ScreensScene/BattleHubScreen/PlayerInfoPanelController.cs
+++ Assets/Scripts/UI/ScreensScene/BattleHubScreen/PlayerInfoPanelController.cs
@@ -1,6 +1,8 @@
 using BattleCruisers.Data;
+using BattleCruisers.Data.Static;
 using BattleCruisers.UI.ScreensScene.ProfileScreen;
 using BattleCruisers.Utils.Fetchers;
+using BattleCruisers.Utils.Fetchers.Sprites;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,15 +21,34 @@ namespace BattleCruisers.UI.ScreensScene.BattleHubScreen
         {
             Instance = this;
         }
-        public void UpdateInfo(IDataProvider dataProvider, IPrefabFactory prefabFactory)
+        public async void UpdateInfo(IDataProvider dataProvider, IPrefabFactory prefabFactory)
         {
             CaptainExo captain = prefabFactory.GetCaptainExo(dataProvider.GameModel.PlayerLoadout.CurrentCaptain);
-            _rankImage.sprite = captain.CaptainExoImage;
+
             _captainImage.sprite = captain.CaptainExoImage;
             _selectedCaptainImage.sprite = captain.CaptainExoImage;
             _coins.text = dataProvider.GameModel.Coins.ToString();
             _credits.text = dataProvider.GameModel.Credits.ToString(); 
             _playerName.text = dataProvider.GameModel.PlayerName;
+
+            int rank = CalculateRank(dataProvider.GameModel.LifetimeDestructionScore);
+            SpriteFetcher fetcher = new SpriteFetcher();
+            _rankImage.sprite = (await fetcher.GetSpriteAsync("Assets/Resources_moved/Sprites/UI/ScreensScene/DestructionScore/" + StaticPrefabKeys.Ranks.AllRanks[rank].RankImage + ".png")).Sprite;
+        }
+
+        private int CalculateRank(long score)
+        {
+
+            for (int i = 0; i <= StaticPrefabKeys.Ranks.AllRanks.Count; i++)
+            {
+                long x = 2500 + 2500 * i * i;
+                //Debug.Log(x);
+                if (score < x)
+                {
+                    return i;
+                }
+            }
+            return StaticPrefabKeys.Ranks.AllRanks.Count;
         }
     }
 }
