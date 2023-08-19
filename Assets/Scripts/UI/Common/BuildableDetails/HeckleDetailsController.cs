cd0cc24cfa92c55d46398278a5ac7f6d1cdc99b6
Sava Spasic <sava@mechaweka.co.nz>
2023-08-19 10:54:03 +0100

    chore

---
+++ Assets/Scripts/UI/Common/BuildableDetails/HeckleDetailsController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BattleCruisers.UI.Common.BuildableDetails
+{
+    public class HeckleDetailsController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
