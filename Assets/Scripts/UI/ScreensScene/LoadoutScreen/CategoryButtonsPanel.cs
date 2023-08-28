9be28aae583ce573746240747c71847b45a4a6e5
Erik Langer <erik@mechaweka.co.nz>
2023-08-28 19:15:27 +0200

    fix: loadout swiping bug fixed!

--- Assets/Scripts/UI/ScreensScene/LoadoutScreen/CategoryButtonsPanel.cs
+++ Assets/Scripts/UI/ScreensScene/LoadoutScreen/CategoryButtonsPanel.cs
@@ -1,4 +1,4 @@
-﻿using BattleCruisers.Data.Models;
+using BattleCruisers.Data.Models;
 using BattleCruisers.UI.ScreensScene.LoadoutScreen.Items;
 using BattleCruisers.UI.Sound.Players;
 using BattleCruisers.Utils;
@@ -53,23 +53,23 @@ namespace BattleCruisers.UI.ScreensScene.LoadoutScreen
             index = Mathf.RoundToInt(index);
             if (index != previuosIndex)
             {
-                if (index <= .5f)
+                if (index <= .5f && _buttons[0].isActiveAndEnabled)
                     _buttons[0].OnClickedAction();
-                else if (index == 1f)
+                else if (index == 1f && _buttons[1].isActiveAndEnabled)
                     _buttons[1].OnClickedAction();
-                else if (index == 2f)
+                else if (index == 2f && _buttons[2].isActiveAndEnabled)
                     _buttons[2].OnClickedAction();
-                else if (index == 3f)
+                else if (index == 3f && _buttons[3].isActiveAndEnabled)
                     _buttons[3].OnClickedAction();
-                else if (index == 4f)
+                else if (index == 4f && _buttons[4].isActiveAndEnabled)
                     _buttons[4].OnClickedAction();
-                else if (index == 5f)
+                else if (index == 5f && _buttons[5].isActiveAndEnabled)
                     _buttons[5].OnClickedAction();
-                else if (index == 6f)
+                else if (index == 6f && _buttons[6].isActiveAndEnabled)
                     _buttons[6].OnClickedAction();
-                else if (index == 7f)
+                else if (index == 7f && _buttons[7].isActiveAndEnabled)
                     _buttons[7].OnClickedAction();
-                else if (index == 8f)
+                else if (index == 8f && _buttons[8].isActiveAndEnabled)
                     transform.FindNamedComponent<HeckleCategoryButton>("HeckleButton").OnClickedAction();
                 previuosIndex = index;
             }
