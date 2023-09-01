43ae7a3d29341cee0f4c0c19d7ce6c8249b2ebef
Sava Spasic <sava@mechaweka.co.nz>
2023-09-01 19:56:20 +0100

    pvp: mortar

--- Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/PvPMortar.cs
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/PvPMortar.cs
@@ -90,7 +90,10 @@ namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Builda
         protected override void CallRpc_PlayDeathSound()
         {
             if (IsServer)
+            {
                 OnPlayDeathSoundClientRpc();
+                base.CallRpc_PlayDeathSound();
+            }
             else
                 base.CallRpc_PlayDeathSound();
         }
@@ -130,9 +133,11 @@ namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Builda
 
         protected override void OnDestroyedEvent()
         {
-
             if (IsServer)
+            {
                 OnDestroyedEventClientRpc();
+                base.OnDestroyedEvent();
+            }
             else
                 base.OnDestroyedEvent();
         }
@@ -161,7 +166,8 @@ namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Builda
         [ClientRpc]
         private void OnValueChangedIsEnabledRendersClientRpc(bool isEnabled)
         {
-            OnValueChangedIsEnableRenderes(isEnabled);
+            if (!IsHost)
+                OnValueChangedIsEnableRenderes(isEnabled);
         }
         [ClientRpc]
         private void OnSetHealthbarOffsetClientRpc(Vector2 offset)
@@ -200,14 +206,16 @@ namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Builda
         [ClientRpc]
         private void OnBuildableCompletedClientRpc()
         {
-            OnBuildableCompleted();
+            if (!IsHost)
+                OnBuildableCompleted();
         }
 
 
         [ClientRpc]
         private void PlayPlacementSoundClientRpc()
         {
-            PlayPlacementSound();
+            if (!IsHost)
+                PlayPlacementSound();
         }
 
 
@@ -221,14 +229,16 @@ namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Builda
         [ClientRpc]
         private void OnPlayDeathSoundClientRpc()
         {
-            CallRpc_PlayDeathSound();
+            if (!IsHost)
+                CallRpc_PlayDeathSound();
         }
 
 
         [ClientRpc]
         private void PlayBuildableConstructionCompletedSoundClientRpc()
         {
-            PlayBuildableConstructionCompletedSound();
+            if (!IsHost)
+                PlayBuildableConstructionCompletedSound();
         }
 
         [ClientRpc]
@@ -261,7 +271,8 @@ namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Builda
         [ClientRpc]
         private void OnDestroyedEventClientRpc()
         {
-            OnDestroyedEvent();
+            if (!IsHost)
+                OnDestroyedEvent();
         }
     }
 }
