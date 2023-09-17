14aed9311192ccaca940b8e6f50d7d39fbc37623
Erik Langer <erik@mechaweka.co.nz>
2023-09-17 13:51:14 +0200

    Adjusted Damage to Star Converters;

--- Assets/Scripts/Utils/Categorisation/AntiCruiserDamageToStarsConverter.cs
+++ Assets/Scripts/Utils/Categorisation/AntiCruiserDamageToStarsConverter.cs
@@ -5,12 +5,27 @@
         private static readonly float[] CATEGORY_THRESHOLDS =
         {
             1,
-            //30, Artillery
-            34, // Rocket launcher
-            50, // Railgun
-            //60, Deathstar
-            90, // Broadsides
-            200 // Nuke
+                    // Bomber:      8.3 DPS
+                    // MissilePod:  12.5 DPS
+            15,
+                    // SteamCopter: 17.8 DPS
+                    // Frigate:     24 DPS (Mortar)
+            30,
+                    // Artillery:   30 DPS
+                    // MLRS:        31.5 DPS
+                    // LasCannon:   33.7 DPS
+                    // IonCannon:   35.7 DPS
+                    // Destroyer:   38 DPS (combined)
+                    // AttackBoat:  40 DPS
+            50,
+
+                    // Nova:        50 DPS
+                    // Orbital:     52.2 DPS
+                    // MissileRev:  68 DPS
+            70
+                    // Mann o' War: 88.9 DPS
+                    // Broadsides:  90 DPS
+
         };
 
         public AntiCruiserDamageToStarsConverter() : base(CATEGORY_THRESHOLDS)
