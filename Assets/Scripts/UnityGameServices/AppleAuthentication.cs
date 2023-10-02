ecb2d3ead3463eb6e9a79588a72bce63542dba30
Martin Neal <martin@mechaweka.co.nz>
2023-10-02 13:39:08 +1300

    Adds Apple auth plugin to project

---
+++ Assets/Scripts/UnityGameServices/AppleAuthentication.cs
@@ -0,0 +1,13 @@
+#if PLATFORM_IOS
+using System.Text;
+using UnityEngine;
+using AppleAuth;
+using AppleAuth.Enums;
+using AppleAuth.Interfaces;
+using AppleAuth.Native;
+
+public class AppleAuthentication : MonoBehaviour
+{
+
+}
+#endif
