cd8ca842ffb2dd99d432af90ee5c18ba16d21ba0
Sava Spasic <sava@mechaweka.co.nz>
2023-10-31 14:36:32 +0100

    chore

--- Assets/Scripts/UI/ScreensScene/ShopScreens/VariantsContainer.cs
+++ Assets/Scripts/UI/ScreensScene/ShopScreens/VariantsContainer.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace BattleCruisers.UI.ScreensScene
 {
     public class VariantsContainer : MonoBehaviour
     {
+        public Image UIFriendlyImage;
+        public Text PerkName;
+        public Text BuildableName;
 
     }
 }
