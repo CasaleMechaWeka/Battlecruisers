8df5a3ee2e5b1e3ae19fba0a40f66d09619f5c2e
Anuj Dyamanna <anuj@mechaweka.co.nz>
2023-11-05 17:55:03 +1300

    Skirmish AI can now use any of the Bodykits (000-035) if they roll the 20% chance to use one!

--- Assets/Scripts/Scenes/ScreensSceneGod.cs
+++ Assets/Scripts/Scenes/ScreensSceneGod.cs
@@ -664,7 +664,7 @@ namespace BattleCruisers.Scenes
             if (hullType != HullType.None)
             {
                 List<int> bodykits = new List<int>();
-                for (int i = 0; i < 12 /*_applicationModel.DataProvider.GameModel.Bodykits.Count*/; i++)
+                for (int i = 0; i < _applicationModel.DataProvider.GameModel.Bodykits.Count; i++)
                 {
                     if (_prefabFactory.GetBodykit(StaticPrefabKeys.BodyKits.AllKeys[i]).cruiserType == hullType)
                     {
