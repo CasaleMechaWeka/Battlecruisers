0cbaf874f2d56ddf4c817791cafc812ab3c0c6da
Erik <erik@mechaweka.co.nz>
2023-12-15 21:05:17 +0100

    Bugfix/Loc: text localisation works now in IAP confirmation screen

--- Assets/Scripts/UI/ScreensScene/BattleHubScreen/BlackMarketIAPConfirmModal.cs
+++ Assets/Scripts/UI/ScreensScene/BattleHubScreen/BlackMarketIAPConfirmModal.cs
@@ -15,6 +15,7 @@ using BattleCruisers.Utils.PlatformAbstractions.UI;
 using BattleCruisers.Scenes;
 using Unity.Services.Authentication;
 using BattleCruisers.UI.ScreensScene.BattleHubScreen;
+using BattleCruisers.Utils.Localisation;
 
 public class BlackMarketIAPConfirmModal : MonoBehaviour
 {
@@ -29,6 +30,8 @@ public class BlackMarketIAPConfirmModal : MonoBehaviour
     private ISingleSoundPlayer _soundPlayer;
 
     private IIAPData _currentIAPData;
+
+    private ILocTable screensSceneStrings;
     public void Initiaize(IDataProvider dataProvider, IPrefabFactory prefabFactory, ISingleSoundPlayer soundPlayer)
     {
         Helper.AssertIsNotNull(dataProvider, prefabFactory, soundPlayer);
@@ -42,11 +45,12 @@ public class BlackMarketIAPConfirmModal : MonoBehaviour
         description.text = "";
         price.text = "";
         coinPack.sprite = null;
+        screensSceneStrings = LandingSceneGod.Instance.screenSceneStrings;
     }
 
     private async void Purchase()
     {
-        if(await LandingSceneGod.CheckForInternetConnection() && AuthenticationService.Instance.IsSignedIn)
+        if (await LandingSceneGod.CheckForInternetConnection() && AuthenticationService.Instance.IsSignedIn)
         {
             Close();
             switch (_currentIAPData.IAPNameKeyBase)
@@ -68,11 +72,11 @@ public class BlackMarketIAPConfirmModal : MonoBehaviour
         }
         else
         {
-            if(await LandingSceneGod.CheckForInternetConnection())
+            if (await LandingSceneGod.CheckForInternetConnection())
             {
                 ScreensSceneGod.Instance.messageBox.ShowMessage("You have no Internet Connection!");
             }
-            else if(AuthenticationService.Instance.IsSignedIn)
+            else if (AuthenticationService.Instance.IsSignedIn)
             {
                 ScreensSceneGod.Instance.messageBox.ShowMessage("You are not signed in.");
             }
@@ -81,33 +85,41 @@ public class BlackMarketIAPConfirmModal : MonoBehaviour
 
     async void OnEnable()
     {
-        
+
         Product product = null;
         string spritePath = "Assets/Resources_moved/Sprites/UI/IAP/";
+
         switch (_currentIAPData.IAPNameKeyBase)
         {
             case "Coins100Name":
                 product = IAPManager.instance.storeController.products.WithID(IAPManager.small_coin_pack);
                 spritePath += "Coins100Pack.png";
+                description.text = screensSceneStrings.GetString("Coins100Description");
+                title.text = screensSceneStrings.GetString("Coins100Name");
                 break;
             case "Coins500Name":
                 product = IAPManager.instance.storeController.products.WithID(IAPManager.medium_coin_pack);
                 spritePath += "Coins500Pack.png";
+                description.text = screensSceneStrings.GetString("Coins500Description");
+                title.text = screensSceneStrings.GetString("Coins500Name");
                 break;
             case "Coins1000Name":
                 product = IAPManager.instance.storeController.products.WithID(IAPManager.large_coin_pack);
                 spritePath += "Coins1000Pack.png";
+                description.text = screensSceneStrings.GetString("Coins1000Description");
+                title.text = screensSceneStrings.GetString("Coins1000Name");
                 break;
             case "Coins5000Name":
                 product = IAPManager.instance.storeController.products.WithID(IAPManager.extralarge_coin_pack);
                 spritePath += "Coins5000Pack.png";
+                description.text = screensSceneStrings.GetString("Coins5000Description");
+                title.text = screensSceneStrings.GetString("Coins5000Name");
                 break;
         }
 
         if (product != null)
         {
             price.text = product.metadata.localizedPriceString;
-            title.text = product.metadata.localizedTitle;
 #if UNITY_ANDROID || UNITY_EDITOR
             // On Android, the Play store appends the app name in parentheses to the defined name value.
             // For example, if the product name is "1,000 Gems" for an app named "Gem Collector", it will return "1,000 Gems (Gem Collector)".
@@ -119,7 +131,7 @@ public class BlackMarketIAPConfirmModal : MonoBehaviour
                 title.text = title.text.Substring(0, lastParen - 1).Trim();
             }
 #endif
-            description.text = product.metadata.localizedDescription;
+
             SpriteFetcher spriteFetcher = new SpriteFetcher();
             ISpriteWrapper spriteWrapper = await spriteFetcher.GetSpriteAsync(spritePath);
             coinPack.sprite = spriteWrapper.Sprite;
