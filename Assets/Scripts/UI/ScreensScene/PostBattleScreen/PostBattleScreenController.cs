bb51e82e22ff742226dad67048096b16eb585842
Erik <erik@mechaweka.co.nz>
2024-03-18 03:13:28 +0100

    Removed unnecessary Debug.Log

--- Assets/Scripts/UI/ScreensScene/PostBattleScreen/PostBattleScreenController.cs
+++ Assets/Scripts/UI/ScreensScene/PostBattleScreen/PostBattleScreenController.cs
@@ -233,7 +233,6 @@ namespace BattleCruisers.UI.ScreensScene.PostBattleScreen
                 ITrashTalkData levelTrashTalkData = await trashTalkList.GetTrashTalkAsync(BattleResult.LevelNum);
                 levelName.Initialise(BattleResult.LevelNum, levelTrashTalkData);
                 unlockedItemSection.Initialise();
-                Debug.Log(desiredBehaviour);
                 if (desiredBehaviour == PostBattleScreenBehaviour.Defeat
                     || !BattleResult.WasVictory)
                 {
