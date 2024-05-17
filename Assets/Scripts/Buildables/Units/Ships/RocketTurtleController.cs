27bdd16db80dd0fe5f69fc09d12c3bae266f3cff
J-Lucas <lucas@mechaweka.co.nz>
2024-05-17 12:58:41 -0500

    Revert "Fixed RocketTurtle sprite during building pvE"
    This reverts commit 4610b9bfebe414676689134b2ecc6aef967a8d96.

--- Assets/Scripts/Buildables/Units/Ships/RocketTurtleController.cs
+++ Assets/Scripts/Buildables/Units/Ships/RocketTurtleController.cs
@@ -16,8 +16,6 @@ namespace BattleCruisers.Buildables.Units.Ships
 
         public override float OptimalArmamentRangeInM => _missileLauncher.RangeInM;
 
-        public GameObject BaseSprite;
-
         protected override IList<IBarrelWrapper> GetTurrets()
         {
             IList<IBarrelWrapper> turrets = new List<IBarrelWrapper>();
@@ -50,7 +48,6 @@ namespace BattleCruisers.Buildables.Units.Ships
             base.OnBuildableCompleted();
             _missileLauncher.ApplyVariantStats(this);
             _shieldController.gameObject.SetActive(true);
-            BaseSprite.SetActive(true);
         }
 
         public override void StopMoving()
