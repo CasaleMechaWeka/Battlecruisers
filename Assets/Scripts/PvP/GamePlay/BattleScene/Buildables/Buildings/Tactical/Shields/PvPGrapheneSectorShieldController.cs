844e1f8d5c1867f5c74f245ada6103b6ff546495
Peter Casale <casalepeter@gmail.com>
2024-05-24 18:14:47 +1200

    GrapheneBarrier scripts

--- Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Tactical/Shields/PvPGrapheneSectorShieldController.cs
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Tactical/Shields/PvPGrapheneSectorShieldController.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Data.Static;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.UI.BattleScene.ProgressBars;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.UI.Sound.Players;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils.Timers;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils.PlatformAbstractions.Time;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Assertions;
+using BattleCruisers.Utils.Localisation;
+using System.Collections.Generic;
+using Unity.Netcode;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.UI.Sound;
+using BattleCruisers.Data.Static;
+using BattleCruisers.UI.ScreensScene.ProfileScreen;
 
-public class PvPGrapheneSectorShieldController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Tactical.Shields {
 
-    // Update is called once per frame
-    void Update()
+    public class PvPGrapheneSectorShieldController : PvPSectorShieldController
     {
-        
+            public UnityEvent onShieldDepleted;
+            public UnityEvent onShieldDamaged;
+
+
+            protected override void OnHealthGone()
+            {
+                base.OnHealthGone();
+                onShieldDepleted.Invoke();
+            }
+
+            protected override void OnTakeDamage()
+            {
+                base.OnTakeDamage();
+                onShieldDamaged.Invoke();
+            }
+
+
+            public void SetShieldHealth(float newHealth)
+            {
+                _healthTracker.SetHealth(newHealth);
+            }
     }
 }
