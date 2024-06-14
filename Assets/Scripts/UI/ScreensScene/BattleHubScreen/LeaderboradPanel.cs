37b6ddb7953350a05ee776a6ffe6c61c952277d7
J-Lucas <lucas@mechaweka.co.nz>
2024-06-13 22:19:28 -0500

    Little details fixed

--- Assets/Scripts/UI/ScreensScene/BattleHubScreen/LeaderboradPanel.cs
+++ Assets/Scripts/UI/ScreensScene/BattleHubScreen/LeaderboradPanel.cs
@@ -1,10 +1,7 @@
-using BattleCruisers.Data.Models.PrefabKeys;
 using BattleCruisers.Data.Static;
 using BattleCruisers.UI.ScreensScene.ProfileScreen;
 using BattleCruisers.UI.Sound.Players;
 using BattleCruisers.Utils.Fetchers;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -24,10 +21,8 @@ namespace BattleCruisers.UI.ScreensScene.BattleHubScreen
             int placenumber,
             string captain)
         {
-            /*            CaptainNameToKey nameToKey = new CaptainNameToKey(StaticPrefabKeys.CaptainExos.AllKeys, prefabFactory);
-                        IPrefabKey key = nameToKey.GetKey(captain);*/
             string str_index = captain.ToUpper().Replace("CAPTAINEXO", "");
-            int index = 10;
+            int index = 0;
             int.TryParse(str_index, out index);
             CaptainExo captainexo = prefabFactory.GetCaptainExo(StaticPrefabKeys.CaptainExos.GetCaptainExoKey(index));
             Text name = PlayerName.gameObject.GetComponent<Text>();
