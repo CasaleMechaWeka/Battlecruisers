3c5d1c7b1e346b20f0d704894627716c4f84ca14
Erik <erik@mechaweka.co.nz>
2024-07-11 15:20:22 +0200

    Fixed Serializer loading wrong stuff on CloudLoad

--- Assets/Scripts/Data/Serialization/Serializer.cs
+++ Assets/Scripts/Data/Serialization/Serializer.cs
@@ -282,7 +282,7 @@ namespace BattleCruisers.Data.Serialization
                         Debug.Log(gameModelData);
 
                         //saveModel.AssignSaveToGameModel(game); <-- Moved to CloudLoad() method in DataProvider
-                        if (saveModel._lifetimeDestructionScore >= game.LifetimeDestructionScore)
+                        if (saveModel._lifetimeDestructionScore > game.LifetimeDestructionScore)
                         {
                             Debug.Log("Cloud save up to date");
                             return saveModel;
