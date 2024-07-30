197065d848bb63628cb5f8e35d3ff336105e1e3b
Erik <erik@mechaweka.co.nz>
2024-07-30 17:19:35 +0200

    Bugfix: UltraCIWS now also gets the Ultra build rate boost

---
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/PvPUltraCIWS.cs
@@ -0,0 +1,17 @@
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Boost;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Boost.GlobalProviders;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Turrets;
+
+public class PvPUltraCIWS : PvPAntiAirTurret
+{
+    // Start is called before the first frame update
+    protected override void AddBuildRateBoostProviders(
+        IPvPGlobalBoostProviders globalBoostProviders,
+        IList<ObservableCollection<IPvPBoostProvider>> buildRateBoostProvidersList)
+    {
+        base.AddBuildRateBoostProviders(globalBoostProviders, buildRateBoostProvidersList);
+        buildRateBoostProvidersList.Add(_cruiserSpecificFactories.GlobalBoostProviders.BuildingBuildRate.UltrasProviders);
+    }
+}
