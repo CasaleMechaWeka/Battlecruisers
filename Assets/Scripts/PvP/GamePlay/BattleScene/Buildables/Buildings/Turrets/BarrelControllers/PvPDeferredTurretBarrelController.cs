537b64828f39005004826fcbebd5886e9ddf1af1
Erik <erik@mechaweka.co.nz>
2024-08-13 22:26:59 +0200

    Removed incorrect summary

--- Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/BarrelControllers/PvPDeferredTurretBarrelController.cs
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/BarrelControllers/PvPDeferredTurretBarrelController.cs
@@ -4,11 +4,6 @@ using UnityEngine.Assertions;
 
 namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Turrets.BarrelControllers
 {
-    /// <summary>
-    /// The firing animation is per turret barrel, instead of for the whole
-    /// turret building (ie, no animation outside of the turret barrel).
-    /// Hence need a custom way of grabbing this animation.
-    /// </summary>
     public class PvPDeferredTurretBarrelController : PvPShellTurretBarrelController
     {
         public float delayInS;
