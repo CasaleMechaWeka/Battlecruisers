8c6b7b3a2e94e16a604699d5c38990a8473eec0b
J-Lucas <lucas@mechaweka.co.nz>
2024-08-13 20:49:00 -0500

    Revert "Hopefully fixed stuck in loading assets"
    This reverts commit 5f28673bfbaed00e8c950b143c512ad5417b3616.

--- Assets/Scripts/PvP/ScreensController/MatchmakingScreenController.cs
+++ Assets/Scripts/PvP/ScreensController/MatchmakingScreenController.cs
@@ -322,7 +322,6 @@ namespace BattleCruisers.UI.ScreensScene.Multiplay.ArenaScreen
                     isProcessing = true;
 
                     await iLoadingAssets();
-                    isProcessing = false;
                 }
                 else
                 {
@@ -332,10 +331,6 @@ namespace BattleCruisers.UI.ScreensScene.Multiplay.ArenaScreen
             catch (Exception e)
             {
                 Debug.LogError($"An error occurred: {e.Message}");
-                isProcessing = false;
-            }
-            finally 
-            {
                 isProcessing = false;
             }
             /*            if(status == MMStatus.LOOKING_VICTIM && m_TimeLimitLookingVictim.CanCall)
