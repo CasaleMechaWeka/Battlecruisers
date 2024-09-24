229fbc74cadebeb38cec467331d1fdf89b1431ef
casalex <casalepeter@gmail.com>
2024-09-24 14:58:07 +1200

    Sledgehammer assembled and cannon animated

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/SledgehammerCannon.cs
@@ -0,0 +1,23 @@
+using BattleCruisers.Buildables.Boost;
+using BattleCruisers.Buildables.Boost.GlobalProviders;
+using BattleCruisers.Data.Static;
+using BattleCruisers.UI.Sound;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets
+{
+    public class SledgehammerCannon : OffenseTurret
+    {
+        protected override ISoundKey FiringSound => SoundKeys.Firing.Artillery;
+        protected override PrioritisedSoundKey ConstructionCompletedSoundKey => PrioritisedSoundKeys.Completed.Ultra;
+
+        protected override void AddBuildRateBoostProviders(
+            IGlobalBoostProviders globalBoostProviders,
+            IList<ObservableCollection<IBoostProvider>> buildRateBoostProvidersList)
+        {
+            base.AddBuildRateBoostProviders(globalBoostProviders, buildRateBoostProvidersList);
+            buildRateBoostProvidersList.Add(_cruiserSpecificFactories.GlobalBoostProviders.BuildingBuildRate.UltrasProviders);
+        }
+    }
+}
