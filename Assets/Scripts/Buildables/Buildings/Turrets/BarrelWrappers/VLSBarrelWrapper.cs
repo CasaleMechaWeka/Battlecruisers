d0526f85c618906467f56e997bf5c2035dbf881e
casalex <casalepeter@gmail.com>
2024-10-04 02:39:21 +1300

    VLS art imported, test scene created, BlastVLS works but needs polish and anim

---
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/VLSBarrelWrapper.cs
@@ -0,0 +1,16 @@
+﻿using BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers;
+using BattleCruisers.Utils;
+
+namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
+{
+    public class VLSBarrelWrapper : StaticBarrelWrapper
+    {
+        protected override float DesiredAngleInDegrees => 90;
+
+        protected override void InitialiseBarrelController(BarrelController barrel, IBarrelControllerArgs args)
+        {
+			RocketBarrelController rocketBarrel = barrel.Parse<RocketBarrelController>();
+            rocketBarrel.InitialiseAsync(args);
+        }
+    }
+}
