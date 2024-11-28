17c533b35e8e9456d2070c4eb8eebe73bb1ce2c8
casalex <casalepeter@gmail.com>
2024-11-28 15:25:02 +1300

    BlastVLS and FirecrackerVLS ready for PvE, Firecracker missile FX embetterened

--- Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/VLSBarrelWrapper.cs
+++ Assets/Scripts/Buildables/Buildings/Turrets/BarrelWrappers/VLSBarrelWrapper.cs
@@ -1,16 +1,25 @@
 ﻿using BattleCruisers.Buildables.Buildings.Turrets.BarrelControllers;
 using BattleCruisers.Utils;
+using UnityEngine;
 
 namespace BattleCruisers.Buildables.Buildings.Turrets.BarrelWrappers
 {
     public class VLSBarrelWrapper : StaticBarrelWrapper
     {
+        [SerializeField]
+        private BarrelController barrelController;
+
         protected override float DesiredAngleInDegrees => 90;
 
         protected override void InitialiseBarrelController(BarrelController barrel, IBarrelControllerArgs args)
         {
-			RocketBarrelController rocketBarrel = barrel.Parse<RocketBarrelController>();
-            rocketBarrel.InitialiseAsync(args);
+            if (barrelController == null)
+            {
+                Debug.LogError("BarrelController is not assigned in the Inspector.");
+                return;
+            }
+
+            barrelController.InitialiseAsync(args);
         }
     }
 }
