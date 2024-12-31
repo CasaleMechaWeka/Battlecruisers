541001e49225e29b0af6f279ef09afba8c58fe6f
Erik Langer <erik@mechaweka.co.nz>
2024-12-31 15:07:46 +0100

    PvP Debloat: Removed Redundant PvPGravityAffectedAngleCalculator

--- Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/AngleCalculators/PvPArtilleryAngleCalculator.cs
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/AngleCalculators/PvPArtilleryAngleCalculator.cs
@@ -3,7 +3,7 @@ using BattleCruisers.Projectiles.Stats;
 
 namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Turrets.AngleCalculators
 {
-    public class PvPArtilleryAngleCalculator : PvPGravityAffectedAngleCalculator
+    public class PvPArtilleryAngleCalculator : GravityAffectedAngleCalculator
     {
         // Choose smaller angle, because we want the artillery to fire in a flat arc instead of a high arc.
         protected override bool UseLargerAngle => false;
--- Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/AngleCalculators/PvPMortarAngleCalculator.cs
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/AngleCalculators/PvPMortarAngleCalculator.cs
@@ -3,7 +3,7 @@ using BattleCruisers.Projectiles.Stats;
 
 namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Turrets.AngleCalculators
 {
-    public class PvPMortarAngleCalculator : PvPGravityAffectedAngleCalculator
+    public class PvPMortarAngleCalculator : GravityAffectedAngleCalculator
     {
         // Choose larger angle, because we want the mortar to fire in a high arc instead of a flat arc.
         protected override bool UseLargerAngle => true;
