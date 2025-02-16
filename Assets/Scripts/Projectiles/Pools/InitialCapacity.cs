4427f446c2ba3b072b995fddc45df231842de982
casalex <casalepeter@gmail.com>
2025-02-16 15:15:29 +1300

    MF seems to be fixed, Does 160 damage per missile (2 per volley) every 10 seconds.

--- Assets/Scripts/Projectiles/Pools/InitialCapacity.cs
+++ Assets/Scripts/Projectiles/Pools/InitialCapacity.cs
@@ -2,17 +2,17 @@
 {
     public static class InitialCapacity
     {
-        public const int BULLET = 20;
-        public const int BOMB = 10;
-        public const int MISSILE_SMALL = 6;
-        public const int MISSILE_MEDIUM = 6;
-        public const int MISSILE_LARGE = 1;
-        public const int MISSILE_SMART = 1;
-        public const int NUKE = 1;
-        public const int ROCKET = 3;
-        public const int SHELL_SMALL = 5;
-        public const int SHELL_LARGE = 2;
-        public const int NOVA_SHELL = 3;
+        public const int BULLET = 50;
+        public const int BOMB = 20;
+        public const int MISSILE_SMALL = 16;
+        public const int MISSILE_MEDIUM = 16;
+        public const int MISSILE_LARGE = 16;
+        public const int MISSILE_SMART = 16;
+        public const int NUKE = 2;
+        public const int ROCKET = 16;
+        public const int SHELL_SMALL = 25;
+        public const int SHELL_LARGE = 12;
+        public const int NOVA_SHELL = 13;
        
     }
 }
