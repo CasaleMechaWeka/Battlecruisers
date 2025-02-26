424da92235980174995ea9881375d07031af9660
Erik <erik@mechaweka.co.nz>
2025-02-26 02:21:25 +0100

    Removed Redundant IPvPNukeStats

--- Assets/Scripts/PvP/GamePlay/BattleScene/Projectiles/Stats/PvPNukeProjectileStats.cs
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Projectiles/Stats/PvPNukeProjectileStats.cs
@@ -1,8 +1,9 @@
+using BattleCruisers.Projectiles.Stats;
 using UnityEngine;
 
 namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Projectiles.Stats
 {
-    public class PvPNukeProjectileStats : PvPCruisingProjectileStats, IPvPNukeStats
+    public class PvPNukeProjectileStats : PvPCruisingProjectileStats, INukeStats
     {
         public Vector2 InitialVelocity => new Vector2(0, 0);
     }
