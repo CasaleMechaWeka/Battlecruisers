d450b05af9b1ba106127b94b93a841ad97cf1b2d
Erik <erik@mechaweka.co.nz>
2025-02-27 16:50:15 +0100

    Removed Redundant IPvPTargetPositionValidatorFactory

--- Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/PositionValidators/PvPTargetPositionValidatorFactory.cs
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Buildings/Turrets/PositionValidators/PvPTargetPositionValidatorFactory.cs
@@ -2,7 +2,7 @@ using BattleCruisers.Buildables.Buildings.Turrets.PositionValidators;
 
 namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Turrets.PositionValidators
 {
-    public class PvPTargetPositionValidatorFactory : IPvPTargetPositionValidatorFactory
+    public class PvPTargetPositionValidatorFactory : ITargetPositionValidatorFactory
     {
         public ITargetPositionValidator CreateDummyValidator()
         {
--- Assets/Scripts/PvP/GamePlay/BattleScene/Utils/Factories/PvPTurretFactoryProvider.cs
+++ Assets/Scripts/PvP/GamePlay/BattleScene/Utils/Factories/PvPTurretFactoryProvider.cs
@@ -2,6 +2,7 @@ using BattleCruisers.Buildables.Buildings.Turrets.AccuracyAdjusters;
 using BattleCruisers.Buildables.Buildings.Turrets.AngleCalculators;
 using BattleCruisers.Buildables.Buildings.Turrets.AngleLimiters;
 using BattleCruisers.Buildables.Buildings.Turrets.AttackablePositionFinders;
+using BattleCruisers.Buildables.Buildings.Turrets.PositionValidators;
 using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Turrets.AttackablePositionFinders;
 using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings.Turrets.PositionValidators;
 
@@ -13,7 +14,7 @@ namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils.
         public IAngleCalculatorFactory AngleCalculatorFactory { get; }
         public IAngleLimiterFactory AngleLimiterFactory { get; }
         public IAttackablePositionFinderFactory AttackablePositionFinderFactory { get; }
-        public IPvPTargetPositionValidatorFactory TargetPositionValidatorFactory { get; }
+        public ITargetPositionValidatorFactory TargetPositionValidatorFactory { get; }
 
         public PvPTurretFactoryProvider()
         {
