c5d3ea4dce0cddf77d7e26bb3988fbc3b559d76b
Erik <erik@mechaweka.co.nz>
2025-03-02 12:57:53 +0100

    Removed Redundant IPvPAudioListener

--- Assets/Scripts/PvP/GamePlay/BattleScene/Utils/PlatformAbstractions/Audio/IPvPAudioListener.cs
+++
@@ -1,8 +0,0 @@
-namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Utils.PlatformAbstractions.Audio
-{
-    public interface IPvPAudioListener
-    {
-        void Pause();
-        void Resume();
-    }
-}
