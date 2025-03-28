79bf3ea380b58105388bc338fc6e090d349a81dc
Erik <erik@mechaweka.co.nz>
2025-03-28 15:48:06 +0100

    Removed unused PvP AI scripts

--- Assets/Scripts/PvP/GamePlay/BattleScene/AI/Drones/IPvPBuildingProvider.cs
+++
@@ -1,9 +0,0 @@
-using BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Buildings;
-
-namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.AI.Drones
-{
-    public interface IPvPBuildingProvider
-    {
-        IPvPBuilding Building { get; }
-    }
-}
