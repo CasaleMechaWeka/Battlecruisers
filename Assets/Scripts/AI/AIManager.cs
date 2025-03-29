da8051c212426ac43055c593981433c0667e6ef0
Erik <erik@mechaweka.co.nz>
2025-03-29 21:04:14 +0100

    Removed AIFactory - functionality can trivially be inlined in AIManager

--- Assets/Scripts/AI/AIManager.cs
+++ Assets/Scripts/AI/AIManager.cs
@@ -1,9 +1,15 @@
-﻿using BattleCruisers.AI.BuildOrders;
+﻿using System.Collections.Generic;
+using BattleCruisers.AI.BuildOrders;
+using BattleCruisers.AI.Drones;
+using BattleCruisers.AI.Drones.BuildingMonitors;
 using BattleCruisers.AI.FactoryManagers;
+using BattleCruisers.AI.TaskProducers;
 using BattleCruisers.AI.Tasks;
 using BattleCruisers.AI.ThreatMonitors;
 using BattleCruisers.Cruisers;
+using BattleCruisers.Cruisers.Slots;
 using BattleCruisers.Data;
+using BattleCruisers.Data.Static;
 using BattleCruisers.Data.Static.Strategies.Helper;
 using BattleCruisers.Utils;
 using BattleCruisers.Utils.PlatformAbstractions.Time;
@@ -17,6 +23,13 @@ namespace BattleCruisers.AI
         private readonly ThreatMonitorFactory _threatMonitorFactory;
         private readonly FactoryManagerFactory _factoryManagerFactory;
         private readonly BuildOrderFactory _buildOrderFactory;
+
+        // For spy satellite launcher and shields.  All cruisers have at least 6
+        // deck slots:
+        // + Anti-air: 2
+        // + Anti-ship: 2
+        // + Shields/spy satellite: 2
+        private const int NUM_OF_DECK_SLOTS_TO_RESERVE = 2;
 
         public AIManager(
             IDeferrer deferrer,
@@ -42,9 +55,77 @@ namespace BattleCruisers.AI
             _factoryManagerFactory.CreateAirfactoryManager(levelInfo.AICruiser);
 
             ITaskFactory taskFactory = new TaskFactory(levelInfo.AICruiser, _deferrer);
-            AIFactory aiFactory = new AIFactory(_buildOrderFactory, taskFactory, _threatMonitorFactory);
 
-            return aiFactory.CreateAdaptiveAI(levelInfo);
+
+            ICruiserController aiCruiser = levelInfo.AICruiser;
+            TaskList tasks = new TaskList();
+            IList<IManagedDisposable> taskProducers = new List<IManagedDisposable>();
+
+            // Base build order, main strategy
+            IDynamicBuildOrder advancedBuildOrder = _buildOrderFactory.CreateAdaptiveBuildOrder(levelInfo);
+            taskProducers.Add(new BasicTaskProducer(tasks, aiCruiser, taskFactory, advancedBuildOrder));
+
+            // Anti air
+            IDynamicBuildOrder antiAirBuildOrder = _buildOrderFactory.CreateAntiAirBuildOrder(levelInfo);
+            BaseThreatMonitor airThreatMonitor = _threatMonitorFactory.CreateDelayedThreatMonitor(_threatMonitorFactory.CreateAirThreatMonitor());
+            int maxNumOfDeckSlots = Helper.Half(aiCruiser.SlotAccessor.GetSlotCount(SlotType.Deck) - NUM_OF_DECK_SLOTS_TO_RESERVE, roundUp: true);
+            SlotNumCalculator slotNumCalculator = new SlotNumCalculator(maxNumOfDeckSlots, 0, 2, 4);
+
+            taskProducers.Add(new AntiThreatTaskProducer(tasks, aiCruiser, taskFactory, antiAirBuildOrder, airThreatMonitor, slotNumCalculator));
+
+            // Anti naval
+            IDynamicBuildOrder antiNavalBuildOrder = _buildOrderFactory.CreateAntiNavalBuildOrder(levelInfo);
+            BaseThreatMonitor navalThreatMonitor = _threatMonitorFactory.CreateDelayedThreatMonitor(_threatMonitorFactory.CreateNavalThreatMonitor());
+            // keep the SlotNumCalculator
+            maxNumOfDeckSlots = Helper.Half(aiCruiser.SlotAccessor.GetSlotCount(SlotType.Deck) - NUM_OF_DECK_SLOTS_TO_RESERVE, roundUp: false);
+
+            taskProducers.Add(new AntiThreatTaskProducer(tasks, aiCruiser, taskFactory, antiNavalBuildOrder, navalThreatMonitor, slotNumCalculator));
+
+            // Anti rocket
+            if (_buildOrderFactory.IsAntiRocketBuildOrderAvailable())
+            {
+                BaseThreatMonitor rocketLauncherThreatMonitor = _threatMonitorFactory.CreateRocketThreatMonitor();
+                slotNumCalculator = new SlotNumCalculator(1, 0, 1, 1);
+
+                taskProducers.Add(new AntiThreatTaskProducer(tasks, aiCruiser, taskFactory, _buildOrderFactory.CreateAntiRocketBuildOrder(), rocketLauncherThreatMonitor, slotNumCalculator));
+            }
+
+            // Anti stealth
+            if (_buildOrderFactory.IsAntiStealthBuildOrderAvailable())
+            {
+                BaseThreatMonitor stealthThreatMonitor = _threatMonitorFactory.CreateStealthThreatMonitor();
+                // keep the SlotNumCalculator
+                taskProducers.Add(new AntiThreatTaskProducer(tasks, aiCruiser, taskFactory, _buildOrderFactory.CreateAntiStealthBuildOrder(), stealthThreatMonitor, slotNumCalculator));
+            }
+
+            taskProducers.Add(new ReplaceDestroyedBuildingsTaskProducer(tasks, aiCruiser, taskFactory, StaticData.BuildingKeys));
+
+            return CreateAI(levelInfo.AICruiser, tasks, taskProducers);
+        }
+
+        private IManagedDisposable CreateAI(ICruiserController aiCruiser, TaskList tasks, IList<IManagedDisposable> taskProducers)
+        {
+            TaskConsumer taskConsumer = new TaskConsumer(tasks);
+            DroneConsumerFocusManager focusManager = CreateDroneFocusManager(aiCruiser);
+            return new ArtificialIntelligence(taskConsumer, taskProducers, focusManager);
+        }
+        private DroneConsumerFocusManager CreateDroneFocusManager(ICruiserController aiCruiser)
+        {
+            FactoryAnalyzer factoryAnalyzer
+                = new FactoryAnalyzer(
+                    new FactoriesMonitor(aiCruiser.BuildingMonitor),
+                    new FactoryWastingDronesFilter());
+            InProgressBuildingMonitor inProgressBuildingMonitor = new InProgressBuildingMonitor(aiCruiser);
+            DroneConsumerFocusHelper focusHelper
+                = new DroneConsumerFocusHelper(
+                    aiCruiser.DroneManager,
+                    factoryAnalyzer,
+                    new AffordableInProgressNonFocusedProvider(aiCruiser.DroneManager, inProgressBuildingMonitor));
+            return
+                new DroneConsumerFocusManager(
+                    new ResponsiveStrategy(),
+                    aiCruiser,
+                    focusHelper);
         }
     }
 }
