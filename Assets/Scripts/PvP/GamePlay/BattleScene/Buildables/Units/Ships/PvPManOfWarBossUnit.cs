a72d493c6472a4bebbce6f98d1b8278237ad0d05
Erik <erik@mechaweka.co.nz>
2025-04-05 20:33:14 +0200

    Removed PvManofWarBoss

--- Assets/Scripts/PvP/GamePlay/BattleScene/Buildables/Units/Ships/PvPManOfWarBossUnit.cs
+++
@@ -1,9 +0,0 @@
-using BattleCruisers.Buildables;
-
-namespace BattleCruisers.Network.Multiplay.Matchplay.MultiplayBattleScene.Buildables.Units.Ships
-{
-    public class PvPManOfWarBossUnit : PvPArchonBattleshipController
-    {
-        public override TargetType TargetType => TargetType.Cruiser;
-    }
-}
