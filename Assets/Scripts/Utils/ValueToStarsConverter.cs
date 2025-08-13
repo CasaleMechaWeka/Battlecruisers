35b018d6b0d85fd45b6cea79a8d834eaf52c738b
Erik <erik@mechaweka.co.nz>
2025-08-14 01:43:36 +0200

    Reduced CoastGuard damage: 120 -> 80; adjusted anti ship DPS to stars conversion

--- Assets/Scripts/Utils/ValueToStarsConverter.cs
+++ Assets/Scripts/Utils/ValueToStarsConverter.cs
@@ -21,7 +21,7 @@ namespace BattleCruisers.Utils.Categorisation
             new float[] { 1, 200, 400, 600, 1000 },     // BuildableHealth
             new float[] { 1, 2400, 3600, 4800, 6000 },  // CruiserHealth
             new float[] { 1, 10, 23, 27, 50 },          // AntiAirDamage
-            new float[] { 1, 24, 50, 87, 119 },         // AntiShipDamage
+            new float[] { 1, 20, 40, 60, 80 },          // AntiShipDamage
             new float[] { 1, 15, 30, 50, 70 }           // AntiCruiserDamage
         };
 
